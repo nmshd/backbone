1b8a65dd4750e511477792b7fa3a9177650083f7
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-02-09 15:03:12 +0000

    feat: add storage sanity check for messages

---
+++ Messages.Jobs.SanityCheck/Infrastructure/DataSource/DataSource.cs
@@ -0,0 +1,31 @@
+﻿using Backbone.Modules.Messages.Application.Infrastructure.Persistence;
+using Backbone.Modules.Messages.Domain.Entities;
+using Backbone.Modules.Messages.Domain.Ids;
+using Enmeshed.BuildingBlocks.Application.Abstractions.Infrastructure.Persistence.BlobStorage;
+using Microsoft.EntityFrameworkCore;
+
+namespace Messages.Jobs.SanityCheck.Infrastructure.DataSource
+{
+    public class DataSource : IDataSource
+    {
+        private readonly IBlobStorage _blobstorage;
+        private readonly IMessagesDbContext _dbcontext;
+
+        public DataSource(IBlobStorage blobStorage, IMessagesDbContext dbContext)
+        {
+            _blobstorage = blobStorage;
+            _dbcontext = dbContext;
+        }
+
+        public async Task<IEnumerable<string>> GetBlobIdsAsync(CancellationToken cancellationToken)
+        {
+            var blobIds = await _blobstorage.FindAllAsync();
+            return await blobIds.ToListAsync(cancellationToken);
+        }
+
+        public async Task<IEnumerable<MessageId>> GetDatabaseIdsAsync(CancellationToken cancellationToken)
+        {
+            return await _dbcontext.SetReadOnly<Message>().Select(u => u.Id).ToListAsync(cancellationToken);
+        }
+    }
+}
---
+++ Messages.Jobs.SanityCheck/Infrastructure/Reporter/LogReporter.cs
@@ -0,0 +1,42 @@
+﻿using Backbone.Modules.Messages.Domain.Ids;
+
+namespace Messages.Jobs.SanityCheck.Infrastructure.Reporter
+{
+    public class LogReporter : IReporter
+    {
+        private readonly ILogger<LogReporter> _logger;
+        private readonly List<MessageId> _databaseids;
+        private readonly List<string> _blobids;
+
+        public LogReporter(ILogger<LogReporter> logger)
+        {
+            _logger = logger;
+
+            _databaseids = new List<MessageId>();
+            _blobids = new List<string>();
+        }
+
+        public void Complete()
+        {
+            foreach (var databaseId in _databaseids)
+            {
+                _logger.LogError("no blob found for message id: {databaseId}", databaseId);
+            }
+
+            foreach (var blobId in _blobids)
+            {
+                _logger.LogError("no database entry found for blob id: {blobId}", blobId);
+            }
+        }
+
+        public void ReportOrphanedBlobId(string id)
+        {
+            _blobids.Add(id);
+        }
+
+        public void ReportOrphanedDatabaseId(MessageId id)
+        {
+            _databaseids.Add(id);
+        }
+    }
+}
