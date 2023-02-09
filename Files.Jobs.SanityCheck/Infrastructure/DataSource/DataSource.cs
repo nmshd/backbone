cddd8c8f82f19c920ca25a3bc2a6469d99bbbc31
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-02-09 16:35:28 +0000

    feature: add storage sanity check for files

---
+++ Files.Jobs.SanityCheck/Infrastructure/DataSource/DataSource.cs
@@ -0,0 +1,30 @@
+﻿using Backbone.Modules.Files.Application.Infrastructure.Persistence;
+using Backbone.Modules.Files.Domain.Entities;
+using Enmeshed.BuildingBlocks.Application.Abstractions.Infrastructure.Persistence.BlobStorage;
+using Microsoft.EntityFrameworkCore;
+
+namespace Files.Jobs.SanityCheck.Infrastructure.DataSource
+{
+    public class DataSource : IDataSource
+    {
+        private readonly IBlobStorage _blobstorage;
+        private readonly IFilesDbContext _dbcontext;
+
+        public DataSource(IBlobStorage blobStorage, IFilesDbContext dbContext)
+        {
+            _blobstorage = blobStorage;
+            _dbcontext = dbContext;
+        }
+
+        public async Task<IEnumerable<string>> GetBlobIdsAsync(CancellationToken cancellationToken)
+        {
+            var blobIds = await _blobstorage.FindAllAsync();
+            return await blobIds.ToListAsync(cancellationToken);
+        }
+
+        public async Task<IEnumerable<FileId>> GetDatabaseIdsAsync(CancellationToken cancellationToken)
+        {
+            return await _dbcontext.SetReadOnly<FileMetadata>().Select(u => u.Id).ToListAsync(cancellationToken);
+        }
+    }
+}
---
+++ Files.Jobs.SanityCheck/Infrastructure/Reporter/LogReporter.cs
@@ -0,0 +1,42 @@
+﻿using Backbone.Modules.Files.Domain.Entities;
+
+namespace Files.Jobs.SanityCheck.Infrastructure.Reporter
+{
+    public class LogReporter : IReporter
+    {
+        private readonly ILogger<LogReporter> _logger;
+        private readonly List<FileId> _databaseids;
+        private readonly List<string> _blobids;
+
+        public LogReporter(ILogger<LogReporter> logger)
+        {
+            _logger = logger;
+
+            _databaseids = new List<FileId>();
+            _blobids = new List<string>();
+        }
+
+        public void Complete()
+        {
+            foreach (var databaseId in _databaseids)
+            {
+                _logger.LogError("no blob found for file id: {databaseId}", databaseId);
+            }
+
+            foreach (var blobId in _blobids)
+            {
+                _logger.LogError("no database entry found for blob id: {blobId}", blobId);
+            }
+        }
+
+        public void ReportOrphanedBlobId(string id)
+        {
+            _blobids.Add(id);
+        }
+
+        public void ReportOrphanedDatabaseId(FileId id)
+        {
+            _databaseids.Add(id);
+        }
+    }
+}
