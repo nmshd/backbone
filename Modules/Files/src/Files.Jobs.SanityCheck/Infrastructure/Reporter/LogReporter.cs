c56dd9f1344b4178e56de3851e8e4ce03d2b594f
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-02-13 13:25:27 +0000

    Feature(files)/add storage sanity check (#10)
    * feature: add storage sanity check for files
    
    * test: add storage sanity check tests for files
    
    * fix: change variable name casing
    
    * fix: change project location
    
    * fix: merge fix

---
+++ Modules/Files/src/Files.Jobs.SanityCheck/Infrastructure/Reporter/LogReporter.cs
@@ -0,0 +1,42 @@
+﻿using Backbone.Modules.Files.Domain.Entities;
+
+namespace Files.Jobs.SanityCheck.Infrastructure.Reporter
+{
+    public class LogReporter : IReporter
+    {
+        private readonly ILogger<LogReporter> _logger;
+        private readonly List<FileId> _databaseIds;
+        private readonly List<string> _blobIds;
+
+        public LogReporter(ILogger<LogReporter> logger)
+        {
+            _logger = logger;
+
+            _databaseIds = new List<FileId>();
+            _blobIds = new List<string>();
+        }
+
+        public void Complete()
+        {
+            foreach (var databaseId in _databaseIds)
+            {
+                _logger.LogError("no blob found for file id: {databaseId}", databaseId);
+            }
+
+            foreach (var blobId in _blobIds)
+            {
+                _logger.LogError("no database entry found for blob id: {blobId}", blobId);
+            }
+        }
+
+        public void ReportOrphanedBlobId(string id)
+        {
+            _blobIds.Add(id);
+        }
+
+        public void ReportOrphanedDatabaseId(FileId id)
+        {
+            _databaseIds.Add(id);
+        }
+    }
+}
