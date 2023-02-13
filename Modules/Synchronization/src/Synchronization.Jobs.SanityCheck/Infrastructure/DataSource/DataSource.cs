7460cfb5e319657b0d542d7de7664c0752c09ddd
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-02-13 14:09:33 +0000

    Feature(synchronization)/add storage sanity check (#11)
    * feat: add storage sanity check for datawallet
    
    * test: add storage sanity check tests for datawallets
    
    * fix: change variable name casing
    
    * fix: change project location
    
    * fix: merge fix
    
    * fix: merge fix

---
+++ Modules/Synchronization/src/Synchronization.Jobs.SanityCheck/Infrastructure/DataSource/DataSource.cs
@@ -0,0 +1,30 @@
+﻿using Backbone.Modules.Synchronization.Application.Infrastructure;
+using Backbone.Modules.Synchronization.Domain.Entities;
+using Enmeshed.BuildingBlocks.Application.Abstractions.Infrastructure.Persistence.BlobStorage;
+using Microsoft.EntityFrameworkCore;
+
+namespace Synchronization.Jobs.SanityCheck.Infrastructure.DataSource
+{
+    public class DataSource : IDataSource
+    {
+        private readonly IBlobStorage _blobStorage;
+        private readonly ISynchronizationDbContext _dbContext;
+
+        public DataSource(IBlobStorage blobStorage, ISynchronizationDbContext dbContext)
+        {
+            _blobStorage = blobStorage;
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<string>> GetBlobIdsAsync(CancellationToken cancellationToken)
+        {
+            var blobIds = await _blobStorage.FindAllAsync();
+            return await blobIds.ToListAsync(cancellationToken);
+        }
+
+        public async Task<IEnumerable<DatawalletModificationId>> GetDatabaseIdsAsync(CancellationToken cancellationToken)
+        {
+            return await _dbContext.SetReadOnly<DatawalletModification>().Select(u => u.Id).ToListAsync(cancellationToken);
+        }
+    }
+}
---
+++ Modules/Synchronization/src/Synchronization.Jobs.SanityCheck/Infrastructure/Reporter/LogReporter.cs
@@ -0,0 +1,42 @@
+﻿using Backbone.Modules.Synchronization.Domain.Entities;
+
+namespace Synchronization.Jobs.SanityCheck.Infrastructure.Reporter
+{
+    public class LogReporter : IReporter
+    {
+        private readonly ILogger<LogReporter> _logger;
+        private readonly List<DatawalletModificationId> _databaseIds;
+        private readonly List<string> _blobIds;
+
+        public LogReporter(ILogger<LogReporter> logger)
+        {
+            _logger = logger;
+
+            _databaseIds = new List<DatawalletModificationId>();
+            _blobIds = new List<string>();
+        }
+
+        public void Complete()
+        {
+            foreach (var databaseId in _databaseIds)
+            {
+                _logger.LogError("no blob found for datawallet modification id: {databaseId}", databaseId);
+            }
+
+            foreach (var blobId in _blobIds)
+            {
+                _logger.LogError("no database entry found for blob id: {blobId}", blobId);
+            }
+        }
+
+        public void ReportOrphanedBlobId(string id)
+        {
+            _blobIds.Add(id);
+        }
+
+        public void ReportOrphanedDatabaseId(DatawalletModificationId id)
+        {
+            _databaseIds.Add(id);
+        }
+    }
+}
