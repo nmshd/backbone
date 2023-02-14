56cee3b99f85bbc4e3a52b863c6c3f79537d6686
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-02-14 08:30:49 +0000

    Feature(tokens)/add storage sanity check (#13)
    * feat: add storage sanity check for tokens
    
    * test: add storage sanity check tests for tokens
    
    * feat: add GetAllTokenIds() method to token repository
    
    * feat: change sanity check datasource dbcontext to tokenrepository
    
    * fix: change variable name casing
    
    * fix: change project location
    
    * fix: merge fix
    
    * fix: merge fix
    
    * feat: add 'includeExpired' parameter to GetAllTokenIds()
    
    * fix: implement review changes
    
    * feat: include expired tokens in the sanity check data source

---
+++ Modules/Tokens/src/Tokens.Jobs.SanityCheck/Infrastructure/DataSource/DataSource.cs
@@ -0,0 +1,29 @@
+﻿using Backbone.Modules.Tokens.Application.Infrastructure;
+using Backbone.Modules.Tokens.Domain.Entities;
+using Enmeshed.BuildingBlocks.Application.Abstractions.Infrastructure.Persistence.BlobStorage;
+
+namespace Tokens.Jobs.SanityCheck.Infrastructure.DataSource
+{
+    public class DataSource : IDataSource
+    {
+        private readonly IBlobStorage _blobStorage;
+        private readonly ITokenRepository _tokenRepository;
+
+        public DataSource(IBlobStorage blobStorage, ITokenRepository tokenRepository)
+        {
+            _blobStorage = blobStorage;
+            _tokenRepository = tokenRepository;
+        }
+
+        public async Task<IEnumerable<string>> GetBlobIdsAsync(CancellationToken cancellationToken)
+        {
+            var blobIds = await _blobStorage.FindAllAsync();
+            return await blobIds.ToListAsync(cancellationToken);
+        }
+
+        public async Task<IEnumerable<TokenId>> GetDatabaseIdsAsync(CancellationToken cancellationToken)
+        {
+            return await _tokenRepository.GetAllTokenIds(includeExpired: true);
+        }
+    }
+}
