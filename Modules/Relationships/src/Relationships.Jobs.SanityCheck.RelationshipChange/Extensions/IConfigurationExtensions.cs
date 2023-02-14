dd5510512d4b6f32be52c53c375064a55588a18d
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-02-14 11:46:56 +0000

    Feature(relationships)/add storage sanity check (#14)
    * feat: add storage sanity check for relationship templates
    
    * feat: add storage sanity check for relationship changes
    
    * test: add storage sanity check tests for relationships
    
    * fix: change project location
    
    * fix: merge fix

---
+++ Modules/Relationships/src/Relationships.Jobs.SanityCheck.RelationshipChange/Extensions/IConfigurationExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Relationships.Jobs.SanityCheck.RelationshipChange.Extensions;
+
+[ExcludeFromCodeCoverage]
+internal static class IConfigurationExtensions
+{
+    public static BlobStorageConfiguration GetBlobStorageConfiguration(this IConfiguration configuration)
+    {
+        return new BlobStorageConfiguration(configuration);
+    }
+
+    public static SqlDatabaseConfiguration GetSqlDatabaseConfiguration(this IConfiguration configuration)
+    {
+        return configuration.GetSection("SqlDatabase").Get<SqlDatabaseConfiguration>() ?? new SqlDatabaseConfiguration();
+    }
+}
+
+public class SqlDatabaseConfiguration
+{
+    public string ConnectionString { get; set; }
+}
+
+public class BlobStorageConfiguration
+{
+    private readonly IConfigurationSection _blobStorageConfiguration;
+
+    public BlobStorageConfiguration(IConfiguration configuration)
+    {
+        _blobStorageConfiguration = configuration.GetSection("BlobStorage");
+    }
+
+    public string ConnectionString => _blobStorageConfiguration["ConnectionString"] ?? "";
+    public string CloudProvider => _blobStorageConfiguration["CloudProvider"] ?? "Azure";
+    public string ContainerName => _blobStorageConfiguration["ContainerName"] ?? "";
+}
---
+++ Modules/Relationships/src/Relationships.Jobs.SanityCheck.RelationshipChange/Infrastructure/DataSource/DataSource.cs
@@ -0,0 +1,31 @@
+﻿using Backbone.Modules.Relationships.Application.Infrastructure;
+using Backbone.Modules.Relationships.Domain.Ids;
+using Enmeshed.BuildingBlocks.Application.Abstractions.Infrastructure.Persistence.BlobStorage;
+using Microsoft.EntityFrameworkCore;
+
+namespace Relationships.Jobs.SanityCheck.RelationshipChange.Infrastructure.DataSource
+{
+    public class DataSource : IDataSource
+    {
+        private readonly IBlobStorage _blobStorage;
+        private readonly IRelationshipsDbContext _dbContext;
+        private const string BLOB_PREFIX = "RCH";
+
+        public DataSource(IBlobStorage blobStorage, IRelationshipsDbContext dbContext)
+        {
+            _blobStorage = blobStorage;
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<string>> GetBlobIdsAsync(CancellationToken cancellationToken)
+        {
+            var blobIds = await _blobStorage.FindAllAsync(BLOB_PREFIX);
+            return await blobIds.ToListAsync(cancellationToken);
+        }
+
+        public async Task<IEnumerable<RelationshipChangeId>> GetDatabaseIdsAsync(CancellationToken cancellationToken)
+        {
+            return await _dbContext.SetReadOnly<Backbone.Modules.Relationships.Domain.Entities.RelationshipChange>().Select(u => u.Id).ToListAsync(cancellationToken);
+        }
+    }
+}
