b3b95ce28b9d667426ad2525076a68802637511c
Diogo Ferraz <118177784+KONKdiogo-ferraz@users.noreply.github.com>
2023-02-16 21:45:26 +0000

    chore: added the exporter as an extension method

---
+++ BuildingBlocks/src/SpecFlowCucumberResultsExporter/Extensions/Exporter.cs
@@ -0,0 +1,24 @@
+﻿using SpecFlowCucumberResultsExporter.Reporting;
+
+namespace SpecFlowCucumberResultsExporter.Extensions;
+public class Exporter : ReportingStepDefinitions
+{
+    public static void ExportToCucumber(string path = default, string fileName = default)
+    {
+        path ??= "../../../TestResults/";
+        fileName ??= $"specflow_cucumber_{DateTimeOffset.UtcNow:yyyyMMdd}";
+        fileName += ".json";
+
+        Reporters.Add(new JsonReporter());
+
+        Reporters.FinishedReport += (sender, args) =>
+        {
+            var file = new System.IO.FileInfo(path);
+            if (file != null)
+            {
+                file.Directory.Create();
+                System.IO.File.WriteAllText(file.FullName, args.Reporter.WriteToString());
+            }
+        };
+    }
+}
