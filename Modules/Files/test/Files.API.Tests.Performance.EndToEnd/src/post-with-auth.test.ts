52e2bc30ba5a10b70fed21705fd3bd6d6a37a5fe
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-02-22 16:01:03 +0000

    test: ran prettier on test files

--- Modules/Files/test/Files.API.Tests.Performance.EndToEnd/src/post-with-auth.test.ts
+++ Modules/Files/test/Files.API.Tests.Performance.EndToEnd/src/post-with-auth.test.ts
@@ -1,58 +1,64 @@
 import http from "k6/http";
 import { Options } from "k6/options";
-import { describe, expect } from "https://jslib.k6.io/k6chaijs/4.3.4.2/index.js";
+import {
+  describe,
+  expect,
+} from "https://jslib.k6.io/k6chaijs/4.3.4.2/index.js";
 import { getJwt } from "./utils";
 
 const host = __ENV.HOST;
 const apiEndpoint = host + "/api/v1";
 
 function translateSize() {
-    switch(__ENV.SIZE){
-        case "S": return { vus: 1, iterations: 1 };
-        case "M": return { vus: 2, iterations: 5 };
-        case "L": return { vus: 3, iterations: 10 };
-        default: throw new Error("Invalid 'Size' value");
-    }
+  switch (__ENV.SIZE) {
+    case "S":
+      return { vus: 1, iterations: 1 };
+    case "M":
+      return { vus: 2, iterations: 5 };
+    case "L":
+      return { vus: 3, iterations: 10 };
+    default:
+      throw new Error("Invalid 'Size' value");
+  }
 }
 
 export const options: Options = {
-    vus: translateSize().vus,
-    thresholds: {
-        http_req_duration: ["p(90)<160", "p(98)<190"],
-    },
-    iterations: translateSize().iterations
+  vus: translateSize().vus,
+  thresholds: {
+    http_req_duration: ["p(90)<160", "p(98)<190"],
+  },
+  iterations: translateSize().iterations,
 };
 
 export function setup() {
-    return getJwt();
+  return getJwt();
 }
 
 export default function (authToken: string): void {
-    describe("Upload a File with Authentication:", () => {
-        const bodyFileContent = {
-            content: http.file("For performance testing purposes.", "PerformanceTest.txt"),
-            cipherHash: "AAAA",
-            expiresAt: tomorrow().toJSON().slice(0, 10),
-            encryptedProperties: "AAAA"
-        }
+  describe("Upload a File with Authentication:", () => {
+    const bodyFileContent = {
+      content: http.file(
+        "For performance testing purposes.",
+        "PerformanceTest.txt"
+      ),
+      cipherHash: "AAAA",
+      expiresAt: tomorrow().toJSON().slice(0, 10),
+      encryptedProperties: "AAAA",
+    };
 
-        const response = http.post(
-            `${apiEndpoint}/Files`,
-            bodyFileContent,
-            {
-                headers: {
-                    "Authorization": `Bearer ${authToken}`,
-                }
-            }
-        );
-
-        expect(response.status, "response status").to.equal(201);
+    const response = http.post(`${apiEndpoint}/Files`, bodyFileContent, {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
     });
-};
 
-function tomorrow(){
-    const date = new Date();
-    date.setDate(date.getDate() + 1)
+    expect(response.status, "response status").to.equal(201);
+  });
+}
 
-    return date;
+function tomorrow() {
+  const date = new Date();
+  date.setDate(date.getDate() + 1);
+
+  return date;
 }
--- Modules/Files/test/Files.API.Tests.Performance.EndToEnd/src/utils.ts
+++ Modules/Files/test/Files.API.Tests.Performance.EndToEnd/src/utils.ts
@@ -3,24 +3,22 @@ import http from "k6/http";
 const host = __ENV.HOST;
 
 export function getJwt() {
-    const bodyConnectToken = {
-        client_id: "test",
-        client_secret: __ENV.CLIENT_SECRET,
-        username: __ENV.USERNAME,
-        password: __ENV.PASSWORD,
-        grant_type: "password"
-    };
+  const bodyConnectToken = {
+    client_id: "test",
+    client_secret: __ENV.CLIENT_SECRET,
+    username: __ENV.USERNAME,
+    password: __ENV.PASSWORD,
+    grant_type: "password",
+  };
 
-    const authToken = http.post(
-        `${host}/connect/token`,
-        bodyConnectToken,
-        {
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-            }
-        }
-    ).json("access_token")!
+  const authToken = http
+    .post(`${host}/connect/token`, bodyConnectToken, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    })
+    .json("access_token")!
     .toString();
 
-    return authToken
+  return authToken;
 }
