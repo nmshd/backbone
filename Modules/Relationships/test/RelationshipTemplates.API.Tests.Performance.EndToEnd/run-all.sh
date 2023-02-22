2d1ce6020bb23dad505ca7c80c0447116c44877a
Luís Costa <118178009+KonkLuisCosta@users.noreply.github.com>
2023-02-22 16:33:05 +0000

    test: refactoring and add prettierrc
    -changes in the .sh and .ps1 scripts
    -changes in the performance tests
    -add prettierrc.json

---
+++ Modules/Relationships/test/RelationshipTemplates.API.Tests.Performance.EndToEnd/run-all.sh
@@ -0,0 +1,20 @@
+while getopts h:u:p:s: opt
+do
+    case $opt in
+        h) BASEURL=$OPTARG ;;
+        u) USERNAME=$OPTARG ;;
+        p) PASSWORD=$OPTARG ;;
+        s) SIZE=$OPTARG ;;
+    esac
+done
+
+echo "Running Relationship Templates performance tests..."
+
+npm install
+npx webpack
+
+for file in "./dist/"*.test.js ; do 
+    k6 run -e HOST=$BASEURL -e USERNAME=$USERNAME -e PASSWORD=$PASSWORD -e CLIENT_SECRET=test -e SIZE=$SIZE $file
+done
+
+echo "OK"
