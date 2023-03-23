90fa7f1e071425740a7d9d98da4a50e49a8e7782
Daniel Almeida <daniel.almeida@konkconsulting.com>
2023-03-23 10:22:28 +0000

    refactor: move strongly typed ids to building blocks domain

---
+++ BuildingBlocks/src/BuildingBlocks.Domain/Errors/GenericDomainErrors.cs
@@ -0,0 +1,27 @@
+﻿using Enmeshed.BuildingBlocks.Domain.Errors;
+
+namespace BuildingBlocks.Domain.Exceptions;
+
+public static class GenericDomainErrors
+{
+    public static DomainError InvalidIdPrefix(string reason = "")
+    {
+        var formattedReason = string.IsNullOrEmpty(reason) ? "" : $" ({reason})";
+        return new DomainError("error.platform.validation.domain.invalidIdPrefix",
+            string.IsNullOrEmpty(reason) ? $"The ID's prefix is invalid {formattedReason}." : reason);
+    }
+
+    public static DomainError InvalidIdLenght(string reason = "")
+    {
+        var formattedReason = string.IsNullOrEmpty(reason) ? "" : $" ({reason})";
+        return new DomainError("error.platform.validation.domain.invalidIdLenght",
+            string.IsNullOrEmpty(reason) ? $"The ID's length is invalid {formattedReason}." : reason);
+    }
+
+    public static DomainError InvalidIdCharacters(string reason = "")
+    {
+        var formattedReason = string.IsNullOrEmpty(reason) ? "" : $" ({reason})";
+        return new DomainError("error.platform.validation.domain.invalidIdCharacters",
+            string.IsNullOrEmpty(reason) ? $"The ID contains invalid characters {formattedReason}." : reason);
+    }
+}
---
+++ Modules/Devices/src/Devices.Domain/DomainErrors.cs
@@ -0,0 +1,14 @@
+﻿using Enmeshed.BuildingBlocks.Domain.Errors;
+
+namespace Backbone.Modules.Devices.Domain;
+
+public static class DomainErrors
+{
+    public static DomainError InvalidTierName(string reason = "")
+    {
+        var formattedReason = string.IsNullOrEmpty(reason) ? "" : $" ({reason})";
+        return new DomainError("error.platform.validation.domain.invalidTierName",
+            string.IsNullOrEmpty(reason) ? $"The Tier Name is invalid {formattedReason}." : reason);
+    }
+
+}
