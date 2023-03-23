3a96ceb37be584f5a9dc28f397e39a7519ccadba
Daniel Almeida <daniel.almeida@konkconsulting.com>
2023-03-23 10:32:03 +0000

    fix: invalid id error code and typos

--- BuildingBlocks/src/BuildingBlocks.Domain/Errors/GenericDomainErrors.cs
+++ BuildingBlocks/src/BuildingBlocks.Domain/Errors/GenericDomainErrors.cs
@@ -7,21 +7,21 @@ public static class GenericDomainErrors
     public static DomainError InvalidIdPrefix(string reason = "")
     {
         var formattedReason = string.IsNullOrEmpty(reason) ? "" : $" ({reason})";
-        return new DomainError("error.platform.validation.domain.invalidIdPrefix",
+        return new DomainError("error.platform.validation.invalidId",
             string.IsNullOrEmpty(reason) ? $"The ID's prefix is invalid {formattedReason}." : reason);
     }
 
-    public static DomainError InvalidIdLenght(string reason = "")
+    public static DomainError InvalidIdLength(string reason = "")
     {
         var formattedReason = string.IsNullOrEmpty(reason) ? "" : $" ({reason})";
-        return new DomainError("error.platform.validation.domain.invalidIdLenght",
+        return new DomainError("error.platform.validation.invalidId",
             string.IsNullOrEmpty(reason) ? $"The ID's length is invalid {formattedReason}." : reason);
     }
 
     public static DomainError InvalidIdCharacters(string reason = "")
     {
         var formattedReason = string.IsNullOrEmpty(reason) ? "" : $" ({reason})";
-        return new DomainError("error.platform.validation.domain.invalidIdCharacters",
+        return new DomainError("error.platform.validation.invalidId",
             string.IsNullOrEmpty(reason) ? $"The ID contains invalid characters {formattedReason}." : reason);
     }
 }
