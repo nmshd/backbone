d99c6a9588907eba32c15a797bddbbac1a70227e
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-03-24 10:26:30 +0000

    test: add step definitions and feature file

---
+++ Modules/Devices/test/Devices.API.Tests.Integration/Features/IdentityList.feature
@@ -0,0 +1,19 @@
+﻿@Integration
+Feature: Get Identity List
+
+User requests an Identity List
+
+# TODO uncomment the Given below and remove the Ignore tag once authentication is enabled.
+# For now, authentication is disabled due to the Admin UI implementation.
+
+Scenario: Requesting an Identity List as an authenticated user
+	#Given the user is authenticated
+	When a GET request is sent to the Identities/ endpoint
+	Then the response status code is 200 (OK)
+	And the response contains a list
+
+@Ignore
+Scenario: Requesting an Identity List as a non-authenticated user
+	Given the user is not authenticated
+	When a GET request is sent to the Identities/ endpoint
+	Then the response status code is 401 (Unauthorized)
---
+++ Modules/Devices/test/Devices.API.Tests.Integration/StepDefinitions/GetIdentityListStepDefinitions.cs
@@ -0,0 +1,66 @@
+using Challenges.API.Tests.Integration.API;
+using Devices.API.Tests.Integration.Models;
+using Microsoft.Extensions.Options;
+    
+using static Challenges.API.Tests.Integration.Configuration.Settings;
+
+namespace Devices.API.Tests.Integration.StepDefinitions
+{
+    [Binding]
+    public class GetIdentityListStepDefinitions
+    {
+
+        private readonly RequestConfiguration _requestConfiguration;
+        private readonly IdentitiesApi _identitiesApi;
+        private HttpResponse<List<IdentityDTO>>? _identitiesResponse;
+
+        public GetIdentityListStepDefinitions(IOptions<HttpConfiguration> httpConfiguration, IdentitiesApi identitiesApi)
+        {
+            _identitiesApi = identitiesApi;
+            _requestConfiguration = new RequestConfiguration
+            {
+                AuthenticationParameters = new AuthenticationParameters
+                {
+                    GrantType = "password",
+                    ClientId = httpConfiguration.Value.ClientCredentials.ClientId,
+                    ClientSecret = httpConfiguration.Value.ClientCredentials.ClientSecret,
+                    Username = "USRa",
+                    Password = "a"
+                }
+            };
+        }
+
+        [Given(@"the user is authenticated")]
+        public void GivenTheUserIsAuthenticated()
+        {
+            _requestConfiguration.Authenticate = true;
+        }
+
+        [Given(@"the user is not authenticated")]
+        public void GivenTheUserIsNotAuthenticated()
+        {
+            _requestConfiguration.Authenticate = false;
+        }
+
+        [When(@"a GET request is sent to the Identities/ endpoint")]
+        public async Task WhenAGETRequestIsSentToTheIdentitiesEndpointAsync()
+        {
+            _identitiesResponse = await _identitiesApi.GetIdentitiesList(_requestConfiguration);
+        }
+
+        [Then(@"the response status code is (\d*) \((?:[a-z]|[A-Z]|\s)+\)")]
+        public void ThenTheResponseStatusCodeIsOK(int code)
+        {
+            _identitiesResponse.Should().NotBeNull();
+            ((int)_identitiesResponse!.StatusCode).Should().Be(code);
+        }
+
+        [Then(@"the response contains a list")]
+        public void ThenTheResponseContainsAList()
+        {
+            _identitiesResponse.Should().NotBeNull();
+            Console.WriteLine(_identitiesResponse!.Data.Count);
+            _identitiesResponse!.Data.Should().NotBeEmpty();
+        }
+    }
+}
