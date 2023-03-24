cf53b55b8a7f88ef6e80206a9e4bb7665362aba6
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-03-24 10:18:12 +0000

    feat: Add IdentityDTO

---
+++ Modules/Devices/src/Devices.Application/DTOs/IdentityDTO.cs
@@ -0,0 +1,24 @@
+﻿using Enmeshed.DevelopmentKit.Identity.ValueObjects;
+
+namespace Backbone.Modules.Devices.Application.DTOs;
+
+public class IdentityDTO
+{
+    public string Address { get; set; }
+    public string ClientId { get; set; }
+    public byte[] PublicKey { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public byte IdentityVersion { get; set; }
+
+    public IdentityDTO(IdentityAddress address, string clientId, byte[] publicKey, byte identityVersion, DateTime createdAt)
+    {
+        Address = address.ToString();
+        ClientId = clientId;
+        PublicKey = publicKey;
+        IdentityVersion = identityVersion;
+        CreatedAt = createdAt;
+    }
+
+}
