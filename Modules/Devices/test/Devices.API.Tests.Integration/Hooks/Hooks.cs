c62b52cf4645938e18e5a416871beb89a3145df4
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-03-24 10:25:24 +0000

    test: Add export hook

---
+++ Modules/Devices/test/Devices.API.Tests.Integration/Hooks/Hooks.cs
@@ -0,0 +1,15 @@
+using SpecFlowCucumberResultsExporter.Extensions;
+
+namespace Challenges.API.Tests.Integration.Hooks;
+[Binding]
+public sealed class Hooks
+{
+    // For additional details on SpecFlow hooks see http://go.specflow.org/doc-hooks
+
+    [BeforeTestRun(Order = 0)]
+    public static void BeforeTestRun()
+    {
+        // Export test results to cucumber format
+        Exporter.ExportToCucumber();
+    }
+}
