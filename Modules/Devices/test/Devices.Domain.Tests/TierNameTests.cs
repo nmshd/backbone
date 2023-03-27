b99d8c2fcca2f9069febd3ee56d0fcd34939d625
Daniel Almeida <daniel.almeida@konkconsulting.com>
2023-03-27 11:33:46 +0100

    test: create unit tests for tier aggregate and respective value objects

---
+++ Modules/Devices/test/Devices.Domain.Tests/TierNameTests.cs
@@ -0,0 +1,35 @@
+﻿using Backbone.Modules.Devices.Domain.Aggregates.Tier;
+using FluentAssertions;
+using Xunit;
+
+namespace Devices.Domain.Tests;
+public class TierNameTests
+{
+    [Fact]
+    public void Can_create_tier_name_with_valid_value()
+    {
+        var validLength = 30;
+        var validTierName = TestDataGenerator.GenerateString(validLength);
+        var tierName = TierName.Create(validTierName);
+
+        var isTierNameValid = tierName.IsSuccess;
+
+        isTierNameValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Cannot_create_tier_name_with_invalid_name_length()
+    {
+        var invalidLength = 31;
+        var invalidTierName = TestDataGenerator.GenerateString(invalidLength);
+        var tierName = TierName.Create(invalidTierName);
+
+        var isTierNameInvalid = tierName.IsFailure;
+        var errorCode = tierName.Error.Code;
+        var errorMessage = tierName.Error.Message;
+
+        isTierNameInvalid.Should().BeTrue();
+        errorCode.Should().Be("error.platform.validation.invalidTierName");
+        errorMessage.Should().Be("Tier Name is longer than the 30 characters");
+    }
+}
