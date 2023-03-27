6513e8c4ba76d964dddf770d5cf9a67289353505
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-03-27 17:06:30 +0100

    test: add pagination support

--- Modules/Devices/test/Devices.Application.Tests/Tests/Identities/Queries/ListIdentities/HandlerTests.cs
+++ Modules/Devices/test/Devices.Application.Tests/Tests/Identities/Queries/ListIdentities/HandlerTests.cs
@@ -10,6 +10,8 @@ using Enmeshed.UnitTestTools.TestDoubles.Fakes;
 using Backbone.Modules.Devices.Domain.Entities;
 using Backbone.Modules.Devices.Application.Identities.Queries.ListIdentities;
 using Handler = Backbone.Modules.Devices.Application.Identities.Queries.ListIdentities.Handler;
+using Enmeshed.BuildingBlocks.Application.Pagination;
+using Backbone.Modules.Devices.Application.AutoMapper;
 
 namespace Backbone.Modules.Devices.Application.Tests.Tests.Identities.Queries.ListIdentities;
 
@@ -35,9 +37,10 @@ public class HandlerTests
         // Arrange
         List<Identity> identitiesList = new();
         _arrangeContext.SaveEntities(identitiesList.ToArray());
+        var request = new PaginationFilter();
 
         // Act
-        var result = await _handler.Handle(new ListIdentitiesQuery(), CancellationToken.None);
+        var result = await _handler.Handle(new ListIdentitiesQuery(request), CancellationToken.None);
 
         // Assert
         result.Identities.Should().HaveCount(0);
@@ -47,6 +50,7 @@ public class HandlerTests
     public async void Returns_valid_list_when_entities_loaded()
     {
         // Arrange
+        var request = new PaginationFilter();
         List<Identity> identitiesList = new()
         {
             new(TestDataGenerator.CreateRandomDeviceId(),
@@ -62,7 +66,7 @@ public class HandlerTests
         _arrangeContext.SaveEntities(identitiesList.ToArray());
 
         // Act
-        var result = await _handler.Handle(new ListIdentitiesQuery(), CancellationToken.None);
+        var result = await _handler.Handle(new ListIdentitiesQuery(request), CancellationToken.None);
 
         // Assert
         result.Identities.Should().HaveCount(2);
@@ -72,20 +76,23 @@ public class HandlerTests
     public async void Returns_valid_list_when_entities_loaded_and_attributes_match()
     {
         // Arrange
+        var request = new PaginationFilter();
+        var expectedClientId = TestDataGenerator.CreateRandomDeviceId();
+        var expectedAddress = TestDataGenerator.CreateRandomIdentityAddress();
         List<Identity> identitiesList = new()
         {
-            new("ClientId_str", "Address_str", Array.Empty<byte>(), 1)
+            new(expectedClientId, expectedAddress, Array.Empty<byte>(), 1)
         };
 
         _arrangeContext.SaveEntities(identitiesList.ToArray());
 
         // Act
-        var result = await _handler.Handle(new ListIdentitiesQuery(), CancellationToken.None);
+        var result = await _handler.Handle(new ListIdentitiesQuery(request), CancellationToken.None);
 
         // Assert
         result.Identities.Should().HaveCount(1);
-        result.Identities[0].ClientId.Should().Be("ClientId_str");
-        result.Identities[0].Address.Should().Be("Address_str");
+        result.Identities[0].ClientId.Should().Be(expectedClientId);
+        result.Identities[0].Address.Should().Be(expectedAddress);
         result.Identities[0].PublicKey.Should().BeEquivalentTo(Array.Empty<byte>());
         result.Identities[0].IdentityVersion.Should().Be(1);
     }
@@ -95,6 +102,8 @@ public class HandlerTests
         var userContext = A.Fake<IUserContext>();
         A.CallTo(() => userContext.GetAddress()).Returns(ActiveIdentity);
 
-        return new Handler(_actContext, null);
+        var mapper = AutoMapperProfile.CreateMapper();
+
+        return new Handler(_actContext, userContext, mapper);
     }
 }
