3a1448aace1d299655cae58bccb25c1a0308f24f
Daniel Almeida <115644988+daniel-almeida-konkconsulting@users.noreply.github.com>
2023-03-28 10:35:31 +0100

    Feature(devices)/create tier--tier aggregate (#47)
    * feat: create tier aggregate
    
    * test: create unit tests for tier aggregate and respective value objects
    
    * fix: unit test error message assertion
    
    * fix: remove private setters from Tier class
    
    * test: use hardcoded tier names as opposed to randomly generated ones
    
    * fix: rename value converters for tierid and tiername
    
    * test: use theory decorator on tiername unit tests to support multiple executions with different values

---
+++ Modules/Devices/src/Devices.Domain/Aggregates/Tier/TierName.cs
@@ -0,0 +1,30 @@
+﻿using CSharpFunctionalExtensions;
+using Enmeshed.BuildingBlocks.Domain.Errors;
+
+namespace Backbone.Modules.Devices.Domain.Aggregates.Tier;
+
+public record TierName
+{
+    public string Value { get; }
+
+    private TierName(string value)
+    {
+        Value = value;
+    }
+
+    public static Result<TierName, DomainError> Create(string value)
+    {
+        var validationResult = Validate(value);
+        if (validationResult != null)
+            return Result.Failure<TierName, DomainError>(validationResult);
+        return Result.Success<TierName, DomainError>(new TierName(value));
+    }
+
+    public static DomainError? Validate(string value)
+    {
+        if (value.Length > 30)
+            return DomainErrors.InvalidTierName("Tier Name is longer than the 30 characters");
+
+        return null;
+    }
+}
---
+++ Modules/Devices/test/Devices.Domain.Tests/TierNameTests.cs
@@ -0,0 +1,34 @@
+﻿using Backbone.Modules.Devices.Domain.Aggregates.Tier;
+using FluentAssertions;
+using Xunit;
+
+namespace Devices.Domain.Tests;
+public class TierNameTests
+{
+    [Theory]
+    [InlineData("a-tier-name")]
+    [InlineData("a tier name")]
+    public void Can_create_tier_name_with_valid_value(string value)
+    {
+        var tierName = TierName.Create(value);
+        var isTierNameValid = tierName.IsSuccess;
+
+        isTierNameValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Cannot_create_tier_name_with_invalid_name_length()
+    {
+        var invalidLength = 31;
+        var invalidTierName = TestDataGenerator.GenerateString(invalidLength);
+        var tierName = TierName.Create(invalidTierName);
+
+        var isTierNameInvalid = tierName.IsFailure;
+        var errorCode = tierName.Error.Code;
+        var errorMessage = tierName.Error.Message;
+
+        isTierNameInvalid.Should().BeTrue();
+        errorCode.Should().Be("error.platform.validation.invalidTierName");
+        errorMessage.Should().Be("Tier Name is longer than the 30 characters");
+    }
+}
