3c356c7035133c075c4d98b688176c75d840720d
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-03-30 12:03:54 +0100

    refactor: remove reference to Authentication in RequestConfiguration

--- Modules/Devices/test/Devices.API.Tests.Integration/Models/RequestConfiguration.cs
+++ Modules/Devices/test/Devices.API.Tests.Integration/Models/RequestConfiguration.cs
@@ -1,14 +1,12 @@
 ﻿namespace Devices.API.Tests.Integration.Models;
 public class RequestConfiguration
 {
-    public bool Authenticate { get; set; } = false;
     public string? ContentType { get; set; }
     public string? AcceptHeader { get; set; }
     public string? Content { get; set; }
 
     public void SupplementWith(RequestConfiguration other)
     {
-        Authenticate = other.Authenticate;
         AcceptHeader ??= other.AcceptHeader;
         ContentType ??= other.ContentType;
         Content ??= other.Content;
