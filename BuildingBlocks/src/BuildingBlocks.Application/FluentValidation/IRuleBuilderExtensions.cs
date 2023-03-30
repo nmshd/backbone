cbed1f4a10832378d3cba10dcf52810e525022d4
Daniel Almeida <daniel.almeida@konkconsulting.com>
2023-03-30 13:12:11 +0100

    feat: replace abstractdomainvalidator with extension method for custom validation

---
+++ BuildingBlocks/src/BuildingBlocks.Application/FluentValidation/IRuleBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Enmeshed.BuildingBlocks.Domain.Errors;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace Enmeshed.BuildingBlocks.Application.FluentValidation;
+
+public static class IRuleBuilderExtensions
+{
+    public static void Valid<T>(this IRuleBuilder<T, string> ruleBuilder,
+        Func<string, DomainError?> validator)
+    {
+        ruleBuilder.Custom((v, context) =>
+        {
+            var r = validator(v);
+            if (r != null)
+                context.AddFailure(new ValidationFailure(context.PropertyName, r.Message, v) { ErrorCode = r.Code });
+        });
+    }
+}
--- Modules/Devices/src/Devices.Application/Tiers/Commands/CreateTier/CreateTierCommandValidator.cs
+++ Modules/Devices/src/Devices.Application/Tiers/Commands/CreateTier/CreateTierCommandValidator.cs
@@ -1,20 +1,14 @@
 ﻿using Backbone.Modules.Devices.Domain.Aggregates.Tier;
 using Enmeshed.BuildingBlocks.Application.FluentValidation;
-using Enmeshed.BuildingBlocks.Domain.Errors;
 using FluentValidation;
 
 namespace Backbone.Modules.Devices.Application.Tiers.Commands.CreateTier;
 
-public class CreateTierCommandValidator : AbstractDomainValidator<CreateTierCommand>
+public class CreateTierCommandValidator : AbstractValidator<CreateTierCommand>
 {
     public CreateTierCommandValidator()
     {
         RuleFor(t => t.Name)
-            .Empty();
-    }
-
-    protected override IEnumerable<(DomainError?, string)> ValidateAgainstDomainModel(CreateTierCommand value)
-    {
-        yield return (TierName.Validate(value.Name), nameof(value.Name));
+            .Valid(TierName.Validate);
     }
 }
