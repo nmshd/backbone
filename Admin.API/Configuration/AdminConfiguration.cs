cc395c32834e42d30617de60be621a41e7ee314a
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-04-03 11:47:39 +0100

    fix: remove Authentication from AdminConfiguration

--- Admin.API/Configuration/AdminConfiguration.cs
+++ Admin.API/Configuration/AdminConfiguration.cs
@@ -4,28 +4,12 @@ namespace Admin.API.Configuration;
 
 public class AdminConfiguration
 {
-    [Required]
-    public AuthenticationConfiguration Authentication { get; set; } = new();
-
     public CorsConfiguration Cors { get; set; } = new();
 
     public SwaggerUiConfiguration SwaggerUi { get; set; } = new();
 
     [Required]
     public ModulesConfiguration Modules { get; set; } = new();
-
-    public class AuthenticationConfiguration
-    {
-        [Required]
-        [MinLength(1)]
-        public string JwtSigningCertificateSource { get; set; } = "";
-
-        public string JwtSigningCertificate { get; set; } = "";
-
-        [Required]
-        [Range(60, 3600)]
-        public int JwtLifetimeInSeconds { get; set; }
-    }
 
     public class CorsConfiguration
     {
@@ -44,6 +28,5 @@ public class AdminConfiguration
 
         [Required]
         public DevicesConfiguration Devices { get; set; } = new();
-
     }
 }
