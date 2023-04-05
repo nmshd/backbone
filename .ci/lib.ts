b8c6532811f2588c87e046105af5666f1d405de5
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-04-05 15:15:46 +0100

    Revert "Merge branch 'main' into feature/DEV-1808-admin-api-identities-list"
    This reverts commit 895515cf957451f7305c4f1a70150b031655d65e, reversing
    changes made to f49032b982a04835c00a999d63d6a19d191b8df0.

--- .ci/lib.ts
+++ .ci/lib.ts
@@ -12,3 +12,14 @@ export function getRequiredEnvVar(envVarName: string) {
   }
   return envVar;
 }
+
+// export function extractVersionFromGitTag(gitTag: string | undefined) {
+//   if (!gitTag) return undefined;
+
+//   const semVerRegex =
+//     /(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?/;
+//   const gitTagRegex = new RegExp(`^.+/(${semVerRegex.source})$`);
+
+//   const dockerTag = gitTagRegex.exec(gitTag)?.[1];
+//   return dockerTag;
+// }
