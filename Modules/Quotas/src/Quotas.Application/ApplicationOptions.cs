84a16663dead93318319935a902ddf0a3703903e
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-04-06 15:45:51 +0100

    feature: add ApplicationOptions

---
+++ Modules/Quotas/src/Quotas.Application/ApplicationOptions.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backbone.Modules.Quotas.Application;
+
+public class ApplicationOptions
+{
+    [Required]
+    [MinLength(3)]
+    [MaxLength(3)]
+    public string AddressPrefix { get; set; }
+
+    [Required]
+    public PaginationOptions Pagination { get; set; } = new();
+}
+
+public class PaginationOptions
+{
+    [Required]
+    [Range(1, 1000)]
+    public int MaxPageSize { get; set; }
+
+    [Required]
+    [Range(1, 1000)]
+    public int DefaultPageSize { get; set; }
+}
