2cb67053b416cd2772b9ffa6c2faf5e30b12e911
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-04-10 15:44:05 +0100

    feat: database service collection extensions

---
+++ Modules/Quotas/src/Quotas.Infrastructure/Persistence/Database/IServiceCollectionExtensions.cs
@@ -0,0 +1,68 @@
+﻿using Backbone.Modules.Files.Application.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Backbone.Modules.Files.Infrastructure.Persistence.Database;
+
+public static class IServiceCollectionExtensions
+{
+    private const string SQLSERVER = "SqlServer";
+    private const string SQLSERVER_MIGRATIONS_ASSEMBLY = "Files.Infrastructure.Database.SqlServer";
+    private const string POSTGRES = "Postgres";
+    private const string POSTGRES_MIGRATIONS_ASSEMBLY = "Files.Infrastructure.Database.Postgres";
+
+    public static void AddDatabase(this IServiceCollection services, Action<DbOptions> setupOptions)
+    {
+        var options = new DbOptions();
+        setupOptions?.Invoke(options);
+
+        services.AddDatabase(options);
+    }
+
+    public static void AddDatabase(this IServiceCollection services, DbOptions options)
+    {
+        switch (options.Provider)
+        {
+            case SQLSERVER:
+                services.AddDbContext<FilesDbContext>(dbContextOptions =>
+                    dbContextOptions.UseSqlServer(options.DbConnectionString, sqlOptions =>
+                    {
+                        sqlOptions.CommandTimeout(20);
+                        sqlOptions.MigrationsAssembly(SQLSERVER_MIGRATIONS_ASSEMBLY);
+                        sqlOptions.EnableRetryOnFailure(options.RetryOptions.MaxRetryCount, TimeSpan.FromSeconds(options.RetryOptions.MaxRetryDelayInSeconds), null);
+                    })
+                );
+                break;
+            case POSTGRES:
+                services.AddDbContext<FilesDbContext>(dbContextOptions =>
+                    dbContextOptions.UseNpgsql(options.DbConnectionString, sqlOptions =>
+                    {
+                        sqlOptions.CommandTimeout(20);
+                        sqlOptions.MigrationsAssembly(POSTGRES_MIGRATIONS_ASSEMBLY);
+                        sqlOptions.EnableRetryOnFailure(options.RetryOptions.MaxRetryCount, TimeSpan.FromSeconds(options.RetryOptions.MaxRetryDelayInSeconds), null);
+                    })
+
+                );
+                break;
+            default:
+                throw new Exception($"Unsupported database provider: {options.Provider}");
+
+        }
+
+        services.AddScoped<IFilesDbContext, FilesDbContext>();
+    }
+}
+
+public class DbOptions
+{
+    public string Provider { get; set; }
+    public string DbConnectionString { get; set; }
+    public RetryOptions RetryOptions { get; set; } = new();
+}
+
+public class RetryOptions
+{
+    public byte MaxRetryCount { get; set; } = 15;
+    public int MaxRetryDelayInSeconds { get; set; } = 30;
+}
+
