9e90e8704e9713d27cbc538dd677ebeaf92650d2
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-04-10 15:44:33 +0100

    feat: add quotas controller to backbone api

---
+++ Backbone.API/Controllers/QuotasController.cs
@@ -0,0 +1,14 @@
+﻿using Backbone.API.Mvc;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using OpenIddict.Validation.AspNetCore;
+
+[Route("api/v1/[controller]")]
+[Authorize(OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme)]
+public class QuotasController : ApiControllerBase
+{
+    public QuotasController(IMediator mediator) : base(mediator)
+    {
+    }
+}
