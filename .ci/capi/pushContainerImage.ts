b2969d389a8004d0c6915ef42039c43d955ca12d
Timo Notheisen <65653426+tnotheis@users.noreply.github.com>
2023-04-11 08:51:06 +0200

    Deploy consumer image with backbone-consumer-api tag (#68)
    * ci: add tag to build command
    
    * ci: add tag to push command

--- .ci/capi/pushContainerImage.ts
+++ .ci/capi/pushContainerImage.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env -S npx ts-node --esm
-import { echo, $ } from "zx";
+import { $ } from "zx";
 import { getRequiredEnvVar } from "../lib.js";
 
 const tag = getRequiredEnvVar("TAG");
 
 await $`docker push ghcr.io/nmshd/backbone:${tag}`;
+await $`docker push ghcr.io/nmshd/backbone-consumer-api:${tag}`;
