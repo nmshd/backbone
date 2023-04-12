70172699cbfe85d297338edbfc3f826c200d9f74
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-04-12 15:57:08 +0100

    feat: add BasicTier Queryable extension

---
+++ Modules/Devices/src/Devices.Application/Extensions/TierQueryableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Backbone.Modules.Devices.Domain.Aggregates.Tier;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backbone.Modules.Devices.Application.Extensions;
+
+public static class TierQueryableExtensions
+{
+    public static async Task<Tier> BasicTier(this IQueryable<Tier> query, CancellationToken cancellationToken)
+    {
+        var basicTier = await query.FirstOrDefaultAsync(t => t.Name == TierName.BASIC_DEFAULT_NAME, cancellationToken);
+
+        return basicTier;
+    }
+}
