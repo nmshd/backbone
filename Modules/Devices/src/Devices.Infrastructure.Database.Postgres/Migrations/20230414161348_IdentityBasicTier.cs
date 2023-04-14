9270aec55530f3a09d1435d094fcbc64d6800ca0
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-04-14 17:15:10 +0100

    fix: add Postrges migrations

---
+++ Modules/Devices/src/Devices.Infrastructure.Database.Postgres/Migrations/20230414161348_IdentityBasicTier.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Devices.Infrastructure.Database.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class IdentityBasicTier : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TierId",
+                table: "Identities",
+                type: "character(20)",
+                unicode: false,
+                fixedLength: true,
+                maxLength: 20,
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tier_Name",
+                table: "Tier",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Identities_TierId",
+                table: "Identities",
+                column: "TierId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Identities_Tier_TierId",
+                table: "Identities",
+                column: "TierId",
+                principalTable: "Tier",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Identities_Tier_TierId",
+                table: "Identities");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tier_Name",
+                table: "Tier");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Identities_TierId",
+                table: "Identities");
+
+            migrationBuilder.DropColumn(
+                name: "TierId",
+                table: "Identities");
+        }
+    }
+}
