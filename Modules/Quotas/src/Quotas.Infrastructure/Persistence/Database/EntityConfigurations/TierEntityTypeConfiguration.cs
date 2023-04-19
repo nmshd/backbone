883715d71a7cf598abc241d36a07b97ed8908059
Daniel Almeida <daniel.almeida@konkconsulting.com>
2023-04-19 11:55:31 +0100

    feat: add entity type configuration for Tier class

---
+++ Modules/Quotas/src/Quotas.Infrastructure/Persistence/Database/EntityConfigurations/TierEntityTypeConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Backbone.Modules.Quotas.Domain.Aggregates.Tiers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Backbone.Modules.Quotas.Infrastructure.Persistence.Database.EntityConfigurations;
+public class TierEntityTypeConfiguration : IEntityTypeConfiguration<Tier>
+{
+    public void Configure(EntityTypeBuilder<Tier> builder)
+    {
+        builder.HasKey(x => x.Id);
+    }
+}
