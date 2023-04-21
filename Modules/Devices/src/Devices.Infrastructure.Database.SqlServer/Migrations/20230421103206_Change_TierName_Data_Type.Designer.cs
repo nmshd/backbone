1e753ba9d8d15c70389fe7a24412d854cc9ecb89
Daniel Almeida <daniel.almeida@konkconsulting.com>
2023-04-21 12:02:09 +0100

    fix: create sql server migration to fix tier name data type

--- Modules/Devices/src/Devices.Infrastructure.Database.SqlServer/Migrations/20230421103206_Change_TierName_Data_Type.Designer.cs
+++ Modules/Devices/src/Devices.Infrastructure.Database.SqlServer/Migrations/20230421103206_Change_TierName_Data_Type.Designer.cs
@@ -4,16 +4,19 @@ using Backbone.Modules.Devices.Infrastructure.Persistence.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace Backbone.Modules.Devices.Infrastructure.Database.SqlServer.Migrations
+namespace Devices.Infrastructure.Database.SqlServer.Migrations
 {
     [DbContext(typeof(DevicesDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230421103206_Change_TierName_Data_Type")]
+    partial class Change_TierName_Data_Type
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
