9bf93a320d8f5e47d15b5fe485315e72d84f24dc
Timo Notheisen <65653426+tnotheis@users.noreply.github.com>
2023-04-24 15:54:05 +0200

    Dummy push notification service (#94)
    * chore: remove accidentally pushed src.zip
    
    * feat: add DummyPushService
    
    * feat: allow configuration of DummyPushService
    
    * feat: configure Admin.API to use DummyPushService
    
    * chore: remove notification hub connection string from docker-compose.yml
    
    * chore: add missing change
    
    * chore: remove notification hub from prepare_local_environment.ps1 script

---
+++ Modules/Devices/src/Devices.Infrastructure/PushNotifications/Dummy/IServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Backbone.Modules.Devices.Application.Infrastructure.PushNotifications;
+using Microsoft.Azure.NotificationHubs;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Backbone.Modules.Devices.Infrastructure.PushNotifications.Dummy;
+
+public static class IServiceCollectionExtensions
+{
+    public static void AddDummyPushNotifications(this IServiceCollection services)
+    {
+        services.AddTransient<IPushService, DummyPushService>();
+    }
+}
