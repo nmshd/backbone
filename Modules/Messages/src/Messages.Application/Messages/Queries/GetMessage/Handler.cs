89b31348634433c54444e044e406f8eed0e3ce50
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-04-28 15:32:56 +0100

    Repositories for Messages module (#83)
    * refactor: use a repository for GetMessageQuery
    
    * fix: setup DI for repository
    
    * refactor: send message
    
    * refactor: use messages repo for FindMessagesOfIdentity
    
    * refactor: remove AddRepositories fn
    
    * fix: missing public modifier
    
    * fix: do not use userContext in MessagesRepo
    
    * feat: add fetch mode enum
    
    * refactor: remove the IMessageDbContext
    
    * refactor: add Relationships repo to the Messages Module
    
    * fix: relationship repo not registered
    
    * refactor: move db access to messages from the service to the repo
    
    * fix: remove needless (and evil) dependency
    
    * fix: restore ct
    
    * fix: do not use pagination for findall
    
    * fix: duplicate param in FindMessagesOfIdentity
    
    * refactor: remove unused FetchMode
    
    * refactor: use DbContext for SanityCheck. Remove needless Repo methods
    
    * refactor: make update return the updated Message
    
    * refactor: use fluent api for query
    
    * fix: return completedTask instead of updated message
    
    * refactor: make the repo responsible for marking messages as received
    
    * move refactor: blob access to repo
    
    * fix: remove dep
    
    * refactor: renamings, passing the smallest paramenters possible
    
    * refactor: remove commands from repo Methods. Renaming
    
    * fix: Messages not tracked were being updated
    
    * refactor: move marking of message as read to handlers
    
    * fix: typo
    
    * fix: add to exception message
    
    Co-authored-by: Timo Notheisen <65653426+tnotheis@users.noreply.github.com>
    
    * refactor: add noBody support
    
    * refactor: received → fetched
    
    * refactor: remove unused
    
    * fix: ensure all repo methods are async
    
    * fix: update set of messages using UpdateRange
    
    * fix: body not loaded on FindMessagesWithIds
    
    * refactor msg → message
    
    * refactor: invert condition, make loading bodies parallel
    
    ---------
    
    Co-authored-by: Timo Notheisen <65653426+tnotheis@users.noreply.github.com>

--- Modules/Messages/src/Messages.Application/Messages/Queries/GetMessage/Handler.cs
+++ Modules/Messages/src/Messages.Application/Messages/Queries/GetMessage/Handler.cs
@@ -1,8 +1,7 @@
 ﻿using AutoMapper;
 using Backbone.Modules.Messages.Application.Extensions;
-using Backbone.Modules.Messages.Application.Infrastructure.Persistence;
+using Backbone.Modules.Messages.Application.Infrastructure.Persistence.Repository;
 using Backbone.Modules.Messages.Application.Messages.DTOs;
-using Backbone.Modules.Messages.Domain.Entities;
 using Enmeshed.BuildingBlocks.Application.Abstractions.Infrastructure.UserContext;
 using MediatR;
 
@@ -10,35 +9,26 @@ namespace Backbone.Modules.Messages.Application.Messages.Queries.GetMessage;
 
 public class Handler : IRequestHandler<GetMessageQuery, MessageDTO>
 {
-    private readonly IMessagesDbContext _dbContext;
+    private readonly IMessagesRepository _messagesRepository;
     private readonly IMapper _mapper;
-    private readonly MessageService _messageService;
     private readonly IUserContext _userContext;
 
-    public Handler(IMessagesDbContext dbContext, IUserContext userContext, IMapper mapper, MessageService messageService)
+    public Handler(IUserContext userContext, IMapper mapper, IMessagesRepository messagesRepository)
     {
-        _dbContext = dbContext;
         _userContext = userContext;
         _mapper = mapper;
-        _messageService = messageService;
+        _messagesRepository = messagesRepository;
     }
 
     public async Task<MessageDTO> Handle(GetMessageQuery request, CancellationToken cancellationToken)
     {
-        var addressOfActiveIdentity = _userContext.GetAddress();
+        var message = await _messagesRepository.Find(request.Id, _userContext.GetAddress(), cancellationToken, track: true);
 
-        var message = await _dbContext
-            .Set<Message>()
-            .IncludeAllReferences(addressOfActiveIdentity)
-            .WithSenderOrRecipient(_userContext.GetAddress())
-            .FirstWithId(request.Id, cancellationToken);
+        message.Recipients.FirstWithIdOrDefault(_userContext.GetAddress()).FetchedMessage(_userContext.GetDeviceId());
 
-        await _messageService.MarkMessageAsReceived(message, cancellationToken);
+        await _messagesRepository.Update(message);
 
         var response = _mapper.Map<MessageDTO>(message);
-
-        if (!request.NoBody)
-            await _messageService.FillBody(response);
 
         response.PrepareForActiveIdentity(_userContext.GetAddress());
 
---
+++ Modules/Messages/src/Messages.Infrastructure/Persistence/Database/Repository/MessagesRepository.cs
@@ -0,0 +1,103 @@
+﻿using Backbone.Modules.Messages.Application.Infrastructure.Persistence.Repository;
+using Backbone.Modules.Messages.Application.Extensions;
+using Backbone.Modules.Messages.Domain.Entities;
+using Backbone.Modules.Messages.Domain.Ids;
+using Enmeshed.BuildingBlocks.Application.Abstractions.Infrastructure.Persistence.Database;
+using Microsoft.EntityFrameworkCore;
+using Enmeshed.BuildingBlocks.Application.Extensions;
+using Enmeshed.DevelopmentKit.Identity.ValueObjects;
+using Enmeshed.BuildingBlocks.Application.Abstractions.Infrastructure.Persistence.BlobStorage;
+using Backbone.Modules.Messages.Application.Infrastructure.Persistence;
+using Microsoft.Extensions.Options;
+using Enmeshed.BuildingBlocks.Application.Pagination;
+
+
+namespace Backbone.Modules.Messages.Infrastructure.Persistence.Database.Repository;
+public class MessagesRepository : IMessagesRepository
+{
+    private readonly DbSet<Message> _messages;
+    private readonly IQueryable<Message> _readOnlyMessages;
+    private readonly MessagesDbContext _dbContext;
+    private readonly IBlobStorage _blobStorage;
+    private readonly BlobOptions _blobOptions;
+
+    public MessagesRepository(MessagesDbContext dbContext, IBlobStorage blobStorage, IOptions<BlobOptions> blobOptions)
+    {
+        _messages = dbContext.Messages;
+        _readOnlyMessages = dbContext.Messages.AsNoTracking();
+        _dbContext = dbContext;
+        _blobStorage = blobStorage;
+        _blobOptions = blobOptions.Value;
+    }
+    public async Task<Message> Find(MessageId id, IdentityAddress address, CancellationToken cancellationToken, bool track = false, bool fillBody = true)
+    {
+        var message = await (track ? _messages : _readOnlyMessages)
+            .IncludeAllReferences()
+            .WithSenderOrRecipient(address)
+            .FirstWithId(id, cancellationToken);
+
+        if (fillBody)
+        {
+            await FillBody(message);
+        }
+
+        return message;
+    }
+
+    public async Task<Message> FindPlain(MessageId id, CancellationToken cancellationToken)
+    {
+        return await _readOnlyMessages.FirstWithId(id, cancellationToken);
+    }
+
+    public async Task<MessageId> Add(Message message, CancellationToken cancellationToken)
+    {
+        var add = await _messages.AddAsync(message, cancellationToken);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+        _blobStorage.Add(_blobOptions.RootFolder, message.Id, message.Body);
+        await _blobStorage.SaveAsync();
+        return add.Entity.Id;
+    }
+
+    public async Task<int> CountUnreceivedMessagesFromSenderToRecipient(IdentityAddress sender, IdentityAddress recipient, CancellationToken cancellationToken)
+    {
+        return await _readOnlyMessages
+            .FromASpecificSender(sender)
+            .WithASpecificRecipientWhoDidNotReceiveTheMessage(recipient)
+            .CountAsync(cancellationToken);
+    }
+
+    public async Task<DbPaginationResult<Message>> FindMessagesWithIds(IEnumerable<MessageId> ids, IdentityAddress requiredParticipant, PaginationFilter paginationFilter, bool track = false)
+    {
+        var query = (track ? _messages : _readOnlyMessages)
+            .AsQueryable()
+            .IncludeAllReferences();
+
+        if (ids.Any())
+            query = query.WithIdsIn(ids);
+
+        var messages = await query.WithSenderOrRecipient(requiredParticipant)
+            .DoNotSendBeforePropertyIsNotInTheFuture()
+            .OrderAndPaginate(d => d.CreatedAt, paginationFilter);
+
+        await Task.WhenAll(messages.ItemsOnPage.Select(FillBody).ToArray());
+
+        return messages;
+    }
+
+    private async Task FillBody(Message message)
+    {
+        await _blobStorage.FindAsync(_blobOptions.RootFolder, message.Id);
+    }
+
+    public async Task Update(Message message)
+    {
+        _messages.Update(message);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task Update(IEnumerable<Message> messages)
+    {
+        _dbContext.UpdateRange(messages);
+        await _dbContext.SaveChangesAsync();
+    }
+}
