30eece0ebc295e31956cb8a55aebf10b6f359d04
Daniel Almeida <daniel.almeida@konkconsulting.com>
2023-05-03 14:40:42 +0100

    refactor: make CreateTokenResponse and HttpResponse properties non nullable

--- Modules/Tokens/test/Tokens.API.Tests.Integration/Models/CreateTokenResponse.cs
+++ Modules/Tokens/test/Tokens.API.Tests.Integration/Models/CreateTokenResponse.cs
@@ -1,6 +1,6 @@
 ﻿namespace Tokens.API.Tests.Integration.Models;
 public class CreateTokenResponse
 {
-    public string? Id { get; set; }
-    public string? CreatedAt { get; set; }
+    public string Id { get; set; }
+    public string CreatedAt { get; set; }
 }
--- Modules/Tokens/test/Tokens.API.Tests.Integration/Models/HttpResponse.cs
+++ Modules/Tokens/test/Tokens.API.Tests.Integration/Models/HttpResponse.cs
@@ -3,7 +3,7 @@
 namespace Tokens.API.Tests.Integration.Models;
 public class HttpResponse<T>
 {
-    public T? Data { get; set; }
+    public T Data { get; set; }
     public string? HttpMethod { get; set; }
     public HttpStatusCode StatusCode { get; set; }
     public bool IsSuccessStatusCode { get; set; }
