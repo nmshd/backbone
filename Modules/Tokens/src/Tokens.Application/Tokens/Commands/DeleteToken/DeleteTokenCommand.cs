73986e45a00615a64d831f90b73040e598ffb9e6
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-05-03 15:22:39 +0100

    fix: remove the ability to delete a token (handler + controller)

--- Modules/Tokens/src/Tokens.Application/Tokens/Commands/DeleteToken/DeleteTokenCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using Backbone.Modules.Tokens.Domain.Entities;
-using MediatR;
-
-namespace Backbone.Modules.Tokens.Application.Tokens.Commands.DeleteToken;
-
-public class DeleteTokenCommand : IRequest
-{
-    public TokenId Id { get; set; }
-}
