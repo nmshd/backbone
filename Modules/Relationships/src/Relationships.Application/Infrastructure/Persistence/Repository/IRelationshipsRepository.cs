0cf06048b0e6da00e3faf20625043fb2a80dfbdd
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-05-03 09:26:16 +0100

    feat: add IRelationshipsRepository

---
+++ Modules/Relationships/src/Relationships.Application/Infrastructure/Persistence/Repository/IRelationshipsRepository.cs
@@ -0,0 +1,8 @@
+﻿using Backbone.Modules.Relationships.Domain.Entities;
+using Backbone.Modules.Relationships.Domain.Ids;
+
+namespace Backbone.Modules.Relationships.Application.Infrastructure.Persistence.Repository;
+public interface IRelationshipsRepository
+{
+    Task<RelationshipTemplateId> AddRelationshipTemplate(RelationshipTemplate template, CancellationToken cancellationToken);
+}
---
+++ Modules/Relationships/src/Relationships.Infrastructure/Persistence/Database/Repository/RelationshipsRepository.cs
@@ -0,0 +1,38 @@
+﻿using Backbone.Modules.Relationships.Application.Infrastructure;
+using Backbone.Modules.Relationships.Application.Infrastructure.Persistence.Repository;
+using Backbone.Modules.Relationships.Domain.Entities;
+using Backbone.Modules.Relationships.Domain.Ids;
+using Enmeshed.BuildingBlocks.Application.Abstractions.Infrastructure.Persistence.BlobStorage;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Backbone.Modules.Relationships.Infrastructure.Persistence.Database.Repository;
+public class RelationshipsRepository : IRelationshipsRepository
+{
+    private readonly DbSet<RelationshipTemplate> _templates;
+    private readonly IQueryable<RelationshipTemplate> _readOnlyTemplates;
+    private readonly RelationshipsDbContext _dbContext;
+    private readonly IBlobStorage _blobStorage;
+    private readonly BlobOptions _blobOptions;
+    private readonly IContentStore _contentStore;
+
+    public RelationshipsRepository(RelationshipsDbContext dbContext, IBlobStorage blobStorage, IOptions<BlobOptions> blobOptions, IContentStore contentStore)
+    {
+        _templates = dbContext.RelationshipTemplates;
+        _readOnlyTemplates = dbContext.RelationshipTemplates.AsNoTracking();
+        _dbContext = dbContext;
+        _blobStorage = blobStorage;
+        _blobOptions = blobOptions.Value;
+        _contentStore = contentStore;
+    }
+
+    public async Task<RelationshipTemplateId> AddRelationshipTemplate(RelationshipTemplate template, CancellationToken cancellationToken)
+    {
+        await _contentStore.SaveContentOfTemplate(template);
+
+        var add = await _templates.AddAsync(template, cancellationToken);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+
+        return add.Entity.Id;
+    }
+}
