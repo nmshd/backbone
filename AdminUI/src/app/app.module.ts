3ce0f661ab1838254d6f170b31fb3ba2a10eca77
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-05-18 09:39:44 +0100

    feat: add material design package

--- AdminUI/src/app/app.module.ts
+++ AdminUI/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarService } from './services/sidebar-service/sidebar.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [SidebarService],
   bootstrap: [AppComponent],
 })
