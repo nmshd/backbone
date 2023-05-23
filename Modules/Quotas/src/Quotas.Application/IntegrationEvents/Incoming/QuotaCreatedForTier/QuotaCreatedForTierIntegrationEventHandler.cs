509150238c09c542cc24f11c94aa509735dfdc67
Daniel Almeida <daniel.almeida@konkconsulting.com>
2023-05-23 11:25:29 +0100

    feature: add QuotaCreatedForTierIntegrationEvent and respective handler

---
+++ Modules/Quotas/src/Quotas.Application/IntegrationEvents/Incoming/QuotaCreatedForTier/QuotaCreatedForTierIntegrationEventHandler.cs
@@ -0,0 +1,33 @@
+﻿using Backbone.Modules.Quotas.Application.Infrastructure.Persistence.Repository;
+using Backbone.Modules.Quotas.Application.IntegrationEvents.Outgoing;
+using Enmeshed.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
+using Microsoft.Extensions.Logging;
+
+namespace Backbone.Modules.Quotas.Application.IntegrationEvents.Incoming.QuotaCreatedForTier;
+
+public class QuotaCreatedForTierIntegrationEventHandler : IIntegrationEventHandler<QuotaCreatedForTierIntegrationEvent>
+{
+    private readonly IIdentitiesRepository _identitiesRepository;
+    private readonly ILogger<QuotaCreatedForTierIntegrationEventHandler> _logger;
+
+    public QuotaCreatedForTierIntegrationEventHandler(IIdentitiesRepository identitiesRepository, ILogger<QuotaCreatedForTierIntegrationEventHandler> logger)
+    {
+        _identitiesRepository = identitiesRepository;
+        _logger = logger;
+    }
+
+    public async Task Handle(QuotaCreatedForTierIntegrationEvent @event)
+    {
+        _logger.LogTrace("Handling QuotaCreatedForTierIntegrationEvent ... ");
+
+        var identitiesWithTier = _identitiesRepository.FindWithTier(@event.TierId).ToList();
+        identitiesWithTier.ForEach(identity =>
+        {
+            identity.AssignTierQuotaFromDefinition(@event.Quota);
+        });
+
+        await _identitiesRepository.Update(identitiesWithTier, CancellationToken.None);
+
+        _logger.LogTrace("Successfully created quotas for Identities!");
+    }
+}
---
+++ Modules/Quotas/src/Quotas.Application/IntegrationEvents/Outgoing/QuotaCreatedForTierIntegrationEvent.cs
@@ -0,0 +1,18 @@
+﻿using Backbone.Modules.Quotas.Domain.Aggregates.Tiers;
+using Enmeshed.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus.Events;
+
+namespace Backbone.Modules.Quotas.Application.IntegrationEvents.Outgoing;
+
+public class QuotaCreatedForTierIntegrationEvent : IntegrationEvent
+{
+    public QuotaCreatedForTierIntegrationEvent(Tier tier, TierQuotaDefinition quota) : base($"{tier.Id}/Created")
+    {
+        TierId = tier.Id;
+        TierName = tier.Name;
+        Quota = quota;
+    }
+
+    public string TierId { get; }
+    public string TierName { get; }
+    public TierQuotaDefinition Quota { get; }
+}
