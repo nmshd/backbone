4ad9cdd0edada21456edede882d2ce437f6995d0
Daniel Almeida <daniel.almeida@konkconsulting.com>
2023-05-23 18:27:19 +0100

    feature: add missing constructor to CreateQuotaForTierCommand

--- Modules/Quotas/src/Quotas.Application/Quotas/Commands/CreateQuotaForTier/CreateQuotaForTierCommand.cs
+++ Modules/Quotas/src/Quotas.Application/Quotas/Commands/CreateQuotaForTier/CreateQuotaForTierCommand.cs
@@ -6,6 +6,14 @@ namespace Backbone.Modules.Quotas.Application.Quotas.Commands.CreateQuotaForTier
 
 public class CreateQuotaForTierCommand : IRequest<CreateQuotaForTierResponse>
 {
+    public CreateQuotaForTierCommand(string tierId, Metric metric, int max, QuotaPeriod period)
+    {
+        TierId = tierId;
+        Metric = metric;
+        Max = max;
+        Period = period;
+    }
+
     public string TierId { get; set; }
     public Metric Metric { get; set; }
     public int Max { get; set; }
