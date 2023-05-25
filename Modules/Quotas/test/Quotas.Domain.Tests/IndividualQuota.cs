a0492b36f3a864fd12ddfa84c9888c98dc1dce96
filipemmagalhaes <129949843+filipemmagalhaes@users.noreply.github.com>
2023-05-25 11:32:56 +0100

    fix: remove duplicate class

--- Modules/Quotas/test/Quotas.Domain.Tests/IndividualQuota.cs
+++ Modules/Quotas/test/Quotas.Domain.Tests/IndividualQuota.cs
@@ -3,22 +3,21 @@ using Backbone.Modules.Quotas.Domain.Aggregates.Metrics;
 using FluentAssertions;
 using Xunit;
 
-namespace Backbone.Modules.Quotas.Domain.Tests
-{
-    public class IndividualQuotaTests
-    {
-        [Fact]
-        public void Creates_individual_quota_with_valid_properties()
-        {
-            var metric = new Metric(MetricKey.NumberOfSentMessages, "Number Of Sent Messages");
-            var max = 10;
-            var period = QuotaPeriod.Month;
-            var individualQuota = new IndividualQuota(metric, max, period);
+namespace Backbone.Modules.Quotas.Domain.Tests;
 
-            individualQuota.Metric.Should().Be(metric);
-            individualQuota.Period.Should().Be(period);
-            individualQuota.Max.Should().Be(max);
-            individualQuota.Weight.Should().Be(2);
-        }
+public class IndividualQuotaTests
+{
+    [Fact]
+    public void Creates_individual_quota_with_valid_properties()
+    {
+        var metric = new Metric(MetricKey.NumberOfSentMessages, "Number Of Sent Messages");
+        var max = 10;
+        var period = QuotaPeriod.Month;
+        var individualQuota = new IndividualQuota(metric, max, period);
+
+        individualQuota.Metric.Should().Be(metric);
+        individualQuota.Period.Should().Be(period);
+        individualQuota.Max.Should().Be(max);
+        individualQuota.Weight.Should().Be(2);
     }
 }
