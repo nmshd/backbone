c45304bc1136437e71aa8f08c302712b5914a195
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-05-25 13:51:08 +0100

    fix: cannot resolve UserManager in the Identities repository

---
+++ Admin.API/AspNetCoreIdentityCustomizations/CustomLookupNormalizer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Admin.API.AspNetCoreIdentityCustomizations;
+
+public class CustomLookupNormalizer : ILookupNormalizer
+{
+    public string? NormalizeName(string? name)
+    {
+        return name?.Trim().Normalize().ToUpperInvariant();
+    }
+
+    public string? NormalizeEmail(string? email)
+    {
+        return email?.Trim().Normalize().ToUpperInvariant();
+    }
+}
