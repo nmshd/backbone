498279994ff0f883e234233253a884d87a1e4bfc
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-05-31 15:57:24 +0100

    feat: add quotas service to the adminUI

---
+++ AdminUI/src/app/services/quotas-service/quotas.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PagedHttpResponseEnvelope } from 'src/app/utils/paged-http-response-envelope';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class QuotasService {
+    apiUrl: string;
+
+    constructor(private http: HttpClient) {
+        this.apiUrl = environment.apiUrl + '/Quotas';
+    }
+
+    getQuotas(
+        pageNumber: number,
+        pageSize: number
+    ): Observable<PagedHttpResponseEnvelope<Quota>> {
+        const httpOptions = {
+            params: new HttpParams()
+                .set('PageNumber', pageNumber + 1)
+                .set('PageSize', pageSize),
+        };
+
+        return this.http.get<PagedHttpResponseEnvelope<Quota>>(
+            this.apiUrl,
+            httpOptions
+        );
+    }
+}
+
+export interface Quota {
+    id?: string;
+    metric?: Metric;
+    max?: number;
+    validFrom?: Date;
+    validTo?: Date;
+    period?: string;
+}
+
+export interface Metric {
+    id?: string;
+    key?: string;
+    displayName?: string;
+}
