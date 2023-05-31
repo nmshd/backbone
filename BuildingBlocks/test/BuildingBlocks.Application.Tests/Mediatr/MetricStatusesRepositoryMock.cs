df73eed6c80c418f0b70dc2511c7104c1137e1bc
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-05-31 10:45:02 +0100

    test: add simple test for QuotaExhaustedException

---
+++ BuildingBlocks/test/BuildingBlocks.Application.Tests/Mediatr/MetricStatusesRepositoryMock.cs
@@ -0,0 +1,16 @@
+﻿using Enmeshed.BuildingBlocks.Domain;
+using Enmeshed.Common.Infrastructure.Persistence.Repository;
+using Enmeshed.DevelopmentKit.Identity.ValueObjects;
+
+public class MockMetricStatusesRepository : IMetricStatusesRepository
+{
+    public Task<IEnumerable<MetricStatus>> GetMetricStatuses(IdentityAddress identity, IEnumerable<MetricKey> keys)
+    {
+        return Task.FromResult(
+            new List<MetricStatus> {
+                new MetricStatus(new MetricKey("KeyOne"), DateTime.UtcNow.AddDays(-1)),
+                new MetricStatus(new MetricKey("KeyTwo"), DateTime.UtcNow.AddDays(1)),
+                new MetricStatus(new MetricKey("KeyThree"), DateTime.UtcNow.AddDays(1)),
+            }.AsEnumerable());
+    }
+}
---
+++ BuildingBlocks/test/BuildingBlocks.Application.Tests/Mediatr/QuotaEnforcerBehaviorTests.cs
@@ -0,0 +1,36 @@
+﻿using Enmeshed.BuildingBlocks.Application.Abstractions.Exceptions;
+using Enmeshed.BuildingBlocks.Application.Attributes;
+using Enmeshed.BuildingBlocks.Application.MediatR;
+using FluentAssertions;
+using MediatR;
+using Xunit;
+
+namespace Enmeshed.BuildingBlocks.Application.Tests.Mediatr;
+public class QuotaEnforcerBehaviorTests
+{
+    public QuotaEnforcerBehaviorTests() { }
+
+    [ApplyQuotasForMetrics("MetricKey")]
+    private class TestCommand : IRequest<IResponse> { }
+
+    private class IResponse { }
+
+    [Fact]
+    public async void Returns_()
+    {
+        // Arrange
+        var metricStatusesRepositoriesMock = new MockMetricStatusesRepository();
+        var userContextMock = new UserContextMock();
+
+        var behavior = new QuotaEnforcerBehavior<TestCommand, IResponse>(metricStatusesRepositoriesMock, userContextMock);
+
+        // Act
+        Func<Task> acting = async () => await behavior.Handle(
+            new TestCommand(),
+            () => { return Task.FromResult(new IResponse()); },
+            CancellationToken.None);
+
+        // Assert
+        await acting.Should().ThrowAsync<QuotaExhaustedException>();
+    }
+}
