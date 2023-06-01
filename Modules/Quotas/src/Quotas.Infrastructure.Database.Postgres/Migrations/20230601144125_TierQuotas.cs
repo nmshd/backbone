f41af378f6617efea3e6b76e19a585bb20a1bb35
Daniel Almeida <daniel.almeida@konkconsulting.com>
2023-06-01 15:43:18 +0100

    feat: add migrations for tier quotas

---
+++ Modules/Quotas/src/Quotas.Infrastructure.Database.Postgres/Migrations/20230601144125_TierQuotas.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quotas.Infrastructure.Database.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class TierQuotas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TierQuotaDefinitions",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "character(20)", unicode: false, fixedLength: true, maxLength: 20, nullable: false),
+                    MetricKey = table.Column<string>(type: "character varying(50)", unicode: false, maxLength: 50, nullable: false),
+                    Max = table.Column<int>(type: "integer", nullable: false),
+                    Period = table.Column<int>(type: "integer", nullable: false),
+                    TierId = table.Column<string>(type: "character(20)", unicode: false, fixedLength: true, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TierQuotaDefinitions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TierQuotaDefinitions_Tiers_TierId",
+                        column: x => x.TierId,
+                        principalTable: "Tiers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TierQuotas",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "character(20)", unicode: false, fixedLength: true, maxLength: 20, nullable: false),
+                    DefinitionId = table.Column<string>(type: "character(20)", unicode: false, fixedLength: true, maxLength: 20, nullable: true),
+                    ApplyTo = table.Column<string>(type: "character(36)", nullable: true),
+                    IsExhaustedUntil = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TierQuotas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TierQuotas_Identities_ApplyTo",
+                        column: x => x.ApplyTo,
+                        principalTable: "Identities",
+                        principalColumn: "Address");
+                    table.ForeignKey(
+                        name: "FK_TierQuotas_TierQuotaDefinitions_DefinitionId",
+                        column: x => x.DefinitionId,
+                        principalTable: "TierQuotaDefinitions",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TierQuotaDefinitions_TierId",
+                table: "TierQuotaDefinitions",
+                column: "TierId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TierQuotas_ApplyTo",
+                table: "TierQuotas",
+                column: "ApplyTo");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TierQuotas_DefinitionId",
+                table: "TierQuotas",
+                column: "DefinitionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TierQuotas");
+
+            migrationBuilder.DropTable(
+                name: "TierQuotaDefinitions");
+        }
+    }
+}
