4a5fcf49d9c36953ae5773401603c49f55f4e86c
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-06-05 12:46:31 +0100

    feat: add QuotaEnforcerBehavior

--- BuildingBlocks/src/BuildingBlocks.Application/MediatR/QuotaEnforcerBehavior.cs
+++ BuildingBlocks/src/BuildingBlocks.Application/MediatR/QuotaEnforcerBehavior.cs
@@ -1,6 +1,9 @@
 ﻿using Enmeshed.Common.Infrastructure.Persistence.Repository;
 using MediatR;
 using Enmeshed.BuildingBlocks.Application.Abstractions.Infrastructure.UserContext;
+using Enmeshed.BuildingBlocks.Application.Abstractions.Exceptions;
+using Enmeshed.BuildingBlocks.Application.Attributes;
+using Enmeshed.BuildingBlocks.Domain;
 
 namespace Enmeshed.BuildingBlocks.Application.MediatR;
 public class QuotaEnforcerBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
@@ -16,7 +19,23 @@ public class QuotaEnforcerBehavior<TRequest, TResponse> : IPipelineBehavior<TReq
 
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
-        
+        var attributes = request.GetType().CustomAttributes;
+
+        var applyQuotasForMetricsAttribute = attributes.FirstOrDefault(attribute => attribute.AttributeType == typeof(ApplyQuotasForMetricsAttribute));
+        if (applyQuotasForMetricsAttribute != null)
+        {
+            var metricKeys = applyQuotasForMetricsAttribute.ConstructorArguments.Select(it => new MetricKey(it.Value as string)).ToList();
+
+            var statuses = await _metricStatusesRepository.GetMetricStatuses(_userContext.GetAddress(), metricKeys);
+
+            var exhaustedStatuses = statuses.Where(m => m.IsExhausted).ToList();
+
+            if (exhaustedStatuses.Any())
+            {
+                throw new QuotaExhaustedException(exhaustedStatuses.ToArray());
+            }
+        }
+
         var response = await next();
         return response;
     }
