77f37cb727c2cb47c71edf81371a034bc04add67
filipemmagalhaes <129949843+filipemmagalhaes@users.noreply.github.com>
2023-06-12 14:00:27 +0100

    refactor: create response client and change code to receive response

---
+++ Modules/Devices/src/Devices.Application/Clients/Queries/ListClients/ListClientsResponse.cs
@@ -0,0 +1,7 @@
+﻿using Backbone.Modules.Devices.Application.Clients.DTOs;
+
+namespace Backbone.Modules.Devices.Application.Clients.Queries.ListClients;
+public class ListClientsResponse
+{
+    public IEnumerable<ClientDTO> Items { get; set; }
+}
