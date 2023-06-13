404f6779218e14b82c53faa26be82dd2752bc671
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-06-13 15:16:22 +0100

    fix: needless repository

--- Modules/Quotas/src/Quotas.Application/Infrastructure/Persistence/Repository/IQuotasRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using Backbone.Modules.Quotas.Domain.Aggregates.Identities;
-
-namespace Backbone.Modules.Quotas.Application.Infrastructure.Persistence.Repository;
-
-public interface IQuotasRepository
-{
-    Task<IEnumerable<Quota>> FindQuotasByMetricKey(string metricKey, CancellationToken cancellationToken);
-}
