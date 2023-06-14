622bf27878a5c0a382f6cc2b21de63b6cb564675
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-06-14 09:44:57 +0100

    fix: bad handler signature

--- Modules/Quotas/src/Quotas.Application/IntegrationEvents/Incoming/MessageCreated/MessageCreatedIntegrationEventHandler.cs
+++ Modules/Quotas/src/Quotas.Application/IntegrationEvents/Incoming/MessageCreated/MessageCreatedIntegrationEventHandler.cs
@@ -3,7 +3,7 @@
 namespace Backbone.Modules.Quotas.Application.IntegrationEvents.Incoming.MessageCreated;
 public class MessageCreatedIntegrationEventHandler : IIntegrationEventHandler<MessageCreatedIntegrationEvent>
 {
-    Task IIntegrationEventHandler<MessageCreatedIntegrationEvent>.Handle(MessageCreatedIntegrationEvent integrationEvent)
+    public Task Handle(MessageCreatedIntegrationEvent integrationEvent)
     {
         Console.WriteLine(integrationEvent.ToString());
         return Task.CompletedTask;
