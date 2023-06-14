ec03a35b574462dbb0e19d250dfcd39a8dce6d46
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-06-14 09:25:12 +0100

    feat: add message queues to quotas module.

---
+++ Modules/Quotas/src/Quotas.Application/IntegrationEvents/Incoming/MessageCreated/MessageCreatedIntegrationEvent.cs
@@ -0,0 +1,9 @@
+﻿using Backbone.Modules.Quotas.Domain.Aggregates.Tiers;
+using Enmeshed.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus.Events;
+
+namespace Backbone.Modules.Quotas.Application.IntegrationEvents.Incoming.MessageCreated;
+public class MessageCreatedIntegrationEvent : IntegrationEvent
+{
+    public string Id { get; private set; }
+    public IEnumerable<string> Recipients { get; private set; }
+}
---
+++ Modules/Quotas/src/Quotas.Application/IntegrationEvents/Incoming/MessageCreated/MessageCreatedIntegrationEventHandler.cs
@@ -0,0 +1,11 @@
+﻿using Enmeshed.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
+
+namespace Backbone.Modules.Quotas.Application.IntegrationEvents.Incoming.MessageCreated;
+public class MessageCreatedIntegrationEventHandler : IIntegrationEventHandler<MessageCreatedIntegrationEvent>
+{
+    Task IIntegrationEventHandler<MessageCreatedIntegrationEvent>.Handle(MessageCreatedIntegrationEvent integrationEvent)
+    {
+        Console.WriteLine(integrationEvent.ToString());
+        return Task.CompletedTask;
+    }
+}
