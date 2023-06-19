872c00e688f9db866e2632b8abb1d43810b41cdf
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-06-19 14:28:23 +0100

    refactor: minor changes to QuotaTests

--- Modules/Quotas/test/Quotas.Domain.Tests/QuotaTests.cs
+++ Modules/Quotas/test/Quotas.Domain.Tests/QuotaTests.cs
@@ -4,6 +4,7 @@ using FluentAssertions;
 using Xunit;
 using Backbone.Modules.Quotas.Domain.Tests.Extensions;
 using Enmeshed.Tooling;
+using Backbone.Modules.Quotas.Domain.Aggregates.Metrics;
 
 namespace Backbone.Modules.Quotas.Domain.Tests;
 public class QuotaTests
@@ -12,11 +13,11 @@ public class QuotaTests
     public void UpdateExhaustion_on_unexhausted_with_newUsage_under_max_quota_keeps_IsExhaustedUntil_null()
     {
         // Arrange
-        var definition = new TierQuotaDefinition(Aggregates.Metrics.MetricKey.NumberOfFiles, 50, QuotaPeriod.Hour);
+        var definition = new TierQuotaDefinition(MetricKey.NumberOfFiles, 2, QuotaPeriod.Hour);
         var quota = new TierQuota(definition, "applyTo");
 
         // Act
-        quota.UpdateExhaustion(20);
+        quota.UpdateExhaustion(1);
 
         // Assert
         quota.IsExhaustedUntil.Should().BeNull();
@@ -26,14 +27,13 @@ public class QuotaTests
     public void UpdateExhaustion_on_unexhausted_with_newUsage_over_max_quota_sets_IsExhaustedUntil()
     {
         // Arrange
-        var currentDate = new DateTime(2023, 01, 01, 13, 45, 00, 000, DateTimeKind.Utc);
-        SystemTime.Set(currentDate);
+        SystemTime.Set(new DateTime(2023, 01, 01, 13, 45, 00, 000, DateTimeKind.Utc));
 
-        var definition = new TierQuotaDefinition(Aggregates.Metrics.MetricKey.NumberOfFiles, 50, QuotaPeriod.Hour);
+        var definition = new TierQuotaDefinition(MetricKey.NumberOfFiles, 2, QuotaPeriod.Hour);
         var quota = new TierQuota(definition, "applyTo");
 
         // Act
-        quota.UpdateExhaustion(50);
+        quota.UpdateExhaustion(2);
 
         // Assert
         quota.IsExhaustedUntil.Should().Be("2023-01-01T13:59:59.999");
@@ -43,15 +43,12 @@ public class QuotaTests
     public void UpdateExhaustion_on_exhausted_with_newUsage_under_max_quota_updates_IsExhaustedUntil_to_null()
     {
         // Arrange
-        var currentDate = new DateTime(2023, 01, 01, 13, 45, 00, 000, DateTimeKind.Utc);
-        SystemTime.Set(currentDate);
-
-        var definition = new TierQuotaDefinition(Aggregates.Metrics.MetricKey.NumberOfFiles, 50, QuotaPeriod.Hour);
+        var definition = new TierQuotaDefinition(MetricKey.NumberOfFiles, 2, QuotaPeriod.Hour);
         var quota = new TierQuota(definition, "applyTo");
 
         // Act
-        quota.UpdateExhaustion(60);
-        quota.UpdateExhaustion(30);
+        quota.UpdateExhaustion(3);
+        quota.UpdateExhaustion(1);
 
         // Assert
         quota.IsExhaustedUntil.Should().BeNull();
