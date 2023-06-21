3127c8be111c6baf12ce01cac001b176366112ed
filipemmagalhaes <129949843+filipemmagalhaes@users.noreply.github.com>
2023-06-21 09:01:28 +0100

    feat: create list quotas for tier return UI

---
+++ Modules/Quotas/src/Quotas.Application/Tiers/Queries/ListQuotasForTier/ListQuotasForTierQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Backbone.Modules.Quotas.Application.Tiers.Queries.ListQuotaForTier;
+public class ListQuotasForTierQuery : IRequest<ListQuotasForTierResponse>
+{
+    public ListQuotasForTierQuery(string tierId)
+    {
+        TierId = tierId;
+    }
+
+    public string TierId { get; set; }
+}
---
+++ Modules/Quotas/src/Quotas.Application/Tiers/Queries/ListQuotasForTier/ListQuotasForTierResponse.cs
@@ -0,0 +1,8 @@
+﻿using Backbone.Modules.Quotas.Application.DTOs;
+using Enmeshed.BuildingBlocks.Application.CQRS.BaseClasses;
+
+namespace Backbone.Modules.Quotas.Application.Tiers.Queries.ListQuotaForTier;
+public class ListQuotasForTierResponse : EnumerableResponseBase<TierQuotaDefinitionDTO>
+{
+    public ListQuotasForTierResponse(IEnumerable<TierQuotaDefinitionDTO> items) : base(items) { }
+}
