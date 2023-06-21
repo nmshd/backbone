1693b0c5fc00109a9dfa09940ccc43237752eea2
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-06-21 14:51:46 +0100

    refactor: fix test data parameters order

--- Modules/Quotas/test/Quotas.Domain.Tests/QuotaPeriodTests.cs
+++ Modules/Quotas/test/Quotas.Domain.Tests/QuotaPeriodTests.cs
@@ -9,13 +9,13 @@ namespace Backbone.Modules.Quotas.Domain.Tests;
 public class QuotaPeriodTests
 {
     [Theory]
-    [InlineData("2023-01-01T13:45:00.000", "2023-01-01T13:00:00.000", QuotaPeriod.Hour)]
-    [InlineData("2023-01-01T13:45:00.000", "2023-01-01T00:00:00.000", QuotaPeriod.Day)]
-    [InlineData("2023-06-01T12:00:00.000", "2023-05-29T00:00:00.000", QuotaPeriod.Week)]
-    [InlineData("2020-01-03T12:00:00.000", "2019-12-30T00:00:00.000", QuotaPeriod.Week)]
-    [InlineData("2020-02-03T12:00:00.000", "2020-02-01T00:00:00.000", QuotaPeriod.Month)]
-    [InlineData("2020-01-03T12:00:00.000", "2020-01-01T00:00:00.000", QuotaPeriod.Year)]
-    public void Begin(string currentDateString, string targetDate, QuotaPeriod quotaPeriod)
+    [InlineData("2023-01-01T13:45:00.000", QuotaPeriod.Hour, "2023-01-01T13:00:00.000")]
+    [InlineData("2023-01-01T13:45:00.000", QuotaPeriod.Day, "2023-01-01T00:00:00.000")]
+    [InlineData("2023-06-01T12:00:00.000", QuotaPeriod.Week, "2023-05-29T00:00:00.000")]
+    [InlineData("2020-01-03T12:00:00.000", QuotaPeriod.Week, "2019-12-30T00:00:00.000")]
+    [InlineData("2020-02-03T12:00:00.000", QuotaPeriod.Month, "2020-02-01T00:00:00.000")]
+    [InlineData("2020-01-03T12:00:00.000", QuotaPeriod.Year, "2020-01-01T00:00:00.000")]
+    public void Begin(string currentDateString,  QuotaPeriod quotaPeriod, string targetDate)
     {
         // Arrange
         var currentDate = DateTime.Parse(currentDateString);
@@ -29,13 +29,13 @@ public class QuotaPeriodTests
     }
 
     [Theory]
-    [InlineData("2023-01-01T13:45:00.000", "2023-01-01T13:59:59.999", QuotaPeriod.Hour)]
-    [InlineData("2023-01-01T13:45:00.000", "2023-01-01T23:59:59.999", QuotaPeriod.Day)]
-    [InlineData("2020-01-01T12:00:00.000", "2020-01-05T23:59:59.999", QuotaPeriod.Week)]
-    [InlineData("2024-01-03T12:00:00.000", "2024-01-07T23:59:59.999", QuotaPeriod.Week)]
-    [InlineData("2024-02-03T12:00:00.000", "2024-02-29T23:59:59.999", QuotaPeriod.Month)]
-    [InlineData("2024-02-03T12:00:00.000", "2024-12-31T23:59:59.999", QuotaPeriod.Year)]
-    public void End(string currentDateString, string targetDate, QuotaPeriod quotaPeriod)
+    [InlineData("2023-01-01T13:45:00.000", QuotaPeriod.Hour, "2023-01-01T13:59:59.999")]
+    [InlineData("2023-01-01T13:45:00.000", QuotaPeriod.Day, "2023-01-01T23:59:59.999")]
+    [InlineData("2020-01-01T12:00:00.000", QuotaPeriod.Week, "2020-01-05T23:59:59.999")]
+    [InlineData("2024-01-03T12:00:00.000", QuotaPeriod.Week, "2024-01-07T23:59:59.999")]
+    [InlineData("2024-02-03T12:00:00.000", QuotaPeriod.Month, "2024-02-29T23:59:59.999")]
+    [InlineData("2024-02-03T12:00:00.000", QuotaPeriod.Year, "2024-12-31T23:59:59.999")]
+    public void End(string currentDateString, QuotaPeriod quotaPeriod, string targetDate)
     {
         // Arrange
         var currentDate = DateTime.Parse(currentDateString);
