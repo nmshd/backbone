c83d37b495273bdddbb6fe78c284c9d5e47448a6
Filipe Magalhães <129949843+filipemmagalhaes@users.noreply.github.com>
2023-06-23 11:03:35 +0100

    Admin UI for creation of Tier Quotas

--- AdminUI/src/app/services/client-service/client-service.ts
+++ AdminUI/src/app/services/client-service/client-service.ts
@@ -5,11 +5,11 @@ import { PagedHttpResponseEnvelope } from 'src/app/utils/paged-http-response-env
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class ClientServiceService {
-  apiUrl: string;
-  constructor(private http: HttpClient) {
+    apiUrl: string;
+    constructor(private http: HttpClient) {
         this.apiUrl = environment.apiUrl + '/Clients';
     }
 
@@ -18,20 +18,19 @@ export class ClientServiceService {
         pageSize: number
     ): Observable<PagedHttpResponseEnvelope<Client>> {
         const httpOptions = {
-          params: new HttpParams()
-              .set('PageNumber', pageNumber + 1)
-              .set('PageSize', pageSize),
+            params: new HttpParams()
+                .set('PageNumber', pageNumber + 1)
+                .set('PageSize', pageSize),
         };
 
-          return this.http.get<PagedHttpResponseEnvelope<Client>>(
-              this.apiUrl,
-              httpOptions
-              );
-            }
-        }
-        
-export interface Client {
-  clientId?: string;
-  displayName?: string;
+        return this.http.get<PagedHttpResponseEnvelope<Client>>(
+            this.apiUrl,
+            httpOptions
+        );
+    }
 }
-        
+
+export interface Client {
+    clientId: string;
+    displayName: string;
+}
