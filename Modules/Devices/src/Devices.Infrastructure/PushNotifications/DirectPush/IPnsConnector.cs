6ddbd7671ec28ddf05547f715887de1369eac697
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-06-28 17:41:23 +0100

    feat: implement IPnsConnector

---
+++ Modules/Devices/src/Devices.Infrastructure/PushNotifications/DirectPush/IPnsConnector.cs
@@ -0,0 +1,5 @@
+﻿namespace Backbone.Modules.Devices.Infrastructure.PushNotifications.DirectPush;
+public interface IPnsConnector
+{
+    Task Send(IEnumerable<PnsRegistration> registrations, object notification);
+}
