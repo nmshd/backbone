3bac97ce0a2bc00f37bd9dca20f99fd9158dba74
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-06-28 17:39:24 +0100

    feat: implement IPnsRegistrationRepository

---
+++ Modules/Devices/src/Devices.Infrastructure/PushNotifications/DirectPush/IPnsRegistrationRepository.cs
@@ -0,0 +1,10 @@
+﻿using Enmeshed.DevelopmentKit.Identity.ValueObjects;
+
+namespace Backbone.Modules.Devices.Infrastructure.PushNotifications.DirectPush;
+public interface IPnsRegistrationRepository
+{
+    Task Add(PnsRegistration registration, CancellationToken cancellationToken);
+    Task Update(PnsRegistration registration, CancellationToken cancellationToken);
+    Task<IEnumerable<PnsRegistration>> FindWithAddress(IdentityAddress address, CancellationToken cancellationToken);
+    Task<PnsRegistration> GetByDeviceId(DeviceId deviceId, CancellationToken cancellationToken);
+}
