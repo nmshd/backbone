51544bb9b67671ac986cc4082b983e6a8b7d70b1
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-07-03 13:37:41 +0100

    feat: add SqlServer migrations for PnsRegistration

--- Modules/Devices/src/Devices.Infrastructure.Database.SqlServer/Migrations/20230703121255_PnsRegistration.Designer.cs
+++ Modules/Devices/src/Devices.Infrastructure.Database.SqlServer/Migrations/20230703121255_PnsRegistration.Designer.cs
@@ -4,16 +4,19 @@ using Backbone.Modules.Devices.Infrastructure.Persistence.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace Backbone.Modules.Devices.Infrastructure.Database.SqlServer.Migrations
+namespace Devices.Infrastructure.Database.SqlServer.Migrations
 {
     [DbContext(typeof(DevicesDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230703121255_PnsRegistration")]
+    partial class PnsRegistration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Modules/Devices/src/Devices.Infrastructure.Database.SqlServer/Migrations/20230703121255_PnsRegistration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Devices.Infrastructure.Database.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class PnsRegistration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PnsRegistration",
+                columns: table => new
+                {
+                    Handle = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    IdentityAddress = table.Column<string>(type: "char(36)", unicode: false, fixedLength: true, maxLength: 36, nullable: true),
+                    DeviceId = table.Column<string>(type: "char(20)", unicode: false, fixedLength: true, maxLength: 20, nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PnsRegistration", x => x.Handle);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PnsRegistration");
+        }
+    }
+}
