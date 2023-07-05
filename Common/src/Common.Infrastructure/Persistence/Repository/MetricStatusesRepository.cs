efc8c07e4a95ac8cf69463c436fb1a9ae9ec9896
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-07-05 09:29:28 +0100

    chore: remove unused variables and simplify MetricStatusesRepository

--- Common/src/Common.Infrastructure/Persistence/Repository/MetricStatusesRepository.cs
+++ Common/src/Common.Infrastructure/Persistence/Repository/MetricStatusesRepository.cs
@@ -16,17 +16,13 @@ public class MetricStatusesRepository : IMetricStatusesRepository
 
     public async Task<IEnumerable<MetricStatus>> GetMetricStatuses(IdentityAddress identityAddress, IEnumerable<MetricKey> keys)
     {
-        var dapperIdentityAddress = identityAddress.ToString();
-        var dapperKeys = keys.Select(k => k.Value);
-        var query = "SELECT * FROM MetricStatus WHERE Owner = @identityAddress and MetricKey IN @keys";
-        using (var connection = _context.Connection)
+        const string QUERY = "SELECT * FROM MetricStatus WHERE Owner = @identityAddress and MetricKey IN @keys";
+        using var connection = _context.Connection;
+        var metricStatuses = await connection.QueryAsync<MetricStatus>(QUERY, new
         {
-            var metricStatuses = await connection.QueryAsync<MetricStatus>(query, new
-            {
-                identityAddress = dapperIdentityAddress = identityAddress.ToString(),
-                keys = keys.Select(x => x.Value)
-            });
-            return metricStatuses.ToList();
-        }
+            identityAddress = identityAddress.ToString(),
+            keys = keys.Select(x => x.Value)
+        });
+        return metricStatuses.ToList();
     }
 }
