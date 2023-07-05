790e5bd8b4cfc89860a9eda812a13594cefe4a53
Timo Notheisen <timo.notheisen@js-soft.com>
2023-07-05 11:23:43 +0200

    test: fix method name

--- Modules/Quotas/test/Quotas.Application.Tests/Tests/QuotaCheck/QuotaCheckerImplTests.cs
+++ Modules/Quotas/test/Quotas.Application.Tests/Tests/QuotaCheck/QuotaCheckerImplTests.cs
@@ -16,7 +16,7 @@ public class QuotaCheckerImplTests
     public async Task ExhaustedStatuses_is_empty_when_the_metric_is_not_exhausted()
     {
         // Arrange
-        var quotaChecker = CreateQuotaCheckerImplementation(new MetricStatus(TestMetricKey, null));
+        var quotaChecker = CreateQuotaCheckerImpl(new MetricStatus(TestMetricKey, null));
 
         // Act
         var result = await quotaChecker.CheckQuotaExhaustion(new[] { TestMetricKey });
@@ -30,7 +30,7 @@ public class QuotaCheckerImplTests
     public async Task ExhaustedStatuses_has_value_when_one_metric_is_exhausted()
     {
         // Arrange
-        var quotaChecker = CreateQuotaCheckerImplementation(new MetricStatus(TestMetricKey, SystemTime.UtcNow.AddMinutes(10)));
+        var quotaChecker = CreateQuotaCheckerImpl(new MetricStatus(TestMetricKey, SystemTime.UtcNow.AddMinutes(10)));
 
         // Act
         var result = await quotaChecker.CheckQuotaExhaustion(new[] { TestMetricKey });
@@ -44,7 +44,7 @@ public class QuotaCheckerImplTests
     public async Task ExhaustedStatuses_has_values_when_several_metrics_are_exhausted()
     {
         // Arrange
-        var quotaChecker = CreateQuotaCheckerImplementation(
+        var quotaChecker = CreateQuotaCheckerImpl(
             new MetricStatus(TestMetricKey, SystemTime.UtcNow.AddMinutes(10)),
             new MetricStatus(AnotherTestMetricKey, SystemTime.UtcNow.AddMinutes(10))
             );
@@ -61,7 +61,7 @@ public class QuotaCheckerImplTests
     public async Task ExhaustedStatuses_has_values_when_one_metric_is_exhausted()
     {
         // Arrange
-        var quotaChecker = CreateQuotaCheckerImplementation(
+        var quotaChecker = CreateQuotaCheckerImpl(
             new MetricStatus(TestMetricKey, SystemTime.UtcNow.AddMinutes(-10)),
             new MetricStatus(AnotherTestMetricKey, SystemTime.UtcNow.AddMinutes(10))
             );
@@ -75,7 +75,7 @@ public class QuotaCheckerImplTests
         result.IsSuccess.Should().BeFalse();
     }
 
-    private static QuotaCheckerImpl CreateQuotaCheckerImplementation(params MetricStatus[] metricStatuses)
+    private static QuotaCheckerImpl CreateQuotaCheckerImpl(params MetricStatus[] metricStatuses)
     {
         return new QuotaCheckerImpl(new UserContextStub(), new MetricStatusesStubRepository(metricStatuses.ToList()));
     }
