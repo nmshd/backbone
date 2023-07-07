929401b4f4b1d789ee957ad8902be986b86ad646
Timo Notheisen <timo.notheisen@js-soft.com>
2023-07-07 13:27:50 +0200

    feat: add ValidateApiKey endpoint

---
+++ AdminUi/src/AdminUi/Controllers/ApiKeyValidationController.cs
@@ -0,0 +1,22 @@
+﻿using AdminUi.AspNet;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminUi.Controllers;
+
+[Route("api/v1/ValidateApiKey")]
+public class ApiKeyValidationController : ControllerBase
+{
+    [HttpPost]
+    [AllowAnonymous]
+    public IActionResult ValidateApiKey([FromBody] ValidateApiKeyRequest? request, [FromServices] ApiKeyValidator apiKeyValidator)
+    {
+        var apiKeyIsValid = apiKeyValidator.IsApiKeyValid(request?.ApiKey);
+        return Ok(new { isValid = apiKeyIsValid });
+    }
+
+    public class ValidateApiKeyRequest
+    {
+        public string? ApiKey { get; set; }
+    }
+}
