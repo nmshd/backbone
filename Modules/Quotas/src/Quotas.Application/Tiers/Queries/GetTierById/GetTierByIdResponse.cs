19748912dd11a5415dde58b2dc1d2c37683232ce
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-07-07 14:00:35 +0100

    feat: add GetTierById query to Quotas module

---
+++ Modules/Quotas/src/Quotas.Application/Tiers/Queries/GetTierById/GetTierByIdResponse.cs
@@ -0,0 +1,8 @@
+﻿using Backbone.Modules.Quotas.Application.DTOs;
+
+namespace Backbone.Modules.Quotas.Application.Tiers.Queries.GetTierById;
+public class GetTierByIdResponse : TierDTO
+{
+    public GetTierByIdResponse(string id, string name, IEnumerable<TierQuotaDefinitionDTO> quotas) : base(id, name, quotas) { }
+
+}
---
+++ Modules/Quotas/src/Quotas.Application/Tiers/Queries/GetTierById/Handler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Backbone.Modules.Quotas.Application.DTOs;
+using Backbone.Modules.Quotas.Application.Infrastructure.Persistence.Repository;
+using MediatR;
+
+namespace Backbone.Modules.Quotas.Application.Tiers.Queries.GetTierById;
+public class Handler : IRequestHandler<GetTierByIdQuery, GetTierByIdResponse>
+{
+    private readonly ITiersRepository _tiersRepository;
+    private readonly IMapper _mapper;
+
+    public Handler(ITiersRepository tiersRepository, IMapper mapper)
+    {
+        _tiersRepository = tiersRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<GetTierByIdResponse> Handle(GetTierByIdQuery request, CancellationToken cancellationToken)
+    {
+        var tier = await _tiersRepository.Find(request.Id, cancellationToken);
+
+        var response = _mapper.Map<TierDTO>(tier);
+
+        return new GetTierByIdResponse(response.Id, response.Name, response.Quotas);
+    }
+}
