bf1dd41a5e1173d35e9d62c71637e24c2853ab4a
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-07-10 10:17:21 +0100

    chore: move Connector Implementation to proper class

--- Modules/Devices/src/Devices.Infrastructure/PushNotifications/DirectPush/PnsConnectorFactoryImpl.cs
+++ Modules/Devices/src/Devices.Infrastructure/PushNotifications/DirectPush/PnsConnectorFactoryImpl.cs
@@ -1,7 +1,18 @@
-﻿namespace Backbone.Modules.Devices.Infrastructure.PushNotifications.DirectPush;
+﻿using Backbone.Modules.Devices.Infrastructure.PushNotifications.FirebaseCloudMessaging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Backbone.Modules.Devices.Infrastructure.PushNotifications.DirectPush;
 public class PnsConnectorFactoryImpl : PnsConnectorFactory
 {
-    public PnsConnectorFactoryImpl(IServiceProvider serviceProvider) : base(serviceProvider)
+    private readonly IServiceProvider _serviceProvider;
+
+    public PnsConnectorFactoryImpl(IServiceProvider serviceProvider)
     {
+        _serviceProvider = serviceProvider;
+    }
+
+    protected override IPnsConnector CreateForFirebaseCloudMessaging()
+    {
+        return _serviceProvider.GetRequiredService<FirebaseCloudMessagingConnector>();
     }
 }
