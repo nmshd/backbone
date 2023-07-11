bf86c2ceba0d60fff6078d9d9a2230a884e550b8
Timo Notheisen <timo.notheisen@js-soft.com>
2023-07-11 14:16:20 +0200

    chore: uncomment postgres volume to reduce work to switch database

--- Common/src/Common.Infrastructure/Persistence/Repository/MetricStatusesRepository.cs
+++ Common/src/Common.Infrastructure/Persistence/Repository/MetricStatusesRepository.cs
@@ -1,29 +1,58 @@
+using System.Data;
+using Dapper;
 using Enmeshed.BuildingBlocks.Domain;
 using Enmeshed.DevelopmentKit.Identity.ValueObjects;
-using Enmeshed.Common.Infrastructure.Persistence.Context;
-using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Options;
+using Npgsql;
 
 namespace Enmeshed.Common.Infrastructure.Persistence.Repository;
 
 public class MetricStatusesRepository : IMetricStatusesRepository
 {
-    private const string QUERY = "SELECT * FROM MetricStatuses WHERE Owner = @identityAddress and MetricKey IN @keys";
+    private const string SQLSERVER = "SqlServer";
+    private const string POSTGRES = "Postgres";
 
-    public MetricStatusesRepository(MetricStatusesDapperContext context)
+    private readonly MetricStatusesRepositoryOptions _options;
+    private readonly string _query;
+
+
+    public MetricStatusesRepository(IOptions<MetricStatusesRepositoryOptions> options)
     {
-        _context = context;
+        _options = options.Value;
+
+        const string query = "SELECT * FROM MetricStatuses WHERE Owner = @identityAddress AND MetricKey IN @keys";
+        const string queryPostgres = """SELECT * FROM "MetricStatuses" WHERE "Owner" = @identityAddress AND "MetricKey" = ANY(@keys)""";
+        _query = _options.Provider == POSTGRES ? queryPostgres : query;
     }
 
-    private readonly MetricStatusesDapperContext _context;
+    private IDbConnection CreateConnection()
+    {
+        return _options.Provider switch
+        {
+            SQLSERVER => new SqlConnection(_options.ConnectionString),
+            POSTGRES => new NpgsqlConnection(_options.ConnectionString),
+            _ => throw new NotSupportedException($"Unsupported database provider: '{_options.Provider}'")
+        };
+    }
 
     public async Task<IEnumerable<MetricStatus>> GetMetricStatuses(IdentityAddress identityAddress, IEnumerable<MetricKey> keys)
     {
-        using var connection = _context.Connection;
-        var metricStatuses = await connection.QueryAsync<MetricStatus>(QUERY, new
+        var connection = CreateConnection();
+        var metricStatuses = await connection.QueryAsync<MetricStatus>(_query, new
         {
             identityAddress = identityAddress.ToString(),
-            keys = keys.Select(x => x.Value)
+            keys = keys.Select(x => x.Value).ToArray()
         });
+
+        connection.Dispose();
+
         return metricStatuses;
     }
+}
+
+public class MetricStatusesRepositoryOptions
+{
+    public string ConnectionString = string.Empty;
+    public string Provider = string.Empty;
 }
