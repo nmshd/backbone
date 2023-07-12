058175f84f807543b442a92450174b2035667da4
Timo Notheisen <timo.notheisen@js-soft.com>
2023-07-12 14:59:01 +0200

    test: simplify/improve FcmMessageBuilderTests

--- Modules/Devices/test/Devices.Infrastructure.Tests/Tests/DirectPush/FcmMessageBuilderTests.cs
+++ Modules/Devices/test/Devices.Infrastructure.Tests/Tests/DirectPush/FcmMessageBuilderTests.cs
@@ -15,23 +15,22 @@ public class FcmMessageBuilderTests
     [Fact]
     public void Message_Without_Content_Matches_Expected_Json()
     {
-        // Arrange
-        var notificationTitle = "someNotificationTextTitle";
-        var notificationText = "someNotificationTextBody";
-
         // Act
         var message = new FcmMessageBuilder()
             .SetTag(1)
-            .SetNotificationText(notificationTitle, notificationText)
+            .SetNotificationText("someNotificationTextTitle", "someNotificationTextBody")
+            .AddContent(new NotificationContent(IdentityAddress.Parse("id1KJnD8ipfckRQ1ivAhNVLtypmcVM5vPX4j"), new { SomeProperty = "someValue" }))
             .Build();
 
         // Assert
-        message.Notification.Title.Should().Be(notificationTitle);
-        message.Notification.Body.Should().Be(notificationText);
-        message.Android.Notification.ChannelId.Should().Be("ENMESHED");
+        message.Notification.Title.Should().Be("someNotificationTextTitle");
+        message.Notification.Body.Should().Be("someNotificationTextBody");
 
+        message.Android.Notification.ChannelId.Should().Be("ENMESHED");
         message.Data.Should().Contain("android_channel_id", "ENMESHED");
-        
+
+        message.Data["content-available"].Should().Be("1");
+
         message.Android.CollapseKey.Should().Be("1");
         message.Data.Should().Contain("tag", "1");
 
@@ -43,15 +42,6 @@ public class FcmMessageBuilderTests
         // Arrange
         SystemTime.Set(DateTime.Parse("2021-01-01T00:00:00.000Z"));
 
-        var expectedContentJson = FormatJson(@"{
-          'accRef': 'id1KJnD8ipfckRQ1ivAhNVLtypmcVM5vPX4j',
-          'eventName': 'dynamic',
-          'sentAt': '2021-01-01T00:00:00Z',
-          'payload': {
-            'SomeProperty': 'someValue'
-          }
-        }");
-
         // Act
         var message = new FcmMessageBuilder()
             .AddContent(new NotificationContent(IdentityAddress.Parse("id1KJnD8ipfckRQ1ivAhNVLtypmcVM5vPX4j"), new { SomeProperty = "someValue" }))
@@ -59,25 +49,26 @@ public class FcmMessageBuilderTests
         var contentJson = FormatJson(message.Data["content"]);
 
         // Assert
-        message.Data["content-available"].Should().Be("1");
         message.Data["content"].Should().BeValidJson();
-        contentJson.Should().Be(expectedContentJson);
+        contentJson.Should().Be(FormatJson(@"{
+          'accRef': 'id1KJnD8ipfckRQ1ivAhNVLtypmcVM5vPX4j',
+          'eventName': 'dynamic',
+          'sentAt': '2021-01-01T00:00:00Z',
+          'payload': {
+            'SomeProperty': 'someValue'
+          }
+        }"));
     }
 
-    private string FormatJson(string jsonString)
+    private static string FormatJson(string jsonString)
     {
         jsonString = jsonString.Replace("'", "\"");
-        var jsonElement = JsonSerializer.Deserialize<JsonElement>(jsonString);
-        return FormatJson(jsonElement);
-    }
 
-    private string FormatJson(object obj)
-    {
-        var options = new JsonSerializerOptions
+        var deserialized = JsonSerializer.Deserialize<JsonElement>(jsonString);
+        
+        return JsonSerializer.Serialize(deserialized, new JsonSerializerOptions
         {
             WriteIndented = true,
-        };
-
-        return JsonSerializer.Serialize(obj, options);
+        });
     }
 }
