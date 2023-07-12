85c494272928ca1aaacc8f449d2f25cc3215c537
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-07-12 08:58:16 +0100

    chore: remove batch processing for FCM Send

--- Modules/Devices/src/Devices.Infrastructure/PushNotifications/FirebaseCloudMessaging/FirebaseCloudMessagingConnector.cs
+++ Modules/Devices/src/Devices.Infrastructure/PushNotifications/FirebaseCloudMessaging/FirebaseCloudMessagingConnector.cs
@@ -6,38 +6,40 @@ using Backbone.Modules.Devices.Infrastructure.PushNotifications.AzureNotificatio
 using Backbone.Modules.Devices.Infrastructure.PushNotifications.DirectPush;
 using Enmeshed.DevelopmentKit.Identity.ValueObjects;
 using FirebaseAdmin.Messaging;
+using Microsoft.Extensions.Logging;
 
 namespace Backbone.Modules.Devices.Infrastructure.PushNotifications.FirebaseCloudMessaging;
 public class FirebaseCloudMessagingConnector : IPnsConnector
 {
     private readonly FirebaseMessaging _firebaseMessaging;
+    private readonly ILogger<FirebaseCloudMessagingConnector> _logger;
 
-    public FirebaseCloudMessagingConnector(FirebaseMessaging firebaseMessaging)
+    public FirebaseCloudMessagingConnector(ILogger<FirebaseCloudMessagingConnector> logger, FirebaseMessaging firebaseMessaging)
     {
         _firebaseMessaging = firebaseMessaging;
+        _logger = logger;
     }
 
-    public async Task Send(IEnumerable<PnsRegistration> registrations, IdentityAddress recipient,
-        object notification)
+    public async Task Send(IEnumerable<PnsRegistration> registrations, IdentityAddress recipient, object notification)
     {
         var recipients = registrations.Select(r => r.Handle.Value).ToList();
-        var recipientsBatches = recipients.Split(500);
+        if (recipients.Count > 500)
+        {
+            _logger.LogWarning($"There was an attempt to send an FCM MulticastMessage to more than 500 devices ({recipients.Count}) for the recipient with address '{recipient}'.");
+        }
 
         var (notificationTitle, notificationBody) = GetNotificationText(notification);
         var notificationId = GetNotificationId(notification);
         var notificationContent = new NotificationContent(recipient, notification);
 
-        foreach (var batch in recipientsBatches)
-        {
-            var message = new FcmMessageBuilder()
-                .AddContent(notificationContent)
-                .SetNotificationText(notificationTitle, notificationBody)
-                .SetTag(notificationId)
-                .SetTokens(batch.ToImmutableList())
-                .Build();
+        var message = new FcmMessageBuilder()
+            .AddContent(notificationContent)
+            .SetNotificationText(notificationTitle, notificationBody)
+            .SetTag(notificationId)
+            .SetTokens(recipients.ToImmutableList())
+            .Build();
 
-            await _firebaseMessaging.SendMulticastAsync(message);
-        }
+        await _firebaseMessaging.SendMulticastAsync(message);
     }
 
     private static (string Title, string Body) GetNotificationText(object pushNotification)
