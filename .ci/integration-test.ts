7cfc02e0a64b20597768b105499cd9b96899b605
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-07-14 08:40:48 +0100

    chore: remove dotnet restore call

--- .ci/integration-test.ts
+++ .ci/integration-test.ts
@@ -2,7 +2,6 @@
 
 import { $ } from "zx";
 
-await $`dotnet restore "Backbone.sln" && dotnet build  /property:WarningLevel=0 --no-restore "Backbone.sln"`;
 await $`docker compose -f ./docker-compose/docker-compose.test.yml up consumer-api admin-ui seed-mssql seed-client -d`;
 await $`dotnet test --no-restore --no-build "Backbone.sln" --filter "Category=Integration&TestCategory!~ignore"`;
 await $`docker compose -f ./docker-compose/docker-compose.test.yml down`;
