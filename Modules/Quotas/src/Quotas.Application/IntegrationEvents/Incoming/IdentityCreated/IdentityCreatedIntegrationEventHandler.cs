e544500c004307ed6a8b9377d4f964d2879980b3
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-07-14 13:52:05 +0100

    chore: use foreach

--- Modules/Quotas/src/Quotas.Application/IntegrationEvents/Incoming/IdentityCreated/IdentityCreatedIntegrationEventHandler.cs
+++ Modules/Quotas/src/Quotas.Application/IntegrationEvents/Incoming/IdentityCreated/IdentityCreatedIntegrationEventHandler.cs
@@ -27,7 +27,10 @@ public class IdentityCreatedIntegrationEventHandler : IIntegrationEventHandler<I
 
         var tier = await _tiersRepository.Find(identity.TierId, CancellationToken.None, track: true);
 
-        tier.Quotas.ForEach(identity.AssignTierQuotaFromDefinition);
+        foreach (var tierQuotaDefinition in tier.Quotas)
+        {
+            identity.AssignTierQuotaFromDefinition(tierQuotaDefinition);
+        }
 
         await _identitiesRepository.Add(identity, CancellationToken.None);
 
