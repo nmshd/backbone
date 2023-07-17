bd2656db27212183c00e614a13cad8f430a65649
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-07-17 11:21:56 +0100

    fix: add custom mapping from Metric key to MetricDTO string

--- Modules/Quotas/src/Quotas.Application/DTOs/MetricDTO.cs
+++ Modules/Quotas/src/Quotas.Application/DTOs/MetricDTO.cs
@@ -1,9 +1,10 @@
-﻿using Backbone.Modules.Quotas.Domain.Aggregates.Metrics;
+﻿using AutoMapper;
+using Backbone.Modules.Quotas.Domain.Aggregates.Metrics;
 using Enmeshed.BuildingBlocks.Application.Abstractions.Infrastructure.Mapping;
 
 namespace Backbone.Modules.Quotas.Application.DTOs;
 
-public class MetricDTO : IMapTo<Metric>
+public class MetricDTO : IHaveCustomMapping
 {
     public MetricDTO() {}
 
@@ -15,4 +16,10 @@ public class MetricDTO : IMapTo<Metric>
 
     public string Key { get; set; }
     public string DisplayName { get; set; }
+
+    public void CreateMappings(Profile configuration)
+    {
+        configuration.CreateMap<Metric, MetricDTO>()
+            .ForMember(dto => dto.Key, expression => expression.MapFrom(m => m.Key.Value));
+    }
 }
