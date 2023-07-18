08c7929a38db98a7e158ed46f5d8f18afaa95d73
Daniel Almeida <daniel.almeida@konkconsulting.com>
2023-07-18 17:33:42 +0100

    feat: add entity type configurations for individual quotas

---
+++ Modules/Quotas/src/Quotas.Infrastructure/Persistence/Database/EntityConfigurations/IndividualQuotaEntityTypeConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Backbone.Modules.Quotas.Domain.Aggregates.Identities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Backbone.Modules.Quotas.Infrastructure.Persistence.Database.EntityConfigurations;
+
+public class IndividualQuotaEntityTypeConfiguration : IEntityTypeConfiguration<IndividualQuota>
+{
+    public void Configure(EntityTypeBuilder<IndividualQuota> builder)
+    {
+        builder.HasKey(x => x.Id);
+    }
+}
