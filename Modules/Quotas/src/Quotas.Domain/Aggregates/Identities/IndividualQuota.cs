c061ef69b6500d6828483b7e81bf98e50f9d9fe6
Daniel Almeida <daniel.almeida@konkconsulting.com>
2023-07-19 11:13:02 +0100

    feat: add dbset for individual quotas and private constructor for migrations

--- Modules/Quotas/src/Quotas.Domain/Aggregates/Identities/IndividualQuota.cs
+++ Modules/Quotas/src/Quotas.Domain/Aggregates/Identities/IndividualQuota.cs
@@ -4,6 +4,8 @@ namespace Backbone.Modules.Quotas.Domain.Aggregates.Identities;
 
 public class IndividualQuota : Quota
 {
+    private IndividualQuota() { }
+
     public IndividualQuota(MetricKey metricKey, int max, QuotaPeriod period, string applyTo) : base(applyTo)
     {
         MetricKey = metricKey;
--- Modules/Quotas/src/Quotas.Infrastructure/Persistence/Database/QuotasDbContext.cs
+++ Modules/Quotas/src/Quotas.Infrastructure/Persistence/Database/QuotasDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Backbone.Modules.Quotas.Domain.Aggregates.Identities;
+using Backbone.Modules.Quotas.Domain.Aggregates.Messages;
 using Backbone.Modules.Quotas.Domain.Aggregates.Metrics;
 using Backbone.Modules.Quotas.Domain.Aggregates.Tiers;
-using Backbone.Modules.Quotas.Domain.Aggregates.Messages;
 using Backbone.Modules.Quotas.Infrastructure.Persistence.Database.ValueConverters;
 using Enmeshed.BuildingBlocks.Infrastructure.Persistence.Database;
 using Enmeshed.BuildingBlocks.Infrastructure.Persistence.Database.ValueConverters;
@@ -18,6 +18,8 @@ public class QuotasDbContext : AbstractDbContextBase
     public DbSet<Tier> Tiers { get; set; }
 
     public DbSet<TierQuota> TierQuotas { get; set; }
+
+    public DbSet<IndividualQuota> IndividualQuotas { get; set; }
 
     public DbSet<TierQuotaDefinition> TierQuotaDefinitions { get; set; }
 
