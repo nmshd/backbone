6523aa3778eadfa630b33312b1dd7c75f63dbe7b
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-07-20 15:22:17 +0100

    feat: add missing ApplyQuotasForMetricsAttribute

--- Modules/Relationships/src/Relationships.Application/Relationships/Commands/CreateRelationship/CreateRelationshipCommand.cs
+++ Modules/Relationships/src/Relationships.Application/Relationships/Commands/CreateRelationship/CreateRelationshipCommand.cs
@@ -1,8 +1,10 @@
 ﻿using Backbone.Modules.Relationships.Domain.Ids;
+using Enmeshed.BuildingBlocks.Application.Attributes;
 using MediatR;
 
 namespace Backbone.Modules.Relationships.Application.Relationships.Commands.CreateRelationship;
 
+[ApplyQuotasForMetrics("NumberOfRelationships")]
 public class CreateRelationshipCommand : IRequest<CreateRelationshipResponse>
 {
     public RelationshipTemplateId RelationshipTemplateId { get; set; }
