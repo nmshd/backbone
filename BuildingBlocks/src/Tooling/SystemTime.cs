543d1806293e9261b544525da75340d9b835b994
Timo Notheisen <timo.notheisen@js-soft.com>
2023-07-20 14:49:57 +0200

    test: remove thread safety from SystemTime

--- BuildingBlocks/src/Tooling/SystemTime.cs
+++ BuildingBlocks/src/Tooling/SystemTime.cs
@@ -6,21 +6,20 @@ namespace Enmeshed.Tooling;
 /// Provides access to system time while allowing it to be set to a fixed <see cref="DateTime"/> value.
 /// </summary>
 /// <remarks>
-/// This class is thread safe.
 /// </remarks>
 public static class SystemTime
 {
-    private static readonly ThreadLocal<Func<DateTime>> GetTime = new(() => () => DateTime.Now);
+    private static Func<DateTime> GetTime = () => DateTime.UtcNow;
 
     /// <inheritdoc cref="DateTime.Today"/>
-    public static DateTime UtcToday => GetTime.Value == null
+    public static DateTime UtcToday => GetTime == null
         ? throw new Exception("Time function is null")
-        : GetTime.Value().ToUniversalTime().Date;
+        : GetTime().ToUniversalTime().Date;
 
     /// <inheritdoc cref="DateTime.UtcNow"/>
-    public static DateTime UtcNow => GetTime.Value == null
+    public static DateTime UtcNow => GetTime == null
         ? throw new Exception("Time function is null")
-        : GetTime.Value().ToUniversalTime();
+        : GetTime().ToUniversalTime();
 
     /// <summary>
     /// Sets a fixed (deterministic) time for the current thread to return by <see cref="SystemTime"/>.
@@ -38,7 +37,7 @@ public static class SystemTime
         if (time.Kind != DateTimeKind.Local)
             time = time.ToLocalTime();
 
-        GetTime.Value = () => time;
+        GetTime = () => time;
     }
 
     /// <summary>
@@ -46,6 +45,6 @@ public static class SystemTime
     /// </summary>
     public static void Reset()
     {
-        GetTime.Value = () => DateTime.Now;
+        GetTime = () => DateTime.UtcNow;
     }
 }
