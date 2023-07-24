6fce91cc2a5288d6a9243fc8636bd997c48ecd3b
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-07-24 11:29:09 +0100

    chore: add Quotas-specific Relationship and other entities

--- Modules/Quotas/src/Quotas.Domain/Aggregates/Relationships/Relationship.cs
+++ Modules/Quotas/src/Quotas.Domain/Aggregates/Relationships/Relationship.cs
@@ -1,6 +1,9 @@
-﻿namespace Backbone.Modules.Quotas.Domain.Aggregates.Relationships;
+﻿using Enmeshed.DevelopmentKit.Identity.ValueObjects;
+
+namespace Backbone.Modules.Quotas.Domain.Aggregates.Relationships;
 public class Relationship
 {
     public string Id { get; set; }
-
+    public IdentityAddress From { get; }
+    public IdentityAddress To { get; }
 }
