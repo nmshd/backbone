f04e65df0b8242b8ea4122f60aa56bc09ea079da
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-07-25 13:20:10 +0100

    feat: add GetIdentityByAddress query

---
+++ Modules/Devices/src/Devices.Application/Identities/Queries/GetIdentityByAddress/GetIdentityByAddressQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Backbone.Modules.Devices.Application.Identities.Queries.GetIdentityByAddress;
+public class GetIdentityByAddressQuery : IRequest<GetIdentityByAddressResponse>
+{
+    public GetIdentityByAddressQuery(string address)
+    {
+        Address = address;
+    }
+    public string Address { get; set; }
+}
---
+++ Modules/Devices/src/Devices.Application/Identities/Queries/GetIdentityByAddress/Handler.cs
@@ -0,0 +1,20 @@
+﻿using Backbone.Modules.Devices.Application.Infrastructure.Persistence.Repository;
+using MediatR;
+
+namespace Backbone.Modules.Devices.Application.Identities.Queries.GetIdentityByAddress;
+public class Handler : IRequestHandler<GetIdentityByAddressQuery, GetIdentityByAddressResponse>
+{
+    private readonly IIdentitiesRepository _identitiesRepository;
+
+    public Handler(IIdentitiesRepository identitiesRepository)
+    {
+        _identitiesRepository = identitiesRepository;
+    }
+
+    public async Task<GetIdentityByAddressResponse> Handle(GetIdentityByAddressQuery request, CancellationToken cancellationToken)
+    {
+        var identity = await _identitiesRepository.FindByAddress(request.Address, cancellationToken);
+
+        return new GetIdentityByAddressResponse(identity);
+    }
+}
