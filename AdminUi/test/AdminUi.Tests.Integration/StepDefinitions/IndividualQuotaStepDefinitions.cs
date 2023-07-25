9bb4a52abdab13a1ad493e2b0f1a7671b7025ae4
Daniel Almeida <daniel.almeida@konkconsulting.com>
2023-07-25 15:03:58 +0100

    test: add integration tests for deletion of individual quotas

--- AdminUi/test/AdminUi.Tests.Integration/StepDefinitions/IndividualQuotaStepDefinitions.cs
+++ AdminUi/test/AdminUi.Tests.Integration/StepDefinitions/IndividualQuotaStepDefinitions.cs
@@ -6,22 +6,59 @@ namespace AdminUi.Tests.Integration.StepDefinitions;
 
 [Binding]
 [Scope(Feature = "POST IndividualQuota")]
+[Scope(Feature = "DELETE IndividualQuota")]
 public class IndividualQuotaStepDefinitions : BaseStepDefinitions
 {
     private readonly IdentitiesApi _identitiesApi;
     private string _identityAddress;
+    private string _quotaId;
     private HttpResponse<IndividualQuotaDTO>? _response;
 
     public IndividualQuotaStepDefinitions(IdentitiesApi identitiesApi)
     {
         _identitiesApi = identitiesApi;
         _identityAddress = string.Empty;
+        _quotaId = string.Empty;
     }
 
     [Given(@"an Identity i")]
     public void GivenAnIdentity()
     {
         _identityAddress = "id12Pbi7CgBHaFxge6uy1h6qUkedjQr8XHfm";
+    }
+
+    [Given(@"an Identity i with an IndividualQuota q")]
+    public async Task GivenAnIdentityIWithAnIndividualQuotaQ()
+    {
+        _identityAddress = "id12Pbi7CgBHaFxge6uy1h6qUkedjQr8XHfm";
+        var createIndividualQuotaRequest = new CreateIndividualQuotaRequest()
+        {
+            MetricKey = "NumberOfSentMessages",
+            Max = 2,
+            Period = "Week"
+        };
+        var requestConfiguration = _requestConfiguration.Clone();
+        requestConfiguration.ContentType = "application/json";
+        requestConfiguration.SetContent(createIndividualQuotaRequest);
+
+        var response = await _identitiesApi.CreateIndividualQuota(requestConfiguration, _identityAddress);
+        var actualStatusCode = (int)response.StatusCode;
+        actualStatusCode.Should().Be(201);
+        _quotaId = response.Content.Result!.Id;
+    }
+
+    [When(@"a DELETE request is sent to the /Identities/{i.address}/Quotas/{q.id} endpoint")]
+    public async Task WhenADeleteRequestIsSentToTheDeleteIndividualQuotaEndpoint()
+    {
+        _response = await _identitiesApi.DeleteIndividualQuota(_identityAddress, _quotaId, _requestConfiguration);
+        _response.Should().NotBeNull();
+    }
+
+    [When(@"a DELETE request is sent to the /Identities/{t.address}/Quotas/inexistentQuotaId endpoint")]
+    public async Task WhenADeleteRequestIsSentToTheDeleteIndividualQuotaEndpointWithAnInexistentQuotaId()
+    {
+        _response = await _identitiesApi.DeleteIndividualQuota(_identityAddress, "some-inexistent-quota-id", _requestConfiguration);
+        _response.Should().NotBeNull();
     }
 
     [When(@"a POST request is sent to the /Identity/{i.id}/Quotas endpoint")]
