a91ea3a823569b7526474a3f544c0e41ffee2972
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-07-26 14:45:47 +0100

    test: add GetIdentityByAddress devices handler tests

---
+++ Modules/Devices/test/Devices.Application.Tests/Tests/Identities/Queries/GetIdentityByAddress/FindByAddressStubRepository.cs
@@ -0,0 +1,46 @@
+﻿using Backbone.Modules.Devices.Application.Infrastructure.Persistence.Repository;
+using Backbone.Modules.Devices.Domain.Entities;
+using Enmeshed.BuildingBlocks.Application.Abstractions.Infrastructure.Persistence.Database;
+using Enmeshed.BuildingBlocks.Application.Pagination;
+using Enmeshed.DevelopmentKit.Identity.ValueObjects;
+
+namespace Backbone.Modules.Devices.Application.Tests.Tests.Identities.Queries.GetIdentityByAddress;
+public class FindByAddressStubRepository : IIdentitiesRepository
+{
+    private readonly Identity _identity;
+
+    public FindByAddressStubRepository(Identity identity)
+    {
+        _identity = identity;
+    }
+
+    public Task AddUser(ApplicationUser user, string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<DbPaginationResult<Identity>> FindAll(PaginationFilter paginationFilter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<DbPaginationResult<Device>> FindAllDevicesOfIdentity(IdentityAddress identity, IEnumerable<DeviceId> ids, PaginationFilter paginationFilter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Identity> FindByAddress(IdentityAddress address, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(_identity);
+    }
+
+    public Task<Device> GetDeviceById(DeviceId deviceId, CancellationToken cancellationToken, bool track = false)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Update(Device device, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Modules/Devices/test/Devices.Application.Tests/Tests/Identities/Queries/GetIdentityByAddress/HandlerTests.cs
@@ -0,0 +1,55 @@
+﻿using Backbone.Modules.Devices.Application.Identities.Queries.GetIdentityByAddress;
+using Backbone.Modules.Devices.Application.Infrastructure.Persistence.Repository;
+using Backbone.Modules.Devices.Domain.Entities;
+using Enmeshed.BuildingBlocks.Application.Abstractions.Exceptions;
+using Enmeshed.DevelopmentKit.Identity.ValueObjects;
+using FakeItEasy;
+using FluentAssertions;
+using Xunit;
+
+namespace Backbone.Modules.Devices.Application.Tests.Tests.Identities.Queries.GetIdentityByAddress;
+public class HandlerTests
+{
+    [Fact]
+    public async void Gets_identity_by_address()
+    {
+        // Arrange
+        var expectedClientId = TestDataGenerator.CreateRandomDeviceId();
+        var expectedAddress = TestDataGenerator.CreateRandomIdentityAddress();
+        var expectedTierId = TestDataGenerator.CreateRandomTierId();
+        var identity = new Identity(expectedClientId, expectedAddress, Array.Empty<byte>(), expectedTierId, 1);
+
+        var handler = CreateHandler(new FindByAddressStubRepository(identity));
+
+        // Act
+        var result = await handler.Handle(new GetIdentityByAddressQuery(identity.Address), CancellationToken.None);
+
+        // Assert
+        result.ClientId.Should().Be(expectedClientId);
+        result.Address.Should().Be(expectedAddress);
+        result.PublicKey.Should().BeEquivalentTo(Array.Empty<byte>());
+        result.TierId.Should().BeEquivalentTo(expectedTierId);
+        result.IdentityVersion.Should().Be(1);
+    }
+
+    [Fact]
+    public async void Fails_when_no_identity_found()
+    {
+        // Arrange
+        var identityRepository = A.Fake<IIdentitiesRepository>();
+        A.CallTo(() => identityRepository.FindByAddress(A<IdentityAddress>._, A<CancellationToken>._)).Returns((Identity)null);
+
+        var handler = CreateHandler(identityRepository);
+
+        // Act
+        var acting = async () => await handler.Handle(new GetIdentityByAddressQuery("some-inexistent-identity-address"), CancellationToken.None);
+
+        // Assert
+        await acting.Should().ThrowAsync<NotFoundException>();
+    }
+
+    private Handler CreateHandler(IIdentitiesRepository identitiesRepository)
+    {
+        return new Handler(identitiesRepository);
+    }
+}
