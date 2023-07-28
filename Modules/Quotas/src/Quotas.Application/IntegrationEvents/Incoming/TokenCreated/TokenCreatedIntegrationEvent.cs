8cc55b95f77a34bf73183adcfc40791bb9ab7354
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-07-28 14:29:18 +0100

    feat: added integration events

---
+++ Modules/Quotas/src/Quotas.Application/IntegrationEvents/Incoming/TokenCreated/TokenCreatedIntegrationEvent.cs
@@ -0,0 +1,8 @@
+﻿using Enmeshed.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus.Events;
+
+namespace Backbone.Modules.Quotas.Application.IntegrationEvents.Incoming.TokenCreated;
+public class TokenCreatedIntegrationEvent : IntegrationEvent
+{
+    public string TokenId { get; set; }
+    public string CreatedBy { get; set; }
+}
