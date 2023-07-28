0de11aad8f517518a6df5da768d5881b460fe1dc
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-07-28 14:29:43 +0100

    feat: add Token entity type configuration to Quotas module

---
+++ Modules/Quotas/src/Quotas.Infrastructure/Persistence/Database/EntityConfigurations/TokenEntityTypeConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Backbone.Modules.Quotas.Domain.Aggregates.Tokens;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Backbone.Modules.Quotas.Infrastructure.Persistence.Database.EntityConfigurations;
+public class TokenEntityTypeConfiguration : IEntityTypeConfiguration<Token>
+{
+    public void Configure(EntityTypeBuilder<Token> builder)
+    {
+        builder.ToTable(nameof(Token) + "s", "Tokens", x => x.ExcludeFromMigrations());
+        builder.HasKey(x => x.Id);
+    }
+}
