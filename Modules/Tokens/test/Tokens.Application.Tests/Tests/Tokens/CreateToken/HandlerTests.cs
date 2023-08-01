2b38772bf035eb2e633475a510e7d2fb62e21f8a
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-08-01 09:53:55 +0100

    test: add token created integration event test

---
+++ Modules/Tokens/test/Tokens.Application.Tests/Tests/Tokens/CreateToken/HandlerTests.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using Backbone.Modules.Tokens.Application.Infrastructure.Persistence.Repository;
+using Backbone.Modules.Tokens.Application.IntegrationEvents;
+using Backbone.Modules.Tokens.Application.Tokens.Commands.CreateToken;
+using Backbone.Modules.Tokens.Domain.Entities;
+using Enmeshed.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
+using Enmeshed.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus.Events;
+using Enmeshed.BuildingBlocks.Application.Abstractions.Infrastructure.UserContext;
+using FakeItEasy;
+using FluentAssertions.Execution;
+using Xunit;
+
+namespace Backbone.Modules.Tokens.Application.Tests.Tests.Tokens.CreateToken;
+public class HandlerTests
+{
+    private readonly IUserContext _userContext;
+    private readonly IMapper _mapper;
+    private readonly IEventBus _eventBus;
+
+    public HandlerTests()
+    {
+        _userContext = A.Fake<IUserContext>();
+        _mapper = A.Fake<IMapper>();
+        _eventBus = A.Fake<IEventBus>();
+        AssertionScope.Current.FormattingOptions.MaxLines = 1000;
+    }
+
+    [Fact]
+    public async void Triggers_TokenCreatedIntegrationEvent()
+    {
+        // Arrange
+        var command = new CreateTokenCommand
+        {
+            ExpiresAt = DateTime.UtcNow,
+            Content = new byte[] { 1, 1, 1, 1, 1, 1, 1, 1 }
+        };
+
+        var tokenRepository = A.Fake<ITokensRepository>();
+        A.CallTo(() => tokenRepository.Add(A<Token>._)).Returns(null);
+
+        var handler = CreateHandler(tokenRepository);
+
+        // Act
+        await handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        A.CallTo(() => _eventBus.Publish(A<IntegrationEvent>.That.IsInstanceOf(typeof(TokenCreatedIntegrationEvent)))).MustHaveHappened();
+    }
+
+    private Handler CreateHandler(ITokensRepository tokens)
+    {
+        return new Handler(_userContext, _mapper, _eventBus, tokens);
+    }
+}
