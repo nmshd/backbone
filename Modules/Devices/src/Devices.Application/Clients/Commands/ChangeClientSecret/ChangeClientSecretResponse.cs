95f9412605de62b8d862517951fc0101f75e3bef
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-08-01 16:25:04 +0100

    feat: add ChangeClientSecret command

---
+++ Modules/Devices/src/Devices.Application/Clients/Commands/ChangeClientSecret/ChangeClientSecretResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Backbone.Modules.Devices.Application.Clients.Commands.ChangeClientSecret;
+public class ChangeClientSecretResponse
+{
+    public ChangeClientSecretResponse(string clientId, string displayName, string clientSecret)
+    {
+        ClientId = clientId;
+        DisplayName = displayName;
+        ClientSecret = clientSecret;
+    }
+
+    public string ClientId { get; set; }
+    public string DisplayName { get; set; }
+    public string ClientSecret { get; set; }
+}
