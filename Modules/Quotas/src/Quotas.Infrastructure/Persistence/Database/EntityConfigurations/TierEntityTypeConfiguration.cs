8def03fab2fb0d2c4ddd327007d2165f3d45a1e4
Timo Notheisen <timo.notheisen@js-soft.com>
2023-08-02 09:48:39 +0200

    fix: explicitly add Id property

--- Modules/Quotas/src/Quotas.Infrastructure/Persistence/Database/EntityConfigurations/TierEntityTypeConfiguration.cs
+++ Modules/Quotas/src/Quotas.Infrastructure/Persistence/Database/EntityConfigurations/TierEntityTypeConfiguration.cs
@@ -7,6 +7,7 @@ public class TierEntityTypeConfiguration : IEntityTypeConfiguration<Tier>
 {
     public void Configure(EntityTypeBuilder<Tier> builder)
     {
+        builder.Property(x => x.Id);
         builder.Property(x => x.Name).IsUnicode(true).IsFixedLength(false).HasMaxLength(30);
     }
 }
