e35e050605a39e7bdecc21e84a39a45406bda736
Daniel Almeida <daniel.almeida@konkconsulting.com>
2023-08-04 14:55:59 +0100

    test: adjust identity overview dto

--- AdminUi/test/AdminUi.Tests.Integration/Models/IdentityOverviewDTO.cs
+++ AdminUi/test/AdminUi.Tests.Integration/Models/IdentityOverviewDTO.cs
@@ -5,7 +5,7 @@ public class IdentityOverviewDTO
     public DateTime CreatedAt { get; set; }
     public DateTime? LastLoginAt { get; set; }
     public string? CreatedWithClient { get; set; }
-    public string? DatawalletVersion { get; set; }
+    public int? DatawalletVersion { get; set; }
     public byte IdentityVersion { get; set; }
     public string TierName { get; set; }
     public string TierId { get; set; }
