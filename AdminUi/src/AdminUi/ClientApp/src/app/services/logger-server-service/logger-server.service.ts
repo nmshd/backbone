b3fe50ecc84d62ac935ad63e901d027166a2c8c2
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-08-10 16:14:15 +0100

    fix: inject AuthService into LoggerServerService

--- AdminUi/src/AdminUi/ClientApp/src/app/services/logger-server-service/logger-server.service.ts
+++ AdminUi/src/AdminUi/ClientApp/src/app/services/logger-server-service/logger-server.service.ts
@@ -1,17 +1,29 @@
-import { HttpRequest } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+import { HttpBackend, HttpRequest } from "@angular/common/http";
+import { Injectable, NgZone } from "@angular/core";
 import { INGXLoggerMetadata, NGXLoggerServerService, NgxLoggerLevel } from "ngx-logger";
+import { AuthService } from "../auth-service/auth.service";
+import { Observable } from "rxjs";
 
 @Injectable({
     providedIn: "root"
 })
 export class LoggerServerService extends NGXLoggerServerService {
+    isLoggedIn$: Observable<boolean> | undefined;
+
+    constructor(httpBackend: HttpBackend, ngZone: NgZone, private authService: AuthService) {
+        super(httpBackend, ngZone);
+    }
+
     protected override alterHttpRequest(httpRequest: HttpRequest<any>): HttpRequest<any> {
-        httpRequest = httpRequest.clone({
-            setHeaders: {
-                ["X-API-KEY"]: localStorage.getItem("api-key")!
-            }
-        });
+        this.isLoggedIn$ = this.authService.isLoggedIn;
+        if (this.isLoggedIn$) {
+            httpRequest = httpRequest.clone({
+                setHeaders: {
+                    "X-API-KEY": this.authService.getApiKey()!
+                }
+            });
+        }
+
         return httpRequest;
     }
 
