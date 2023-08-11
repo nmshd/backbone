7f78dca654af329a61658d53e312eadc8c45aa85
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-08-11 16:03:20 +0100

    fix: no internal

--- AdminUi/test/AdminUi.Tests.Integration/Models/HttpResponse.cs
+++ AdminUi/test/AdminUi.Tests.Integration/Models/HttpResponse.cs
@@ -10,8 +10,8 @@ public class HttpResponse<T>
     public bool IsSuccessStatusCode { get; set; }
     public string? ContentType { get; set; }
     public string? RawContent { get; set; }
-    public IReadOnlyCollection<HeaderParameter>? Headers { get; internal set; }
-    public CookieCollection? Cookies { get; internal set; }
+    public IReadOnlyCollection<HeaderParameter>? Headers { get; set; }
+    public CookieCollection? Cookies { get; set; }
 }
 
 public class HttpResponse
