7deef4f9988366fdca8dd9e4c5e9365aa1798587
Timo Notheisen <timo.notheisen@js-soft.com>
2023-08-31 07:40:18 +0200

    chore: fix typo of error message of Infrastructure error

--- BuildingBlocks/src/BuildingBlocks.Infrastructure/Exceptions/GenericInfrastructureErrors.cs
+++ BuildingBlocks/src/BuildingBlocks.Infrastructure/Exceptions/GenericInfrastructureErrors.cs
@@ -5,6 +5,6 @@ public static class GenericInfrastructureErrors
     public static InfrastructureError InvalidPushNotificationConfiguration(List<string> supportedAppIds)
     {
         return new InfrastructureError("error.platform.unsupportedAppId",
-            $"The given app id is not supported. Support app ids are: {string.Join(", ", supportedAppIds)}.");
+            $"The given app id is not supported. Supported app ids are: {string.Join(", ", supportedAppIds)}.");
     }
 }
