c953be1c0226b88e5fb9e339171d0617c20cd047
Timo Notheisen <timo.notheisen@js-soft.com>
2023-08-31 14:46:11 +0200

    chore: improve error message for InvalidPushNotificationConfiguration

--- Modules/Devices/src/Devices.Infrastructure/InfrastructureErrors.cs
+++ Modules/Devices/src/Devices.Infrastructure/InfrastructureErrors.cs
@@ -5,7 +5,9 @@ public static class InfrastructureErrors
 {
     public static InfrastructureError InvalidPushNotificationConfiguration(List<string> supportedAppIds)
     {
+        var supportedAppIdsString = string.Join(", ", supportedAppIds.Select(x => $"'{x}'"));
+
         return new InfrastructureError("error.platform.unsupportedAppId",
-            $"The given app id is not supported. Supported app ids are: {string.Join(", ", supportedAppIds)}.");
+            $"The given app id is not supported. Supported app ids are: {supportedAppIdsString}.");
     }
 }
