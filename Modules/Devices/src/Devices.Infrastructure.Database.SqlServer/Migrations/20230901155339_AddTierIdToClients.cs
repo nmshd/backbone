e5d6591e6da2151c2429232d55bc341a7560bd2e
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-09-01 16:54:51 +0100

    feat: add SqlServer migrations for Client changes

---
+++ Modules/Devices/src/Devices.Infrastructure.Database.SqlServer/Migrations/20230901155339_AddTierIdToClients.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backbone.Modules.Devices.Infrastructure.Database.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTierIdToClients : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AppId",
+                table: "PnsRegistrations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TierId",
+                table: "OpenIddictApplications",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TierId",
+                table: "OpenIddictApplications");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AppId",
+                table: "PnsRegistrations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
