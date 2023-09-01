e15415aec6df927de46d44d6bb34e19a05e45a25
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-09-01 10:04:45 +0100

    chore: add docker-compose files for both mssql and postgres

---
+++ .ci/integrationTest.mssql.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env -S npx ts-node --esm
+
+import { $ } from "zx";
+
+await $`docker compose -f ./docker-compose/docker-compose.test.mssql.yml up test-consumer-api test-admin-ui test-seed-mssql test-seed-client -d`;
+await $`dotnet restore "Backbone.sln"`;
+await $`dotnet build /property:WarningLevel=0 --no-restore "Backbone.sln"`;
+await $`dotnet test --no-restore --no-build "Backbone.sln" --filter "Category=Integration&TestCategory!~ignore"`;
+await $`docker compose -f ./docker-compose/docker-compose.test.yml down`;
