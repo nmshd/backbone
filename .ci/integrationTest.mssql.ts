8d4c6fd27a1076dc496cfbcb98733b84f94ea977
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-09-01 14:11:09 +0100

    fix: bad paths

--- .ci/integrationTest.mssql.ts
+++ .ci/integrationTest.mssql.ts
@@ -6,4 +6,4 @@ await $`docker compose -f ./docker-compose/docker-compose.test.mssql.yml up test
 await $`dotnet restore "Backbone.sln"`;
 await $`dotnet build /property:WarningLevel=0 --no-restore "Backbone.sln"`;
 await $`dotnet test --no-restore --no-build "Backbone.sln" --filter "Category=Integration&TestCategory!~ignore"`;
-await $`docker compose -f ./docker-compose/docker-compose.test.yml down`;
+await $`docker compose -f ./docker-compose/docker-compose.test.mssql.yml down`;
