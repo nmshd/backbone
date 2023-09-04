b9533c14e2e0a6e5edc5042a7b19beb25f26fbad
Daniel Almeida <daniel.almeida@konkconsulting.com>
2023-09-04 17:01:19 +0100

    fix: add in memory collection for app settings only on local environment

--- ConsumerApi.Tests.Integration/CustomWebApplicationFactory.cs
+++ ConsumerApi.Tests.Integration/CustomWebApplicationFactory.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using Enmeshed.Tooling.Extensions;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
@@ -10,19 +11,20 @@ public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStar
     {
         builder.ConfigureHostConfiguration(config =>
         {
-            config.AddInMemoryCollection(new Dictionary<string, string?>
-            {
-                { "Modules:Challenges:Infrastructure:SqlDatabase:ConnectionString", "Server=localhost;Database=enmeshed;User Id=challenges;Password=Passw0rd;TrustServerCertificate=True"},
-                { "Modules:Quotas:Infrastructure:SqlDatabase:ConnectionString", "Server=localhost;Database=enmeshed;User Id=quotas;Password=Passw0rd;TrustServerCertificate=True"},
-                { "Modules:Devices:Infrastructure:SqlDatabase:ConnectionString", "Server=localhost;Database=enmeshed;User Id=devices;Password=Passw0rd;TrustServerCertificate=True"},
-                { "Modules:Files:Infrastructure:SqlDatabase:ConnectionString", "Server=localhost;Database=enmeshed;User Id=files;Password=Passw0rd;TrustServerCertificate=True"},
-                { "Modules:Messages:Infrastructure:SqlDatabase:ConnectionString", "Server=localhost;Database=enmeshed;User Id=messages;Password=Passw0rd;TrustServerCertificate=True"},
-                { "Modules:Relationships:Infrastructure:SqlDatabase:ConnectionString", "Server=localhost;Database=enmeshed;User Id=relationships;Password=Passw0rd;TrustServerCertificate=True"},
-                { "Modules:Synchronization:Infrastructure:SqlDatabase:ConnectionString", "Server=localhost;Database=enmeshed;User Id=synchronization;Password=Passw0rd;TrustServerCertificate=True"},
-                { "Modules:Tokens:Infrastructure:SqlDatabase:ConnectionString", "Server=localhost;Database=enmeshed;User Id=tokens;Password=Passw0rd;TrustServerCertificate=True"},
-                { "Modules:Tokens:Infrastructure:BlobStorage:ConnectionInfo", Environment.GetEnvironmentVariable("ENMESHED_BLOB_STORAGE_CONNECTION_STRING") },
-                { "Infrastructure:EventBus:ConnectionInfo", "localhost" }
-            });
+            if (Environment.GetEnvironmentVariable("CI").IsNullOrEmpty())
+                config.AddInMemoryCollection(new Dictionary<string, string?>
+                {
+                    { "Modules:Challenges:Infrastructure:SqlDatabase:ConnectionString", "Server=localhost;Database=enmeshed;User Id=challenges;Password=Passw0rd;TrustServerCertificate=True"},
+                    { "Modules:Quotas:Infrastructure:SqlDatabase:ConnectionString", "Server=localhost;Database=enmeshed;User Id=quotas;Password=Passw0rd;TrustServerCertificate=True"},
+                    { "Modules:Devices:Infrastructure:SqlDatabase:ConnectionString", "Server=localhost;Database=enmeshed;User Id=devices;Password=Passw0rd;TrustServerCertificate=True"},
+                    { "Modules:Files:Infrastructure:SqlDatabase:ConnectionString", "Server=localhost;Database=enmeshed;User Id=files;Password=Passw0rd;TrustServerCertificate=True"},
+                    { "Modules:Messages:Infrastructure:SqlDatabase:ConnectionString", "Server=localhost;Database=enmeshed;User Id=messages;Password=Passw0rd;TrustServerCertificate=True"},
+                    { "Modules:Relationships:Infrastructure:SqlDatabase:ConnectionString", "Server=localhost;Database=enmeshed;User Id=relationships;Password=Passw0rd;TrustServerCertificate=True"},
+                    { "Modules:Synchronization:Infrastructure:SqlDatabase:ConnectionString", "Server=localhost;Database=enmeshed;User Id=synchronization;Password=Passw0rd;TrustServerCertificate=True"},
+                    { "Modules:Tokens:Infrastructure:SqlDatabase:ConnectionString", "Server=localhost;Database=enmeshed;User Id=tokens;Password=Passw0rd;TrustServerCertificate=True"},
+                    { "Modules:Tokens:Infrastructure:BlobStorage:ConnectionInfo", Environment.GetEnvironmentVariable("ENMESHED_BLOB_STORAGE_CONNECTION_STRING") },
+                    { "Infrastructure:EventBus:ConnectionInfo", "localhost" }
+                });
         });
 
         return base.CreateHost(builder);
