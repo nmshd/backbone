1f4be3762ccf5e25ff2023d126cf45e4160d940c
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-09-07 17:18:11 +0100

    feat: add CreateClientCommand validator

---
+++ Modules/Devices/src/Devices.Application/Clients/Commands/CreateClients/CreateClientCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using Enmeshed.BuildingBlocks.Application.FluentValidation;
+using FluentValidation;
+
+namespace Backbone.Modules.Devices.Application.Clients.Commands.CreateClients;
+public class CreateClientCommandValidator : AbstractValidator<CreateClientCommand>
+{
+    public CreateClientCommandValidator()
+    {
+        RuleFor(c => c.DefaultTier).DetailedNotEmpty();
+    }
+}
