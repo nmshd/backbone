ba94af811bdb6811e9c8df3d24ee96baa2f5b9ce
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-09-08 11:38:54 +0100

    fix: re-add migrations with updated DefaultTier name

---
+++ Modules/Devices/src/Devices.Infrastructure.Database.Postgres/Migrations/20230908103806_AddTierIdToClients.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backbone.Modules.Devices.Infrastructure.Database.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTierIdToClients : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AppId",
+                table: "PnsRegistrations",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DefaultTier",
+                table: "OpenIddictApplications",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DefaultTier",
+                table: "OpenIddictApplications");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AppId",
+                table: "PnsRegistrations",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
