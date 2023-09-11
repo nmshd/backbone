41fa887dc366e44628574dcb9f17025b799ebdc0
tnotheis <timo.notheisen@js-soft.com>
2023-09-11 16:49:39 +0200

    ci: delete zx files for integration tests

--- .ci/integrationTest.mssql.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env -S npx ts-node --esm
-
-import { $ } from "zx";
-
-await $`docker compose -f ./.ci/docker-compose.test.yml -f ./.ci/docker-compose.test.mssql.yml up -d`;
-await $`dotnet restore "Backbone.sln"`;
-await $`dotnet build /property:WarningLevel=0 --no-restore "Backbone.sln"`;
-await $`dotnet test --no-restore --no-build --filter "Category=Integration&TestCategory!~ignore" "Backbone.sln"`;
-await $`docker compose -f ./.ci/docker-compose.test.yml -f ./.ci/docker-compose.test.mssql.yml down`;
