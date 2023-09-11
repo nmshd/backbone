1625443333ad49bda6923822456e8e6fface083b
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-09-11 13:51:14 +0100

    fix: logs not saving

--- .ci/integrationTest.pgsql.ts
+++ .ci/integrationTest.pgsql.ts
@@ -6,4 +6,5 @@ await $`docker compose -f ./.ci/docker-compose.test.yml -f ./.ci/docker-compose.
 await $`dotnet restore "Backbone.sln"`;
 await $`dotnet build /property:WarningLevel=0 --no-restore "Backbone.sln"`;
 await $`dotnet test --no-restore --no-build --filter "Category=Integration&TestCategory!~ignore" "Backbone.sln"`;
+await $`docker compose -f ./.ci/docker-compose.test.yml -f ./.ci/docker-compose.test.postgres.yml logs > docker-log.txt`;
 await $`docker compose -f ./.ci/docker-compose.test.yml -f ./.ci/docker-compose.test.postgres.yml down`;
