250d3089843fbbe4a042427bcd477f2b38d9fd55
tnotheis <timo.notheisen@js-soft.com>
2023-09-11 15:26:10 +0200

    ci: create and use shell scripts for integration tests

---
+++ .ci/integrationTest.pgsql.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker compose -f ./.ci/docker-compose.test.yml -f ./.ci/docker-compose.test.postgres.yml up -d --build
+dotnet restore "Backbone.sln"
+dotnet build /property:WarningLevel=0 --no-restore "Backbone.sln"
+dotnet test --no-restore --no-build --filter "Category=Integration&TestCategory!~ignore" "Backbone.sln"
+docker compose -f ./.ci/docker-compose.test.yml -f ./.ci/docker-compose.test.postgres.yml down
