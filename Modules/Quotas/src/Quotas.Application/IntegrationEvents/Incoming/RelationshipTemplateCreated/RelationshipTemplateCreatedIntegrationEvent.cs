03e5c6b2113899f5742bcf50ef111c6a839e6f34
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-09-12 11:11:13 +0100

    feat: add integration events

---
+++ Modules/Quotas/src/Quotas.Application/IntegrationEvents/Incoming/RelationshipTemplateCreated/RelationshipTemplateCreatedIntegrationEvent.cs
@@ -0,0 +1,8 @@
+﻿using Enmeshed.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus.Events;
+
+namespace Backbone.Modules.Quotas.Application.IntegrationEvents.Incoming.RelationshipTemplateCreated;
+public class RelationshipTemplateCreatedIntegrationEvent : IntegrationEvent
+{
+    public string TemplateId { get; set; }
+    public string CreatedBy { get; set; }
+}
---
+++ Modules/Quotas/src/Quotas.Application/IntegrationEvents/Incoming/RelationshipTemplateCreated/RelationshipTemplateCreatedIntegrationEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using Backbone.Modules.Quotas.Application.Metrics;
+using Backbone.Modules.Quotas.Domain.Aggregates.Metrics;
+using Enmeshed.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
+
+namespace Backbone.Modules.Quotas.Application.IntegrationEvents.Incoming.RelationshipTemplateCreated;
+internal class RelationshipTemplateCreatedIntegrationEventHandler : IIntegrationEventHandler<RelationshipTemplateCreatedIntegrationEvent>
+{
+    private readonly IMetricStatusesService _metricStatusesService;
+
+    public RelationshipTemplateCreatedIntegrationEventHandler(IMetricStatusesService metricStatusesService)
+    {
+        _metricStatusesService = metricStatusesService;
+    }
+
+    public async Task Handle(RelationshipTemplateCreatedIntegrationEvent @event)
+    {
+        var identities = new List<string> { @event.CreatedBy };
+        var metrics = new List<string> { MetricKey.NumberOfRelationshipTemplates.Value };
+
+        await _metricStatusesService.RecalculateMetricStatuses(identities, metrics, CancellationToken.None);
+    }
+}
