47f5aa3ce7717a2d97fe092362176271283de908
Daniel Almeida <115644988+daniel-almeida-konkconsulting@users.noreply.github.com>
2023-09-12 16:22:20 +0100

    Identities with N devices appear N times (#282)
    * fix: fix database view for identities overview and create migrations for sqlserver and postgres
    
    * ci: trigger pipelines
    
    * ci: trigger pipelines
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- AdminUi/src/AdminUi.Infrastructure.Database.SqlServer/Migrations/20230911140220_Fix_IdentitiesOverview_View.Designer.cs
+++ AdminUi/src/AdminUi.Infrastructure.Database.SqlServer/Migrations/20230911140220_Fix_IdentitiesOverview_View.Designer.cs
@@ -4,6 +4,7 @@ using AdminUi.Infrastructure.Persistence.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AdminUi.Infrastructure.Database.SqlServer.Migrations
 {
     [DbContext(typeof(AdminUiDbContext))]
-    partial class AdminUiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230911140220_Fix_IdentitiesOverview_View")]
+    partial class Fix_IdentitiesOverview_View
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
