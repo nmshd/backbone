79b1bd566ab6a3cbae98a7cdb383e416b7aee727
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-09-13 10:22:00 +0100

    chore: split tests

--- Modules/Quotas/test/Quotas.Application.Tests/Tests/IntegrationEvents/TierDeleted/TierOfIdentityChangedIntegrationEventHandlerTests.cs
+++ Modules/Quotas/test/Quotas.Application.Tests/Tests/IntegrationEvents/TierDeleted/TierOfIdentityChangedIntegrationEventHandlerTests.cs
@@ -1,27 +1,20 @@
 ﻿using Backbone.Modules.Quotas.Application.Infrastructure.Persistence.Repository;
-using Backbone.Modules.Quotas.Application.IntegrationEvents.Incoming.TierDeleted;
 using Backbone.Modules.Quotas.Application.IntegrationEvents.Incoming.TierOfIdentityChanged;
 using Backbone.Modules.Quotas.Application.Metrics;
+using Backbone.Modules.Quotas.Application.Tests.Extensions;
 using Backbone.Modules.Quotas.Domain.Aggregates.Identities;
 using Backbone.Modules.Quotas.Domain.Aggregates.Tiers;
+using Enmeshed.BuildingBlocks.Domain;
 using Enmeshed.UnitTestTools.Data;
 using FakeItEasy;
 using FluentAssertions;
-using Microsoft.Extensions.Logging;
 using Xunit;
 
 namespace Backbone.Modules.Quotas.Application.Tests.Tests.IntegrationEvents.TierDeleted;
 public class TierOfIdentityChangedIntegrationEventHandlerTests
 {
-    private readonly ILogger<TierOfIdentityChangedIntegrationEventHandler> _logger;
-
-    public TierOfIdentityChangedIntegrationEventHandlerTests()
-    {
-        _logger = A.Fake<ILogger<TierOfIdentityChangedIntegrationEventHandler>>();
-    }
-
     [Fact]
-    public async Task Updates_identity_with_new_tier_and_calls_MetricStatusService()
+    public async Task Handler_updates_identity_with_new_tier()
     {
         // Arrange
         var tiersRepository = A.Fake<ITiersRepository>();
@@ -47,7 +40,42 @@ public class TierOfIdentityChangedIntegrationEventHandlerTests
             OldTier = oldTier.Id,
             NewTier = newTier.Id,
             IdentityAddress = identity.Address
+        };
 
+        // Act
+        await handler.Handle(tierDeletedIntegrationEvent);
+
+        // Assert
+        A.CallTo(() => identitiesRepository.Update(identity, A<CancellationToken>._)).MustHaveHappened();
+        identity.TierId.Should().Be(newTier.Id);
+    }
+    [Fact]
+    public async Task Handler_calls_MetricStatusService()
+    {
+        // Arrange
+        var tiersRepository = A.Fake<ITiersRepository>();
+
+        var oldTier = new Tier(new("old-tier-id"), "old-tier");
+        var newTier = new Tier(new("new-tier-id"), "new-tier");
+
+        A.CallTo(() => tiersRepository.Find(oldTier.Id, A<CancellationToken>._, A<bool>._)).Returns(oldTier);
+        A.CallTo(() => tiersRepository.Find(newTier.Id, A<CancellationToken>._, A<bool>._)).Returns(newTier);
+
+        var identitiesRepository = A.Fake<IIdentitiesRepository>();
+
+        var identity = new Identity(TestDataGenerator.CreateRandomIdentityAddress(), oldTier.Id);
+
+        A.CallTo(() => identitiesRepository.Find(identity.Address, A<CancellationToken>._, A<bool>._)).Returns(identity);
+
+        var metricStatusesService = A.Fake<IMetricStatusesService>();
+
+        var handler = CreateHandler(identitiesRepository, tiersRepository, metricStatusesService);
+
+        var tierDeletedIntegrationEvent = new TierOfIdentityChangedIntegrationEvent()
+        {
+            OldTier = oldTier.Id,
+            NewTier = newTier.Id,
+            IdentityAddress = identity.Address
         };
 
         // Act
@@ -55,8 +83,6 @@ public class TierOfIdentityChangedIntegrationEventHandlerTests
 
         // Assert
         A.CallTo(metricStatusesService).Where(x => x.Method.Name == nameof(metricStatusesService.RecalculateMetricStatuses)).WithAnyArguments().MustHaveHappened();
-        A.CallTo(() => identitiesRepository.Update(identity, A<CancellationToken>._)).MustHaveHappened();
-        identity.TierId.Should().Be(newTier.Id);
     }
 
     private TierOfIdentityChangedIntegrationEventHandler CreateHandler(IIdentitiesRepository identitiesRepository, ITiersRepository tiersRepository, IMetricStatusesService metricStatusesService)
