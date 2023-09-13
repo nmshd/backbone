4e94648b9ccbbe9048e801f1d32d28ade65541bf
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-09-13 09:35:14 +0100

    fix: did not ensure the new tier existed

--- Modules/Devices/src/Devices.Application/Identities/Commands/UpdateIdentity/Handler.cs
+++ Modules/Devices/src/Devices.Application/Identities/Commands/UpdateIdentity/Handler.cs
@@ -3,6 +3,7 @@ using Backbone.Modules.Devices.Application.IntegrationEvents.Outgoing;
 using Backbone.Modules.Devices.Domain.Aggregates.Tier;
 using Backbone.Modules.Devices.Domain.Entities;
 using Enmeshed.BuildingBlocks.Application.Abstractions.Exceptions;
+using ApplicationException = Enmeshed.BuildingBlocks.Application.Abstractions.Exceptions.ApplicationException;
 using Enmeshed.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
 using MediatR;
 
@@ -22,10 +23,16 @@ public class Handler : IRequestHandler<UpdateIdentityCommand, Identity>
 
     public async Task<Identity> Handle(UpdateIdentityCommand request, CancellationToken cancellationToken)
     {
+        var newTierId = TierId.Create(request.TierId);
+        if (!newTierId.IsSuccess)
+        {
+            throw new ApplicationException(new ApplicationError(newTierId.Error.Code, newTierId.Error.Message));
+        }
+
         var identity = await _identitiesRepository.FindByAddress(request.Address, cancellationToken) ?? throw new NotFoundException(nameof(Identity));
 
         var oldTier = await _tiersRepository.FindById(identity.TierId, cancellationToken) ?? throw new NotFoundException(nameof(Tier));
-        var newTier = await _tiersRepository.FindById(TierId.Create(request.TierId).Value, cancellationToken) ?? throw new NotFoundException(nameof(Tier));
+        var newTier = await _tiersRepository.FindById(newTierId.Value, cancellationToken) ?? throw new NotFoundException(nameof(Tier));
 
         identity.TierId = newTier.Id;
         await _identitiesRepository.Update(identity, cancellationToken);
