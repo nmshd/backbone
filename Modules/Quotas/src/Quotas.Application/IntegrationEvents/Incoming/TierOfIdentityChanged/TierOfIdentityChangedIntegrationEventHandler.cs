23937acb140d1ee8a4ee4ee23dc365c695109c11
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-09-13 15:52:54 +0100

    chore: fix formatting

--- Modules/Quotas/src/Quotas.Application/IntegrationEvents/Incoming/TierOfIdentityChanged/TierOfIdentityChangedIntegrationEventHandler.cs
+++ Modules/Quotas/src/Quotas.Application/IntegrationEvents/Incoming/TierOfIdentityChanged/TierOfIdentityChangedIntegrationEventHandler.cs
@@ -20,12 +20,15 @@ public class TierOfIdentityChangedIntegrationEventHandler : IIntegrationEventHan
     {
         var identity = await _identitiesRepository.Find(@event.IdentityAddress, CancellationToken.None, track: true);
         var newTier = await _tiersRepository.Find(@event.NewTier, CancellationToken.None, track: true);
+        
         identity.ChangeTier(newTier);
+        
         await _identitiesRepository.Update(identity, CancellationToken.None);
+
         await _metricStatusesService.RecalculateMetricStatuses(
             new List<string>() { identity.Address },
             identity.MetricStatuses.Select(x => x.MetricKey).ToList(),
             CancellationToken.None
-            );
+        );
     }
 }
