374cfb1b5c6019b5cd2794642d12acae383edce2
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-09-14 10:03:55 +0100

    feat: add ClientOverview DTO

---
+++ AdminUi/src/AdminUi.Infrastructure/DTOs/ClientOverview.cs
@@ -0,0 +1,7 @@
+﻿namespace AdminUi.Infrastructure.DTOs;
+public class ClientOverview
+{
+    public string ClientId { get; set; }
+    public string DisplayName { get; set; }
+    public int NumberOfIdentities { get; set; }
+}
