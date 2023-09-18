bd7664138d16b18f0bb997a29626b1e54d17ca07
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-09-18 11:14:13 +0100

    test: revert changes to Tiers GET and TierDetailsDTO integration tests

--- AdminUi/test/AdminUi.Tests.Integration/Models/TierDetailsDTO.cs
+++ AdminUi/test/AdminUi.Tests.Integration/Models/TierDetailsDTO.cs
@@ -4,5 +4,4 @@ public class TierDetailsDTO
     public string Id { get; set; }
     public string Name { get; set; }
     public IEnumerable<TierQuotaDTO> Quotas { get; set; }
-    public int NumberOfIdentities { get; set; }
 }
