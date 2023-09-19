bd792adbe685a2215f54b71d5c832af3cc46c19a
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-09-19 14:56:02 +0100

    fix: ignore adminUi package for the license checks

--- AdminUi/src/AdminUi/ClientApp/.ci/runChecks.mjs
+++ AdminUi/src/AdminUi/ClientApp/.ci/runChecks.mjs
@@ -5,5 +5,5 @@ process.env.FORCE_COLOR = 3;
 await $`npm ci`;
 await $`npx eslint --ext ts ./src`;
 await $`npx prettier --check .`;
-await $`npx license-check`;
+await $`npx license-check --ignorePackages adminui@0.0.0`;
 await $`npx better-npm-audit audit`;
