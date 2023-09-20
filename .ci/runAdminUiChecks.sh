fc8369458b43176dff04908e71288486bd49b940
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-09-20 15:59:37 +0100

    test: move runAdminUiChecks to .ci folder

--- .ci/runAdminUiChecks.sh
+++ .ci/runAdminUiChecks.sh
@@ -1,9 +1,11 @@
 set -e
 set -x
 
+$INITIAL_DIR=$pwd
+cd AdminUi/src/AdminUi/ClientApp
 npm ci
 npx eslint --ext ts ./src
 npx prettier --check .
 npx license-check --ignorePackages adminui@0.0.0
-
 npx better-npm-audit audit
+cd $INITIAL_DIR
