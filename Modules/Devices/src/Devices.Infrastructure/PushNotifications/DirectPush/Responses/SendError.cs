bf93f291d6de76fad9692937991c3dd2ab9e9971
Daniel Almeida <115644988+daniel-almeida-konkconsulting@users.noreply.github.com>
2023-09-21 10:56:55 +0100

    Delete device registrations when send fails (#293)
    * test: add dummy behavior to test multicast message order
    
    * feat: delete device registration when send fails for fcm
    
    * feat: delete device registration when send fails for apns
    
    * test: missing registration repository
    
    * refactor: direct push service is now responsible for deleting registrations while the connectors simply map the responses
    
    * ci: trigger pipelines
    
    * refactor: use bulk delete for registrations and introduce a SendResults class to simplify the DirectPushService logic.
    
    * refactor: create responses folder with all classes for handling push notification responses
    
    * ci: trigger pipelines
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>

---
+++ Modules/Devices/src/Devices.Infrastructure/PushNotifications/DirectPush/Responses/SendError.cs
@@ -0,0 +1,13 @@
+﻿namespace Backbone.Modules.Devices.Infrastructure.PushNotifications.DirectPush.Responses;
+
+public class SendError
+{
+    public SendError(ErrorReason reason, string message)
+    {
+        Reason = reason;
+        Message = message;
+    }
+
+    public ErrorReason Reason { get; private set; }
+    public string Message { get; private set; }
+}
