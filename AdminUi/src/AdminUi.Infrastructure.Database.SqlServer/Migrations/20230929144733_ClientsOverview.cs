664e4e04d3a34ec759c2eedacfe0ad7f840c26df
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-09-29 16:24:10 +0100

    chore: create SqlServer migrations for ClientOverviews

---
+++ AdminUi/src/AdminUi.Infrastructure.Database.SqlServer/Migrations/20230929144733_ClientsOverview.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdminUi.Infrastructure.Database.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class ClientsOverview : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                CREATE OR ALTER VIEW ClientOverviews AS
+                    SELECT
+                        CLIENTS.ClientId,
+                        CLIENTS.DisplayName,
+        	            CLIENTS.DefaultTier,
+                        COUNT (IDENTITIES.ClientId) AS NumberOfIdentities
+                    FROM Devices.OpenIddictApplications CLIENTS
+                    LEFT JOIN Devices.Identities IDENTITIES
+                    ON IDENTITIES.ClientId = CLIENTS.ClientId
+                    GROUP BY CLIENTS.ClientId, CLIENTS.DisplayName, CLIENTS.DefaultTier
+        """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
