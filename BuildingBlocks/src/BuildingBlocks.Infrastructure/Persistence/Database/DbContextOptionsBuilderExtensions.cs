ee2bd400c92eaae6ed6c2585db892791ac87e03d
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-10-04 15:47:32 +0200

    unused directives removed from file

--- BuildingBlocks/src/BuildingBlocks.Infrastructure/Persistence/Database/DbContextOptionsBuilderExtensions.cs
+++ BuildingBlocks/src/BuildingBlocks.Infrastructure/Persistence/Database/DbContextOptionsBuilderExtensions.cs
@@ -1,7 +1,5 @@
 ﻿using Enmeshed.Tooling.Extensions;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace Enmeshed.BuildingBlocks.Infrastructure.Persistence.Database;
 public static class DbContextOptionsBuilderExtensions
