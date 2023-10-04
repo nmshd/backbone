5b46c753fc24c076f8026ae50e2c9b3294db6ac1
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-10-04 08:45:40 +0100

    Revert "test: counts within timeframe"
    This reverts commit c923ab5fd3b48095ae167e22f4f49e8167039afd.

--- Modules/Quotas/src/Quotas.Domain/Aggregates/Messages/Message.cs
+++ Modules/Quotas/src/Quotas.Domain/Aggregates/Messages/Message.cs
@@ -1,17 +1,6 @@
 ﻿namespace Backbone.Modules.Quotas.Domain.Aggregates.Messages;
 public class Message : ICreatedAt
 {
-    public Message(string id, string createdBy, DateTime createdAt)
-    {
-        Id = id;
-        CreatedBy = createdBy;
-        CreatedAt = createdAt;
-    }
-
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-    private Message() { }
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-
     public string Id { get; set; }
     public string CreatedBy { get; set; }
     public DateTime CreatedAt { get; set; }
