6050b984923df20303099f76af442d8f6d73562a
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-10-04 10:45:58 +0100

    test: Quotas.MessagesRepository.Count must make use of from and to params

--- BuildingBlocks/src/UnitTestTools/TestDoubles/Fakes/FakeDbContextFactory.cs
+++ BuildingBlocks/src/UnitTestTools/TestDoubles/Fakes/FakeDbContextFactory.cs
@@ -1,14 +1,15 @@
 ﻿using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace Enmeshed.UnitTestTools.TestDoubles.Fakes;
 
 public static class FakeDbContextFactory
 {
-    public static (TContext arrangeContext, TContext assertionContext, TContext actContext)
-        CreateDbContexts<TContext>() where TContext : DbContext
+    public static (TContext arrangeContext, TContext assertionContext, TContext actContext, SqliteConnection connection)
+        CreateDbContexts<TContext>(SqliteConnection? connection = null) where TContext : DbContext
     {
-        var connection = new SqliteConnection("DataSource=:memory:");
+        connection ??= new SqliteConnection("DataSource=:memory:");
         connection.Open();
 
         var options = new DbContextOptionsBuilder<TContext>()
@@ -25,6 +26,6 @@ public static class FakeDbContextFactory
         var assertionContext = (TContext)Activator.CreateInstance(typeof(TContext), args);
         var actContext = (TContext)Activator.CreateInstance(typeof(TContext), args);
 
-        return (arrangeContext, assertionContext, actContext);
+        return (arrangeContext, assertionContext, actContext, connection);
     }
 }
---
+++ Modules/Messages/src/Messages.Domain/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Backbone.Modules.Quotas.Application.Tests")]
