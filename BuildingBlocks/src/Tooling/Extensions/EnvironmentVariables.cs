abcd41aeb7fdea7605582d728fb2fcc12f829f71
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-10-04 15:42:56 +0200

    unused directives in file removed

--- BuildingBlocks/src/Tooling/Extensions/EnvironmentVariables.cs
+++ BuildingBlocks/src/Tooling/Extensions/EnvironmentVariables.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Enmeshed.Tooling.Extensions;
+﻿namespace Enmeshed.Tooling.Extensions;
 public static class EnvironmentVariables
 {
     public static readonly bool DEBUG_PERFORMANCE = !Environment.GetEnvironmentVariable("DEBUG_PERFORMANCE").IsNullOrEmpty();
