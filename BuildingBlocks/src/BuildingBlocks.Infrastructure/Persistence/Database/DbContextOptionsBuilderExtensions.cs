02f2f3786389bfc1f2dc84932d391dfed2dbcfd4
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-10-04 15:46:39 +0200

    performance logging interceptor as DbContextOptionsBuilder extension

---
+++ BuildingBlocks/src/BuildingBlocks.Infrastructure/Persistence/Database/DbContextOptionsBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Enmeshed.Tooling.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Enmeshed.BuildingBlocks.Infrastructure.Persistence.Database;
+public static class DbContextOptionsBuilderExtensions
+{
+    public static IServiceCollection AddDebugPerformanceInterceptor(this IServiceCollection services)
+    {
+        if (EnvironmentVariables.DEBUG_PERFORMANCE)
+            services.AddTransient<SaveChangesTimeInterceptor>();
+
+        return services;
+    }
+}
