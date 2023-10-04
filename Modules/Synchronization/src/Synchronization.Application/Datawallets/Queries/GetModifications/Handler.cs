3cd7c87ee53fa09bebcc1019cdd3452a1588d0ec
Timo Notheisen <timo.notheisen@js-soft.com>
2023-10-04 09:49:47 +0200

    feat: update GetModifications handler

--- Modules/Synchronization/src/Synchronization.Application/Datawallets/Queries/GetModifications/Handler.cs
+++ Modules/Synchronization/src/Synchronization.Application/Datawallets/Queries/GetModifications/Handler.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using System.Text.Json;
+using AutoMapper;
 using Backbone.Modules.Synchronization.Application.Datawallets.DTOs;
 using Backbone.Modules.Synchronization.Application.Infrastructure;
 using Backbone.Modules.Synchronization.Domain.Entities;
@@ -6,7 +7,10 @@ using Enmeshed.BuildingBlocks.Application.Abstractions.Exceptions;
 using Enmeshed.BuildingBlocks.Application.Abstractions.Infrastructure.Persistence.BlobStorage;
 using Enmeshed.BuildingBlocks.Application.Abstractions.Infrastructure.UserContext;
 using Enmeshed.DevelopmentKit.Identity.ValueObjects;
+using Enmeshed.Tooling.Extensions;
+using Google.Apis.Logging;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Backbone.Modules.Synchronization.Application.Datawallets.Queries.GetModifications;
@@ -15,15 +19,17 @@ public class Handler : IRequestHandler<GetModificationsQuery, GetModificationsRe
 {
     private readonly IdentityAddress _activeIdentity;
     private readonly IBlobStorage _blobStorage;
+    private readonly ILogger<Handler> _logger;
     private readonly ISynchronizationDbContext _dbContext;
     private readonly IMapper _mapper;
     private readonly BlobOptions _blobOptions;
 
-    public Handler(ISynchronizationDbContext dbContext, IMapper mapper, IUserContext userContext, IBlobStorage blobStorage, IOptions<BlobOptions> blobOptions)
+    public Handler(ISynchronizationDbContext dbContext, IMapper mapper, IUserContext userContext, IBlobStorage blobStorage, IOptions<BlobOptions> blobOptions, ILogger<Handler> logger)
     {
         _dbContext = dbContext;
         _mapper = mapper;
         _blobStorage = blobStorage;
+        _logger = logger;
         _blobOptions = blobOptions.Value;
         _activeIdentity = userContext.GetAddress();
     }
@@ -39,27 +45,60 @@ public class Handler : IRequestHandler<GetModificationsQuery, GetModificationsRe
 
         var dbPaginationResult = await _dbContext.GetDatawalletModifications(_activeIdentity, request.LocalIndex, request.PaginationFilter, cancellationToken);
 
-        var dtos = _mapper.Map<IEnumerable<DatawalletModificationDTO>>(dbPaginationResult.ItemsOnPage).ToArray();
-
-        await FillEncryptedPayloads(dtos);
+        var dtos = await MapToDtos(dbPaginationResult.ItemsOnPage);
 
         return new GetModificationsResponse(dtos, request.PaginationFilter, dbPaginationResult.TotalNumberOfItems);
     }
 
-    private async Task FillEncryptedPayloads(IEnumerable<DatawalletModificationDTO> modifications)
+    private async Task<List<DatawalletModificationDTO>> MapToDtos(IEnumerable<DatawalletModification> modifications)
     {
-        await Task.WhenAll(modifications.Select(FillEncryptedPayload));
+        var datawalletModifications = modifications as DatawalletModification[] ?? modifications.ToArray();
+
+        var blobReferences = datawalletModifications.Where(m => !m.BlobReference.IsNullOrEmpty()).Select(m => m.BlobReference).Distinct();
+        var blobs = await Task.WhenAll(blobReferences.Select(r =>
+        {
+            try
+            {
+                return _blobStorage.FindAsync(_blobOptions.RootFolder, r);
+            }
+            catch (NotFoundException)
+            {
+                _logger.LogWarning("Blob with reference '{blobReference}' not found.", r);
+                throw new Exception($"Blob with reference '{r}' not found.");
+            }
+        }));
+
+        var payloads = blobs
+            .Select(b => JsonSerializer.Deserialize<Dictionary<long, byte[]>>(b))
+            .SelectMany(b => b)
+            .ToDictionary(b => b.Key, b => b.Value);
+
+        var mappingTasks = datawalletModifications.Select(m => MapToDto(m, payloads));
+
+        return (await Task.WhenAll(mappingTasks)).ToList();
     }
 
-    private async Task FillEncryptedPayload(DatawalletModificationDTO datawalletModification)
+    private async Task<DatawalletModificationDTO> MapToDto(DatawalletModification modification, Dictionary<long, byte[]> payloads)
     {
-        try
+        var dto = _mapper.Map<DatawalletModificationDTO>(modification);
+
+        if (modification.BlobReference.IsNullOrEmpty())
         {
-            datawalletModification.EncryptedPayload = await _blobStorage.FindAsync(_blobOptions.RootFolder, datawalletModification.Id);
+            try
+            {
+                dto.EncryptedPayload = await _blobStorage.FindAsync(_blobOptions.RootFolder, modification.Id);
+            }
+            catch (NotFoundException)
+            {
+                // blob not found means that there is no payload for this modification
+            }
         }
-        catch (NotFoundException)
+        else
         {
-            // if the payload was not found, it means that the modification had no payload
+            payloads.TryGetValue(modification.Index, out var payload);
+            dto.EncryptedPayload = payload;
         }
+
+        return dto;
     }
 }
