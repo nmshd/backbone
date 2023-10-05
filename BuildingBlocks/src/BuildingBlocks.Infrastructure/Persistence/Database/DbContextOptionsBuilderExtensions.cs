4d14e71f3ea5edfb76eaa12b471d0fac098f9c0e
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-10-05 21:27:16 +0200

    refactor: method renamed for consistency fix: AddTransient() changed to TryAddScoped()

--- BuildingBlocks/src/BuildingBlocks.Infrastructure/Persistence/Database/DbContextOptionsBuilderExtensions.cs
+++ BuildingBlocks/src/BuildingBlocks.Infrastructure/Persistence/Database/DbContextOptionsBuilderExtensions.cs
@@ -1,13 +1,15 @@
 ﻿using Enmeshed.Tooling.Extensions;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Enmeshed.BuildingBlocks.Infrastructure.Persistence.Database;
 public static class DbContextOptionsBuilderExtensions
 {
-    public static IServiceCollection AddDebugPerformanceInterceptor(this IServiceCollection services)
+    public static IServiceCollection AddSaveChangesTimeInterceptor(this IServiceCollection services)
     {
         if (EnvironmentVariables.DEBUG_PERFORMANCE)
-            services.AddTransient<SaveChangesTimeInterceptor>();
+            services.TryAddScoped<SaveChangesTimeInterceptor>();
 
         return services;
     }
