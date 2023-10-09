b07360a88887862bcf372c8d3dfd1249317e3367
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-10-09 18:59:24 +0200

    feat: added the DeleteRegistration() method signature

--- Modules/Devices/src/Devices.Application/Infrastructure/PushNotifications/IPushService.cs
+++ Modules/Devices/src/Devices.Application/Infrastructure/PushNotifications/IPushService.cs
@@ -6,5 +6,6 @@ namespace Backbone.Modules.Devices.Application.Infrastructure.PushNotifications;
 public interface IPushService
 {
     Task UpdateRegistration(IdentityAddress address, DeviceId deviceId, PnsHandle handle, string appId, CancellationToken cancellationToken);
+    Task DeleteRegistration(IdentityAddress address, DeviceId deviceId, PnsHandle handle, string appId, CancellationToken cancellationToken);
     Task SendNotification(IdentityAddress recipient, object notification, CancellationToken cancellationToken);
 }
