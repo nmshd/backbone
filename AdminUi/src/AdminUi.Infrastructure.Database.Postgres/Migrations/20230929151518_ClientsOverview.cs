e3c10a50a4be886769cf34b9b6aed599f4e7d29f
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-10-09 13:33:39 +0100

    fix: merge fix

--- AdminUi/src/AdminUi.Infrastructure.Database.Postgres/Migrations/20230929151518_ClientsOverview.cs
+++ AdminUi/src/AdminUi.Infrastructure.Database.Postgres/Migrations/20230929151518_ClientsOverview.cs
@@ -11,16 +11,17 @@ namespace AdminUi.Infrastructure.Database.Postgres.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql("""
-                CREATE OR REPLACE VIEW "ClientOverviews" AS
+                CREATE VIEW "ClientOverviews" AS
                     SELECT
                         CLIENTS."ClientId" AS "ClientId",
                         CLIENTS."DisplayName" AS "DisplayName",
                         CLIENTS."DefaultTier" AS "DefaultTier",
+                        CLIENTS."CreatedAt" AS "CreatedAt",
                         COUNT (IDENTITIES."ClientId") AS "NumberOfIdentities"
                     FROM "Devices"."OpenIddictApplications" CLIENTS
                     LEFT JOIN "Devices"."Identities" IDENTITIES
                     ON IDENTITIES."ClientId" = CLIENTS."ClientId"
-                    GROUP BY CLIENTS."ClientId", CLIENTS."DisplayName", CLIENTS."DefaultTier"
+                    GROUP BY CLIENTS."ClientId", CLIENTS."DisplayName", CLIENTS."DefaultTier", CLIENTS."CreatedAt"
         """);
         }
 
--- AdminUi/src/AdminUi.Infrastructure.Database.SqlServer/Migrations/20230929144733_ClientsOverview.cs
+++ AdminUi/src/AdminUi.Infrastructure.Database.SqlServer/Migrations/20230929144733_ClientsOverview.cs
@@ -11,16 +11,17 @@ namespace AdminUi.Infrastructure.Database.SqlServer.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql("""
-                CREATE OR ALTER VIEW ClientOverviews AS
+                CREATE VIEW ClientOverviews AS
                     SELECT
                         CLIENTS.ClientId,
                         CLIENTS.DisplayName,
-        	            CLIENTS.DefaultTier,
+                        CLIENTS.DefaultTier,
+                        CLIENTS.CreatedAt,
                         COUNT (IDENTITIES.ClientId) AS NumberOfIdentities
                     FROM Devices.OpenIddictApplications CLIENTS
                     LEFT JOIN Devices.Identities IDENTITIES
                     ON IDENTITIES.ClientId = CLIENTS.ClientId
-                    GROUP BY CLIENTS.ClientId, CLIENTS.DisplayName, CLIENTS.DefaultTier
+                    GROUP BY CLIENTS.ClientId, CLIENTS.DisplayName, CLIENTS.DefaultTier, CLIENTS.CreatedAt
         """);
         }
 
