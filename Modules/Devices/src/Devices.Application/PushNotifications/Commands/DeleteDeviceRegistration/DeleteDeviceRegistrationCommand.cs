bea34cb9b40f522153036e49a4a965f3e42bdfb9
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-10-09 18:58:40 +0200

    feat: added the DeleteDeviceRegistrationCommand class

---
+++ Modules/Devices/src/Devices.Application/PushNotifications/Commands/DeleteDeviceRegistration/DeleteDeviceRegistrationCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Backbone.Modules.Devices.Application.PushNotifications.Commands.DeleteDeviceRegistration;
+
+public class DeleteDeviceRegistrationCommand : IRequest<Unit>
+{
+    public string Platform { get; set; }
+    public string Handle { get; set; }
+    public string AppId { get; set; }
+}
