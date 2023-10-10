9a540e34b4ae102eab8e297898cc6c46e2049c8b
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-10-10 11:57:05 +0100

    feat: add DefaultTierName to ClientOverview

--- AdminUi/src/AdminUi.Infrastructure/DTOs/ClientOverview.cs
+++ AdminUi/src/AdminUi.Infrastructure/DTOs/ClientOverview.cs
@@ -3,7 +3,8 @@ public class ClientOverview
 {
     public string ClientId { get; set; }
     public string DisplayName { get; set; }
-    public string DefaultTier { get; set; }
+    public string DefaultTierId { get; set; }
+    public string DefaultTierName { get; set; }
     public DateTime CreatedAt { get; set; }
     public int NumberOfIdentities { get; set; }
 }
