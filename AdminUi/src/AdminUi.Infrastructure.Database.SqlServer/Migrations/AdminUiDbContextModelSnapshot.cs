329342d85727aab34d89b59be364fc00c88ad2a4
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-10-11 09:43:20 +0100

    fix: remove AddDefaultTierNameToClientsOverview SqlServer migration

--- AdminUi/src/AdminUi.Infrastructure.Database.SqlServer/Migrations/AdminUiDbContextModelSnapshot.cs
+++ AdminUi/src/AdminUi.Infrastructure.Database.SqlServer/Migrations/AdminUiDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace AdminUi.Infrastructure.Database.SqlServer.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.11")
+                .HasAnnotation("ProductVersion", "7.0.10")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
@@ -28,14 +28,7 @@ namespace AdminUi.Infrastructure.Database.SqlServer.Migrations
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<DateTime>("CreatedAt")
-                        .HasColumnType("datetime2");
-
-                    b.Property<string>("DefaultTierId")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("DefaultTierName")
+                    b.Property<string>("DefaultTier")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
