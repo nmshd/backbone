f80e8b0f5777fe74ebe201b2f944f50d1065b38b
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-10-11 14:00:13 +0200

    feat: introduced log event ids

--- BuildingBlocks/src/BuildingBlocks.Infrastructure/EventBus/AzureServiceBus/EventBusAzureServiceBus.cs
+++ BuildingBlocks/src/BuildingBlocks.Infrastructure/EventBus/AzureServiceBus/EventBusAzureServiceBus.cs
@@ -67,12 +67,12 @@ public class EventBusAzureServiceBus : IEventBus, IDisposable
             Subject = eventName
         };
 
-        _logger.LogTrace("Sending integration event with id '{MessageId}'...", message.MessageId);
+        _logger.SendingIntegrationEvent(message.MessageId);
 
         await _logger.TraceTime(async () =>
             await _sender.SendMessageAsync(message), nameof(_sender.SendMessageAsync));
 
-        _logger.LogTrace("Successfully sent integration event with id '{MessageId}'.", message.MessageId);
+        _logger.LogDebug("Successfully sent integration event with id '{MessageId}'.", message.MessageId);
     }
 
     public void Subscribe<T, TH>()
@@ -85,7 +85,7 @@ public class EventBusAzureServiceBus : IEventBus, IDisposable
         if (!containsKey)
             try
             {
-                _logger.LogTrace("Trying to create subscription on Service Bus...");
+                _logger.LogInformation("Trying to create subscription on Service Bus...");
 
                 _serviceBusPersisterConnection.AdministrationClient.CreateRuleAsync(TOPIC_NAME, _subscriptionName,
                     new CreateRuleOptions
@@ -94,7 +94,7 @@ public class EventBusAzureServiceBus : IEventBus, IDisposable
                         Name = eventName
                     }).GetAwaiter().GetResult();
 
-                _logger.LogTrace("Successfully created subscription on Service Bus.");
+                _logger.LogInformation("Successfully created subscription on Service Bus.");
             }
             catch (ServiceBusException)
             {
@@ -118,8 +118,7 @@ public class EventBusAzureServiceBus : IEventBus, IDisposable
                 if (await ProcessEvent(eventName, messageData))
                     await args.CompleteMessageAsync(args.Message);
                 else
-                    _logger.LogInformation(
-                        "The event with the MessageId '{messageId}' wasn't processed and will therefore not be completed.", args.Message.MessageId);
+                    _logger.EventWasNotProcessed(args.Message.MessageId);
             };
 
         _processor.ProcessErrorAsync += ErrorHandler;
@@ -131,8 +130,7 @@ public class EventBusAzureServiceBus : IEventBus, IDisposable
         var ex = args.Exception;
         var context = args.ErrorSource;
 
-        _logger.LogError(ex, "ERROR handling message: {ExceptionMessage} - Context: {@ExceptionContext}",
-            ex.Message, context);
+        _logger.ErrorHandlingMessage(ex.Message, context, ex);
 
         return Task.CompletedTask;
     }
@@ -140,7 +138,10 @@ public class EventBusAzureServiceBus : IEventBus, IDisposable
     private async Task<bool> ProcessEvent(string eventName, string message)
     {
         if (!_subscriptionManager.HasSubscriptionsForEvent(eventName))
+        {
+            _logger.NoSubscriptionForEvent(eventName);
             return false;
+        }
 
         await using var scope = _autofac.BeginLifetimeScope(AUTOFAC_SCOPE_NAME);
 
@@ -160,25 +161,92 @@ public class EventBusAzureServiceBus : IEventBus, IDisposable
             try
             {
                 var policy = EventBusRetryPolicyFactory.Create(
-                    _handlerRetryBehavior,
-                    (ex, _) => _logger.LogWarning(
-                        "The following error was thrown while executing '{eventHandlerType}':\n'{errorMessage}'\n{stacktrace}.\nAttempting to retry...",
-                        eventType.Name,
-                        ex.Message,
-                        ex.StackTrace)
-                    );
+                    _handlerRetryBehavior, (ex, _) => _logger.ErrorWhileExecutingEventHandlerType(eventType.Name, ex.Message, ex.StackTrace, ex));
 
                 await policy.ExecuteAsync(() => (Task)concreteType.GetMethod("Handle")!.Invoke(handler, new[] { integrationEvent })!);
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex,
-                    $"An error occurred while processing the integration event with id '{integrationEvent.IntegrationEventId}'.");
+                _logger.ErrorWhileProcessingIntegrationEvent(integrationEvent.IntegrationEventId, ex);
                 return false;
             }
         }
 
-
         return true;
+    }
+}
+
+file static class LoggerExtensions
+{
+    private static readonly Action<ILogger, string, Exception> SENDING_INTEGRATION_EVENT =
+        LoggerMessage.Define<string>(
+            LogLevel.Debug,
+            new EventId(302940, "EventBusAzureServiceBus.SendingIntegrationEvent"),
+            "Sending integration event with id '{messageId}'..."
+        );
+
+    private static readonly Action<ILogger, string, Exception> EVENT_WAS_NOT_PROCESSED =
+        LoggerMessage.Define<string>(
+            LogLevel.Information,
+            new EventId(630568, "EventBusAzureServiceBus.EventWasNotProcessed"),
+            "The event with the MessageId '{messageId}' wasn't processed and will therefore not be completed."
+        );
+
+    private static readonly Action<ILogger, string, ServiceBusErrorSource, Exception> ERROR_HANDLING_MESSAGE =
+        LoggerMessage.Define<string, ServiceBusErrorSource>(
+            LogLevel.Error,
+            new EventId(949322, "EventBusAzureServiceBus.ErrorHandlingMessage"),
+            "ERROR handling message: '{exceptionMessage}' - Context: '{@exceptionContext}'."
+        );
+
+    private static readonly Action<ILogger, string, Exception> NO_SUBSCRIPTION_FOR_EVENT =
+        LoggerMessage.Define<string>(
+            LogLevel.Warning,
+            new EventId(341537, "EventBusAzureServiceBus.NoSubscriptionForEvent"),
+            "No subscription for event: '{eventName}'."
+        );
+
+    private static readonly Action<ILogger, string, string, string, Exception> ERROR_WHILE_EXECUTING_EVENT_HANDLER_TYPE =
+        LoggerMessage.Define<string, string, string>(
+            LogLevel.Warning,
+            new EventId(726744, "EventBusAzureServiceBus.ErrorWhileExecutingEventHandlerType"),
+            "The following error was thrown while executing '{eventHandlerType}':\n'{errorMessage}'\n{stackTrace}.\nAttempting to retry..."
+        );
+
+    private static readonly Action<ILogger, string, Exception> ERROR_WHILE_PROCESSING_INTEGRATION_EVENT =
+        LoggerMessage.Define<string>(
+            LogLevel.Error,
+            new EventId(146670, "EventBusAzureServiceBus.ErrorWhileProcessingIntegrationEvent"),
+            "An error occurred while processing the integration event with id '{integrationEventId}'."
+        );
+
+    public static void SendingIntegrationEvent(this ILogger logger, string messageId)
+    {
+        SENDING_INTEGRATION_EVENT(logger, messageId, default!);
+    }
+
+    public static void EventWasNotProcessed(this ILogger logger, string messageId)
+    {
+        EVENT_WAS_NOT_PROCESSED(logger, messageId, default!);
+    }
+
+    public static void ErrorHandlingMessage(this ILogger logger, string exceptionMessage, ServiceBusErrorSource exceptionContext, Exception e)
+    {
+        ERROR_HANDLING_MESSAGE(logger, exceptionMessage, exceptionContext, e);
+    }
+
+    public static void NoSubscriptionForEvent(this ILogger logger, string eventName)
+    {
+        NO_SUBSCRIPTION_FOR_EVENT(logger, eventName, default!);
+    }
+
+    public static void ErrorWhileExecutingEventHandlerType(this ILogger logger, string eventHandlerType, string errorMessage, string stackTrace, Exception e)
+    {
+        ERROR_WHILE_EXECUTING_EVENT_HANDLER_TYPE(logger, eventHandlerType, errorMessage, stackTrace, e);
+    }
+
+    public static void ErrorWhileProcessingIntegrationEvent(this ILogger logger, string integrationEventId, Exception e)
+    {
+        ERROR_WHILE_PROCESSING_INTEGRATION_EVENT(logger, integrationEventId, e);
     }
 }
