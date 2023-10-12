be1ac5962d1b99fdb087cd26bae2ce60edc0c4e8
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-10-12 10:48:46 +0100

    refactor: fix naming violation

--- Modules/Quotas/src/Quotas.Domain/Aggregates/Identities/TierQuota.cs
+++ Modules/Quotas/src/Quotas.Domain/Aggregates/Identities/TierQuota.cs
@@ -5,18 +5,20 @@ namespace Backbone.Modules.Quotas.Domain.Aggregates.Identities;
 
 public class TierQuota : Quota
 {
-    internal readonly TierQuotaDefinition Definition;
+#pragma warning disable IDE1006 // Naming Styles
+    internal readonly TierQuotaDefinition _definition;
+#pragma warning restore IDE1006 // Naming Styles
 
     private TierQuota() { }
 
     public TierQuota(TierQuotaDefinition definition, string applyTo) : base(applyTo)
     {
-        Definition = definition;
+        _definition = definition;
     }
 
     public override int Weight => 1;
-    public override MetricKey MetricKey => Definition.MetricKey;
-    public override int Max => Definition.Max;
-    public override QuotaPeriod Period => Definition.Period;
-    public TierQuotaDefinitionId DefinitionId => Definition.Id;
+    public override MetricKey MetricKey => _definition.MetricKey;
+    public override int Max => _definition.Max;
+    public override QuotaPeriod Period => _definition.Period;
+    public TierQuotaDefinitionId DefinitionId => _definition.Id;
 }
