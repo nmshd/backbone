9d719bc96104ba527f87583e9e0f470a547981f2
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-10-13 10:22:49 +0100

    test: fix ClientOverviewDTO in the integration tests

--- AdminUi/test/AdminUi.Tests.Integration/Models/ClientOverviewDTO.cs
+++ AdminUi/test/AdminUi.Tests.Integration/Models/ClientOverviewDTO.cs
@@ -3,7 +3,6 @@ public class ClientOverviewDTO
 {
     public string ClientId { get; set; }
     public string DisplayName { get; set; }
-    public string DefaultTierId { get; set; }
-    public string DefaultTierName { get; set; }
+    public TierDTO DefaultTier { get; set; }
     public int NumberOfIdentities { get; set; }
 }
