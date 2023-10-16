33efa543dfdf4acc62986088048d61e5e655939f
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-10-16 09:28:30 +0200

    fix: assigned unique event id chore: updated event name

--- Modules/Quotas/src/Quotas.Application/Tiers/Commands/DeleteTierQuotaDefinition/Handler.cs
+++ Modules/Quotas/src/Quotas.Application/Tiers/Commands/DeleteTierQuotaDefinition/Handler.cs
@@ -44,7 +44,7 @@ file static class LoggerExtensions
     private static readonly Action<ILogger, string, Exception> DELETED_TIER_QUOTA =
         LoggerMessage.Define<string>(
             LogLevel.Information,
-            new EventId(247156, "DeleteTierQuotaDefinition.Handler.DeletedTierQuota"),
+            new EventId(519284, "DeletedTierQuota"),
             "Successfully deleted tier quota definition with id: '{tierQuotaDefinitionId}'."
         );
 
