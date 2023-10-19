cff8508294a1d307787b499bee03b467141b8bf6
Daniel Almeida <daniel.almeida@konkconsulting.com>
2023-10-19 15:44:01 +0100

    refactor: remove unused error property from ODataResponseContent

--- AdminUi/test/AdminUi.Tests.Integration/Models/ODataResponseContent.cs
+++ AdminUi/test/AdminUi.Tests.Integration/Models/ODataResponseContent.cs
@@ -1,11 +1,6 @@
-﻿using Newtonsoft.Json;
-
-namespace AdminUi.Tests.Integration.Models;
+﻿namespace AdminUi.Tests.Integration.Models;
 
 public class ODataResponseContent<T>
 {
     public T? Value { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public Error? Error { get; set; }
 }
