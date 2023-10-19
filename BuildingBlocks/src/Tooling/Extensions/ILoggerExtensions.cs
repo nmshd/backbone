e7f1ab64a59021cb8f2ebf4e57e35f0dfe3c7f5d
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-10-19 14:44:01 +0200

    chore: ILoggerExtensionsLogs refactored

--- BuildingBlocks/src/Tooling/Extensions/ILoggerExtensions.cs
+++ BuildingBlocks/src/Tooling/Extensions/ILoggerExtensions.cs
@@ -44,17 +44,12 @@ public static class ILoggerExtensions
     }
 }
 
-internal static partial class ILoggerExtensionsLogs
+internal static partial class SaveChangesTimeInterceptorLogs
 {
-    private static readonly Action<ILogger, string, long, Exception> EXECUTED_ACTION =
-        LoggerMessage.Define<string, long>(
-            LogLevel.Debug,
-            LogEventIds.EXECUTION_TIME,
-            "Executed '{action}' in {elapsedMilliseconds}ms."
-        );
-
-    public static void ExecutedAction(this ILogger logger, string actionName, long elapsedMilliseconds)
-    {
-        EXECUTED_ACTION(logger, actionName, elapsedMilliseconds, default!);
-    }
+    [LoggerMessage(
+        EventId = 293800,
+        EventName = "ExecutionTime",
+        Level = LogLevel.Information,
+        Message = "Executed '{actionName}' in {elapsedMilliseconds}ms.")]
+    public static partial void ExecutedAction(this ILogger logger, string actionName, long elapsedMilliseconds);
 }
