c8f49b200c98d96bec3251556c30948ff17d2eba
Timo Notheisen <timo.notheisen@js-soft.com>
2023-10-23 13:49:13 +0200

    wip

--- Modules/Devices/src/Devices.Domain/Entities/Identity.cs
+++ Modules/Devices/src/Devices.Domain/Entities/Identity.cs
@@ -1,6 +1,8 @@
 ﻿using Backbone.Modules.Devices.Domain.Aggregates.Tier;
+using CSharpFunctionalExtensions;
 using Enmeshed.BuildingBlocks.Domain;
 using Enmeshed.BuildingBlocks.Domain.Errors;
+using Enmeshed.BuildingBlocks.Domain.StronglyTypedIds.Records;
 using Enmeshed.DevelopmentKit.Identity.ValueObjects;
 using Enmeshed.Tooling;
 
@@ -8,6 +10,9 @@ namespace Backbone.Modules.Devices.Domain.Entities;
 
 public class Identity
 {
+    private readonly List<IdentityDeletionProcess> _deletionProcesses;
+
+
     public Identity(string? clientId, IdentityAddress address, byte[] publicKey, TierId tierId, byte identityVersion)
     {
         ClientId = clientId;
@@ -17,19 +22,22 @@ public class Identity
         CreatedAt = SystemTime.UtcNow;
         Devices = new List<Device>();
         TierId = tierId;
+        _deletionProcesses = new List<IdentityDeletionProcess>();
     }
 
-    public string? ClientId { get; set; }
+    public string? ClientId { get; private set; }
 
-    public IdentityAddress Address { get; set; }
-    public byte[] PublicKey { get; set; }
-    public DateTime CreatedAt { get; set; }
+    public IdentityAddress Address { get; private set; }
+    public byte[] PublicKey { get; private set; }
+    public DateTime CreatedAt { get; private set; }
 
-    public List<Device> Devices { get; set; }
+    public List<Device> Devices { get; }
 
-    public byte IdentityVersion { get; set; }
+    public byte IdentityVersion { get; private set; }
 
-    public TierId? TierId { get; set; }
+    public TierId? TierId { get; private set; }
+
+    public IReadOnlyList<IdentityDeletionProcess> DeletionProcesses => _deletionProcesses;
 
     public bool IsNew()
     {
@@ -44,5 +52,72 @@ public class Identity
         }
 
         TierId = id;
+    }
+
+    public void StartDeletionProcess(DeviceId asDevice)
+    {
+        _deletionProcesses.Add(new IdentityDeletionProcess(Address, asDevice));
+    }
+}
+
+public class IdentityDeletionProcess
+{
+    public IdentityDeletionProcess(IdentityAddress createdBy, DeviceId createdByDevice)
+    {
+        Id = IdentityDeletionProcessId.Generate();
+        CreatedBy = createdBy;
+        CreatedByDevice = createdByDevice;
+        Status = DeletionProcessStatus.WaitingForApproval;
+        CreatedAt = SystemTime.UtcNow;
+    }
+
+    public IdentityDeletionProcessId Id { get; }
+    public IdentityAddress CreatedBy { get; }
+    public DeviceId CreatedByDevice { get; }
+    public DeletionProcessStatus Status { get; }
+    public DateTime CreatedAt { get; }
+    public IReadOnlyList<IdentityDeletionProcessAuditLogEntry> AuditLog { get; set; }
+}
+
+public class IdentityDeletionProcessAuditLogEntry
+{
+    public IdentityDeletionProcessId ProcessId { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public string Message { get; set; }
+    public byte[] IdentityAddressHash { get; set; }
+    public byte[]? DeviceIdHash { get; set; }
+    public DeletionProcessStatus? OldStatus { get; set; }
+    public DeletionProcessStatus NewStatus { get; set; }
+}
+
+public enum DeletionProcessStatus
+{
+    WaitingForApproval
+}
+
+public record IdentityDeletionProcessId : StronglyTypedId
+{
+    public const int MAX_LENGTH = DEFAULT_MAX_LENGTH;
+
+    private const string PREFIX = "IDP";
+
+    private static readonly StronglyTypedIdHelpers UTILS = new(PREFIX, DEFAULT_VALID_CHARS, MAX_LENGTH);
+
+    private IdentityDeletionProcessId(string value) : base(value) { }
+
+    public static IdentityDeletionProcessId Generate()
+    {
+        var randomPart = StringUtils.Generate(DEFAULT_VALID_CHARS, DEFAULT_MAX_LENGTH_WITHOUT_PREFIX);
+        return new IdentityDeletionProcessId(PREFIX + randomPart);
+    }
+
+    public static Result<IdentityDeletionProcessId, DomainError> Create(string value)
+    {
+        var validationError = UTILS.Validate(value);
+
+        if (validationError != null)
+            return Result.Failure<IdentityDeletionProcessId, DomainError>(validationError);
+
+        return Result.Success<IdentityDeletionProcessId, DomainError>(new IdentityDeletionProcessId(value));
     }
 }
---
+++ Modules/Devices/test/Devices.Domain.Tests/Identities/StartDeletionProcessTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Backbone.Modules.Devices.Domain.Aggregates.Tier;
+using Backbone.Modules.Devices.Domain.Entities;
+using Enmeshed.DevelopmentKit.Identity.ValueObjects;
+using Enmeshed.Tooling;
+using FluentAssertions;
+using Xunit;
+
+namespace Backbone.Modules.Devices.Domain.Tests.Identities;
+
+public class StartDeletionProcessTests
+{
+    [Fact]
+    public void Adds_a_new_deletion_process()
+    {
+        // Arrange
+        SystemTime.Set(DateTime.Parse("2000-01-01"));
+        var identity = new Identity("", IdentityAddress.Create(Array.Empty<byte>(), "id1"), Array.Empty<byte>(), TierId.Generate(), 1);
+        var asDevice = DeviceId.Parse("DVC");
+
+        // Act
+        identity.StartDeletionProcess(asDevice);
+
+        // Assert
+        identity.DeletionProcesses.Should().HaveCount(1);
+        var deletionProcess = identity.DeletionProcesses[0];
+        deletionProcess.Should().NotBeNull();
+
+        deletionProcess.Id.Should().NotBeNull();
+        deletionProcess.Id.Value.Should().HaveLength(20);
+
+        deletionProcess.CreatedBy.Should().Be(identity.Address);
+        deletionProcess.Status.Should().Be(DeletionProcessStatus.WaitingForApproval);
+        deletionProcess.CreatedAt.Should().Be(SystemTime.UtcNow);
+        deletionProcess.CreatedByDevice.Should().Be(asDevice);
+    }
+
+    [Fact]
+    public void The_deletion_process_has_an_audit_log_entry()
+    {
+        // Arrange
+        SystemTime.Set(DateTime.Parse("2000-01-01"));
+        var identity = new Identity("", IdentityAddress.Create(Array.Empty<byte>(), "id1"), Array.Empty<byte>(), TierId.Generate(), 1);
+        var asDevice = DeviceId.Parse("DVC");
+
+        // Act
+        identity.StartDeletionProcess(asDevice);
+
+        // Assert
+        var deletionProcess = identity.DeletionProcesses[0];
+
+        deletionProcess.AuditLog.Should().HaveCount(1);
+
+        var auditLogEntry = deletionProcess.AuditLog[0];
+        auditLogEntry.ProcessId.Should().Be(deletionProcess.Id);
+        auditLogEntry.CreatedAt.Should().Be(SystemTime.UtcNow);
+        auditLogEntry.Message.Should().NotBe("Created"); // TODO: real message
+        auditLogEntry.IdentityAddressHash.Should().NotBeEmpty();
+        auditLogEntry.DeviceIdHash.Should().NotBeEmpty();
+        auditLogEntry.OldStatus.Should().NotBeNull();
+        auditLogEntry.NewStatus.Should().Be(DeletionProcessStatus.WaitingForApproval);
+    }
+
+    public void OnlyOneActiveProcess()MinimumNumberOfApprovals
+}
