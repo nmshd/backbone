5673beab707f00d674677f5a538a914ab475fccf
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-10-25 10:21:50 +0200

    chore: extracted IdentDeletionProcessId from the Identity class

--- Modules/Devices/src/Devices.Domain/Entities/Identities/Identity.cs
+++ Modules/Devices/src/Devices.Domain/Entities/Identities/Identity.cs
@@ -1,10 +1,6 @@
-﻿using System.Collections.Generic;
-using Backbone.Modules.Devices.Domain.Aggregates.Tier;
-using CSharpFunctionalExtensions;
+﻿using Backbone.Modules.Devices.Domain.Aggregates.Tier;
 using Enmeshed.BuildingBlocks.Domain;
 using Enmeshed.BuildingBlocks.Domain.Errors;
-using Enmeshed.BuildingBlocks.Domain.StronglyTypedIds.Records;
-using Enmeshed.DevelopmentKit.Identity.Entities;
 using Enmeshed.DevelopmentKit.Identity.ValueObjects;
 using Enmeshed.Tooling;
 
@@ -70,31 +66,4 @@ public class Identity
 public enum DeletionProcessStatus
 {
     WaitingForApproval
-}
-
-public record IdentityDeletionProcessId : StronglyTypedId
-{
-    public const int MAX_LENGTH = DEFAULT_MAX_LENGTH;
-
-    private const string PREFIX = "IDP";
-
-    private static readonly StronglyTypedIdHelpers UTILS = new(PREFIX, DEFAULT_VALID_CHARS, MAX_LENGTH);
-
-    private IdentityDeletionProcessId(string value) : base(value) { }
-
-    public static IdentityDeletionProcessId Generate()
-    {
-        var randomPart = StringUtils.Generate(DEFAULT_VALID_CHARS, DEFAULT_MAX_LENGTH_WITHOUT_PREFIX);
-        return new IdentityDeletionProcessId(PREFIX + randomPart);
-    }
-
-    public static Result<IdentityDeletionProcessId, DomainError> Create(string value)
-    {
-        var validationError = UTILS.Validate(value);
-
-        if (validationError != null)
-            return Result.Failure<IdentityDeletionProcessId, DomainError>(validationError);
-
-        return Result.Success<IdentityDeletionProcessId, DomainError>(new IdentityDeletionProcessId(value));
-    }
 }
---
+++ Modules/Devices/src/Devices.Domain/Entities/Identities/IdentityDeletionProcessId.cs
@@ -0,0 +1,33 @@
+﻿using CSharpFunctionalExtensions;
+using Enmeshed.BuildingBlocks.Domain;
+using Enmeshed.BuildingBlocks.Domain.Errors;
+using Enmeshed.BuildingBlocks.Domain.StronglyTypedIds.Records;
+
+namespace Backbone.Modules.Devices.Domain.Entities.Identities;
+
+public record IdentityDeletionProcessId : StronglyTypedId
+{
+    public const int MAX_LENGTH = DEFAULT_MAX_LENGTH;
+
+    private const string PREFIX = "IDP";
+
+    private static readonly StronglyTypedIdHelpers UTILS = new(PREFIX, DEFAULT_VALID_CHARS, MAX_LENGTH);
+
+    private IdentityDeletionProcessId(string value) : base(value) { }
+
+    public static IdentityDeletionProcessId Generate()
+    {
+        var randomPart = StringUtils.Generate(DEFAULT_VALID_CHARS, DEFAULT_MAX_LENGTH_WITHOUT_PREFIX);
+        return new IdentityDeletionProcessId(PREFIX + randomPart);
+    }
+
+    public static Result<IdentityDeletionProcessId, DomainError> Create(string value)
+    {
+        var validationError = UTILS.Validate(value);
+
+        if (validationError != null)
+            return Result.Failure<IdentityDeletionProcessId, DomainError>(validationError);
+
+        return Result.Success<IdentityDeletionProcessId, DomainError>(new IdentityDeletionProcessId(value));
+    }
+}
