19ce6838aef5813e905c4e74eef657111fc9cbbb
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-10-26 09:55:19 +0200

    feat: ListIndividualQuotas command added

---
+++ Modules/Quotas/src/Quotas.Application/Identities/Queries/GetIndividualQuotas/ListIndividualQuotasQuery.cs
@@ -0,0 +1,16 @@
+﻿using Enmeshed.BuildingBlocks.Application.Pagination;
+using MediatR;
+
+namespace Backbone.Modules.Quotas.Application.Identities.Queries.GetIndividualQuotas;
+
+public class ListIndividualQuotasQuery : IRequest<ListIndividualQuotasResponse>
+{
+    public ListIndividualQuotasQuery(PaginationFilter paginationFilter, string address)
+    {
+        PaginationFilter = paginationFilter;
+        Address = address;
+    }
+
+    public PaginationFilter PaginationFilter { get; set; }
+    public string Address { get; set; }
+}
