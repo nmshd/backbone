ab476cc94aa3302f2e27b015def2e0aff039d273
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-10-30 17:23:44 +0000

    chore: add assembly info for tests

--- Modules/Devices/src/Devices.Domain/AssemblyInfo.cs
+++ Modules/Devices/src/Devices.Domain/AssemblyInfo.cs
@@ -1,3 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Backbone.Modules.Devices.Application.Tests")]
+[assembly: InternalsVisibleTo("Backbone.Modules.Devices.Domain.Tests")]
