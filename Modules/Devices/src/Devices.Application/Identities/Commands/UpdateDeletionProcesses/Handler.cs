169f622b4c2e7c1e9f107ad002ed998950208bb2
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-10-30 17:31:52 +0000

    feat: command

---
+++ Modules/Devices/src/Devices.Application/Identities/Commands/UpdateDeletionProcesses/Handler.cs
@@ -0,0 +1,29 @@
+﻿using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
+using Backbone.Modules.Devices.Application.Infrastructure.Persistence.Repository;
+using Backbone.Modules.Devices.Application.IntegrationEvents.Outgoing;
+using MediatR;
+
+namespace Backbone.Modules.Devices.Application.Identities.Commands.UpdateDeletionProcesses;
+public class Handler : IRequestHandler<UpdateDeletionProcessesCommand>
+{
+    private readonly IIdentitiesRepository _identitiesRepository;
+    private readonly IEventBus _eventBus;
+
+    public Handler(IIdentitiesRepository identitiesRepository, IEventBus eventBus)
+    {
+        _identitiesRepository = identitiesRepository;
+        _eventBus = eventBus;
+    }
+    public async Task Handle(UpdateDeletionProcessesCommand request, CancellationToken cancellationToken)
+    {
+        var identities = await _identitiesRepository.FindAllWithAtLeastOneApprovedDeletionProcess(cancellationToken);
+        foreach (var identity in identities)
+        {
+            var relevantDeletionProcess = identity.DeletionProcesses.FirstOrDefault(dp => dp.Status == Domain.Entities.Identities.DeletionProcessStatus.Approved);
+            if(relevantDeletionProcess != null) {
+                identity.MarkAsToBeDeleted();
+                _eventBus.Publish(new IdentityStatusChangedIntegrationEvent(identity.Address, identity.IdentityStatus));
+            }
+        }
+    }
+}
---
+++ Modules/Devices/src/Devices.Application/Identities/Commands/UpdateDeletionProcesses/UpdateDeletionProcessesCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace Backbone.Modules.Devices.Application.Identities.Commands.UpdateDeletionProcesses;
+public class UpdateDeletionProcessesCommand : IRequest
+{
+}
