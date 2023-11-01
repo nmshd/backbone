792ee61f999523c2d24532f7e78f9088f18f8a1e
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-11-01 14:28:04 +0100

    feat: change UpdateRegistration() return type

--- Modules/Devices/src/Devices.Application/Infrastructure/PushNotifications/IPushService.cs
+++ Modules/Devices/src/Devices.Application/Infrastructure/PushNotifications/IPushService.cs
@@ -1,4 +1,5 @@
 using Backbone.DevelopmentKit.Identity.ValueObjects;
+using Backbone.Modules.Devices.Application.Tests.Tests.PushNotifications;
 using Backbone.Modules.Devices.Domain.Aggregates.PushNotifications.Handles;
 using Environment = Backbone.Modules.Devices.Domain.Aggregates.PushNotifications.Environment;
 
@@ -6,7 +7,7 @@ namespace Backbone.Modules.Devices.Application.Infrastructure.PushNotifications;
 
 public interface IPushService
 {
-    Task UpdateRegistration(IdentityAddress address, DeviceId deviceId, PnsHandle handle, string appId, Environment environment, CancellationToken cancellationToken);
+    Task<DevicePushIdentifier> UpdateRegistration(IdentityAddress address, DeviceId deviceId, PnsHandle handle, string appId, Environment environment, CancellationToken cancellationToken);
     Task DeleteRegistration(DeviceId deviceId, CancellationToken cancellationToken);
     Task SendNotification(IdentityAddress recipient, object notification, CancellationToken cancellationToken);
 }
