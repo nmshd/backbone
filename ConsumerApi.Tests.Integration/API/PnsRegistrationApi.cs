1530594284ccf64f9ecb0beff245cd9b445dc770
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-11-04 17:56:06 +0100

    feat: add PnsRegistrationApi for use in integration tests

---
+++ ConsumerApi.Tests.Integration/API/PnsRegistrationApi.cs
@@ -0,0 +1,13 @@
+﻿using Backbone.ConsumerApi.Tests.Integration.Models;
+using Backbone.Modules.Devices.Application.PushNotifications.Commands.UpdateDeviceRegistration;
+
+namespace Backbone.ConsumerApi.Tests.Integration.API;
+internal class PnsRegistrationApi : BaseApi // NV_ADD
+{
+    public PnsRegistrationApi(HttpClientFactory factory) : base(factory) { }
+
+    internal async Task<HttpResponse<UpdateDeviceRegistrationResponse>> TestRegisterForPushNotification(RequestConfiguration requestConfiguration)
+    {
+        return await Put<UpdateDeviceRegistrationResponse>("/Devices/Self/PushNotifications", requestConfiguration);
+    }
+}
