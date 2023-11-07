3187eb07c02da838a5fba3c687f3b1913c8f1092
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-11-07 16:53:21 +0100

    feat: add PersistTokenContentInDb migration

---
+++ Modules/Tokens/src/Tokens.Infrastructure.Database.SqlServer/Migrations/20231107153049_PersistTokenBodyInDb.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backbone.Modules.Tokens.Infrastructure.Database.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class PersistTokenBodyInDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Content",
+                table: "Tokens",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Content",
+                table: "Tokens");
+        }
+    }
+}
