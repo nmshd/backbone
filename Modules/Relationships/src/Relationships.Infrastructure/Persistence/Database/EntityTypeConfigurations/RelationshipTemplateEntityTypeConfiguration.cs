043cca3624ecc68b99552d5078db976d17dd8453
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-11-14 14:12:06 +0100

    chore: return builder.Ignore() to avoid potential problems while merging before the blob storage one

--- Modules/Relationships/src/Relationships.Infrastructure/Persistence/Database/EntityTypeConfigurations/RelationshipTemplateEntityTypeConfiguration.cs
+++ Modules/Relationships/src/Relationships.Infrastructure/Persistence/Database/EntityTypeConfigurations/RelationshipTemplateEntityTypeConfiguration.cs
@@ -11,5 +11,7 @@ public class RelationshipTemplateEntityTypeConfiguration : IEntityTypeConfigurat
         builder.HasIndex(x => x.CreatedBy);
         builder.HasIndex(x => x.DeletedAt);
         builder.HasIndex(x => x.ExpiresAt);
+
+        builder.Ignore(x => x.Content);
     }
 }
