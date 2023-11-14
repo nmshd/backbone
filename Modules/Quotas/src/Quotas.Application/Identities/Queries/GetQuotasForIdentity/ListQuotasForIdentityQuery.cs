82bf7a74e4a41b55f5b14175ab0b93ab8bc9a107
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-11-14 16:50:23 +0100

    chore: remove the unnecessary properties

--- Modules/Quotas/src/Quotas.Application/Identities/Queries/GetQuotasForIdentity/ListQuotasForIdentityQuery.cs
+++ Modules/Quotas/src/Quotas.Application/Identities/Queries/GetQuotasForIdentity/ListQuotasForIdentityQuery.cs
@@ -3,14 +3,4 @@ using MediatR;
 
 namespace Backbone.Modules.Quotas.Application.Identities.Queries.GetQuotasForIdentity;
 
-public class ListQuotasForIdentityQuery : IRequest<ListQuotasForIdentityResponse>
-{
-    public ListQuotasForIdentityQuery(PaginationFilter paginationFilter, string address)
-    {
-        PaginationFilter = paginationFilter;
-        Address = address;
-    }
-
-    public PaginationFilter PaginationFilter { get; set; }
-    public string Address { get; set; }
-}
+public class ListQuotasForIdentityQuery : IRequest<ListQuotasForIdentityResponse> { }
