15dc6e4163345fe338064c938113dce55fb84457
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-11-15 16:58:43 +0000

    chore: add Postgres migrations for AddMaxIdentitiesToClients

---
+++ Modules/Devices/src/Devices.Infrastructure.Database.Postgres/Migrations/20231115162705_AddMaxIdentitiesToClients.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backbone.Modules.Devices.Infrastructure.Database.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMaxIdentitiesToClients : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MaxIdentities",
+                table: "OpenIddictApplications",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaxIdentities",
+                table: "OpenIddictApplications");
+        }
+    }
+}
--- Modules/Devices/src/Devices.Infrastructure/CompiledModels/Postgres/CustomOpenIddictEntityFrameworkCoreApplicationEntityType.cs
+++ Modules/Devices/src/Devices.Infrastructure/CompiledModels/Postgres/CustomOpenIddictEntityFrameworkCoreApplicationEntityType.cs
@@ -94,6 +94,12 @@ namespace Backbone.Modules.Devices.Infrastructure.CompiledModels.Postgres
                 fieldInfo: typeof(OpenIddictEntityFrameworkCoreApplication<string, CustomOpenIddictEntityFrameworkCoreAuthorization, CustomOpenIddictEntityFrameworkCoreToken>).GetField("<DisplayNames>k__BackingField", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly),
                 nullable: true);
 
+            var maxIdentities = runtimeEntityType.AddProperty(
+                "MaxIdentities",
+                typeof(int),
+                propertyInfo: typeof(CustomOpenIddictEntityFrameworkCoreApplication).GetProperty("MaxIdentities", BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly),
+                fieldInfo: typeof(CustomOpenIddictEntityFrameworkCoreApplication).GetField("<MaxIdentities>k__BackingField", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly));
+
             var permissions = runtimeEntityType.AddProperty(
                 "Permissions",
                 typeof(string),
