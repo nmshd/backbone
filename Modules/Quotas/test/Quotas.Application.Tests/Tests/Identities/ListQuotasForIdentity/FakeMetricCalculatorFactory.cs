fe55a8eae5351317a318ffb13910a189799aa28c
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-11-17 13:33:32 +0100

    test: add FakeMetricCalculatorFactory class to correctly test the metric usage test: fix the assertions to not be as easily broken

---
+++ Modules/Quotas/test/Quotas.Application.Tests/Tests/Identities/ListQuotasForIdentity/FakeMetricCalculatorFactory.cs
@@ -0,0 +1,85 @@
+﻿using Backbone.Modules.Quotas.Domain;
+using Backbone.Modules.Quotas.Domain.Metrics;
+
+namespace Backbone.Modules.Quotas.Application.Tests.Tests.Identities.ListQuotasForIdentity;
+
+public class FakeMetricCalculatorFactory : MetricCalculatorFactory
+{
+    private readonly int? _numberOfFiles;
+    private readonly int? _numberOfSentMessages;
+    private readonly int? _numberOfRelationships;
+    private readonly int? _numberOfRelationshipTemplates;
+    private readonly int? _numberOfTokens;
+    private readonly int? _amountOfUsedFileStorageSpace;
+
+    public FakeMetricCalculatorFactory(int? numberOfFiles = null, int? numberOfSentMessages = null, int? numberOfRelationships = null,
+        int? numberOfRelationshipTemplates = null, int? numberOfTokens = null, int? amountOfUsedFileStorageSpace = null)
+    {
+        _numberOfFiles = numberOfFiles;
+        _numberOfSentMessages = numberOfSentMessages;
+        _numberOfRelationships = numberOfRelationships;
+        _numberOfRelationshipTemplates = numberOfRelationshipTemplates;
+        _numberOfTokens = numberOfTokens;
+        _amountOfUsedFileStorageSpace = amountOfUsedFileStorageSpace;
+    }
+
+    protected override IMetricCalculator CreateNumberOfFilesMetricCalculator()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override IMetricCalculator CreateNumberOfSentMessagesMetricCalculator()
+    {
+        return _numberOfSentMessages.HasValue ? new FakeNumberOfSentMessagesMetricCalculator(_numberOfSentMessages.Value) : throw new ArgumentNullException(nameof(_numberOfSentMessages));
+    }
+
+    protected override IMetricCalculator CreateNumberOfRelationshipsMetricCalculator()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override IMetricCalculator CreateNumberOfRelationshipTemplatesMetricCalculator()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override IMetricCalculator CreateNumberOfTokensMetricCalculator()
+    {
+        return _numberOfTokens.HasValue ? new FakeNumberOfTokensMetricCalculator(_numberOfTokens.Value) : throw new ArgumentNullException(nameof(_numberOfTokens));
+    }
+
+    protected override IMetricCalculator CreateUsedFileStorageSpaceCalculator()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class FakeNumberOfSentMessagesMetricCalculator : IMetricCalculator
+{
+    public FakeNumberOfSentMessagesMetricCalculator(int numberOfSentMessages)
+    {
+        NumberOfSentMessages = numberOfSentMessages;
+    }
+
+    public int NumberOfSentMessages { get; set; }
+
+    public Task<uint> CalculateUsage(DateTime from, DateTime to, string identityAddress, CancellationToken cancellationToken)
+    {
+        return Task.FromResult((uint)NumberOfSentMessages);
+    }
+}
+
+public class FakeNumberOfTokensMetricCalculator : IMetricCalculator
+{
+    public FakeNumberOfTokensMetricCalculator(int numberOfTokens)
+    {
+        NumberOfTokens = numberOfTokens;
+    }
+
+    public int NumberOfTokens { get; set; }
+
+    public Task<uint> CalculateUsage(DateTime from, DateTime to, string identityAddress, CancellationToken cancellationToken)
+    {
+        return Task.FromResult((uint)NumberOfTokens);
+    }
+}
--- Modules/Quotas/test/Quotas.Application.Tests/Tests/Identities/ListQuotasForIdentity/HandlerTests.cs
+++ Modules/Quotas/test/Quotas.Application.Tests/Tests/Identities/ListQuotasForIdentity/HandlerTests.cs
@@ -12,29 +12,28 @@ using Backbone.Modules.Quotas.Domain;
 using Backbone.Modules.Quotas.Domain.Aggregates.Identities;
 using Backbone.Modules.Quotas.Domain.Aggregates.Metrics;
 using Backbone.Modules.Quotas.Domain.Aggregates.Tiers;
-using Backbone.Modules.Quotas.Domain.Metrics;
 using FakeItEasy;
 using FluentAssertions;
 using Xunit;
 using static Backbone.UnitTestTools.Data.TestDataGenerator;
 
-namespace Backbone.Modules.Quotas.Application.Tests.Tests.Identities.GetQuotasForIdentity;
+namespace Backbone.Modules.Quotas.Application.Tests.Tests.Identities.ListQuotasForIdentity;
+
 public class HandlerTests
 {
     [Fact]
     public async Task Returns_individual_and_tier_quotas_for_identity()
     {
         // Arrange
-        var metric = new Metric(MetricKey.NumberOfSentMessages, "Number Of Sent Messages");
+        var metric1 = new Metric(MetricKey.NumberOfSentMessages, "Number Of Sent Messages");
+        var metric2 = new Metric(MetricKey.NumberOfTokens, "Number Of Tokens");
         var identity = new Identity(CreateRandomIdentityAddress(), new TierId("SomeTierId"));
 
         var max = 5;
         var period = QuotaPeriod.Month;
 
-        identity.AssignTierQuotaFromDefinition(new TierQuotaDefinition(metric.Key, max, period));
-        identity.CreateIndividualQuota(metric.Key, max, period);
-
-        var dummyMetricCalculationFactory = A.Fake<MetricCalculatorFactory>();
+        identity.AssignTierQuotaFromDefinition(new TierQuotaDefinition(metric1.Key, max, period));
+        identity.CreateIndividualQuota(metric2.Key, max, period);
 
         var fakeUserContext = A.Fake<IUserContext>();
         A.CallTo(() => fakeUserContext.GetAddress()).Returns(identity.Address);
@@ -42,22 +41,34 @@ public class HandlerTests
         var fakeIdentitiesRepository = A.Fake<IIdentitiesRepository>();
         A.CallTo(() => fakeIdentitiesRepository.Find(identity.Address, A<CancellationToken>._, A<bool>._)).Returns(identity);
 
-        var handler = new Handler(fakeUserContext, fakeIdentitiesRepository, dummyMetricCalculationFactory);
+        var fakeMetricsRepository = A.Fake<IMetricsRepository>();
+        A.CallTo(() => fakeMetricsRepository.FindAll(CancellationToken.None, false)).Returns(new List<Metric> { metric1, metric2 });
+
+        var fakeMetricCalculationFactory = new FakeMetricCalculatorFactory(numberOfSentMessages: 1, numberOfTokens: 1);
+
+        var handler = new Handler(fakeUserContext, fakeIdentitiesRepository, fakeMetricsRepository, fakeMetricCalculationFactory);
 
         // Act
-        var quotas = (await handler.Handle(new ListQuotasForIdentityQuery(), CancellationToken.None)).Items.ToList();
+        var quotas = (await handler.Handle(new ListQuotasForIdentityQuery(identity.Address), CancellationToken.None)).ToList();
 
         // Assert
         quotas.Should().HaveCount(2);
 
-        quotas[0].Id.Should().Be(identity.IndividualQuotas.First().Id);
-        quotas[0].Max.Should().Be(max);
-        quotas[0].Period.Should().Be(period.ToString());
-        quotas[0].Source.Should().Be(QuotaSource.Individual);
+        var tierQuota = quotas.Single(q => q.Source == QuotaSource.Tier);
+        var individualQuota = quotas.Single(q => q.Source == QuotaSource.Individual);
 
-        quotas[1].Id.Should().Be(identity.TierQuotas.First().Id);
-        quotas[1].Max.Should().Be(max);
-        quotas[1].Period.Should().Be(period.ToString());
-        quotas[1].Source.Should().Be(QuotaSource.Tier);
+        tierQuota.Should().NotBeNull();
+        tierQuota.Max.Should().Be(5);
+        tierQuota.Usage.Should().Be(1);
+        tierQuota.Period.Should().Be(identity.TierQuotas.First().Period.ToString());
+        tierQuota.Metric.Key.Should().Be(metric1.Key.Value);
+        tierQuota.Metric.DisplayName.Should().Be(metric1.DisplayName);
+
+        individualQuota.Should().NotBeNull();
+        individualQuota.Max.Should().Be(5);
+        individualQuota.Usage.Should().Be(1);
+        individualQuota.Period.Should().Be(identity.IndividualQuotas.First().Period.ToString());
+        individualQuota.Metric.Key.Should().Be(metric2.Key.Value);
+        individualQuota.Metric.DisplayName.Should().Be(metric2.DisplayName);
     }
 }
