e63f6120a1df75fa480fe7956ff7ca6f16622346
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-11-17 09:16:14 +0100

    chore: remove unused directives

--- Modules/Quotas/src/Quotas.ConsumerApi/Controllers/QuotasController.cs
+++ Modules/Quotas/src/Quotas.ConsumerApi/Controllers/QuotasController.cs
@@ -1,16 +1,10 @@
-﻿using Backbone.BuildingBlocks.API;
-using Backbone.BuildingBlocks.API.Mvc;
-using Backbone.BuildingBlocks.Application.Abstractions.Exceptions;
-using Backbone.BuildingBlocks.Application.Pagination;
-using Backbone.Modules.Quotas.Application;
+﻿using Backbone.BuildingBlocks.API.Mvc;
 using Backbone.Modules.Quotas.Application.DTOs;
 using Backbone.Modules.Quotas.Application.Identities.Queries.GetQuotasForIdentity;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
-using ApplicationException = Backbone.BuildingBlocks.Application.Abstractions.Exceptions.ApplicationException;
 
 namespace Backbone.Modules.Quotas.ConsumerApi.Controllers;
 
