fe32de4b20f57f613dc4a82723a76bee06d5a776
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-11-17 08:54:01 +0100

    chore: move property below ctor

--- Modules/Quotas/src/Quotas.Application/Identities/Queries/GetQuotasForIdentity/ListQuotasForIdentityResponse.cs
+++ Modules/Quotas/src/Quotas.Application/Identities/Queries/GetQuotasForIdentity/ListQuotasForIdentityResponse.cs
@@ -1,14 +1,13 @@
-﻿using Backbone.BuildingBlocks.Application.Pagination;
-using Backbone.Modules.Quotas.Application.DTOs;
+﻿using Backbone.Modules.Quotas.Application.DTOs;
 
 namespace Backbone.Modules.Quotas.Application.Identities.Queries.GetQuotasForIdentity;
 
 public class ListQuotasForIdentityResponse
 {
-    public IEnumerable<QuotaDTO> Items { get; }
-
     public ListQuotasForIdentityResponse(IEnumerable<QuotaDTO> items)
     {
         Items = items;
     }
+
+    public IEnumerable<QuotaDTO> Items { get; }
 }
