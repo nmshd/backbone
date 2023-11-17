a317a9ab78aba671df3418b3c8b4970f9f775344
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-11-17 13:36:02 +0100

    chore: fix formatting issues

--- Modules/Quotas/src/Quotas.Application/ApplicationErrors.cs
+++ Modules/Quotas/src/Quotas.Application/ApplicationErrors.cs
@@ -5,7 +5,7 @@ public class ApplicationErrors
 {
     public static ApplicationError QueryAddressDoesNotMatchTheCurrentUser(string queryAddress = "")
     {
-        return new ApplicationError("error.platform.validation.quota.queryAddressDoesNotMatchTheCurrentUser", 
+        return new ApplicationError("error.platform.validation.quota.queryAddressDoesNotMatchTheCurrentUser",
             $"The identity address {queryAddress} passed through the URL does not match the current user's identity address.");
     }
 }
