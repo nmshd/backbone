b8750c4dbdb30acc0d7ebb135476803c6c8f6f85
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-11-21 12:26:08 +0100

    feat: remove DeletionCertificate and SignedChallengeDTO from DeleteDeviceCommand

--- Modules/Devices/src/Devices.Application/Devices/Commands/DeleteDevice/DeleteDeviceCommand.cs
+++ Modules/Devices/src/Devices.Application/Devices/Commands/DeleteDevice/DeleteDeviceCommand.cs
@@ -1,5 +1,4 @@
 ﻿using Backbone.DevelopmentKit.Identity.ValueObjects;
-using Backbone.Modules.Devices.Application.Devices.DTOs;
 using MediatR;
 
 namespace Backbone.Modules.Devices.Application.Devices.Commands.DeleteDevice;
@@ -7,6 +6,4 @@ namespace Backbone.Modules.Devices.Application.Devices.Commands.DeleteDevice;
 public class DeleteDeviceCommand : IRequest
 {
     public DeviceId DeviceId { get; set; }
-    public byte[] DeletionCertificate { get; set; }
-    public SignedChallengeDTO SignedChallenge { get; set; }
 }
--- Modules/Devices/src/Devices.Application/Devices/Commands/DeleteDevice/DeleteDeviceValidator.cs
+++ Modules/Devices/src/Devices.Application/Devices/Commands/DeleteDevice/DeleteDeviceValidator.cs
@@ -1,5 +1,4 @@
 ﻿using Backbone.BuildingBlocks.Application.FluentValidation;
-using Backbone.Modules.Devices.Application.Devices.DTOs.Validators;
 using FluentValidation;
 
 namespace Backbone.Modules.Devices.Application.Devices.Commands.DeleteDevice;
@@ -10,12 +9,5 @@ public class DeleteDeviceValidator : AbstractValidator<DeleteDeviceCommand>
     public DeleteDeviceValidator()
     {
         RuleFor(c => c.DeviceId).DetailedNotEmpty();
-
-        RuleFor(c => c.DeletionCertificate).DetailedNotEmpty();
-
-        RuleFor(c => c.SignedChallenge)
-            .Cascade(CascadeMode.Stop)
-            .DetailedNotEmpty()
-            .SetValidator(new SignedChallengeDTOValidator());
     }
 }
