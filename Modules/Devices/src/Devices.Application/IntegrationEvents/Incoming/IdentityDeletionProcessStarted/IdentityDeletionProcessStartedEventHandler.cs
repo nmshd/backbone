92c7c32a674e31304938157bd9eff8a01fe0ffd8
Daniel Almeida <115644988+daniel-almeida-konkconsulting@users.noreply.github.com>
2023-11-24 18:06:47 +0000

    feat: add empty event handler for identity deletion process started

---
+++ Modules/Devices/src/Devices.Application/IntegrationEvents/Incoming/IdentityDeletionProcessStarted/IdentityDeletionProcessStartedEventHandler.cs
@@ -0,0 +1,12 @@
+﻿using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
+using Backbone.Modules.Devices.Application.IntegrationEvents.Outgoing;
+
+namespace Backbone.Modules.Devices.Application.IntegrationEvents.Incoming.IdentityDeletionProcessStarted;
+
+public class IdentityDeletionProcessStartedEventHandler : IIntegrationEventHandler<IdentityDeletionProcessStartedEvent>
+{
+    public Task Handle(IdentityDeletionProcessStartedEvent @event)
+    {
+        throw new NotImplementedException();
+    }
+}
