37dc10be6cc8601a9caf0520f3a4a953f6c6520a
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-11-28 18:30:52 +0000

    feat: add load script for postgres

--- docker-compose/dump_postgres.bat
+++ docker-compose/dump_postgres.bat
@@ -1,3 +1,3 @@
 
-docker exec --env PGPASSWORD=admin -it bkb-postgres pg_dump -h postgres -U postgres --create enmeshed -f tmp/enmeshed.pg
+docker exec --env PGPASSWORD=admin -it bkb-postgres pg_dump -h postgres -U postgres enmeshed -f tmp/enmeshed.pg
 docker cp bkb-postgres:/tmp/enmeshed.pg ./dumps/enmeshed.pg
