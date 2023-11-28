be6d778d0e47b92b542f4f012d8e58926e83c77d
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-11-28 16:27:00 +0000

    feat: change backup method for sql server

--- docker-compose/dump_sqlserver.bat
+++ docker-compose/dump_sqlserver.bat
@@ -1,2 +1,3 @@
-docker exec -it bkb-mssql_server /opt/mssql-tools/bin/sqlcmd -b -V16 -P Passw0rd -S localhost -U SA  -Q "BACKUP DATABASE enmeshed TO DISK = N'/tmp/enmeshed.bak' with NOREWIND"
-docker cp bkb-mssql_server:/tmp/enmeshed.bak ./dumps/enmeshed.bak
+docker cp dump_sqlserver.sh bkb-mssql_server:/tmp/dump_sqlserver.sh
+docker exec -it bkb-mssql_server /tmp/dump_sqlserver.sh
+docker cp bkb-mssql_server:/tmp/dump.sql ./dumps/enmeshed.csv
