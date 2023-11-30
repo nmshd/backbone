ae7268f8be219caf1265e7e46373266f55c09884
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-11-30 09:50:10 +0000

    fix: remove typo from Devices ApplicationErrors error

--- Modules/Devices/src/Devices.Application/ApplicationErrors.cs
+++ Modules/Devices/src/Devices.Application/ApplicationErrors.cs
@@ -63,7 +63,7 @@ public static class ApplicationErrors
 
         public static ApplicationError ClientReachedIdentitiesLimit()
         {
-            return new ApplicationError("error.platform.validation.device.clientReachedIdentitiesLimit", "The clients Identity limit has been reached. A new Identity cannot be created with this client.");
+            return new ApplicationError("error.platform.validation.device.clientReachedIdentitiesLimit", "The client's Identity limit has been reached. A new Identity cannot be created with this client.");
         }
     }
 }
