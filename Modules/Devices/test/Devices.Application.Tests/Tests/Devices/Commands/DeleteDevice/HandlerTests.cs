043e3676c1727806e618f99933c681eead52bec5
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-12-01 11:34:31 +0100

    feat: refactor test to conform to changes in the handler

--- Modules/Devices/test/Devices.Application.Tests/Tests/Devices/Commands/DeleteDevice/HandlerTests.cs
+++ Modules/Devices/test/Devices.Application.Tests/Tests/Devices/Commands/DeleteDevice/HandlerTests.cs
@@ -12,40 +12,80 @@ namespace Backbone.Modules.Devices.Application.Tests.Tests.Devices.Commands.Dele
 public class HandlerTests
 {
     [Fact]
-    public async Task Deletes_device_owned_by_identity()
+    public async Task Deletes_un_onboarded_device_owned_by_identity()
     {
         // Arrange
         var startTime = SystemTime.UtcNow;
+        var deleteDeviceTestSetup = new IdentityWithOnboardedAndUnonboardedDevicesSetup();
 
-        var identity = TestDataGenerator.CreateIdentity();
-        var device = new Device(identity);
-
-        var mockIdentitiesRepository = A.Fake<IIdentitiesRepository>();
-        A.CallTo(() => mockIdentitiesRepository.GetDeviceById(device.Id, A<CancellationToken>._, A<bool>._)).Returns(device);
-
-        var fakeUserContext = A.Fake<IUserContext>();
-        A.CallTo(() => fakeUserContext.GetAddress()).Returns(identity.Address);
-        A.CallTo(() => fakeUserContext.GetDeviceId()).Returns(device.Id);
-
-        var deleteDeviceCommand = new DeleteDeviceCommand
+        var deleteUnOnboardedDeviceCommand = new DeleteDeviceCommand()
         {
-            DeviceId = device.Id
+            DeviceId = deleteDeviceTestSetup.UnOnboardedDevice.Id
         };
 
-        var handler = CreateHandler(mockIdentitiesRepository, fakeUserContext);
-
         // Act
-        await handler.Handle(deleteDeviceCommand, CancellationToken.None);
+        await deleteDeviceTestSetup.Handler.Handle(deleteUnOnboardedDeviceCommand, CancellationToken.None);
 
         // Assert
-        device.DeletedAt.Should().NotBeNull();
-        device.DeletedAt.Should().BeAfter(startTime);
-        device.DeletedByDevice.Should().Be(deleteDeviceCommand.DeviceId);
+        deleteDeviceTestSetup.UnOnboardedDevice.DeletedAt.Should().NotBeNull();
+        deleteDeviceTestSetup.UnOnboardedDevice.DeletedAt.Should().BeAfter(startTime);
+        deleteDeviceTestSetup.UnOnboardedDevice.DeletedByDevice.Should().Be(deleteDeviceTestSetup.OnboardedDevice.Id);
 
-        A.CallTo(() => mockIdentitiesRepository.Update(
-            device,
+        A.CallTo(() => deleteDeviceTestSetup.MockIdentitiesRepository.Update(
+            deleteDeviceTestSetup.UnOnboardedDevice,
             A<CancellationToken>._
         )).MustHaveHappenedOnceExactly();
+    }
+}
+
+public class IdentityWithOnboardedAndUnonboardedDevicesSetup
+{
+    public readonly Identity Identity;
+    public readonly Device UnOnboardedDevice;
+    public readonly Device OnboardedDevice;
+    public readonly IIdentitiesRepository MockIdentitiesRepository;
+    public readonly IUserContext FakeUserContext;
+    public readonly Handler Handler;
+
+    public IdentityWithOnboardedAndUnonboardedDevicesSetup()
+    {
+        Identity = TestDataGenerator.CreateIdentity();
+        UnOnboardedDevice = CreateUnOnboardedDevice(Identity);
+        OnboardedDevice = CreateOnboardedDevice(Identity);
+
+        MockIdentitiesRepository = A.Fake<IIdentitiesRepository>();
+        A.CallTo(() => MockIdentitiesRepository.GetDeviceById(UnOnboardedDevice.Id, A<CancellationToken>._, A<bool>._)).Returns(UnOnboardedDevice);
+        A.CallTo(() => MockIdentitiesRepository.GetDeviceById(OnboardedDevice.Id, A<CancellationToken>._, A<bool>._)).Returns(OnboardedDevice);
+
+        FakeUserContext = A.Fake<IUserContext>();
+        A.CallTo(() => FakeUserContext.GetAddress()).Returns(Identity.Address);
+        A.CallTo(() => FakeUserContext.GetDeviceId()).Returns(OnboardedDevice.Id);
+
+        Handler = CreateHandler(MockIdentitiesRepository, FakeUserContext);
+    }
+
+    private static Device CreateUnOnboardedDevice(Identity identity)
+    {
+        var unOnboardedDevice = new Device(identity);
+        var unOnboardedDeviceUser = new ApplicationUser(identity, unOnboardedDevice.Id)
+        {
+            LastLoginAt = null
+        };
+        unOnboardedDevice.User = unOnboardedDeviceUser;
+
+        return unOnboardedDevice;
+    }
+
+    private static Device CreateOnboardedDevice(Identity identity)
+    {
+        var onboardedDevice = new Device(identity);
+        var onboardedDeviceUser = new ApplicationUser(identity, onboardedDevice.Id)
+        {
+            LastLoginAt = SystemTime.UtcNow
+        };
+        onboardedDevice.User = onboardedDeviceUser;
+
+        return onboardedDevice;
     }
 
     private static Handler CreateHandler(IIdentitiesRepository mockIdentitiesRepository, IUserContext fakeUserContext)
