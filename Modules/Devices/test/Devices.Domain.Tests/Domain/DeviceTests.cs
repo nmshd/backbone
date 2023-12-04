4e325921737793312daf891bbdc99536fd2b91f8
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-12-04 16:50:42 +0100

    test: add device domain tests

---
+++ Modules/Devices/test/Devices.Domain.Tests/Domain/DeviceTests.cs
@@ -0,0 +1,43 @@
+﻿using Backbone.BuildingBlocks.Domain;
+using Backbone.DevelopmentKit.Identity.ValueObjects;
+using Backbone.Modules.Devices.Domain.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace Backbone.Modules.Devices.Domain.Tests.Domain;
+public class DeviceTests
+{
+    [Fact]
+    public void Device_can_be_deleted()
+    {
+        // Arrange
+        var identity = UnitTestTools.Data.TestDataGenerator.CreateIdentity();
+        var device = new Device(identity);
+
+        device.User = new ApplicationUser(identity, device.Id);
+
+        // Act
+        device.MarkAsDeleted(DeviceId.New());
+
+        // Assert
+        device.DeletedAt.Should().NotBeNull();
+        device.DeletedByDevice.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void Test_deleting_an_onboarded_device_which_throws_DomainException()
+    {
+        // Arrange
+        var identity = UnitTestTools.Data.TestDataGenerator.CreateIdentity();
+        var device = new Device(identity);
+
+        device.User = new ApplicationUser(identity, device.Id);
+        device.User.LoginOccurred();
+
+        // Act
+        var action = () => device.MarkAsDeleted(device.Id);
+
+        // Assert
+        action.Should().Throw<DomainException>();
+    }
+}
