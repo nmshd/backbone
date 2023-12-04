356e390e61fe5e617e0cc3b7179545246961a2e8
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-12-04 14:30:11 +0000

    test: IdentityDeleter calls correct command

---
+++ Modules/Relationships/test/Relationships.Application.Tests/Tests/Identities/IdentityDeleterTests.cs
@@ -0,0 +1,24 @@
+﻿using Backbone.Modules.Relationships.Application.Identities;
+using Backbone.Modules.Relationships.Application.Identities.Commands.DeleteRelationshipCommand;
+using FakeItEasy;
+using MediatR;
+using Xunit;
+
+namespace Backbone.Modules.Relationships.Application.Tests.Tests.Identities;
+public class IdentityDeleterTests
+{
+    [Fact]
+    public async Task Deleter_calls_correct_command()
+    {
+        // Arrange
+        var identityAddress = TestDataGenerator.CreateRandomAddress();
+        var mediator = A.Fake<IMediator>();
+        var deleter = new IdentityDeleter(mediator);
+
+        // Act
+        await deleter.Delete(identityAddress);
+
+        // Assert
+        A.CallTo(() => mediator.Send(A<DeleteRelationshipsCommand>._, A<CancellationToken>._)).MustHaveHappenedOnceExactly();
+    }
+}
