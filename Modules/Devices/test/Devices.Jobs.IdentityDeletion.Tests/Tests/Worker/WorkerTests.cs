debe6e7e178f6f5b44bc71b3e62a3e1439bd46ee
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-12-04 10:44:38 +0000

    test: deleters are called

--- Modules/Devices/test/Devices.Jobs.IdentityDeletion.Tests/Tests/Worker/WorkerTests.cs
+++ Modules/Devices/test/Devices.Jobs.IdentityDeletion.Tests/Tests/Worker/WorkerTests.cs
@@ -1,7 +1,10 @@
 ﻿using Backbone.BuildingBlocks.Application.Identities;
+using Backbone.DevelopmentKit.Identity.ValueObjects;
 using Backbone.Modules.Devices.Application.Identities.Commands.UpdateDeletionProcesses;
+using Backbone.UnitTestTools.Data;
 using FakeItEasy;
 using MediatR;
+using Microsoft.AspNetCore.Components.Forms;
 using Xunit;
 using static Backbone.Modules.Devices.Jobs.IdentityDeletion.Worker;
 
@@ -9,16 +12,13 @@ namespace Backbone.Modules.Devices.Jobs.IdentityDeletion.Tests.Tests.Worker;
 public class WorkerTests
 {
     [Fact]
-    public async Task Worker_Call_Command_To_Get_Identities()
+    public async Task Worker_Calls_Command_To_Get_Identities()
     {
         // Arrange
         var mediator = A.Fake<IMediator>();
-        var commandResponse = new UpdateDeletionProcessesResponse
-        {
-            IdentityAddresses = new List<string>()
-        };
 
-        A.CallTo(() => mediator.Send(A<UpdateDeletionProcessesCommand>._, A<CancellationToken>._)).Returns(commandResponse);
+        RegisterCommand(mediator);
+
         var identityDeleters = new List<IIdentityDeleter>();
 
         // Act
@@ -26,5 +26,34 @@ public class WorkerTests
 
         // Assert
         A.CallTo(() => mediator.Send(A<UpdateDeletionProcessesCommand>._, A<CancellationToken>._)).MustHaveHappenedOnceExactly();
+    }
+
+    [Fact]
+    public async Task Worker_Calls_Deletors_For_Each_Identity()
+    {
+        // Arrange
+        var mediator = A.Fake<IMediator>();
+        var identityAddress1 = TestDataGenerator.CreateRandomIdentityAddress();
+        var identityAddress2 = TestDataGenerator.CreateRandomIdentityAddress();
+        RegisterCommand(mediator, identityAddress1, identityAddress2);
+        var identityDeleter = A.Dummy<IIdentityDeleter>();
+
+        var identityDeleters = new List<IIdentityDeleter>([identityDeleter]);
+
+        // Act
+        await StartProcessing(mediator, identityDeleters, CancellationToken.None);
+
+        // Assert
+        A.CallTo(() => identityDeleter.Delete(A<IdentityAddress>._)).MustHaveHappened(2, Times.Exactly);
+    }
+
+    private void RegisterCommand(IMediator mediator, params string[] identityAddresses)
+    {
+        var commandResponse = new UpdateDeletionProcessesResponse
+        {
+            IdentityAddresses = [.. identityAddresses]
+        };
+
+        A.CallTo(() => mediator.Send(A<UpdateDeletionProcessesCommand>._, A<CancellationToken>._)).Returns(commandResponse);
     }
 }
