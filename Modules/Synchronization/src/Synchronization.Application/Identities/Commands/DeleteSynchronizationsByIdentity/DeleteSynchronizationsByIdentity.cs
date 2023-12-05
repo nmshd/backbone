5be63a9df6d8a8e009d718f64bfb95248b8ff8a6
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-12-05 15:36:06 +0000

    refactor: fix namespaces on Synchronizations

---
+++ Modules/Synchronization/src/Synchronization.Application/Identities/Commands/DeleteSynchronizationsByIdentity/DeleteSynchronizationsByIdentity.cs
@@ -0,0 +1,7 @@
+﻿using Backbone.BuildingBlocks.Application.Identities;
+using Backbone.DevelopmentKit.Identity.ValueObjects;
+
+namespace Backbone.Modules.Synchronization.Application.Identities.Commands.DeleteSynchronizationsByIdentity;
+public class DeleteSynchronizationsByIdentity(IdentityAddress identityAddress) : RequestWithIdentityAddress(identityAddress)
+{
+}
--- Modules/Synchronization/src/Synchronization.Application/Identities/Commands/DeleteSynchronizationsByIdentity/Handler.cs
+++ Modules/Synchronization/src/Synchronization.Application/Identities/Commands/DeleteSynchronizationsByIdentity/Handler.cs
@@ -4,17 +4,12 @@ using Backbone.Modules.Synchronization.Domain.Entities.Sync;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
-namespace Backbone.Modules.Synchronization.Application.Identities.Commands.DeleteIdentity;
-public class Handler : IRequestHandler<DeleteIdentityCommand>
+namespace Backbone.Modules.Synchronization.Application.Identities.Commands.DeleteSynchronizationsByIdentity;
+public class Handler(ISynchronizationDbContext dbContext) : IRequestHandler<DeleteSynchronizationsByIdentity>
 {
-    private readonly ISynchronizationDbContext _dbContext;
+    private readonly ISynchronizationDbContext _dbContext = dbContext;
 
-    public Handler(ISynchronizationDbContext dbContext)
-    {
-        _dbContext = dbContext;
-    }
-
-    public async Task Handle(DeleteIdentityCommand request, CancellationToken cancellationToken)
+    public async Task Handle(DeleteSynchronizationsByIdentity request, CancellationToken cancellationToken)
     {
         await _dbContext.Set<Datawallet>().Where(d => d.Owner == request.IdentityAddress).ExecuteDeleteAsync(cancellationToken);
         await _dbContext.Set<SyncRun>().Where(d => d.CreatedBy == request.IdentityAddress).ExecuteDeleteAsync(cancellationToken);
