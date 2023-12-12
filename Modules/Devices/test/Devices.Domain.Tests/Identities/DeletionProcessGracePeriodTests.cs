8b23b5b0edd4ab15a2bdd545426184919f5bc1cf
Joaquim Rocha <118175875+JLSRKonk@users.noreply.github.com>
2023-12-12 15:02:23 +0000

    test: simplify DeletionProcessGracePeriodTests domain tests

--- Modules/Devices/test/Devices.Domain.Tests/Identities/DeletionProcessGracePeriodTests.cs
+++ Modules/Devices/test/Devices.Domain.Tests/Identities/DeletionProcessGracePeriodTests.cs
@@ -10,7 +10,7 @@ namespace Backbone.Modules.Devices.Domain.Tests.Identities;
 public class DeletionProcessGracePeriodTests : IDisposable
 {
     [Fact]
-    public void DeletionGracePeriodReminderSent_should_update_GracePeriodReminderSentAt()
+    public void DeletionGracePeriodReminder1Sent_should_update_GracePeriodReminder1SentAt()
     {
         // Arrange
         var currentDateTime = DateTime.Parse("2000-01-01");
@@ -23,30 +23,62 @@ public class DeletionProcessGracePeriodTests : IDisposable
 
         // Act
         activeIdentity.DeletionGracePeriodReminder1Sent();
+
+        // Assert
+        AssertAuditLogEntryWasCreated(deletionProcess);
+        deletionProcess.GracePeriodReminder1SentAt.Should().NotBeNull().And.Be(currentDateTime);
+    }
+
+    [Fact]
+    public void DeletionGracePeriodReminder2Sent_should_update_GracePeriodReminder2SentAt()
+    {
+        // Arrange
+        var currentDateTime = DateTime.Parse("2000-01-01");
+        SystemTime.Set(currentDateTime);
+        var activeIdentity = CreateIdentity();
+
+        Hasher.SetHasher(new DummyHasher(new byte[] { 1, 2, 3 }));
+
+        var deletionProcess = activeIdentity.StartDeletionProcessAsOwner(new Device(activeIdentity).Id);
+
+        // Act
         activeIdentity.DeletionGracePeriodReminder2Sent();
+
+        // Assert
+        AssertAuditLogEntryWasCreated(deletionProcess);
+        deletionProcess.GracePeriodReminder1SentAt.Should().NotBeNull().And.Be(currentDateTime);
+    }
+
+    [Fact]
+    public void DeletionGracePeriodReminder3Sent_should_update_GracePeriodReminder3SentAt()
+    {
+        // Arrange
+        var currentDateTime = DateTime.Parse("2000-01-01");
+        SystemTime.Set(currentDateTime);
+        var activeIdentity = CreateIdentity();
+
+        Hasher.SetHasher(new DummyHasher(new byte[] { 1, 2, 3 }));
+
+        var deletionProcess = activeIdentity.StartDeletionProcessAsOwner(new Device(activeIdentity).Id);
+
+        // Act
         activeIdentity.DeletionGracePeriodReminder3Sent();
 
         // Assert
-        activeIdentity.DeletionProcesses.Should().HaveCount(1);
-        AssertAuditLogEntriesWereCreated(deletionProcess);
+        AssertAuditLogEntryWasCreated(deletionProcess);
         deletionProcess.GracePeriodReminder1SentAt.Should().NotBeNull().And.Be(currentDateTime);
-        deletionProcess.GracePeriodReminder2SentAt.Should().NotBeNull().And.Be(currentDateTime);
-        deletionProcess.GracePeriodReminder3SentAt.Should().NotBeNull().And.Be(currentDateTime);
     }
 
-    private static void AssertAuditLogEntriesWereCreated(IdentityDeletionProcess deletionProcess)
+    private static void AssertAuditLogEntryWasCreated(IdentityDeletionProcess deletionProcess)
     {
-        deletionProcess.AuditLog.Should().HaveCount(4);
+        deletionProcess.AuditLog.Should().HaveCount(2);
 
-        for (var i = 1; i <= 3; i++)
-        {
-            var auditLogEntry = deletionProcess.AuditLog[i];
-            auditLogEntry.ProcessId.Should().Be(deletionProcess.Id);
-            auditLogEntry.CreatedAt.Should().Be(SystemTime.UtcNow);
-            auditLogEntry.IdentityAddressHash.Should().BeEquivalentTo(new byte[] { 1, 2, 3 });
-            auditLogEntry.OldStatus.Should().Be(DeletionProcessStatus.Approved);
-            auditLogEntry.NewStatus.Should().Be(DeletionProcessStatus.Approved);
-        }
+        var auditLogEntry = deletionProcess.AuditLog[1];
+        auditLogEntry.ProcessId.Should().Be(deletionProcess.Id);
+        auditLogEntry.CreatedAt.Should().Be(SystemTime.UtcNow);
+        auditLogEntry.IdentityAddressHash.Should().BeEquivalentTo(new byte[] { 1, 2, 3 });
+        auditLogEntry.OldStatus.Should().Be(DeletionProcessStatus.Approved);
+        auditLogEntry.NewStatus.Should().Be(DeletionProcessStatus.Approved);
     }
 
     private static Identity CreateIdentity()
