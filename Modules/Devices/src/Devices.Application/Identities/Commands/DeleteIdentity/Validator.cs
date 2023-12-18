b2fd0cf3c60fe6085810084cd0f271c65e55b539
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-12-18 16:58:35 +0000

    feat: add missing call to DeleteIdentity

---
+++ Modules/Devices/src/Devices.Application/Identities/Commands/DeleteIdentity/Validator.cs
@@ -0,0 +1,9 @@
+﻿using Backbone.DevelopmentKit.Identity.ValueObjects;
+using Backbone.Modules.Devices.Application.Identities.Commands.CreateAuditLog;
+using FluentValidation;
+
+namespace Backbone.Modules.Devices.Application.Identities.Commands.DeleteIdentity;
+public class Validator : AbstractValidator<DeleteIdentityCommand>
+{
+    public Validator() => RuleFor(x => x.IdentityAddress).Must(x => IdentityAddress.IsValid(x));
+}
--- Modules/Devices/src/Devices.Application/Identities/IdentityDeleter.cs
+++ Modules/Devices/src/Devices.Application/Identities/IdentityDeleter.cs
@@ -1,5 +1,6 @@
 ﻿using Backbone.BuildingBlocks.Application.Identities;
 using Backbone.DevelopmentKit.Identity.ValueObjects;
+using Backbone.Modules.Devices.Application.Identities.Commands.CreateAuditLog;
 using Backbone.Modules.Devices.Application.PushNotifications.Commands.DeleteRegistrationsByIdentityAddress;
 using MediatR;
 
@@ -11,5 +12,6 @@ public class IdentityDeleter(IMediator mediator) : IIdentityDeleter
     public async Task Delete(IdentityAddress identityAddress)
     {
         await _mediator.Send(new DeleteRegistrationsByIdentityAddressCommand(identityAddress));
+        await _mediator.Send(new DeleteIdentityCommand(identityAddress));
     }
 }
