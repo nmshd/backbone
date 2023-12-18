ccb46dffc74fa378dd37089909c565cba050d6ea
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-12-18 11:19:27 +0000

    fix: failing test

--- Modules/Devices/src/Devices.Application/Identities/Commands/UpdateDeletionProcesses/UpdateDeletionProcessesResponse.cs
+++ Modules/Devices/src/Devices.Application/Identities/Commands/UpdateDeletionProcesses/UpdateDeletionProcessesResponse.cs
@@ -1,5 +1,5 @@
 ﻿namespace Backbone.Modules.Devices.Application.Identities.Commands.UpdateDeletionProcesses;
 public class UpdateDeletionProcessesResponse
 {
-    public List<string> IdentityAddresses { get; set; }
+    public List<string> IdentityAddresses { get; set; } = [];
 }
