ed8e7c5cacdd8e7d2cc73300847af33f23fb3f55
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2023-12-19 09:42:35 +0100

    chore: update code so that all items are migrated and set page size to 500

--- ConsumerApi/SynchronizationDbContextSeeder.cs
+++ ConsumerApi/SynchronizationDbContextSeeder.cs
@@ -14,6 +14,8 @@ namespace Backbone.ConsumerApi;
 
 public class SynchronizationDbContextSeeder : IDbSeeder<SynchronizationDbContext>
 {
+    private const int PAGE_SIZE = 500;
+
     private readonly IBlobStorage? _blobStorage;
     private readonly string? _blobRootFolder;
 
@@ -34,18 +36,26 @@ public class SynchronizationDbContextSeeder : IDbSeeder<SynchronizationDbContext
         if (_blobRootFolder == null)
             return;
 
-        var paginationResult = await context.DatawalletModifications
-            .Where(t => t.EncryptedPayload == null)
-            .OrderAndPaginate(m => m.Id, new PaginationFilter(1, 20), CancellationToken.None);
+        var morePages = true;
 
-        foreach (var datawalletModification in paginationResult.ItemsOnPage)
-            await FillEncryptedContentForModification(context, datawalletModification);
+        while (morePages)
+        {
+            var paginationResult = await context.DatawalletModifications
+                .Where(t => t.EncryptedPayload == null)
+                .OrderAndPaginate(m => m.Id, new PaginationFilter(1, PAGE_SIZE), CancellationToken.None);
+
+            foreach (var datawalletModification in paginationResult.ItemsOnPage)
+                await FillEncryptedContentForModification(context, datawalletModification);
+
+            morePages = paginationResult.ItemsOnPage.Any();
+        }
     }
 
     private async Task FillEncryptedContentForModification(SynchronizationDbContext context, DatawalletModification datawalletModification)
     {
         if (datawalletModification.BlobReference.Trim().IsEmpty())
         {
+            // fetching the blob storage content for modifications whose encrypted content is saved individually and which do not have blob reference value
             try
             {
                 var blobContent = await _blobStorage!.FindAsync(_blobRootFolder!, datawalletModification.Id);
@@ -60,12 +70,13 @@ public class SynchronizationDbContextSeeder : IDbSeeder<SynchronizationDbContext
         }
         else
         {
+            // fetching the blob storage content for modifications whose encrypted content is saved in bulk and which have blob reference value
             try
             {
-                var blob = await _blobStorage!.FindAsync(_blobRootFolder, datawalletModification.BlobReference);
+                var blob = await _blobStorage!.FindAsync(_blobRootFolder!, datawalletModification.BlobReference);
                 var payload = JsonSerializer.Deserialize<Dictionary<long, byte[]>>(blob);
 
-                if (payload.TryGetValue(datawalletModification.Index, out var encryptedPayload))
+                if (payload != null && payload.TryGetValue(datawalletModification.Index, out var encryptedPayload))
                 {
                     datawalletModification.LoadEncryptedPayload(encryptedPayload);
                     context.DatawalletModifications.Update(datawalletModification);
