55bf75e95c8e634a7a8584dc9650dcb695d6a4c5
Timo Notheisen <timo.notheisen@js-soft.com>
2023-12-19 15:06:22 +0100

    fix: add platform and remove --load

--- .ci/aui/buildContainerImage.js
+++ .ci/aui/buildContainerImage.js
@@ -6,6 +6,6 @@ import { getRequiredEnvVar } from "../lib.js";
 const tag = getRequiredEnvVar("TAG");
 
 const platforms = process.env.PLATFORMS ?? "linux/amd64,linux/arm64";
-const push = process.env.PUSH === "1" ? "--push --provenance=true --sbom=true --platform ${platforms}" : "--load";
+const push = process.env.PUSH === "1" ? "--push --provenance=true --sbom=true" : "";
 
-await $`docker buildx build --file ./AdminUi/src/AdminUi/Dockerfile --tag ghcr.io/nmshd/backbone-admin-ui:${tag} ${push} .`;
+await $`docker buildx build --file ./AdminUi/src/AdminUi/Dockerfile --tag ghcr.io/nmshd/backbone-admin-ui:${tag} --platform ${platforms} ${push} .`;
--- .ci/fsc/buildContainerImage.js
+++ .ci/fsc/buildContainerImage.js
@@ -6,6 +6,6 @@ import { getRequiredEnvVar } from "../lib.js";
 const tag = getRequiredEnvVar("TAG");
 
 const platforms = process.env.PLATFORMS ?? "linux/amd64,linux/arm64";
-const push = process.env.PUSH === "1" ? `--push --provenance=true --sbom=true --platform ${platforms}` : "--load";
+const push = process.env.PUSH === "1" ? "--push --provenance=true --sbom=true" : "";
 
-await $`docker buildx build --file ./Modules/Files/src/Files.Jobs.SanityCheck/Dockerfile --tag ghcr.io/nmshd/backbone-files-sanity-check:${tag} ${push} .`;
+await $`docker buildx build --file ./Modules/Files/src/Files.Jobs.SanityCheck/Dockerfile --tag ghcr.io/nmshd/backbone-files-sanity-check:${tag} --platform ${platforms} ${push} .`;
