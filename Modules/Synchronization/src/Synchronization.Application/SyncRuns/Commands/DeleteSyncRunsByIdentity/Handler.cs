8a1f3d7875688b7f151d0e59fd86b5fd2e26209a
Daniel Silva <daniel.p.silva@konkconsulting.com>
2023-12-20 09:53:44 +0000

    fix: not deleting ExternalEvents

--- Modules/Synchronization/src/Synchronization.Application/SyncRuns/Commands/DeleteSyncRunsByIdentity/Handler.cs
+++ Modules/Synchronization/src/Synchronization.Application/SyncRuns/Commands/DeleteSyncRunsByIdentity/Handler.cs
@@ -8,6 +8,7 @@ public class Handler(ISynchronizationDbContext dbContext) : IRequestHandler<Dele
 {
     public async Task Handle(DeleteSyncRunsByIdentityCommand request, CancellationToken cancellationToken)
     {
+        await dbContext.Set<ExternalEvent>().Where(d => d.Owner == request.IdentityAddress).ExecuteDeleteAsync(cancellationToken);
         await dbContext.Set<SyncRun>().Where(d => d.CreatedBy == request.IdentityAddress).ExecuteDeleteAsync(cancellationToken);
     }
 }
