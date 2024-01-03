bfdc2d3fa20e171d7fafae0bc86ff2bea43f2538
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-01-03 21:46:00 +0100

    chore: remove unnecessary QuotasForIdentityDTO class and handle all mapping to DTO in the extension method chore: add MetricKey to SingleQuotaDTO to facilitate grouping into QuotaGroupDTO

--- Modules/Quotas/src/Quotas.Application/Identities/Queries/ListQuotasForIdentity/ListQuotasForIdentityResponse.cs
+++ Modules/Quotas/src/Quotas.Application/Identities/Queries/ListQuotasForIdentity/ListQuotasForIdentityResponse.cs
@@ -1,5 +1,4 @@
 ﻿using Backbone.BuildingBlocks.Application.CQRS.BaseClasses;
-using Backbone.Modules.Quotas.Application.DTOs;
 using Backbone.Modules.Quotas.Domain.Aggregates.Identities;
 
 namespace Backbone.Modules.Quotas.Application.Identities.Queries.ListQuotasForIdentity;
@@ -9,49 +8,10 @@ public class ListQuotasForIdentityResponse : EnumerableResponseBase<QuotaGroupDT
     public ListQuotasForIdentityResponse(IEnumerable<QuotaGroupDTO> items) : base(items) { }
 }
 
-public class QuotasForIdentityDTO
-{
-    public List<QuotaGroupDTO> Quotas { get; }
-
-    public QuotasForIdentityDTO(List<QuotaDTO> quotas)
-    {
-        Quotas = new List<QuotaGroupDTO>();
-
-        foreach (var quota in quotas)
-        {
-            AddQuota(quota);
-        }
-    }
-
-    private void AddQuota(QuotaDTO quota)
-    {
-        var uniqueMetricKeysInQuotas = Quotas.Select(q => q.MetricKey).Distinct().ToList();
-
-        if (!uniqueMetricKeysInQuotas.Contains(quota.Metric.Key))
-        {
-            Quotas.Add(new QuotaGroupDTO
-            {
-                MetricKey = quota.Metric.Key,
-                Quotas = new List<SingleQuotaDTO>()
-            });
-        }
-
-        var singleQuotaDto = new SingleQuotaDTO
-        {
-            Source = quota.Source,
-            Max = quota.Max,
-            Usage = quota.Usage,
-            Period = quota.Period
-        };
-
-        foreach (var quotaGroupDto in Quotas.Where(quotaGroupDto => quotaGroupDto.MetricKey == quota.Metric.Key))
-            quotaGroupDto.Quotas.Add(singleQuotaDto);
-    }
-}
-
 public class SingleQuotaDTO
 {
     public QuotaSource Source { get; set; }
+    public string MetricKey { get; set; }
     public int Max { get; set; }
     public uint Usage { get; set; }
     public string Period { get; set; }
