9758decd24e1dd4f10ab444796bcc806ee2bdd00
Daniel Almeida <115644988+daniel-almeida-konkconsulting@users.noreply.github.com>
2024-01-11 10:47:57 +0000

    chore: update license-check

--- .ci/runAdminUiChecks.sh
+++ .ci/runAdminUiChecks.sh
@@ -6,6 +6,6 @@ cd AdminUi/src/AdminUi/ClientApp
 npm ci
 npx eslint --ext ts ./src
 npx prettier --check .
-npx license-check --ignorePackages adminui@0.0.0 path-scurry@1.10.1 jackspeak@2.3.6
+npx license-check --ignorePackages adminui@0.0.0
 npx better-npm-audit audit --exclude 1094889
 cd $INITIAL_DIR
