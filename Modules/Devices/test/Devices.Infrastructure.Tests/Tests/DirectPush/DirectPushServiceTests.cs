0c4f31bf4fe8485bca06c8b1e2acf55bdf4f6540
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-01-17 12:29:39 +0100

    refactor: suppress nullable warnings for mocked repository return values

--- Modules/Devices/test/Devices.Infrastructure.Tests/Tests/DirectPush/DirectPushServiceTests.cs
+++ Modules/Devices/test/Devices.Infrastructure.Tests/Tests/DirectPush/DirectPushServiceTests.cs
@@ -23,7 +23,7 @@ public class DirectPushServiceTests
         var mockPnsRegistrationsRepository = A.Fake<IPnsRegistrationsRepository>();
 
         A.CallTo(() => mockPnsRegistrationsRepository.FindByDeviceId(deviceId, A<CancellationToken>._, A<bool>._))
-           .Returns((PnsRegistration)null).Once();
+           .Returns((PnsRegistration)null!).Once();
 
         var directPushService = CreateDirectPushService(mockPnsRegistrationsRepository);
 
@@ -98,7 +98,7 @@ public class DirectPushServiceTests
         var mockPnsRegistrationsRepository = A.Fake<IPnsRegistrationsRepository>();
 
         A.CallTo(() => mockPnsRegistrationsRepository.FindByDeviceId(deviceId, A<CancellationToken>._, A<bool>._))
-           .Returns((PnsRegistration)null);
+           .Returns((PnsRegistration)null!);
 
         var directPushService = CreateDirectPushService(mockPnsRegistrationsRepository);
 
