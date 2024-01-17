d1a044dde313ab29dfc1bf0f58a800214124b10e
Daniel Silva <daniel.p.silva@konkconsulting.com>
2024-01-17 14:39:40 +0000

    refactor: rename lambda param

--- Modules/Challenges/test/WebApplication1/Tests/Identities/IdentityDeleterTests.cs
+++ Modules/Challenges/test/WebApplication1/Tests/Identities/IdentityDeleterTests.cs
@@ -20,6 +20,6 @@ public class IdentityDeleterTests
         await deleter.Delete(identityAddress);
 
         // Assert
-        A.CallTo(() => mediator.Send(A<DeleteChallengesOfIdentityCommand>.That.Matches(r => r.IdentityAddress == identityAddress), A<CancellationToken>._)).MustHaveHappened();
+        A.CallTo(() => mediator.Send(A<DeleteChallengesOfIdentityCommand>.That.Matches(command => command.IdentityAddress == identityAddress), A<CancellationToken>._)).MustHaveHappened();
     }
 }
