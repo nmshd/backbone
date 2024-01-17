0153c94193fb5d9bc6916fdf359de03fd54e3a30
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-01-17 15:36:22 +0100

    refactor: initialize properties from ctors

--- Modules/Devices/src/Devices.Infrastructure/Persistence/Database/DevicesDbContext.cs
+++ Modules/Devices/src/Devices.Infrastructure/Persistence/Database/DevicesDbContext.cs
@@ -29,11 +29,22 @@ public class DevicesDbContext : IdentityDbContext<ApplicationUser>, IDevicesDbCo
     public DevicesDbContext(DbContextOptions<DevicesDbContext> options)
         : base(options)
     {
+        Identities = null!;
+        Devices = null!;
+        Challenges = null!;
+        Tiers = null!;
+        PnsRegistrations = null!;
     }
 
     public DevicesDbContext(DbContextOptions<DevicesDbContext> options, IServiceProvider serviceProvider)
         : base(options)
     {
+        Identities = null!;
+        Devices = null!;
+        Challenges = null!;
+        Tiers = null!;
+        PnsRegistrations = null!;
+
         _serviceProvider = serviceProvider;
     }
 
