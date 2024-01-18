997e48365e6acfa6a7792b9a913c15b6107e9cf2
Vladimir Vuckovic <vidrak97@gmail.com>
2024-01-18 12:28:39 +0100

    feat: implement BreadcrumbService and add appropriate tests for BreadcrumbService

---
+++ AdminUi/src/AdminUi/ClientApp/src/app/services/breadcrumb-service/breadcrumb.service.ts
@@ -0,0 +1,96 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRoute, NavigationEnd, Router } from "@angular/router";
+import { filter } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root"
+})
+export class BreadcrumbService {
+  private breadcrumbHistory: Breadcrumb[] = [];
+  private readonly maxHistorySize = 10;
+
+  public constructor(private readonly router: Router, private readonly activatedRoute: ActivatedRoute) {
+    const storedHistory = localStorage.getItem("breadcrumb-history");
+    if (storedHistory) {
+      this.breadcrumbHistory = JSON.parse(storedHistory);
+    }
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.updateBreadcrumbHistory();
+        localStorage.setItem("breadcrumb-history", JSON.stringify(this.breadcrumbHistory));
+      });
+  }
+
+  public getBreadcrumbHistory(): Breadcrumb[] {
+    return [...this.breadcrumbHistory];
+  }
+
+  public clearBreadcrumbHistoryAfterIndex(index: number): void {
+    if (index >= 0 && index < this.breadcrumbHistory.length - 1) {
+      this.breadcrumbHistory.splice(index + 1);
+    }
+  }
+
+  private updateBreadcrumbHistory(): void {
+    const breadcrumbs: Breadcrumb[] = [];
+    this.generateBreadcrumbs(this.activatedRoute.root, "", breadcrumbs);
+
+    const breadcrumbTrail = [...breadcrumbs];
+
+    if (this.shouldClearBreadcrumbHistory(breadcrumbTrail)) {
+      this.breadcrumbHistory = [];
+      return;
+    }
+
+    if (this.shouldPushBreadcrumbTrail(breadcrumbTrail)) {
+      this.breadcrumbHistory.push(...breadcrumbTrail);
+    }
+
+    if (this.breadcrumbHistory.length > this.maxHistorySize) {
+      this.trimBreadcrumbHistory();
+    }
+  }
+
+  private shouldClearBreadcrumbHistory(trail: Breadcrumb[]): boolean {
+    return trail.some(breadcrumb => breadcrumb.url.includes("login"));
+  }
+
+  private shouldPushBreadcrumbTrail(breadcrumbTrail: Breadcrumb[]): boolean {
+    return this.breadcrumbHistory.length === 0 ||
+      this.breadcrumbHistory[this.breadcrumbHistory.length - 1].url !== breadcrumbTrail[breadcrumbTrail.length - 1].url;
+  }
+
+  private trimBreadcrumbHistory(): void {
+    const currentSize = this.breadcrumbHistory.length;
+    if (currentSize > this.maxHistorySize) {
+      this.breadcrumbHistory = this.breadcrumbHistory.slice(currentSize - this.maxHistorySize);
+    }
+  }
+
+  private generateBreadcrumbs(route: ActivatedRoute | null, url = "", breadcrumbs: Breadcrumb[] = []): void {
+    if (!route?.children) {
+      return;
+    }
+
+    const children: ActivatedRoute[] = route.children;
+
+    for (const child of children) {
+      const routeURL: string = child.snapshot.url.map((segment) => segment.path).join("/");
+      
+      const breadcrumbLabel: string = child.snapshot.data.breadcrumb;
+
+      if (routeURL !== "") {
+        url += `/${routeURL}`;
+        breadcrumbs.push({ label: breadcrumbLabel, url: url });
+      }
+
+      this.generateBreadcrumbs(child, url, breadcrumbs);
+    }
+  }
+}
+
+export interface Breadcrumb {
+  label: string;
+  url: string;
+}
