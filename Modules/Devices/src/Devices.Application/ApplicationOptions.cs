e6d96a7a4ae9d8766ca2a61f4aa193b6729375a4
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-01-19 15:56:13 +0100

    fix: wip

--- Modules/Devices/src/Devices.Application/ApplicationOptions.cs
+++ Modules/Devices/src/Devices.Application/ApplicationOptions.cs
@@ -7,7 +7,7 @@ public class ApplicationOptions
     [Required]
     [MinLength(3)]
     [MaxLength(3)]
-    public string AddressPrefix { get; set; } = null!;
+    public string AddressPrefix { get; set; }
 
     [Required]
     public PaginationOptions Pagination { get; set; } = new();
