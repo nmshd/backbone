d41275e6046e6f15d959115010f3393081bbdf48
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-19 15:31:33 +0100

    refactor: fix null reference, return 1 for camparing object to null (msdn standard)

--- Modules/Quotas/src/Quotas.Domain/Aggregates/Identities/ExhaustionDate.cs
+++ Modules/Quotas/src/Quotas.Domain/Aggregates/Identities/ExhaustionDate.cs
@@ -33,8 +33,8 @@ public record ExhaustionDate(DateTime Value) : IComparable<ExhaustionDate>
         return left.Value > right.Value;
     }
 
-    public int CompareTo(ExhaustionDate other)
+    public int CompareTo(ExhaustionDate? other)
     {
-        return Value.CompareTo(other.Value);
+        return other == null ? 1 : Value.CompareTo(other.Value);
     }
 }
