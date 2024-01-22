7859562fbba7c2365d7fcbe83d463514724d4079
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-01-22 10:18:57 +0100

    Revert "refactor: check expression for null"
    This reverts commit 2851b0ee74f9429dbd4d0f22c54ef67d5f7bd93d.

--- Modules/Devices/src/Devices.Infrastructure/PushNotifications/IServiceCollectionExtensions.cs
+++ Modules/Devices/src/Devices.Infrastructure/PushNotifications/IServiceCollectionExtensions.cs
@@ -19,8 +19,7 @@ public static class IServiceCollectionExtensions
                 services.AddDummyPushNotifications();
                 break;
             case PROVIDER_DIRECT:
-                if (options.DirectPnsCommunication != null) 
-                    services.AddDirectPushNotifications(options.DirectPnsCommunication);
+                services.AddDirectPushNotifications(options.DirectPnsCommunication);
                 break;
             default:
                 throw new Exception($"Push Notification Provider {options.Provider} does not exist.");
