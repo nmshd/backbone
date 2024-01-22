c886ac45a03012597db1aa3703df4a26b02e2f4a
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-01-22 16:03:10 +0100

    refactor: declare properties as nullable

--- Modules/Messages/src/Messages.Application/Messages/DTOs/RecipientInformationDTO.cs
+++ Modules/Messages/src/Messages.Application/Messages/DTOs/RecipientInformationDTO.cs
@@ -6,8 +6,8 @@ namespace Backbone.Modules.Messages.Application.Messages.DTOs;
 
 public class RecipientInformationDTO : IMapTo<RecipientInformation>
 {
-    public IdentityAddress Address { get; set; }
-    public byte[] EncryptedKey { get; set; }
+    public IdentityAddress? Address { get; set; }
+    public byte[]? EncryptedKey { get; set; }
     public DateTime? ReceivedAt { get; set; }
-    public DeviceId ReceivedByDevice { get; set; }
+    public DeviceId? ReceivedByDevice { get; set; }
 }
