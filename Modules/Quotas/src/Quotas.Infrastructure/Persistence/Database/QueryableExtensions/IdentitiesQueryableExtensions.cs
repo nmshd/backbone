2aec0f73bfc754d31ea7a4a255672dd18b76a21d
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-22 15:01:45 +0100

    refactor: supress issue ( null reference throws NotFoundException as shown in related test )

--- Modules/Quotas/src/Quotas.Infrastructure/Persistence/Database/QueryableExtensions/IdentitiesQueryableExtensions.cs
+++ Modules/Quotas/src/Quotas.Infrastructure/Persistence/Database/QueryableExtensions/IdentitiesQueryableExtensions.cs
@@ -12,6 +12,6 @@ public static class IdentitiesQueryableExtensions
 
     public static async Task<Identity> FirstWithAddress(this IQueryable<Identity> query, string address, CancellationToken cancellationToken)
     {
-        return await query.Where(identity => identity.Address == address).FirstOrDefaultAsync(cancellationToken);
+        return (await query.Where(identity => identity.Address == address).FirstOrDefaultAsync(cancellationToken))!;
     }
 }
