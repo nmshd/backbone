ed6f19ddc96df178c6fa6db215d4db1d8f4826e8
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-01-22 17:22:29 +0100

    refactor: mark properties as nullable (insantiated manually once where values are set)

--- Modules/Devices/src/Devices.Application/Identities/Commands/UpdateIdentity/UpdateIdentityCommand.cs
+++ Modules/Devices/src/Devices.Application/Identities/Commands/UpdateIdentity/UpdateIdentityCommand.cs
@@ -3,6 +3,6 @@
 namespace Backbone.Modules.Devices.Application.Identities.Commands.UpdateIdentity;
 public class UpdateIdentityCommand : IRequest
 {
-    public string Address { get; set; }
-    public string TierId { get; set; }
+    public string? Address { get; set; }
+    public string? TierId { get; set; }
 }
