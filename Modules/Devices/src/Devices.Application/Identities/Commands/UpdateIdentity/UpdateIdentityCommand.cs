4ff9af6b7af90454e7179c7ef211e39914f85b9c
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-01-23 15:30:25 +0100

    refactor: make Properties required

--- Modules/Devices/src/Devices.Application/Identities/Commands/UpdateIdentity/UpdateIdentityCommand.cs
+++ Modules/Devices/src/Devices.Application/Identities/Commands/UpdateIdentity/UpdateIdentityCommand.cs
@@ -3,6 +3,6 @@
 namespace Backbone.Modules.Devices.Application.Identities.Commands.UpdateIdentity;
 public class UpdateIdentityCommand : IRequest
 {
-    public string? Address { get; set; }
-    public string? TierId { get; set; }
+    public required string Address { get; set; }
+    public required string TierId { get; set; }
 }
