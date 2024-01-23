69fc54b0313aff0f2a78e2051a1b23c086e8dd3f
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-23 10:30:09 +0100

    refactor: make prop type nullable as it has public setter and the class only has default ctor, FileMetadata

--- Modules/Quotas/src/Quotas.Domain/Aggregates/FileMetadata/FileMetadata.cs
+++ Modules/Quotas/src/Quotas.Domain/Aggregates/FileMetadata/FileMetadata.cs
@@ -1,8 +1,8 @@
 ﻿namespace Backbone.Modules.Quotas.Domain.Aggregates.FileMetadata;
 public class FileMetadata : ICreatedAt
 {
-    public string Id { get; set; }
+    public string? Id { get; set; }
     public DateTime CreatedAt { get; set; }
-    public string CreatedBy { get; set; }
+    public string? CreatedBy { get; set; }
     public long CipherSize { get; set; }
 }
