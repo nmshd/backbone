a48889057f2495b69fdaab78159e78a20bf887a3
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-01-23 11:24:33 +0100

    refactor: solve non-nullable property for MessageDTO

--- Modules/Messages/src/Messages.Application/Messages/DTOs/MessageDTO.cs
+++ Modules/Messages/src/Messages.Application/Messages/DTOs/MessageDTO.cs
@@ -7,21 +7,21 @@ namespace Backbone.Modules.Messages.Application.Messages.DTOs;
 
 public class MessageDTO : IMapTo<Message>
 {
-    public MessageId Id { get; set; }
+    public MessageId? Id { get; set; }
 
     public DateTime CreatedAt { get; set; }
-    public IdentityAddress CreatedBy { get; set; }
-    public DeviceId CreatedByDevice { get; set; }
+    public IdentityAddress? CreatedBy { get; set; }
+    public DeviceId? CreatedByDevice { get; set; }
 
     public DateTime? DoNotSendBefore { get; set; }
-    public byte[] Body { get; set; }
+    public byte[]? Body { get; set; }
 
-    public List<AttachmentDTO> Attachments { get; set; }
-    public List<RecipientInformationDTO> Recipients { get; set; }
+    public List<AttachmentDTO>? Attachments { get; set; }
+    public List<RecipientInformationDTO>? Recipients { get; set; }
 
     public void PrepareForActiveIdentity(IdentityAddress activeIdentity)
     {
-        if (CreatedBy != activeIdentity)
+        if (CreatedBy != activeIdentity && Recipients != null)
             Recipients.RemoveAll(r => r.Address != activeIdentity);
     }
 }
