af9e419c6cebf2e900da3df18f37fe408ed214ee
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-01-24 10:32:05 +0100

    refactor: make EncryptedKey required

--- Modules/Messages/src/Messages.Application/Messages/DTOs/RecipientInformationDTO.cs
+++ Modules/Messages/src/Messages.Application/Messages/DTOs/RecipientInformationDTO.cs
@@ -7,7 +7,7 @@ namespace Backbone.Modules.Messages.Application.Messages.DTOs;
 public class RecipientInformationDTO : IMapTo<RecipientInformation>
 {
     public required IdentityAddress Address { get; set; }
-    public byte[]? EncryptedKey { get; set; }
+    public required byte[] EncryptedKey { get; set; }
     public DateTime? ReceivedAt { get; set; }
     public DeviceId? ReceivedByDevice { get; set; }
 }
