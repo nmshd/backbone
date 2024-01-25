bad0008fdc0cd448a3d8049be4b101249e859aef
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-01-25 22:54:54 +0100

    chore: add line break

--- Modules/Devices/src/Devices.Infrastructure/PushNotifications/DirectPush/ApplePushNotificationService/ApnsMessageBuilder.cs
+++ Modules/Devices/src/Devices.Infrastructure/PushNotifications/DirectPush/ApplePushNotificationService/ApnsMessageBuilder.cs
@@ -2,6 +2,7 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using Backbone.Tooling.Extensions;
+
 // ReSharper disable UnusedAutoPropertyAccessor.Local
 
 namespace Backbone.Modules.Devices.Infrastructure.PushNotifications.DirectPush.ApplePushNotificationService;
