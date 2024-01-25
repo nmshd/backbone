d518b2f010d6c28084fd9ddd3ae29c4c49c749a2
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-01-25 12:43:53 +0100

    refactor: make fields required

--- Modules/Tokens/src/Tokens.Application/Tokens/DTOs/TokenDTO.cs
+++ Modules/Tokens/src/Tokens.Application/Tokens/DTOs/TokenDTO.cs
@@ -6,10 +6,10 @@ namespace Backbone.Modules.Tokens.Application.Tokens.DTOs;
 
 public class TokenDTO : IMapTo<Token>
 {
-    public TokenId Id { get; set; }
+    public required TokenId Id { get; set; }
 
-    public IdentityAddress CreatedBy { get; set; }
-    public DeviceId CreatedByDevice { get; set; }
+    public required IdentityAddress CreatedBy { get; set; }
+    public required DeviceId CreatedByDevice { get; set; }
 
     public DateTime CreatedAt { get; set; }
     public DateTime ExpiresAt { get; set; }
