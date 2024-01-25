915c8ae443bc2ac202cb17ef63804f0350f3cee8
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-01-25 15:01:41 +0100

    refactor: suppress the null warning

--- Modules/Messages/src/Messages.Common/Range.cs
+++ Modules/Messages/src/Messages.Common/Range.cs
@@ -15,12 +15,12 @@ public class Range<T>
 
     public bool HasFrom()
     {
-        return From != null && !From.Equals(default(T));
+        return !From!.Equals(default(T));
     }
 
     public bool HasTo()
     {
-        return To != null && !To.Equals(default(T));
+        return !To!.Equals(default(T));
     }
 }
 
