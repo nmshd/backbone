16723da7af56cc8ee190845cfa4a5b0a7d33a0b6
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-01-25 21:54:08 +0100

    chore: remove pointless line break

--- Modules/Devices/src/Devices.Application/Extensions/TierQueryableExtensions.cs
+++ Modules/Devices/src/Devices.Application/Extensions/TierQueryableExtensions.cs
@@ -8,7 +8,6 @@ public static class TierQueryableExtensions
     public static async Task<Tier> GetBasicTier(this IQueryable<Tier> query, CancellationToken cancellationToken)
     {
         var basicTier = await query.FirstOrDefaultAsync(t => t.Name == TierName.BASIC_DEFAULT_NAME, cancellationToken);
-
         return basicTier!;
     }
 }
