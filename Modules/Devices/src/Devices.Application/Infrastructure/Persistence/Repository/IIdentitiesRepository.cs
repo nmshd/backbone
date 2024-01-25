1ef9d84af6290271ea5bcf62e4e4f144a7c19bf2
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-01-25 22:48:20 +0100

    refactor: update return type

--- Modules/Devices/src/Devices.Application/Infrastructure/Persistence/Repository/IIdentitiesRepository.cs
+++ Modules/Devices/src/Devices.Application/Infrastructure/Persistence/Repository/IIdentitiesRepository.cs
@@ -10,7 +10,7 @@ public interface IIdentitiesRepository
     #region Identities
     Task<DbPaginationResult<Identity>> FindAll(PaginationFilter paginationFilter, CancellationToken cancellationToken);
     Task Update(Identity identity, CancellationToken cancellationToken);
-    Task<Identity> FindByAddress(IdentityAddress address, CancellationToken cancellationToken, bool track = false);
+    Task<Identity?> FindByAddress(IdentityAddress address, CancellationToken cancellationToken, bool track = false);
 #nullable disable
     Task<bool> Exists(IdentityAddress address, CancellationToken cancellationToken);
     Task<int> CountByClientId(string clientId, CancellationToken cancellationToken);
