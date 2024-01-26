9e3fa4a7d7d238e0bcd1487d95c1dcf933f7a888
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-26 10:48:44 +0100

    refactor: make prop required

--- Modules/Relationships/src/Relationships.Application/Relationships/DTOs/RelationshipChangeDTO.cs
+++ Modules/Relationships/src/Relationships.Application/Relationships/DTOs/RelationshipChangeDTO.cs
@@ -7,12 +7,12 @@ namespace Backbone.Modules.Relationships.Application.Relationships.DTOs;
 
 public class RelationshipChangeDTO : IMapTo<RelationshipChange>
 {
-    public RelationshipChangeId Id { get; set; }
+    public required RelationshipChangeId Id { get; set; }
 
-    public RelationshipId RelationshipId { get; set; }
+    public required RelationshipId RelationshipId { get; set; }
 
-    public RelationshipChangeRequestDTO Request { get; set; }
-    public RelationshipChangeResponseDTO Response { get; set; }
+    public required RelationshipChangeRequestDTO Request { get; set; }
+    public required RelationshipChangeResponseDTO Response { get; set; }
 
     public RelationshipChangeType Type { get; set; }
 
@@ -22,15 +22,15 @@ public class RelationshipChangeDTO : IMapTo<RelationshipChange>
 public class RelationshipChangeRequestDTO : IMapTo<RelationshipChangeRequest>
 {
     public DateTime CreatedAt { get; set; }
-    public IdentityAddress CreatedBy { get; set; }
-    public DeviceId CreatedByDevice { get; set; }
-    public byte[] Content { get; set; }
+    public required IdentityAddress CreatedBy { get; set; }
+    public required DeviceId CreatedByDevice { get; set; }
+    public required byte[] Content { get; set; }
 }
 
 public class RelationshipChangeResponseDTO : IMapTo<RelationshipChangeResponse>
 {
     public DateTime CreatedAt { get; set; }
-    public IdentityAddress CreatedBy { get; set; }
-    public DeviceId CreatedByDevice { get; set; }
-    public byte[] Content { get; set; }
+    public required IdentityAddress CreatedBy { get; set; }
+    public required DeviceId CreatedByDevice { get; set; }
+    public required byte[] Content { get; set; }
 }
