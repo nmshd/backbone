50468609039ee983cb45c36c7240e10739a060c5
Vladimir Vuckovic <vidrak97@gmail.com>
2024-01-30 11:54:19 +0100

    chore: change router nav to dialog opening

--- AdminUi/src/AdminUi/ClientApp/src/app/components/client/client-list/client-list.component.ts
+++ AdminUi/src/AdminUi/ClientApp/src/app/components/client/client-list/client-list.component.ts
@@ -13,6 +13,8 @@ import { TierOverview, TierService } from "src/app/services/tier-service/tier.se
 import { PagedHttpResponseEnvelope } from "src/app/utils/paged-http-response-envelope";
 import { ConfirmationDialogComponent } from "../../shared/confirmation-dialog/confirmation-dialog.component";
 import { ChangeSecretDialogComponent } from "../change-secret-dialog/change-secret-dialog.component";
+import { CreateClientDialogComponent } from "../create-client-dialog/create-client-dialog.component";
+
 @Component({
     selector: "app-client-list",
     templateUrl: "./client-list.component.html",
@@ -183,8 +185,11 @@ export class ClientListComponent {
         this.table.renderRows();
     }
 
-    public async addClient(): Promise<void> {
-        await this.router.navigate(["/clients/create"]);
+    public addClientDialog(): void {
+        this.dialog.open(CreateClientDialogComponent, {
+            width: "450px",
+            height: "750px"
+        });
     }
 
     public openConfirmationDialog(): void {
