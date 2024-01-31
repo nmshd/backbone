19357bd2dfafcd9937b1b367dae399b5df971a6e
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-01-31 11:14:47 +0100

    feat: add message method that returns a decrypted body provided correct key is passed in (wip)

---
+++ Modules/Messages/test/Messages.Domain.Tests/Tests/Entities/MessageTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Backbone.Modules.Messages.Domain.Tests.Tests.Entities;
+
+public class MessageTests
+{
+    [Fact]
+    public void Message_should_return_a_decrypted_body_provided_correct_symmetric_key_has_been_passed_as_a_parameter()
+    {
+
+    }
+}
+
