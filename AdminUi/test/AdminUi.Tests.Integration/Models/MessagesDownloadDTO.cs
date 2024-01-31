b0c91ea59bb478d22a11ccc26fa5b7df8e9d4b88
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-01-31 16:00:07 +0100

    feat: add message method that returns a decrypted body provided correct key is passed in (wip)

---
+++ AdminUi/test/AdminUi.Tests.Integration/Models/MessagesDownloadDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace Backbone.AdminUi.Tests.Integration.Models;
+
+public class MessagesDownloadDTO
+{
+    public string Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public MessageBody Body { get; set; }
+}
+
+public class MessageBody
+{
+    public string Type { get; set; }
+    public string To { get; set; }
+    public string Content { get; set; }
+}
--- Modules/Messages/test/Messages.Domain.Tests/Tests/Entities/MessageTests.cs
+++ Modules/Messages/test/Messages.Domain.Tests/Tests/Entities/MessageTests.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Backbone.Crypto;
+using Backbone.Crypto.Implementations.Deprecated.BouncyCastle;
+using Backbone.Crypto.Implementations.Deprecated.BouncyCastle.Symmetric;
+using Backbone.Modules.Messages.Domain.Entities;
 using Backbone.UnitTestTools.Data;
 using FluentAssertions;
 using Xunit;
@@ -14,7 +13,66 @@ public class MessageTests
     [Fact]
     public void Message_should_return_a_decrypted_body_provided_correct_symmetric_key_has_been_passed_as_a_parameter()
     {
+        // Arrange
         var address = TestDataGenerator.CreateRandomIdentityAddress();
+        var deviceId = TestDataGenerator.CreateRandomDeviceId();
+        var body = new byte[] {1,2,3,4};
+        var attachments = new List<Attachment>();
+        var recipents = new List<RecipientInformation>();
+
+        var message = new Message(address, deviceId, null, body, attachments, recipents);
+
+        // Act
+        var symmetricKey = "code";
+        var decryptedBody = message.DecryptBody(symmetricKey);
+
+        // Assert
+        decryptedBody.Should().Be("01-02-03-04");
+    }
+
+
+    [Fact]
+    public void GenerateSymmetricKey_ShouldGenerateKeyThatCanBeUsedForSymmetricEncryption()
+    {
+        // Arrange
+        var aesEncryptionHelper = AesSymmetricEncrypter.CreateWith96BitIv128BitMac();
+        var testString = "Test";
+
+        // Act
+        KeyGenerator keyGeneratorUnderTest = new KeyGenerator();
+        var symmetricKey = keyGeneratorUnderTest.GenerateSymmetricKey(256);
+        var encrypted = aesEncryptionHelper.Encrypt(ConvertibleString.FromUtf8(testString), symmetricKey);
+        var result = aesEncryptionHelper.Decrypt(encrypted, symmetricKey);
+
+        // Assert
+        testString.Should().Be(result.Utf8Representation);
+    }
+
+
+    [Fact]
+    public void Should_decrypt_body_with_given_symmetric_key()
+    {
+        // Arrange
+        var aesEncryptionHelper = AesSymmetricEncrypter.CreateWith96BitIv128BitMac();
+
+        var address = TestDataGenerator.CreateRandomIdentityAddress();
+        var deviceId = TestDataGenerator.CreateRandomDeviceId();
+        var body = new byte[] { 0, 0, 7};
+        var attachments = new List<Attachment>();
+        var recipents = new List<RecipientInformation>();
+
+        var message = new Message(address, deviceId, null, body, attachments, recipents);
+
+        var testString = BitConverter.ToString(body);
+
+        // Act
+        var keyGeneratorUnderTest = new KeyGenerator();
+        var symmetricKey = keyGeneratorUnderTest.GenerateSymmetricKey(256);
+        var encryptedMessage = aesEncryptionHelper.Encrypt(ConvertibleString.FromUtf8(testString), symmetricKey);
+        var result = message.DecryptBodyWithSymmetricKey(encryptedMessage, symmetricKey);
+
+        // Assert
+        result.Utf8Representation.Should().Be(testString);
     }
 }
 
