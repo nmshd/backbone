3c667602cac89309d21c21d7f506f65f700ad1dd
Vladimir Vuckovic <vidrak97@gmail.com>
2024-02-08 07:48:10 +0100

    chore: prettier

--- AdminUi/src/AdminUi/ClientApp/src/app/components/quotas/identity/identity-details/download-message-dialog/download-message-dialog.component.ts
+++ AdminUi/src/AdminUi/ClientApp/src/app/components/quotas/identity/identity-details/download-message-dialog/download-message-dialog.component.ts
@@ -39,26 +39,26 @@ export class DownloadMessageDialogComponent {
     }
 
     public downloadMessage(): void {
-            const message = btoa(this.message)
-            const byteCharacters = atob(message);
-            const byteArrays = [];
-        
-            for (let offset = 0; offset < byteCharacters.length; offset += 512) {
-              const slice = byteCharacters.slice(offset, offset + 512);
-              const byteNumbers = new Array(slice.length);
-              for (let i = 0; i < slice.length; i++) {
+        const message = btoa(this.message);
+        const byteCharacters = atob(message);
+        const byteArrays = [];
+
+        for (let offset = 0; offset < byteCharacters.length; offset += 512) {
+            const slice = byteCharacters.slice(offset, offset + 512);
+            const byteNumbers = new Array(slice.length);
+            for (let i = 0; i < slice.length; i++) {
                 byteNumbers[i] = slice.charCodeAt(i);
-              }
-              const byteArray = new Uint8Array(byteNumbers);
-              byteArrays.push(byteArray);
             }
-        
-            const blob = new Blob(byteArrays, { type: "application/pdf" });
-            const url = window.URL.createObjectURL(blob);
-            const link = document.createElement("a");
-            link.href = url;
-            link.download = "file.pdf";
-            link.click();
-            window.URL.revokeObjectURL(url);
+            const byteArray = new Uint8Array(byteNumbers);
+            byteArrays.push(byteArray);
+        }
+
+        const blob = new Blob(byteArrays, { type: "application/pdf" });
+        const url = window.URL.createObjectURL(blob);
+        const link = document.createElement("a");
+        link.href = url;
+        link.download = "file.pdf";
+        link.click();
+        window.URL.revokeObjectURL(url);
     }
 }
