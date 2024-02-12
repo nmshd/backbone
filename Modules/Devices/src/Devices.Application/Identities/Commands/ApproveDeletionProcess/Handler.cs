4f7bb6059c4252ef71da3f215605a8a30ce30853
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-02-12 11:58:40 +0100

    refactor: remove unnecessary tiers repository calls

--- Modules/Devices/src/Devices.Application/Identities/Commands/ApproveDeletionProcess/Handler.cs
+++ Modules/Devices/src/Devices.Application/Identities/Commands/ApproveDeletionProcess/Handler.cs
@@ -12,11 +12,10 @@ namespace Backbone.Modules.Devices.Application.Identities.Commands.ApproveDeleti
 public class Handler : IRequestHandler<ApproveDeletionProcessCommand, ApproveDeletionProcessResponse>
 {
     private readonly IIdentitiesRepository _identitiesRepository;
-    private readonly ITiersRepository _tiersRepository;
     private readonly IUserContext _userContext;
     private readonly IEventBus _eventBus;
 
-    public Handler(IIdentitiesRepository identitiesRepository, ITiersRepository tiersRepository, IUserContext userContext, IEventBus eventBus)
+    public Handler(IIdentitiesRepository identitiesRepository, IUserContext userContext, IEventBus eventBus)
     {
         _identitiesRepository = identitiesRepository;
         _tiersRepository = tiersRepository;
@@ -29,12 +28,9 @@ public class Handler : IRequestHandler<ApproveDeletionProcessCommand, ApproveDel
         var identity = await _identitiesRepository.FindByAddress(_userContext.GetAddress(), cancellationToken) ?? throw new NotFoundException(nameof(Identity));
 
         var oldTierId = identity.TierId;
-        var oldTier = await _tiersRepository.FindById(oldTierId, cancellationToken) ?? throw new NotFoundException(nameof(Tier));
-
         var deletionProcess = identity.ApproveDeletionProcess(request.DeletionProcessId, _userContext.GetDeviceId());
         
         var newTierId = identity.TierId;
-        var newTier = await _tiersRepository.FindById(newTierId, cancellationToken) ?? throw new NotFoundException(nameof(Tier));
 
         await _identitiesRepository.Update(identity, cancellationToken);
 
