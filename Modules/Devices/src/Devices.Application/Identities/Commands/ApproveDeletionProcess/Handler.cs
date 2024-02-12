4281ed139b457a250382fc13f71242d76a6358ac
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-02-12 11:43:35 +0100

    feat: trigger integration event for tier change

--- Modules/Devices/src/Devices.Application/Identities/Commands/ApproveDeletionProcess/Handler.cs
+++ Modules/Devices/src/Devices.Application/Identities/Commands/ApproveDeletionProcess/Handler.cs
@@ -3,6 +3,7 @@ using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
 using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.UserContext;
 using Backbone.Modules.Devices.Application.Infrastructure.Persistence.Repository;
 using Backbone.Modules.Devices.Application.IntegrationEvents.Outgoing;
+using Backbone.Modules.Devices.Domain.Aggregates.Tier;
 using Backbone.Modules.Devices.Domain.Entities.Identities;
 using MediatR;
 
@@ -11,12 +12,14 @@ namespace Backbone.Modules.Devices.Application.Identities.Commands.ApproveDeleti
 public class Handler : IRequestHandler<ApproveDeletionProcessCommand, ApproveDeletionProcessResponse>
 {
     private readonly IIdentitiesRepository _identitiesRepository;
+    private readonly ITiersRepository _tiersRepository;
     private readonly IUserContext _userContext;
     private readonly IEventBus _eventBus;
 
-    public Handler(IIdentitiesRepository identitiesRepository, IUserContext userContext, IEventBus eventBus)
+    public Handler(IIdentitiesRepository identitiesRepository, ITiersRepository tiersRepository, IUserContext userContext, IEventBus eventBus)
     {
         _identitiesRepository = identitiesRepository;
+        _tiersRepository = tiersRepository;
         _userContext = userContext;
         _eventBus = eventBus;
     }
@@ -25,10 +28,18 @@ public class Handler : IRequestHandler<ApproveDeletionProcessCommand, ApproveDel
     {
         var identity = await _identitiesRepository.FindByAddress(_userContext.GetAddress(), cancellationToken) ?? throw new NotFoundException(nameof(Identity));
 
+        var oldTierId = identity.TierId;
+        var oldTier = await _tiersRepository.FindById(oldTierId, cancellationToken) ?? throw new NotFoundException(nameof(Tier));
+
         var deletionProcess = identity.ApproveDeletionProcess(request.DeletionProcessId, _userContext.GetDeviceId());
+        
+        var newTierId = identity.TierId;
+        var newTier = await _tiersRepository.FindById(newTierId, cancellationToken) ?? throw new NotFoundException(nameof(Tier));
+
         await _identitiesRepository.Update(identity, cancellationToken);
 
         _eventBus.Publish(new IdentityToBeDeletedIntegrationEvent(identity.Address, deletionProcess.Id));
+        _eventBus.Publish(new TierOfIdentityChangedIntegrationEvent(identity, oldTier, newTier));
 
         return new ApproveDeletionProcessResponse(deletionProcess);
     }
