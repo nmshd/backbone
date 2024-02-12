529db842c10f578633cd84540077605a7d595bae
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-02-12 14:44:40 +0100

    feat: add FirstOf3FailedAt prop to ApplicationUser

--- Modules/Devices/src/Devices.Domain/Entities/ApplicationUser.cs
+++ Modules/Devices/src/Devices.Domain/Entities/ApplicationUser.cs
@@ -34,6 +34,7 @@ public class ApplicationUser : IdentityUser
 
     public DateTime CreatedAt { get; set; }
     public DateTime? LastLoginAt { get; set; }
+    public DateTimeOffset? FirstOf3FailedAt { get; set; }
 
     public bool HasLoggedIn => LastLoginAt.HasValue;
 
