0f2ffafae27ec1c0cc94032a9c5b83abb2f3a94d
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-02-13 11:25:23 +0100

    refactor: make nullable

--- Modules/Synchronization/test/Synchronization.Application.Tests/Tests/SyncRuns/Commands/StartSyncRun/HandlerTests.cs
+++ Modules/Synchronization/test/Synchronization.Application.Tests/Tests/SyncRuns/Commands/StartSyncRun/HandlerTests.cs
@@ -253,7 +253,7 @@ public class HandlerTests
         return handler;
     }
 
-    private Handler CreateHandler(IdentityAddress activeIdentity, DeviceId createdByDevice, SynchronizationDbContext dbContext = null)
+    private Handler CreateHandler(IdentityAddress activeIdentity, DeviceId createdByDevice, SynchronizationDbContext? dbContext = null)
     {
         var userContext = A.Fake<IUserContext>();
         A.CallTo(() => userContext.GetAddress()).Returns(activeIdentity);
