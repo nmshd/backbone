26eb394f25045b6001d4c0957e9932209c83e72d
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-02-13 12:54:05 +0100

    refactor: make Modifications nad Id required

--- Modules/Synchronization/src/Synchronization.Application/Datawallets/Commands/PushDatawalletModifications/PushDatawalletModificationsResponse.cs
+++ Modules/Synchronization/src/Synchronization.Application/Datawallets/Commands/PushDatawalletModifications/PushDatawalletModificationsResponse.cs
@@ -7,12 +7,12 @@ public class PushDatawalletModificationsResponse
 {
     public long NewIndex { get; set; }
 
-    public IEnumerable<PushDatawalletModificationsResponseItem> Modifications { get; set; }
+    public required IEnumerable<PushDatawalletModificationsResponseItem> Modifications { get; set; }
 }
 
 public class PushDatawalletModificationsResponseItem : IMapTo<DatawalletModification>
 {
-    public DatawalletModificationId Id { get; set; }
+    public required DatawalletModificationId Id { get; set; }
     public long Index { get; set; }
     public DateTime CreatedAt { get; set; }
 }
