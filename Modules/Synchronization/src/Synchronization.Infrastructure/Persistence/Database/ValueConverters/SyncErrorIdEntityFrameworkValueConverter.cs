20525ea82c05a9e57fb70379113de635ba9c6175
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-02-13 14:09:33 +0100

    refactor: expression is always false

--- Modules/Synchronization/src/Synchronization.Infrastructure/Persistence/Database/ValueConverters/SyncErrorIdEntityFrameworkValueConverter.cs
+++ Modules/Synchronization/src/Synchronization.Infrastructure/Persistence/Database/ValueConverters/SyncErrorIdEntityFrameworkValueConverter.cs
@@ -9,7 +9,7 @@ public class SyncErrorIdEntityFrameworkValueConverter : ValueConverter<SyncError
 
     public SyncErrorIdEntityFrameworkValueConverter(ConverterMappingHints mappingHints)
         : base(
-            id => id == null ? null : id.StringValue,
+            id => id.StringValue,
             value => SyncErrorId.Parse(value),
             mappingHints?.With(new ConverterMappingHints(SyncErrorId.MAX_LENGTH))
         )
