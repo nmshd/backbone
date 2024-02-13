aafd71f12ace6088f6daa6bc30a4c3c91363c046
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-02-13 13:45:17 +0100

    refactor: change error message

--- ConsumerApi/Controllers/AuthorizationController.cs
+++ ConsumerApi/Controllers/AuthorizationController.cs
@@ -103,7 +103,7 @@ public class AuthorizationController : ApiControllerBase
         {
             [OpenIddictServerAspNetCoreConstants.Properties.Error] = Errors.AccessDenied,
             [OpenIddictServerAspNetCoreConstants.Properties.ErrorDescription] =
-                "The user is temporarily locked out."
+                "The user is temporarily locked out, please try again in few minutes."
         });
 
         return Forbid(properties, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
