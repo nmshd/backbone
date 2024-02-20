0971f0ab6cc41e966fb3d721e85c25501a19c890
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-02-20 15:48:10 +0100

    wip

--- Jobs/src/Job.IdentityDeletion/CancelDeletionProcessWorker.cs
+++ Jobs/src/Job.IdentityDeletion/CancelDeletionProcessWorker.cs
@@ -1,19 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
+using Backbone.BuildingBlocks.Application.Identities;
+using Backbone.BuildingBlocks.Application.PushNotifications;
+using Backbone.Modules.Devices.Application.Identities.Commands.TriggerRipeDeletionProcesses;
+using Backbone.Modules.Devices.Application.IntegrationEvents.Outgoing;
+using Backbone.Modules.Devices.Domain.Entities.Identities;
+using Backbone.Modules.Relationships.Application.Relationships.Commands.FindRelationshipsOfIdentity;
+using MediatR;
+using DeletionStartsNotification = Backbone.Modules.Devices.Application.Infrastructure.PushNotifications.DeletionProcess.DeletionStartsNotification;
 
 namespace Backbone.Job.IdentityDeletion;
 public class CancelDeletionProcessWorker : IHostedService
 {
-    public Task StartAsync(CancellationToken cancellationToken)
+    private readonly IEventBus _eventBus;
+    private readonly IHostApplicationLifetime _host;
+    private readonly IEnumerable<IIdentityDeleter> _identityDeleters;
+    private readonly IMediator _mediator;
+    private readonly IPushNotificationSender _pushNotificationSender;
+    private readonly ILogger<CancelDeletionProcessWorker> _logger;
+
+    public CancelDeletionProcessWorker(IHostApplicationLifetime host,
+        IEnumerable<IIdentityDeleter> identityDeleters,
+        IMediator mediator,
+        IPushNotificationSender pushNotificationSender,
+        IEventBus eventBus,
+        ILogger<CancelDeletionProcessWorker> logger)
     {
-        throw new NotImplementedException();
+        _host = host;
+        _identityDeleters = identityDeleters;
+        _mediator = mediator;
+        _pushNotificationSender = pushNotificationSender;
+        _eventBus = eventBus;
+        _logger = logger;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await StartProcessing(cancellationToken);
+
+        _host.StopApplication();
+    }
+
+    private async Task StartProcessing(CancellationToken cancellationToken)
+    {
+        var identities = await _mediator.Send(new TriggerRipeDeletionProcessesCommand(), cancellationToken);
+
+        foreach (var identityAddress in identities.DeletedIdentityAddresses)
+        {
+            await _pushNotificationSender.SendNotification(identityAddress, new DeletionStartsNotification(IdentityDeletionConfiguration.DeletionStartsNotification.Message), cancellationToken);
+
+            var relationships = (await _mediator.Send(new FindRelationshipsOfIdentityQuery(identityAddress), cancellationToken)).Relationships;
+
+            foreach (var relationship in relationships)
+            {
+                _eventBus.Publish(new PeerIdentityDeletedIntegrationEvent(relationship.Id, identityAddress));
+            }
+
+            foreach (var identityDeleter in _identityDeleters)
+            {
+                await identityDeleter.Delete(identityAddress);
+            }
+        }
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        return Task.CompletedTask;
     }
 }
