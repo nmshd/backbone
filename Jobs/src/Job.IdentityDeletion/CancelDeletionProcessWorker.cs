764ee666d819a7c3ee6200b3aa6498fd39b5725e
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-02-20 13:58:49 +0100

    feat: add CancelDeletionProcessWorker class

---
+++ Jobs/src/Job.IdentityDeletion/CancelDeletionProcessWorker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backbone.Job.IdentityDeletion;
+public class CancelDeletionProcessWorker : IHostedService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
