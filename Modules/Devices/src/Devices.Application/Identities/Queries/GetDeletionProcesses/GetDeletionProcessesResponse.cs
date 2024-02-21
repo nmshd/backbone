3fbf6ad3df14a8e7c2b20ef3d07964dc9f61ec3b
Mika Herrmann <mika.herrmann@js-soft.com>
2024-02-21 14:23:43 +0100

    Add GetDeletionProcesses Query

---
+++ Modules/Devices/src/Devices.Application/Identities/Queries/GetDeletionProcesses/GetDeletionProcessesResponse.cs
@@ -0,0 +1,15 @@
+﻿using Backbone.Modules.Devices.Application.DTOs;
+using Backbone.Modules.Devices.Domain.Entities.Identities;
+
+namespace Backbone.Modules.Devices.Application.Identities.Queries.GetDeletionProcesses;
+public class GetDeletionProcessesResponse
+{
+    public GetDeletionProcessesResponse(Identity identity)
+    {
+        DeletionProcesses = identity.DeletionProcesses
+            .Select(p => new IdentityDeletionProcessDTO(p))
+            .ToList();
+    }
+
+    public List<IdentityDeletionProcessDTO> DeletionProcesses { get; set; }
+}
---
+++ Modules/Devices/src/Devices.Application/Identities/Queries/GetDeletionProcesses/Handler.cs
@@ -0,0 +1,27 @@
+﻿using Backbone.BuildingBlocks.Application.Abstractions.Exceptions;
+using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.UserContext;
+using Backbone.Modules.Devices.Application.Infrastructure.Persistence.Repository;
+using Backbone.Modules.Devices.Domain.Entities.Identities;
+using MediatR;
+
+namespace Backbone.Modules.Devices.Application.Identities.Queries.GetDeletionProcesses;
+
+public class Handler : IRequestHandler<GetDeletionProcessesQuery, GetDeletionProcessesResponse>
+{
+    private readonly IIdentitiesRepository _identityRepository;
+    private readonly IUserContext _userContext;
+
+    public Handler(IIdentitiesRepository identityRepository, IUserContext userContext)
+    {
+        _identityRepository = identityRepository;
+        _userContext = userContext;
+    }
+
+    public async Task<GetDeletionProcessesResponse> Handle(GetDeletionProcessesQuery request, CancellationToken cancellationToken)
+    {
+        var identity = await _identityRepository.FindByAddress(_userContext.GetAddress(), cancellationToken) ?? throw new NotFoundException(nameof(Identity));
+        var response = new GetDeletionProcessesResponse(identity);
+
+        return response;
+    }
+}
