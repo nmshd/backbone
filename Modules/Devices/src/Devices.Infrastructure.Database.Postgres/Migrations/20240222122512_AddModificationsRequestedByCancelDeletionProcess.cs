829fb97612aef210bbd86310b9749521dfaf7e41
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-02-22 13:45:19 +0100

    fear: add migration for CancelledAt & CancelledByDevice

---
+++ Modules/Devices/src/Devices.Infrastructure.Database.Postgres/Migrations/20240222122512_AddModificationsRequestedByCancelDeletionProcess.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backbone.Modules.Devices.Infrastructure.Database.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddModificationsRequestedByCancelDeletionProcess : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CancelledAt",
+                table: "IdentityDeletionProcesses",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CancelledByDevice",
+                table: "IdentityDeletionProcesses",
+                type: "character(20)",
+                unicode: false,
+                fixedLength: true,
+                maxLength: 20,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CancelledAt",
+                table: "IdentityDeletionProcesses");
+
+            migrationBuilder.DropColumn(
+                name: "CancelledByDevice",
+                table: "IdentityDeletionProcesses");
+        }
+    }
+}
--- Modules/Devices/src/Devices.Infrastructure.Database.Postgres/Migrations/DevicesDbContextModelSnapshot.cs
+++ Modules/Devices/src/Devices.Infrastructure.Database.Postgres/Migrations/DevicesDbContextModelSnapshot.cs
@@ -279,6 +279,15 @@ namespace Devices.Infrastructure.Database.Postgres.Migrations
                         .HasColumnType("character(20)")
                         .IsFixedLength();
 
+                    b.Property<DateTime?>("CancelledAt")
+                        .HasColumnType("timestamp with time zone");
+
+                    b.Property<string>("CancelledByDevice")
+                        .HasMaxLength(20)
+                        .IsUnicode(false)
+                        .HasColumnType("character(20)")
+                        .IsFixedLength();
+
                     b.Property<DateTime>("CreatedAt")
                         .HasColumnType("timestamp with time zone");
 
