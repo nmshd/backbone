818b792006a1a4b20f96d428cdee225889575738
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-02-23 08:33:13 +0100

    test: add new project

---
+++ Jobs/test/Job.IdentityDeletion.Tests/CancelDeletionProcessWorkerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backbone.Job.IdentityDeletion.Tests
+{
+    public class CancelDeletionProcessWorkerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+    }
+}
