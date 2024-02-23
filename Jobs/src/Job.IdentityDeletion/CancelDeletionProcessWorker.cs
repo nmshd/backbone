f21d92adf8a06803a5c6dd54cac125d60600f0a6
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-02-23 08:33:19 +0100

    wip

--- Jobs/src/Job.IdentityDeletion/CancelDeletionProcessWorker.cs
+++ Jobs/src/Job.IdentityDeletion/CancelDeletionProcessWorker.cs
@@ -1,28 +1,27 @@
 ﻿using System.Reflection;
 using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
-using Backbone.BuildingBlocks.Application.Identities;
 using Backbone.BuildingBlocks.Application.PushNotifications;
-using Backbone.Modules.Devices.Application.Identities.Commands.TriggerRipeDeletionProcesses;
+using Backbone.DevelopmentKit.Identity.ValueObjects;
+
+//using Backbone.Modules.Devices.Application.Identities.Commands.TriggerRipeDeletionProcesses;
 using Backbone.Modules.Devices.Application.Infrastructure.PushNotifications;
 using Backbone.Modules.Devices.Application.IntegrationEvents.Outgoing;
 using Backbone.Modules.Devices.Domain.Entities.Identities;
-using Backbone.Modules.Relationships.Application.Relationships.Commands.FindRelationshipsOfIdentity;
+//using Backbone.Modules.Relationships.Application.Relationships.Commands.FindRelationshipsOfIdentity;
 using MediatR;
-using DeletionStartsNotification = Backbone.Modules.Devices.Application.Infrastructure.PushNotifications.DeletionProcess.DeletionStartsNotification;
+//using DeletionStartsNotification = Backbone.Modules.Devices.Application.Infrastructure.PushNotifications.DeletionProcess.DeletionStartsNotification;
 
 namespace Backbone.Job.IdentityDeletion;
 public class CancelDeletionProcessWorker : IHostedService
 {
     private readonly IEventBus _eventBus;
     private readonly IHostApplicationLifetime _host;
-    private readonly IEnumerable<IIdentityDeleter> _identityDeleters;
     private readonly IMediator _mediator;
     private readonly IPushNotificationSender _pushNotificationSender;
     private readonly ILogger<CancelDeletionProcessWorker> _logger;
     private readonly IReadOnlyList<IdentityDeletionProcess> _deletionProcesses;
 
     public CancelDeletionProcessWorker(IHostApplicationLifetime host,
-        IEnumerable<IIdentityDeleter> identityDeleters,
         IMediator mediator,
         IPushNotificationSender pushNotificationSender,
         IEventBus eventBus,
@@ -30,7 +29,6 @@ public class CancelDeletionProcessWorker : IHostedService
         IReadOnlyList<IdentityDeletionProcess> deletionProcesses)
     {
         _host = host;
-        _identityDeleters = identityDeleters;
         _mediator = mediator;
         _pushNotificationSender = pushNotificationSender;
         _eventBus = eventBus;
@@ -51,28 +49,41 @@ public class CancelDeletionProcessWorker : IHostedService
 
         foreach (var identityAddress in identities.DeletedIdentityAddresses)
         {
-            await _pushNotificationSender.SendNotification(identityAddress, new DeletionCanceledNotification(IdentityDeletionConfiguration.DeletionCanceledNotification.Message), cancellationToken);
+            //await _pushNotificationSender.SendNotification(identityAddress, new DeletionCanceledNotification(IdentityDeletionConfiguration.DeletionCanceledNotification.Message), cancellationToken);
 
-            var relationships = (await _mediator.Send(new FindRelationshipsOfIdentityQuery(identityAddress), cancellationToken)).Relationships;
+            //var relationships = (await _mediator.Send(new FindRelationshipsOfIdentityQuery(identityAddress), cancellationToken)).Relationships;
 
             foreach (var relationship in relationships)
             {
                 _eventBus.Publish(new PeerIdentityDeletedIntegrationEvent(relationship.Id, identityAddress));
             }
 
-            foreach (var identityDeleter in _identityDeleters)
+            //foreach (var identityDeleter in _identityDeleters)
+            //{
+            //    await identityDeleter.Delete(identityAddress);
+            //}
+        }
+
+        foreach (var identity in identities.Identities)
+        {
+            foreach (var identityDeletionProcess in _deletionProcesses)
             {
-                await identityDeleter.Delete(identityAddress);
+                if (identityDeletionProcess.CreatedAt < DateTime.UtcNow)
+                {
+                    await _pushNotificationSender.SendNotification
+                        (identity.Address, new DeletionCanceledNotification(IdentityDeletionConfiguration.DeletionCanceledNotification.Message), cancellationToken);
+                    identityDeletionProcess.Cancel(identity.Address);
+                }
             }
         }
 
-        foreach (var identityDeletionProcess in _deletionProcesses)
-        {
-            if (identityDeletionProcess.CreatedAt < DateTime.UtcNow)
-            {
-                //identityDeletionProcess.Status
-            }
-        }
+        //foreach (var identityDeletionProcess in _deletionProcesses)
+        //{
+        //    if (identityDeletionProcess.CreatedAt < DateTime.UtcNow)
+        //    {
+        //        identityDeletionProcess.Cancel();
+        //    }
+        //}
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
@@ -89,4 +100,5 @@ public record DeletionCanceledNotification
 
 public class TriggerRipeDeletionProcessesCommand : IRequest<TriggerRipeDeletionProcessesCommand>
 {
+    public List<Identity> Identities { get; set; }
 }
