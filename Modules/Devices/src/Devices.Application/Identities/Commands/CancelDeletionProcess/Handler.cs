002557386a26f119452e811d49a127aa7dd25b92
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-02-26 15:52:35 +0100

    fix: handler didn't have IRequestHandler<> and now properly returns response

--- Modules/Devices/src/Devices.Application/Identities/Commands/CancelDeletionProcess/Handler.cs
+++ Modules/Devices/src/Devices.Application/Identities/Commands/CancelDeletionProcess/Handler.cs
@@ -2,9 +2,10 @@
 using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.UserContext;
 using Backbone.Modules.Devices.Application.Infrastructure.Persistence.Repository;
 using Backbone.Modules.Devices.Domain.Entities.Identities;
+using MediatR;
 
 namespace Backbone.Modules.Devices.Application.Identities.Commands.CancelDeletionProcess;
-public class Handler
+public class Handler : IRequestHandler<CancelDeletionProcessCommand, CancelDeletionProcessResponse>
 {
     private readonly IIdentitiesRepository _identitiesRepository;
     private readonly IUserContext _userContext;
@@ -15,11 +16,13 @@ public class Handler
         _userContext = userContext;
     }
 
-    public async Task Handle(CancelDeletionProcessCommand request, CancellationToken cancellationToken)
+    public async Task<CancelDeletionProcessResponse> Handle(CancelDeletionProcessCommand request, CancellationToken cancellationToken)
     {
         var identity = await _identitiesRepository.FindByAddress(_userContext.GetAddress(), cancellationToken, true) ?? throw new NotFoundException(nameof(Identity));
-        identity.CancelDeletionProcess(_userContext.GetDeviceId());
+        var deletionProcess = identity.CancelDeletionProcess(_userContext.GetDeviceId());
 
         await _identitiesRepository.Update(identity, cancellationToken);
+
+        return new CancelDeletionProcessResponse(deletionProcess);
     }
 }
