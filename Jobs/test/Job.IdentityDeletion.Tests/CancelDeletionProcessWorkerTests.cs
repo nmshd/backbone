99394b44f5c987f0c66ef39c099da9b0e7f95294
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-02-26 15:15:54 +0100

    test: check that correct handler is triggered from within the worker

--- Jobs/test/Job.IdentityDeletion.Tests/CancelDeletionProcessWorkerTests.cs
+++ Jobs/test/Job.IdentityDeletion.Tests/CancelDeletionProcessWorkerTests.cs
@@ -3,30 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
+using Backbone.BuildingBlocks.Application.PushNotifications;
+using Backbone.DevelopmentKit.Identity.ValueObjects;
+using Backbone.Modules.Devices.Application.Identities.Commands.TriggerStaleDeletionProcesses;
+using Backbone.Modules.Devices.Domain.Aggregates.Tier;
+using Backbone.Modules.Devices.Domain.Entities.Identities;
 using FakeItEasy;
 using MediatR;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Backbone.Job.IdentityDeletion.Tests
 {
     public class CancelDeletionProcessWorkerTests
     {
         [Fact]
-        public void Test1()
+        public async void Test1()
         {
             // Arrange
-            _ = A.Fake<IMediator>();
+            var mockMediator = A.Fake<IMediator>();
+            var hostApplicationLifetime = A.Dummy<IHostApplicationLifetime>();
 
-            //var commandResponse = new TriggerOldDeletionProcessesResponse
-            //{
-            //    WaitingOnAppruvalAddress = [.. identityAddresses]
-            //};
-            //A.CallTo(() => mockMediator.Send(A<TriggerOldDeletionProcessesCommand>._, A<CancellationToken>._)).Returns(commandResponse);
+            var activeIdentity = new Identity("", IdentityAddress.Create([], "id1"), [], TierId.Generate(), 1);
 
-            //// Act
-            //await CancelDeletionProcessWorker.StartAsync(CancellationToken.None);
+            var identityDeletion = activeIdentity.StartDeletionProcessAsSupport();
+            var identityDeleters = new List<IdentityDeletionProcess>() { identityDeletion };
 
-            //// Assert
-            //A.CallTo(() => mockMediator.Send(A<TriggerOldDeletionProcessesCommand>._, A<CancellationToken>._)).MustHaveHappenedOnceExactly();
+            var commandResponse = new TriggerStaleDeletionProcessesResponse(identityDeletion);
+
+            var cancelDeletionProcessWorker = new CancelDeletionProcessWorker(
+                hostApplicationLifetime,
+                mockMediator,
+                A.Dummy<IPushNotificationSender>(),
+                A.Dummy<IEventBus>(),
+                A.Dummy<ILogger<CancelDeletionProcessWorker>>(),
+                identityDeleters);
+
+            A.CallTo(() => mockMediator.Send(A<TriggerStaleDeletionProcessesCommand>._, A<CancellationToken>._)).Returns(commandResponse);
+
+            // Act
+            await cancelDeletionProcessWorker.StartAsync(CancellationToken.None);
+
+            // Assert
+            A.CallTo(() => mockMediator.Send(A<TriggerStaleDeletionProcessesCommand>._, A<CancellationToken>._)).MustHaveHappenedOnceExactly();
         }
     }
 }
