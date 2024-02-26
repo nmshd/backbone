2c80bc9c84ebe3ec0768f6193642eb0128d4b9ee
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-02-26 09:49:14 +0100

    refactor: improve code structure of CancelStaleDeletionProcessTests

--- Modules/Devices/test/Devices.Domain.Tests/Identities/CancelStaleDeletionProcessTests.cs
+++ Modules/Devices/test/Devices.Domain.Tests/Identities/CancelStaleDeletionProcessTests.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Backbone.DevelopmentKit.Identity.ValueObjects;
+﻿using Backbone.DevelopmentKit.Identity.ValueObjects;
 using Backbone.Modules.Devices.Domain.Aggregates.Tier;
 using Backbone.Modules.Devices.Domain.Entities.Identities;
-using FakeItEasy;
 using FluentAssertions;
 using Xunit;
 
@@ -17,19 +11,20 @@ public class CancelStaleDeletionProcessTests
     public void Cancel_stale_deletion_process_still_waiting_for_approval()
     {
         // Arrange
-        var activeIdentity = new Identity("", IdentityAddress.Create([], "id1"), [], TierId.Generate(), 1);
-        var deletionProcess = activeIdentity.StartDeletionProcessAsSupport();
+        var identity = new Identity("", IdentityAddress.Create([], "id1"), [], TierId.Generate(), 1);
+        var deletionProcess = identity.StartDeletionProcessAsSupport();
 
         // Act
-        activeIdentity.CancelStaleDeletionProcess(activeIdentity.GetDeletionProcessInStatus(DeletionProcessStatus.WaitingForApproval).Id);
+        identity.CancelStaleDeletionProcess(identity.GetDeletionProcessInStatus(DeletionProcessStatus.WaitingForApproval)!.Id);
 
         // Assert
-        activeIdentity.Status.Should().Be(IdentityStatus.Active);
+        identity.Status.Should().Be(IdentityStatus.Active);
 
-        var deletionProcess1 = activeIdentity.DeletionProcesses.FirstOrDefault(d => d.Status == DeletionProcessStatus.Canceled);
-        deletionProcess1.AuditLog.Should().HaveCount(2);
-        deletionProcess1.AuditLog[1].ProcessId.Should().Be(deletionProcess.Id);
-        deletionProcess1.AuditLog[1].OldStatus.Should().Be(DeletionProcessStatus.WaitingForApproval);
-        deletionProcess1.AuditLog[1].NewStatus.Should().Be(DeletionProcessStatus.Canceled);
+        var canceledDeletionProcess = identity.DeletionProcesses.FirstOrDefault(d => d.Status == DeletionProcessStatus.Canceled)!;
+
+        canceledDeletionProcess.AuditLog.Should().HaveCount(2); // count 2 because the first one was creation of a deletion process
+        canceledDeletionProcess.AuditLog[1].ProcessId.Should().Be(deletionProcess.Id);
+        canceledDeletionProcess.AuditLog[1].OldStatus.Should().Be(DeletionProcessStatus.WaitingForApproval);
+        canceledDeletionProcess.AuditLog[1].NewStatus.Should().Be(DeletionProcessStatus.Canceled);
     }
 }
