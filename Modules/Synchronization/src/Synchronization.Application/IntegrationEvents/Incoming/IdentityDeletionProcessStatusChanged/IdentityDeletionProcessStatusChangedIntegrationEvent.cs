47a76d42ea366c6319feb3bd05640d73f20e7864
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-02-28 16:23:27 +0100

    refactor: improve code structure of IdentityDeletionProcessStatusChangedIntegrationEvent

---
+++ Modules/Synchronization/src/Synchronization.Application/IntegrationEvents/Incoming/IdentityDeletionProcessStatusChanged/IdentityDeletionProcessStatusChangedIntegrationEvent.cs
@@ -0,0 +1,15 @@
+﻿using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus.Events;
+
+namespace Backbone.Modules.Synchronization.Application.IntegrationEvents.Incoming.IdentityDeletionProcessStatusChanged;
+public class IdentityDeletionProcessStatusChangedIntegrationEvent : IntegrationEvent
+{
+    public IdentityDeletionProcessStatusChangedIntegrationEvent(string identityAddress, string deletionProcessId)
+        : base($"{identityAddress}/DeletionProcessStarted/{deletionProcessId}") // unsure about if this is needed
+    {
+        Address = identityAddress;
+        DeletionProcessId = deletionProcessId;
+    }
+
+    public string Address { get; internal set; }
+    public object DeletionProcessId { get; internal set; }
+}
