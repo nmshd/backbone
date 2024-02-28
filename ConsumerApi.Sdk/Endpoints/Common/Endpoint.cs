3e84ad4171f1a85fb024b454d7820795a885e8ac
Mika Herrmann <mika.herrmann@js-soft.com>
2024-02-28 13:33:36 +0100

    Add Abstract Endpoint Superclass

---
+++ ConsumerApi.Sdk/Endpoints/Common/Endpoint.cs
@@ -0,0 +1,11 @@
+﻿namespace Backbone.ConsumerApi.Sdk.Endpoints.Common;
+
+public abstract class Endpoint
+{
+    protected readonly EndpointClient _client;
+
+    protected Endpoint(EndpointClient client)
+    {
+        _client = client;
+    }
+}
