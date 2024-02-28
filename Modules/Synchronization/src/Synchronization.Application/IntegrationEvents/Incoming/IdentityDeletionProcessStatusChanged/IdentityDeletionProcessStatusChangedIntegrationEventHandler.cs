4c410eb327cd094abb970999ca0f5e9fadaa30da
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-02-28 16:22:17 +0100

    fix: set correct external event type when creating external event

---
+++ Modules/Synchronization/src/Synchronization.Application/IntegrationEvents/Incoming/IdentityDeletionProcessStatusChanged/IdentityDeletionProcessStatusChangedIntegrationEventHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
+using Backbone.DevelopmentKit.Identity.ValueObjects;
+using Backbone.Modules.Synchronization.Application.Infrastructure;
+using Backbone.Modules.Synchronization.Application.IntegrationEvents.Outgoing;
+using Backbone.Modules.Synchronization.Domain.Entities.Sync;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Backbone.Modules.Synchronization.Application.IntegrationEvents.Incoming.IdentityDeletionProcessStatusChanged;
+public class IdentityDeletionProcessStatusChangedIntegrationEventHandler : IIntegrationEventHandler<IdentityDeletionProcessStatusChangedIntegrationEvent>
+{
+    private readonly ISynchronizationDbContext _dbContext;
+    private readonly IEventBus _eventBus;
+    private readonly ILogger<IdentityDeletionProcessStatusChangedIntegrationEventHandler> _logger;
+
+    public IdentityDeletionProcessStatusChangedIntegrationEventHandler(ISynchronizationDbContext dbContext, 
+        IEventBus eventBus, 
+        ILogger<IdentityDeletionProcessStatusChangedIntegrationEventHandler> logger)
+    {
+        _dbContext = dbContext;
+        _eventBus = eventBus;
+        _logger = logger;
+    }
+
+    public async Task Handle(IdentityDeletionProcessStatusChangedIntegrationEvent @event)
+    {
+#pragma warning disable IDE0037
+        var payload = new { DeletionProcessId = @event.DeletionProcessId };
+#pragma warning restore IDE0037
+        try
+        {
+            var externalEvent = await _dbContext.CreateExternalEvent(IdentityAddress.Parse(@event.Address), ExternalEventType.IdentityDeletionProcessStatusChanged, payload);
+            _eventBus.Publish(new ExternalEventCreatedIntegrationEvent(externalEvent));
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while processing an integration event.");
+            throw;
+        }
+    }
+}
