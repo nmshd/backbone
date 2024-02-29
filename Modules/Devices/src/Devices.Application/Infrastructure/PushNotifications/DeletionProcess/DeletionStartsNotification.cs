95c81c9c238a054796b858dfb06ee18e65fb7800
Daniel Silva <daniel.p.silva@konkconsulting.com>
2024-02-29 11:01:03 +0000

    chore: remove message from DeletionStartsNotification ctor

--- Modules/Devices/src/Devices.Application/Infrastructure/PushNotifications/DeletionProcess/DeletionStartsNotification.cs
+++ Modules/Devices/src/Devices.Application/Infrastructure/PushNotifications/DeletionProcess/DeletionStartsNotification.cs
@@ -1,9 +1,10 @@
 ﻿using System.Reflection;
+using Backbone.Modules.Devices.Domain.Entities.Identities;
 
 namespace Backbone.Modules.Devices.Application.Infrastructure.PushNotifications.DeletionProcess;
 
 [NotificationText(Title = "Your Identity will be deleted", Body = "")]
 public record DeletionStartsNotification
 {
-    public DeletionStartsNotification(string message) => GetType().GetCustomAttribute<NotificationTextAttribute>().Body = message;
+    public DeletionStartsNotification() => GetType().GetCustomAttribute<NotificationTextAttribute>()!.Body = IdentityDeletionConfiguration.DeletionStartsNotification.Text;
 };
