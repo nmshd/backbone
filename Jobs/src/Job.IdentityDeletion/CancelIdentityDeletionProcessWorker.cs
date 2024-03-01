5a9cad912f052ce0d13657e2256bfc83f7c31604
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-03-01 14:51:59 +0100

    feat: use logger to log ids of canceled identity deletion processes

--- Jobs/src/Job.IdentityDeletion/CancelIdentityDeletionProcessWorker.cs
+++ Jobs/src/Job.IdentityDeletion/CancelIdentityDeletionProcessWorker.cs
@@ -1,10 +1,4 @@
 ﻿using Backbone.Modules.Devices.Application.Identities.Commands.CancelStaleDeletionProcesses;
-using Backbone.Modules.Devices.Application.Identities.Commands.UpdateIdentity;
-using Backbone.Modules.Devices.Application.Infrastructure.PushNotifications;
-//using DeletionStartsNotification = Backbone.Modules.Devices.Application.Infrastructure.PushNotifications.DeletionProcess.DeletionStartsNotification;
-using Backbone.Modules.Devices.Application.Infrastructure.PushNotifications.DeletionProcess;
-using Backbone.Modules.Devices.Application.IntegrationEvents.Outgoing;
-using Backbone.Modules.Devices.Domain.Entities.Identities;
 using MediatR;
 
 namespace Backbone.Job.IdentityDeletion;
@@ -32,7 +26,11 @@ public class CancelIdentityDeletionProcessWorker : IHostedService
 
     private async Task StartProcessing(CancellationToken cancellationToken)
     {
-        await _mediator.Send(new CancelStaleDeletionProcessesCommand(), cancellationToken);
+        var identityDeletionProcessIds = await _mediator.Send(new CancelStaleDeletionProcessesCommand(), cancellationToken);
+
+        var logCanceledDeletionProcessIds = string.Join(", ", identityDeletionProcessIds.CanceledIdentityDeletionPrecessIds);
+
+        _logger.LogInformation("Automatically canceled identity deletion processes: [{logCanceledDeletionProcessIds}]" , logCanceledDeletionProcessIds);
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
