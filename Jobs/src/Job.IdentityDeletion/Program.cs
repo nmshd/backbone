f9f8ddd458135e1150b26efe179715f4b2ca2a00
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-03-01 14:05:43 +0100

    refactor: improve code structure of Program refactor: remove unnecessary lines

--- Jobs/src/Job.IdentityDeletion/Program.cs
+++ Jobs/src/Job.IdentityDeletion/Program.cs
@@ -2,19 +2,7 @@
 using Autofac.Extensions.DependencyInjection;
 using Backbone.BuildingBlocks.API.Extensions;
 using Backbone.BuildingBlocks.Application.QuotaCheck;
-using Backbone.Infrastructure.EventBus;
-using Backbone.Modules.Challenges.ConsumerApi;
-using Backbone.Modules.Devices.ConsumerApi;
-using Backbone.Modules.Devices.Infrastructure.PushNotifications;
-using Backbone.Modules.Files.ConsumerApi;
-using Backbone.Modules.Messages.ConsumerApi;
-using Backbone.Modules.Quotas.ConsumerApi;
-using Backbone.Modules.Relationships.ConsumerApi;
-using Backbone.Modules.Synchronization.ConsumerApi;
-using Backbone.Modules.Tokens.ConsumerApi;
 using FluentValidation.AspNetCore;
-using Microsoft.Extensions.Options;
-using DevicesConfiguration = Backbone.Modules.Devices.ConsumerApi.Configuration;
 
 namespace Backbone.Job.IdentityDeletion
 {
@@ -52,30 +40,12 @@ namespace Backbone.Job.IdentityDeletion
                     var configuration = hostContext.Configuration;
                     services.AddHostedService<CancelIdentityDeletionProcessWorker>();
 
-                    services
-                        .AddModule<DevicesModule>(configuration)
-                        .AddModule<RelationshipsModule>(configuration)
-                        .AddModule<ChallengesModule>(configuration)
-                        .AddModule<FilesModule>(configuration)
-                        .AddModule<MessagesModule>(configuration)
-                        .AddModule<QuotasModule>(configuration)
-                        .AddModule<SynchronizationModule>(configuration)
-                        .AddModule<TokensModule>(configuration);
-
                     services.AddTransient<IQuotaChecker, AlwaysSuccessQuotaChecker>();
                     services.AddFluentValidationAutoValidation(config => { config.DisableDataAnnotationsValidation = true; });
 
                     services.AddCustomIdentity(hostContext.HostingEnvironment);
 
                     services.ConfigureAndValidate<DeletionProcessJobConfiguration>(configuration.Bind);
-
-                    var parsedConfiguration = services.BuildServiceProvider().GetRequiredService<IOptions<DeletionProcessJobConfiguration>>().Value;
-
-                    services.AddEventBus(parsedConfiguration.Infrastructure.EventBus);
-
-                    var devicesConfiguration = new DevicesConfiguration();
-                    configuration.GetSection("Modules:Devices").Bind(devicesConfiguration);
-                    services.AddPushNotifications(devicesConfiguration.Infrastructure.PushNotifications);
                 })
                 .UseServiceProviderFactory(new AutofacServiceProviderFactory());
         }
