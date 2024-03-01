c5dd85109a43fff3adc9f92a8a978de96371caa5
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-03-01 11:59:21 +0100

    feat: create outgoing integration event

---
+++ Modules/Devices/src/Devices.Application/IntegrationEvents/Outgoing/IdentityDeletionProcessStatusChangedIntegrationEvent.cs
@@ -0,0 +1,15 @@
+﻿using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus.Events;
+
+namespace Backbone.Modules.Devices.Application.IntegrationEvents.Outgoing;
+public class IdentityDeletionProcessStatusChangedIntegrationEvent : IntegrationEvent
+{
+    public IdentityDeletionProcessStatusChangedIntegrationEvent(string identityAddress, string deletionProcessId)
+        : base($"{identityAddress}/IdentityDeletionProcessStatusChanged/{deletionProcessId}")
+    {
+        Address = identityAddress;
+        DeletionProcessId = deletionProcessId;
+    }
+
+    public string Address { get; }
+    public object DeletionProcessId { get; }
+}
