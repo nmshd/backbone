7a020596e1013520bdef7ec494e30f1e6475577a
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-03-01 11:31:15 +0100

    refactor: improve code structure of worker

--- Jobs/src/Job.IdentityDeletion/CancelDeletionProcessWorker.cs
+++ Jobs/src/Job.IdentityDeletion/CancelDeletionProcessWorker.cs
@@ -1,34 +1,25 @@
-﻿using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
-using Backbone.BuildingBlocks.Application.PushNotifications;
-using Backbone.Modules.Devices.Application.Identities.Commands.CancelStaleDeletionProcesses;
+﻿using Backbone.Modules.Devices.Application.Identities.Commands.CancelStaleDeletionProcesses;
 using Backbone.Modules.Devices.Application.Identities.Commands.UpdateIdentity;
 using Backbone.Modules.Devices.Application.Infrastructure.PushNotifications;
 //using DeletionStartsNotification = Backbone.Modules.Devices.Application.Infrastructure.PushNotifications.DeletionProcess.DeletionStartsNotification;
 using Backbone.Modules.Devices.Application.Infrastructure.PushNotifications.DeletionProcess;
 using Backbone.Modules.Devices.Application.IntegrationEvents.Outgoing;
 using Backbone.Modules.Devices.Domain.Entities.Identities;
-using Backbone.Modules.Synchronization.Application.IntegrationEvents.Incoming.IdentityDeletionProcessStatusChanged;
 using MediatR;
 
 namespace Backbone.Job.IdentityDeletion;
 public class CancelDeletionProcessWorker : IHostedService
 {
-    private readonly IEventBus _eventBus;
     private readonly IHostApplicationLifetime _host;
     private readonly IMediator _mediator;
-    private readonly IPushNotificationSender _pushNotificationSender;
     private readonly ILogger<CancelDeletionProcessWorker> _logger;
 
     public CancelDeletionProcessWorker(IHostApplicationLifetime host,
         IMediator mediator,
-        IPushNotificationSender pushNotificationSender,
-        IEventBus eventBus,
         ILogger<CancelDeletionProcessWorker> logger)
     {
         _host = host;
         _mediator = mediator;
-        _pushNotificationSender = pushNotificationSender;
-        _eventBus = eventBus;
         _logger = logger;
     }
 
@@ -42,20 +33,10 @@ public class CancelDeletionProcessWorker : IHostedService
     private async Task StartProcessing(CancellationToken cancellationToken)
     {
         await _mediator.Send(new CancelStaleDeletionProcessesCommand(), cancellationToken);
-
-        // this should be moved to handler or should not exist at all
-        //await _pushNotificationSender.SendNotification
-        //    (identity.Address, new DeletionProcessCanceledPushNotification(), cancellationToken);
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
         return Task.CompletedTask;
     }
-}
-
-[NotificationText(Title = "Deletion process canceled", Body = "Your Identity deletion process is canceled due to no response within the approval period")]
-public record DeletionProcessCanceledPushNotification
-{
-    //public DeletionCanceledNotification(string message) => GetType().GetCustomAttribute<NotificationTextAttribute>().Body = message;
 }
