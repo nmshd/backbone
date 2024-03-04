1be35c3c08507347a1949614609bf553e5e41809
Timo Notheisen <timo.notheisen@js-soft.com>
2024-03-04 07:36:03 +0100

    chore: format

--- Modules/Devices/src/Devices.Application/Identities/Queries/GetDeletionProcesses/GetDeletionProcessesResponse.cs
+++ Modules/Devices/src/Devices.Application/Identities/Queries/GetDeletionProcesses/GetDeletionProcessesResponse.cs
@@ -3,8 +3,11 @@ using Backbone.Modules.Devices.Application.DTOs;
 using Backbone.Modules.Devices.Domain.Entities.Identities;
 
 namespace Backbone.Modules.Devices.Application.Identities.Queries.GetDeletionProcesses;
+
 public class GetDeletionProcessesResponse : CollectionResponseBase<IdentityDeletionProcessDTO>
 {
     public GetDeletionProcessesResponse(IEnumerable<IdentityDeletionProcess> processes)
-        : base(processes.Select(p => new IdentityDeletionProcessDTO(p))) {}
+        : base(processes.Select(p => new IdentityDeletionProcessDTO(p)))
+    {
+    }
 }
