47da926dd580b958ef4ab189c95f879378009a49
Julian König <julian.koenig@js-soft.com>
2024-03-04 14:59:42 +0100

    feat: use client in the app

--- AdminUI.new/apps/admin_ui/lib/main.dart
+++ AdminUI.new/apps/admin_ui/lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:admin_api_sdk/admin_api_sdk.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 void main() {
+  final client = AdminApiClient.create(
+    baseUrl: const String.fromEnvironment('BASE_URL'),
+    apiKey: const String.fromEnvironment('API_KEY'),
+  );
+  GetIt.I.registerSingleton(client);
+
   runApp(const MainApp());
 }
 
@@ -9,10 +17,27 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            children: [
+              FutureBuilder(
+                future: GetIt.I.get<AdminApiClient>().clients.getClients(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) return const Text('No data');
+                  return Text('Clients: ${snapshot.data!.data.map((e) => e.displayName).join(', ')}');
+                },
+              ),
+              FutureBuilder(
+                future: GetIt.I.get<AdminApiClient>().tiers.getTiers(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) return const Text('No data');
+                  return Text('Clients: ${snapshot.data!.data.map((e) => e.name).join(', ')}');
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
