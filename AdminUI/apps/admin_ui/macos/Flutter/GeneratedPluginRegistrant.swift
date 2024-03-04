e0594bca7d382aa5d3372ee056e6b689f4103447
Julian König <julian.koenig@js-soft.com>
2024-03-04 22:00:06 +0100

    feat: add shared preferences

--- AdminUI/apps/admin_ui/macos/Flutter/GeneratedPluginRegistrant.swift
+++ AdminUI/apps/admin_ui/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import shared_preferences_foundation
 import window_size
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
 }
