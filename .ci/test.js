f371874bbae61ac3d9d098b6ebd3a0a9fe96550c
Timo Notheisen <timo.notheisen@js-soft.com>
2024-03-05 20:37:58 +0100

    ci: convert test script to sh

--- .ci/test.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-import { $ } from "zx";
-
-await $`dotnet restore "Backbone.sln"`;
-await $`dotnet build  /property:WarningLevel=0 --no-restore "Backbone.sln"`;
-await $`dotnet test --no-restore --no-build --filter "Category!=Integration" --logger "GitHubActions;summary.includePassedTests=true;summary.includeSkippedTests=true" "Backbone.sln"`;
