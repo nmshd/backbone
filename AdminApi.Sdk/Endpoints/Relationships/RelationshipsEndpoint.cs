f13241661b526355068144c3714cecbcd52efec1
Mika Herrmann <mika.herrmann@js-soft.com>
2024-03-05 12:26:40 +0100

    Add Relationships Endpoint

---
+++ AdminApi.Sdk/Endpoints/Relationships/RelationshipsEndpoint.cs
@@ -0,0 +1,11 @@
+﻿using Backbone.AdminApi.Sdk.Endpoints.Common;
+using Backbone.AdminApi.Sdk.Endpoints.Common.Types;
+using Backbone.AdminApi.Sdk.Endpoints.Relationships.Types;
+
+namespace Backbone.AdminApi.Sdk.Endpoints.Relationships;
+
+public class RelationshipsEndpoint(EndpointClient client) : Endpoint(client)
+{
+    public async Task<AdminApiResponse<List<Relationship>>> GetAllRelationships(string participant, PaginationFilter? pagination = null)
+        => await _client.Get<List<Relationship>>("Relationships", null, pagination);
+}
---
+++ AdminApi.Sdk/Endpoints/Relationships/Types/Relationship.cs
@@ -0,0 +1,13 @@
+﻿namespace Backbone.AdminApi.Sdk.Endpoints.Relationships.Types;
+
+public class Relationship
+{
+    public required string Peer { get; set; }
+    public required string RequestedBy { get; set; }
+    public required string TemplateId { get; set; }
+    public required string Status { get; set; }
+    public DateTime CreationDate { get; set; }
+    public DateTime? AnsweredAt { get; set; }
+    public required string CreatedByDevice { get; set; }
+    public string? AnsweredByDevice { get; set; }
+}
