c7fee595dd46e5b6adb05c4846b1fcaa54292392
Timo Notheisen <timo.notheisen@js-soft.com>
2024-03-05 20:06:52 +0100

    ci: use logger in all dotnet test calls

--- .ci/test.js
+++ .ci/test.js
@@ -4,4 +4,4 @@ import { $ } from "zx";
 
 await $`dotnet restore "Backbone.sln"`;
 await $`dotnet build  /property:WarningLevel=0 --no-restore "Backbone.sln"`;
-await $`dotnet test --no-restore --no-build "Backbone.sln" --filter "Category!=Integration"`;
+await $`dotnet test --no-restore --no-build --filter "Category!=Integration" --logger "GitHubActions;summary.includePassedTests=true;summary.includeSkippedTests=true" "Backbone.sln"`;
