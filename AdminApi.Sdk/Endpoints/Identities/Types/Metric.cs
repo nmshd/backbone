7ed50c24fd8fda4a21d46c67524b6f486a741ce1
Mika Herrmann <mika.herrmann@js-soft.com>
2024-03-05 12:26:25 +0100

    Add Metrics Endpoint

---
+++ AdminApi.Sdk/Endpoints/Identities/Types/Metric.cs
@@ -0,0 +1,7 @@
+﻿namespace Backbone.AdminApi.Sdk.Endpoints.Identities.Types;
+
+public class Metric
+{
+    public required string Key { get; set; }
+    public required string DisplayName { get; set; }
+}
---
+++ AdminApi.Sdk/Endpoints/Metrics/MetricsEndpoint.cs
@@ -0,0 +1,10 @@
+﻿using Backbone.AdminApi.Sdk.Endpoints.Common;
+using Backbone.AdminApi.Sdk.Endpoints.Common.Types;
+using Backbone.AdminApi.Sdk.Endpoints.Identities.Types;
+
+namespace Backbone.AdminApi.Sdk.Endpoints.Metrics;
+
+public class MetricsEndpoint(EndpointClient client) : Endpoint(client)
+{
+    public async Task<AdminApiResponse<List<Metric>>> GetAllMetrics() => await _client.Get<List<Metric>>("Metrics");
+}
