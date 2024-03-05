0391c1149c3c88a4a9a1d29f22fea0d388bcac65
Timo Notheisen <timo.notheisen@js-soft.com>
2024-03-05 21:20:23 +0100

    chore: remove redundant backtick

--- .ci/test.sh
+++ .ci/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-dotnet restore "Backbone.sln"`;
+dotnet restore "Backbone.sln"
 dotnet build  /property:WarningLevel=0 --no-restore "Backbone.sln"
 dotnet test --no-restore --no-build --filter "Category!=Integration" --logger "GitHubActions;summary.includePassedTests=true;summary.includeSkippedTests=true" "Backbone.sln"
