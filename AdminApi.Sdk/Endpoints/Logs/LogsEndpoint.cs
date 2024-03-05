9697c925d8bdec7d0ba416f895455fc64d4a9302
Mika Herrmann <mika.herrmann@js-soft.com>
2024-03-05 12:26:13 +0100

    Add Logs Endpoint

---
+++ AdminApi.Sdk/Endpoints/Logs/LogsEndpoint.cs
@@ -0,0 +1,10 @@
+﻿using Backbone.AdminApi.Sdk.Endpoints.Common;
+using Backbone.AdminApi.Sdk.Endpoints.Common.Types;
+using Backbone.AdminApi.Sdk.Endpoints.Logs.Types;
+
+namespace Backbone.AdminApi.Sdk.Endpoints.Logs;
+
+public class LogsEndpoint(EndpointClient client) : Endpoint(client)
+{
+    public async Task<AdminApiResponse<EmptyResponse>> CreateLog(LogRequest request) => await _client.Post<EmptyResponse>("Logs", request);
+}
