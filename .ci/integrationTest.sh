a3034f2c9514ba4af00ce9dccda03a35e01f5850
Mika Herrmann <mika.herrmann@js-soft.com>
2024-03-06 17:01:33 +0100

    wip

---
+++ .ci/integrationTest.sh
@@ -0,0 +1,28 @@
+﻿#!/bin/bash
+
+postgres() {
+    docker compose -f ./.ci/docker-compose.test.yml -f ./.ci/docker-compose.test.postgres.yml "$@"
+}
+
+sqlserver() {
+    docker compose -f ./.ci/docker-compose.test.yml -f ./.ci/docker-compose.test.sqlserver.yml "$@"
+}
+
+buildAndRun() {
+    debugRun dotnet restore "Backbone.sln"
+    debugRun dotnet build --no-restore "Backbone.sln"
+    export CONSUMER_API_BASE_ADDRESS="http://localhost:5000"
+    export ADMIN_API_BASE_ADDRESS="http://localhost:5173"
+    debugRun dotnet test --no-restore --no-build --filter "Category=Integration&TestCategory!~ignore" --logger "GitHubActions;summary.includePassedTests=true;summary.includeSkippedTests=true" "Backbone.sln"
+}
+
+debugRun() {
+    echo
+    echo "---------------------------------------------------------------------------------------"
+    echo
+    echo "$@:"
+    
+    time $@
+}
+
+debugRun $@
