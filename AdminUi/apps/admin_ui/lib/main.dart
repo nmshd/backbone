1c8d2eb283ce2b77ca8fe6d5c24a506f736eb5ad
Julian König <julian.koenig@js-soft.com>
2024-03-06 13:17:31 +0100

    chore: do not copy code from go_router package

--- AdminUi/apps/admin_ui/lib/main.dart
+++ AdminUi/apps/admin_ui/lib/main.dart
@@ -78,21 +78,3 @@ class MainApp extends StatelessWidget {
     );
   }
 }
-
-/// Custom transition page with no transition.
-class NoTransitionPage<T> extends CustomTransitionPage<T> {
-  /// Constructor for a page with no transition functionality.
-  const NoTransitionPage({
-    required super.child,
-    super.name,
-    super.arguments,
-    super.restorationId,
-    super.key,
-  }) : super(
-          transitionsBuilder: _transitionsBuilder,
-          transitionDuration: Duration.zero,
-          reverseTransitionDuration: Duration.zero,
-        );
-
-  static Widget _transitionsBuilder(BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) => child;
-}
