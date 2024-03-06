c3042bad3bba654ab0c6e5834098e8208aa708e9
Timo Notheisen <timo.notheisen@js-soft.com>
2024-03-06 16:12:45 +0100

    refactor: improve Authenticator

--- ConsumerApi.Sdk/Endpoints/Common/Authenticator.cs
+++ ConsumerApi.Sdk/Endpoints/Common/Authenticator.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using System.Net.Http.Json;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -8,78 +9,82 @@ public class Authenticator
 {
     private static readonly JsonSerializerOptions SERIALIZER_OPTIONS = new() { PropertyNameCaseInsensitive = true };
 
-    private readonly Configuration.AuthenticationConfiguration _config;
     private readonly HttpClient _httpClient;
-    private string? _jwt;
-    private DateTime? _expiresAt;
+    private readonly Dictionary<string, string> _jwtRequestData;
+    private Jwt? _jwt;
 
     public Authenticator(Configuration.AuthenticationConfiguration config, HttpClient httpClient)
     {
-        _config = config;
         _httpClient = httpClient;
+
+        _jwtRequestData = new Dictionary<string, string>
+        {
+            { "grant_type", "password" },
+            { "username", config.Username },
+            { "password", config.Password },
+            { "client_id", config.ClientId },
+            { "client_secret", config.ClientSecret }
+        };
     }
 
     public async Task<string> GetJwt()
     {
-        if (_jwt == null || DateTime.Now > _expiresAt - TimeSpan.FromSeconds(30)) // we consider JWTs expired 30 seconds before they actually are
+        if (_jwt == null || _jwt.IsExpired)
         {
             await RefreshToken();
         }
 
-        return _jwt;
+        return _jwt.AccessToken;
     }
 
     [MemberNotNull(nameof(_jwt))]
     private async Task RefreshToken()
     {
-        var form = new Dictionary<string, string>
-        {
-            { "grant_type", "password" },
-            { "username", _config.Username },
-            { "password", _config.Password },
-            { "client_id", _config.ClientId },
-            { "client_secret", _config.ClientSecret }
-        };
-
-        var request = new HttpRequestMessage(HttpMethod.Post, "/connect/token") { Content = new FormUrlEncodedContent(form) };
+        var requestContent = new FormUrlEncodedContent(_jwtRequestData);
+        var request = new HttpRequestMessage(HttpMethod.Post, "/connect/token") { Content = requestContent };
 
 #pragma warning disable CS8774 // This warning ("Member must have a non-null value when exiting") must currently be disabled. (see https://github.com/dotnet/csharplang/discussions/ for details)
         var httpResponse = await _httpClient.SendAsync(request).ConfigureAwait(false);
 #pragma warning restore CS8774
 
 #pragma warning disable CS8774 // This warning ("Member must have a non-null value when exiting") must currently be disabled. (see https://github.com/dotnet/csharplang/discussions/ for details)
-        var responseRawContent = await httpResponse.Content.ReadAsStringAsync();
+        var accessTokenResponse = (await httpResponse.Content.ReadFromJsonAsync<AccessTokenResponse>(SERIALIZER_OPTIONS))!;
 #pragma warning restore CS8774
 
-        var accessTokenResponse = JsonSerializer.Deserialize<AccessTokenResponse>(responseRawContent, SERIALIZER_OPTIONS)!;
-
-        _expiresAt = accessTokenResponse.ExpiresAt;
-        _jwt = accessTokenResponse.AccessToken;
+        _jwt = new Jwt(accessTokenResponse.AccessToken, accessTokenResponse.ExpiresIn);
     }
 
+    // ReSharper disable once ClassNeverInstantiated.Local
     private class AccessTokenResponse
     {
         public AccessTokenResponse(
             string accessToken,
-            string tokenType,
             int expiresIn)
         {
             AccessToken = accessToken;
-            TokenType = tokenType;
             ExpiresIn = expiresIn;
-            ExpiresAt = DateTime.UtcNow.AddSeconds(expiresIn);
         }
 
         [JsonPropertyName("access_token")]
-        public string AccessToken { get; set; }
-
-        [JsonPropertyName("token_type")]
-        public string TokenType { get; set; }
+        public string AccessToken { get; }
 
         [JsonPropertyName("expires_in")]
-        public int ExpiresIn { get; set; }
+        public int ExpiresIn { get; }
+    }
 
-        public DateTime ExpiresAt { get; }
-        public bool IsExpired => (ExpiresAt - DateTime.UtcNow).TotalSeconds <= 15;
+    private class Jwt
+    {
+        private readonly DateTime _expiresAt;
+
+        public Jwt(string accessToken, int expiresIn)
+        {
+            AccessToken = accessToken;
+            _expiresAt = DateTime.Now + TimeSpan.FromSeconds(expiresIn);
+        }
+
+        public string AccessToken { get; set; }
+
+        // we consider JWTs expired 30 seconds before they actually are
+        public bool IsExpired => DateTime.Now > _expiresAt - TimeSpan.FromSeconds(30);
     }
 }
