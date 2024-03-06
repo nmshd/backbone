ed47b99150b067e4f39b202aad412aa4fa30a7b9
Timo Notheisen <65653426+tnotheis@users.noreply.github.com>
2024-03-06 09:12:33 +0100

    Improve `dotnet test` output in pipelines (#550)
    * ci: test
    
    * test: let test fail
    
    * ci: add missing quote
    
    * ci: correct path
    
    * chore: install logger in all test projects
    
    * chore: set default formatter for xml
    
    * ci: use logger in all dotnet test calls
    
    * ci: remove test pipeline
    
    * test: install logger in remaining projects
    
    * test: install logger in remaining projects
    
    * test: remove duplicated references to GitHubActionsTestLogger
    
    * ci: convert test script to sh
    
    * ci: convert checkFormatting script to sh
    
    * ci: fix checkFormatting script name
    
    * test: remove intentionally failing test
    
    * ci: try to not use logger for postgres integration tests
    
    * ci: try to not include passed and skipped tests for sql server integration tests
    
    * ci: make scripts executable
    
    * chore: try to remove everything regarding SpecflowCucumberExporter
    
    * chore: remove redundant backtick
    
    * Revert "ci: try to not include passed and skipped tests for sql server integration tests"
    
    This reverts commit 85dadf7278013fae6feef599b513789dad3989ab.
    
    * Revert "ci: try to not use logger for postgres integration tests"
    
    This reverts commit 5b350c55f377f1969ad1df67c13a359e067eb2fa.
    
    * ci: set DOTNET_CONSOLE_ANSI_COLOR for all dotnet test actions
    
    * ci: use github action to run transport tests
    
    * chore: add "--no-build" flag to `dotnet publish` command in AdminCli's Dockerfile
    
    * chore: remove "/property:WarningLevel=0" from all commands
    
    * ci: let test fail
    
    * chore: remove publish stage from Admin CLI Dockerfile
    
    * chore: remove dotnet build from Admin CLI Dockerfile
    
    * chore: fix compiler warnings
    
    * Revert "ci: let test fail"
    
    This reverts commit b25a1292073bb90b08474c87622f2f5eb85c16ad.
    
    * Revert "ci: use github action to run transport tests"
    
    This reverts commit fae8312a19c9d017c34d0ba8cf7d082d997e7ba5.
    
    * chore: remove unnecessary using directive

---
+++ .ci/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dotnet restore "Backbone.sln"
+dotnet build --no-restore "Backbone.sln"
+dotnet test --no-restore --no-build --filter "Category!=Integration" --logger "GitHubActions;summary.includePassedTests=true;summary.includeSkippedTests=true" "Backbone.sln"
