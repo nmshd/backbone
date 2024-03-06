2c79a2d704b106f728a1fc753275611097b78b5c
Mika Herrmann <mika.herrmann@js-soft.com>
2024-03-06 13:44:51 +0100

    wip

--- .ci/integrationTest.postgres.sh
+++ .ci/integrationTest.postgres.sh
@@ -5,11 +5,20 @@ dockerCompose() {
     docker compose -f ./.ci/docker-compose.test.yml -f ./.ci/docker-compose.test.postgres.yml "$@"
 }
 
-dockerCompose down
-dockerCompose build
-dockerCompose up -d
-dotnet restore "Backbone.sln"
-dotnet build --no-restore "Backbone.sln"
+debugRun() {
+    echo
+    echo "---------------------------------------------------------------------------------------"
+    echo
+    echo "$@:"
+    
+    $@
+}
+
+debugRun dockerCompose down
+debugRun dockerCompose build
+debugRun dockerCompose up -d
+debugRun dotnet restore "Backbone.sln"
+debugRun dotnet build --no-restore "Backbone.sln"
 export CONSUMER_API_BASE_ADDRESS="http://localhost:5000"
 export ADMIN_API_BASE_ADDRESS="http://localhost:5173"
-dotnet test --no-restore --no-build --filter "Category=Integration&TestCategory!~ignore" --logger "GitHubActions;summary.includePassedTests=true;summary.includeSkippedTests=true" "Backbone.sln"
+debugRun dotnet test --no-restore --no-build --filter "Category=Integration&TestCategory!~ignore" --logger "GitHubActions;summary.includePassedTests=true;summary.includeSkippedTests=true" "Backbone.sln"
