ea4e784a2703ccb908fca354cfb336d1676763ff
Mika Herrmann <mika.herrmann@js-soft.com>
2024-03-07 17:03:11 +0100

    Refine CreateFile Request

--- ConsumerApi.Sdk/Endpoints/Files/Types/Requests/CreateFileRequest.cs
+++ ConsumerApi.Sdk/Endpoints/Files/Types/Requests/CreateFileRequest.cs
@@ -1,11 +1,10 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
+﻿using Backbone.Tooling.Extensions;
 
 namespace Backbone.ConsumerApi.Sdk.Endpoints.Files.Types.Requests;
 
 public class CreateFileRequest
 {
-    public required FileData Content { get; set; }
+    public required Stream Content { get; set; }
     public required string Owner { get; set; }
     public required string OwnerSignature { get; set; }
 
@@ -17,46 +16,14 @@ public class CreateFileRequest
 
     public MultipartFormDataContent ToMultipartContent()
     {
-        MultipartFormDataContent content = [];
-
-        if (Content.IsRealFile) content.Add(new StreamContent(Content.ReadStream), "content", Content.FilePath);
-        else content.Add(new StreamContent(Content.ReadStream), "content");
-
-        content.Add(new StringContent(Owner), "owner");
-        content.Add(new StringContent(OwnerSignature), "ownerSignature");
-        content.Add(new StringContent(CipherHash), "cipherHash");
-        content.Add(new StringContent(ExpiresAt.ToString(CultureInfo.InvariantCulture)), "expiresAt"); //TODO: Specify Date
-        content.Add(new StringContent(EncryptedProperties), "encryptedProperties");
-
-        return content;
+        return new MultipartFormDataContent
+        {
+            { new StreamContent(Content), "content" },
+            { new StringContent(Owner), "owner" },
+            { new StringContent(OwnerSignature), "ownerSignature" },
+            { new StringContent(CipherHash), "cipherHash" },
+            { new StringContent(ExpiresAt.ToUniversalString()), "expiresAt" },
+            { new StringContent(EncryptedProperties), "encryptedProperties" }
+        };
     }
-}
-
-public class FileData
-{
-    private readonly string? _filePath;
-    private readonly Stream? _stream;
-
-    public FileData(string filePath)
-    {
-        _filePath = filePath;
-        _stream = null;
-    }
-
-    public FileData(Stream stream)
-    {
-        _filePath = null;
-        _stream = stream;
-    }
-
-    [MemberNotNullWhen(true, nameof(_filePath))]
-    [MemberNotNullWhen(false, nameof(_stream))]
-    public bool IsRealFile => _filePath != null;
-
-    [MemberNotNullWhen(false, nameof(_filePath))]
-    [MemberNotNullWhen(true, nameof(_stream))]
-    public bool IsStream => !IsRealFile;
-
-    public Stream ReadStream => IsStream ? _stream : File.OpenRead(_filePath);
-    public string FilePath => _filePath ?? "";
 }
