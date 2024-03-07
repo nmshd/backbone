f8f51e6fd4a7361a5fdd0d5c879ba0e85467e4fa
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-03-07 15:34:41 +0100

    test: check for edge cases with HasApprovalPeriodExpired

--- Modules/Devices/test/Devices.Domain.Tests/Identities/IdentityDeletionProcessTests.cs
+++ Modules/Devices/test/Devices.Domain.Tests/Identities/IdentityDeletionProcessTests.cs
@@ -1,15 +1,23 @@
 ﻿using Backbone.Tooling;
+using Backbone.Modules.Devices.Domain.Entities.Identities;
 using Xunit;
 using FluentAssertions;
 
 namespace Backbone.Modules.Devices.Domain.Tests.Identities;
 public class IdentityDeletionProcessTests
 {
-    [Fact]
-    public void HasApprovalPeriodExpired_is_set_to_false_for_stale_deletion_process()
+    [Theory]
+    [InlineData("2020-01-01T00:00:00")]
+    [InlineData("2020-01-01T00:00:01")]
+    [InlineData("2020-01-05T00:00:00")]
+    public void HasApprovalPeriodExpired_is_false_for_stale_deletion_process(string time)
     {
         // Arrange
+        SystemTime.Set(DateTime.Parse("2020-01-01T00:00:00"));
         var identity = TestDataGenerator.CreateIdentityWithDeletionProcessWaitingForApproval();
+        SystemTime.Set(DateTime.Parse(time));
+
+        IdentityDeletionConfiguration.MaxApprovalTime = 10;
 
         // Act
 
@@ -17,15 +25,22 @@ public class IdentityDeletionProcessTests
         identity.DeletionProcesses[0].HasApprovalPeriodExpired.Should().BeFalse();
     }
 
-    [Fact]
-    public void HasApprovalPeriodExpired_is_set_to_ture_for_deletion_process_still_in_approval_period()
+    [Theory]
+    [InlineData("2020-01-11T00:00:00")]
+    [InlineData("2020-01-11T00:00:01")]
+    [InlineData("2020-01-21T08:45:15")]
+    [InlineData("2025-01-01T00:00:00")]
+    public void HasApprovalPeriodExpired_is_true_for_deletion_process_still_in_approval_period(string time)
     {
         // Arrange
+        SystemTime.Set(DateTime.Parse("2020-01-01T00:00:00"));
         var identity = TestDataGenerator.CreateIdentityWithDeletionProcessWaitingForApproval();
-        SystemTime.Set(SystemTime.UtcNow.AddDays(11));
+        SystemTime.Set(DateTime.Parse(time));
+
+        IdentityDeletionConfiguration.MaxApprovalTime = 10;
 
         // Act
-        
+
         // Assert
         identity.DeletionProcesses[0].HasApprovalPeriodExpired.Should().BeTrue();
     }
