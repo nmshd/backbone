a8ba7decab3a8887f219336a996621a13f7bcb20
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-03-08 10:15:54 +0100

    refactor: imrpove code structure of test class

--- Modules/Devices/test/Devices.Domain.Tests/Identities/IdentityDeletionProcessTests.cs
+++ Modules/Devices/test/Devices.Domain.Tests/Identities/IdentityDeletionProcessTests.cs
@@ -9,7 +9,7 @@ public class IdentityDeletionProcessTests
     [Theory]
     [InlineData("2020-01-01T00:00:00")]
     [InlineData("2020-01-01T00:00:01")]
-    [InlineData("2020-01-05T00:00:00")]
+    [InlineData("2020-01-09T23:59:59")]
     public void HasApprovalPeriodExpired_is_false_for_stale_deletion_process(string time)
     {
         // Arrange
@@ -27,7 +27,6 @@ public class IdentityDeletionProcessTests
 
     [Theory]
     [InlineData("2020-01-11T00:00:00")]
-    [InlineData("2020-01-11T00:00:01")]
     [InlineData("2020-01-21T08:45:15")]
     [InlineData("2025-01-01T00:00:00")]
     public void HasApprovalPeriodExpired_is_true_for_deletion_process_still_in_approval_period(string time)
