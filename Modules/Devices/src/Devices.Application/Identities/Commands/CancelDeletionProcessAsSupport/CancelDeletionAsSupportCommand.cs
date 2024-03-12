f59263e8b26de11f14a8256ce138542d31cc8a90
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-03-12 11:16:33 +0100

    feat: add cancel deletion process as support handler, command and response

---
+++ Modules/Devices/src/Devices.Application/Identities/Commands/CancelDeletionProcessAsSupport/CancelDeletionAsSupportCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Backbone.Modules.Devices.Application.Identities.Commands.CancelDeletionProcessAsSupport;
+
+public class CancelDeletionAsSupportCommand : IRequest<CancelDeletionAsSupportResponse>
+{
+    public CancelDeletionAsSupportCommand(string identityAddress)
+    {
+        IdentityAddress = identityAddress;
+    }
+
+    public string IdentityAddress { get; set; }
+}
