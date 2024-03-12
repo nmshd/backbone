bec89fdb1e04b69d8c267bf839c6e5c25f0b42f4
Vladimir Vuckovic <vidrak97@gmail.com>
2024-03-12 10:55:19 +0100

    chore: simplify relationship type

--- AdminUi/packages/admin_api_types/lib/src/relationships/relationship.dart
+++ AdminUi/packages/admin_api_types/lib/src/relationships/relationship.dart
@@ -3,38 +3,6 @@ import 'package:json_annotation/json_annotation.dart';
 part 'relationship.g.dart';
 
 @JsonSerializable()
-class PagedHttpResponseEnvelope {
-  final List<Relationship> result;
-  final PaginationData pagination;
-
-  PagedHttpResponseEnvelope({
-    required this.result,
-    required this.pagination,
-  });
-
-  factory PagedHttpResponseEnvelope.fromJson(dynamic json) => _$PagedHttpResponseEnvelopeFromJson(json as Map<String, dynamic>);
-  Map<String, dynamic> toJson() => _$PagedHttpResponseEnvelopeToJson(this);
-}
-
-@JsonSerializable()
-class PaginationData {
-  final int? pageNumber;
-  final int? pageSize;
-  final int? totalPages;
-  final int? totalRecords;
-
-  PaginationData({
-    this.pageNumber,
-    this.pageSize,
-    this.totalPages,
-    this.totalRecords,
-  });
-
-  factory PaginationData.fromJson(dynamic json) => _$PaginationDataFromJson(json as Map<String, dynamic>);
-  Map<String, dynamic> toJson() => _$PaginationDataToJson(this);
-}
-
-@JsonSerializable()
 class Relationship {
   final String peer;
   final String requestedBy;
--- AdminUi/packages/admin_api_types/lib/src/relationships/relationship.g.dart
+++ AdminUi/packages/admin_api_types/lib/src/relationships/relationship.g.dart
@@ -6,39 +6,6 @@ part of 'relationship.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-PagedHttpResponseEnvelope _$PagedHttpResponseEnvelopeFromJson(
-        Map<String, dynamic> json) =>
-    PagedHttpResponseEnvelope(
-      result: (json['result'] as List<dynamic>)
-          .map((e) => Relationship.fromJson(e as Map<String, dynamic>))
-          .toList(),
-      pagination:
-          PaginationData.fromJson(json['pagination'] as Map<String, dynamic>),
-    );
-
-Map<String, dynamic> _$PagedHttpResponseEnvelopeToJson(
-        PagedHttpResponseEnvelope instance) =>
-    <String, dynamic>{
-      'result': instance.result,
-      'pagination': instance.pagination,
-    };
-
-PaginationData _$PaginationDataFromJson(Map<String, dynamic> json) =>
-    PaginationData(
-      pageNumber: json['pageNumber'] as int?,
-      pageSize: json['pageSize'] as int?,
-      totalPages: json['totalPages'] as int?,
-      totalRecords: json['totalRecords'] as int?,
-    );
-
-Map<String, dynamic> _$PaginationDataToJson(PaginationData instance) =>
-    <String, dynamic>{
-      'pageNumber': instance.pageNumber,
-      'pageSize': instance.pageSize,
-      'totalPages': instance.totalPages,
-      'totalRecords': instance.totalRecords,
-    };
-
 Relationship _$RelationshipFromJson(Map<String, dynamic> json) => Relationship(
       peer: json['peer'] as String,
       requestedBy: json['requestedBy'] as String,
@@ -50,8 +17,7 @@ Relationship _$RelationshipFromJson(Map<String, dynamic> json) => Relationship(
       answeredByDevice: json['answeredByDevice'] as String,
     );
 
-Map<String, dynamic> _$RelationshipToJson(Relationship instance) =>
-    <String, dynamic>{
+Map<String, dynamic> _$RelationshipToJson(Relationship instance) => <String, dynamic>{
       'peer': instance.peer,
       'requestedBy': instance.requestedBy,
       'templateId': instance.templateId,
