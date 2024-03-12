d655cdc1143cd34f4b962811940e233aac8210f8
Daniel Silva <daniel.p.silva@konkconsulting.com>
2024-03-12 17:11:17 +0000

    fix: remove unnecessary class

--- Modules/Relationships/test/Relationships.Application.Tests/TestDataGenerator.cs
+++
@@ -1,7 +0,0 @@
-using Backbone.DevelopmentKit.Identity.ValueObjects;
-
-namespace Backbone.Modules.Relationships.Application.Tests;
-
-public class TestDataGenerator : Backbone.UnitTestTools.Data.TestDataGenerator
-{
-}
