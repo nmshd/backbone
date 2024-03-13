fadc187ef47009f83b08fd3f99b4a24be2d74b05
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-03-13 09:16:54 +0100

    feat: add query

---
+++ Modules/Devices/src/Devices.Application/Identities/Queries/GetDeletionProcessAsSupport/GetDeletionProcessAsSupportQuery.cs
@@ -0,0 +1,15 @@
+﻿using Backbone.Modules.Devices.Application.DTOs;
+using MediatR;
+
+namespace Backbone.Modules.Devices.Application.Identities.Queries.GetDeletionProcessAsSupport;
+public class GetDeletionProcessAsSupportQuery: IRequest<IdentityDeletionProcessDTO>
+{
+    public GetDeletionProcessAsSupportQuery(string address, string id)
+    {
+        Address = address;
+        Id = id;
+    }
+
+    public string Address { get; set; }
+    public string Id { get; set; }
+}
