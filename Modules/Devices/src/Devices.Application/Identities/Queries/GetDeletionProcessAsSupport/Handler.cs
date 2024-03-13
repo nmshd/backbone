1ec48cdeb5f670c60dd39c940fbc60a1722239a1
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-03-13 09:17:06 +0100

    feat: add handler

---
+++ Modules/Devices/src/Devices.Application/Identities/Queries/GetDeletionProcessAsSupport/Handler.cs
@@ -0,0 +1,25 @@
+﻿using Backbone.BuildingBlocks.Application.Abstractions.Exceptions;
+using Backbone.Modules.Devices.Application.DTOs;
+using Backbone.Modules.Devices.Application.Infrastructure.Persistence.Repository;
+using Backbone.Modules.Devices.Domain.Entities.Identities;
+using MediatR;
+
+namespace Backbone.Modules.Devices.Application.Identities.Queries.GetDeletionProcessAsSupport;
+public class Handler : IRequestHandler<GetDeletionProcessAsSupportQuery, IdentityDeletionProcessDTO>
+{
+    private readonly IIdentitiesRepository _identitiesRepository;
+
+    public Handler(IIdentitiesRepository identitiesRepository)
+    {
+        _identitiesRepository = identitiesRepository;
+    }
+
+    public async Task<IdentityDeletionProcessDTO> Handle(GetDeletionProcessAsSupportQuery request, CancellationToken cancellationToken)
+    {
+        var identity = await _identitiesRepository.FindByAddress(request.Address, cancellationToken) ?? throw new NotFoundException(nameof(Identity));
+        var deletionProcess = identity.DeletionProcesses.FirstOrDefault(p => p.Id == request.Id) ?? throw new NotFoundException(nameof(IdentityDeletionProcess));
+        var response = new IdentityDeletionProcessDTO(deletionProcess);
+
+        return response;
+    }
+}
