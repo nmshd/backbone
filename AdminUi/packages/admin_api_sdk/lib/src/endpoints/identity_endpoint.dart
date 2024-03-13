4fb84e7bd6d6d90de84f7ff8c3365efd9109b88c
Vladimir Vuckovic <vidrak97@gmail.com>
2024-03-13 09:16:58 +0100

    chore: add filtering for odata

--- AdminUi/packages/admin_api_sdk/lib/src/endpoints/identity_endpoint.dart
+++ AdminUi/packages/admin_api_sdk/lib/src/endpoints/identity_endpoint.dart
@@ -27,17 +27,91 @@ class IdentitiesEndpoint extends Endpoint {
       );
 
   Future<ApiResponse<List<IdentityOverview>>> getIdentities({
+    required IdentityOverviewFilter filter,
     int pageNumber = 0,
     int pageSize = 10,
   }) =>
       getOData(
         '/odata/Identities',
         query: {
-          r'$count': 'true',
-          r'$expand': 'Tier',
+          r'$filter': buildFilter(filter: filter),
           r'$top': '$pageSize',
           r'$skip': '${pageNumber * pageSize}',
+          r'$count': 'true',
+          r'$expand': 'Tier',
         },
         transformer: (e) => (e as List).map(IdentityOverview.fromJson).toList(),
       );
+
+  String buildFilter({IdentityOverviewFilter? filter}) {
+    final filterBuilder = ODataFilterBuilder();
+
+    if (filter?.address != null) {
+      filterBuilder.addFilter("contains(address, '${filter!.address}')");
+    }
+
+    if (filter?.tiers != null) {
+      final tiersFilter = filter!.tiers!.map((tier) => "tier/Id eq '$tier'").join(' or ');
+      filterBuilder.addFilter('($tiersFilter)');
+    }
+
+    if (filter?.clients != null) {
+      final clientsFilter = filter!.clients!.map((client) => "createdWithClient eq '$client'").join(' or ');
+      filterBuilder.addFilter('($clientsFilter)');
+    }
+
+    if (filter?.createdAt != null) {
+      filterBuilder.addFilter('createdAt ${getOperatorString(filter!.createdAt!.operator)} ${filter.createdAt!.value.substring(0, 10)}');
+    }
+
+    if (filter?.lastLoginAt != null) {
+      filterBuilder.addFilter('lastLoginAt ${getOperatorString(filter!.lastLoginAt!.operator)} ${filter.lastLoginAt!.value.substring(0, 10)}');
+    }
+
+    if (filter?.numberOfDevices != null) {
+      filterBuilder.addFilter('numberOfDevices ${getOperatorString(filter!.numberOfDevices!.operator)} ${filter.numberOfDevices!.value}');
+    }
+
+    if (filter?.datawalletVersion != null) {
+      filterBuilder.addFilter('datawalletVersion ${getOperatorString(filter!.datawalletVersion!.operator)} ${filter.datawalletVersion!.value}');
+    }
+
+    if (filter?.identityVersion != null) {
+      filterBuilder.addFilter('identityVersion ${getOperatorString(filter!.identityVersion!.operator)} ${filter.identityVersion!.value}');
+    }
+
+    return filterBuilder.buildFilter();
+  }
+}
+
+class ODataFilterBuilder {
+  String _filter = '';
+
+  void addFilter(String filter) {
+    if (_filter.isNotEmpty) {
+      _filter += ' and ';
+    }
+    _filter += '($filter)';
+  }
+
+  String buildFilter() {
+    return _filter.isNotEmpty ? _filter : '';
+  }
+}
+
+String getOperatorString(FilterOperator operator) {
+  switch (operator) {
+    case FilterOperator.equal:
+      return 'eq';
+    case FilterOperator.notEqual:
+      return 'ne';
+    case FilterOperator.greaterThan:
+      return 'gt';
+    case FilterOperator.greaterThanOrEqual:
+      return 'ge';
+    case FilterOperator.lessThan:
+      return 'lt';
+    case FilterOperator.lessThanOrEqual:
+      return 'le';
+  }
 }
---
+++ AdminUi/packages/admin_api_sdk/lib/src/types/identity_overview_filter.dart
@@ -0,0 +1,37 @@
+class IdentityOverviewFilter {
+  final String? address;
+  final List<String>? tiers;
+  final List<String>? clients;
+  final FilterOperatorValue? createdAt;
+  final FilterOperatorValue? lastLoginAt;
+  final FilterOperatorValue? numberOfDevices;
+  final FilterOperatorValue? datawalletVersion;
+  final FilterOperatorValue? identityVersion;
+
+  IdentityOverviewFilter({
+    this.address,
+    this.tiers,
+    this.clients,
+    this.createdAt,
+    this.lastLoginAt,
+    this.numberOfDevices,
+    this.datawalletVersion,
+    this.identityVersion,
+  });
+}
+
+enum FilterOperator {
+  equal,
+  notEqual,
+  greaterThan,
+  greaterThanOrEqual,
+  lessThan,
+  lessThanOrEqual,
+}
+
+class FilterOperatorValue {
+  final FilterOperator operator;
+  final String value;
+
+  FilterOperatorValue(this.operator, this.value);
+}
