e799890da76ed65a78f3f56fa711fafcb25bd331
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-03-13 15:23:56 +0100

    feat: add migration

--- Modules/Devices/src/Devices.Infrastructure.Database.Postgres/Migrations/20240313141732_AddNewTierProperties.Designer.cs
+++ Modules/Devices/src/Devices.Infrastructure.Database.Postgres/Migrations/20240313141732_AddNewTierProperties.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Backbone.Modules.Devices.Infrastructure.Persistence.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace Devices.Infrastructure.Database.Postgres.Migrations
+namespace Backbone.Modules.Devices.Infrastructure.Database.Postgres.Migrations
 {
     [DbContext(typeof(DevicesDbContext))]
-    partial class DevicesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240313141732_AddNewTierProperties")]
+    partial class AddNewTierProperties
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Modules/Devices/src/Devices.Infrastructure.Database.SqlServer/Migrations/20240313141729_AddNewTierProperties.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backbone.Modules.Devices.Infrastructure.Database.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewTierProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "CanBeManuallyAssigned",
+                table: "Tiers",
+                type: "bit",
+                nullable: false,
+                defaultValue: true);
+
+            migrationBuilder.UpdateData("Tiers", "Id", "TIR00000000000000001", "CanBeManuallyAssigned", false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CanBeUsedAsDefaultForUser",
+                table: "Tiers",
+                type: "bit",
+                nullable: false,
+                defaultValue: true);
+
+            migrationBuilder.UpdateData("Tiers", "Id", "TIR00000000000000001", "CanBeUsedAsDefaultForUser", false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CanBeManuallyAssigned",
+                table: "Tiers");
+
+            migrationBuilder.DropColumn(
+                name: "CanBeUsedAsDefaultForUser",
+                table: "Tiers");
+        }
+    }
+}
