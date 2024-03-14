3e070abfcb66cd6e4e1ad9db555d8f918a8faf41
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-03-14 13:08:08 +0100

    test: fix test names refactor: minor improvements

--- Modules/Devices/test/Devices.Domain.Tests/Identities/IdentityDeletionProcessTests.cs
+++ Modules/Devices/test/Devices.Domain.Tests/Identities/IdentityDeletionProcessTests.cs
@@ -11,7 +11,7 @@ public class IdentityDeletionProcessTests
     [InlineData("2020-01-01T00:00:00")]
     [InlineData("2020-01-01T00:00:01")]
     [InlineData("2020-01-09T23:59:59")]
-    public void HasApprovalPeriodExpired_is_false_for_stale_deletion_process(string utcNow)
+    public void HasApprovalPeriodExpired_is_false_for_deletion_process_still_in_approval_period(string utcNow)
     {
         // Arrange
         SystemTime.Set("2020-01-01T00:00:00");
@@ -31,12 +31,12 @@ public class IdentityDeletionProcessTests
     [InlineData("2020-01-11T00:00:00")]
     [InlineData("2020-01-21T08:45:15")]
     [InlineData("2025-01-01T00:00:00")]
-    public void HasApprovalPeriodExpired_is_true_for_deletion_process_still_in_approval_period(string time)
+    public void HasApprovalPeriodExpired_is_true_for_stale_deletion_process(string utcNow)
     {
         // Arrange
-        SystemTime.Set(DateTime.Parse("2020-01-01T00:00:00"));
+        SystemTime.Set("2020-01-01T00:00:00");
         var identity = TestDataGenerator.CreateIdentityWithDeletionProcessWaitingForApproval();
-        SystemTime.Set(DateTime.Parse(time));
+        SystemTime.Set(utcNow);
 
         IdentityDeletionConfiguration.MaxApprovalTime = 10;
 
