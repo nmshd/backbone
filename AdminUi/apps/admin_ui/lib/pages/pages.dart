c840c4d5cf4d40472f27412b7a0ec1e2176a209c
Julian König <33655937+jkoenig134@users.noreply.github.com>
2024-03-15 11:52:15 +0100

    Admin UI: Initialize flutter setup (#547)
    * chore: update settings.json with dart
    
    * chore: add readme
    
    * fix: git-un-ignore adminui packages
    
    * chore: add initial flutter setup
    
    * feat: add adminui boilerplate
    
    * feat: add types
    
    * feat: add sdk
    
    * feat: use client in the app
    
    * chore: move AdminUI to AdminUI folder
    
    * fix: allow internet access for the mac app
    
    * chore: add setup for configs
    
    * feat: async creation of client
    
    * chore: update mac icons
    
    * chore: change name from admin_ui to AdminUI
    
    * feat: add window_size plugin
    
    * fix: use empty baseurl for web
    
    * feat: add shared preferences
    
    * feat: add routing
    
    * feat: add packages
    
    * chore: move AdminUI to AdminUi
    
    * chore: add packages
    
    * chore: move stuff
    
    * chore: ignore again
    
    * chore: simplify logout
    
    * chore: make more readable
    
    * fix: check for api key validity
    
    * chore: do not copy code from go_router package
    
    * fix: use enmeshed logo as svg
    
    * chore: update default window size on macos
    
    * chore: yeet unused launch config
    
    * chore: update extensions
    
    * fix: obscure text in pw field
    
    * chore: add tier get and delete endpoints
    
    * feat: add quota types and endpoints
    
    * feat: add quota endpoint
    
    * chore format
    
    * wip
    
    * chore: wip
    
    * chore: update identity
    
    * chore: update, change method name and type
    
    * chore: remove unnecessary imports
    
    * feat: add relationships type and endpoint
    
    * ci: move runAdminUiChecks to aui folder and rename it to runChecks
    
    * ci: add and execute runFlutterChecks script
    
    * ci: rename flutter checks jobs
    
    * ci: use correct script folder and name
    
    * chore: simplify relationship type
    
    * chore: finalize identities odata
    
    * chore: add separate get and make result for odata
    
    * chore: comment now follows flutter style to-do
    
    * chore: simplify relationship endpoint
    
    * chore: show the results of getting the data
    
    * ci: trigger pipelines
    
    * chore: add some basic pagination
    
    * chore: add filtering for odata
    
    * chore: add filtering example
    
    * chore: export identity overview filter
    
    * fix: syntax
    
    * chore: bump melos
    
    * chore: use builtin melos analyze command
    
    * refactor: identities
    
    * chore: add types to dependencies
    
    * refactor: move enum
    
    * chore: rename parameters
    
    * feat: add identity overview
    
    * chore: display only identity overview
    
    * chore: add trailing comma and add named parameter
    
    * refactor: use port 8082 for local testing
    
    * chore: remove UI code
    
    * fix: make pagination usable
    
    * chore: remove comment
    
    * fix: early return
    
    * refactor: make pretty
    
    * chore: remove unused file
    
    ---------
    
    Co-authored-by: Vladimir Vuckovic <vidrak97@gmail.com>
    Co-authored-by: Timo Notheisen <timo.notheisen@js-soft.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ AdminUi/apps/admin_ui/lib/pages/pages.dart
@@ -0,0 +1,3 @@
+export 'home_page.dart';
+export 'login_page.dart';
+export 'splash_page.dart';
---
+++ AdminUi/apps/admin_ui/lib/pages/splash_page.dart
@@ -0,0 +1,64 @@
+import 'package:admin_api_sdk/admin_api_sdk.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get_it/get_it.dart';
+import 'package:go_router/go_router.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    route();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SvgPicture.asset('assets/logo.svg', width: 200, height: 200),
+            const SizedBox(height: 40),
+            const SizedBox(width: 300, child: LinearProgressIndicator()),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> route() async {
+    await GetIt.I.allReady();
+    final sp = await SharedPreferences.getInstance();
+
+    if (!sp.containsKey('api_key')) {
+      if (mounted) context.go('/login');
+
+      return;
+    }
+
+    final apiKey = sp.getString('api_key')!;
+    const baseUrl = kIsWeb ? '' : String.fromEnvironment('base_url');
+
+    final isValid = await AdminApiClient.validateApiKey(baseUrl: baseUrl, apiKey: apiKey);
+    if (!isValid) {
+      await sp.remove('api_key');
+      if (mounted) context.go('/login');
+
+      return;
+    }
+
+    GetIt.I.registerSingleton(await AdminApiClient.create(baseUrl: baseUrl, apiKey: apiKey));
+    if (mounted) context.go('/dashboard');
+  }
+}
