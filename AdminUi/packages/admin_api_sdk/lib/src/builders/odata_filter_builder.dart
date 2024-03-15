722629ef7dbc3810d40cf03cb8b0c26f8dc898aa
Julian König <julian.koenig@js-soft.com>
2024-03-15 09:53:11 +0100

    fix: early return

--- AdminUi/packages/admin_api_sdk/lib/src/builders/odata_filter_builder.dart
+++ AdminUi/packages/admin_api_sdk/lib/src/builders/odata_filter_builder.dart
@@ -17,38 +17,42 @@ class ODataFilterBuilder {
   String buildOdataFilter(IdentityOverviewFilter? filter) {
     final filterBuilder = ODataFilterBuilder();
 
-    if (filter?.address != null) {
-      filterBuilder.addFilter("contains(address, '${filter!.address}')");
+    if (filter == null) {
+      return filterBuilder.buildFilter();
     }
 
-    if (filter?.tiers != null) {
-      final tiersFilter = filter!.tiers!.map((tier) => "tier/Id eq '$tier'").join(' or ');
+    if (filter.address != null) {
+      filterBuilder.addFilter("contains(address, '${filter.address}')");
+    }
+
+    if (filter.tiers != null) {
+      final tiersFilter = filter.tiers!.map((tier) => "tier/Id eq '$tier'").join(' or ');
       filterBuilder.addFilter('($tiersFilter)');
     }
 
-    if (filter?.clients != null) {
-      final clientsFilter = filter!.clients!.map((client) => "createdWithClient eq '$client'").join(' or ');
+    if (filter.clients != null) {
+      final clientsFilter = filter.clients!.map((client) => "createdWithClient eq '$client'").join(' or ');
       filterBuilder.addFilter('($clientsFilter)');
     }
 
-    if (filter?.createdAt != null) {
-      filterBuilder.addFilter('createdAt ${getOperatorString(filter!.createdAt!.operator)} ${filter.createdAt!.value.substring(0, 10)}');
+    if (filter.createdAt != null) {
+      filterBuilder.addFilter('createdAt ${getOperatorString(filter.createdAt!.operator)} ${filter.createdAt!.value.substring(0, 10)}');
     }
 
-    if (filter?.lastLoginAt != null) {
-      filterBuilder.addFilter('lastLoginAt ${getOperatorString(filter!.lastLoginAt!.operator)} ${filter.lastLoginAt!.value.substring(0, 10)}');
+    if (filter.lastLoginAt != null) {
+      filterBuilder.addFilter('lastLoginAt ${getOperatorString(filter.lastLoginAt!.operator)} ${filter.lastLoginAt!.value.substring(0, 10)}');
     }
 
-    if (filter?.numberOfDevices != null) {
-      filterBuilder.addFilter('numberOfDevices ${getOperatorString(filter!.numberOfDevices!.operator)} ${filter.numberOfDevices!.value}');
+    if (filter.numberOfDevices != null) {
+      filterBuilder.addFilter('numberOfDevices ${getOperatorString(filter.numberOfDevices!.operator)} ${filter.numberOfDevices!.value}');
     }
 
-    if (filter?.datawalletVersion != null) {
-      filterBuilder.addFilter('datawalletVersion ${getOperatorString(filter!.datawalletVersion!.operator)} ${filter.datawalletVersion!.value}');
+    if (filter.datawalletVersion != null) {
+      filterBuilder.addFilter('datawalletVersion ${getOperatorString(filter.datawalletVersion!.operator)} ${filter.datawalletVersion!.value}');
     }
 
-    if (filter?.identityVersion != null) {
-      filterBuilder.addFilter('identityVersion ${getOperatorString(filter!.identityVersion!.operator)} ${filter.identityVersion!.value}');
+    if (filter.identityVersion != null) {
+      filterBuilder.addFilter('identityVersion ${getOperatorString(filter.identityVersion!.operator)} ${filter.identityVersion!.value}');
     }
 
     return filterBuilder.buildFilter();
