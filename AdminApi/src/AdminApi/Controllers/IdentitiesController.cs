f81f77a0c869c16245c58bc1bd6fb6e8e3f67789
Timo Notheisen <timo.notheisen@js-soft.com>
2024-03-15 07:54:24 +0100

    refactor: rename UpdateIdentityTierRequest to UpdateIdentityRequest

--- AdminApi/src/AdminApi/Controllers/IdentitiesController.cs
+++ AdminApi/src/AdminApi/Controllers/IdentitiesController.cs
@@ -73,7 +73,7 @@ public class IdentitiesController : ApiControllerBase
     [HttpPut("{identityAddress}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesError(StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> UpdateIdentity([FromRoute] string identityAddress, [FromBody] UpdateIdentityTierRequest request, CancellationToken cancellationToken)
+    public async Task<IActionResult> UpdateIdentity([FromRoute] string identityAddress, [FromBody] UpdateIdentityRequest request, CancellationToken cancellationToken)
     {
         var command = new UpdateIdentityCommand() { Address = identityAddress, TierId = request.TierId };
         await _mediator.Send(command, cancellationToken);
@@ -132,7 +132,7 @@ public class CreateQuotaForIdentityRequest
     public required QuotaPeriod Period { get; set; }
 }
 
-public class UpdateIdentityTierRequest
+public class UpdateIdentityRequest
 {
     public required string TierId { get; set; }
 }
