77a178dcb3e9bf37b91a410f18c0bdc3cfdcb2e4
Mika Herrmann <mika.herrmann@js-soft.com>
2024-03-15 16:15:37 +0100

    Add common renamed types

---
+++ BuildingBlocks/src/BuildingBlocks.SDK/Endpoints/Common/Types/EnumerableResponseBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+namespace Backbone.BuildingBlocks.SDK.Endpoints.Common.Types;
+
+public abstract class EnumerableResponseBase<TItem> : IEnumerable<TItem>
+{
+    private readonly IEnumerable<TItem> _items;
+
+    protected EnumerableResponseBase(IEnumerable<TItem> items)
+    {
+        _items = items;
+    }
+
+    public IEnumerator<TItem> GetEnumerator()
+    {
+        return _items.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return _items.GetEnumerator();
+    }
+}
