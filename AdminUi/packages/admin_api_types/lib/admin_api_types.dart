f8dae37301fe50894f1b3950b50bf207953d27e0
Julian König <julian.koenig@js-soft.com>
2024-03-15 09:49:01 +0100

    chore: remove comment

--- AdminUi/packages/admin_api_types/lib/admin_api_types.dart
+++ AdminUi/packages/admin_api_types/lib/admin_api_types.dart
@@ -1,8 +1,1 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library;
-
 export 'src/admin_api_types_base.dart';
-
-// TODO(stamenione): Export any libraries intended for clients of this package .
