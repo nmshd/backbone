a8b19ccf7fd7893b759849c03b59134d2096fb60
Mika Herrmann <mika.herrmann@js-soft.com>
2024-03-19 16:52:03 +0100

    Fix Create Tier Request

--- AdminApi.Sdk/Endpoints/Relationships/RelationshipsEndpoint.cs
+++ AdminApi.Sdk/Endpoints/Relationships/RelationshipsEndpoint.cs
@@ -6,6 +6,13 @@ namespace Backbone.AdminApi.Sdk.Endpoints.Relationships;
 
 public class RelationshipsEndpoint(EndpointClient client) : Endpoint(client)
 {
-    public async Task<AdminApiResponse<ListRelationshipsResponse>> GetAllRelationships(string participant, PaginationFilter? pagination = null)
+    public async Task<AdminApiResponse<ListRelationshipsResponse>> GetAllRelationships(PaginationFilter? pagination = null)
         => await _client.Get<ListRelationshipsResponse>("Relationships", null, pagination);
+
+    public async Task<AdminApiResponse<ListRelationshipsResponse>> GetAllRelationships(string participant, PaginationFilter? pagination = null) => await _client
+        .Request<ListRelationshipsResponse>(HttpMethod.Get, "Relationships")
+        .Authenticate()
+        .WithPagination(pagination)
+        .AddQueryParameter("participant", participant)
+        .Execute();
 }
