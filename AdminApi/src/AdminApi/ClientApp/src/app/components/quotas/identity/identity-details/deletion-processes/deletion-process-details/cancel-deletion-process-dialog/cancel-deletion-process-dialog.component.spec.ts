5dc44ff6d2fe899941432f75d47503caa0048636
Vladimir Vuckovic <vidrak97@gmail.com>
2024-03-19 13:46:05 +0100

    chore: prettier

--- AdminApi/src/AdminApi/ClientApp/src/app/components/quotas/identity/identity-details/deletion-processes/deletion-process-details/cancel-deletion-process-dialog/cancel-deletion-process-dialog.component.spec.ts
+++ AdminApi/src/AdminApi/ClientApp/src/app/components/quotas/identity/identity-details/deletion-processes/deletion-process-details/cancel-deletion-process-dialog/cancel-deletion-process-dialog.component.spec.ts
@@ -2,11 +2,11 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { CancelDeletionProcessDialogComponent } from "./cancel-deletion-process-dialog.component";
 
-describe("CancelDeletionProcessDialogComponent", function() {
+describe("CancelDeletionProcessDialogComponent", function () {
     let component: CancelDeletionProcessDialogComponent;
     let fixture: ComponentFixture<CancelDeletionProcessDialogComponent>;
 
-    beforeEach(async function() {
+    beforeEach(async function () {
         await TestBed.configureTestingModule({
             imports: [CancelDeletionProcessDialogComponent]
         }).compileComponents();
@@ -16,7 +16,7 @@ describe("CancelDeletionProcessDialogComponent", function() {
         fixture.detectChanges();
     });
 
-    it("should create", async function() {
+    it("should create", async function () {
         await expect(component).toBeTruthy();
     });
 });
