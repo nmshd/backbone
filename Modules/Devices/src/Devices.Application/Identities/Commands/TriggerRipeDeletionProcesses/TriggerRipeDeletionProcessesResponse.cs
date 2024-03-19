45208c179ed3f146ecad220b4f60d6fc97f81290
Timo Notheisen <timo.notheisen@js-soft.com>
2024-03-19 11:20:20 +0100

    refactor: improve TriggerRipeDeletionProcessesResponse

--- Modules/Devices/src/Devices.Application/Identities/Commands/TriggerRipeDeletionProcesses/TriggerRipeDeletionProcessesResponse.cs
+++ Modules/Devices/src/Devices.Application/Identities/Commands/TriggerRipeDeletionProcesses/TriggerRipeDeletionProcessesResponse.cs
@@ -5,10 +5,25 @@ using CSharpFunctionalExtensions;
 namespace Backbone.Modules.Devices.Application.Identities.Commands.TriggerRipeDeletionProcesses;
 public class TriggerRipeDeletionProcessesResponse
 {
+    public TriggerRipeDeletionProcessesResponse()
+    {
+        Results = new Dictionary<IdentityAddress, UnitResult<DomainError>>();
+    }
+
     public TriggerRipeDeletionProcessesResponse(Dictionary<IdentityAddress, UnitResult<DomainError>> results)
     {
         Results = results;
     }
 
     public Dictionary<IdentityAddress, UnitResult<DomainError>> Results { get; }
+
+    public void AddSuccess(IdentityAddress address)
+    {
+        Results.Add(address, UnitResult.Success<DomainError>());
+    }
+
+    public void AddError(IdentityAddress address, DomainError error)
+    {
+        Results.Add(address, UnitResult.Failure(error));
+    }
 }
