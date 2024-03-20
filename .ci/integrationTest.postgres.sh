1c8ca6152fbf403ed07950dd5e3dfa906ca2dcbd
Mika Herrmann <mika.herrmann@js-soft.com>
2024-03-20 14:16:29 +0100

    test: add env variable

--- .ci/integrationTest.postgres.sh
+++ .ci/integrationTest.postgres.sh
@@ -2,7 +2,7 @@
 set -e
 
 dockerCompose() {
-    docker compose -f ./.ci/docker-compose.test.yml -f ./.ci/docker-compose.test.postgres.yml "$@"
+    DOCKER_BUILDKIT=1 COMPOSE_DOCKER_CLI_BUILD=1 docker compose -f ./.ci/docker-compose.test.yml -f ./.ci/docker-compose.test.postgres.yml "$@"
 }
 
 debugRun() {
