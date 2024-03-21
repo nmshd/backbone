2c2d2e2836a20c7886675f8348db89c8c3aea3d2
Timo Notheisen <timo.notheisen@js-soft.com>
2024-03-21 08:30:53 +0100

    test: split relationship tests into multiple files

---
+++ Modules/Relationships/test/Relationships.Domain.Tests/Tests/Aggregates/Relationships/CreateRelationshipTests.cs
@@ -0,0 +1,81 @@
+using Backbone.BuildingBlocks.Domain;
+using Backbone.Modules.Relationships.Domain.Aggregates.Relationships;
+using Backbone.Modules.Relationships.Domain.Tests.Extensions;
+using Backbone.Tooling;
+using FluentAssertions;
+using Xunit;
+using static Backbone.Modules.Relationships.Domain.Tests.TestHelpers.TestData;
+
+namespace Backbone.Modules.Relationships.Domain.Tests.Tests.Aggregates.Relationships;
+
+public class CreateRelationshipTests
+{
+    [Fact]
+    public void New_Relationship_Has_Correct_Data()
+    {
+        // Arrange
+        SystemTime.Set("2000-01-01");
+
+        // Act
+        var relationship = new Relationship(RELATIONSHIP_TEMPLATE_OF_2, IDENTITY_1, DEVICE_1, [0, 1, 2], []);
+
+        // Assert
+        relationship.Id.Should().NotBeNull();
+        relationship.From.Should().Be(IDENTITY_1);
+        relationship.To.Should().Be(IDENTITY_2);
+        relationship.Status.Should().Be(RelationshipStatus.Pending);
+        relationship.RelationshipTemplateId.Should().Be(RELATIONSHIP_TEMPLATE_OF_2.Id);
+        relationship.RelationshipTemplate.Should().Be(RELATIONSHIP_TEMPLATE_OF_2);
+        relationship.CreatedAt.Should().Be(DateTime.Parse("2000-01-01"));
+        relationship.CreationContent.Should().Equal([0, 1, 2]);
+    }
+
+    [Fact]
+    public void New_relationship_has_AuditLogEntry()
+    {
+        // Arrange
+        SystemTime.Set("2000-01-01");
+
+        // Act
+        var relationship = new Relationship(RELATIONSHIP_TEMPLATE_OF_2, IDENTITY_1, DEVICE_1, null, []);
+
+        // Assert
+        relationship.AuditLog.Should().HaveCount(1);
+
+        var auditLogEntry = relationship.AuditLog.First();
+
+        auditLogEntry.Id.Should().NotBeNull();
+        auditLogEntry.Reason.Should().Be(RelationshipAuditLogEntryReason.Creation);
+        auditLogEntry.OldStatus.Should().Be(null);
+        auditLogEntry.NewStatus.Should().Be(RelationshipStatus.Pending);
+        auditLogEntry.CreatedBy.Should().Be(IDENTITY_1);
+        auditLogEntry.CreatedByDevice.Should().Be(DEVICE_1);
+        auditLogEntry.CreatedAt.Should().Be(DateTime.Parse("2000-01-01"));
+    }
+
+    [Fact]
+    public void Cannot_create_Relationship_to_self()
+    {
+        // Act
+        var acting = () => new Relationship(RELATIONSHIP_TEMPLATE_OF_1, IDENTITY_1, DEVICE_1, null, []);
+
+        // Assert
+        acting.Should().Throw<DomainException>().WithError("error.platform.validation.relationshipRequest.cannotSendRelationshipRequestToYourself");
+    }
+
+    [Fact]
+    public void Cannot_create_multiple_relationships_between_two_identities()
+    {
+        // Arrange
+        var existingRelationships = new List<Relationship>
+        {
+            CreateActiveRelationship()
+        };
+
+        // Act
+        var acting = () => new Relationship(RELATIONSHIP_TEMPLATE_OF_1, IDENTITY_2, DEVICE_2, null, existingRelationships);
+
+        // Assert
+        acting.Should().Throw<DomainException>().WithError("error.platform.validation.relationshipRequest.relationshipToTargetAlreadyExists");
+    }
+}
