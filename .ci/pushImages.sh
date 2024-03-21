d57a52f3e112bf02ed1bfa9313bcc6e573bd44ef
Mika Herrmann <mika.herrmann@js-soft.com>
2024-03-21 14:25:31 +0100

    test: Use local registry

---
+++ .ci/pushImages.sh
@@ -0,0 +1,5 @@
+﻿#! /bin/bash
+
+docker tag consumer-api localhost:5000/test-consumerapi && docker push localhost:5000/consumerapi || true
+docker tag admin-ui localhost:5000/test-adminui && docker push localhost:5000/adminui || true
+docker tag seed-client localhost:5000/test-seed && docker push localhost:5000/seed || true
