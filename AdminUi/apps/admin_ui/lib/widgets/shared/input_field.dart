5ae1a89ae9754bcf620dbdcb814cd2c9ebb587ed
Vladimir Vuckovic <vidrak97@gmail.com>
2024-03-21 16:37:41 +0100

    feat: add input field widget

---
+++ AdminUi/apps/admin_ui/lib/widgets/shared/input_field.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class InputField extends StatefulWidget {
+  const InputField({required this.onEnteredText, super.key});
+
+  final void Function(String enteredText) onEnteredText;
+
+  @override
+  State<InputField> createState() => _InputFieldState();
+}
+
+class _InputFieldState extends State<InputField> {
+  final _searchAddressController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 150,
+      height: 60,
+      child: TextField(
+        controller: _searchAddressController,
+        onChanged: (value) {
+          setState(() {
+            widget.onEnteredText(value);
+          });
+        },
+        decoration: const InputDecoration(
+          labelText: 'Search Address',
+          border: OutlineInputBorder(),
+        ),
+      ),
+    );
+  }
+}
