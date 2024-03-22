3a1e6bd0cb0f3dc01d052e5b8bfd6567bf5155d8
Mika Herrmann <mika.herrmann@js-soft.com>
2024-03-22 11:15:11 +0100

    test: Fix image names again, parallel building

--- .ci/integrationTest.postgres.sh
+++ .ci/integrationTest.postgres.sh
@@ -15,10 +15,17 @@ debugRun() {
 }
 
 debugRun dockerCompose down
-debugRun dockerCompose build
-debugRun dockerCompose up -d
-debugRun dotnet restore "Backbone.sln"
-debugRun dotnet build --no-restore "Backbone.sln"
+{
+  debugRun dockerCompose build;
+  debugRun dockerCompose up -d
+} &
+{
+  debugRun dotnet restore "Backbone.sln";
+  debugRun dotnet build --no-restore "Backbone.sln"
+}
+wait
+
 export CONSUMER_API_BASE_ADDRESS="http://localhost:5000"
 export ADMIN_API_BASE_ADDRESS="http://localhost:5173"
+
 debugRun dotnet test --no-restore --no-build --filter "Category=Integration&TestCategory!~ignore" --logger "GitHubActions;summary.includePassedTests=true;summary.includeSkippedTests=true" "Backbone.sln"
