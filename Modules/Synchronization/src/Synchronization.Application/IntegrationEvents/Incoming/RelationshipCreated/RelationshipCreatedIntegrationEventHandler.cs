ecb95e410299e6c9e883ce7ec9e2de12b240e55e
Timo Notheisen <timo.notheisen@js-soft.com>
2024-03-25 14:37:05 +0100

    feat: handle new integration events in Synchronization module

---
+++ Modules/Synchronization/src/Synchronization.Application/IntegrationEvents/Incoming/RelationshipCreated/RelationshipCreatedIntegrationEventHandler.cs
@@ -0,0 +1,37 @@
+﻿using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
+using Backbone.Modules.Synchronization.Application.Infrastructure;
+using Backbone.Modules.Synchronization.Application.IntegrationEvents.Outgoing;
+using Backbone.Modules.Synchronization.Domain.Entities.Sync;
+using Microsoft.Extensions.Logging;
+
+namespace Backbone.Modules.Synchronization.Application.IntegrationEvents.Incoming.RelationshipCreated;
+
+public class RelationshipCreatedIntegrationEventHandler : IIntegrationEventHandler<RelationshipCreatedIntegrationEvent>
+{
+    private readonly ISynchronizationDbContext _dbContext;
+    private readonly IEventBus _eventBus;
+    private readonly ILogger<RelationshipCreatedIntegrationEventHandler> _logger;
+
+    public RelationshipCreatedIntegrationEventHandler(ISynchronizationDbContext dbContext, IEventBus eventBus, ILogger<RelationshipCreatedIntegrationEventHandler> logger)
+    {
+        _dbContext = dbContext;
+        _eventBus = eventBus;
+        _logger = logger;
+    }
+
+    public async Task Handle(RelationshipCreatedIntegrationEvent @event)
+    {
+        try
+        {
+            // ReSharper disable once RedundantAnonymousTypePropertyName
+            var payload = new { RelationshipId = @event.RelationshipId };
+            var externalEvent = await _dbContext.CreateExternalEvent(@event.To, ExternalEventType.RelationshipCreated, payload);
+            _eventBus.Publish(new ExternalEventCreatedIntegrationEvent(externalEvent));
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occured while processing an integration event.");
+            throw;
+        }
+    }
+}
---
+++ Modules/Synchronization/test/Synchronization.Application.Tests/Tests/IntegrationEvents/RelationshipStatusChangedIntegrationEventHandlerTests.cs
@@ -0,0 +1,90 @@
+﻿using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
+using Backbone.DevelopmentKit.Identity.ValueObjects;
+using Backbone.Modules.Synchronization.Application.Infrastructure;
+using Backbone.Modules.Synchronization.Application.IntegrationEvents.Incoming.RelationshipStatusChanged;
+using Backbone.Modules.Synchronization.Application.IntegrationEvents.Outgoing;
+using Backbone.Modules.Synchronization.Domain.Entities.Sync;
+using FakeItEasy;
+using Microsoft.Extensions.Logging;
+using Xunit;
+
+namespace Backbone.Modules.Synchronization.Application.Tests.Tests.IntegrationEvents;
+
+public class RelationshipStatusChangedIntegrationEventHandlerTests
+{
+    [Fact]
+    public async Task Creates_an_external_event()
+    {
+        // Arrange
+        var relationshipFrom = TestDataGenerator.CreateRandomIdentityAddress();
+        var relationshipTo = TestDataGenerator.CreateRandomIdentityAddress();
+        var @event = new RelationshipStatusChangedIntegrationEvent
+        {
+            RelationshipId = "REL1",
+            Peer = relationshipTo
+        };
+
+        var mockDbContext = A.Fake<ISynchronizationDbContext>();
+
+        // ReSharper disable once RedundantAnonymousTypePropertyName
+        var externalEvent = new ExternalEvent(ExternalEventType.RelationshipStatusChanged, relationshipTo, 1,
+            new { RelationshipId = @event.RelationshipId });
+
+        A.CallTo(() => mockDbContext.CreateExternalEvent(
+            relationshipTo,
+            ExternalEventType.RelationshipStatusChanged,
+            A<object>._)
+        ).Returns(externalEvent);
+
+        var handler = CreateHandler(mockDbContext);
+
+        // Act
+        await handler.Handle(@event);
+
+        // Assert
+        A.CallTo(() => mockDbContext.CreateExternalEvent(relationshipTo, ExternalEventType.RelationshipStatusChanged, externalEvent.Payload))
+            .MustHaveHappenedOnceExactly();
+    }
+
+    [Fact]
+    public async Task Publishes_an_ExternalEventCreatedIntegrationEvent()
+    {
+        // Arrange
+        var relationshipFrom = TestDataGenerator.CreateRandomIdentityAddress();
+        var relationshipTo = TestDataGenerator.CreateRandomIdentityAddress();
+        var @event = new RelationshipStatusChangedIntegrationEvent
+        {
+            RelationshipId = "REL1",
+            Peer = relationshipTo
+        };
+
+        var fakeDbContext = A.Fake<ISynchronizationDbContext>();
+        var mockEventBus = A.Fake<IEventBus>();
+
+        // ReSharper disable once RedundantAnonymousTypePropertyName
+        var externalEvent = new ExternalEvent(ExternalEventType.IdentityDeletionProcessStarted, IdentityAddress.Parse(relationshipTo), 1,
+            new { RelationshipId = @event.RelationshipId });
+
+        A.CallTo(() => fakeDbContext.CreateExternalEvent(
+            relationshipTo,
+            ExternalEventType.RelationshipStatusChanged,
+            A<object>._)
+        ).Returns(externalEvent);
+
+        var handler = CreateHandler(fakeDbContext, mockEventBus);
+
+        // Act
+        await handler.Handle(@event);
+
+        // Assert
+        A.CallTo(() => mockEventBus.Publish(
+            A<ExternalEventCreatedIntegrationEvent>.That.Matches(e => e.Owner == relationshipTo))
+        ).MustHaveHappenedOnceExactly();
+    }
+
+    private RelationshipStatusChangedIntegrationEventHandler CreateHandler(ISynchronizationDbContext dbContext, IEventBus? eventBus = null)
+    {
+        eventBus ??= A.Fake<IEventBus>();
+        return new RelationshipStatusChangedIntegrationEventHandler(dbContext, eventBus, A.Fake<ILogger<RelationshipStatusChangedIntegrationEventHandler>>());
+    }
+}
