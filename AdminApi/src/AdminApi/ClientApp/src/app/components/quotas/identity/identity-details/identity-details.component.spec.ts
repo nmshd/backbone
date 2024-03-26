b00c23435f360249e0ace0f7d1d0682c8244642c
Vladimir Vuckovic <vidrak97@gmail.com>
2024-03-26 13:06:27 +0100

    chore: add tests for isTierDisabled method

--- AdminApi/src/AdminApi/ClientApp/src/app/components/quotas/identity/identity-details/identity-details.component.spec.ts
+++ AdminApi/src/AdminApi/ClientApp/src/app/components/quotas/identity/identity-details/identity-details.component.spec.ts
@@ -1,22 +1,121 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import { TestBed } from "@angular/core/testing";
+import { ActivatedRoute, NavigationEnd, Router } from "@angular/router";
+import { BehaviorSubject } from "rxjs";
+import { Identity } from "../../../../services/identity-service/identity.service";
+import { TierOverview, TierService } from "../../../../services/tier-service/tier.service";
+import { IdentityDetailsComponent, TierValidator } from "./identity-details.component";
 
-import { IdentityDetailsComponent } from "./identity-details.component";
+class MockActivatedRoute {
+    private readonly subject = new BehaviorSubject(this.testParams);
+    public params = this.subject.asObservable();
+
+    private _testParams = {};
+    public get testParams() {
+        return this._testParams;
+    }
+    public set testParams(params: {}) {
+        this._testParams = params;
+        this.subject.next(params);
+    }
+}
 
 describe("IdentityDetailsComponent", function () {
-    let component: IdentityDetailsComponent;
-    let fixture: ComponentFixture<IdentityDetailsComponent>;
+    let tierService: TierService;
+    let mockRouter: Router;
+    let identityDetailsComponent: IdentityDetailsComponent;
+    let mockActivatedRoute: MockActivatedRoute;
+    let httpTestingController: HttpTestingController;
 
-    beforeEach(async function () {
-        await TestBed.configureTestingModule({
-            declarations: [IdentityDetailsComponent]
-        }).compileComponents();
+    beforeEach(function () {
+        mockRouter = {
+            events: new BehaviorSubject(new NavigationEnd(0, "", ""))
+        } as any;
 
-        fixture = TestBed.createComponent(IdentityDetailsComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
+        mockActivatedRoute = new MockActivatedRoute();
+
+        TestBed.configureTestingModule({
+            declarations: [IdentityDetailsComponent],
+            imports: [HttpClientTestingModule],
+            providers: [TierService, { provide: Router, useValue: mockRouter }, { provide: ActivatedRoute, useValue: mockActivatedRoute }]
+        });
+
+        tierService = TestBed.inject(TierService);
+        httpTestingController = TestBed.inject(HttpTestingController);
     });
 
-    it("should create", async function () {
-        await expect(component).toBeTruthy();
+    afterEach(function () {
+        httpTestingController.verify();
+    });
+
+    it("should be created", async function () {
+        const fixture = TestBed.createComponent(IdentityDetailsComponent);
+        identityDetailsComponent = fixture.componentInstance;
+        await expect(identityDetailsComponent).toBeTruthy();
+    });
+
+    describe("TierValidator", function () {
+        const tier1: TierOverview = {
+            id: "1",
+            name: "Tier 1",
+            numberOfIdentities: 0,
+            canBeUsedAsDefaultForUser: true,
+            canBeManuallyAssigned: true
+        };
+
+        const tier2: TierOverview = {
+            id: "2",
+            name: "Tier 2",
+            numberOfIdentities: 0,
+            canBeUsedAsDefaultForUser: false,
+            canBeManuallyAssigned: false
+        };
+
+        const tiers: TierOverview[] = [tier1, tier2];
+
+        it("should return false if identity is not in a tier that cannot be unassigned", async function () {
+            const identity: Identity = {
+                address: "id1",
+                clientId: "test",
+                tierId: "1",
+                devices: [],
+                quotas: [],
+                identityVersion: "1",
+                createdAt: new Date(),
+                publicKey: ""
+            };
+
+            await expect(TierValidator.isTierDisabled(tier1, tiers, identity)).toBe(false);
+        });
+
+        it("should return true if identity is in a tier that cannot be unassigned", async function () {
+            const identity: Identity = {
+                address: "id1",
+                clientId: "test",
+                tierId: "2",
+                devices: [],
+                quotas: [],
+                identityVersion: "1",
+                createdAt: new Date(),
+                publicKey: ""
+            };
+
+            await expect(TierValidator.isTierDisabled(tier1, tiers, identity)).toBe(true);
+        });
+
+        it("should return false if tier cannot be manually assigned and is not the identity's tier", async function () {
+            const identity: Identity = {
+                address: "id1",
+                clientId: "test",
+                tierId: "1",
+                devices: [],
+                quotas: [],
+                identityVersion: "1",
+                createdAt: new Date(),
+                publicKey: ""
+            };
+
+            await expect(TierValidator.isTierDisabled(tier2, tiers, identity)).toBe(false);
+        });
     });
 });
