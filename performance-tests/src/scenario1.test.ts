d0f014ed8bc353cfaaece201818beb961481e714
Daniel Silva <daniel.p.silva@konkconsulting.com>
2024-03-26 17:17:50 +0000

    chore: add first proposal, failing build on webpack

--- performance-tests/src/scenario1.test.ts
+++ performance-tests/src/scenario1.test.ts
@@ -1,26 +1,42 @@
 import { SimpleLoggerFactory } from "@js-soft/simple-logger";
-import { TransportLoggerFactory } from "@nmshd/transport";
-import { DateTime } from "luxon";
+import { SyncRunType, TransportLoggerFactory } from "@nmshd/transport";
 import { LogLevel } from "typescript-logging";
 import { BackboneClient } from "./BackboneClient";
 
 TransportLoggerFactory.init(new SimpleLoggerFactory(LogLevel.Fatal));
 
-const config = {
-    baseUrl: "http://localhost:8081",
-    platformClientId: "test",
-    platformClientSecret: "test"
+export const options = {
+    vus: 1,
+    stages: [
+        { duration: "20s", target: 5 },
+        { duration: "20s", target: 20 },
+        { duration: "90s", target: 20 },
+        { duration: "20s", target: 0 }
+    ],
+    thresholds: { http_req_duration: ["avg<100", "p(95)<200"] },
+    noConnectionReuse: true
 };
 
-(async () => {
+export default async function () {
+    const config = {
+        baseUrl: "http://localhost:8081",
+        platformClientId: "test",
+        platformClientSecret: "test"
+    };
+
     const backboneClient1 = await BackboneClient.initWithNewIdentity(config);
 
-    const template1 = await backboneClient1.relationshipTemplates.createRelationshipTemplate({ content: "AAAA", expiresAt: DateTime.utc().plus({ days: 1 }).toISO() });
-
     const backboneClient2 = await backboneClient1.createNewClientForNewDevice();
+    const backboneClient3 = await backboneClient1.createNewClientForNewDevice();
 
-    const template2 = await backboneClient2.relationshipTemplates.createRelationshipTemplate({ content: "BBBB", expiresAt: DateTime.utc().plus({ days: 1 }).toISO() });
+    let syncRes = await backboneClient1.sync.startSyncRun({ type: SyncRunType.DatawalletVersionUpgrade, duration: 2000 });
+    await backboneClient1.sync.finalizeDatawalletVersionUpgrade(syncRes.value.syncRun?.id!, {
+        newDatawalletVersion: 1,
+        datawalletModifications: [{ objectIdentifier: "test", collection: "Messages", type: "Create", encryptedPayload: "encryptedPayload", datawalletVersion: 1 }]
+    });
 
-    console.log(template1);
-    console.log(template2);
-})();
+    await Promise.all([backboneClient2.sync.startSyncRun(), backboneClient3.sync.startSyncRun()]);
+
+    (await backboneClient2.sync.getDatawalletModifications({ localIndex: -100 })).value.collect();
+    (await backboneClient3.sync.getDatawalletModifications({ localIndex: -100 })).value.collect();
+}
