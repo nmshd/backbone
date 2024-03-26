902f5b468d74d76d8060810d1ff5a26d1065e26e
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-03-26 16:27:37 +0100

    fix: use correct code in down method

--- AdminApi/src/AdminApi.Infrastructure.Database.Postgres/Migrations/20240315123223_Update_TiersOverview_NewProperties.cs
+++ AdminApi/src/AdminApi.Infrastructure.Database.Postgres/Migrations/20240315123223_Update_TiersOverview_NewProperties.cs
@@ -28,7 +28,17 @@ namespace Backbone.AdminApi.Infrastructure.Database.Postgres.Migrations
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql(""" DROP VIEW "TierOverviews" """);
+            migrationBuilder.Sql("""
+                CREATE OR REPLACE VIEW "TierOverviews" AS
+                    SELECT
+                        TIERS."Id" AS "Id",
+                        TIERS."Name" AS "Name",
+                        COUNT (IDENTITIES."TierId") AS "NumberOfIdentities"
+                    FROM "Devices"."Tiers" TIERS
+                    LEFT JOIN "Devices"."Identities" IDENTITIES
+                    ON IDENTITIES."TierId" = TIERS."Id"
+                    GROUP BY TIERS."Id", TIERS."Name"
+             """);
         }
     }
 }
