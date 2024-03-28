236497c646987dcf7ca623ee365c6ba21a82debd
Daniel Silva <daniel.p.silva@konkconsulting.com>
2024-03-28 15:47:00 +0000

    chore: add missing utils

--- performance-tests/src/utils.ts
+++ performance-tests/src/utils.ts
@@ -1,7 +1,47 @@
+import { CreateDatawalletModificationsRequestItem } from "@nmshd/transport";
+import { randomBytes } from "crypto";
+
 /**
- * Generates a random number between {@link min} and {@link max} inclusive.
- * @returns integer
+/**
+ * Generates a random number between min and max inclusive.
+ * @param min minimum value
+ * @param max maximum value
+ * @returns random integer
  */
 export function randomIntFromInterval(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+/**
+ * Generates a list of datawallet modifications.
+ * @param numberOfModifications number of modifications
+ * @param sizeOfModifications size of each modification in bytes
+ * @returns list of generated datawallet modifications
+ */
+export function generateDataWalletModifications(numberOfModifications: number, sizeOfModifications: number): CreateDatawalletModificationsRequestItem[] {
+    const userDataWalletModifications: CreateDatawalletModificationsRequestItem[] = [];
+
+    for (let i = 0; i < numberOfModifications; i++) {
+        const payload = randomBytesAsBase64String(sizeOfModifications);
+        userDataWalletModifications.push({
+            objectIdentifier: "test",
+            collection: "Userdata",
+            type: "Create",
+            encryptedPayload: payload,
+            datawalletVersion: 1
+        });
+    }
+    return userDataWalletModifications;
+}
+
+export function randomBytesAsBase64String(sizeOfModifications: number) {
+    return Buffer.from(randomBytes(sizeOfModifications)).toString("base64");
+}
+
+/**
+ * Waits for the specified number of milliseconds.
+ * @param milis the number of milliseconds to wait
+ */
+export async function sleep(milis: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, milis));
 }
