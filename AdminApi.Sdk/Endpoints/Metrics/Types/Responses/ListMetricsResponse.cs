a7ffd458bcbb255b4945d99f39f99da844cdea6c
Mika Aaron Herrmann <65243124+MH321Productions@users.noreply.github.com>
2024-04-03 12:01:51 +0200

    Admin API: C# SDK (#548)
    * Create new project
    
    * Set up basic structure
    
    * Add ApiKeyValidation Endpoint
    
    * Add Clients Endpoint
    
    * Add Identities Endpoint
    
    * Add Logs Endpoint
    
    * Add Metrics Endpoint
    
    * Add Relationships Endpoint
    
    * Add Endpoints to client
    
    * Add Tiers Endpoint
    
    * Make Project buildable
    
    * Make fields required
    
    * Fix Formatting #1
    
    * Fix Formatting #2
    
    * Fix merge conflict (Consumer API SDK was contained twice)
    
    * Add new common project
    
    * Add common renamed types
    
    * Add project file
    
    * Remove common types from ConsumerApi
    
    * Include common project in ConsumerApi
    
    * Update Types, Methods and classes in ConsumerAPI
    
    * Move Requests and Responses into own folders
    
    * Remove validated request
    
    * Add Enumerable Responses
    
    * Add AdminAPI SDK project
    
    * Rename StartDeletionProcess Method
    
    * Remove signed challenge from Create identity request
    
    * Make fields required
    
    * Reference Tooling project for custom Json Converter
    
    * Use Authenticator for Xsrf
    
    * Fix Enumerable Response
    
    * Fix Create Tier Request
    
    * Fix Request
    
    * Fix formatting
    
    * Delete redundant classes
    
    * Reference Common project
    
    * chore: Reference common classes
    
    * chore: Add patch method, remove unnecessary unauthenticated methods
    
    * fix: Make EnumerableResponseBase an ICollection
    
    * fix: Fix formatting
    
    * Admin UI: Login page (#584)
    
    * chore: adapt the header appearance
    
    * chore: add new design for login  box
    
    * chore: adapt login position in padding widget  via screen size
    
    * chore: logic for displaying the error message for attempted login
    
    * chore: remove error message
    
    * chore: add error message
    
    * chore: change the button colors and add a text button
    
    * chore: remove Visibility widget
    
    * chore: add text style for text button
    
    * chore: change todo comment according to the flutter style
    
    * refactor: extract the app title to be a separate widget
    
    * refactor: extract text field to be reusable
    
    * chore: use new app title widget
    
    * chore: use custom text field
    
    * chore: make the text field fixed height
    
    * chore: rename variables and make them private
    
    * chore: update imports
    
    * chore: use extracted app title widget
    
    * chore: remove unnecessary widgets and center the card
    
    * chore: add custom colors and move login button to the bottom
    
    * chore: remove unused import
    
    * refactor: rename folder and files appropriately
    
    * refactor: extract sized box into a separate custom widget
    
    * refactor: extract elevated button into a separate custom widget
    
    * chore: make the variable private
    
    * chore: local variable should not start with underscore
    
    * chore: add DI for baseUrl
    
    * fix: untangle coding
    
    * fix: imports
    
    * fix: make CustomColors easier accessible
    
    * fix: make prettier
    
    * feat: add gaps
    
    * fix: rename file
    
    * refactor: remove CustomX, make whole logic with one variable
    
    * fix: bool logic
    
    * ci: add jkoenig134 as codeowner
    
    * chore: remove accidentally committet code
    
    * fix: undo change
    
    * refactor: simplify login screen
    
    * refactor: simplify app title
    
    * refactor: PR comments
    
    * chore: inline svg picture
    
    * chore: make multi line params last
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: Julian König <julian.koenig@js-soft.com>
    Co-authored-by: Timo Notheisen <timo.notheisen@js-soft.com>
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: Vladimir Vuckovic <37671301+stamenione@users.noreply.github.com>
    Co-authored-by: Julian König <julian.koenig@js-soft.com>
    Co-authored-by: Timo Notheisen <timo.notheisen@js-soft.com>
    Co-authored-by: Timo Notheisen <65653426+tnotheis@users.noreply.github.com>

---
+++ AdminApi.Sdk/Endpoints/Metrics/Types/Responses/ListMetricsResponse.cs
@@ -0,0 +1,6 @@
+﻿using Backbone.AdminApi.Sdk.Endpoints.Identities.Types;
+using Backbone.BuildingBlocks.SDK.Endpoints.Common.Types;
+
+namespace Backbone.AdminApi.Sdk.Endpoints.Metrics.Types.Responses;
+
+public class ListMetricsResponse : EnumerableResponseBase<Metric>;
