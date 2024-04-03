0c059cbc91b6ceb7500776635ee0b216d6acc90d
Daniel Silva <daniel.p.silva@konkconsulting.com>
2024-04-03 13:00:39 +0100

    chore: add new migrations specifically for actual deletion

---
+++ Modules/Relationships/src/Relationships.Infrastructure.Database.SqlServer/Migrations/20240403115724_ActualDeletion.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backbone.Modules.Relationships.Infrastructure.Database.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActualDeletion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
