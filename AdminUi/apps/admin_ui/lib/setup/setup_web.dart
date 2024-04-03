171dd3485822f9185afb963a58ba415b211bdbdb
Vladimir Vuckovic <37671301+stamenione@users.noreply.github.com>
2024-04-03 11:26:12 +0200

    Admin UI: Login page (#584)
    * chore: adapt the header appearance
    
    * chore: add new design for login  box
    
    * chore: adapt login position in padding widget  via screen size
    
    * chore: logic for displaying the error message for attempted login
    
    * chore: remove error message
    
    * chore: add error message
    
    * chore: change the button colors and add a text button
    
    * chore: remove Visibility widget
    
    * chore: add text style for text button
    
    * chore: change todo comment according to the flutter style
    
    * refactor: extract the app title to be a separate widget
    
    * refactor: extract text field to be reusable
    
    * chore: use new app title widget
    
    * chore: use custom text field
    
    * chore: make the text field fixed height
    
    * chore: rename variables and make them private
    
    * chore: update imports
    
    * chore: use extracted app title widget
    
    * chore: remove unnecessary widgets and center the card
    
    * chore: add custom colors and move login button to the bottom
    
    * chore: remove unused import
    
    * refactor: rename folder and files appropriately
    
    * refactor: extract sized box into a separate custom widget
    
    * refactor: extract elevated button into a separate custom widget
    
    * chore: make the variable private
    
    * chore: local variable should not start with underscore
    
    * chore: add DI for baseUrl
    
    * fix: untangle coding
    
    * fix: imports
    
    * fix: make CustomColors easier accessible
    
    * fix: make prettier
    
    * feat: add gaps
    
    * fix: rename file
    
    * refactor: remove CustomX, make whole logic with one variable
    
    * fix: bool logic
    
    * ci: add jkoenig134 as codeowner
    
    * chore: remove accidentally committet code
    
    * fix: undo change
    
    * refactor: simplify login screen
    
    * refactor: simplify app title
    
    * refactor: PR comments
    
    * chore: inline svg picture
    
    * chore: make multi line params last
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: Julian König <julian.koenig@js-soft.com>
    Co-authored-by: Timo Notheisen <timo.notheisen@js-soft.com>

--- AdminUi/apps/admin_ui/lib/setup/setup_web.dart
+++ AdminUi/apps/admin_ui/lib/setup/setup_web.dart
@@ -1,3 +1,7 @@
+import 'package:get_it/get_it.dart';
+
+import '/core/app_config.dart';
+
 Future<void> setup() async {
-  // No setup required for web
+  GetIt.I.registerSingleton<AppConfig>(AppConfig(baseUrl: ''));
 }
