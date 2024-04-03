8367fdfca807ea6a42ed525d44d982c27b6bba7b
Daniel Silva <daniel.p.silva@konkconsulting.com>
2024-04-03 14:32:43 +0100

    chore: fix formatting

--- Modules/Relationships/src/Relationships.Infrastructure/Persistence/Database/EntityTypeConfigurations/RelationshipTemplateEntityTypeConfiguration.cs
+++ Modules/Relationships/src/Relationships.Infrastructure/Persistence/Database/EntityTypeConfigurations/RelationshipTemplateEntityTypeConfiguration.cs
@@ -1,4 +1,4 @@
- using Backbone.Modules.Relationships.Domain.Entities;
+using Backbone.Modules.Relationships.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
