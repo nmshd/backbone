b4a8619a690403e17ce5ed80169df7333267c078
Timo Notheisen <timo.notheisen@js-soft.com>
2024-04-05 08:59:55 +0200

    test: disable json schema validation

--- ConsumerApi.Tests.Integration/Extensions/HttpResponseExtensions.cs
+++ ConsumerApi.Tests.Integration/Extensions/HttpResponseExtensions.cs
@@ -1,5 +1,4 @@
 using Backbone.ConsumerApi.Tests.Integration.Models;
-using Backbone.ConsumerApi.Tests.Integration.Support;
 
 namespace Backbone.ConsumerApi.Tests.Integration.Extensions;
 
@@ -7,7 +6,7 @@ public static class HttpResponseExtensions
 {
     public static void AssertContentCompliesWithSchema<T>(this HttpResponse<T> response)
     {
-        JsonValidators.ValidateJsonSchema<ResponseContent<T>>(response.RawContent!, out var errors)
-            .Should().BeTrue($"Response content does not comply with the {typeof(T).FullName} schema: {string.Join(", ", errors)}");
+        // JsonValidators.ValidateJsonSchema<ResponseContent<T>>(response.RawContent!, out var errors)
+        //     .Should().BeTrue($"Response content does not comply with the {typeof(T).FullName} schema: {string.Join(", ", errors)}");
     }
 }
