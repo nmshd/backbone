32e3a04b450fc8c94802055830384a5f437b1624
Mika Herrmann <mika.herrmann@js-soft.com>
2024-04-08 14:51:38 +0200

    fix: Use custom URI creation method

---
+++ BuildingBlocks/src/BuildingBlocks.SDK/UriUtils.cs
@@ -0,0 +1,7 @@
+﻿namespace Backbone.BuildingBlocks.SDK;
+
+public class UriUtils
+{
+    public static Uri CreateUriFromParts(params string[] parts)
+        => new Uri(parts.Aggregate((a, b) => $"{a.TrimEnd('/')}/{b.TrimStart('/')}"));
+}
