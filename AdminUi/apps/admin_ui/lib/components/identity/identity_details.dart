aa8670e220b151a366bfde61dd488fda7cbd06c9
Vladimir Vuckovic <vidrak97@gmail.com>
2024-04-08 08:09:45 +0200

    chore: identity details (wip)

---
+++ AdminUi/apps/admin_ui/lib/components/identity/identity_details.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class IdentityDetails extends StatelessWidget {
+  const IdentityDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('I am at identity details.');
+  }
+}
