e4d9ef7173cdac9eb096bb9cd1dce905fafab14d
Timo Notheisen <timo.notheisen@js-soft.com>
2024-04-10 11:02:27 +0200

    chore: readd `--ext` to eslint command

--- .ci/aui/runChecks.sh
+++ .ci/aui/runChecks.sh
@@ -5,7 +5,7 @@ set -x
 INITIAL_DIR=$(pwd)
 cd AdminApi/src/AdminApi/ClientApp
 npm ci
-npx eslint
+npx eslint --ext ts ./src
 npx prettier --check .
 npx license-check --ignorePackages adminui@0.0.0
 npx better-npm-audit audit --exclude 1096890,1096887
