a35fb038f6c4676c1c248286355dc8cc08cadfa1
Vladimir Vuckovic <vidrak97@gmail.com>
2024-04-12 14:40:01 +0200

    chore: change navigation rail into a drawer

--- AdminUi/apps/admin_ui/lib/screens/home_screen.dart
+++ AdminUi/apps/admin_ui/lib/screens/home_screen.dart
@@ -16,8 +16,30 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
   bool extended = false;
+
+  late AnimationController _controller;
+  late Animation<double> _widthAnimation;
+  final maxWidth = 250.0;
+  final minWidth = 64.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      reverseDuration: const Duration(milliseconds: 300),
+      vsync: this,
+    )..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          extended = true;
+        } else if (status == AnimationStatus.dismissed) {
+          extended = false;
+        }
+      });
+    _widthAnimation = Tween<double>(begin: minWidth, end: maxWidth).animate(_controller);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,11 +48,7 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const AppTitle(),
         leading: IconButton(
           icon: const Icon(Icons.menu),
-          onPressed: () {
-            setState(() {
-              extended = !extended;
-            });
-          },
+          onPressed: toggleDrawer,
         ),
         actions: [
           SizedBox(
@@ -51,30 +69,42 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
       body: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          NavigationRail(
-            extended: extended,
-            destinations: const [
-              NavigationRailDestination(icon: Icon(Icons.apps), label: Text('Dashboard')),
-              NavigationRailDestination(icon: Icon(Icons.badge), label: Text('Identities')),
-              NavigationRailDestination(icon: Icon(Icons.clear_all), label: Text('Tiers')),
-              NavigationRailDestination(icon: Icon(Icons.person), label: Text('Clients')),
-            ],
-            selectedIndex: _selectedIndex,
-            onDestinationSelected: (int index) {
-              if (index == _selectedIndex) return;
-
-              context.go(
-                switch (index) {
-                  0 => '/dashboard',
-                  1 => '/identities',
-                  2 => '/tiers',
-                  3 => '/clients',
-                  _ => throw Exception(),
-                },
-              );
-            },
-          ),
+          AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) {
+                return Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: SizedBox(
+                    width: _widthAnimation.value,
+                    height: 235,
+                    child: Drawer(
+                      elevation: 0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            child: ListView(
+                              children: [
+                                buildHeaderSection('Functionalities'),
+                                if (_widthAnimation.value == maxWidth)
+                                  const Padding(
+                                    padding: EdgeInsets.only(left: 8, right: 8),
+                                    child: Divider(),
+                                  ),
+                                buildNavigationTile(context, 'Identities', Icons.account_circle_sharp, isSelected: true),
+                                buildNavigationTile(context, 'Tiers', Icons.cable, index: 1),
+                                buildNavigationTile(context, 'Clients', Icons.layers, index: 2),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              }),
           Expanded(child: widget.child),
         ],
       ),
@@ -82,12 +112,32 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   int get _selectedIndex {
-    if (widget.location.startsWith('/dashboard')) return 0;
-    if (widget.location.startsWith('/identities')) return 1;
-    if (widget.location.startsWith('/tiers')) return 2;
-    if (widget.location.startsWith('/clients')) return 3;
+    if (widget.location.startsWith('/identities')) return 0;
+    if (widget.location.startsWith('/tiers')) return 1;
+    if (widget.location.startsWith('/clients')) return 2;
 
     throw Exception();
+  }
+
+  void navigate(int index) {
+    switch (index) {
+      case 0:
+        context.go('/identities');
+      case 1:
+        context.go('/tiers');
+      case 2:
+        context.go('/clients');
+      default:
+        throw Exception('Invalid index');
+    }
+  }
+
+  void toggleDrawer() {
+    if (extended) {
+      _controller.reverse();
+    } else {
+      _controller.forward();
+    }
   }
 
   Future<void> _logout() async {
@@ -96,5 +146,71 @@ class _HomeScreenState extends State<HomeScreen> {
     await GetIt.I.unregisterIfRegistered<AdminApiClient>();
 
     if (mounted) context.go('/login');
+  }
+
+  Widget buildHeaderSection(String title) {
+    return _widthAnimation.value == maxWidth
+        ? Padding(
+            padding: const EdgeInsets.only(left: 12),
+            child: Text(
+              title,
+              style: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          )
+        : Padding(
+            padding: const EdgeInsets.symmetric(vertical: 18),
+            child: Container(),
+          );
+  }
+
+  Widget buildNavigationTile(BuildContext context, String title, IconData icon, {int index = 0, bool isSelected = false}) {
+    final isSelected = _selectedIndex == index;
+
+    final borderRadius = _widthAnimation.value > minWidth ? BorderRadius.circular(40) : BorderRadius.circular(100);
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+      child: InkWell(
+        onTap: () => navigate(index),
+        splashColor: Theme.of(context).colorScheme.secondaryContainer.withOpacity(0.3),
+        borderRadius: borderRadius,
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            color: isSelected ? Theme.of(context).colorScheme.secondaryContainer : Colors.transparent,
+            borderRadius: borderRadius,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: Icon(
+                    icon,
+                    size: 30,
+                    color: isSelected ? Theme.of(context).colorScheme.onSecondaryContainer : Theme.of(context).colorScheme.onSecondaryContainer,
+                  ),
+                ),
+                if (_widthAnimation.value == maxWidth) ...[
+                  Gaps.w16,
+                  Text(
+                    title,
+                    style: TextStyle(
+                      color: isSelected ? Theme.of(context).colorScheme.onSecondaryContainer : Theme.of(context).colorScheme.onSurface,
+                      fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+                    ),
+                  ),
+                ] else
+                  Container(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
