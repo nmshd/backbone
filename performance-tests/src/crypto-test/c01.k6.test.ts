76e40e3d863fdeb084fe2b47040789c1bbd5d30a
Daniel Silva <daniel.p.silva@konkconsulting.com>
2024-04-12 10:46:16 +0100

    chore: add (failing) usage of noble-curves.js

---
+++ performance-tests/src/crypto-test/c01.k6.test.ts
@@ -0,0 +1,16 @@
+export default function () {
+    const nobleCurves = require("../libs/noble-curves");
+    // const url = "http://test.k6.io/login";
+    // const payload = JSON.stringify({
+    //     email: "aaa",
+    //     password: "bbb"
+    // });
+    // const params = {
+    //     headers: {
+    //         "Content-Type": "application/json"
+    //     }
+    // };
+    // http.post(url, payload, params);
+
+    console.log(nobleCurves);
+}
