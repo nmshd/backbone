503616caa45d970857955fbba18ff5b9323d349e
Daniel Silva <daniel.p.silva@konkconsulting.com>
2024-04-12 17:01:32 +0100

    chore: work on load phase

--- performance-tests/src/scenario-01/s01.test.ts
+++ performance-tests/src/scenario-01/s01.test.ts
@@ -1,6 +1,6 @@
 import { SimpleLoggerFactory } from "@js-soft/simple-logger";
 import { SyncRunType, TransportLoggerFactory } from "@nmshd/transport";
-import pLimit from "p-limit";
+import pLimit, { LimitFunction } from "p-limit";
 import { LogLevel } from "typescript-logging";
 import { BackboneClient } from "../BackboneClient";
 import { generateDataWalletModifications, randomIntFromInterval } from "../utils";
@@ -33,21 +33,78 @@ let main = async () => {
         (await backboneClient2.sync.getDatawalletModifications({ localIndex: -100 })).value.collect();
         (await backboneClient3.sync.getDatawalletModifications({ localIndex: -100 })).value.collect();
 
-        console.log("finished run");
         // TODO: implement k6 checks for the received changes
     }
+
+    console.log("finished run");
 };
-(async () => {
-    const limit = pLimit(10); // only 10 at once
 
-    let calls: Promise<void>[] = [];
-    for (let i = 0; i < 8; i++) {
-        calls.push(limit(() => main()));
+function divideByTwoUntilSmallerThan(seconds: number, threshold: number, ceil = false): number[] {
+    const result: number[] = [];
+    let current = seconds;
+
+    while (current > threshold) {
+        result.push(current);
+        current /= 2;
+        if (ceil) {
+            current = Math.ceil(current);
+        }
     }
-    setInterval(() => {
-        console.log(limit.activeCount, limit.pendingCount);
-    }, 500);
+    return result.reverse();
+}
 
-    let res = await Promise.all(calls).then(() => console.log("done"));
-    console.log(res);
+function distributeAmongBags(p: number, b: number): number[] | null {
+    if (p <= 0 || b <= 0) return null; // Invalid input
+
+    const distribution: number[] = [];
+    let remaining = p;
+    let currentAmount = p;
+
+    // Calculate the amount to be distributed in each bag
+    for (let i = 0; i < b; i++) {
+        const amountInBag = Math.ceil(currentAmount / 2 ** (b - i - 1));
+        if (amountInBag <= 0) return null; // Invalid distribution
+        distribution.push(amountInBag);
+        remaining -= amountInBag;
+        currentAmount = amountInBag; // Update current amount for next bag
+    }
+
+    return distribution;
+}
+
+/**
+ *
+ * @param totalSteps number of times to call the function
+ * @param totalTime time in seconds
+ */
+const createTasks = (totalSteps: number, totalTime: number): Map<number, LimitFunction> => {
+    const timeSteps = divideByTwoUntilSmallerThan(totalTime, 2);
+    const stepsCount = distributeAmongBags(totalSteps, timeSteps.length);
+    const ret = new Map<number, LimitFunction>();
+
+    console.log(timeSteps, stepsCount);
+    let i = 0;
+    timeSteps.forEach((timeStep) => {
+        ret.set(timeStep, pLimit(stepsCount![i++]));
+    });
+
+    return ret;
+};
+
+(async () => {
+    // const limit = pLimit(5);
+
+    // let calls: Promise<void>[] = [];
+    // for (let i = 0; i < 30; i++) {
+    //     calls.push(limit(() => main()));
+    // }
+    // let a = setInterval(() => {
+    //     console.log(limit.activeCount, limit.pendingCount);
+    // }, 500);
+
+    // await Promise.all(calls).then(() => console.log("done"));
+    // clearTimeout(a);
+    let a = createTasks(50, 600);
+
+    console.log(a);
 })();
