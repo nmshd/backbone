36c5b3906012a24a194830731811f05aaa3b7279
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-04-16 08:32:48 +0200

    feat: add integration event in quotas module

---
+++ Modules/Quotas/src/Quotas.Application/IntegrationEvents/Incoming/RelationshipReactivationRequestCreated/RelationshipReactivationRequestIntegrationEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
+using Backbone.Modules.Quotas.Application.Metrics;
+using Backbone.Modules.Quotas.Domain.Aggregates.Metrics;
+
+namespace Backbone.Modules.Quotas.Application.IntegrationEvents.Incoming.RelationshipReactivationRequestCreated;
+public class RelationshipReactivationRequestIntegrationEventHandler : IIntegrationEventHandler<RelationshipReactivationRequestIntegrationEvent>
+{
+    private readonly IMetricStatusesService _metricStatusesService;
+
+    public RelationshipReactivationRequestIntegrationEventHandler(IMetricStatusesService metricStatusesService)
+    {
+        _metricStatusesService = metricStatusesService;
+    }
+
+    public async Task Handle(RelationshipReactivationRequestIntegrationEvent integrationEvent)
+    {
+        var identities = new List<string> { integrationEvent.CreatedBy };
+        var metrics = new List<string> { MetricKey.NumberOfRelationships.Value };
+
+        await _metricStatusesService.RecalculateMetricStatuses(identities, metrics, CancellationToken.None);
+    }
+}
