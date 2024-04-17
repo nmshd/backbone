270903861ff97d2a3a8900289b2998fa5d5cf7cb
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-04-17 16:01:02 +0200

    chore: rename

--- Modules/Relationships/src/Relationships.Application/IntegrationEvents/Outgoing/RelationshipReactivationRequestIntegrationEvent.cs
+++ Modules/Relationships/src/Relationships.Application/IntegrationEvents/Outgoing/RelationshipReactivationRequestIntegrationEvent.cs
@@ -5,12 +5,12 @@ using Backbone.Modules.Relationships.Domain.Aggregates.Relationships;
 namespace Backbone.Modules.Relationships.Application.IntegrationEvents.Outgoing;
 public class RelationshipReactivationRequestIntegrationEvent : IntegrationEvent
 {
-    public RelationshipReactivationRequestIntegrationEvent(Relationship relationship, IdentityAddress partner) : base($"{relationship.Id}/Reactivate")
+    public RelationshipReactivationRequestIntegrationEvent(Relationship relationship, IdentityAddress peer) : base($"{relationship.Id}/Reactivate")
     {
         RelationshipId = relationship.Id;
-        Partner = partner.StringValue;
+        Peer = peer.StringValue;
     }
 
     public string RelationshipId { get; }
-    public string Partner { get; }
+    public string Peer { get; }
 }
