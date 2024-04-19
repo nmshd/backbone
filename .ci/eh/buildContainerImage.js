c8239a06f833ceb77d3a40edcfd1300eb1f69b41
Timo Notheisen <timo.notheisen@js-soft.com>
2024-04-19 15:05:22 +0200

    chore: change namein pipelines and dockerimage to event handler

--- .ci/eh/buildContainerImage.js
+++ .ci/eh/buildContainerImage.js
@@ -8,4 +8,4 @@ const tag = getRequiredEnvVar("TAG");
 const platforms = process.env.PLATFORMS ?? "linux/amd64,linux/arm64";
 const push = process.env.PUSH === "1" ? ["--push", "--provenance=true", "--sbom=true"] : "";
 
-await $`docker buildx build --file ./EventHandlerService/src/EventHandlerService/Dockerfile --tag ghcr.io/nmshd/backbone-event-handler-service:${tag} --platform ${platforms} ${push} .`;
+await $`docker buildx build --file ./EventHandlerService/src/EventHandlerService/Dockerfile --tag ghcr.io/nmshd/backbone-event-handler:${tag} --platform ${platforms} ${push} .`;
