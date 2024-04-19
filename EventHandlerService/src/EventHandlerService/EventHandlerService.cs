8998521a92e04bd44bdedaf01e334505d4a042b8
Timo Notheisen <timo.notheisen@js-soft.com>
2024-04-19 16:00:47 +0200

    refactor: fix method naming

--- EventHandlerService/src/EventHandlerService/EventHandlerService.cs
+++ EventHandlerService/src/EventHandlerService/EventHandlerService.cs
@@ -7,11 +7,13 @@ public class EventHandlerService : IHostedService
 {
     private readonly IEventBus _eventBus;
     private readonly IEnumerable<AbstractModule> _modules;
+    private readonly ILogger<EventHandlerService> _logger;
 
-    public EventHandlerService(IEventBus eventBus, IEnumerable<AbstractModule> modules)
+    public EventHandlerService(IEventBus eventBus, IEnumerable<AbstractModule> modules, ILogger<EventHandlerService> logger)
     {
         _eventBus = eventBus;
         _modules = modules;
+        _logger = logger;
     }
 
     public Task StartAsync(CancellationToken cancellationToken)
@@ -27,15 +29,18 @@ public class EventHandlerService : IHostedService
         return Task.CompletedTask;
     }
 
-    private void StartConsuming()
+    private void SubscribeToEvents()
     {
+        _logger.LogInformation("Subscribing to events...");
         foreach (var module in _modules)
         {
             module.ConfigureEventBus(_eventBus);
         }
+
+        _logger.LogInformation("Successfully subscribed to events.");
     }
 
-    private void SubscribeToEvents()
+    private void StartConsuming()
     {
         _eventBus.StartConsuming();
     }
