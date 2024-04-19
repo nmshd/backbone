f774da8da6b878a3238eabb30415b824644857f5
Timo Notheisen <timo.notheisen@js-soft.com>
2024-04-19 13:20:59 +0200

    ci: add building of ehs container image to test pipeline

---
+++ .ci/ehs/buildContainerImage.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import { $ } from "zx";
+import { getRequiredEnvVar } from "../lib.js";
+
+const tag = getRequiredEnvVar("TAG");
+
+const platforms = process.env.PLATFORMS ?? "linux/amd64,linux/arm64";
+const push = process.env.PUSH === "1" ? ["--push", "--provenance=true", "--sbom=true"] : "";
+
+await $`docker buildx build --file ./EventHandlerService/src/EventHandlerService/Dockerfile --tag ghcr.io/nmshd/backbone-event-handler-service:${tag} --platform ${platforms} ${push} .`;
