9b80131b5885e981db5213fd7cdb46f97766771a
Timo Notheisen <timo.notheisen@js-soft.com>
2024-04-23 15:33:36 +0200

    ci: replace __appVersion__ in all Chart.yaml files

--- .ci/helm/buildChart.js
+++ .ci/helm/buildChart.js
@@ -6,4 +6,8 @@ import { getRequiredEnvVar } from "../lib.js";
 const version = getRequiredEnvVar("VERSION");
 
 await $`helm dependency update helm`;
-await $`helm package --set global.chartVersion=${version} --version ${version} helm`;
+
+// replace <<app_version>> in all files in helm/**/Chart.yaml with the value of `version`
+await $`find helm -name Chart.yaml -exec sed -i -e 's/__app_version__/${version}/g' {} +`;
+
+await $`helm package --version ${version} helm`;
