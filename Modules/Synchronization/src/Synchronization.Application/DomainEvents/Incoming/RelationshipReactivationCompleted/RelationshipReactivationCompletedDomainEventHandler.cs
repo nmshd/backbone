21a3dcb5955281e7d9a648579fc76d8b62ce99ec
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-04-23 11:44:19 +0200

    merge release/v5 into branch

--- Modules/Synchronization/src/Synchronization.Application/DomainEvents/Incoming/RelationshipReactivationCompleted/RelationshipReactivationCompletedDomainEventHandler.cs
+++ Modules/Synchronization/src/Synchronization.Application/DomainEvents/Incoming/RelationshipReactivationCompleted/RelationshipReactivationCompletedDomainEventHandler.cs
@@ -1,30 +1,31 @@
 ﻿using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
 using Backbone.DevelopmentKit.Identity.ValueObjects;
 using Backbone.Modules.Synchronization.Application.Infrastructure;
-using Backbone.Modules.Synchronization.Application.IntegrationEvents.Outgoing;
+using Backbone.Modules.Synchronization.Application.DomainEvents.Outgoing;
 using Backbone.Modules.Synchronization.Domain.Entities.Sync;
 using Microsoft.Extensions.Logging;
+using Backbone.Modules.Synchronization.Domain.DomainEvents.Incoming.RelationshipReactivationCompleted;
 
-namespace Backbone.Modules.Synchronization.Application.IntegrationEvents.Incoming.RelationshipReactivationCompleted;
-public class RelationshipReactivationCompletedIntegrationEventHandler : IIntegrationEventHandler<RelationshipReactivationCompletedIntegrationEvent>
+namespace Backbone.Modules.Synchronization.Application.DomainEvents.Incoming.RelationshipReactivationCompleted;
+public class RelationshipReactivationCompletedDomainEventHandler : IDomainEventHandler<RelationshipReactivationCompletedDomainEvent>
 {
     private readonly ISynchronizationDbContext _dbContext;
     private readonly IEventBus _eventBus;
-    private readonly ILogger<RelationshipReactivationCompletedIntegrationEventHandler> _logger;
+    private readonly ILogger<RelationshipReactivationCompletedDomainEventHandler> _logger;
 
-    public RelationshipReactivationCompletedIntegrationEventHandler(ISynchronizationDbContext dbContext, IEventBus eventBus, ILogger<RelationshipReactivationCompletedIntegrationEventHandler> logger)
+    public RelationshipReactivationCompletedDomainEventHandler(ISynchronizationDbContext dbContext, IEventBus eventBus, ILogger<RelationshipReactivationCompletedDomainEventHandler> logger)
     {
         _dbContext = dbContext;
         _eventBus = eventBus;
         _logger = logger;
     }
 
-    public async Task Handle(RelationshipReactivationCompletedIntegrationEvent integrationEvent)
+    public async Task Handle(RelationshipReactivationCompletedDomainEvent integrationEvent)
     {
         await CreateExternalEvent(integrationEvent);
     }
 
-    private async Task CreateExternalEvent(RelationshipReactivationCompletedIntegrationEvent @event)
+    private async Task CreateExternalEvent(RelationshipReactivationCompletedDomainEvent @event)
     {
 #pragma warning disable IDE0037
         var payload = new { RelationshipId = @event.RelationshipId };
@@ -33,7 +34,7 @@ public class RelationshipReactivationCompletedIntegrationEventHandler : IIntegra
         {
             var externalEvent = await _dbContext.CreateExternalEvent(
                 IdentityAddress.Parse(@event.Peer), ExternalEventType.RelationshipReactivationCompleted, payload);
-            _eventBus.Publish(new ExternalEventCreatedIntegrationEvent(externalEvent));
+            _eventBus.Publish(new ExternalEventCreatedDomainEvent(externalEvent));
         }
         catch (Exception ex)
         {
