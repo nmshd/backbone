6e3cc92bdc3d2370e2db8685b6029b99fac3da56
Timo Notheisen <timo.notheisen@js-soft.com>
2024-04-24 07:20:16 +0200

    ci: fix script to replace appVersion

--- .ci/helm/buildChart.js
+++ .ci/helm/buildChart.js
@@ -8,6 +8,6 @@ const version = getRequiredEnvVar("VERSION");
 await $`helm dependency update helm`;
 
 // replace <<app_version>> with the value of `version` in all Chart.yaml files in helm folder
-await $`find helm -name Chart.yaml -exec sed -i -e 's/__app_version__/${version}/g' {} +`;
+await $`find helm -name Chart.yaml -exec sed -i -e 's/__appVersion__/${version}/g' {} +`;
 
 await $`helm package --version ${version} helm`;
