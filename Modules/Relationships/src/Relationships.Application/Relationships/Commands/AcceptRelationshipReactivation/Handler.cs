a378894dd654c1d57760ab56c43d5e1e16c50ec2
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-04-25 09:40:53 +0200

    feat: implement Handler

--- Modules/Relationships/src/Relationships.Application/Relationships/Commands/AcceptRelationshipReactivation/Handler.cs
+++ Modules/Relationships/src/Relationships.Application/Relationships/Commands/AcceptRelationshipReactivation/Handler.cs
@@ -1,10 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
+using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.UserContext;
+using Backbone.DevelopmentKit.Identity.ValueObjects;
+using Backbone.Modules.Relationships.Application.Infrastructure.Persistence.Repository;
+using Backbone.Modules.Relationships.Domain.Aggregates.Relationships;
+using Backbone.Modules.Relationships.Domain.DomainEvents.Outgoing;
+using MediatR;
 
 namespace Backbone.Modules.Relationships.Application.Relationships.Commands.AcceptRelationshipReactivation;
-internal class Handler
+public class Handler : IRequestHandler<AcceptRelationshipReactivationCommand, AcceptRelationshipReactivationResponse>
 {
+    private readonly IRelationshipsRepository _relationshipsRepository;
+    private readonly IEventBus _eventBus;
+    private readonly IdentityAddress _activeIdentity;
+    private readonly DeviceId _activeDevice;
+
+    public Handler(IRelationshipsRepository relationshipsRepository, IUserContext userContext, IEventBus eventBus)
+    {
+        _relationshipsRepository = relationshipsRepository;
+        _eventBus = eventBus;
+        _activeIdentity = userContext.GetAddress();
+        _activeDevice = userContext.GetDeviceId();
+    }
+
+    public async Task<AcceptRelationshipReactivationResponse> Handle(AcceptRelationshipReactivationCommand request, CancellationToken cancellationToken)
+    {
+        var relationshipId = RelationshipId.Parse(request.RelationshipId);
+        var relationship = await _relationshipsRepository.FindRelationship(relationshipId, _activeIdentity, cancellationToken, track: true);
+
+        relationship.AcceptReactivation(_activeIdentity, _activeDevice);
+
+        await _relationshipsRepository.Update(relationship);
+
+        _eventBus.Publish(new RelationshipStatusChangedDomainEvent(relationship));
+
+        return new AcceptRelationshipReactivationResponse(relationship);
+    }
 }
--- Modules/Relationships/test/Relationships.Application.Tests/Tests/Relationships/Commands/AcceptRelationshipReactivation/HandlerTests.cs
+++ Modules/Relationships/test/Relationships.Application.Tests/Tests/Relationships/Commands/AcceptRelationshipReactivation/HandlerTests.cs
@@ -1,6 +1,118 @@
-﻿using Xunit;
+﻿using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
+using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.UserContext;
+using Backbone.Modules.Relationships.Application.Infrastructure.Persistence.Repository;
+using Backbone.Modules.Relationships.Application.Relationships.Commands.AcceptRelationshipReactivation;
+using Backbone.Modules.Relationships.Application.Relationships.DTOs;
+using Backbone.Modules.Relationships.Application.Tests.TestHelpers;
+using Backbone.Modules.Relationships.Domain.Aggregates.Relationships;
+using Backbone.Modules.Relationships.Domain.DomainEvents.Outgoing;
+using Backbone.UnitTestTools.Data;
+using FakeItEasy;
+using FluentAssertions;
+using Xunit;
 
 namespace Backbone.Modules.Relationships.Application.Tests.Tests.Relationships.Commands.AcceptRelationshipReactivation;
 public class HandlerTests
 {
+    [Fact]
+    public async Task Returns_the_updated_relationship()
+    {
+        // Arrange
+        var activeIdentity = TestDataGenerator.CreateRandomIdentityAddress();
+        var activeDevice = TestDataGenerator.CreateRandomDeviceId();
+
+        var fakeRelationshipsRepository = A.Fake<IRelationshipsRepository>();
+        var relationship = TestData.CreateTerminatedRelationshipWithPendingReactivationRequest(activeIdentity);
+        A.CallTo(() => fakeRelationshipsRepository.FindRelationship(relationship.Id, activeIdentity, A<CancellationToken>._, true)).Returns(relationship);
+
+        var fakeUserContext = A.Fake<IUserContext>();
+        A.CallTo(() => fakeUserContext.GetAddress()).Returns(activeIdentity);
+        A.CallTo(() => fakeUserContext.GetDeviceId()).Returns(activeDevice);
+
+        var handler = CreateHandler(fakeRelationshipsRepository, fakeUserContext);
+
+        // Act
+        var response = await handler.Handle(new AcceptRelationshipReactivationCommand
+        {
+            RelationshipId = relationship.Id
+        }, CancellationToken.None);
+
+        // Assert
+        response.Id.Should().NotBeNull();
+        response.Status.Should().Be(RelationshipStatus.Active);
+        response.AuditLog.Should().HaveCount(5);
+    }
+
+    [Fact]
+    public async Task Saves_the_updated_relationship()
+    {
+        // Arrange
+        var activeIdentity = TestDataGenerator.CreateRandomIdentityAddress();
+        var activeDevice = TestDataGenerator.CreateRandomDeviceId();
+
+        var mockRelationshipsRepository = A.Fake<IRelationshipsRepository>();
+        var relationship = TestData.CreateTerminatedRelationshipWithPendingReactivationRequest(activeIdentity);
+        A.CallTo(() => mockRelationshipsRepository.FindRelationship(relationship.Id, activeIdentity, A<CancellationToken>._, true)).Returns(relationship);
+
+        var fakeUserContext = A.Fake<IUserContext>();
+        A.CallTo(() => fakeUserContext.GetAddress()).Returns(activeIdentity);
+        A.CallTo(() => fakeUserContext.GetDeviceId()).Returns(activeDevice);
+
+        var handler = CreateHandler(mockRelationshipsRepository, fakeUserContext);
+
+        // Act
+        await handler.Handle(new AcceptRelationshipReactivationCommand
+        {
+            RelationshipId = relationship.Id
+        }, CancellationToken.None);
+
+        // Assert
+        A.CallTo(
+                () => mockRelationshipsRepository.Update(
+                    A<Relationship>.That.Matches(r => r.Id == relationship.Id && r.Status == RelationshipStatus.Active))
+            )
+            .MustHaveHappenedOnceExactly();
+    }
+
+    [Fact]
+    public async Task Publishes_RelationshipStatusChangedDomainEvent()
+    {
+        // Arrange
+        var activeIdentity = TestDataGenerator.CreateRandomIdentityAddress();
+        var activeDevice = TestDataGenerator.CreateRandomDeviceId();
+
+        var fakeRelationshipsRepository = A.Fake<IRelationshipsRepository>();
+        var relationship = TestData.CreateTerminatedRelationshipWithPendingReactivationRequest(activeIdentity);
+        A.CallTo(() => fakeRelationshipsRepository.FindRelationship(relationship.Id, activeIdentity, A<CancellationToken>._, true)).Returns(relationship);
+
+        var fakeUserContext = A.Fake<IUserContext>();
+        A.CallTo(() => fakeUserContext.GetAddress()).Returns(activeIdentity);
+        A.CallTo(() => fakeUserContext.GetDeviceId()).Returns(activeDevice);
+
+        var mockEventBus = A.Fake<IEventBus>();
+
+        var handler = CreateHandler(fakeRelationshipsRepository, fakeUserContext, mockEventBus);
+
+        // Act
+        await handler.Handle(new AcceptRelationshipReactivationCommand
+        {
+            RelationshipId = relationship.Id
+        }, CancellationToken.None);
+
+        // Assert
+        A.CallTo(
+                () => mockEventBus.Publish(A<RelationshipStatusChangedDomainEvent>.That.Matches(e =>
+                    e.RelationshipId == relationship.Id &&
+                    e.Status == RelationshipStatus.Active.ToDtoString() &&
+                    e.Initiator == activeIdentity &&
+                    e.Peer == relationship.From)
+                ))
+            .MustHaveHappenedOnceExactly();
+    }
+
+    private static Handler CreateHandler(IRelationshipsRepository relationshipsRepository, IUserContext userContext, IEventBus? eventBus = null)
+    {
+        eventBus ??= A.Fake<IEventBus>();
+        return new Handler(relationshipsRepository, userContext, eventBus);
+    }
 }
