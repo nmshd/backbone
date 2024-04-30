7c236a274ecce890361d0f88739e731dd6e9f510
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-05-01 00:24:00 +0200

    feat: improve Count()

---
+++ Modules/Quotas/src/Quotas.Domain/Aggregates/Relationships/RelationshipAuditLogEntry.cs
@@ -0,0 +1,28 @@
+﻿namespace Backbone.Modules.Quotas.Domain.Aggregates.Relationships;
+
+public class RelationshipAuditLogEntry
+{
+    // ReSharper disable once UnusedMember.Local
+    private RelationshipAuditLogEntry()
+    {
+        // This constructor is for EF Core only; initializing the properties with null is therefore not a problem
+        Id = null!;
+    }
+
+    public string Id { get; set; }
+    public RelationshipAuditLogEntryReason Reason { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
+
+public enum RelationshipAuditLogEntryReason
+{
+    Creation = 0,
+    AcceptanceOfCreation = 1,
+    RejectionOfCreation = 2,
+    RevocationOfCreation = 3,
+    Termination = 4,
+    Reactivation = 5,
+    AcceptanceOfReactivation = 6,
+    RejectionOfReactivation = 7,
+    RevocationOfReactivation = 8
+}
---
+++ Modules/Quotas/src/Quotas.Infrastructure/Persistence/Database/EntityConfigurations/RelationshipAuditLogEntryEntityTypeConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Backbone.Modules.Quotas.Domain.Aggregates.Relationships;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Backbone.Modules.Quotas.Infrastructure.Persistence.Database.EntityConfigurations;
+public class RelationshipAuditLogEntryEntityTypeConfiguration : IEntityTypeConfiguration<RelationshipAuditLogEntry>
+{
+    public void Configure(EntityTypeBuilder<RelationshipAuditLogEntry> builder)
+    {
+        builder.ToTable("RelationshipAuditLog", "Relationships", x => x.ExcludeFromMigrations());
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Reason);
+        builder.Property(x => x.CreatedAt);
+    }
+}
