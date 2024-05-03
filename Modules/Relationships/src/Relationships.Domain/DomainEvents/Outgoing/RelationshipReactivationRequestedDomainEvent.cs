a3412898258cf844f262ae693fc3533e8152ddab
Timo Notheisen <timo.notheisen@js-soft.com>
2024-05-03 12:59:49 +0200

    fix: compiler errors

--- Modules/Relationships/src/Relationships.Domain/DomainEvents/Outgoing/RelationshipReactivationRequestedDomainEvent.cs
+++ Modules/Relationships/src/Relationships.Domain/DomainEvents/Outgoing/RelationshipReactivationRequestedDomainEvent.cs
@@ -3,14 +3,15 @@ using Backbone.DevelopmentKit.Identity.ValueObjects;
 using Backbone.Modules.Relationships.Domain.Aggregates.Relationships;
 
 namespace Backbone.Modules.Relationships.Domain.DomainEvents.Outgoing;
+
 public class RelationshipReactivationRequestedDomainEvent : DomainEvent
 {
     public RelationshipReactivationRequestedDomainEvent(Relationship relationship, IdentityAddress requestingIdentity, IdentityAddress peer) :
         base($"{relationship.Id}/ReactivationRequested/{relationship.AuditLog.Last().CreatedAt}")
     {
         RelationshipId = relationship.Id;
-        RequestingIdentity = requestingIdentity.StringValue;
-        Peer = peer.StringValue;
+        RequestingIdentity = requestingIdentity.Value;
+        Peer = peer.Value;
     }
 
 
