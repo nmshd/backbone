60777faec7a74e3779121edc70e6d9167710cb8c
Timo Notheisen <65653426+tnotheis@users.noreply.github.com>
2024-05-03 15:02:08 +0200

    Improve Admin API and Consumer API SDKs (#633)
    * refactor: specify api version in each route
    
    * feat: factory methods for consumer api Client
    
    * refactor: improve code quality
    
    * refactor: improve code quality
    
    * feat: add factory methods for Admin API SDK
    
    * refactor: introduce ConsumerApiEndpoint
    
    * refactor: use apiVersion on each endpoint instead of in http client
    
    * refactor: get rid of Configuration
    
    * chore: make method public
    
    * chore: fix namespace
    
    * chore: remove redundant parantheses
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ BuildingBlocks/src/BuildingBlocks.SDK/Crypto/CryptoExchangeAlgorithm.cs
@@ -0,0 +1,12 @@
+﻿// ReSharper disable InconsistentNaming
+
+#pragma warning disable IDE1006
+
+namespace Backbone.BuildingBlocks.SDK.Crypto;
+
+public enum CryptoExchangeAlgorithm
+{
+    ECDH_P256 = 1,
+    ECDH_P521 = 2,
+    ECDH_X25519 = 3
+}
