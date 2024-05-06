bee6ba332f9b1b324336e4c9697e05f3d770021d
Mika Herrmann <mika.herrmann@js-soft.com>
2024-05-06 12:56:27 +0200

    fix: Add fake event bus to AbstractDbContextBase constructor call

--- BuildingBlocks/src/UnitTestTools/TestDoubles/Fakes/FakeDbContextFactory.cs
+++ BuildingBlocks/src/UnitTestTools/TestDoubles/Fakes/FakeDbContextFactory.cs
@@ -1,3 +1,6 @@
+using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
+using Backbone.BuildingBlocks.Infrastructure.Persistence.Database;
+using FakeItEasy;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,7 +9,7 @@ namespace Backbone.UnitTestTools.TestDoubles.Fakes;
 public static class FakeDbContextFactory
 {
     public static (TContext arrangeContext, TContext assertionContext, TContext actContext)
-        CreateDbContexts<TContext>(SqliteConnection? connection = null) where TContext : DbContext
+        CreateDbContexts<TContext>(SqliteConnection? connection = null) where TContext : AbstractDbContextBase
     {
         connection ??= CreateDbConnection();
         connection.Open();
@@ -15,7 +18,7 @@ public static class FakeDbContextFactory
             .UseSqlite(connection)
             .Options;
 
-        object[] args = [options];
+        object[] args = [options, A.Fake<IEventBus>()];
 
         var context = (TContext)Activator.CreateInstance(typeof(TContext), args)!;
         context.Database.EnsureCreated();
