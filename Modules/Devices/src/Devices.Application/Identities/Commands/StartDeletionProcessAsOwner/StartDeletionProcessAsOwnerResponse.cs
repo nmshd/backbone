d4d842fa9c0a3bcdb7a9912d4400c652c8b4781b
Timo Notheisen <timo.notheisen@js-soft.com>
2024-05-06 15:28:46 +0200

    feat: return full deletion process on start

--- Modules/Devices/src/Devices.Application/Identities/Commands/StartDeletionProcessAsOwner/StartDeletionProcessAsOwnerResponse.cs
+++ Modules/Devices/src/Devices.Application/Identities/Commands/StartDeletionProcessAsOwner/StartDeletionProcessAsOwnerResponse.cs
@@ -1,26 +1,11 @@
-using Backbone.DevelopmentKit.Identity.ValueObjects;
+using Backbone.Modules.Devices.Application.DTOs;
 using Backbone.Modules.Devices.Domain.Entities.Identities;
 
 namespace Backbone.Modules.Devices.Application.Identities.Commands.StartDeletionProcessAsOwner;
 
-public class StartDeletionProcessAsOwnerResponse
+public class StartDeletionProcessAsOwnerResponse : IdentityDeletionProcessOverviewDTO
 {
-    public StartDeletionProcessAsOwnerResponse(IdentityDeletionProcess deletionProcess)
+    public StartDeletionProcessAsOwnerResponse(IdentityDeletionProcess deletionProcess) : base(deletionProcess)
     {
-        Id = deletionProcess.Id;
-        Status = deletionProcess.Status;
-        CreatedAt = deletionProcess.CreatedAt;
-        ApprovedAt = deletionProcess.ApprovedAt ?? throw new Exception($"The '{nameof(IdentityDeletionProcess.ApprovedAt)}' property of the given deletion process must not be null.");
-        ApprovedByDevice = deletionProcess.ApprovedByDevice ?? throw new Exception($"The '{nameof(IdentityDeletionProcess.ApprovedByDevice)}' property of the given deletion process must not be null.");
-        GracePeriodEndsAt = deletionProcess.GracePeriodEndsAt.GetValueOrDefault();
     }
-
-    public string Id { get; set; }
-    public DeletionProcessStatus Status { get; set; }
-    public DateTime CreatedAt { get; set; }
-
-    public DateTime ApprovedAt { get; set; }
-    public DeviceId ApprovedByDevice { get; set; }
-
-    public DateTime GracePeriodEndsAt { get; set; }
 }
