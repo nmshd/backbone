a6d76545e6caadc49f6bc862edb2c33e01f79918
Mika Herrmann <mika.herrmann@js-soft.com>
2024-05-06 13:06:24 +0200

    fix: Make IdentityPublicKey a byte array (again)

--- AdminApi.Sdk/Endpoints/Identities/Types/Requests/CreateIdentityRequest.cs
+++ AdminApi.Sdk/Endpoints/Identities/Types/Requests/CreateIdentityRequest.cs
@@ -6,7 +6,7 @@ public class CreateIdentityRequest
 {
     public required string ClientId { get; set; }
     public required string ClientSecret { get; set; }
-    public required string IdentityPublicKey { get; set; }
+    public required byte[] IdentityPublicKey { get; set; }
     public required string DevicePassword { get; set; }
     public required byte IdentityVersion { get; set; }
     public required SignedChallenge SignedChallenge { get; set; }
