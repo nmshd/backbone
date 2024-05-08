70b82b1f464f023a8772528ab7f3fa63b632c511
Julian König <julian.koenig@js-soft.com>
2024-05-08 17:21:56 +0200

    feat: card around the table

--- AdminUi/apps/admin_ui/lib/home/tiers_detail/tiers_detail.dart
+++ AdminUi/apps/admin_ui/lib/home/tiers_detail/tiers_detail.dart
@@ -224,46 +224,52 @@ class _IdentitiesListState extends State<_IdentitiesList> {
       title: const Text('Identities'),
       subtitle: const Text('View Identities associated with this Tier.'),
       children: [
-        IdentitiesFilter(
-          fixedTierId: widget.tierDetails.id,
-          onFilterChanged: ({IdentityOverviewFilter? filter}) async {
-            _dataSource
-              ..filter = filter
-              ..refreshDatasource();
-          },
-        ),
-        SizedBox(
-          height: 500,
-          child: AsyncPaginatedDataTable2(
-            rowsPerPage: _rowsPerPage,
-            onRowsPerPageChanged: _setRowsPerPage,
-            sortColumnIndex: _sortColumnIndex,
-            sortAscending: _sortColumnAscending,
-            showFirstLastButtons: true,
-            columnSpacing: 5,
-            source: _dataSource,
-            isVerticalScrollBarVisible: true,
-            renderEmptyRowsInTheEnd: false,
-            availableRowsPerPage: const [5, 10, 25, 50, 100],
-            errorBuilder: (error) => Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Text('An error occurred loading the data.'),
-                  Gaps.h16,
-                  FilledButton(onPressed: _dataSource.refreshDatasource, child: const Text('Retry')),
-                ],
+        Card(
+          child: Column(
+            children: [
+              IdentitiesFilter(
+                fixedTierId: widget.tierDetails.id,
+                onFilterChanged: ({IdentityOverviewFilter? filter}) async {
+                  _dataSource
+                    ..filter = filter
+                    ..refreshDatasource();
+                },
               ),
-            ),
-            columns: <DataColumn2>[
-              DataColumn2(label: const Text('Address'), size: ColumnSize.L, onSort: _sort),
-              const DataColumn2(label: Text('Tier'), size: ColumnSize.S),
-              DataColumn2(label: const Text('Created with Client'), onSort: _sort),
-              DataColumn2(label: const Text('Number of Devices'), onSort: _sort),
-              DataColumn2(label: const Text('Created at'), size: ColumnSize.S, onSort: _sort),
-              DataColumn2(label: const Text('Last Login at'), size: ColumnSize.S, onSort: _sort),
-              DataColumn2(label: const Text('Datawallet version'), onSort: _sort),
-              DataColumn2(label: const Text('Identity Version'), onSort: _sort),
+              SizedBox(
+                height: 500,
+                child: AsyncPaginatedDataTable2(
+                  rowsPerPage: _rowsPerPage,
+                  onRowsPerPageChanged: _setRowsPerPage,
+                  sortColumnIndex: _sortColumnIndex,
+                  sortAscending: _sortColumnAscending,
+                  showFirstLastButtons: true,
+                  columnSpacing: 5,
+                  source: _dataSource,
+                  isVerticalScrollBarVisible: true,
+                  renderEmptyRowsInTheEnd: false,
+                  availableRowsPerPage: const [5, 10, 25, 50, 100],
+                  errorBuilder: (error) => Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Text('An error occurred loading the data.'),
+                        Gaps.h16,
+                        FilledButton(onPressed: _dataSource.refreshDatasource, child: const Text('Retry')),
+                      ],
+                    ),
+                  ),
+                  columns: <DataColumn2>[
+                    DataColumn2(label: const Text('Address'), size: ColumnSize.L, onSort: _sort),
+                    const DataColumn2(label: Text('Tier'), size: ColumnSize.S),
+                    DataColumn2(label: const Text('Created with Client'), onSort: _sort),
+                    DataColumn2(label: const Text('Number of Devices'), onSort: _sort),
+                    DataColumn2(label: const Text('Created at'), size: ColumnSize.S, onSort: _sort),
+                    DataColumn2(label: const Text('Last Login at'), size: ColumnSize.S, onSort: _sort),
+                    DataColumn2(label: const Text('Datawallet version'), onSort: _sort),
+                    DataColumn2(label: const Text('Identity Version'), onSort: _sort),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
