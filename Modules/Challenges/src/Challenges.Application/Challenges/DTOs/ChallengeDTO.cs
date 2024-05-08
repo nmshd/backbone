b63a921d604e757b467608a1abb3b7a8b1226dde
Daniel Almeida <115644988+daniel-almeida-konkconsulting@users.noreply.github.com>
2024-05-08 14:55:38 +0100

    fix: add custom mapping for challenge id due to lack of json converter

--- Modules/Challenges/src/Challenges.Application/Challenges/DTOs/ChallengeDTO.cs
+++ Modules/Challenges/src/Challenges.Application/Challenges/DTOs/ChallengeDTO.cs
@@ -1,12 +1,19 @@
+using AutoMapper;
 using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.Mapping;
 using Backbone.Modules.Challenges.Domain.Entities;
 
 namespace Backbone.Modules.Challenges.Application.Challenges.DTOs;
 
-public class ChallengeDTO : IMapTo<Challenge>
+public class ChallengeDTO : IHaveCustomMapping
 {
     public required string Id { get; set; }
     public required DateTime ExpiresAt { get; set; }
     public string? CreatedBy { get; set; }
     public string? CreatedByDevice { get; set; }
+
+    public void CreateMappings(Profile configuration)
+    {
+        configuration.CreateMap<Challenge, ChallengeDTO>()
+            .ForMember(dto => dto.Id, expression => expression.MapFrom(m => m.Id.Value));
+    }
 }
