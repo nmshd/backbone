ee9c5f4ee6ed471cf3af7db35cccea7b4530ab55
Daniel Silva <daniel.p.silva@konkconsulting.com>
2024-05-10 16:47:48 +0100

    chore: remove unwanted parameters

--- Modules/Devices/src/Devices.Application/Infrastructure/PushNotifications/DeletionProcess/DeletionProcessCancelledBySupportNotification.cs
+++ Modules/Devices/src/Devices.Application/Infrastructure/PushNotifications/DeletionProcess/DeletionProcessCancelledBySupportNotification.cs
@@ -1,4 +1,4 @@
 namespace Backbone.Modules.Devices.Application.Infrastructure.PushNotifications.DeletionProcess;
 
 [NotificationText(Title = "A deletion process has been cancelled", Body = "One of your identity's deletion processes was cancelled by the support team.")]
-public record DeletionProcessCancelledBySupportNotification(int DaysUntilDeletion);
+public record DeletionProcessCancelledBySupportNotification();
