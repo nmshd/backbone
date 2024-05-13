219d0f2b387e8a94ff8c83960177a7a67d0c5fab
Mika Aaron Herrmann <65243124+MH321Productions@users.noreply.github.com>
2024-05-13 12:56:44 +0200

    Integrate domain events into Domain (Synchronization Module) (#635)
    * fix: Move DomainEvents into correct namespace
    
    * feat: Add IEventBus to AbstractDbContextBase
    
    * feat: Add IEventBus to AbstractDbContextBase
    
    * feat: Add Entity class and set subclasses
    
    * chore: Add utility method to publish multiple events
    
    * feat: Use RaiseDomainEvent for PushDatawalletModificationsCommand
    
    * feat: Add first overridden save method
    
    * fix: Rewrite Entity again to use StronglyTypedId records
    
    * chore: Remove redundant event bus calls
    
    * feat: Implement other SaveChanges methods
    
    * feat: Raise ExternalEventCreatedDomainEvent when creating the external event
    
    * fix: Undo rearranging of code
    
    * chore: Remove GetRealType method
    
    * chore: Use dummy event busses instead of fake ones
    
    * chore: Remove the redundant event bus parameter
    
    * fix: Let tests check if the external event is created and if the domain event is valid
    
    * fix: Apply same checks to new (merged) tests
    
    * fix: Add fake event bus to AbstractDbContextBase constructor call
    
    * fix: add dummy event bus for database migrator to avoid runtime error
    
    * chore: add accessibility modifier to DummyEventBus
    
    * feat: turn off event publishing for all but synchronization module
    
    * feat: inherit from EntityEntityTypeConfiguration
    
    * fix: make EntityEntityTypeConfiguration virtual
    
    * chore: remove `publishDomainEvents` parameter, as it is not required
    
    * fix: Make fake event bus a dummy event bus
    
    * chore: Change "Handle" to "Assert"
    
    * chore: Move domain tests from handler tests into own domain test
    
    * chore: Remove test which is not needed anymore (already tested by another test)
    
    * chore: Rename identity and don't parse it to an IdentityAddress (bc it already is)
    
    * feat: Add FluentAssertions extension methods for entities
    
    * chore: Rename ExternalEventCreatedDomainEventTests to ExternalEventTests
    
    * test: Add domain test for Datawallet
    
    * chore: Move the domain tests into the domain tests project
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: Timo Notheisen <timo.notheisen@js-soft.com>

---
+++ BuildingBlocks/src/UnitTestTools/FluentAssertions/Assertions/EntityAssertions.cs
@@ -0,0 +1,31 @@
+﻿using Backbone.BuildingBlocks.Domain;
+using Backbone.BuildingBlocks.Domain.Events;
+using FluentAssertions.Execution;
+using FluentAssertions.Primitives;
+
+namespace Backbone.UnitTestTools.FluentAssertions.Assertions;
+
+public class EntityAssertions : ReferenceTypeAssertions<Entity, EntityAssertions>
+{
+    public EntityAssertions(Entity instance) : base(instance)
+    {
+    }
+
+    protected override string Identifier => "entity";
+
+    public TEvent HaveASingleDomainEvent<TEvent>(string because = "", params object[] becauseArgs) where TEvent : DomainEvent
+    {
+        Execute.Assertion
+            .BecauseOf(because, becauseArgs)
+            .Given(() => Subject.DomainEvents)
+            .ForCondition(events => events.Count == 1)
+            .FailWith("Expected {context:entity} to have 1 domain event, but found {0}.",
+                Subject.DomainEvents.Count)
+            .Then
+            .ForCondition(events => events[0].GetType() == typeof(TEvent))
+            .FailWith("Expected the domain event to be of type {0}, but found {1}.",
+                typeof(TEvent), Subject.DomainEvents[0].GetType());
+
+        return (TEvent)Subject.DomainEvents[0];
+    }
+}
--- Modules/Synchronization/src/Synchronization.Infrastructure/Persistence/Database/Configurations/DatawalletEntityTypeConfiguration.cs
+++ Modules/Synchronization/src/Synchronization.Infrastructure/Persistence/Database/Configurations/DatawalletEntityTypeConfiguration.cs
@@ -1,13 +1,16 @@
+using Backbone.BuildingBlocks.Infrastructure.Persistence.Database.EntityTypeConfigurations;
 using Backbone.Modules.Synchronization.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Backbone.Modules.Synchronization.Infrastructure.Persistence.Database.Configurations;
 
-public class DatawalletEntityTypeConfiguration : IEntityTypeConfiguration<Datawallet>
+public class DatawalletEntityTypeConfiguration : EntityEntityTypeConfiguration<Datawallet>
 {
-    public void Configure(EntityTypeBuilder<Datawallet> builder)
+    public override void Configure(EntityTypeBuilder<Datawallet> builder)
     {
+        base.Configure(builder);
+
         builder.HasIndex(p => p.Owner).IsUnique();
 
         builder.HasKey(x => x.Id);
