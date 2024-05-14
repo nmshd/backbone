4b5984edcad4f544abe4d0222dbb288a5283df02
Vladimir Vuckovic <vidrak97@gmail.com>
2024-05-14 07:07:33 +0200

    chore: get identity details

--- AdminUi/apps/admin_ui/lib/home/identity_details/identity_details.dart
+++ AdminUi/apps/admin_ui/lib/home/identity_details/identity_details.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
 
+import 'package:admin_api_sdk/admin_api_sdk.dart';
+import 'package:admin_api_types/admin_api_types.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 class IdentityDetails extends StatefulWidget {
   final String address;
@@ -12,6 +15,7 @@ class IdentityDetails extends StatefulWidget {
 }
 
 class _IdentityDetailsState extends State<IdentityDetails> {
+  Identity? _identityDetails;
   late final ScrollController _scrollController;
 
   @override
@@ -19,6 +23,8 @@ class _IdentityDetailsState extends State<IdentityDetails> {
     super.initState();
 
     _scrollController = ScrollController();
+
+    _reload();
   }
 
   @override
@@ -30,6 +36,9 @@ class _IdentityDetailsState extends State<IdentityDetails> {
 
   @override
   Widget build(BuildContext context) {
+    if (_identityDetails == null) return const Center(child: CircularProgressIndicator());
+
+    final identityDetails = _identityDetails!;
     return Scrollbar(
       controller: _scrollController,
       child: SingleChildScrollView(
@@ -46,11 +55,19 @@ class _IdentityDetailsState extends State<IdentityDetails> {
                       padding: const EdgeInsets.all(16),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
+                          Text.rich(
+                            TextSpan(
+                              text: 'Identities Overview',
+                              style: Theme.of(context).textTheme.bodyLarge!.copyWith(fontWeight: FontWeight.bold),
+                            ),
+                          ),
                           Text.rich(
                             TextSpan(
                               children: [
                                 TextSpan(text: 'Address: ', style: Theme.of(context).textTheme.bodyLarge!.copyWith(fontWeight: FontWeight.bold)),
+                                TextSpan(text: identityDetails.address, style: Theme.of(context).textTheme.bodyLarge),
                               ],
                             ),
                           ),
@@ -58,6 +75,7 @@ class _IdentityDetailsState extends State<IdentityDetails> {
                             TextSpan(
                               children: [
                                 TextSpan(text: 'Client ID: ', style: Theme.of(context).textTheme.bodyLarge!.copyWith(fontWeight: FontWeight.bold)),
+                                TextSpan(text: identityDetails.clientId, style: Theme.of(context).textTheme.bodyLarge),
                               ],
                             ),
                           ),
@@ -72,5 +90,10 @@ class _IdentityDetailsState extends State<IdentityDetails> {
         ),
       ),
     );
+  }
+
+  Future<void> _reload() async {
+    final identityDetails = await GetIt.I.get<AdminApiClient>().identities.getIdentity(widget.address);
+    if (mounted) setState(() => _identityDetails = identityDetails.data);
   }
 }
