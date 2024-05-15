17ca4a6a2f98c19816b54aa82ca23bd5561fbafa
Daniel Silva <daniel.p.silva@konkconsulting.com>
2024-05-15 16:12:27 +0100

    chore: code review changes

--- ConsumerApi.Sdk/Endpoints/Challenges/ChallengesEndpoint.cs
+++ ConsumerApi.Sdk/Endpoints/Challenges/ChallengesEndpoint.cs
@@ -6,9 +6,23 @@ namespace Backbone.ConsumerApi.Sdk.Endpoints.Challenges;
 
 public class ChallengesEndpoint(EndpointClient client) : ConsumerApiEndpoint(client)
 {
-    public async Task<ApiResponse<Challenge>> CreateChallenge() => await _client.Post<Challenge>($"api/{API_VERSION}/Challenges");
-    public async Task<ApiResponse<Challenge>> CreateChallengeUnauthenticated() => await _client.PostUnauthenticated<Challenge>($"api/{API_VERSION}/Challenges");
+    public async Task<ApiResponse<Challenge>> CreateChallenge()
+    {
+        return await _client.Post<Challenge>("Challenges");
+    }
 
-    public async Task<ApiResponse<Challenge>> GetChallenge(string id) => await _client.Get<Challenge>($"api/{API_VERSION}/Challenges/{id}");
-    public async Task<ApiResponse<Challenge>> GetChallengeUnauthenticated(string id) => await _client.GetUnauthenticated<Challenge>($"api/{API_VERSION}/Challenges/{id}");
+    public async Task<ApiResponse<Challenge>> CreateChallengeUnauthenticated()
+    {
+        return await _client.PostUnauthenticated<Challenge>("Challenges");
+    }
+    
+    public async Task<ApiResponse<Challenge>> GetChallenge(string id)
+    {
+        return await _client.Get<Challenge>($"Challenges/{id}");
+    }
+
+    public async Task<ApiResponse<Challenge>> GetChallengeUnauthenticated(string id)
+    {
+        return  await _client.GetUnauthenticated<Challenge>($"api/{API_VERSION}/Challenges/{id}");
+    }
 }
