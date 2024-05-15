53194099d4df140b9f4b0a819f8d34338858ae74
Daniel Almeida <115644988+daniel-almeida-konkconsulting@users.noreply.github.com>
2024-05-15 15:40:03 +0100

    refactor: properties below constructor

--- AdminApi.Sdk/Endpoints/Messages/Types/MessageType.cs
+++ AdminApi.Sdk/Endpoints/Messages/Types/MessageType.cs
@@ -5,10 +5,10 @@ public record MessageType
     public static readonly MessageType INCOMING = new("Incoming");
     public static readonly MessageType OUTGOING = new("Outgoing");
 
-    public string Value { get; }
-
     public MessageType(string value)
     {
         Value = value;
     }
+
+    public string Value { get; }
 }
