b689a6020be67759514aebb8a6e27e5aecd7530b
Daniel Silva <daniel.p.silva@konkconsulting.com>
2024-05-16 14:53:29 +0100

    Usage of Consumer API SDK for running challenges integration tests (#610)
    * fix: consumer SDK generating paths witl backslashes
    
    * chore: merge main
    
    * feat: add password helper
    
    * chore: add basic AccountController
    
    * feat: identity creation using consumer sdk
    
    * refactor: format and extract classes
    
    * feat: complete example for challenges integration tests
    
    * chore: refactor
    
    * fix: not using DI Factory
    
    * fix: cannot parse body when there is no body to parse
    
    * chore: remove unused method
    
    * refactor: when string
    
    * chore: fix merge
    
    * chore: code review fixes
    
    * fix: missing using
    
    * chore: formatting
    
    * chore: move PasswordHelper
    
    * chore: remove accountController
    
    * chore: identityPublicKey should be passed as a Bas64 string
    
    * fix: using bad dsk method for challenge creation
    
    * chore: ensure empty payloads do not break the sdk
    
    * chore: code review changes
    
    * refactor: use fluent assertion extensions and fix challenge creation with byte representation
    
    * chore: remove unauthorized integration test
    
    * refactor: GivenTheUserIsAuthenticated
    
    * refactor: remove exception handling for response deserialization
    
    * chore: revert changes to test appsettings
    
    * chore: remove default _sdk variable for ChallengesApiStepDefinitions
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: Daniel Almeida <115644988+daniel-almeida-konkconsulting@users.noreply.github.com>

--- ConsumerApi.Sdk/Endpoints/Identities/IdentitiesEndpoint.cs
+++ ConsumerApi.Sdk/Endpoints/Identities/IdentitiesEndpoint.cs
@@ -9,17 +9,27 @@ namespace Backbone.ConsumerApi.Sdk.Endpoints.Identities;
 public class IdentitiesEndpoint(EndpointClient client) : ConsumerApiEndpoint(client)
 {
     public async Task<ApiResponse<CreateIdentityResponse>> CreateIdentity(CreateIdentityRequest request)
-        => await _client.PostUnauthenticated<CreateIdentityResponse>($"api/{API_VERSION}/Identities", request);
+    {
+        return await _client.PostUnauthenticated<CreateIdentityResponse>($"api/{API_VERSION}/Identities", request);
+    }
 
     public async Task<ApiResponse<StartDeletionProcessResponse>> StartDeletionProcess()
-        => await _client.Post<StartDeletionProcessResponse>($"api/{API_VERSION}/Identities/Self/DeletionProcesses");
+    {
+        return await _client.Post<StartDeletionProcessResponse>($"api/{API_VERSION}/Identities/Self/DeletionProcesses");
+    }
 
     public async Task<ApiResponse<ListDeletionProcessesResponse>> ListDeletionProcesses()
-        => await _client.Get<ListDeletionProcessesResponse>($"api/{API_VERSION}/Identities/Self/DeletionProcesses");
+    {
+        return await _client.Get<ListDeletionProcessesResponse>($"api/{API_VERSION}/Identities/Self/DeletionProcesses");
+    }
 
     public async Task<ApiResponse<IdentityDeletionProcess>> GetDeletionProcess(string id)
-        => await _client.Get<IdentityDeletionProcess>($"api/{API_VERSION}/Identities/Self/DeletionProcesses/{id}");
+    {
+        return await _client.Get<IdentityDeletionProcess>($"api/{API_VERSION}/Identities/Self/DeletionProcesses/{id}");
+    }
 
     public async Task<ApiResponse<ApproveDeletionProcessResponse>> ApproveDeletionProcess(string id)
-        => await _client.Put<ApproveDeletionProcessResponse>($"api/{API_VERSION}/Identities/Self/DeletionProcesses/{id}/Approve");
+    {
+        return await _client.Put<ApproveDeletionProcessResponse>($"api/{API_VERSION}/Identities/Self/DeletionProcesses/{id}/Approve");
+    }
 }
--- ConsumerApi.Tests.Integration/Features/Challenges/{id}/GET.feature
+++ ConsumerApi.Tests.Integration/Features/Challenges/{id}/GET.feature
@@ -10,12 +10,6 @@ Scenario: Requesting a Challenge as an authenticated user
 	Then the response status code is 200 (OK)
 	And the response contains a Challenge
 
-Scenario: Requesting a Challenge as an anonymous user
-	Given the user is unauthenticated
-	And a Challenge c
-	When a GET request is sent to the Challenges/{id} endpoint with c.Id
-	Then the response status code is 401 (Unauthorized)
-
 Scenario: Requesting a nonexistent Challenge as an authenticated user
 	Given the user is authenticated
 	When a GET request is sent to the Challenges/{id} endpoint with "CHLthisisnonexisting"
