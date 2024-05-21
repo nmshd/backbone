c8101b3f1cef92e33804e3768c5e401e7e1993a2
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-05-21 14:12:01 +0200

    feat: add migration

---
+++ Modules/Devices/src/Devices.Infrastructure.Database.SqlServer/Migrations/20240521113634_PreparationForTranslatableAuditLogEntries.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backbone.Modules.Devices.Infrastructure.Database.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class PreparationForTranslatableAuditLogEntries : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Message",
+                schema: "Devices",
+                table: "IdentityDeletionProcessAuditLog",
+                newName: "MessageKey");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MessageKey",
+                schema: "Devices",
+                table: "IdentityDeletionProcessAuditLog",
+                newName: "Message");
+        }
+    }
+}
