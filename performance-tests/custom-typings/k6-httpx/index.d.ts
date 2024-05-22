d802c510fd9b7148e11b3582b7df5b2a9cd277b6
Timo Notheisen <timo.notheisen@js-soft.com>
2024-05-22 07:43:24 +0200

    chore: fix custom typings

--- performance-tests/custom-typings/k6-httpx/index.d.ts
+++ performance-tests/custom-typings/k6-httpx/index.d.ts
@@ -1,11 +1,9 @@
-import { Response } from "k6/http";
 declare module "https://jslib.k6.io/httpx/0.1.0/index.js" {
-
     export class Httpx {
         constructor(options: Options);
-        get(...args: any[]): Response;
-        post(...args: any[]): Response;
-        put(...args: any[]): Response;
+        get(...args: any[]): any;
+        post(...args: any[]): any;
+        put(...args: any[]): any;
     }
 
     export interface Options {
