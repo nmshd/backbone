f8dbce1a4032275b43caf1f4c730a248a0bb1fbd
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-05-23 21:15:42 +0200

    feat: replace old strings from Message column

--- Modules/Devices/src/Devices.Infrastructure.Database.SqlServer/Migrations/20240522063206_PreparationForTranslatableAuditLogEntries.cs
+++ Modules/Devices/src/Devices.Infrastructure.Database.SqlServer/Migrations/20240522063206_PreparationForTranslatableAuditLogEntries.cs
@@ -15,6 +15,26 @@ namespace Backbone.Modules.Devices.Infrastructure.Database.SqlServer.Migrations
                 schema: "Devices",
                 table: "IdentityDeletionProcessAuditLog",
                 newName: "MessageKey");
+
+            migrationBuilder.Sql(@"
+                UPDATE [Devices].[IdentityDeletionProcessAuditLog]
+                SET [MessageKey] = CASE [MessageKey]
+                    WHEN 'The deletion process was started by the owner. It was automatically approved.' THEN 'ProcessStartedByOwner'
+                    WHEN 'The deletion process was started by support. It is now waiting for approval.' THEN 'ProcessStartedBySupport'
+                    WHEN 'The deletion process was approved.' THEN 'ProcessApproved'
+                    WHEN 'The deletion process was rejected.' THEN 'ProcessRejected'
+                    WHEN 'The deletion process was cancelled by the owner of the identity.' THEN 'ProcessCancelledByOwner'
+                    WHEN 'The deletion process was cancelled by a support employee.' THEN 'ProcessCancelledBySupport'
+                    WHEN 'The deletion process was cancelled automatically, because it wasn''t approved by the owner within the approval period.' THEN 'ProcessCancelledAutomatically'
+                    WHEN 'The first approval reminder notification has been sent.' THEN 'ApprovalReminder1Sent'
+                    WHEN 'The second approval reminder notification has been sent.' THEN 'ApprovalReminder2Sent'
+                    WHEN 'The third approval reminder notification has been sent.' THEN 'ApprovalReminder3Sent'
+                    WHEN 'The first grace period reminder notification has been sent.' THEN 'GracePeriodReminder1Sent'
+                    WHEN 'The second grace period reminder notification has been sent.' THEN 'GracePeriodReminder2Sent'
+                    WHEN 'The third grace period reminder notification has been sent.' THEN 'GracePeriodReminder3Sent'
+                    ELSE [MessageKey]
+                END;
+            ");
         }
 
         /// <inheritdoc />
@@ -25,6 +45,26 @@ namespace Backbone.Modules.Devices.Infrastructure.Database.SqlServer.Migrations
                 schema: "Devices",
                 table: "IdentityDeletionProcessAuditLog",
                 newName: "Message");
+
+            migrationBuilder.Sql(@"
+                UPDATE [Devices].[IdentityDeletionProcessAuditLog]
+                SET [Message] = CASE [Message]
+                    WHEN 'ProcessStartedByOwner' THEN 'The deletion process was started by the owner. It was automatically approved.'
+                    WHEN 'ProcessStartedBySupport' THEN 'The deletion process was started by support. It is now waiting for approval.'
+                    WHEN 'ProcessApproved' THEN 'The deletion process was approved.'
+                    WHEN 'ProcessRejected' THEN 'The deletion process was rejected.'
+                    WHEN 'ProcessCancelledByOwner' THEN 'The deletion process was cancelled by the owner of the identity.'
+                    WHEN 'ProcessCancelledBySupport' THEN 'The deletion process was cancelled by a support employee.'
+                    WHEN 'ProcessCancelledAutomatically' THEN 'The deletion process was cancelled automatically, because it wasn''t approved by the owner within the approval period.'
+                    WHEN 'ApprovalReminder1Sent' THEN 'The first approval reminder notification has been sent.'
+                    WHEN 'ApprovalReminder2Sent' THEN 'The second approval reminder notification has been sent.'
+                    WHEN 'ApprovalReminder3Sent' THEN 'The third approval reminder notification has been sent.'
+                    WHEN 'GracePeriodReminder1Sent' THEN 'The first grace period reminder notification has been sent.'
+                    WHEN 'GracePeriodReminder2Sent' THEN 'The second grace period reminder notification has been sent.'
+                    WHEN 'GracePeriodReminder3Sent' THEN 'The third grace period reminder notification has been sent.'
+                    ELSE [Message]
+                END;
+            ");
         }
     }
 }
