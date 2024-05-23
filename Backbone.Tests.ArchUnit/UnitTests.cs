987152d6232e605f6d8c6c5f8b2686d88ae9985b
Daniel Almeida <115644988+daniel-almeida-konkconsulting@users.noreply.github.com>
2024-05-23 13:15:27 +0100

    test: add arch unit test to ensure unit tests inherit from AbstractTestsBase

---
+++ Backbone.Tests.ArchUnit/UnitTests.cs
@@ -0,0 +1,17 @@
+﻿using ArchUnitNET.xUnit;
+using Backbone.UnitTestTools.BaseClasses;
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
+
+namespace Backbone.Backbone.Tests.ArchUnit;
+
+public class UnitTests
+{
+    [Fact]
+    public void QueriesShouldResideInQueriesNamespace()
+    {
+        Classes().That().HaveName(".+Tests$", true)
+            .And().AreNot(typeof(UnitTests))
+            .Should().BeAssignableTo(typeof(AbstractTestsBase))
+            .Check(Backbone.ARCHITECTURE);
+    }
+}
