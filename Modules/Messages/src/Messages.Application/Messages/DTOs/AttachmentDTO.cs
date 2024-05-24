a1700c4fd339aba6ee209174c328bfb355d4458d
Mika Herrmann <mika.herrmann@js-soft.com>
2024-05-24 13:41:18 +0200

    fix: Give DTOs a custom mapping

--- Modules/Messages/src/Messages.Application/Messages/DTOs/AttachmentDTO.cs
+++ Modules/Messages/src/Messages.Application/Messages/DTOs/AttachmentDTO.cs
@@ -1,10 +1,16 @@
+using AutoMapper;
 using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.Mapping;
 using Backbone.Modules.Messages.Domain.Entities;
-using Backbone.Modules.Messages.Domain.Ids;
 
 namespace Backbone.Modules.Messages.Application.Messages.DTOs;
 
-public class AttachmentDTO : IMapTo<Attachment>
+public class AttachmentDTO : IHaveCustomMapping
 {
-    public required FileId Id { get; set; }
+    public required string Id { get; set; }
+
+    public void CreateMappings(Profile configuration)
+    {
+        configuration.CreateMap<Attachment, AttachmentDTO>()
+            .ForMember(dto => dto.Id, expression => expression.MapFrom(e => e.Id.Value));
+    }
 }
