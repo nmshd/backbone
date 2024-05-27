e2fe93c09ca4aa7e96c1c06d2a3f27a3f3a287f8
Julian König <julian.koenig@js-soft.com>
2024-05-27 15:04:19 +0200

    feat: add clipboard utilities

---
+++ AdminUi/apps/admin_ui/lib/core/widgets/copy_to_clipboard_button.dart
@@ -0,0 +1,31 @@
+import 'package:admin_ui/core/core.dart';
+import 'package:flutter/material.dart';
+
+class CopyToClipboardButton extends StatelessWidget {
+  final String clipboardText;
+  final String successMessage;
+
+  final ButtonStyle? style;
+  final String? tooltip;
+
+  const CopyToClipboardButton({
+    required this.clipboardText,
+    required this.successMessage,
+    this.style,
+    this.tooltip,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.copy),
+      style: style,
+      tooltip: tooltip,
+      onPressed: () => context.setClipboardDataWithSuccessNotification(
+        clipboardText: clipboardText,
+        successMessage: successMessage,
+      ),
+    );
+  }
+}
