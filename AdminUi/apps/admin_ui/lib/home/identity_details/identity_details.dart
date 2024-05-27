2cc4bc9f3b1248907d14c4c6eb64630ee0d80186
Vladimir Vuckovic <vidrak97@gmail.com>
2024-05-27 11:08:30 +0200

    fix: add trailing comma

--- AdminUi/apps/admin_ui/lib/home/identity_details/identity_details.dart
+++ AdminUi/apps/admin_ui/lib/home/identity_details/identity_details.dart
@@ -103,7 +103,7 @@ class _IdentityDetailsState extends State<IdentityDetails> {
                                     items: _buildTierDropdownItems(context),
                                   ),
                                 ],
-                              )
+                              ),
                             ],
                           ),
                         ],
