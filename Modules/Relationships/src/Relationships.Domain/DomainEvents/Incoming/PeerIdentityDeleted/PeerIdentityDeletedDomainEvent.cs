806c8a10c7b4df37b4bb1b3c1362ae2eb82b39b5
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-06-06 10:45:36 +0200

    feat: add PeerIdentityDeleted incoming event to Relationships module

---
+++ Modules/Relationships/src/Relationships.Domain/DomainEvents/Incoming/PeerIdentityDeleted/PeerIdentityDeletedDomainEvent.cs
@@ -0,0 +1,15 @@
+﻿using Backbone.BuildingBlocks.Domain.Events;
+using Backbone.DevelopmentKit.Identity.ValueObjects;
+
+namespace Backbone.Modules.Relationships.Domain.DomainEvents.Incoming.PeerIdentityDeleted;
+public class PeerIdentityDeletedDomainEvent : DomainEvent
+{
+    public PeerIdentityDeletedDomainEvent(string relationshipId, IdentityAddress identityAddress)
+    {
+        IdentityAddress = identityAddress;
+        RelationshipId = relationshipId;
+    }
+
+    public string IdentityAddress { get; }
+    public string RelationshipId { get; }
+}
---
+++ Modules/Relationships/src/Relationships.Domain/DomainEvents/Outgoing/PeerIdentityDeletedDomainEvent.cs
@@ -0,0 +1,15 @@
+﻿using Backbone.BuildingBlocks.Domain.Events;
+using Backbone.DevelopmentKit.Identity.ValueObjects;
+
+namespace Backbone.Modules.Relationships.Domain.DomainEvents.Outgoing;
+public class PeerIdentityDeletedDomainEvent : DomainEvent
+{
+    public PeerIdentityDeletedDomainEvent(IdentityAddress identityAddress, string relationshipId) : base($"{identityAddress}/PeerIdentityDeleted/{relationshipId}")
+    {
+        IdentityAddress = identityAddress;
+        RelationshipId = relationshipId;
+    }
+
+    public string IdentityAddress { get; }
+    public string RelationshipId { get; }
+}
