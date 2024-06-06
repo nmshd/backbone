32e171532c09587f4c2738b228d4beecaf898dbc
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-06-06 13:30:36 +0200

    chore: change string

--- Modules/Devices/src/Devices.Domain/DomainEvents/Outgoing/PeerIdentityDeletedDomainEvent.cs
+++ Modules/Devices/src/Devices.Domain/DomainEvents/Outgoing/PeerIdentityDeletedDomainEvent.cs
@@ -5,7 +5,7 @@ namespace Backbone.Modules.Devices.Domain.DomainEvents.Outgoing;
 
 public class PeerIdentityDeletedDomainEvent : DomainEvent
 {
-    public PeerIdentityDeletedDomainEvent(string relationshipId, IdentityAddress identityAddress) : base($"{relationshipId}/PeerIdentityDeleted/${identityAddress}")
+    public PeerIdentityDeletedDomainEvent(string relationshipId, IdentityAddress identityAddress) : base($"{relationshipId}/PeerIdentityDeleted/{identityAddress}")
     {
         IdentityAddress = identityAddress;
         RelationshipId = relationshipId;
