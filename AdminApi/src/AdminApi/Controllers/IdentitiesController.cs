c572a723fe7e2b4b291a295dbc9d602d124e12aa
Daniel Almeida <115644988+daniel-almeida-konkconsulting@users.noreply.github.com>
2024-06-06 17:25:28 +0100

    refactor: remove unnecessary produces error attribute

--- AdminApi/src/AdminApi/Controllers/IdentitiesController.cs
+++ AdminApi/src/AdminApi/Controllers/IdentitiesController.cs
@@ -129,7 +129,6 @@ public class IdentitiesController : ApiControllerBase
 
     [HttpGet("{identityAddress}/DeletionProcesses/AuditLogs")]
     [ProducesResponseType(typeof(GetDeletionProcessesAuditLogsResponse), StatusCodes.Status200OK)]
-    [ProducesError(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> GetDeletionProcessesAuditLogs([FromRoute] string identityAddress, CancellationToken cancellationToken)
     {
         var response = await _mediator.Send(new GetDeletionProcessesAuditLogsQuery(identityAddress), cancellationToken);
