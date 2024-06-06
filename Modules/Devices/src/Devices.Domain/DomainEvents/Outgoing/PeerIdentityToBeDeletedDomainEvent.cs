bd55545513f5e6fd8a159a8212a1c273f89b398d
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-06-06 13:30:17 +0200

    feat: add PeerIdentityToBeDeleted domain event

---
+++ Modules/Devices/src/Devices.Domain/DomainEvents/Outgoing/PeerIdentityToBeDeletedDomainEvent.cs
@@ -0,0 +1,13 @@
+﻿using Backbone.BuildingBlocks.Domain.Events;
+using Backbone.DevelopmentKit.Identity.ValueObjects;
+
+namespace Backbone.Modules.Devices.Domain.DomainEvents.Outgoing;
+public class PeerIdentityToBeDeletedDomainEvent : DomainEvent
+{
+    public PeerIdentityToBeDeletedDomainEvent(IdentityAddress identityAddress) : base($"PeerIdentityToBeDeleted/{identityAddress}")
+    {
+        IdentityAddress = identityAddress;
+    }
+
+    public IdentityAddress IdentityAddress { get; }
+}
