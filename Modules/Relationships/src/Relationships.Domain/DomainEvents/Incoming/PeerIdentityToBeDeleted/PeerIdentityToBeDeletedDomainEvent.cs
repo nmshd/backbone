b97aec5d453362d6321424af8490ba89588e7b50
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-06-07 09:47:20 +0200

    feat: implement incoming event handler

---
+++ Modules/Relationships/src/Relationships.Domain/DomainEvents/Incoming/PeerIdentityToBeDeleted/PeerIdentityToBeDeletedDomainEvent.cs
@@ -0,0 +1,13 @@
+﻿using Backbone.BuildingBlocks.Domain.Events;
+using Backbone.DevelopmentKit.Identity.ValueObjects;
+
+namespace Backbone.Modules.Relationships.Domain.DomainEvents.Incoming.PeerIdentityToBeDeleted;
+public class PeerIdentityToBeDeletedDomainEvent : DomainEvent
+{
+    public PeerIdentityToBeDeletedDomainEvent(IdentityAddress identityAddress)
+    {
+        IdentityAddress = identityAddress;
+    }
+
+    public IdentityAddress IdentityAddress { get; }
+}
---
+++ Modules/Relationships/src/Relationships.Domain/DomainEvents/Outgoing/PeerIsToBeDeletedDomainEvent.cs
@@ -0,0 +1,16 @@
+﻿using Backbone.BuildingBlocks.Domain.Events;
+
+namespace Backbone.Modules.Relationships.Domain.DomainEvents.Outgoing;
+public class PeerIsToBeDeletedDomainEvent : DomainEvent
+{
+    public PeerIsToBeDeletedDomainEvent(string identityAddress, string relationshipId, string peerIdentityAddress)
+    {
+        IdentityAddress = identityAddress;
+        RelationshipId = relationshipId;
+        PeerIdentityAddress = peerIdentityAddress;
+    }
+
+    public string IdentityAddress { get; }
+    public string RelationshipId { get; }
+    public string PeerIdentityAddress { get; }
+}
