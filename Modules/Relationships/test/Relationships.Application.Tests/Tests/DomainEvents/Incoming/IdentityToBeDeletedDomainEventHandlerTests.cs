422d3a345e5e78a489493789433e7f50cd1ea822
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-06-10 10:31:40 +0200

    test: add IdentityToBeDeleted event tests

---
+++ Modules/Relationships/test/Relationships.Application.Tests/Tests/DomainEvents/Incoming/IdentityToBeDeletedDomainEventHandlerTests.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq.Expressions;
+using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
+using Backbone.Modules.Relationships.Application.DomainEvents.Incoming.IdentityToBeDeleted;
+using Backbone.Modules.Relationships.Application.Infrastructure.Persistence.Repository;
+using Backbone.Modules.Relationships.Application.Tests.TestHelpers;
+using Backbone.Modules.Relationships.Domain.Aggregates.Relationships;
+using Backbone.Modules.Relationships.Domain.DomainEvents.Incoming.IdentityToBeDeleted;
+using Backbone.Modules.Relationships.Domain.DomainEvents.Outgoing;
+using Backbone.UnitTestTools.BaseClasses;
+using Backbone.UnitTestTools.Data;
+using FakeItEasy;
+using Xunit;
+
+namespace Backbone.Modules.Relationships.Application.Tests.Tests.DomainEvents.Incoming;
+public class IdentityToBeDeletedDomainEventHandlerTests : AbstractTestsBase
+{
+    [Fact]
+    public static async Task FindRelationships_method_is_called()
+    {
+        //Arrange
+        var identity = TestDataGenerator.CreateRandomIdentityAddress();
+        var mockRelationshipsRepository = A.Fake<IRelationshipsRepository>();
+
+        var handler = new IdentityToBeDeletedDomainEventHandler(mockRelationshipsRepository, A.Dummy<IEventBus>());
+
+        //Act
+        await handler.Handle(new IdentityToBeDeletedDomainEvent(identity));
+
+        //Assert
+        A.CallTo(() => mockRelationshipsRepository.FindRelationships(A<Expression<Func<Relationship, bool>>>._, A<CancellationToken>._)).MustHaveHappenedOnceExactly();
+    }
+
+    [Fact]
+    public static async Task Publishes_PeerToBeDeletedDomainEvent()
+    {
+        //Arrange
+        var identity = TestDataGenerator.CreateRandomIdentityAddress();
+        var identityToBeDeleted = TestDataGenerator.CreateRandomIdentityAddress();
+        var relationship = TestData.CreateActiveRelationship(identity, identityToBeDeleted);
+
+        var fakeRelationshipsRepository = A.Dummy<IRelationshipsRepository>();
+        var mockEventBus = A.Fake<IEventBus>();
+
+        A.CallTo(() => fakeRelationshipsRepository.FindRelationships(A<Expression<Func<Relationship, bool>>>._, A<CancellationToken>._))
+            .Returns(new List<Relationship>() { relationship });
+
+        var handler = new IdentityToBeDeletedDomainEventHandler(fakeRelationshipsRepository, mockEventBus);
+
+        //Act
+        await handler.Handle(new IdentityToBeDeletedDomainEvent(identityToBeDeleted));
+
+        //Assert
+        A.CallTo(() => mockEventBus.Publish(A<PeerToBeDeletedDomainEvent>.That.Matches(e =>
+            e.IdentityAddress == identity &&
+            e.RelationshipId == relationship.Id &&
+            e.PeerIdentityAddress == identityToBeDeleted))
+        ).MustHaveHappenedOnceExactly();
+    }
+}
