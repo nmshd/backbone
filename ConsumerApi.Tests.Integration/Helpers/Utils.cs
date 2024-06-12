643e853fc1298445778dbdbe809fb26da6c671e3
Daniel Almeida <115644988+daniel-almeida-konkconsulting@users.noreply.github.com>
2024-06-12 14:19:52 +0100

    test: update helper methods with new sdk changes

--- ConsumerApi.Tests.Integration/Helpers/Utils.cs
+++ ConsumerApi.Tests.Integration/Helpers/Utils.cs
@@ -27,12 +27,12 @@ public static class Utils
         var createRelationshipResponse = await client2.Relationships.CreateRelationship(createRelationshipRequest);
         createRelationshipResponse.Should().BeASuccess();
 
-        var completeRelationshipChangeRequest = new CompleteRelationshipChangeRequest
+        var acceptRelationshipRequest = new AcceptRelationshipRequest
         {
-            Content = ConvertibleString.FromUtf8("AAA").BytesRepresentation
+            CreationResponseContent = ConvertibleString.FromUtf8("AAA").BytesRepresentation
         };
-        var acceptRelationChangeResponse =
-            await client1.Relationships.AcceptChange(createRelationshipResponse.Result!.Id, createRelationshipResponse.Result.Changes.First().Id, completeRelationshipChangeRequest);
-        acceptRelationChangeResponse.Should().BeASuccess();
+
+        var acceptRelationshipResponse = await client1.Relationships.AcceptRelationship(createRelationshipResponse.Result!.Id, acceptRelationshipRequest);
+        acceptRelationshipResponse.Should().BeASuccess();
     }
 }
