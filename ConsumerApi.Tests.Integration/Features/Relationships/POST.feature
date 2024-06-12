cc77a34927d3eb041013fff5d55763552a1dbeec
Daniel Almeida <115644988+daniel-almeida-konkconsulting@users.noreply.github.com>
2024-06-12 11:42:28 +0100

    test: add relationship integration tests

---
+++ ConsumerApi.Tests.Integration/Features/Relationships/POST.feature
@@ -0,0 +1,64 @@
+﻿@Integration
+Feature: POST Relationship
+
+User creates a Relationship
+
+Scenario: Creating a Relationship
+	Given Identities i1 and i2
+	And a Relationship Template rt created by i1
+	When a POST request is sent to the /Relationships endpoint by i2 with rt.id
+	Then the response status code is 201 (Created)
+	And the response contains a Relationship
+
+Scenario: Creating a Relationship with Identity "ToBeDeleted"
+	Given Identities i1 and i2
+	And a Relationship Template rt created by i1
+	And i1 is in status "ToBeDeleted"
+	When a POST request is sent to the /Relationships endpoint by i2 with rt.id
+	Then the response status code is 400 (Bad Request)
+	And the response content contains an error with the error code "error.platform.validation.relationship.peerIsToBeDeleted"
+
+Scenario: Accept Relationship Change
+	Given Identities i1 and i2
+	And a pending Relationship between i1 and i2 initiated by i1
+	When a POST request is sent to the /Relationships/{r.Id}/Changes/{r.Changes.Id}/Accept endpoint by i1
+	Then the response status code is 200 (OK)
+	And the response contains an AcceptRelationshipChangeResponse
+
+Scenario: Accept Relationship Change with Identity "ToBeDeleted"
+	Given Identities i1 and i2
+	And a pending Relationship between i1 and i2 initiated by i1
+	And i1 is in status "ToBeDeleted"
+	When a POST request is sent to the /Relationships/{r.Id}/Changes/{r.Changes.Id}/Accept endpoint by i1
+	Then the response status code is 400 (Bad Request)
+	And the response content contains an error with the error code "error.platform.validation.relationship.peerIsToBeDeleted"
+
+Scenario: Reject Relationship Change
+	Given Identities i1 and i2
+	And a pending Relationship between i1 and i2 initiated by i1
+	When a POST request is sent to the /Relationships/{r.Id}/Changes/{r.Changes.Id}/Reject endpoint by i1
+	Then the response status code is 200 (OK)
+	And the response contains an RejectRelationshipChangeResponse
+
+Scenario: Reject Relationship Change with Identity "ToBeDeleted"
+	Given Identities i1 and i2
+	And a pending Relationship between i1 and i2 initiated by i1
+	And i1 is in status "ToBeDeleted"
+	When a POST request is sent to the /Relationships/{r.Id}/Changes/{r.Changes.Id}/Reject endpoint by i1
+	Then the response status code is 400 (Bad Request)
+	And the response content contains an error with the error code "error.platform.validation.relationship.peerIsToBeDeleted"
+
+Scenario: Revoke Relationship Change
+	Given Identities i1 and i2
+	And a pending Relationship between i1 and i2 initiated by i1
+	When a POST request is sent to the /Relationships/{r.Id}/Changes/{r.Changes.Id}/Revoke endpoint by i2
+	Then the response status code is 200 (OK)
+	And the response contains an RevokeRelationshipChangeResponse
+
+Scenario: Revoke Relationship Change with Identity "ToBeDeleted"
+	Given Identities i1 and i2
+	And a pending Relationship between i1 and i2 initiated by i1
+	And i1 is in status "ToBeDeleted"
+	When a POST request is sent to the /Relationships/{r.Id}/Changes/{r.Changes.Id}/Revoke endpoint by i2
+	Then the response status code is 400 (Bad Request)
+	And the response content contains an error with the error code "error.platform.validation.relationship.peerIsToBeDeleted"
---
+++ ConsumerApi.Tests.Integration/StepDefinitions/RelationshipsStepDefinitions.cs
@@ -0,0 +1,196 @@
+﻿using Backbone.BuildingBlocks.SDK.Endpoints.Common.Types;
+using Backbone.ConsumerApi.Sdk;
+using Backbone.ConsumerApi.Sdk.Authentication;
+using Backbone.ConsumerApi.Sdk.Endpoints.Relationships.Types;
+using Backbone.ConsumerApi.Sdk.Endpoints.Relationships.Types.Requests;
+using Backbone.ConsumerApi.Sdk.Endpoints.RelationshipTemplates.Types.Requests;
+using Backbone.ConsumerApi.Sdk.Endpoints.RelationshipTemplates.Types.Responses;
+using Backbone.ConsumerApi.Tests.Integration.Configuration;
+using Backbone.ConsumerApi.Tests.Integration.Extensions;
+using Backbone.ConsumerApi.Tests.Integration.Support;
+using Backbone.Crypto;
+using Microsoft.Extensions.Options;
+
+namespace Backbone.ConsumerApi.Tests.Integration.StepDefinitions;
+
+[Binding]
+[Scope(Feature = "POST Relationship")]
+internal class RelationshipsStepDefinitions
+{
+    private Client _client1 = null!;
+    private Client _client2 = null!;
+    private readonly ClientCredentials _clientCredentials;
+    private readonly HttpClient _httpClient;
+    private ApiResponse<CreateRelationshipTemplateResponse>? _relationshipTemplateResponse;
+    private ApiResponse<RelationshipMetadata>? _createRelationshipResponse;
+    private ApiResponse<RelationshipMetadata>? _acceptRelationshipChangeResponse;
+    private ApiResponse<RelationshipMetadata>? _rejectRelationshipChangeResponse;
+    private ApiResponse<RelationshipMetadata>? _revokeRelationshipChangeResponse;
+    private string _relationshipId = string.Empty;
+    private string _relationshipChangeId = string.Empty;
+
+    public RelationshipsStepDefinitions(HttpClientFactory factory, IOptions<HttpConfiguration> httpConfiguration)
+    {
+        _httpClient = factory.CreateClient();
+        _clientCredentials = new ClientCredentials(httpConfiguration.Value.ClientCredentials.ClientId, httpConfiguration.Value.ClientCredentials.ClientSecret);
+    }
+
+    [Given("Identities i1 and i2")]
+    public async Task GivenIdentitiesI1AndI2()
+    {
+        _client1 = await Client.CreateForNewIdentity(_httpClient, _clientCredentials, Constants.DEVICE_PASSWORD);
+        _client2 = await Client.CreateForNewIdentity(_httpClient, _clientCredentials, Constants.DEVICE_PASSWORD);
+    }
+
+    [Given("a Relationship Template rt created by i1")]
+    public async Task GivenARelationshipTemplateRtCreatedByI1()
+    {
+        _relationshipTemplateResponse = await CreateRelationshipTemplate(_client1);
+    }
+
+    [Given("a pending Relationship between i1 and i2 initiated by i1")]
+    public async Task GivenAPendingRelationshipBetweenI1AndI2InitiatedByI1()
+    {
+        var relationshipTemplateResponse = await CreateRelationshipTemplate(_client1);
+        var createRelationshipResponse = await CreateRelationship(_client2, relationshipTemplateResponse.Result!.Id);
+
+        _relationshipId = createRelationshipResponse.Result!.Id;
+        _relationshipChangeId = createRelationshipResponse!.Result!.Changes.First().Id;
+    }
+
+    [Given("i1 is in status \"ToBeDeleted\"")]
+    public async Task GivenIdentityI1IsToBeDeleted()
+    {
+        var startDeletionProcessResponse = await _client1.Identities.StartDeletionProcess();
+        startDeletionProcessResponse.Should().BeASuccess();
+    }
+
+    [When("a POST request is sent to the /Relationships endpoint by i2 with rt.id")]
+    public async Task WhenAPostRequestIsSentToTheRelationshipsEndpointByI2With()
+    {
+        _createRelationshipResponse = await CreateRelationship(_client2, _relationshipTemplateResponse!.Result!.Id);
+    }
+
+    [When("a POST request is sent to the /Relationships/{r.Id}/Changes/{r.Changes.Id}/Accept endpoint by i1")]
+    public async Task WhenAPostRequestIsSentToTheAcceptRelationshipChangeEndpointByI1()
+    {
+        var completeRelationshipChangeRequest = new CompleteRelationshipChangeRequest
+        {
+            Content = ConvertibleString.FromUtf8("AAA").BytesRepresentation
+        };
+        _acceptRelationshipChangeResponse = await _client1.Relationships.AcceptChange(_relationshipId, _relationshipChangeId, completeRelationshipChangeRequest);
+    }
+
+    [When("a POST request is sent to the /Relationships/{r.Id}/Changes/{r.Changes.Id}/Reject endpoint by i1")]
+    public async Task WhenAPostRequestIsSentToTheRejectRelationshipChangeEndpointByI1()
+    {
+        var completeRelationshipChangeRequest = new CompleteRelationshipChangeRequest
+        {
+            Content = ConvertibleString.FromUtf8("AAA").BytesRepresentation
+        };
+        _rejectRelationshipChangeResponse = await _client1.Relationships.RejectChange(_relationshipId, _relationshipChangeId, completeRelationshipChangeRequest);
+    }
+
+    [When("a POST request is sent to the /Relationships/{r.Id}/Changes/{r.Changes.Id}/Revoke endpoint by i2")]
+    public async Task WhenAPostRequestIsSentToTheRevokeRelationshipChangeEndpointByI2()
+    {
+        var completeRelationshipChangeRequest = new CompleteRelationshipChangeRequest
+        {
+            Content = ConvertibleString.FromUtf8("AAA").BytesRepresentation
+        };
+        _revokeRelationshipChangeResponse = await _client2.Relationships.RevokeChange(_relationshipId, _relationshipChangeId, completeRelationshipChangeRequest);
+    }
+
+    [Then(@"the response status code is (\d\d\d) \(.+\)")]
+    public void ThenTheResponseStatusCodeIs(int expectedStatusCode)
+    {
+        if (_createRelationshipResponse != null)
+            ((int)_createRelationshipResponse!.Status).Should().Be(expectedStatusCode);
+
+        if (_acceptRelationshipChangeResponse != null)
+            ((int)_acceptRelationshipChangeResponse!.Status).Should().Be(expectedStatusCode);
+
+        if (_rejectRelationshipChangeResponse != null)
+            ((int)_rejectRelationshipChangeResponse!.Status).Should().Be(expectedStatusCode);
+
+        if (_revokeRelationshipChangeResponse != null)
+            ((int)_revokeRelationshipChangeResponse!.Status).Should().Be(expectedStatusCode);
+    }
+
+    [Then(@"the response content contains an error with the error code ""([^""]*)""")]
+    public void ThenTheResponseContentIncludesAnErrorWithTheErrorCode(string errorCode)
+    {
+        if (_createRelationshipResponse != null)
+        {
+            _createRelationshipResponse!.Error.Should().NotBeNull();
+            _createRelationshipResponse.Error!.Code.Should().Be(errorCode);
+        }
+
+        if (_acceptRelationshipChangeResponse != null)
+        {
+            _acceptRelationshipChangeResponse!.Error.Should().NotBeNull();
+            _acceptRelationshipChangeResponse.Error!.Code.Should().Be(errorCode);
+        }
+
+        if (_rejectRelationshipChangeResponse != null)
+        {
+            _rejectRelationshipChangeResponse!.Error.Should().NotBeNull();
+            _rejectRelationshipChangeResponse.Error!.Code.Should().Be(errorCode);
+        }
+
+        if (_revokeRelationshipChangeResponse != null)
+        {
+            _revokeRelationshipChangeResponse!.Error.Should().NotBeNull();
+            _revokeRelationshipChangeResponse.Error!.Code.Should().Be(errorCode);
+        }
+    }
+
+    [Then("the response contains a Relationship")]
+    public void ThenTheResponseContainsARelationship()
+    {
+        _createRelationshipResponse!.Should().BeASuccess();
+        _createRelationshipResponse!.Should().ComplyWithSchema();
+    }
+
+    [Then("the response contains an AcceptRelationshipChangeResponse")]
+    public void ThenTheResponseContainsAnAcceptRelationshipChangeResponse()
+    {
+        _acceptRelationshipChangeResponse!.Should().BeASuccess();
+        _acceptRelationshipChangeResponse!.Should().ComplyWithSchema();
+    }
+
+    [Then("the response contains an RejectRelationshipChangeResponse")]
+    public void ThenTheResponseContainsAnRejectRelationshipChangeResponse()
+    {
+        _rejectRelationshipChangeResponse!.Should().BeASuccess();
+        _rejectRelationshipChangeResponse!.Should().ComplyWithSchema();
+    }
+
+    [Then("the response contains an RevokeRelationshipChangeResponse")]
+    public void ThenTheResponseContainsAnRevokeRelationshipChangeResponse()
+    {
+        _revokeRelationshipChangeResponse!.Should().BeASuccess();
+        _revokeRelationshipChangeResponse!.Should().ComplyWithSchema();
+    }
+
+    private async Task<ApiResponse<CreateRelationshipTemplateResponse>> CreateRelationshipTemplate(Client client)
+    {
+        var createRelationshipTemplateRequest = new CreateRelationshipTemplateRequest
+        {
+            Content = ConvertibleString.FromUtf8("AAA").BytesRepresentation
+        };
+
+        return await client.RelationshipTemplates.CreateTemplate(createRelationshipTemplateRequest);
+    }
+
+    private async Task<ApiResponse<RelationshipMetadata>> CreateRelationship(Client client, string relationshipTemplateId)
+    {
+        var createRelationshipRequest = new CreateRelationshipRequest
+        {
+            RelationshipTemplateId = relationshipTemplateId,
+            Content = ConvertibleString.FromUtf8("AAA").BytesRepresentation
+        };
+
+        return await client.Relationships.CreateRelationship(createRelationshipRequest);
+    }
+}
