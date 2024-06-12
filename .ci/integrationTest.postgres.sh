3edce8ab166ca658586e912ed4ce5581bb3a85eb
Mika Herrmann <mika.herrmann@js-soft.com>
2024-06-12 11:12:34 +0200

    chore: Remove unused/commented out code

--- .ci/integrationTest.postgres.sh
+++ .ci/integrationTest.postgres.sh
@@ -5,26 +5,17 @@ dockerCompose() {
     docker compose -f ./.ci/docker-compose.test.yml -f ./.ci/docker-compose.test.postgres.yml "$@"
 }
 
-debugRun() {
-    echo
-    echo "---------------------------------------------------------------------------------------"
-    echo
-    echo "$@:"
-    
-    time $@
-}
-
 {
-  debugRun dockerCompose down;
-  debugRun dockerCompose up -d --no-build
+  dockerCompose down;
+  dockerCompose up -d --no-build
 } &
 {
-  debugRun dotnet restore "Backbone.sln";
-  debugRun dotnet build --no-restore "Backbone.sln"
+  dotnet restore "Backbone.sln";
+  dotnet build --no-restore "Backbone.sln"
 }
 wait
 
 export CONSUMER_API_BASE_ADDRESS="http://localhost:5000"
 export ADMIN_API_BASE_ADDRESS="http://localhost:5173"
 
-debugRun dotnet test --no-restore --no-build --filter "Category=Integration&TestCategory!~ignore" --logger "GitHubActions;summary.includePassedTests=true;summary.includeSkippedTests=true" "Backbone.sln"
+dotnet test --no-restore --no-build --filter "Category=Integration&TestCategory!~ignore" --logger "GitHubActions;summary.includePassedTests=true;summary.includeSkippedTests=true" "Backbone.sln"
