6088ec2a153f4fda76a3edbade49cefe4694cebf
Vladimir Vuckovic <vidrak97@gmail.com>
2024-06-14 11:19:45 +0200

    fix: run dart format

--- AdminUi/packages/admin_api_types/lib/src/messages/message.g.dart
+++ AdminUi/packages/admin_api_types/lib/src/messages/message.g.dart
@@ -6,20 +6,16 @@ part of 'message.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-MessageOverview _$MessageOverviewFromJson(Map<String, dynamic> json) =>
-    MessageOverview(
+MessageOverview _$MessageOverviewFromJson(Map<String, dynamic> json) => MessageOverview(
       messageId: json['messageId'] as String,
       senderAddress: json['senderAddress'] as String,
       senderDevice: json['senderDevice'] as String,
       sendDate: DateTime.parse(json['sendDate'] as String),
       numberOfAttachments: (json['numberOfAttachments'] as num).toInt(),
-      recipients: (json['recipients'] as List<dynamic>)
-          .map(MessageRecipients.fromJson)
-          .toList(),
+      recipients: (json['recipients'] as List<dynamic>).map(MessageRecipients.fromJson).toList(),
     );
 
-Map<String, dynamic> _$MessageOverviewToJson(MessageOverview instance) =>
-    <String, dynamic>{
+Map<String, dynamic> _$MessageOverviewToJson(MessageOverview instance) => <String, dynamic>{
       'messageId': instance.messageId,
       'senderAddress': instance.senderAddress,
       'senderDevice': instance.senderDevice,
@@ -28,14 +24,12 @@ Map<String, dynamic> _$MessageOverviewToJson(MessageOverview instance) =>
       'recipients': instance.recipients,
     };
 
-MessageRecipients _$MessageRecipientsFromJson(Map<String, dynamic> json) =>
-    MessageRecipients(
+MessageRecipients _$MessageRecipientsFromJson(Map<String, dynamic> json) => MessageRecipients(
       messageId: json['messageId'] as String,
       address: json['address'] as String,
     );
 
-Map<String, dynamic> _$MessageRecipientsToJson(MessageRecipients instance) =>
-    <String, dynamic>{
+Map<String, dynamic> _$MessageRecipientsToJson(MessageRecipients instance) => <String, dynamic>{
       'messageId': instance.messageId,
       'address': instance.address,
     };
