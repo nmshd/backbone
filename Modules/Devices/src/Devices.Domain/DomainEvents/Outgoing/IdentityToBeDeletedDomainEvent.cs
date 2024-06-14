44aeadc7271f6af57b2ca01d5737b7ee184047a8
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-06-14 14:23:15 +0200

    chore: change type

--- Modules/Devices/src/Devices.Domain/DomainEvents/Outgoing/IdentityToBeDeletedDomainEvent.cs
+++ Modules/Devices/src/Devices.Domain/DomainEvents/Outgoing/IdentityToBeDeletedDomainEvent.cs
@@ -1,13 +1,12 @@
 ﻿using Backbone.BuildingBlocks.Domain.Events;
-using Backbone.DevelopmentKit.Identity.ValueObjects;
 
 namespace Backbone.Modules.Devices.Domain.DomainEvents.Outgoing;
 public class IdentityToBeDeletedDomainEvent : DomainEvent
 {
-    public IdentityToBeDeletedDomainEvent(IdentityAddress identityAddress) : base($"{identityAddress}/IdentityToBeDeleted", randomizeId: true)
+    public IdentityToBeDeletedDomainEvent(string identityAddress) : base($"{identityAddress}/IdentityToBeDeleted", randomizeId: true)
     {
         IdentityAddress = identityAddress;
     }
 
-    public IdentityAddress IdentityAddress { get; }
+    public string IdentityAddress { get; }
 }
