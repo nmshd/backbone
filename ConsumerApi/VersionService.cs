01caf0afb311449d364021e1d067c52c9e8b82fb
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-06-17 14:00:23 +0200

    fix: implement another approach for getting the major version of the backbone

--- ConsumerApi/VersionService.cs
+++ ConsumerApi/VersionService.cs
@@ -1,26 +1,14 @@
-﻿using System.Text.Json;
-
-namespace Backbone.ConsumerApi;
+﻿namespace Backbone.ConsumerApi;
 
 public class VersionService
 {
-    private static readonly string FILE_PATH = AppDomain.CurrentDomain.BaseDirectory + "../../../../AdminApi/src/AdminApi/ClientApp/package-lock.json";
-
     public async Task<string?> GetBackboneMajorVersion()
     {
-        await using var stream = new FileStream(FILE_PATH, FileMode.Open, FileAccess.Read);
-        using var jsonDocument = await JsonDocument.ParseAsync(stream);
+        var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+        var version = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location).FileVersion;
 
-        jsonDocument.RootElement.TryGetProperty("packages", out var packages);
-        packages.TryGetProperty("node_modules/less/node_modules/make-dir", out var nodeModulesLessNodeModulesMakeDir);
-        nodeModulesLessNodeModulesMakeDir.TryGetProperty("dependencies", out var dependencies);
-        dependencies.TryGetProperty("semver", out var semver);
+        var majorVersion = version?.Split(['.'], 2)[0];
 
-        var version = semver.ToString();
-
-        var caretIndex = version.IndexOf('^');
-        var dotIndex = version.IndexOf('.', caretIndex);
-
-        return version.Substring(caretIndex + 1, dotIndex - caretIndex - 1);
+        return await Task.FromResult(majorVersion);
     }
 }
