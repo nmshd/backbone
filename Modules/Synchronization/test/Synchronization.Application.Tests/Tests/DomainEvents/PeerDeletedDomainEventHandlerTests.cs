ab36f7aaa8c3dc9aef6819b9d0304c178548b3b8
Nikola Dmitrasinovic <118458592+NikolaDmitrasinovic@users.noreply.github.com>
2024-06-19 10:56:26 +0200

    Create External Events for peers of Identity to be deleted, Identity with canceled deletion process and deleted Identity (#699)
    * refactor: remove event bus from StartDeletionProcessAsOwnerCommand handler
    
    * feat: add outgoing domain event for `ToBeDeleted` status change
    
    * feat: add domain event for ToBeDeleted status
    
    * test: add domain event test
    
    * test: remove test
    
    * feat: raise domain event when identity deletion stats
    refactor: improve code structure of tests
    
    * refactor: remove domain event from actual deletion worker
    
    * feat: raise doman event when deletion process is canceled
    
    * refactor: improve code structure of Devices\Domain Tests
    
    * feat: handle IdentityToBeDeleted domain event, Relationship module
    
    * feat: raise PeerDeletionCanceledDomainEvent
    
    * feat: raise PeerDeletedDomainEvent
    
    * feat: handle PeerToBeDeleted domain event and create external event
    
    * feat: handle PeerDeletionCanceledDomainEvent and create external event
    
    * feat: handle PeerDeletedDomainEvent and create external event
    
    * chore: fix formatting issues
    
    * refactor: various small improvements
    
    * refactor: improve code structure of Identity
    
    * chore: fix message and method name in EntityAssertions
    
    * chore: change type
    
    * chore: remove empty lines
    
    * chroe: rename var
    
    * chore: rename var
    
    * test: improve HaveDomainEvents and its usings
    
    * test: directly test for QueuedForDeletion tier
    
    * refactor: rename Canceled to Cancelled
    
    * refactor: renaming of private methods
    
    * chore: remove IChangeLogExtensions
    
    * test: rename some variables
    
    * refactor: rename canceled to cancelled
    
    * ci: let publish-helm-chart depend on publish-sse-server, publish-identity-deletion-jobs and publish-database-migrator
    
    * test: minor simplification in EntitiesShouldHaveEmptyDefaultConstructors archunit test
    
    * feat: subscribe for identities events in relationships module
    
    * feat: call Update in Relationships event handlers in order to publish the domain events
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: Timo Notheisen <timo.notheisen@js-soft.com>

---
+++ Modules/Synchronization/test/Synchronization.Application.Tests/Tests/DomainEvents/PeerDeletedDomainEventHandlerTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Backbone.DevelopmentKit.Identity.ValueObjects;
+using Backbone.Modules.Synchronization.Application.DomainEvents.Incoming.PeerDeleted;
+using Backbone.Modules.Synchronization.Application.Infrastructure;
+using Backbone.Modules.Synchronization.Domain.DomainEvents.Incoming.PeerDeleted;
+using Backbone.Modules.Synchronization.Domain.Entities.Sync;
+using Backbone.UnitTestTools.BaseClasses;
+using FakeItEasy;
+using Microsoft.Extensions.Logging;
+using Xunit;
+
+namespace Backbone.Modules.Synchronization.Application.Tests.Tests.DomainEvents;
+public class PeerDeletedDomainEventHandlerTests : AbstractTestsBase
+{
+    [Fact]
+    public async Task Creates_an_external_event()
+    {
+        // Arrange
+        var peerOfDeletedIdentity = TestDataGenerator.CreateRandomIdentityAddress();
+        var peerDeletedDomainEvent = new PeerDeletedDomainEvent(peerOfDeletedIdentity, "some-relationship-id", "some-deletedIdentity-id");
+
+        var mockDbContext = A.Fake<ISynchronizationDbContext>();
+
+        var externalEvent = new ExternalEvent(ExternalEventType.PeerDeleted, IdentityAddress.Parse(peerOfDeletedIdentity), 1,
+            new { peerDeletedDomainEvent.RelationshipId });
+
+        A.CallTo(() => mockDbContext.CreateExternalEvent(
+            peerOfDeletedIdentity,
+            ExternalEventType.PeerDeleted,
+            A<object>._)
+        ).Returns(externalEvent);
+
+        var handler = new PeerDeletedDomainEventHandler(mockDbContext,
+            A.Fake<ILogger<PeerDeletedDomainEventHandler>>());
+
+        // Act
+        await handler.Handle(peerDeletedDomainEvent);
+
+        // Assert
+        A.CallTo(() => mockDbContext.CreateExternalEvent(peerOfDeletedIdentity, ExternalEventType.PeerDeleted, A<object>._))
+            .MustHaveHappenedOnceExactly();
+    }
+}
