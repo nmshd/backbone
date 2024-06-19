1d7300615d5bcfbb81369b9bef1a61914d176a5b
Mika Herrmann <mika.herrmann@js-soft.com>
2024-06-19 16:35:37 +0200

    test: Use ghcr for artifacts

--- .ci/loadDockerImages.sh
+++ .ci/loadDockerImages.sh
@@ -1,6 +1,10 @@
 ﻿#!/bin/bash
 set -e
 
-for f in /tmp/*.tar; do
-    docker load --input $f
+#for f in /tmp/*.tar; do
+#    docker load --input $f
+#done
+
+for f in /tmp/*.txt; do
+    docker load $(cat($f))
 done
