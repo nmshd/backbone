f6a411fc82d455614cb533890e00351792709563
Timo Notheisen <timo.notheisen@js-soft.com>
2024-06-20 13:40:28 +0200

    fix: don't publish ExternalEventCreatedDomainEvent manually

--- Modules/Synchronization/test/Synchronization.Application.Tests/Tests/DomainEvents/RelationshipStatusChangedDomainEventHandlerTests.cs
+++ Modules/Synchronization/test/Synchronization.Application.Tests/Tests/DomainEvents/RelationshipStatusChangedDomainEventHandlerTests.cs
@@ -1,9 +1,6 @@
-﻿using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
-using Backbone.DevelopmentKit.Identity.ValueObjects;
-using Backbone.Modules.Synchronization.Application.DomainEvents.Incoming.RelationshipStatusChanged;
+﻿using Backbone.Modules.Synchronization.Application.DomainEvents.Incoming.RelationshipStatusChanged;
 using Backbone.Modules.Synchronization.Application.Infrastructure;
 using Backbone.Modules.Synchronization.Domain.DomainEvents.Incoming.RelationshipStatusChanged;
-using Backbone.Modules.Synchronization.Domain.DomainEvents.Outgoing;
 using Backbone.Modules.Synchronization.Domain.Entities.Sync;
 using Backbone.UnitTestTools.BaseClasses;
 using FakeItEasy;
@@ -46,43 +43,8 @@ public class RelationshipStatusChangedDomainEventHandlerTests : AbstractTestsBas
             .MustHaveHappenedOnceExactly();
     }
 
-    [Fact]
-    public async Task Publishes_an_ExternalEventCreatedDomainEvent()
+    private RelationshipStatusChangedDomainEventHandler CreateHandler(ISynchronizationDbContext dbContext)
     {
-        // Arrange
-        var relationshipTo = TestDataGenerator.CreateRandomIdentityAddress();
-        var @event = new RelationshipStatusChangedDomainEvent
-        {
-            RelationshipId = "REL1",
-            Peer = relationshipTo
-        };
-
-        var fakeDbContext = A.Fake<ISynchronizationDbContext>();
-        var mockEventBus = A.Fake<IEventBus>();
-
-        var externalEvent = new ExternalEvent(ExternalEventType.IdentityDeletionProcessStarted, IdentityAddress.Parse(relationshipTo), 1,
-            new { @event.RelationshipId });
-
-        A.CallTo(() => fakeDbContext.CreateExternalEvent(
-            relationshipTo,
-            ExternalEventType.RelationshipStatusChanged,
-            A<object>._)
-        ).Returns(externalEvent);
-
-        var handler = CreateHandler(fakeDbContext, mockEventBus);
-
-        // Act
-        await handler.Handle(@event);
-
-        // Assert
-        A.CallTo(() => mockEventBus.Publish(
-            A<ExternalEventCreatedDomainEvent>.That.Matches(e => e.Owner == relationshipTo))
-        ).MustHaveHappenedOnceExactly();
-    }
-
-    private RelationshipStatusChangedDomainEventHandler CreateHandler(ISynchronizationDbContext dbContext, IEventBus? eventBus = null)
-    {
-        eventBus ??= A.Fake<IEventBus>();
-        return new RelationshipStatusChangedDomainEventHandler(dbContext, eventBus, A.Fake<ILogger<RelationshipStatusChangedDomainEventHandler>>());
+        return new RelationshipStatusChangedDomainEventHandler(dbContext, A.Fake<ILogger<RelationshipStatusChangedDomainEventHandler>>());
     }
 }
