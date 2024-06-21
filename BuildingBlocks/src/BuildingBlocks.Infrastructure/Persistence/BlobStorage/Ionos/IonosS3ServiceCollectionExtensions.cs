8a3330a260aa4b9db980097a1dd49641946ba8ba
Nikola Vetnic <nikola.pacek.vetnic@gmail.com>
2024-06-21 10:37:45 +0200

    feat: implement ionos s3 storage

---
+++ BuildingBlocks/src/BuildingBlocks.Infrastructure/Persistence/BlobStorage/Ionos/IonosS3ServiceCollectionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.Persistence.BlobStorage;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Backbone.BuildingBlocks.Infrastructure.Persistence.BlobStorage.Ionos;
+public static class IonosS3ServiceCollectionExtensions
+{
+    public static void AddIonosS3(this IServiceCollection services,
+        Action<IonosS3Options> setupOptions)
+    {
+        var options = new IonosS3Options();
+        setupOptions.Invoke(options);
+
+        services.AddIonosS3(options);
+    }
+
+    public static void AddIonosS3(this IServiceCollection services, IonosS3Options options)
+    {
+        services.Configure<IonosS3Options>(opt =>
+        {
+            opt.ServiceUrl = options.ServiceUrl;
+            opt.AccessKey = options.AccessKey;
+            opt.SecretKey = options.SecretKey;
+            opt.BucketName = options.BucketName;
+        });
+
+        services.AddSingleton<IonosS3ClientFactory>();
+        services.AddScoped<IBlobStorage, IonosS3BlobStorage>();
+    }
+}
+
+public class IonosS3Options
+{
+    public string ServiceUrl { get; set; } = string.Empty;
+    public string AccessKey { get; set; } = string.Empty;
+    public string SecretKey { get; set; } = string.Empty;
+    public string BucketName { get; set; } = string.Empty;
+}
--- Modules/Files/src/Files.ConsumerApi/Configuration.cs
+++ Modules/Files/src/Files.ConsumerApi/Configuration.cs
@@ -23,12 +23,14 @@ public class Configuration
         {
             [Required]
             [MinLength(1)]
-            [RegularExpression("Azure|GoogleCloud")]
+            [RegularExpression("Azure|GoogleCloud|Ionos")]
             public string CloudProvider { get; set; } = string.Empty;
 
             public string ConnectionInfo { get; set; } = string.Empty;
 
             public string ContainerName { get; set; } = string.Empty;
+
+            public IonosS3Config IonosS3Config { get; set; } = new IonosS3Config();
         }
 
         public class SqlDatabaseConfiguration
@@ -41,6 +43,21 @@ public class Configuration
             [Required]
             [MinLength(1)]
             public string ConnectionString { get; set; } = string.Empty;
+        }
+
+        public class IonosS3Config
+        {
+            [Required]
+            public string ServiceUrl { get; set; } = string.Empty;
+
+            [Required]
+            public string AccessKey { get; set; } = string.Empty;
+
+            [Required]
+            public string SecretKey { get; set; } = string.Empty;
+
+            [Required]
+            public string BucketName { get; set; } = string.Empty;
         }
     }
 }
