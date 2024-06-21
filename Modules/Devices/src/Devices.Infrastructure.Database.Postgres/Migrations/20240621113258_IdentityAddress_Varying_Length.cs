51ff54910b879f91da05bff09c3accaec71b836b
Daniel Almeida <115644988+daniel-almeida-konkconsulting@users.noreply.github.com>
2024-06-21 12:37:47 +0100

    refactor: change identity address database type and generate postgres migrations

---
+++ Modules/Devices/src/Devices.Infrastructure.Database.Postgres/Migrations/20240621113258_IdentityAddress_Varying_Length.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backbone.Modules.Devices.Infrastructure.Database.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class IdentityAddress_Varying_Length : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "IdentityAddress",
+                schema: "Devices",
+                table: "PnsRegistrations",
+                type: "character varying(80)",
+                unicode: false,
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character(80)",
+                oldUnicode: false,
+                oldFixedLength: true,
+                oldMaxLength: 80);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IdentityAddress",
+                schema: "Devices",
+                table: "IdentityDeletionProcesses",
+                type: "character varying(80)",
+                unicode: false,
+                maxLength: 80,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character(80)",
+                oldUnicode: false,
+                oldFixedLength: true,
+                oldMaxLength: 80,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                schema: "Devices",
+                table: "Identities",
+                type: "character varying(80)",
+                unicode: false,
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character(80)",
+                oldUnicode: false,
+                oldFixedLength: true,
+                oldMaxLength: 80);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IdentityAddress",
+                schema: "Devices",
+                table: "Devices",
+                type: "character varying(80)",
+                unicode: false,
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character(80)",
+                oldUnicode: false,
+                oldFixedLength: true,
+                oldMaxLength: 80);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "IdentityAddress",
+                schema: "Devices",
+                table: "PnsRegistrations",
+                type: "character(80)",
+                unicode: false,
+                fixedLength: true,
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(80)",
+                oldUnicode: false,
+                oldMaxLength: 80);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IdentityAddress",
+                schema: "Devices",
+                table: "IdentityDeletionProcesses",
+                type: "character(80)",
+                unicode: false,
+                fixedLength: true,
+                maxLength: 80,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(80)",
+                oldUnicode: false,
+                oldMaxLength: 80,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                schema: "Devices",
+                table: "Identities",
+                type: "character(80)",
+                unicode: false,
+                fixedLength: true,
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(80)",
+                oldUnicode: false,
+                oldMaxLength: 80);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IdentityAddress",
+                schema: "Devices",
+                table: "Devices",
+                type: "character(80)",
+                unicode: false,
+                fixedLength: true,
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(80)",
+                oldUnicode: false,
+                oldMaxLength: 80);
+        }
+    }
+}
---
+++ Modules/Quotas/src/Quotas.Infrastructure.Database.Postgres/Migrations/20240621113541_IdentityAddresses_Varying_Length.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backbone.Modules.Quotas.Infrastructure.Database.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class IdentityAddresses_Varying_Length : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ApplyTo",
+                schema: "Quotas",
+                table: "TierQuotas",
+                type: "character varying(80)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character(80)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Owner",
+                schema: "Quotas",
+                table: "MetricStatuses",
+                type: "character varying(80)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character(80)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ApplyTo",
+                schema: "Quotas",
+                table: "IndividualQuotas",
+                type: "character varying(80)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character(80)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                schema: "Quotas",
+                table: "Identities",
+                type: "character varying(80)",
+                unicode: false,
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character(80)",
+                oldUnicode: false,
+                oldFixedLength: true,
+                oldMaxLength: 80);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ApplyTo",
+                schema: "Quotas",
+                table: "TierQuotas",
+                type: "character(80)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(80)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Owner",
+                schema: "Quotas",
+                table: "MetricStatuses",
+                type: "character(80)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(80)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ApplyTo",
+                schema: "Quotas",
+                table: "IndividualQuotas",
+                type: "character(80)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(80)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                schema: "Quotas",
+                table: "Identities",
+                type: "character(80)",
+                unicode: false,
+                fixedLength: true,
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(80)",
+                oldUnicode: false,
+                oldMaxLength: 80);
+        }
+    }
+}
