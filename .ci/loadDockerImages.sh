f38e6ae04ed789ce96e21704558eba0fc6142a99
Mika Herrmann <mika.herrmann@js-soft.com>
2024-06-21 09:41:29 +0200

    test: Try multiple outputs

--- .ci/loadDockerImages.sh
+++ .ci/loadDockerImages.sh
@@ -1,10 +1,10 @@
 ﻿#!/bin/bash
 set -e
 
-#for f in /tmp/*.tar; do
-#    docker load --input $f
-#done
-
-for f in /tmp/*.txt; do
-    docker pull $(cat $f)
+for f in /tmp/*.tar; do
+    docker load --input $f
 done
+
+#for f in /tmp/*.txt; do
+#    docker pull $(cat $f)
+#done
