b12f05ff1e648ff74cd01c4d1fbfdecf9db7e4c7
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-06-24 14:52:08 +0200

    refactor: add VersionResponse

--- ConsumerApi/Controllers/VersionController.cs
+++ ConsumerApi/Controllers/VersionController.cs
@@ -1,30 +1,29 @@
-﻿using Backbone.BuildingBlocks.API.Mvc.ControllerAttributes;
-using Backbone.ConsumerApi.Mvc;
-using MediatR;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backbone.ConsumerApi.Controllers;
 
 [Route("api/v1/[controller]")]
-public class VersionController : ApiControllerBase
+public class VersionController : ControllerBase
 {
     private readonly VersionService _versionService;
 
-    public VersionController(VersionService versionService, IMediator mediator) : base(mediator)
+    public VersionController(VersionService versionService)
     {
         _versionService = versionService;
     }
 
     [HttpGet]
     [AllowAnonymous]
-    [ProducesError(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(VersionResponse), StatusCodes.Status200OK)]
     public async Task<IActionResult> GetBackboneMajorVersion(CancellationToken cancellationToken)
     {
         var majorVersion = await _versionService.GetBackboneMajorVersion();
-        if (majorVersion != null)
-            return Ok(new { majorVersion });
-
-        return NotFound(new { message = "Version not found" });
+        return Ok(new VersionResponse { MajorVersion = majorVersion });
     }
+}
+
+public class VersionResponse
+{
+    public required string MajorVersion { get; set; }
 }
