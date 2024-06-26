67db025d893b6b6eb474df26941cd20ce0f4e9c5
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-06-26 11:28:48 +0200

    fix: wip

--- BuildingBlocks/src/BuildingBlocks.Infrastructure/Persistence/BlobStorage/BlobStorageServiceCollectionExtensions.cs
+++ BuildingBlocks/src/BuildingBlocks.Infrastructure/Persistence/BlobStorage/BlobStorageServiceCollectionExtensions.cs
@@ -66,13 +66,7 @@ public class BlobStorageOptions
     public string? ConnectionInfo { get; set; } = string.Empty;
     public string? Container { get; set; } = string.Empty;
 
-    public IonosS3Config IonosS3Config { get; set; } = new()
-    {
-        ServiceUrl = string.Empty,
-        AccessKey = string.Empty,
-        SecretKey = string.Empty,
-        BucketName = string.Empty
-    };
+    public IonosS3Config? IonosS3Config { get; set; }
 }
 
 public class IonosS3Config
