fe73f89d19e929e0695fa087c4d280618e84aaeb
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-06-27 12:37:20 +0200

    feat: enable health check configuration for sse server checks

--- Modules/Devices/src/Devices.Infrastructure/PushNotifications/Connectors/Sse/SseOptions.cs
+++ Modules/Devices/src/Devices.Infrastructure/PushNotifications/Connectors/Sse/SseOptions.cs
@@ -7,6 +7,9 @@ public class SseOptions
     [Required]
     public required bool Enabled { get; set; }
 
+    [Required]
+    public bool EnableHealthCheck { get; set; } = true;
+
     [Url]
     [Required]
     public required string SseServerBaseAddress { get; set; }
