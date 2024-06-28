466396890c249ec59ba1741d63b9726e423000aa
Daniel Almeida <115644988+daniel-almeida-konkconsulting@users.noreply.github.com>
2024-06-28 15:33:31 +0100

    refactor: make excluded metric keys clear

--- Modules/Quotas/src/Quotas.Application/Tiers/Commands/SeedQueuedForDeletionTier/Handler.cs
+++ Modules/Quotas/src/Quotas.Application/Tiers/Commands/SeedQueuedForDeletionTier/Handler.cs
@@ -27,7 +27,12 @@ public class Handler : IRequestHandler<SeedQueuedForDeletionTierCommand>
         }
 
         var metrics = await _metricsRepository.FindAll(CancellationToken.None);
-        queuedForDeletionTier.AddQuotaForAllMetricsOnQueuedForDeletion(metrics.Where(m => m.Key != MetricKey.NumberOfCreatedDatawalletModifications && m.Key != MetricKey.NumberOfStartedDeletionProcesses));
+        var excludedMetricKeys = new List<MetricKey>
+        {
+            MetricKey.NumberOfCreatedDatawalletModifications, // Identities to be deleted should still be able to modify the datawallet
+            MetricKey.NumberOfStartedDeletionProcesses // Identities to be deleted cannot start new deletion processes
+        };
+        queuedForDeletionTier.AddQuotaForAllMetricsOnQueuedForDeletion(metrics.Where(m => !excludedMetricKeys.Contains(m.Key)));
         await _tiersRepository.Update(queuedForDeletionTier, CancellationToken.None);
     }
 }
