08ffb8892dd14df71f51187b91240f59748a3833
Timo Notheisen <timo.notheisen@js-soft.com>
2024-07-04 08:17:00 +0200

    ci: fix integrationTest scripts and run job tests first

--- .ci/integrationTest.sqlserver.sh
+++ .ci/integrationTest.sqlserver.sh
@@ -18,9 +18,9 @@ wait
 export CONSUMER_API_BASE_ADDRESS="http://localhost:5000"
 export ADMIN_API_BASE_ADDRESS="http://localhost:5173"
 
-dotnet test --no-restore --no-build --logger "GitHubActions;summary.includeNotFoundTests=false" AdminApi/test/AdminApi.Tests.Integration.csproj
-dotnet test --no-restore --no-build --logger "GitHubActions;summary.includeNotFoundTests=false" ConsumerApi.Tests.Integration
+mv Jobs/test/Job.IdentityDeletion.Tests.Integration/appsettings.override.json ./Jobs/test/Job.IdentityDeletion.Tests.Integration/appsettings.override.json.bak
+cp .ci/appsettings.override.sqlserver.local.json Jobs/test/Job.IdentityDeletion.Tests.Integration/appsettings.override.json
+dotnet test --no-restore --no-build --logger "GitHubActions;summary.includeNotFoundTests=false" Jobs/test/Job.IdentityDeletion.Tests.Integration
 
-mv ./Jobs/test/Job.IdentityDeletion.Tests.Integration/appsettings.override.json ./Jobs/test/Job.IdentityDeletion.Tests.Integration/appsettings.override.json.bak
-cp ./.ci/appsettings.override.sqlserver.local.json ./Jobs/test/Job.IdentityDeletion.Tests.Integration/appsettings.override.json
+dotnet test --no-restore --no-build --logger "GitHubActions;summary.includeNotFoundTests=false" AdminApi/test/AdminApi.Tests.Integration.csproj
 dotnet test --no-restore --no-build --logger "GitHubActions;summary.includeNotFoundTests=false" ConsumerApi.Tests.Integration
