b262b105a477087c15205a393f2c3f28c09b40a5
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-07-08 08:30:23 +0200

    chore: extract into separate files

--- BuildingBlocks/src/BuildingBlocks.Application/Identities/IIdentityDeleter.cs
+++ BuildingBlocks/src/BuildingBlocks.Application/Identities/IIdentityDeleter.cs
@@ -5,25 +5,3 @@ public interface IIdentityDeleter
 {
     Task Delete(IdentityAddress identityAddress, IDeletionProcessLogger deletionProcessLogger);
 }
-
-public interface IDeletionProcessLogger
-{
-    Task LogDeletion(IdentityAddress identityAddress, AggregateType aggregateType);
-}
-
-public enum AggregateType
-{
-    Challenges,
-    PnsRegistrations,
-    Identities,
-    Files,
-    Messages,
-    QuotaIdentities,
-    Relationships,
-    RelationshipTemplates,
-    RelationshipTemplateAllocations,
-    ExternalEvents,
-    SyncRuns,
-    Datawallets,
-    Tokens
-}
