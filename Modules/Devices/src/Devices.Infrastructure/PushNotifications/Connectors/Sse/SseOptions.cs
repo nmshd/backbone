d0cabee96384b39b00685e89d6af2788409b6ddf
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-07-09 14:22:44 +0200

    feat: remove prop

--- Modules/Devices/src/Devices.Infrastructure/PushNotifications/Connectors/Sse/SseOptions.cs
+++ Modules/Devices/src/Devices.Infrastructure/PushNotifications/Connectors/Sse/SseOptions.cs
@@ -7,9 +7,6 @@ public class SseOptions
     [Required]
     public required bool Enabled { get; set; }
 
-    [Required]
-    public bool EnableHealthCheck { get; set; } = true;
-
     [Url]
     [Required]
     public required string SseServerBaseAddress { get; set; }
