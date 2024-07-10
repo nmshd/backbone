37883edf9412b5e714df57a891b80e63dfda9780
Timo Notheisen <timo.notheisen@js-soft.com>
2024-07-10 08:52:11 +0200

    fix: actually use anonymized address on anonymization of RelationshipTemplateAllocations

--- Modules/Messages/src/Messages.Application/DomainEvents/Incoming/RelationshipStatusChanged/RelationshipStatusChangedDomainEventHandler.cs
+++ Modules/Messages/src/Messages.Application/DomainEvents/Incoming/RelationshipStatusChanged/RelationshipStatusChangedDomainEventHandler.cs
@@ -1,5 +1,4 @@
-﻿using System.Text;
-using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
+﻿using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
 using Backbone.DevelopmentKit.Identity.ValueObjects;
 using Backbone.Modules.Messages.Application.Infrastructure.Persistence.Repository;
 using Backbone.Modules.Messages.Domain.DomainEvents.Incoming;
@@ -11,7 +10,6 @@ namespace Backbone.Modules.Messages.Application.DomainEvents.Incoming.Relationsh
 
 public class RelationshipStatusChangedDomainEventHandler : IDomainEventHandler<RelationshipStatusChangedDomainEvent>
 {
-    private const string DELETED_IDENTITY_STRING = "deleted identity";
     private readonly IMessagesRepository _messagesRepository;
     private readonly ILogger<RelationshipStatusChangedDomainEventHandler> _logger;
     private readonly ApplicationOptions _applicationOptions;
@@ -32,7 +30,7 @@ public class RelationshipStatusChangedDomainEventHandler : IDomainEventHandler<R
             return;
         }
 
-        var anonymizedIdentityAddress = IdentityAddress.Create(Encoding.Unicode.GetBytes(DELETED_IDENTITY_STRING), _applicationOptions.DidDomainName);
+        var anonymizedIdentityAddress = IdentityAddress.GetAnonymized(_applicationOptions.DidDomainName);
         var messagesExchangedBetweenRelationshipParticipants = (await _messagesRepository.Find(Message.WasExchangedBetween(@event.Initiator, @event.Peer), CancellationToken.None)).ToList();
         foreach (var message in messagesExchangedBetweenRelationshipParticipants)
         {
