bf686e3b344294540c2a1459a29889cd13168272
Mika Herrmann <mika.herrmann@js-soft.com>
2024-07-10 14:54:25 +0200

    feat: Add TreeHandler

---
+++ DatabaseMigrator/TreeHandler.cs
@@ -0,0 +1,17 @@
+﻿using Backbone.BuildingBlocks.Infrastructure.Persistence.Database.Attributes;
+
+namespace Backbone.DatabaseMigrator;
+
+public class TreeHandler
+{
+    private List<MigrationInfo> _rawMigrations;
+
+    public TreeHandler(List<MigrationInfo> rawMigrations)
+    {
+        _rawMigrations = rawMigrations;
+    }
+}
+
+public record MigrationInfo(ModuleType Type, string Name, bool IsApplied, IList<MigrationDependency> Dependencies);
+
+public record MigrationDependency(ModuleType Type, string Id);
