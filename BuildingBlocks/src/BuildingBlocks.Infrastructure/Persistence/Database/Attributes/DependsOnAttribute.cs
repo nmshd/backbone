6741b286c17e600b6b6fe13d5c4c3a3cc79c306e
Mika Herrmann <mika.herrmann@js-soft.com>
2024-07-10 14:54:14 +0200

    feat: Add DependsOnAttribute

---
+++ BuildingBlocks/src/BuildingBlocks.Infrastructure/Persistence/Database/Attributes/DependsOnAttribute.cs
@@ -0,0 +1,27 @@
+﻿namespace Backbone.BuildingBlocks.Infrastructure.Persistence.Database.Attributes;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+public class DependsOnAttribute : Attribute
+{
+    public DependsOnAttribute(ModuleType module, string migrationId)
+    {
+        Module = module;
+        MigrationId = migrationId;
+    }
+
+    public ModuleType Module { get; }
+    public string MigrationId { get; }
+}
+
+public enum ModuleType
+{
+    AdminApi,
+    Challenges,
+    Devices,
+    Files,
+    Messages,
+    Quotas,
+    Relationships,
+    Synchronization,
+    Tokens
+}
