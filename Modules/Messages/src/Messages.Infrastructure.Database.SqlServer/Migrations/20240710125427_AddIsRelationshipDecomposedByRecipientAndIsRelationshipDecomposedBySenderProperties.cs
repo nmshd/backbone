0e5b60b240d40e0c6c4da911086acc7a859eb486
Timo Notheisen <timo.notheisen@js-soft.com>
2024-07-11 10:31:52 +0200

    feat: add migrations

---
+++ Modules/Messages/src/Messages.Infrastructure.Database.SqlServer/Migrations/20240710125427_AddIsRelationshipDecomposedByRecipientAndIsRelationshipDecomposedBySenderProperties.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backbone.Modules.Messages.Infrastructure.Database.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsRelationshipDecomposedByRecipientAndIsRelationshipDecomposedBySenderProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRelationshipDecomposedByRecipient",
+                schema: "Messages",
+                table: "RecipientInformation",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRelationshipDecomposedBySender",
+                schema: "Messages",
+                table: "RecipientInformation",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRelationshipDecomposedByRecipient",
+                schema: "Messages",
+                table: "RecipientInformation");
+
+            migrationBuilder.DropColumn(
+                name: "IsRelationshipDecomposedBySender",
+                schema: "Messages",
+                table: "RecipientInformation");
+        }
+    }
+}
