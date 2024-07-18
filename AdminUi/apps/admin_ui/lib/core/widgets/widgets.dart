003c9ae52dddc254f24f7481590364ac77a0a151
Vladimir Vuckovic <vidrak97@gmail.com>
2024-07-18 08:22:07 +0200

    chore: add audi logs table

--- AdminUi/apps/admin_ui/lib/core/widgets/widgets.dart
+++ AdminUi/apps/admin_ui/lib/core/widgets/widgets.dart
@@ -1,5 +1,6 @@
 export 'app_title.dart';
 export 'copy_to_clipboard_button.dart';
+export 'deletion_process_auditlogs_table.dart';
 export 'filters/filters.dart';
 export 'identities_data_table/identities_data_table.dart';
 export 'quotas_button_group.dart';
