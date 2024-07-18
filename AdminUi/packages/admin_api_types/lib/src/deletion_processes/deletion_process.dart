a87fdd8d2c447e875b2e7ec7ffad5f94df35ab6e
Vladimir Vuckovic <vidrak97@gmail.com>
2024-07-18 08:24:11 +0200

    chore: shorten the type because of unused information

--- AdminUi/packages/admin_api_types/lib/src/deletion_processes/deletion_process.dart
+++ AdminUi/packages/admin_api_types/lib/src/deletion_processes/deletion_process.dart
@@ -3,44 +3,6 @@ import 'package:json_annotation/json_annotation.dart';
 part 'deletion_process.g.dart';
 
 @JsonSerializable()
-class DeletionProcessDetail {
-  final String id;
-  final String status;
-  final DateTime createdAt;
-  final DateTime approvalPeriodEndsAt;
-  final DateTime? approvalReminder1SentAt;
-  final DateTime? approvalReminder2SentAt;
-  final DateTime? approvalReminder3SentAt;
-  final DateTime? approvedAt;
-  final String? approvedByDevice;
-  final DateTime? gracePeriodEndsAt;
-  final DateTime? gracePeriodReminder1SentAt;
-  final DateTime? gracePeriodReminder2SentAt;
-  final DateTime? gracePeriodReminder3SentAt;
-  final List<AuditLog>? auditLogs;
-
-  DeletionProcessDetail({
-    required this.id,
-    required this.status,
-    required this.createdAt,
-    required this.approvalPeriodEndsAt,
-    this.approvalReminder1SentAt,
-    this.approvalReminder2SentAt,
-    this.approvalReminder3SentAt,
-    this.approvedAt,
-    this.approvedByDevice,
-    this.gracePeriodEndsAt,
-    this.gracePeriodReminder1SentAt,
-    this.gracePeriodReminder2SentAt,
-    this.gracePeriodReminder3SentAt,
-    this.auditLogs,
-  });
-
-  factory DeletionProcessDetail.fromJson(dynamic json) => _$DeletionProcessDetailFromJson(json as Map<String, dynamic>);
-  Map<String, dynamic> toJson() => _$DeletionProcessDetailToJson(this);
-}
-
-@JsonSerializable()
 class DeletionProcess {
   final String id;
   final String status;
@@ -74,6 +36,24 @@ class DeletionProcess {
 
   factory DeletionProcess.fromJson(dynamic json) => _$DeletionProcessFromJson(json as Map<String, dynamic>);
   Map<String, dynamic> toJson() => _$DeletionProcessToJson(this);
+}
+
+@JsonSerializable()
+class DeletionProcessDetail {
+  final String id;
+  final String status;
+  final DateTime createdAt;
+  final List<AuditLog> auditLog;
+
+  DeletionProcessDetail({
+    required this.id,
+    required this.status,
+    required this.createdAt,
+    required this.auditLog,
+  });
+
+  factory DeletionProcessDetail.fromJson(dynamic json) => _$DeletionProcessDetailFromJson(json as Map<String, dynamic>);
+  Map<String, dynamic> toJson() => _$DeletionProcessDetailToJson(this);
 }
 
 @JsonSerializable()
