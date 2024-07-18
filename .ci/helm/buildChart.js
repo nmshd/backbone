3ac7ed5bd66ff73a37d351cbe5c065a972a95741
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-07-18 20:54:31 +0200

    fix: helm

--- .ci/helm/buildChart.js
+++ .ci/helm/buildChart.js
@@ -5,6 +5,6 @@ import { getRequiredEnvVar } from "../lib.js";
 
 const version = getRequiredEnvVar("VERSION");
 
-await $`helm dependency update helm`;
+await $`Executables/helm dependency update helm`;
 
 await $`helm package --version ${version} --app-version ${version} helm`;
