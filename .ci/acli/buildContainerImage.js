bf3ab37bb1ccaabc35124714930fd5b292342bbb
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-07-22 13:09:39 +0200

    chore: move AdminCli to Executable folder & fix pathing

--- .ci/acli/buildContainerImage.js
+++ .ci/acli/buildContainerImage.js
@@ -8,4 +8,4 @@ const tag = getRequiredEnvVar("TAG");
 const platforms = process.env.PLATFORMS ?? "linux/amd64,linux/arm64";
 const push = process.env.PUSH === "1" ? ["--push", "--provenance=true", "--sbom=true"] : "";
 
-await $`docker buildx build --file ./Modules/Devices/src/Devices.AdminCli/Dockerfile --tag ghcr.io/nmshd/backbone-admin-cli:${tag} --platform ${platforms} --build-arg VERSION=${tag} ${push} .`;
+await $`docker buildx build --file ./Executables/AdminCli/src/AdminCli/Dockerfile --tag ghcr.io/nmshd/backbone-admin-cli:${tag} --platform ${platforms} --build-arg VERSION=${tag} ${push} .`;
