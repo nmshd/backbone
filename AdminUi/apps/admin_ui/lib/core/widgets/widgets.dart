6c9c01a74d6153ec3c95afe9b0636f6753beb6a3
Vladimir Vuckovic <vidrak97@gmail.com>
2024-07-23 08:37:06 +0200

    chore: add entity details

--- AdminUi/apps/admin_ui/lib/core/widgets/widgets.dart
+++ AdminUi/apps/admin_ui/lib/core/widgets/widgets.dart
@@ -1,6 +1,7 @@
 export 'app_title.dart';
 export 'copy_to_clipboard_button.dart';
 export 'deletion_process_auditlogs_table.dart';
+export 'entity_details.dart';
 export 'filters/filters.dart';
 export 'identities_data_table/identities_data_table.dart';
 export 'quotas_button_group.dart';
