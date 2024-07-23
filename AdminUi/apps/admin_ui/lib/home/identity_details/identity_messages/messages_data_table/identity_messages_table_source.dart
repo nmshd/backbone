88c4d2bb928afe026b885744d79171d2ce612641
Vladimir Vuckovic <vidrak97@gmail.com>
2024-07-23 15:18:44 +0200

    fix: received message different row heights

--- AdminUi/apps/admin_ui/lib/home/identity_details/identity_messages/messages_data_table/identity_messages_table_source.dart
+++ AdminUi/apps/admin_ui/lib/home/identity_details/identity_messages/messages_data_table/identity_messages_table_source.dart
@@ -56,13 +56,13 @@ class IdentityMessagesTableSource extends AsyncDataTableSource {
           .map(
             (message) => DataRow2.byIndex(
               index: pageNumber * count + message.$1,
-              specificRowHeight: message.$2.recipients.length > 3
+              specificRowHeight: message.$2.recipients.length > 3 && _checkIfTheTypeIsOutgoing()
                   ? 100.0
-                  : message.$2.recipients.length == 3
+                  : message.$2.recipients.length == 3 && _checkIfTheTypeIsOutgoing()
                       ? 65.0
                       : null,
               cells: [
-                if (type == MessageType.outgoing) DataCell(_RecipientsCell(recipients: message.$2.recipients)),
+                if (_checkIfTheTypeIsOutgoing()) DataCell(_RecipientsCell(recipients: message.$2.recipients)),
                 if (type == MessageType.incoming) ...[
                   DataCell(Text(message.$2.senderAddress)),
                   DataCell(Text(message.$2.senderDevice)),
@@ -88,6 +88,10 @@ class IdentityMessagesTableSource extends AsyncDataTableSource {
   int _totalPages(int count, List<Message>? data) {
     if (data == null || data.isEmpty) return 1;
     return (data.length / count).ceil();
+  }
+
+  bool _checkIfTheTypeIsOutgoing() {
+    return type == MessageType.outgoing;
   }
 }
 
