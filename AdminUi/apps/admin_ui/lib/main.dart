e994013f4085e335e6c4265aca1738f820930556
Julian König <julian.koenig@js-soft.com>
2024-07-23 15:55:30 +0200

    fix: better conditional import

--- AdminUi/apps/admin_ui/lib/main.dart
+++ AdminUi/apps/admin_ui/lib/main.dart
@@ -9,7 +9,7 @@ import 'core/models/models.dart';
 import 'core/theme/theme.dart';
 import 'home/home.dart';
 import 'screens/screens.dart';
-import 'setup/setup_desktop.dart' if (dart.library.html) 'setup/setup_web.dart';
+import 'setup/setup_desktop.dart' if (dart.library.html) 'setup/setup_web.dart' if (dart.library.js_interop) 'setup/setup_web.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
