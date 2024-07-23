a18f3148a815bb52ac41a6af2e1a3df1fd2b588b
Julian König <julian.koenig@js-soft.com>
2024-07-23 16:19:10 +0200

    chore: delete ClientApp

--- AdminApi/src/AdminApi/ClientApp/src/app/utils/custom-route-reuse-strategy.ts
+++
@@ -1,7 +0,0 @@
-import { ActivatedRouteSnapshot, BaseRouteReuseStrategy } from "@angular/router";
-
-export class CustomRouteReuseStrategy extends BaseRouteReuseStrategy {
-    public override shouldReuseRoute(future: ActivatedRouteSnapshot, _curr: ActivatedRouteSnapshot): boolean {
-        return future.data["reuseComponent"];
-    }
-}
