855b66668ee53df82efd4c640d92e2ac386bdf00
Vladimir Vuckovic <vidrak97@gmail.com>
2024-07-24 09:38:12 +0200

    chore: add additional data and rename a class

--- AdminUi/packages/admin_api_types/lib/src/deletion_processes/deletion_process.dart
+++ AdminUi/packages/admin_api_types/lib/src/deletion_processes/deletion_process.dart
@@ -3,21 +3,37 @@ import 'package:json_annotation/json_annotation.dart';
 part 'deletion_process.g.dart';
 
 @JsonSerializable()
-class AuditLogs {
+class AuditLog {
   final String id;
   final DateTime createdAt;
   final String messageKey;
   final String? oldStatus;
   final String newStatus;
+  final Map<String, String> additionalData;
 
-  AuditLogs({
+  AuditLog({
     required this.id,
     required this.createdAt,
     required this.messageKey,
     required this.newStatus,
+    required this.additionalData,
     this.oldStatus,
   });
 
-  factory AuditLogs.fromJson(dynamic json) => _$AuditLogsFromJson(json as Map<String, dynamic>);
-  Map<String, dynamic> toJson() => _$AuditLogsToJson(this);
+  factory AuditLog.fromJson(dynamic json) => _$AuditLogFromJson(json as Map<String, dynamic>);
+  Map<String, dynamic> toJson() => _$AuditLogToJson(this);
+}
+
+@JsonSerializable()
+class AdditionalData {
+  final String key;
+  final String value;
+
+  AdditionalData({
+    required this.key,
+    required this.value,
+  });
+
+  factory AdditionalData.fromJson(dynamic json) => _$AdditionalDataFromJson(json as Map<String, dynamic>);
+  Map<String, dynamic> toJson() => _$AdditionalDataToJson(this);
 }
--- AdminUi/packages/admin_api_types/lib/src/deletion_processes/deletion_process.g.dart
+++ AdminUi/packages/admin_api_types/lib/src/deletion_processes/deletion_process.g.dart
@@ -6,18 +6,30 @@ part of 'deletion_process.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-AuditLogs _$AuditLogsFromJson(Map<String, dynamic> json) => AuditLogs(
+AuditLog _$AuditLogFromJson(Map<String, dynamic> json) => AuditLog(
       id: json['id'] as String,
       createdAt: DateTime.parse(json['createdAt'] as String),
       messageKey: json['messageKey'] as String,
       newStatus: json['newStatus'] as String,
+      additionalData: Map<String, String>.from(json['additionalData'] as Map),
       oldStatus: json['oldStatus'] as String?,
     );
 
-Map<String, dynamic> _$AuditLogsToJson(AuditLogs instance) => <String, dynamic>{
+Map<String, dynamic> _$AuditLogToJson(AuditLog instance) => <String, dynamic>{
       'id': instance.id,
       'createdAt': instance.createdAt.toIso8601String(),
       'messageKey': instance.messageKey,
       'oldStatus': instance.oldStatus,
       'newStatus': instance.newStatus,
+      'additionalData': instance.additionalData,
+    };
+
+AdditionalData _$AdditionalDataFromJson(Map<String, dynamic> json) => AdditionalData(
+      key: json['key'] as String,
+      value: json['value'] as String,
+    );
+
+Map<String, dynamic> _$AdditionalDataToJson(AdditionalData instance) => <String, dynamic>{
+      'key': instance.key,
+      'value': instance.value,
     };
