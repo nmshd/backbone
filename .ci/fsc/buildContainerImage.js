5ecbb93a659b34ef95b24b778c07b6b23b1de5f0
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-07-24 08:27:02 +0200

    chore: add missing slash for other buildDockerImage files

--- .ci/fsc/buildContainerImage.js
+++ .ci/fsc/buildContainerImage.js
@@ -8,4 +8,4 @@ const tag = getRequiredEnvVar("TAG");
 const platforms = process.env.PLATFORMS ?? "linux/amd64,linux/arm64";
 const push = process.env.PUSH === "1" ? ["--push", "--provenance=true", "--sbom=true"] : "";
 
-await $`docker buildx build --file .Executables/FilesSanityCheck/src/FilesSanityCheck/Dockerfile --tag ghcr.io/nmshd/backbone-files-sanity-check:${tag} --platform ${platforms} --build-arg VERSION=${tag} ${push} .`;
+await $`docker buildx build --file ./Executables/FilesSanityCheck/src/FilesSanityCheck/Dockerfile --tag ghcr.io/nmshd/backbone-files-sanity-check:${tag} --platform ${platforms} --build-arg VERSION=${tag} ${push} .`;
