a8db532b9579b52314aff5853733ad3ade1d12a7
Julian König <julian.koenig@js-soft.com>
2024-07-24 14:58:38 +0200

    chore: naming / simplify

--- AdminUi/apps/admin_ui/lib/home/identities_overview/deletion_process_audit_log_details/deletion_process_audit_log_details.dart
+++ AdminUi/apps/admin_ui/lib/home/identities_overview/deletion_process_audit_log_details/deletion_process_audit_log_details.dart
@@ -5,24 +5,25 @@ import 'package:get_it/get_it.dart';
 
 import '/core/core.dart';
 
-class DeletionProcessAuditLogsDetails extends StatefulWidget {
+class DeletionProcessAuditLogDetails extends StatefulWidget {
   final String identityAddress;
 
-  const DeletionProcessAuditLogsDetails({
+  const DeletionProcessAuditLogDetails({
     required this.identityAddress,
     super.key,
   });
 
   @override
-  State<DeletionProcessAuditLogsDetails> createState() => _DeletionProcessAuditLogsDetailsState();
+  State<DeletionProcessAuditLogDetails> createState() => _DeletionProcessAuditLogDetailsState();
 }
 
-class _DeletionProcessAuditLogsDetailsState extends State<DeletionProcessAuditLogsDetails> {
+class _DeletionProcessAuditLogDetailsState extends State<DeletionProcessAuditLogDetails> {
   List<IdentityDeletionProcessAuditLogEntry>? _identityDeletionProcessAuditLogs;
 
   @override
   void initState() {
     super.initState();
+
     _loadIdentityDeletionProcessAuditLogs();
   }
 
@@ -34,26 +35,14 @@ class _DeletionProcessAuditLogsDetailsState extends State<DeletionProcessAuditLo
 
     final identityDeletionProcessAuditLogs = _identityDeletionProcessAuditLogs!;
     return Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        if (kIsDesktop) const BackButton(),
-        Row(
-          children: [
-            Expanded(
-              child: Card(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: EntityDetails(title: context.l10n.identity, value: widget.identityAddress),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
+        if (kIsDesktop) const Align(alignment: Alignment.centerLeft, child: BackButton()),
+        Card(
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Align(alignment: Alignment.centerLeft, child: EntityDetails(title: context.l10n.identity, value: widget.identityAddress)),
+          ),
         ),
         Expanded(child: DeletionProcessAuditLogsTable(auditLogs: identityDeletionProcessAuditLogs)),
       ],
--- AdminUi/apps/admin_ui/lib/main.dart
+++ AdminUi/apps/admin_ui/lib/main.dart
@@ -60,7 +60,7 @@ final _router = GoRouter(
               parentNavigatorKey: _shellNavigatorKey,
               path: ':address/deletion-process-audit-logs',
               pageBuilder: (context, state) => NoTransitionPage(
-                child: DeletionProcessAuditLogsDetails(identityAddress: state.pathParameters['address']!),
+                child: DeletionProcessAuditLogDetails(identityAddress: state.pathParameters['address']!),
               ),
             ),
           ],
