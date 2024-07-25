759eb59169d5c84f61c170f5e403e2189bac624c
Vladimir Vuckovic <37671301+stamenione@users.noreply.github.com>
2024-07-25 09:11:17 +0200

    AdminUI: Show identity deletion process audit logs (#731)
    * feat: add audit logs endpoint
    
    * chore: wip
    
    * feat: add deletion process audit log input
    
    * chore: make old status optional
    
    * chore: prepare for translation
    
    * chore: add more translations
    
    * chore: add find to translations
    
    * chore: add address to translation
    
    * feat: add deletion process audit logs
    
    * refactor: rename a file
    
    * chore: add a new route
    
    * fix: import and add gaps
    
    * fix: add trailing comma
    
    * fix: change sizedbox with gaps
    
    * fix: missing translations
    
    * chore: remove unnecessary text
    
    * chore: add additional data and rename a class
    
    * refactor: rename
    
    * fix: naming
    
    * chore: move the label inside the text field
    
    * chore use Expanded instead of fixed sizes
    
    * chore: update lockfile
    
    * chore: remove import
    
    * chore: extract audit logs table
    
    * chore: extract details because it will be used in multiple views
    
    * chore: add missing exports
    
    * chore: add missing export
    
    * chore: rename translations
    
    * chore: change export
    
    * chore: change type
    
    * chore: move deletion process auditlogs details into the folder
    
    * fix: add translations back
    
    * fix: add missing translations
    
    * chore: move identity deletion process audit log entries into identity type
    
    * fix: add identity deletion process audit log entry into separate file
    
    * refactor: rename files
    
    * chore: do not pass unused locale
    
    * fix: remove dart io
    
    * refactor: make EntityDetails really reusable
    
    * chore: move widget to widgets
    
    * chore: naming / simplify
    
    * refactor: simplify even more code
    
    * fix: remove gap
    
    * chore: remove padding we have nowhere else
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: Julian König <julian.koenig@js-soft.com>

--- AdminUi/apps/admin_ui/lib/home/home.dart
+++ AdminUi/apps/admin_ui/lib/home/home.dart
@@ -1,4 +1,5 @@
 export 'clients_overview/clients_overview.dart';
+export 'identities_overview/deletion_process_audit_log_details/deletion_process_audit_log_details.dart';
 export 'identities_overview/identities_overview.dart';
 export 'identity_details/identity_details.dart';
 export 'tier_detail/tier_detail.dart';
