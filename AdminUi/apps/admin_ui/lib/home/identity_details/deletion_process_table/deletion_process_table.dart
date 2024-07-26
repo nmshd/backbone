0ceecbaf2b896eaa979b43d9ead4722aef4f7656
Julian König <julian.koenig@js-soft.com>
2024-07-26 15:19:30 +0200

    refactor: cleaner deletion process table

--- AdminUi/apps/admin_ui/lib/home/identity_details/deletion_process_table/deletion_process_table.dart
+++ AdminUi/apps/admin_ui/lib/home/identity_details/deletion_process_table/deletion_process_table.dart
@@ -1,5 +1,6 @@
 import 'package:admin_api_sdk/admin_api_sdk.dart';
 import 'package:admin_api_types/admin_api_types.dart';
+import 'package:collection/collection.dart';
 import 'package:data_table_2/data_table_2.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
@@ -62,11 +63,12 @@ class _DeletionProcessTableState extends State<DeletionProcessTable> {
                           return DataRow2(
                             specificRowHeight: 60,
                             onTap: () async {
-                              final result =
-                                  await context.push<bool?>('/identities/${widget.address}/deletion-process-details/${deletionProcess.id}');
-                              if (result! == true) {
-                                await _reloadIdentityDeletionProcesses();
-                              }
+                              final result = await context.push<bool?>(
+                                '/identities/${widget.address}/deletion-process-details/${deletionProcess.id}',
+                              );
+                              if (result != true) return;
+
+                              await _reloadIdentityDeletionProcesses();
                             },
                             cells: [
                               DataCell(Text(deletionProcess.id, style: TextStyle(color: textColor))),
@@ -90,11 +92,9 @@ class _DeletionProcessTableState extends State<DeletionProcessTable> {
                               ),
                               DataCell(
                                 _RemindersCell(
-                                  reminders: [
-                                    deletionProcess.approvalReminder1SentAt,
-                                    deletionProcess.approvalReminder2SentAt,
-                                    deletionProcess.approvalReminder3SentAt,
-                                  ],
+                                  name: context.l10n.deletionProcessTable_approvalRemindersCell_reminder,
+                                  noDataText: context.l10n.deletionProcessTable_approvalRemindersCell_noData,
+                                  reminders: deletionProcess.approvalReminders,
                                   textColor: textColor,
                                 ),
                               ),
@@ -109,17 +109,15 @@ class _DeletionProcessTableState extends State<DeletionProcessTable> {
                               DataCell(Text(deletionProcess.approvedByDevice ?? '', style: TextStyle(color: textColor))),
                               DataCell(
                                 _RemindersCell(
-                                  reminders: [
-                                    deletionProcess.gracePeriodReminder1SentAt,
-                                    deletionProcess.gracePeriodReminder2SentAt,
-                                    deletionProcess.gracePeriodReminder3SentAt,
-                                  ],
+                                  name: context.l10n.deletionProcessTable_gracePeriodRemindersCell_reminder,
+                                  noDataText: context.l10n.deletionProcessTable_gracePeriodRemindersCell_noData,
+                                  reminders: deletionProcess.gracePeriodReminders,
                                   textColor: textColor,
                                 ),
                               ),
                               DataCell(
                                 Text(
-                                  deletionProcess.approvedAt != null
+                                  deletionProcess.gracePeriodEndsAt != null
                                       ? DateFormat.yMd(Localizations.localeOf(context).languageCode).format(deletionProcess.gracePeriodEndsAt!)
                                       : '',
                                   style: TextStyle(color: textColor),
@@ -151,34 +149,45 @@ class _DeletionProcessTableState extends State<DeletionProcessTable> {
 }
 
 class _RemindersCell extends StatelessWidget {
-  final List<DateTime?> reminders;
+  final String name;
+  final String noDataText;
+  final List<DateTime> reminders;
   final Color textColor;
 
   const _RemindersCell({
+    required this.name,
+    required this.noDataText,
     required this.reminders,
     required this.textColor,
   });
 
   @override
   Widget build(BuildContext context) {
-    final validReminders = reminders.where((date) => date != null).toList();
-
-    if (validReminders.isEmpty) {
-      return Text(context.l10n.deletionProcessTable_approvalRemindersCell_noData, style: TextStyle(color: textColor));
-    }
+    if (reminders.isEmpty) return Text(noDataText, style: TextStyle(color: textColor));
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: validReminders.asMap().entries.map((entry) {
-        final index = entry.key + 1;
-        final date = entry.value!;
-        final color = textColor;
+      children: reminders.mapIndexed((index, date) {
         return Text(
-          '${context.l10n.deletionProcessTable_approvalRemindersCell_reminder} $index: ${DateFormat.yMd(Localizations.localeOf(context).languageCode).format(date)} ${DateFormat.Hms().format(date)}',
-          style: TextStyle(color: color),
+          '$name ${index + 1}: ${DateFormat.yMd(Localizations.localeOf(context).languageCode).format(date)} ${DateFormat.Hms().format(date)}',
+          style: TextStyle(color: textColor),
         );
       }).toList(),
     );
   }
+}
+
+extension _Reminders on IdentityDeletionProcess {
+  List<DateTime> get approvalReminders => [
+        if (approvalReminder1SentAt != null) approvalReminder1SentAt!,
+        if (approvalReminder2SentAt != null) approvalReminder2SentAt!,
+        if (approvalReminder3SentAt != null) approvalReminder3SentAt!,
+      ];
+
+  List<DateTime> get gracePeriodReminders => [
+        if (gracePeriodReminder1SentAt != null) gracePeriodReminder1SentAt!,
+        if (gracePeriodReminder2SentAt != null) gracePeriodReminder2SentAt!,
+        if (gracePeriodReminder3SentAt != null) gracePeriodReminder3SentAt!,
+      ];
 }
