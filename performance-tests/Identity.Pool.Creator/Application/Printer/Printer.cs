f61777215e3e7a4b793fb20862608cb1ae37ccda
Daniel Silva <daniel.p.silva@konkconsulting.com>
2024-07-30 10:02:34 +0100

    chore: update snapshot

--- performance-tests/Identity.Pool.Creator/Application/Printer/Printer.cs
+++ performance-tests/Identity.Pool.Creator/Application/Printer/Printer.cs
@@ -82,7 +82,7 @@ public class Printer : IPrinter
                 }
             }
         }
-        File.WriteAllTextAsync($@"{outputDirName}\rts.csv", stringBuilder.ToString());
+        File.WriteAllTextAsync($@"{outputDirName}\relationshipTemplates.csv", stringBuilder.ToString());
     }
 
     private static void OutputDatawalletModifications(string outputDirName, IList<PoolEntry> pools)
@@ -99,7 +99,7 @@ public class Printer : IPrinter
                 }
             }
         }
-        File.WriteAllTextAsync($@"{outputDirName}\dwms.csv", stringBuilder.ToString());
+        File.WriteAllTextAsync($@"{outputDirName}\datawalletModifications.csv", stringBuilder.ToString());
     }
 
     private static void OutputChallenges(string outputDirName, IList<PoolEntry> pools)
