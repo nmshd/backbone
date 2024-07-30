1e499bcf3f3c6f043831e00bed5f7caff9125aaa
Daniel Almeida <115644988+daniel-almeida-konkconsulting@users.noreply.github.com>
2024-07-30 19:18:39 +0100

    test: unit tests for validators in files module

---
+++ Modules/Files/test/Files.Application.Tests/Tests/Files/Queries/GetFileMetadata/ValidatorTests.cs
@@ -0,0 +1,34 @@
+﻿using Backbone.Modules.Files.Application.Files.Queries.GetFileMetadata;
+using Backbone.Modules.Files.Domain.Entities;
+using Backbone.UnitTestTools.BaseClasses;
+using FluentAssertions;
+using FluentValidation.TestHelper;
+using Xunit;
+
+namespace Backbone.Modules.Files.Application.Tests.Tests.Files.Queries.GetFileMetadata;
+
+public class ValidatorTests : AbstractTestsBase
+{
+    [Fact]
+    public void Happy_path()
+    {
+        var validator = new Validator();
+
+        var validationResult = validator.TestValidate(new GetFileMetadataQuery { Id = FileId.New() });
+
+        validationResult.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Fact]
+    public void Fails_when_file_id_is_invalid()
+    {
+        var validator = new Validator();
+
+        var validationResult = validator.TestValidate(new GetFileMetadataQuery { Id = "some-invalid-file-id" });
+
+        validationResult.ShouldHaveValidationErrorFor(x => x.Id);
+        validationResult.Errors.Should().HaveCount(1);
+        validationResult.Errors.First().ErrorCode.Should().Be("error.platform.validation.invalidPropertyValue");
+        validationResult.Errors.First().ErrorMessage.Should().Be("The ID is not valid. Check length, prefix and the used characters.");
+    }
+}
---
+++ Modules/Files/test/Files.Application.Tests/Tests/Identities/Commands/DeleteFilesOfIdentity/ValidatorTests.cs
@@ -0,0 +1,34 @@
+﻿using Backbone.Modules.Files.Application.Identities.Commands.DeleteFilesOfIdentity;
+using Backbone.UnitTestTools.BaseClasses;
+using Backbone.UnitTestTools.Data;
+using FluentAssertions;
+using FluentValidation.TestHelper;
+using Xunit;
+
+namespace Backbone.Modules.Files.Application.Tests.Tests.Identities.Commands.DeleteFilesOfIdentity;
+
+public class ValidatorTests : AbstractTestsBase
+{
+    [Fact]
+    public void Happy_path()
+    {
+        var validator = new Validator();
+
+        var validationResult = validator.TestValidate(new DeleteFilesOfIdentityCommand(TestDataGenerator.CreateRandomIdentityAddress()));
+
+        validationResult.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Fact]
+    public void Fails_when_identity_address_is_invalid()
+    {
+        var validator = new Validator();
+
+        var validationResult = validator.TestValidate(new DeleteFilesOfIdentityCommand("invalid-identity-address"));
+
+        validationResult.ShouldHaveValidationErrorFor(x => x.IdentityAddress);
+        validationResult.Errors.Should().HaveCount(1);
+        validationResult.Errors.First().ErrorCode.Should().Be("error.platform.validation.invalidPropertyValue");
+        validationResult.Errors.First().ErrorMessage.Should().Be("The ID is not valid. Check length, prefix and the used characters.");
+    }
+}
