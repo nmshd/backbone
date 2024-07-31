5ddc21ef57431e1092e8b316ab34086ba2e5c2b3
Daniel Almeida <115644988+daniel-almeida-konkconsulting@users.noreply.github.com>
2024-07-31 16:57:17 +0100

    fix: add mappings after removing json converters

--- Modules/Tokens/src/Tokens.Application/Tokens/Commands/CreateToken/CreateTokenResponse.cs
+++ Modules/Tokens/src/Tokens.Application/Tokens/Commands/CreateToken/CreateTokenResponse.cs
@@ -1,10 +1,17 @@
+using AutoMapper;
 using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.Mapping;
 using Backbone.Modules.Tokens.Domain.Entities;
 
 namespace Backbone.Modules.Tokens.Application.Tokens.Commands.CreateToken;
 
-public class CreateTokenResponse : IMapTo<Token>
+public class CreateTokenResponse : IHaveCustomMapping
 {
-    public required TokenId Id { get; set; }
+    public required string Id { get; set; }
     public required DateTime CreatedAt { get; set; }
+
+    public void CreateMappings(Profile configuration)
+    {
+        configuration.CreateMap<Token, CreateTokenResponse>()
+            .ForMember(createTokenResponse => createTokenResponse.Id, expression => expression.MapFrom(m => m.Id.Value));
+    }
 }
--- Modules/Tokens/src/Tokens.Application/Tokens/DTOs/TokenDTO.cs
+++ Modules/Tokens/src/Tokens.Application/Tokens/DTOs/TokenDTO.cs
@@ -1,18 +1,26 @@
+using AutoMapper;
 using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.Mapping;
-using Backbone.DevelopmentKit.Identity.ValueObjects;
 using Backbone.Modules.Tokens.Domain.Entities;
 
 namespace Backbone.Modules.Tokens.Application.Tokens.DTOs;
 
-public class TokenDTO : IMapTo<Token>
+public class TokenDTO : IHaveCustomMapping
 {
-    public required TokenId Id { get; set; }
+    public required string Id { get; set; }
 
-    public required IdentityAddress CreatedBy { get; set; }
-    public required DeviceId CreatedByDevice { get; set; }
+    public required string CreatedBy { get; set; }
+    public required string CreatedByDevice { get; set; }
 
     public required DateTime CreatedAt { get; set; }
     public required DateTime ExpiresAt { get; set; }
 
     public required byte[] Content { get; set; }
+
+    public void CreateMappings(Profile configuration)
+    {
+        configuration.CreateMap<Token, TokenDTO>()
+            .ForMember(dto => dto.Id, expression => expression.MapFrom(t => t.Id.Value))
+            .ForMember(dto => dto.CreatedBy, expression => expression.MapFrom(t => t.CreatedBy.Value))
+            .ForMember(dto => dto.CreatedByDevice, expression => expression.MapFrom(t => t.CreatedByDevice.Value));
+    }
 }
