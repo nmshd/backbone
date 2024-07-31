ff10f4ed52e175385caffd45fb804339cd33e00a
Daniel Almeida <115644988+daniel-almeida-konkconsulting@users.noreply.github.com>
2024-07-31 17:12:52 +0100

    fix: mapping for value objects in relationships module

--- Modules/Relationships/src/Relationships.Application/RelationshipTemplates/Commands/CreateRelationshipTemplate/CreateRelationshipTemplateResponse.cs
+++ Modules/Relationships/src/Relationships.Application/RelationshipTemplates/Commands/CreateRelationshipTemplate/CreateRelationshipTemplateResponse.cs
@@ -1,10 +1,17 @@
+using AutoMapper;
 using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.Mapping;
 using Backbone.Modules.Relationships.Domain.Aggregates.RelationshipTemplates;
 
 namespace Backbone.Modules.Relationships.Application.RelationshipTemplates.Commands.CreateRelationshipTemplate;
 
-public class CreateRelationshipTemplateResponse : IMapTo<RelationshipTemplate>
+public class CreateRelationshipTemplateResponse : IHaveCustomMapping
 {
-    public required RelationshipTemplateId Id { get; set; }
+    public required string Id { get; set; }
     public required DateTime CreatedAt { get; set; }
+
+    public void CreateMappings(Profile configuration)
+    {
+        configuration.CreateMap<RelationshipTemplate, CreateRelationshipTemplateResponse>()
+            .ForMember(dto => dto.Id, expression => expression.MapFrom(m => m.Id.Value));
+    }
 }
--- Modules/Relationships/src/Relationships.Application/Relationships/DTOs/RelationshipTemplateDTO.cs
+++ Modules/Relationships/src/Relationships.Application/Relationships/DTOs/RelationshipTemplateDTO.cs
@@ -1,19 +1,27 @@
+using AutoMapper;
 using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.Mapping;
-using Backbone.DevelopmentKit.Identity.ValueObjects;
 using Backbone.Modules.Relationships.Domain.Aggregates.RelationshipTemplates;
 
 namespace Backbone.Modules.Relationships.Application.Relationships.DTOs;
 
 public class RelationshipTemplateDTO : IMapTo<RelationshipTemplate>
 {
-    public required RelationshipTemplateId Id { get; set; }
+    public required string Id { get; set; }
 
-    public required IdentityAddress CreatedBy { get; set; }
-    public required DeviceId CreatedByDevice { get; set; }
+    public required string CreatedBy { get; set; }
+    public required string CreatedByDevice { get; set; }
     public int? MaxNumberOfAllocations { get; set; }
     public DateTime? ExpiresAt { get; set; }
     public byte[]? Content { get; set; }
 
     public required DateTime CreatedAt { get; set; }
     public DateTime? DeletedAt { get; set; }
+
+    public void CreateMappings(Profile configuration)
+    {
+        configuration.CreateMap<RelationshipTemplate, RelationshipTemplateDTO>()
+            .ForMember(dto => dto.Id, expression => expression.MapFrom(t => t.Id.Value))
+            .ForMember(dto => dto.CreatedBy, expression => expression.MapFrom(t => t.CreatedBy.Value))
+            .ForMember(dto => dto.CreatedByDevice, expression => expression.MapFrom(t => t.CreatedByDevice.Value));
+    }
 }
