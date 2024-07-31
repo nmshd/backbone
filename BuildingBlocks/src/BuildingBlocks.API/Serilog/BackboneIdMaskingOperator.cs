6f360b256421244fe60dbaa1c53d2b7ed550d2be
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-07-31 09:56:18 +0200

    chore: fix formatting

--- BuildingBlocks/src/BuildingBlocks.API/Serilog/BackboneIdMaskingOperator.cs
+++ BuildingBlocks/src/BuildingBlocks.API/Serilog/BackboneIdMaskingOperator.cs
@@ -5,5 +5,5 @@ public class BackboneIdMaskingOperator : RegexMaskingOperator
 {
     public BackboneIdMaskingOperator(string regexString) : base(regexString) { }
     private static string IdRegexPattern(string prefix, int maxLength) => $@"{prefix}[a-zA-Z0-9]{{{maxLength}}}";
-    public static BackboneIdMaskingOperator ForId(string prefix, int maxLength) => new BackboneIdMaskingOperator(IdRegexPattern(prefix, maxLength));
+    public static BackboneIdMaskingOperator ForId(string prefix, int maxLength) => new(IdRegexPattern(prefix, maxLength));
 }
--- BuildingBlocks/src/BuildingBlocks.API/Serilog/IdentityAddressMaskingOperator.cs
+++ BuildingBlocks/src/BuildingBlocks.API/Serilog/IdentityAddressMaskingOperator.cs
@@ -5,5 +5,5 @@ namespace Backbone.BuildingBlocks.API.Serilog;
 public class IdentityAddressMaskingOperator : RegexMaskingOperator
 {
     public IdentityAddressMaskingOperator(string regexString) : base(regexString) { }
-    public static IdentityAddressMaskingOperator Create() => new IdentityAddressMaskingOperator(IdentityAddress.IdentityAddressValidatorRegex().ToString());
+    public static IdentityAddressMaskingOperator Create() => new(IdentityAddress.IdentityAddressValidatorRegex().ToString());
 }
