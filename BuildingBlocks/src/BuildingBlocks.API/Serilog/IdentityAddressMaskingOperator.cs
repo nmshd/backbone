229faa1a9d39fc77c57d7b7817fa3f1f577e9bf0
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-08-01 08:38:26 +0200

    refactor: introduce method with explanatory name

--- BuildingBlocks/src/BuildingBlocks.API/Serilog/IdentityAddressMaskingOperator.cs
+++ BuildingBlocks/src/BuildingBlocks.API/Serilog/IdentityAddressMaskingOperator.cs
@@ -4,6 +4,12 @@ using Serilog.Enrichers.Sensitive;
 namespace Backbone.BuildingBlocks.API.Serilog;
 public class IdentityAddressMaskingOperator : RegexMaskingOperator
 {
-    public IdentityAddressMaskingOperator() : base(IdentityAddressRegex.Substring(1, IdentityAddressRegex.Length - 2)) { }
+    public IdentityAddressMaskingOperator() : base(TrimRegexForMidStringUse()) { }
+
     private static string IdentityAddressRegex => IdentityAddress.IdentityAddressValidatorRegex().ToString();
+
+    private static string TrimRegexForMidStringUse()
+    {
+        return IdentityAddressRegex[1..^1];
+    }
 }
