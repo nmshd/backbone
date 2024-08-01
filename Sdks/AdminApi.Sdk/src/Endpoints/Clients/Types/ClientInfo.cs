fae4cfc82730f94c147471d15875f070d28a7c53
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-08-01 18:15:42 +0200

    feat: add properties to match ClientDTO

--- Sdks/AdminApi.Sdk/src/Endpoints/Clients/Types/ClientInfo.cs
+++ Sdks/AdminApi.Sdk/src/Endpoints/Clients/Types/ClientInfo.cs
@@ -4,7 +4,8 @@ public class ClientInfo
 {
     public required string ClientId { get; set; }
     public required string DisplayName { get; set; }
-    public required string DefaultTier { get; set; }
+    public required string DefaultTierId { get; set; }
+    public required string DefaultTierName { get; set; }
     public required DateTime CreatedAt { get; set; }
     public int? MaxIdentities { get; set; }
 }
