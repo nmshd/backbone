b9d863e7fdc2e97ae9ba6ed2d78268eb08da8a08
Daniel Silva <daniel.p.silva@konkconsulting.com>
2024-08-01 08:28:04 +0100

    chore: update drept

---
+++ Applications/ConsumerApi/test/PerformanceTests/src/libs/drept/csv-types.ts
@@ -0,0 +1,9 @@
+// CSV types for parsing
+
+export interface CSVIdentity {
+    Address: string;
+    DeviceId: string;
+    Username: string;
+    Password: string;
+    Alias: string;
+}
--- Applications/ConsumerApi/test/PerformanceTests/src/libs/drept/index.ts
+++ Applications/ConsumerApi/test/PerformanceTests/src/libs/drept/index.ts
@@ -1,1 +1,22 @@
-// DATA REPRESENTATION FOR ENMESHED BACKBONE PERFORMANCE TESTS PROTOCOL
+// DATA REPRESENTATION FOR ENMESHED PERFORMANCE TESTS PROTOCOL
+
+export interface Identity {
+    address: string;
+    devices: Device[];
+    poolAlias: string;
+}
+
+export interface Pool {
+    name: string;
+    identities: Identity[];
+}
+
+interface Device {
+    deviceId: string;
+    username: string;
+    password: string;
+}
+
+export interface DREPT {
+    pools: Pool[];
+}
