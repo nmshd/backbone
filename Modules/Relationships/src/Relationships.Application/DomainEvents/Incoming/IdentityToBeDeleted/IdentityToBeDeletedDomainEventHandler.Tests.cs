bcdbc8887d4d67b740c93cb4cd78b4612a510c32
Daniel Almeida <115644988+daniel-almeida-konkconsulting@users.noreply.github.com>
2024-08-02 09:06:54 +0100

    Unit Tests Next to Production Code - Relationships (#744)
    * refactor: unit tests next to production code for relationships module
    
    * fix: add exceptions for cleanup and cli
    
    * refactor: add file nesting and rename test files
    
    * chore: update solution file to remove test folder
    
    * refactor: remove test helpers from release build and adjust namespaces
    
    * test: fix test class not extending AbstractTestsBase
    
    * chore: add reference to github actions logger for debug builds
    
    * refactor: named parameters
    
    * refactor: revert removel of named parameters
    
    * refactor: only include test references in relationship projects
    
    * refactor: add explanatory comment
    
    * refactor: use default item excludes instead of Compile Remove
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ Modules/Relationships/src/Relationships.Application/DomainEvents/Incoming/IdentityToBeDeleted/IdentityToBeDeletedDomainEventHandler.Tests.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq.Expressions;
+using Backbone.Modules.Relationships.Application.Infrastructure.Persistence.Repository;
+using Backbone.Modules.Relationships.Application.TestHelpers;
+using Backbone.Modules.Relationships.Domain.Aggregates.Relationships;
+using Backbone.Modules.Relationships.Domain.DomainEvents.Incoming;
+using Backbone.Modules.Relationships.Domain.DomainEvents.Outgoing;
+using Backbone.UnitTestTools.BaseClasses;
+using Backbone.UnitTestTools.Data;
+using Backbone.UnitTestTools.FluentAssertions.Extensions;
+using FakeItEasy;
+using FluentAssertions;
+using Xunit;
+
+namespace Backbone.Modules.Relationships.Application.DomainEvents.Incoming.IdentityToBeDeleted;
+
+public class IdentityToBeDeletedDomainEventHandlerTests : AbstractTestsBase
+{
+    [Fact]
+    public static async Task Publishes_PeerToBeDeletedDomainEvent()
+    {
+        //Arrange
+        var identityToBeDeleted = TestDataGenerator.CreateRandomIdentityAddress();
+
+        var peer1 = TestDataGenerator.CreateRandomIdentityAddress();
+        var peer2 = TestDataGenerator.CreateRandomIdentityAddress();
+
+        var relationshipToPeer1 = TestData.CreateActiveRelationship(peer1, identityToBeDeleted);
+        var relationshipToPeer2 = TestData.CreateActiveRelationship(peer2, identityToBeDeleted);
+
+        var fakeRelationshipsRepository = A.Dummy<IRelationshipsRepository>();
+
+        A.CallTo(() => fakeRelationshipsRepository.FindRelationships(A<Expression<Func<Relationship, bool>>>._, A<CancellationToken>._))
+            .Returns(new List<Relationship>() { relationshipToPeer1, relationshipToPeer2 });
+
+        var handler = CreateHandler(fakeRelationshipsRepository);
+
+        //Act
+        await handler.Handle(new IdentityToBeDeletedDomainEvent(identityToBeDeleted));
+
+        //Assert
+        var event1 = relationshipToPeer1.Should().HaveASingleDomainEvent<PeerToBeDeletedDomainEvent>();
+        event1.PeerOfIdentityToBeDeleted.Should().Be(peer1);
+        event1.RelationshipId.Should().Be(relationshipToPeer1.Id);
+        event1.IdentityToBeDeleted.Should().Be(identityToBeDeleted);
+
+        var event2 = relationshipToPeer2.Should().HaveASingleDomainEvent<PeerToBeDeletedDomainEvent>();
+        event2.PeerOfIdentityToBeDeleted.Should().Be(peer2);
+        event2.RelationshipId.Should().Be(relationshipToPeer2.Id);
+        event2.IdentityToBeDeleted.Should().Be(identityToBeDeleted);
+    }
+
+    private static IdentityToBeDeletedDomainEventHandler CreateHandler(IRelationshipsRepository relationshipsRepository)
+    {
+        return new IdentityToBeDeletedDomainEventHandler(relationshipsRepository);
+    }
+}
---
+++ Modules/Relationships/src/Relationships.Domain/Aggregates/Relationships/Relationship.DeletionOfParticipantCancelledTests.cs
@@ -0,0 +1,30 @@
+﻿using Backbone.Modules.Relationships.Domain.DomainEvents.Outgoing;
+using Backbone.UnitTestTools.BaseClasses;
+using Backbone.UnitTestTools.Data;
+using Backbone.UnitTestTools.FluentAssertions.Extensions;
+using FluentAssertions;
+using Xunit;
+using static Backbone.Modules.Relationships.Domain.TestHelpers.TestData;
+
+namespace Backbone.Modules.Relationships.Domain.Aggregates.Relationships;
+
+public class RelationshipDeletionOfParticipantCancelledTests : AbstractTestsBase
+{
+    [Fact]
+    public void Raises_PeerDeletionCancelledDomainEvent()
+    {
+        // Arrange
+        var identityToBeDeleted = TestDataGenerator.CreateRandomIdentityAddress();
+        var peer = TestDataGenerator.CreateRandomIdentityAddress();
+        var relationship = CreateActiveRelationship(peer, identityToBeDeleted);
+
+        // Act
+        relationship.DeletionOfParticipantCancelled(identityToBeDeleted);
+
+        // Assert
+        var domainEvent = relationship.Should().HaveASingleDomainEvent<PeerDeletionCancelledDomainEvent>();
+        domainEvent.PeerOfIdentityWithDeletionCancelled.Should().Be(peer);
+        domainEvent.RelationshipId.Should().Be(relationship.Id);
+        domainEvent.IdentityWithDeletionCancelled.Should().Be(identityToBeDeleted);
+    }
+}
