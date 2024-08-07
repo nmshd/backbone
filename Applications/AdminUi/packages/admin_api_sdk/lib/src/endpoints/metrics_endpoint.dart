25715e7106a86d3b85b60654d20eda1f6d249a38
Vladimir Vuckovic <37671301+stamenione@users.noreply.github.com>
2024-08-07 12:39:27 +0200

    Admin UI: Restructure and rename API types  (#789)
    * refactor: rename message
    
    * chore: add metrics endpoint and response type
    
    * refactor: extract tier types and refactor their names
    
    * refactor: rename and extract quotas
    
    * refactor: extract and rename identity types
    
    * refactor: extract rest of the types into separate files and extract enum
    
    * refactor: undo strange naming choices
    
    * refactor: undo strange naming choices
    
    * chore: move stuff to correct endpoint
    
    * chore: wording
    
    * fix: imports
    
    * refactor: remove strange enums folder
    
    * chore: naming
    
    * fix: make non-optionals non-optional
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: Julian König <julian.koenig@js-soft.com>

---
+++ Applications/AdminUi/packages/admin_api_sdk/lib/src/endpoints/metrics_endpoint.dart
@@ -0,0 +1,13 @@
+import 'package:admin_api_types/admin_api_types.dart';
+
+import '../types/types.dart';
+import 'endpoint.dart';
+
+class MetricsEndpoint extends Endpoint {
+  MetricsEndpoint(super._dio);
+
+  Future<ApiResponse<List<Metric>>> getMetrics() => get(
+        '/api/v1/Metrics',
+        transformer: (e) => (e as List).map(Metric.fromJson).toList(),
+      );
+}
--- Applications/AdminUi/packages/admin_api_types/lib/src/messages/messages.dart
+++ Applications/AdminUi/packages/admin_api_types/lib/src/messages/messages.dart
@@ -1,1 +1,2 @@
-export 'message.dart';
+export 'message_overview.dart';
+export 'message_recipient.dart';
