1532b57e011ac851a35bea896e84c01424c84046
Mika Herrmann <mika.herrmann@js-soft.com>
2024-08-07 13:55:47 +0200

    chore: Add TreeHandler to the moved project

---
+++ Applications/DatabaseMigrator/src/DatabaseMigrator/TreeHandler.cs
@@ -0,0 +1,19 @@
+﻿using Backbone.BuildingBlocks.Infrastructure.Persistence.Database.Attributes;
+
+namespace Backbone.DatabaseMigrator;
+
+public class TreeHandler
+{
+    private readonly List<MigrationInfo> _rawMigrations;
+
+    public TreeHandler(List<MigrationInfo> rawMigrations)
+    {
+        _rawMigrations = rawMigrations;
+        Console.WriteLine("Migrations:");
+        foreach (var info in rawMigrations) Console.WriteLine($"{info.Type}: {info.Name}, {info.IsApplied}, {info.Dependencies.Count} dependencies");
+    }
+}
+
+public record MigrationInfo(ModuleType Type, string Name, bool IsApplied, IList<MigrationDependency> Dependencies);
+
+public record MigrationDependency(ModuleType Type, string Id);
