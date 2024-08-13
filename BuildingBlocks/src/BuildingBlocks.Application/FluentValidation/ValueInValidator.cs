ee8eee975567fdf090bed22b2c1b46b618ca4e83
Daniel Almeida <115644988+daniel-almeida-konkconsulting@users.noreply.github.com>
2024-08-13 14:46:20 +0100

    refactor: add cache for reflection

--- BuildingBlocks/src/BuildingBlocks.Application/FluentValidation/ValueInValidator.cs
+++ BuildingBlocks/src/BuildingBlocks.Application/FluentValidation/ValueInValidator.cs
@@ -1,4 +1,6 @@
 using System.Collections;
+using System.Collections.Concurrent;
+using System.Reflection;
 using Backbone.BuildingBlocks.Application.Abstractions.Exceptions;
 using Backbone.BuildingBlocks.Domain.StronglyTypedIds.Records;
 using FluentValidation;
@@ -60,6 +62,8 @@ public class ValueInValidator<T, TProperty> : PropertyValidator<T, TProperty>
 
 public static class ValidatorExtensions
 {
+    private static readonly ConcurrentDictionary<Type, MethodInfo> IS_VALID_CACHE = new();
+
     public static IRuleBuilderOptions<T, TProperty> In<T, TProperty>(this IRuleBuilder<T, TProperty> ruleBuilder,
         params TProperty[] validOptions)
     {
@@ -70,8 +74,15 @@ public static class ValidatorExtensions
 
     public static IRuleBuilderOptions<T, string?> ValidId<T, TId>(this IRuleBuilder<T, string?> ruleBuilder) where TId : StronglyTypedId
     {
+        var method = IS_VALID_CACHE.FirstOrDefault(x => x.Key == typeof(TId)).Value;
+        if (method == null)
+        {
+            method = typeof(TId).GetMethod("IsValid")!;
+            IS_VALID_CACHE.TryAdd(typeof(TId), method);
+        }
+
         return ruleBuilder
-            .Must(x => (bool)typeof(TId).GetMethod("IsValid")!.Invoke(null, [x])!)
+            .Must(x => (bool)method.Invoke(null, [x])!)
             .WithErrorCode(GenericApplicationErrors.Validation.InvalidPropertyValue().Code)
             .WithMessage("The ID is not valid. Check length, prefix and the used characters.");
     }
