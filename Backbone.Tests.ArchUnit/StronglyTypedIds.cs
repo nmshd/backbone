e3f52c6e529999d4a78d7363dadb812f3f3388ee
Daniel Almeida <115644988+daniel-almeida-konkconsulting@users.noreply.github.com>
2024-08-19 17:13:32 +0100

    test: add arch unit test to ensure isValid method for strongly typed ids

---
+++ Backbone.Tests.ArchUnit/StronglyTypedIds.cs
@@ -0,0 +1,24 @@
+﻿using ArchUnitNET.Domain.Extensions;
+using ArchUnitNET.Fluent.Conditions;
+using ArchUnitNET.xUnit;
+using Backbone.BuildingBlocks.Domain.StronglyTypedIds.Records;
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
+
+namespace Backbone.Backbone.Tests.ArchUnit;
+
+public class StronglyTypedIds
+{
+    [Fact]
+    public void StronglyTypedIdsShouldHaveIsValidMethod()
+    {
+        Classes().That().AreAssignableTo(typeof(StronglyTypedId))
+            .And().AreNot(typeof(StronglyTypedId))
+            .Should().FollowCustomCondition((type) =>
+            {
+                var methods = type.GetMethodMembers();
+
+                return methods.Any(c => c.NameContains("IsValid")) ? new ConditionResult(type, true) : new ConditionResult(type, false, "Entity should have 'IsValid' method");
+            }, "")
+            .Check(Backbone.ARCHITECTURE);
+    }
+}
