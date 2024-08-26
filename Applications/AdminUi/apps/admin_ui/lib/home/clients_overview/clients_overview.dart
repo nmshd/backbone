65b1d65d627df7309fd60d03e63aede62107d039
Vladimir Vuckovic <vidrak97@gmail.com>
2024-08-26 18:03:49 +0200

    fix: remove unnecessary row

--- Applications/AdminUi/apps/admin_ui/lib/home/clients_overview/clients_overview.dart
+++ Applications/AdminUi/apps/admin_ui/lib/home/clients_overview/clients_overview.dart
@@ -48,17 +48,13 @@ class _ClientsOverviewState extends State<ClientsOverview> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   if (kIsDesktop)
-                    Row(
-                      children: [
-                        IconButton(
-                          icon: const Icon(Icons.refresh),
-                          onPressed: () {
-                            _reloadClients();
-                            _loadTiers();
-                          },
-                          tooltip: context.l10n.reload,
-                        ),
-                      ],
+                    IconButton(
+                      icon: const Icon(Icons.refresh),
+                      onPressed: () {
+                        _reloadClients();
+                        _loadTiers();
+                      },
+                      tooltip: context.l10n.reload,
                     ),
                   IconButton(
                     icon: Icon(
