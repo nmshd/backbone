f1c742d2b2dcb23961727f59761c71df8ae25212
Timo Notheisen <timo.notheisen@js-soft.com>
2024-08-27 08:16:19 +0200

    refactor: fix file name and location of FindRelationshipsOfIdentityQuery

--- Modules/Relationships/src/Relationships.Application/Relationships/Queries/FindRelationshipsOfIdentity/FindRelationshipsOfIdentityQuery.cs
+++ Modules/Relationships/src/Relationships.Application/Relationships/Queries/FindRelationshipsOfIdentity/FindRelationshipsOfIdentityQuery.cs
@@ -1,7 +1,7 @@
 using Backbone.DevelopmentKit.Identity.ValueObjects;
 using MediatR;
 
-namespace Backbone.Modules.Relationships.Application.Relationships.Commands.FindRelationshipsOfIdentity;
+namespace Backbone.Modules.Relationships.Application.Relationships.Queries.FindRelationshipsOfIdentity;
 
 public class FindRelationshipsOfIdentityQuery : IRequest<FindRelationshipsOfIdentityResponse>
 {
