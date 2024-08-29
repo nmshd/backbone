0b6a2569b679180dd08dc9b309b160bcaa9f5764
Mika Herrmann <mika.herrmann@js-soft.com>
2024-08-29 14:08:29 +0200

    feat: Use Semaphore (only temporary)

--- Modules/Devices/src/Devices.Application/Identities/Commands/StartDeletionProcessAsOwner/Handler.cs
+++ Modules/Devices/src/Devices.Application/Identities/Commands/StartDeletionProcessAsOwner/Handler.cs
@@ -13,6 +13,7 @@ public class Handler : IRequestHandler<StartDeletionProcessAsOwnerCommand, Start
     private readonly IIdentitiesRepository _identitiesRepository;
     private readonly IUserContext _userContext;
     private readonly IPushNotificationSender _notificationSender;
+    private static readonly SemaphoreSlim SEMAPHORE = new(1);
 
     public Handler(IIdentitiesRepository identitiesRepository, IUserContext userContext, IPushNotificationSender notificationSender)
     {
@@ -23,14 +24,22 @@ public class Handler : IRequestHandler<StartDeletionProcessAsOwnerCommand, Start
 
     public async Task<StartDeletionProcessAsOwnerResponse> Handle(StartDeletionProcessAsOwnerCommand request, CancellationToken cancellationToken)
     {
-        var identity = await _identitiesRepository.FindByAddress(_userContext.GetAddress(), cancellationToken, true) ?? throw new NotFoundException(nameof(Identity));
+        await SEMAPHORE.WaitAsync(cancellationToken);
+        try
+        {
+            var identity = await _identitiesRepository.FindByAddress(_userContext.GetAddress(), cancellationToken, true) ?? throw new NotFoundException(nameof(Identity));
 
-        var deletionProcess = identity.StartDeletionProcessAsOwner(_userContext.GetDeviceId());
+            var deletionProcess = identity.StartDeletionProcessAsOwner(_userContext.GetDeviceId());
 
-        await _identitiesRepository.Update(identity, cancellationToken);
+            await _identitiesRepository.Update(identity, cancellationToken);
 
-        await _notificationSender.SendNotification(identity.Address, new DeletionProcessStartedPushNotification(), cancellationToken);
+            await _notificationSender.SendNotification(identity.Address, new DeletionProcessStartedPushNotification(), cancellationToken);
 
-        return new StartDeletionProcessAsOwnerResponse(deletionProcess);
+            return new StartDeletionProcessAsOwnerResponse(deletionProcess);
+        }
+        finally
+        {
+            SEMAPHORE.Release();
+        }
     }
 }
