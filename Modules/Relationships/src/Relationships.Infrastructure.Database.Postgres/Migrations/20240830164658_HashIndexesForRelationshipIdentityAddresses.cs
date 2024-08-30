a239603abee001e7605c134c4e2774b073d27b2f
Daniel Almeida <115644988+daniel-almeida-konkconsulting@users.noreply.github.com>
2024-08-30 17:53:26 +0100

    feat: use hash indexes for eligible devices, messages and relationships columns with existing btree indexes

---
+++ Modules/Relationships/src/Relationships.Infrastructure.Database.Postgres/Migrations/20240830164658_HashIndexesForRelationshipIdentityAddresses.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backbone.Modules.Relationships.Infrastructure.Database.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class HashIndexesForRelationshipIdentityAddresses : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Relationships_From",
+                schema: "Relationships",
+                table: "Relationships");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Relationships_To",
+                schema: "Relationships",
+                table: "Relationships");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Relationships_From",
+                schema: "Relationships",
+                table: "Relationships",
+                column: "From")
+                .Annotation("Npgsql:IndexMethod", "hash");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Relationships_To",
+                schema: "Relationships",
+                table: "Relationships",
+                column: "To")
+                .Annotation("Npgsql:IndexMethod", "hash");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Relationships_From",
+                schema: "Relationships",
+                table: "Relationships");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Relationships_To",
+                schema: "Relationships",
+                table: "Relationships");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Relationships_From",
+                schema: "Relationships",
+                table: "Relationships",
+                column: "From");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Relationships_To",
+                schema: "Relationships",
+                table: "Relationships",
+                column: "To");
+        }
+    }
+}
