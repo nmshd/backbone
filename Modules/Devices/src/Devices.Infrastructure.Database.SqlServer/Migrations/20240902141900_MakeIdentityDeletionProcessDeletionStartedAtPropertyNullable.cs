2b3e4beced6a8b53f7dd2683da90936842c72d60
Timo Notheisen <timo.notheisen@js-soft.com>
2024-09-02 16:19:41 +0200

    feat: add migrations

---
+++ Modules/Devices/src/Devices.Infrastructure.Database.SqlServer/Migrations/20240902141900_MakeIdentityDeletionProcessDeletionStartedAtPropertyNullable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backbone.Modules.Devices.Infrastructure.Database.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeIdentityDeletionProcessDeletionStartedAtPropertyNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DeletionStartedAt",
+                schema: "Devices",
+                table: "IdentityDeletionProcesses",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DeletionStartedAt",
+                schema: "Devices",
+                table: "IdentityDeletionProcesses",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
