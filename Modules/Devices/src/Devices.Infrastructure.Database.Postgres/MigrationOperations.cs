f9837f144cf859401b707af92d11f0a6d9c5480f
Mika Herrmann <mika.herrmann@js-soft.com>
2024-09-04 12:51:29 +0200

    fix: Use unique filtered index

---
+++ Modules/Devices/src/Devices.Infrastructure.Database.Postgres/MigrationOperations.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Backbone.Modules.Devices.Infrastructure.Database.Postgres;
+
+public static class MigrationOperations
+{
+    private const string INDEX_NAME_ONLY_ONE_ACTIVE_DELETION_PROCESS = "IX_only_one_active_deletion_process";
+
+    public static void AddCheckConstraintForAtMostOneActiveDeletionProcessPerIdentity(this MigrationBuilder migrationBuilder)
+    {
+        AddIndex(migrationBuilder);
+    }
+
+    private static void AddIndex(MigrationBuilder migrationBuilder)
+    {
+        var sqlBuilder = new StringBuilder();
+        sqlBuilder
+            .AppendLine($"CREATE UNIQUE INDEX {INDEX_NAME_ONLY_ONE_ACTIVE_DELETION_PROCESS}")
+            .AppendLine(@"ON ""Devices"".""IdentityDeletionProcesses""(""IdentityAddress"")")
+            .AppendLine(@"WHERE ""Status"" = 1");
+
+        migrationBuilder.Sql(sqlBuilder.ToString());
+    }
+
+    public static void DeleteCheckConstraintForAtMostOneActiveDeletionProcessPerIdentity(this MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql($"DROP Index \"Devices\".{INDEX_NAME_ONLY_ONE_ACTIVE_DELETION_PROCESS}");
+    }
+}
--- Modules/Devices/src/Devices.Infrastructure.Database.Postgres/Migrations/20240701074627_Init.cs
+++ Modules/Devices/src/Devices.Infrastructure.Database.Postgres/Migrations/20240701074627_Init.cs
@@ -562,6 +562,8 @@ namespace Backbone.Modules.Devices.Infrastructure.Database.Postgres.Migrations
                 table: "Tiers",
                 column: "Name",
                 unique: true);
+            
+            migrationBuilder.AddCheckConstraintForAtMostOneActiveDeletionProcessPerIdentity();
         }
 
         /// <inheritdoc />
@@ -634,6 +636,8 @@ namespace Backbone.Modules.Devices.Infrastructure.Database.Postgres.Migrations
             migrationBuilder.DropTable(
                 name: "Tiers",
                 schema: "Devices");
+            
+            migrationBuilder.DeleteCheckConstraintForAtMostOneActiveDeletionProcessPerIdentity();
         }
     }
 }
