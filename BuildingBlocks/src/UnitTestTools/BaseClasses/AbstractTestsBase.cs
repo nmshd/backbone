01607ccd34e2a1b2c238ef2477da78a187f77fd3
Timo Notheisen <timo.notheisen@js-soft.com>
2024-09-04 08:18:06 +0200

    chore: don't implement IAsyncDisposable in AbstractTestsBase

--- BuildingBlocks/src/UnitTestTools/BaseClasses/AbstractTestsBase.cs
+++ BuildingBlocks/src/UnitTestTools/BaseClasses/AbstractTestsBase.cs
@@ -2,7 +2,7 @@ using Backbone.Tooling;
 
 namespace Backbone.UnitTestTools.BaseClasses;
 
-public abstract class AbstractTestsBase : IDisposable, IAsyncDisposable
+public abstract class AbstractTestsBase : IDisposable
 {
     protected DateTime _dateTimeNow;
     protected DateTime _dateTimeTomorrow;
@@ -17,12 +17,6 @@ public abstract class AbstractTestsBase : IDisposable, IAsyncDisposable
 
     public virtual void Dispose()
     {
-        Task.Run(async () => await DisposeAsync()).GetAwaiter().GetResult();
-    }
-
-    public virtual ValueTask DisposeAsync()
-    {
         SystemTime.Reset();
-        return ValueTask.CompletedTask;
     }
 }
