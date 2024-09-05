1117b07e0ecc64bd0de913a19f6c02583864445a
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-09-05 10:46:57 +0200

    chore: remove using directive

--- Modules/Relationships/src/Relationships.ConsumerApi/Controllers/RelationshipsController.cs
+++ Modules/Relationships/src/Relationships.ConsumerApi/Controllers/RelationshipsController.cs
@@ -3,7 +3,6 @@ using Backbone.BuildingBlocks.API.Mvc;
 using Backbone.BuildingBlocks.API.Mvc.ControllerAttributes;
 using Backbone.BuildingBlocks.Application.Abstractions.Exceptions;
 using Backbone.BuildingBlocks.Application.Pagination;
-using Backbone.DevelopmentKit.Identity.ValueObjects;
 using Backbone.Modules.Devices.Application.Identities.Queries.GetIdentity;
 using Backbone.Modules.Devices.Domain.Entities.Identities;
 using Backbone.Modules.Relationships.Application;
