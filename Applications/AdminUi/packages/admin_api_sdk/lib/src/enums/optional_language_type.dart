5f5db2e222838f6e0c801aea85b261fa3c1ec699
Vladimir Vuckovic <vidrak97@gmail.com>
2024-09-09 10:36:36 +0200

    feat: add enums

---
+++ Applications/AdminUi/packages/admin_api_sdk/lib/src/enums/optional_language_type.dart
@@ -0,0 +1,7 @@
+enum OptionalLanguageType {
+  german,
+  portuguese,
+  italian;
+
+  String get name => switch (this) { OptionalLanguageType.german => 'German', OptionalLanguageType.portuguese => 'Portuguese', OptionalLanguageType.italian => 'Italian' };
+}
