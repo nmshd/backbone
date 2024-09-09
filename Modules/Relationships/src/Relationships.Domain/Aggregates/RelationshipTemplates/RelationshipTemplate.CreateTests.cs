b0ad6618f46a6fc02d07ae4f1c8b7e37b53aa9af
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-09-09 09:59:35 +0200

    refactor: improve code structure of tests

--- Modules/Relationships/src/Relationships.Domain/Aggregates/RelationshipTemplates/RelationshipTemplate.CreateTests.cs
+++ Modules/Relationships/src/Relationships.Domain/Aggregates/RelationshipTemplates/RelationshipTemplate.CreateTests.cs
@@ -1,4 +1,5 @@
-﻿using Backbone.Modules.Relationships.Domain.DomainEvents.Outgoing;
+﻿using Backbone.DevelopmentKit.Identity.ValueObjects;
+using Backbone.Modules.Relationships.Domain.DomainEvents.Outgoing;
 using Backbone.UnitTestTools.BaseClasses;
 using Backbone.UnitTestTools.Data;
 using Backbone.UnitTestTools.FluentAssertions.Extensions;
@@ -9,36 +10,20 @@ namespace Backbone.Modules.Relationships.Domain.Aggregates.RelationshipTemplates
 
 public class RelationshipTemplateCreateTests : AbstractTestsBase
 {
-    [Fact]
-    public void Raises_RelationshipTemplateCreatedDomainEvent_when_created_without_optional_parameter()
+    [Theory]
+    [InlineData(null)]
+    [InlineData("did:e:prod.enmeshed.eu:dids:70cf4f3e6edf6bca33d35f")]
+    public void Raises_RelationshipTemplateCreatedDomainEvent(string? forAddress)
     {
         // Arrange
         var address = TestDataGenerator.CreateRandomIdentityAddress();
+        var forIdentity = forAddress == null ? null : IdentityAddress.ParseUnsafe(forAddress);
         var deviceId = TestDataGenerator.CreateRandomDeviceId();
         var expiresAt = DateTime.UtcNow;
         byte[] content = [1, 1, 1, 1, 1, 1, 1, 1];
 
         // Act
-        var template = new RelationshipTemplate(address, deviceId, null, expiresAt, content);
-
-        // Assert
-        var domainEvent = template.Should().HaveASingleDomainEvent<RelationshipTemplateCreatedDomainEvent>();
-        domainEvent.TemplateId.Should().Be(template.Id);
-        domainEvent.CreatedBy.Should().Be(address);
-    }
-
-    [Fact]
-    public void Raises_RelationshipTemplateCreatedDomainEvent_when_created_with_optional_parameter()
-    {
-        // Arrange
-        var address = TestDataGenerator.CreateRandomIdentityAddress();
-        var forAddress = TestDataGenerator.CreateRandomIdentityAddress();
-        var deviceId = TestDataGenerator.CreateRandomDeviceId();
-        var expiresAt = DateTime.UtcNow;
-        byte[] content = [1, 1, 1, 1, 1, 1, 1, 1];
-
-        // Act
-        var template = new RelationshipTemplate(address, deviceId, null, expiresAt, content, forAddress);
+        var template = new RelationshipTemplate(address, deviceId, null, expiresAt, content, forIdentity);
 
         // Assert
         var domainEvent = template.Should().HaveASingleDomainEvent<RelationshipTemplateCreatedDomainEvent>();
