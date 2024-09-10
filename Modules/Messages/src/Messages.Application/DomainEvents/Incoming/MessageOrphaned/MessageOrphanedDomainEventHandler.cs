11048f5d7319cef8cf9f8212f0f578e50f99f716
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-09-10 15:56:55 +0200

    feat: add MessageOrphanedDomainEvent

---
+++ Modules/Messages/src/Messages.Application/DomainEvents/Incoming/MessageOrphaned/MessageOrphanedDomainEventHandler.cs
@@ -0,0 +1,27 @@
+﻿using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
+using Backbone.Modules.Messages.Application.Infrastructure.Persistence.Repository;
+using Backbone.Modules.Messages.Domain.DomainEvents.Incoming;
+using Backbone.Modules.Messages.Domain.Ids;
+using Microsoft.Extensions.Logging;
+
+namespace Backbone.Modules.Messages.Application.DomainEvents.Incoming.MessageOrphaned;
+
+public class MessageOrphanedDomainEventHandler : IDomainEventHandler<MessageOrphanedDomainEvent>
+{
+    private readonly IMessagesRepository _messagesRepository;
+    private readonly ILogger<MessageOrphanedDomainEventHandler> _logger;
+
+    public MessageOrphanedDomainEventHandler(IMessagesRepository messagesRepository, ILogger<MessageOrphanedDomainEventHandler> logger)
+    {
+        _messagesRepository = messagesRepository;
+        _logger = logger;
+    }
+
+    public async Task Handle(MessageOrphanedDomainEvent @event)
+    {
+        var message = await _messagesRepository.Find(MessageId.Parse(@event.MessageId), @event.IdentityAddress, CancellationToken.None);
+
+        await _messagesRepository.Delete(message);
+        _logger.LogTrace("Message with id {Message.Id} deleted because all participants where deleted.", @event.MessageId);
+    }
+}
---
+++ Modules/Messages/src/Messages.Domain/DomainEvents/Outgoing/MessageOrphanedDomainEvent.cs
@@ -0,0 +1,17 @@
+﻿using Backbone.BuildingBlocks.Domain.Events;
+using Backbone.DevelopmentKit.Identity.ValueObjects;
+using Backbone.Modules.Messages.Domain.Entities;
+
+namespace Backbone.Modules.Messages.Domain.DomainEvents.Outgoing;
+
+public class MessageOrphanedDomainEvent : DomainEvent
+{
+    public MessageOrphanedDomainEvent(Message message, IdentityAddress identityAddress) : base($"{message.Id}/Orphaned")
+    {
+        MessageId = message.Id;
+        IdentityAddress = identityAddress;
+    }
+
+    public string MessageId { get; set; }
+    public string IdentityAddress { get; set; }
+}
