b9de447b04423ad47bb416e9c7faeb47898e9bb3
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-09-12 13:38:19 +0200

    chore: remove Domain Events

--- Modules/Messages/src/Messages.Domain/DomainEvents/Incoming/MessageOrphanedDomainEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿using Backbone.BuildingBlocks.Domain.Events;
-
-namespace Backbone.Modules.Messages.Domain.DomainEvents.Incoming;
-
-public class MessageOrphanedDomainEvent : DomainEvent
-{
-    public required string MessageId { get; set; }
-    public required string IdentityAddress { get; set; }
-}
