d1ffcd61f7e63daf5ce376dd50c7f02af4ea3d81
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-09-16 15:29:37 +0200

    fix: make GracePeriodEndsAt property DateTime

--- Modules/Synchronization/src/Synchronization.Domain/DomainEvents/Incoming/PeerToBeDeleted/PeerToBeDeletedDomainEvent.cs
+++ Modules/Synchronization/src/Synchronization.Domain/DomainEvents/Incoming/PeerToBeDeleted/PeerToBeDeletedDomainEvent.cs
@@ -3,7 +3,7 @@
 namespace Backbone.Modules.Synchronization.Domain.DomainEvents.Incoming.PeerToBeDeleted;
 public class PeerToBeDeletedDomainEvent : DomainEvent
 {
-    public PeerToBeDeletedDomainEvent(string peerOfIdentityToBeDeleted, string relationshipId, string identityToBeDeleted, string gracePeriodEndsAt)
+    public PeerToBeDeletedDomainEvent(string peerOfIdentityToBeDeleted, string relationshipId, string identityToBeDeleted, DateTime gracePeriodEndsAt)
     {
         PeerOfIdentityToBeDeleted = peerOfIdentityToBeDeleted;
         RelationshipId = relationshipId;
@@ -14,5 +14,5 @@ public class PeerToBeDeletedDomainEvent : DomainEvent
     public string PeerOfIdentityToBeDeleted { get; }
     public string RelationshipId { get; }
     public string IdentityToBeDeleted { get; }
-    public string GracePeriodEndsAt { get; }
+    public DateTime GracePeriodEndsAt { get; }
 }
