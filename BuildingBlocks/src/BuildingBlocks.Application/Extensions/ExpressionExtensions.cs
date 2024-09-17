db385a02b3d71fc6a8d8614528d5672a2fb68f06
Timo Notheisen <timo.notheisen@js-soft.com>
2024-09-17 15:25:38 +0200

    chore: add ExpressionExtensions `Or` and `And`

---
+++ BuildingBlocks/src/BuildingBlocks.Application/Extensions/ExpressionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace Backbone.BuildingBlocks.Application.Extensions;
+
+public static class ExpressionExtensions
+{
+    public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> expression1, Expression<Func<T, bool>> expression2)
+    {
+        return Expression.Lambda<Func<T, bool>>(Expression.OrElse(expression1, expression2));
+    }
+
+    public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expression1, Expression<Func<T, bool>> expression2)
+    {
+        return Expression.Lambda<Func<T, bool>>(Expression.AndAlso(expression1, expression2));
+    }
+}
