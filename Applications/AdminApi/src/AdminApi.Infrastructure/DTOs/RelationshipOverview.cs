76fae0bb9da988712d761a10bcb7c8ce2e9f2f85
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-09-20 11:59:43 +0200

    fix: add `DeletionProposed` & `ReadyForDeletion` to RelationshipStatus enum

--- Applications/AdminApi/src/AdminApi.Infrastructure/DTOs/RelationshipOverview.cs
+++ Applications/AdminApi/src/AdminApi.Infrastructure/DTOs/RelationshipOverview.cs
@@ -17,5 +17,7 @@ public enum RelationshipStatus
     Active = 20,
     Rejected = 30,
     Revoked = 40,
-    Terminated = 50
+    Terminated = 50,
+    DeletionProposed = 60,
+    ReadyForDeletion = 70
 }
