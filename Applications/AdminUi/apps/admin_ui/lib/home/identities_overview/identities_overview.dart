68cf431efdf705f43ebc5e768f463a6eb35f5b97
Vladimir Vuckovic <vidrak97@gmail.com>
2024-09-23 14:35:18 +0200

    fix: make reload button available in all modes

--- Applications/AdminUi/apps/admin_ui/lib/home/identities_overview/identities_overview.dart
+++ Applications/AdminUi/apps/admin_ui/lib/home/identities_overview/identities_overview.dart
@@ -54,12 +54,11 @@ class _IdentitiesOverviewState extends State<IdentitiesOverview> {
                           ..refreshDatasource();
                       },
                     ),
-                    if (kIsDesktop)
-                      IconButton(
-                        icon: const Icon(Icons.refresh),
-                        onPressed: () async => _dataSource.refreshDatasource(),
-                        tooltip: context.l10n.reload,
-                      ),
+                    IconButton(
+                      icon: const Icon(Icons.refresh),
+                      onPressed: () async => _dataSource.refreshDatasource(),
+                      tooltip: context.l10n.reload,
+                    ),
                     Expanded(child: IdentitiesDataTable(dataSource: _dataSource)),
                   ],
                 ),
