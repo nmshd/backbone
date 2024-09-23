938556c6cb27556d6c2a12db21fd08e0e5574db9
Timo Notheisen <timo.notheisen@js-soft.com>
2024-09-23 07:48:25 +0200

    fix: make MessageOrphanedDomainEvent deserializable

--- Modules/Messages/src/Messages.Domain/DomainEvents/Outgoing/MessageOrphanedDomainEvent.cs
+++ Modules/Messages/src/Messages.Domain/DomainEvents/Outgoing/MessageOrphanedDomainEvent.cs
@@ -5,10 +5,18 @@ namespace Backbone.Modules.Messages.Domain.DomainEvents.Outgoing;
 
 public class MessageOrphanedDomainEvent : DomainEvent
 {
+    /**
+     * This constructor is used by the deserializer.
+     */
+    public MessageOrphanedDomainEvent()
+    {
+        MessageId = null!;
+    }
+
     public MessageOrphanedDomainEvent(Message message) : base($"{message.Id}/MessageOrphaned")
     {
         MessageId = message.Id;
     }
 
-    public string MessageId { get; }
+    public string MessageId { get; set; }
 }
