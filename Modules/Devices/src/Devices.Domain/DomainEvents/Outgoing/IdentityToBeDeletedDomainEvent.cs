ed9753fa63a44bae40e21adf2d1cc5c7d2fb30c8
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-09-23 17:16:51 +0200

    refactor: use value object instead of string

--- Modules/Devices/src/Devices.Domain/DomainEvents/Outgoing/IdentityToBeDeletedDomainEvent.cs
+++ Modules/Devices/src/Devices.Domain/DomainEvents/Outgoing/IdentityToBeDeletedDomainEvent.cs
@@ -1,10 +1,11 @@
 ﻿using Backbone.BuildingBlocks.Domain.Events;
+using Backbone.DevelopmentKit.Identity.ValueObjects;
 
 namespace Backbone.Modules.Devices.Domain.DomainEvents.Outgoing;
 
 public class IdentityToBeDeletedDomainEvent : DomainEvent
 {
-    public IdentityToBeDeletedDomainEvent(string identityAddress, DateTime gracePeriodEndsAt) : base($"{identityAddress}/IdentityToBeDeleted", randomizeId: true)
+    public IdentityToBeDeletedDomainEvent(IdentityAddress identityAddress, DateTime gracePeriodEndsAt) : base($"{identityAddress}/IdentityToBeDeleted", randomizeId: true)
     {
         IdentityAddress = identityAddress;
         GracePeriodEndsAt = gracePeriodEndsAt;
