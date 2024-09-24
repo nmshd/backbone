5386c71979af7d82f94694b1edecf5f706b9cb50
Mika Herrmann <mika.herrmann@js-soft.com>
2024-09-24 16:07:12 +0200

    chore: Rewrite health check to only show healthy or unhealthy

--- BuildingBlocks/src/BuildingBlocks.API/Extensions/HealthCheckWriter.cs
+++ BuildingBlocks/src/BuildingBlocks.API/Extensions/HealthCheckWriter.cs
@@ -1,5 +1,3 @@
-using System.Text;
-using System.Text.Json;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
@@ -9,44 +7,10 @@ public static class HealthCheckWriter
 {
     public static Task WriteResponse(HttpContext context, HealthReport healthReport)
     {
-        context.Response.ContentType = "application/json; charset=utf-8";
-
-        var options = new JsonWriterOptions { Indented = true };
-
-        using var memoryStream = new MemoryStream();
-        using (var jsonWriter = new Utf8JsonWriter(memoryStream, options))
-        {
-            jsonWriter.WriteStartObject();
-            jsonWriter.WriteString("status", healthReport.Status.ToString());
-            jsonWriter.WriteStartObject("results");
-
-            foreach (var healthReportEntry in healthReport.Entries)
-            {
-                jsonWriter.WriteStartObject(healthReportEntry.Key);
-                jsonWriter.WriteString("status",
-                    healthReportEntry.Value.Status.ToString());
-                jsonWriter.WriteString("description",
-                    healthReportEntry.Value.Description);
-                jsonWriter.WriteStartObject("data");
-
-                foreach (var item in healthReportEntry.Value.Data)
-                {
-                    jsonWriter.WritePropertyName(item.Key);
-
-                    JsonSerializer.Serialize(jsonWriter, item.Value,
-                        item.Value.GetType());
-                }
-
-                jsonWriter.WriteEndObject();
-                jsonWriter.WriteEndObject();
-            }
-
-            jsonWriter.WriteEndObject();
-            jsonWriter.WriteEndObject();
-        }
+        context.Response.ContentType = "text/plain; charset=utf-8";
 
         return context.Response.WriteAsync(
-            Encoding.UTF8.GetString(memoryStream.ToArray())
+            healthReport.Status.ToString()
         );
     }
 }
