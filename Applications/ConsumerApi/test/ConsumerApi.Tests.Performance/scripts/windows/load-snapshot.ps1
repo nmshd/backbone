0af6a1e37f161070e8412e6afa2749eacdfa4597
Timo Notheisen <timo.notheisen@js-soft.com>
2024-09-30 10:48:39 +0200

    refactor: simplify load-snapshot and load-postgres scripts

--- Applications/ConsumerApi/test/ConsumerApi.Tests.Performance/scripts/windows/load-snapshot.ps1
+++ Applications/ConsumerApi/test/ConsumerApi.Tests.Performance/scripts/windows/load-snapshot.ps1
@@ -8,21 +8,18 @@ param(
     [string]$DbName = "enmeshed"
 )
 
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
 # Check if the snapshot is provided; if not, prompt the user
 if (-not $SnapshotName) {
-    $SnapshotName = Read-Host "Enter the snapshot name"
+    $SnapshotName = Read-Host "Enter the name of the zip file (without extension). The file should be present in the 'snapshots' folder"
 }
 
-# Get the current script location
-$scriptPath = $PSScriptRoot
-$rootPath = Resolve-Path "$scriptPath\..\.."
-$snapshotsFolder = Join-Path $rootPath "snapshots"
-$snapshotFilePath = Join-Path $snapshotsFolder ($SnapshotName + ".zip")
-$repoRoot = Resolve-Path $scriptPath\..\..\..\..\..\..
+$repoRoot = git rev-parse --show-toplevel
+$snapshotZipPath = "$repoRoot\Applications\ConsumerApi\test\ConsumerApi.Tests.Performance\snapshots\$SnapshotName.zip"
 
 # Check if the file exists
-Write-Host $snapshotFilePath
-if (-not (Test-Path $snapshotFilePath)) {
+if (-not (Test-Path $snapshotZipPath)) {
     Write-Host "Snapshot file '$SnapshotName' not found in the 'snapshots' folder."
     exit 1
 }
@@ -31,15 +28,11 @@ if (-not (Test-Path $snapshotFilePath)) {
 # Extract the zip file
 try {
     Write-Host "Extracting '$SnapshotName'..."
-    Add-Type -AssemblyName System.IO.Compression.FileSystem
-    [System.IO.Compression.ZipFile]::ExtractToDirectory($snapshotFilePath, $snapshotsFolder, 1)
-    Write-Host "Extraction complete. Files are in '$snapshotsFolder'."
-    Move-Item -Path (Join-Path $snapshotsFolder $SnapshotName "enmeshed.pg") -Destination (Join-Path $repoRoot "scripts\dumps\dump-files") -Force
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($snapshotZipPath, "$repoRoot\scripts\windows\dumps\dump-files", 1)
 
 }
 catch {
     Write-Host "An error occurred during extraction: $_"
 }
 
-$loadPostgres = Join-Path $repoRoot "scripts\dumps\load-postgres.ps1"
-& $loadPostgres -Hostname $Hostname -Username $Username -Password $Password -DbName $DbName -Dumpfile enmeshed.pg
+& "$repoRoot\scripts\windows\dumps\load-postgres.ps1" -Hostname $Hostname -Username $Username -Password $Password -DbName $DbName -Dumpfile enmeshed.pg
--- scripts/windows/dumps/load-postgres.ps1
+++ scripts/windows/dumps/load-postgres.ps1
@@ -8,22 +8,16 @@ param (
 )
 
 $ContainerName = "tmp-postgres-container"
-$volumeArg = ($PSScriptRoot + "\dump-files") + ":/tmp/df";
 
-# Run a PostgreSQL container
-Write-Host "Creating container $ContainerName for loading dump onto the database"
-docker container run --name $ContainerName -v $volumeArg -e POSTGRES_PASSWORD="admin" -d postgres
+docker container run --rm --name $ContainerName -v $PSScriptRoot\dump-files:/dump -e POSTGRES_PASSWORD="admin" -d postgres
 
-Write-Host "Creating the Database $DbName"
 docker container exec --env PGPASSWORD=admin -it $containerName dropdb --if-exists -h $Hostname -U $Username $DbName
 docker container exec --env PGPASSWORD=admin -it $containerName psql -h $Hostname -U $Username postgres -c "CREATE DATABASE $DbName WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'en_US.utf8'"
 docker container exec --env PGPASSWORD=admin -it $containerName psql -h $Hostname -U $Username postgres -c "ALTER DATABASE $DbName OWNER TO $Username;"
-docker container exec --env PGPASSWORD=admin -it $containerName psql -h $Hostname -U $Username $DbName -f /tmp/df/$DumpFile
+docker container exec --env PGPASSWORD=admin -it $containerName psql -h $Hostname -U $Username $DbName -f /dump/$DumpFile
 
 if ($LASTEXITCODE -eq 0) {
     Write-Host "Database dump loaded successfully from $DumpFile."
 }
 
-# Stop and remove the container
 docker container stop $ContainerName
-docker container rm $ContainerName
