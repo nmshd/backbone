53a15319e8d65c6e5197d2fd71a1fc0626b5e07b
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-09-30 14:25:41 +0200

    wip

---
+++ Modules/Announcements/src/Announcements.ConsumerApi/Controllers/AnnouncementsController.cs
@@ -0,0 +1,24 @@
+﻿using Backbone.BuildingBlocks.API.Mvc;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backbone.Modules.Announcements.ConsumerApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [Authorize("OpenIddict.Validation.AspNetCore")]
+    public class AnnouncementsController : ApiControllerBase
+    {
+        public AnnouncementsController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> CreateAnnouncement()
+        {
+            return Ok();
+        }
+    }
+}
