456314d4a9c3b24ea20a6bf52a5d9bed55999510
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-10-02 08:59:24 +0200

    refactor: use IsValid() instead of Validate()

--- Modules/Devices/src/Devices.Application/Extensions/ValidatorExtensions.cs
+++ Modules/Devices/src/Devices.Application/Extensions/ValidatorExtensions.cs
@@ -1,25 +1,14 @@
-﻿using System.Collections.Concurrent;
-using System.Reflection;
-using Backbone.BuildingBlocks.Application.Abstractions.Exceptions;
+﻿using Backbone.BuildingBlocks.Application.Abstractions.Exceptions;
 using Backbone.Modules.Devices.Domain.Entities.Identities;
 using FluentValidation;
 
 namespace Backbone.Modules.Devices.Application.Extensions;
 public static class ValidatorExtensions
 {
-    private static readonly ConcurrentDictionary<Type, MethodInfo> IS_VALID_CACHE = new();
-
-    public static IRuleBuilderOptions<T, string?> ValidCommunicationLanguage<T, TLanguage>(this IRuleBuilder<T, string?> ruleBuilder) where TLanguage : CommunicationLanguage
+    public static IRuleBuilderOptions<T, string?> ValidCommunicationLanguage<T>(this IRuleBuilder<T, string?> ruleBuilder)
     {
-        if (!IS_VALID_CACHE.TryGetValue(typeof(TLanguage), out var method))
-        {
-            method = typeof(TLanguage).GetMethod("IsValid", BindingFlags.Public | BindingFlags.Static) ??
-                     throw new Exception($"Type '{typeof(TLanguage)}' does not implement required 'IsValid' method.");
-            IS_VALID_CACHE.TryAdd(typeof(TLanguage), method);
-        }
-
         return ruleBuilder
-            .Must(x => (bool)method.Invoke(null, [x])!)
+            .Must(x => x != null && CommunicationLanguage.IsValid(x))
             .WithErrorCode(GenericApplicationErrors.Validation.InvalidPropertyValue().Code)
             .WithMessage("The device language is not valid. Check length and used characters.");
     }
--- Modules/Devices/src/Devices.Application/Identities/Commands/CreateIdentity/Validator.cs
+++ Modules/Devices/src/Devices.Application/Identities/Commands/CreateIdentity/Validator.cs
@@ -1,7 +1,6 @@
 using Backbone.BuildingBlocks.Application.FluentValidation;
 using Backbone.Modules.Devices.Application.Devices.DTOs.Validators;
 using Backbone.Modules.Devices.Application.Extensions;
-using Backbone.Modules.Devices.Domain.Entities.Identities;
 using FluentValidation;
 
 namespace Backbone.Modules.Devices.Application.Identities.Commands.CreateIdentity;
@@ -14,6 +13,6 @@ public class CreateIdentityCommandValidator : AbstractValidator<CreateIdentityCo
         RuleFor(c => c.IdentityPublicKey).DetailedNotEmpty();
         RuleFor(c => c.DevicePassword).DetailedNotEmpty();
         RuleFor(c => c.SignedChallenge).DetailedNotEmpty().SetValidator(new SignedChallengeDTOValidator());
-        RuleFor(c => c.CommunicationLanguage).DetailedNotEmpty().ValidCommunicationLanguage<CreateIdentityCommand, CommunicationLanguage>();
+        RuleFor(c => c.CommunicationLanguage).DetailedNotEmpty().ValidCommunicationLanguage<CreateIdentityCommand>();
     }
 }
