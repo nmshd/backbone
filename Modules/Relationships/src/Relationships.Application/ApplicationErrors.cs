22bfefc18bbeb21ead0f61e3d9c33391d1377a64
Timo Notheisen <timo.notheisen@js-soft.com>
2024-10-08 10:28:30 +0200

    feat: return code

--- Modules/Relationships/src/Relationships.Application/ApplicationErrors.cs
+++ Modules/Relationships/src/Relationships.Application/ApplicationErrors.cs
@@ -6,10 +6,10 @@ public static class ApplicationErrors
 {
     public static class Relationship
     {
-        public static ApplicationError PeerIsToBeDeleted(string peerToBeDeleted)
+        public static ApplicationError PeerIsToBeDeleted()
         {
-            return new ApplicationError("error.platform.validation.relationship.peerIsToBeDeleted",
-                $"Cannot establish relationship with '{peerToBeDeleted}' because they are in status 'ToBeDeleted'.");
+            return new ApplicationError("error.platform.validation.relationshipRequest.peerIsToBeDeleted",
+                "Cannot establish relationship with the owner of the template because they are in status 'ToBeDeleted'.");
         }
 
         public static ApplicationError RelationshipToTargetAlreadyExists(string targetIdentity = "")
