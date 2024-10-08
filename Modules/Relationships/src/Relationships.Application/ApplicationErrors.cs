ab6a51cba83e4bb64196ffb3a39ebd9648f1cfc4
Timo Notheisen <timo.notheisen@js-soft.com>
2024-10-08 10:29:03 +0200

    chore: remove unused ApplicationErrors

--- Modules/Relationships/src/Relationships.Application/ApplicationErrors.cs
+++ Modules/Relationships/src/Relationships.Application/ApplicationErrors.cs
@@ -11,17 +11,5 @@ public static class ApplicationErrors
             return new ApplicationError("error.platform.validation.relationshipRequest.peerIsToBeDeleted",
                 "Cannot establish relationship with the owner of the template because they are in status 'ToBeDeleted'.");
         }
-
-        public static ApplicationError RelationshipToTargetAlreadyExists(string targetIdentity = "")
-        {
-            var targetIdentityString = string.IsNullOrEmpty(targetIdentity) ? "the target identity" : targetIdentity;
-
-            return new ApplicationError("error.platform.validation.relationshipRequest.relationshipToTargetAlreadyExists", $"A relationship to {targetIdentityString} already exists.");
-        }
-
-        public static ApplicationError CannotSendRelationshipRequestToYourself()
-        {
-            return new ApplicationError("error.platform.validation.relationshipRequest.cannotSendRelationshipRequestToYourself", "The template you provided is your own. You cannot send a relationship request to yourself.");
-        }
     }
 }
