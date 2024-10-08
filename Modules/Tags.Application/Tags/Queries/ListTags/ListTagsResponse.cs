b8edd4133c5de58bb9a0b0c13d079285165fc6c0
Mika Herrmann <mika.herrmann@js-soft.com>
2024-10-08 17:27:21 +0200

    feat: Add Tags controller and basic GET /Tags endpoint

---
+++ Modules/Tags.Application/Tags/Queries/ListTags/ListTagsResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Backbone.Modules.Tags.Application.Tags.Queries.ListTags;
+
+public class ListTagsResponse : Dictionary<string, IEnumerable<TagInfo>>
+{
+    public ListTagsResponse(IReadOnlyDictionary<string, IEnumerable<TagInfo>> tags) : base(tags)
+    {
+    }
+}
---
+++ Modules/Tags.ConsumerApi/Controllers/TagsController.cs
@@ -0,0 +1,21 @@
+﻿using Backbone.BuildingBlocks.API.Mvc;
+using Backbone.Modules.Tags.Application.Tags.Queries.ListTags;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backbone.Modules.Tags.ConsumerApi.Controllers;
+
+[Route("api/v1/[controller]")]
+public class TagsController : ApiControllerBase
+{
+    public TagsController(IMediator mediator) : base(mediator)
+    {
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> ListTags(CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(new ListTagsQuery(), cancellationToken);
+        return Ok(response);
+    }
+}
