7a6301465079aa038e1354a9c4c5804614822b9d
Mika Herrmann <mika.herrmann@js-soft.com>
2024-10-08 12:54:41 +0200

    refactor: Rewrite and simplify Google Cloud Health Check

--- BuildingBlocks/src/BuildingBlocks.Infrastructure/Persistence/BlobStorage/GoogleCloudStorage/GoogleCloudStorageHealthCheck.cs
+++ BuildingBlocks/src/BuildingBlocks.Infrastructure/Persistence/BlobStorage/GoogleCloudStorage/GoogleCloudStorageHealthCheck.cs
@@ -7,45 +7,71 @@ namespace Backbone.BuildingBlocks.Infrastructure.Persistence.BlobStorage.GoogleC
 
 public class GoogleCloudStorageHealthCheck : IHealthCheck
 {
+    private const string FILE_TEXT = "healthcheck";
+    private static bool? _isHealthy;
+
+    private readonly StorageClient _storage;
+    private readonly GoogleCloudStorageOptions _options;
+
     public GoogleCloudStorageHealthCheck(StorageClient storage, GoogleCloudStorageOptions options)
     {
         _storage = storage;
         _options = options;
     }
 
-    private readonly StorageClient _storage;
-    private readonly GoogleCloudStorageOptions _options;
-
-    private static bool? _isHealthy;
-    private const string FILE_TEXT = "healthcheck";
-
     public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
     {
         if (!_isHealthy.HasValue)
         {
-            try
-            {
-                //Upload random Blob with predefined text
-                var filename = Guid.NewGuid().ToString();
-                await _storage.UploadObjectAsync(_options.BucketName, filename, "text/plain", new MemoryStream(FILE_TEXT.GetBytes()), null, cancellationToken);
+            var filename = Guid.NewGuid().ToString();
+            var isUploadPossible = await IsUploadPossible(filename, cancellationToken);
+            var isDownloadPossible = await IsDownloadPossible(filename, cancellationToken);
+            var isDeletionPossible = await IsDeletionPossible(filename, cancellationToken);
 
-                //Download the Blob again and check the text
-                var downloadStream = new MemoryStream();
-                await _storage.DownloadObjectAsync(_options.BucketName, filename, downloadStream, null, cancellationToken);
-                var downloadedString = Encoding.UTF8.GetString(downloadStream.ToArray());
-                if (downloadedString != FILE_TEXT) _isHealthy = false;
-
-                //Delete the Blob
-                await _storage.DeleteObjectAsync(_options.BucketName, filename, null, cancellationToken);
-
-                _isHealthy = true;
-            }
-            catch (Exception)
-            {
-                _isHealthy = false;
-            }
+            _isHealthy = isUploadPossible && isDownloadPossible && isDeletionPossible;
         }
 
         return _isHealthy.Value ? HealthCheckResult.Healthy() : HealthCheckResult.Unhealthy();
+    }
+
+    private async Task<bool> IsUploadPossible(string filename, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            await _storage.UploadObjectAsync(_options.BucketName, filename, "text/plain", new MemoryStream(FILE_TEXT.GetBytes()), null, cancellationToken);
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    private async Task<bool> IsDownloadPossible(string filename, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var downloadStream = new MemoryStream();
+            await _storage.DownloadObjectAsync(_options.BucketName, filename, downloadStream, null, cancellationToken);
+            var downloadedString = Encoding.UTF8.GetString(downloadStream.ToArray());
+            return downloadedString == FILE_TEXT;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    private async Task<bool> IsDeletionPossible(string filename, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            await _storage.DeleteObjectAsync(_options.BucketName, filename, null, cancellationToken);
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
     }
 }
