fec126ab735bf9d830dc759e06ad15391177b24b
Mika Herrmann <mika.herrmann@js-soft.com>
2024-10-09 13:21:55 +0200

    chore: Add custom Json converter for the List Tags response to prevent camel case

--- Modules/Tags.Application/Tags/Queries/ListTags/ListTagsResponse.cs
+++ Modules/Tags.Application/Tags/Queries/ListTags/ListTagsResponse.cs
@@ -1,8 +1,40 @@
-﻿namespace Backbone.Modules.Tags.Application.Tags.Queries.ListTags;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
 
+namespace Backbone.Modules.Tags.Application.Tags.Queries.ListTags;
+
+[JsonConverter(typeof(ListTagsResponseConverter))]
 public class ListTagsResponse : Dictionary<string, IEnumerable<TagInfo>>
 {
     public ListTagsResponse(IReadOnlyDictionary<string, IEnumerable<TagInfo>> tags) : base(tags)
     {
+    }
+}
+
+public class ListTagsResponseConverter : JsonConverter<ListTagsResponse>
+{
+    public override ListTagsResponse? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return null;
+    }
+
+    public override void Write(Utf8JsonWriter writer, ListTagsResponse value, JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+        foreach (var (name, tags) in value)
+        {
+            writer.WriteStartArray(name);
+            foreach (var tag in tags)
+            {
+                writer.WriteStartObject();
+                writer.WriteString("tag", tag.Tag);
+                if (tag.Title != null) writer.WriteString("title", tag.Title);
+                writer.WriteEndObject();
+            }
+
+            writer.WriteEndArray();
+        }
+
+        writer.WriteEndObject();
     }
 }
