6809747e45706642138ba162735e6377bbf78ed6
Nikola Vetnić <nikola.vojislav-vetnic@js-soft.rs>
2024-10-10 23:08:04 +0200

    feat: add password with required length in a single migration

---
+++ Modules/Relationships/src/Relationships.Infrastructure.Database.Postgres/Migrations/20241010204941_AddPasswordProtectionToRelationshipTemplates.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backbone.Modules.Relationships.Infrastructure.Database.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasswordProtectionToRelationshipTemplates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Password",
+                schema: "Relationships",
+                table: "RelationshipTemplates",
+                type: "bytea",
+                maxLength: 200,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                schema: "Relationships",
+                table: "RelationshipTemplates");
+        }
+    }
+}
