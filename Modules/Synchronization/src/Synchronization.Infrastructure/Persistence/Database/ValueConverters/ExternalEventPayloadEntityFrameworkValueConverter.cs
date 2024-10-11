c9fc25acafcd83636ad3082248caf579155cddc4
Timo Notheisen <timo.notheisen@js-soft.com>
2024-10-11 16:43:41 +0200

    refactor: fix typo in ExternalEventIdPayloadEntityFrameworkValueConverter

--- Modules/Synchronization/src/Synchronization.Infrastructure/Persistence/Database/ValueConverters/ExternalEventPayloadEntityFrameworkValueConverter.cs
+++ Modules/Synchronization/src/Synchronization.Infrastructure/Persistence/Database/ValueConverters/ExternalEventPayloadEntityFrameworkValueConverter.cs
@@ -3,9 +3,9 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Newtonsoft.Json;
 
 namespace Backbone.Modules.Synchronization.Infrastructure.Persistence.Database.ValueConverters;
-public class ExternalEventIdPayloadEntityFrameworkValueConverter : ValueConverter<object, string>
+public class ExternalEventPayloadEntityFrameworkValueConverter : ValueConverter<object, string>
 {
-    public ExternalEventIdPayloadEntityFrameworkValueConverter() : base(
+    public ExternalEventPayloadEntityFrameworkValueConverter() : base(
         o => JsonConvert.SerializeObject(o),
         s => JsonConvert.DeserializeObject<ExpandoObject>(s)!)
     { }
