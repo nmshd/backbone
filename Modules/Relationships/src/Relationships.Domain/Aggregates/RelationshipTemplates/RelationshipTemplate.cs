d9e726a68c2e5feddb7e165f26968df6b10c3f3e
Timo Notheisen <timo.notheisen@js-soft.com>
2024-10-14 16:29:25 +0200

    fix: in CanBeCollectedUsingPassword, return true if the template is already allocated by the active identity

--- Modules/Relationships/src/Relationships.Domain/Aggregates/RelationshipTemplates/RelationshipTemplate.cs
+++ Modules/Relationships/src/Relationships.Domain/Aggregates/RelationshipTemplates/RelationshipTemplate.cs
@@ -75,7 +75,10 @@ public class RelationshipTemplate : Entity
 
     public bool CanBeCollectedUsingPassword(IdentityAddress address, byte[]? password)
     {
-        return Password == null || password != null && Password.SequenceEqual(password) || CreatedBy == address;
+        return Password == null ||
+               password != null && Password.SequenceEqual(password) ||
+               CreatedBy == activeIdentity || // The owner shouldn't need a password to get the template
+               Allocations.Any(a => a.AllocatedBy == activeIdentity); // if the template has already been allocated by the active identity, it doesn't need to pass the password again 
     }
 
     #region Expressions
