b81d9f2f29b8e92fe4a41ac959eaa29f0cd10c6f
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-10-15 08:23:45 +0200

    feat: add new project feat: add new class

---
+++ Modules/Announcements/src/Announcements.Domain/Entities/Announcement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backbone.Modules.Announcements.Domain.Entities
+{
+    public class Announcement
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ExpiresAt { get; set; }
+        public AnnouncementSeverity Severity { get; set; }
+    }
+}
+
+public enum AnnouncementSeverity
+{
+    Low,
+    Medium,
+    High
+}
