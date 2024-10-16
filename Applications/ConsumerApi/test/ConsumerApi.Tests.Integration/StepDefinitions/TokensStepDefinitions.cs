ddcf3e181014015f2fda32a31f8b25e762071ebc
Nikola Vetnić <62119280+NikolaVetnic@users.noreply.github.com>
2024-10-16 07:34:20 +0200

    Consumer API: Password-protected Tokens (#909)
    * feat: implement password protected tokens
    
    * chore: remove unused methods
    
    * chore: remove empty line
    
    * test: add tests for anonymous user fetching tokens
    
    * fix: add check for existing allocations to relationship template CanBeCollectedWithPassword expression
    
    * chore: add explaining comments to password checks in Token.cs
    
    * refactor: don't use queryable extensions; instead use expressions in entity
    
    * refactor: remove redundant "when not null" check from validator
    
    * refactor: rename things
    
    * fix: re-add check for "password is not null" to validators
    
    * test: add can be collected with password tests
    
    * test: check for status code in get multiple templates feature file
    
    * test: check for status code in get multiple tokens feature file
    
    * fix: don't throw ArgumentNullException in ReplaceExpressionVisitor
    
    * test: fix naming
    
    * fix: use correct error message in validator tests
    
    * fix: use correct error message in CreateToken validator tests
    
    * test: add tests for anonymous user
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: Timo Notheisen <timo.notheisen@js-soft.com>

--- Applications/ConsumerApi/test/ConsumerApi.Tests.Integration/StepDefinitions/TokensStepDefinitions.cs
+++ Applications/ConsumerApi/test/ConsumerApi.Tests.Integration/StepDefinitions/TokensStepDefinitions.cs
@@ -1,9 +1,11 @@
-﻿using Backbone.BuildingBlocks.SDK.Endpoints.Common.Types;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Backbone.BuildingBlocks.SDK.Endpoints.Common.Types;
 using Backbone.ConsumerApi.Sdk.Endpoints.Tokens.Types.Requests;
 using Backbone.ConsumerApi.Sdk.Endpoints.Tokens.Types.Responses;
 using Backbone.ConsumerApi.Tests.Integration.Contexts;
-using Backbone.ConsumerApi.Tests.Integration.Extensions;
 using Backbone.ConsumerApi.Tests.Integration.Helpers;
+using TechTalk.SpecFlow.Assist;
 
 namespace Backbone.ConsumerApi.Tests.Integration.StepDefinitions;
 
@@ -31,46 +33,40 @@ internal class TokensStepDefinitions
 
     #region Given
 
-    [Given($"Tokens? {RegexFor.LIST_OF_THINGS} belonging to {RegexFor.SINGLE_THING}")]
-    public async Task GivenTheIdentityCreatedMultipleTokens(string tokenNames, string identityName)
-    {
-        foreach (var tokenName in Utils.SplitNames(tokenNames))
-        {
-            var client = _clientPool.FirstForIdentityName(identityName);
-
-            var response = await client.Tokens.CreateToken(new CreateTokenRequest { Content = TestData.SOME_BYTES, ExpiresAt = TOMORROW });
-            response.Should().BeASuccess();
-
-            _tokensContext.CreateTokenResponses[tokenName] = response.Result!;
-        }
-    }
-
-    [Given($"Token {RegexFor.SINGLE_THING} belonging to {RegexFor.SINGLE_THING} where ForIdentity is the address of {RegexFor.SINGLE_THING}")]
-    public async Task GivenTokenTBelongingToIWhereForIdentityIsTheAddressOfI(string tokenName, string identityName, string forIdentityName)
+    [Given($@"Token {RegexFor.SINGLE_THING} created by {RegexFor.SINGLE_THING} with password ""([^""]*)"" and forIdentity {RegexFor.OPTIONAL_SINGLE_THING}")]
+    public async Task GivenRelationshipTemplateCreatedByTokenOwnerWithPasswordAndForIdentity(string relationshipTemplateName, string identityName, string passwordString, string forIdentityName)
     {
         var client = _clientPool.FirstForIdentityName(identityName);
-        var forClient = _clientPool.FirstForIdentityName(forIdentityName);
+        var forClient = forIdentityName != "-" ? _clientPool.FirstForIdentityName(forIdentityName).IdentityData!.Address : null;
+        var password = passwordString.Trim() != "-" ? Convert.FromBase64String(passwordString.Trim()) : null;
 
-        var response = await client.Tokens.CreateToken(new CreateTokenRequest { Content = TestData.SOME_BYTES, ExpiresAt = TOMORROW, ForIdentity = forClient.IdentityData!.Address });
-        response.Should().BeASuccess();
+        var response = await client.Tokens.CreateToken(
+            new CreateTokenRequest { Content = TestData.SOME_BYTES, ExpiresAt = TOMORROW, ForIdentity = forClient, Password = password });
 
-        _tokensContext.CreateTokenResponses[tokenName] = response.Result!;
+        _tokensContext.CreateTokenResponses[relationshipTemplateName] = response.Result!;
+    }
+
+    [Given(@"the following Tokens")]
+    public async Task GivenTheFollowingTokens(Table table)
+    {
+        var tokenPropertiesSet = table.CreateSet<TokenProperties>();
+
+        foreach (var tokenProperties in tokenPropertiesSet)
+        {
+            var client = _clientPool.FirstForIdentityName(tokenProperties.TokenOwner);
+            var forClient = tokenProperties.ForIdentity != "-" ? _clientPool.FirstForIdentityName(tokenProperties.ForIdentity).IdentityData!.Address : null;
+            var password = tokenProperties.Password.Trim() != "-" ? Convert.FromBase64String(tokenProperties.Password.Trim()) : null;
+
+            var response = await client.Tokens
+                .CreateToken(new CreateTokenRequest { Content = TestData.SOME_BYTES, ExpiresAt = TOMORROW, ForIdentity = forClient, Password = password });
+
+            _tokensContext.CreateTokenResponses[tokenProperties.TokenName] = response.Result!;
+        }
     }
 
     #endregion
 
     #region When
-
-    [When($"{RegexFor.SINGLE_THING} sends a GET request to the /Tokens endpoint with the ids of {RegexFor.LIST_OF_THINGS}")]
-    public async Task WhenIdentitySendsAGetRequestToTheTokensEndpointWithAListOfIdsOfOwnTokens(string identityName, string tokenNames)
-    {
-        var client = _clientPool.FirstForIdentityName(identityName);
-
-        var tokenIds = Utils.SplitNames(tokenNames).Select(tokenName => _tokensContext.CreateTokenResponses[tokenName].Id).ToArray();
-
-        _responseContext.WhenResponse = _listTokensResponse = await client.Tokens.ListTokens(tokenIds);
-        _responseContext.WhenResponse.Should().NotBeNull();
-    }
 
     [When($"{RegexFor.SINGLE_THING} sends a POST request to the /Tokens endpoint")]
     public async Task WhenIdentitySendsAPostRequestToTheTokensEndpoint(string identityName)
@@ -85,50 +81,92 @@ internal class TokensStepDefinitions
         _responseContext.WhenResponse = await _clientPool.Anonymous.Tokens.CreateTokenUnauthenticated(new CreateTokenRequest { Content = TestData.SOME_BYTES, ExpiresAt = TOMORROW });
     }
 
-    [When($"{RegexFor.SINGLE_THING} sends a GET request to the /Tokens/{{id}} endpoint with {RegexFor.SINGLE_THING}.Id")]
-    public async Task WhenIdentitySendsAGetRequestToTheTokensIdEndpointWithTokenId(string identityName, string tokenName)
+    [When($@"{RegexFor.SINGLE_THING} sends a POST request to the /Tokens endpoint with the password ""([^""]*)""")]
+    public async Task WhenISendsAPOSTRequestToTheTokensEndpointWithThePassword(string identityName, string passwordString)
     {
         var client = _clientPool.FirstForIdentityName(identityName);
-        var tokenId = _tokensContext.CreateTokenResponses[tokenName].Id;
+        var password = Encoding.UTF8.GetBytes(passwordString);
 
-        _responseContext.WhenResponse = await client.Tokens.GetToken(tokenId);
+        _responseContext.WhenResponse = await client.Tokens.CreateToken(
+            new CreateTokenRequest { Content = TestData.SOME_BYTES, ExpiresAt = TOMORROW, Password = password });
     }
 
-    [When($"an anonymous user sends a GET request to the /Tokens/{{id}} endpoint with {RegexFor.SINGLE_THING}.Id")]
-    public async Task WhenAnAnonymousUserSendsAGetRequestToTheTokensIdEndpointWithTokenId(string tokenName)
-    {
-        var tokenId = _tokensContext.CreateTokenResponses[tokenName].Id;
-        _responseContext.WhenResponse = await _clientPool.Anonymous.Tokens.GetTokenUnauthenticated(tokenId);
-    }
-
-    [When($"{RegexFor.SINGLE_THING} sends a GET request to the /Tokens/{{id}} endpoint with \"([^\"]*)\"")]
-    public async Task WhenIdentitySendsAGetRequestToTheTokensIdEndpointWithNonExistingTokenId(string identityName, string nonExistingTokenId)
+    [When($@"{RegexFor.SINGLE_THING} sends a POST request to the /Tokens endpoint with password ""([^""]*)"" and forIdentity {RegexFor.OPTIONAL_SINGLE_THING}")]
+    public async Task WhenISendsAPOSTRequestToTheTokensEndpointWithPasswordAndForIdentityI(string identityName, string passwordString, string forIdentityName)
     {
         var client = _clientPool.FirstForIdentityName(identityName);
-        _responseContext.WhenResponse = await client.Tokens.GetToken(nonExistingTokenId);
+        var forClient = _clientPool.FirstForIdentityName(forIdentityName);
+        var password = Encoding.UTF8.GetBytes(passwordString);
+
+        _responseContext.WhenResponse = await client.Tokens.CreateToken(
+            new CreateTokenRequest { Content = TestData.SOME_BYTES, ExpiresAt = TOMORROW, ForIdentity = forClient.IdentityData!.Address, Password = password });
+    }
+
+    [When($@"{RegexFor.OPTIONAL_SINGLE_THING} sends a GET request to the /Tokens/{RegexFor.SINGLE_THING}.Id endpoint with password ""([^""]*)""")]
+    public async Task WhenIdentitySendsAGetRequestToTheTokensIdEndpointWithPassword(string identityName, string tokenName, string password)
+    {
+        var isAuthenticated = identityName != "-";
+        var isPasswordProvided = password != "-";
+
+        var client = isAuthenticated ? _clientPool.FirstForIdentityName(identityName) : _clientPool.Anonymous;
+        var tokenId = _tokensContext.CreateTokenResponses[tokenName].Id;
+
+        if (isAuthenticated)
+            _responseContext.WhenResponse = isPasswordProvided
+                ? await client.Tokens.GetToken(tokenId, Convert.FromBase64String(password.Trim()))
+                : await client.Tokens.GetToken(tokenId);
+        else
+            _responseContext.WhenResponse = isPasswordProvided
+                ? await client.Tokens.GetTokenUnauthenticated(tokenId, Convert.FromBase64String(password.Trim()))
+                : await client.Tokens.GetTokenUnauthenticated(tokenId);
+    }
+
+    [When($@"{RegexFor.SINGLE_THING} sends a GET request to the /Tokens endpoint with the following payloads")]
+    public async Task WhenISendsAGETRequestToTheTokensEndpointWithTheFollowingPayloads(string identityName, Table table)
+    {
+        var client = _clientPool.FirstForIdentityName(identityName);
+
+        var getRequestPayloadSet = table.CreateSet<GetRequestPayload>();
+
+        var queryItems = getRequestPayloadSet.Select(payload =>
+        {
+            var tokenId = _tokensContext.CreateTokenResponses[payload.TokenName].Id;
+            var password = payload.PasswordOnGet == "-" ? null : Convert.FromBase64String(payload.PasswordOnGet.Trim());
+
+            return new ListTokensQueryItem() { Id = tokenId, Password = password };
+        }).ToList();
+
+        _responseContext.WhenResponse = _listTokensResponse = await client.Tokens.ListTokens(queryItems);
     }
 
     #endregion
 
     #region Then
 
-    [Then($"the response contains the Tokens? {RegexFor.LIST_OF_THINGS}")]
+    [Then($@"the response contains Token\(s\) {RegexFor.LIST_OF_THINGS}")]
     public void ThenTheResponseContainsTokens(string tokenNames)
     {
-        foreach (var tokenId in Utils.SplitNames(tokenNames).Select(tokenName => _tokensContext.CreateTokenResponses[tokenName].Id))
-        {
-            _listTokensResponse!.Result.Should().Contain(token => token.Id == tokenId);
-        }
+        var tokens = tokenNames.Split(',').Select(item => _tokensContext.CreateTokenResponses[item.Trim()]).ToList();
+        _listTokensResponse!.Result!.Should().BeEquivalentTo(tokens, options => options.WithStrictOrdering());
     }
-
-    [Then($"the response does not contain the Token {RegexFor.SINGLE_THING}")]
-    public void ThenTheResponseDoesNotContainTheTokenT(string tokenName)
-    {
-        var tokenId = _tokensContext.CreateTokenResponses[tokenName].Id;
-
-        _listTokensResponse!.Result.Should().NotContain(token => token.Id == tokenId);
-    }
-
 
     #endregion
+}
+
+// ReSharper disable once ClassNeverInstantiated.Local
+[SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Local")]
+file class TokenProperties
+{
+    public required string TokenName { get; set; }
+    public required string TokenOwner { get; set; }
+    public required string ForIdentity { get; set; }
+    public required string Password { get; set; }
+}
+
+// ReSharper disable once ClassNeverInstantiated.Local
+[SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Local")]
+file class GetRequestPayload
+{
+    public required string TokenName { get; set; }
+    public required string PasswordOnGet { get; set; }
 }
