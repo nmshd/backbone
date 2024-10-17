ffac3276060a975ade2a7f9a38720f01181aa0ce
Timo Notheisen <timo.notheisen@js-soft.com>
2024-10-17 16:26:31 +0200

    test: add tests for RelationshipReactivationCompletedDomainEventHandler

--- Modules/Synchronization/test/Synchronization.Application.Tests/Tests/DomainEvents/RelationshipReactivationCompletedDomainEventHandlerTests.cs
+++ Modules/Synchronization/test/Synchronization.Application.Tests/Tests/DomainEvents/RelationshipReactivationCompletedDomainEventHandlerTests.cs
@@ -3,10 +3,8 @@ using Backbone.Modules.Synchronization.Application.Infrastructure;
 using Backbone.Modules.Synchronization.Domain.DomainEvents.Incoming.RelationshipReactivationCompleted;
 using Backbone.Modules.Synchronization.Domain.Entities.Relationships;
 using Backbone.Modules.Synchronization.Domain.Entities.Sync;
-using Backbone.UnitTestTools.BaseClasses;
 using FakeItEasy;
 using Microsoft.Extensions.Logging;
-using Xunit;
 
 namespace Backbone.Modules.Synchronization.Application.Tests.Tests.DomainEvents;
 
@@ -16,22 +14,60 @@ public class RelationshipReactivationCompletedDomainEventHandlerTests : Abstract
     public async Task Creates_an_external_event()
     {
         // Arrange
-        var relationshipReactivationCompletedIntegrationEvent = new RelationshipReactivationCompletedDomainEvent
-        {
-            NewRelationshipStatus = "Terminated",
-            RelationshipId = RelationshipId.New(),
-            Peer = TestDataGenerator.CreateRandomIdentityAddress()
-        };
+        var relationshipReactivationCompletedIntegrationEvent = CreateReactivationCompletedDomainEventForRelationship(RelationshipId.New());
 
         var mockDbContext = A.Fake<ISynchronizationDbContext>();
 
-        var handler = new RelationshipReactivationCompletedDomainEventHandler(mockDbContext,
-            A.Fake<ILogger<RelationshipReactivationCompletedDomainEventHandler>>());
+        var handler = CreateHandler(mockDbContext);
 
         // Act
         await handler.Handle(relationshipReactivationCompletedIntegrationEvent);
 
         // Assert
         A.CallTo(() => mockDbContext.CreateExternalEvent(A<RelationshipReactivationCompletedExternalEvent>._)).MustHaveHappenedOnceExactly();
+    }
+
+    [Fact]
+    public async Task Unblocks_MessageReceivedExternalEvents()
+    {
+        // Arrange
+        var idOfReactivatedRelationship = RelationshipId.New();
+
+        var relationshipReactivationCompletedIntegrationEvent = CreateReactivationCompletedDomainEventForRelationship(idOfReactivatedRelationship);
+
+        var mockDbContext = A.Fake<ISynchronizationDbContext>();
+
+        var messageReceivedExternalEvent =
+            new MessageReceivedExternalEvent(CreateRandomIdentityAddress(), new MessageReceivedExternalEvent.EventPayload { Id = "MSG11111111111111111" }, idOfReactivatedRelationship);
+        messageReceivedExternalEvent.BlockDelivery();
+
+        A.CallTo(() => mockDbContext.GetBlockedExternalEventsWithTypeAndContext(ExternalEventType.MessageReceived, A<string>._, A<CancellationToken>._))
+            .Returns([messageReceivedExternalEvent]);
+
+        var handler = CreateHandler(mockDbContext);
+
+        // Act
+        await handler.Handle(relationshipReactivationCompletedIntegrationEvent);
+
+        // Assert
+        messageReceivedExternalEvent.IsDeliveryBlocked.Should().BeFalse();
+        A.CallTo(() => mockDbContext.SaveChangesAsync(A<CancellationToken>._)).MustHaveHappenedOnceExactly();
+    }
+
+    private static RelationshipReactivationCompletedDomainEvent CreateReactivationCompletedDomainEventForRelationship(RelationshipId idOfReactivatedRelationship)
+    {
+        return new RelationshipReactivationCompletedDomainEvent
+        {
+            NewRelationshipStatus = "Active",
+            RelationshipId = idOfReactivatedRelationship,
+            Peer = TestDataGenerator.CreateRandomIdentityAddress()
+        };
+    }
+
+    private static RelationshipReactivationCompletedDomainEventHandler CreateHandler(ISynchronizationDbContext dbContext)
+    {
+        var logger = A.Dummy<ILogger<RelationshipReactivationCompletedDomainEventHandler>>();
+
+        return new RelationshipReactivationCompletedDomainEventHandler(dbContext, logger);
     }
 }
