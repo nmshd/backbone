ab6fde5112b98e3b269244ac3fc3035ffeaab969
Timo Notheisen <timo.notheisen@js-soft.com>
2024-10-17 13:26:39 +0200

    chore: fix formatting

--- Modules/Messages/test/Messages.Domain.Tests/ImplicitUsings.cs
+++ Modules/Messages/test/Messages.Domain.Tests/ImplicitUsings.cs
@@ -1,3 +1,3 @@
-﻿global using static Backbone.UnitTestTools.Data.TestDataGenerator;
+﻿global using FluentAssertions;
 global using Xunit;
-global using FluentAssertions;
+global using static Backbone.UnitTestTools.Data.TestDataGenerator;
