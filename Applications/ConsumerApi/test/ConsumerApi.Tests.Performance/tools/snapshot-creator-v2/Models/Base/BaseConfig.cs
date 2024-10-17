b72d9eb4cb2e286e11a62b741e34e9ea7412102c
Eric Brunner <erich.brunner@live.at>
2024-10-17 14:19:10 +0200

    test: added Models and test DeserializeFromJson

---
+++ Applications/ConsumerApi/test/ConsumerApi.Tests.Performance/tools/snapshot-creator-v2/Models/Base/BaseConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Backbone.ConsumerApi.Tests.Performance.SnapshotCreator.V2.Models.Base;
+
+public record BaseConfig
+{
+    [JsonPropertyName(nameof(TotalNumberOfSentMessages))]
+    public int TotalNumberOfSentMessages { get; set; }
+
+    [JsonPropertyName(nameof(TotalNumberOfReceivedMessages))]
+    public int TotalNumberOfReceivedMessages { get; set; }
+
+    [JsonPropertyName(nameof(NumberOfReceivedMessagesAddOn))]
+    public int NumberOfReceivedMessagesAddOn { get; set; }
+}
---
+++ Applications/ConsumerApi/test/ConsumerApi.Tests.Performance/tools/snapshot-creator-v2/Models/ConnectorConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+using Backbone.ConsumerApi.Tests.Performance.SnapshotCreator.V2.Models.Base;
+
+namespace Backbone.ConsumerApi.Tests.Performance.SnapshotCreator.V2.Models;
+
+public record ConnectorConfig : BaseConfig
+{
+    [JsonPropertyName(nameof(TotalNumberOfAvailableRelationships))]
+    public int TotalNumberOfAvailableRelationships { get; set; }
+}
