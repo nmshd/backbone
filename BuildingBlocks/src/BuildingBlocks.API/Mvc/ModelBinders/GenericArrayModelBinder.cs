36343c0ccad1088435da52ec2c13e13c528eb677
Timo Notheisen <65653426+tnotheis@users.noreply.github.com>
2024-10-17 13:49:51 +0200

    Change deserialization of RelationshipTemplate query to match standard (#914)
    * fix: enable deserialization of standard format
    
    * fix: use correct address in error message of "relationshipToRecipientNotActive"
    
    * refactor: clean up of GenericArrayModelBinder
    
    * chore: add docs for GenericArrayModelBinder
    
    * Revert "fix: use correct address in error message of "relationshipToRecipientNotActive""
    
    This reverts commit 87ee792cbc0e5af2413e555a5ad33549fa6c740c.
    
    * chore: format comment
    
    * chore: add deprecation comment to RelationshipTemplatesController.GetAll method
    
    * chore: fix formatting
    
    * fix: use the new serialization in the SDK
    
    * fix: check for length of templates parameter for fallback to ids

---
+++ BuildingBlocks/src/BuildingBlocks.API/Mvc/ModelBinders/GenericArrayModelBinder.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using Org.BouncyCastle.Utilities.Encoders;
+
+namespace Backbone.BuildingBlocks.API.Mvc.ModelBinders;
+
+/**
+ * <summary>
+ * This model binder allows for binding arrays of complex types from query parameters. The query parameters should be formatted as follows:
+ * <code>
+ *   paramName.0.propertyName1=value&amp;paramName.0.propertyName2=value&amp;paramName.1.propertyName1=value&amp;paramName.1.propertyName2=value
+ * </code>
+ * </summary>
+ */
+public class GenericArrayModelBinder : IModelBinder
+{
+    public Task BindModelAsync(ModelBindingContext bindingContext)
+    {
+        var elementType = bindingContext.ModelType.GetElementType()!;
+        var templates = (IList)Activator.CreateInstance(typeof(List<>).MakeGenericType(elementType))!;
+        var query = bindingContext.HttpContext.Request.Query;
+
+        for (var i = 0; ; i++)
+        {
+            var instance = Activator.CreateInstance(elementType);
+            var properties = elementType.GetProperties();
+
+            var queryValueFound = false;
+            foreach (var property in properties)
+            {
+                var key = $"templates.{i}.{property.Name.ToLower()}";
+
+                if (!query.TryGetValue(key, out var queryValue))
+                    continue;
+
+                object? value;
+
+                if (property.PropertyType.IsAssignableTo(typeof(byte[])))
+                    value = Base64.Decode(queryValue.ToString());
+                else
+                    value = Convert.ChangeType(queryValue.ToString(), property.PropertyType);
+
+                property.SetValue(instance, value);
+
+                queryValueFound = true;
+            }
+
+            if (!queryValueFound)
+                break;
+
+            templates.Add(instance);
+        }
+
+        var resultArray = Array.CreateInstance(elementType, templates.Count);
+
+        for (var i = 0; i < templates.Count; i++)
+        {
+            resultArray.SetValue(templates[i], i);
+        }
+
+        bindingContext.Result = ModelBindingResult.Success(resultArray);
+        return Task.CompletedTask;
+    }
+}
+
+public class GenericArrayModelBinderProvider : IModelBinderProvider
+{
+    public IModelBinder? GetBinder(ModelBinderProviderContext context)
+    {
+        return context.Metadata is { IsEnumerableType: true, ElementMetadata.IsComplexType: true } ? new BinderTypeModelBinder(typeof(GenericArrayModelBinder)) : null;
+    }
+}
