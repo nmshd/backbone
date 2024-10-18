c32832f3f9d0cb31a4e2bf7b5b2d8ac12b7474fa
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-10-18 12:58:13 +0200

    test: update test

--- Modules/Devices/test/Devices.Application.Tests/Tests/Identities/Commands/StartDeletionProcessAsOwner/HandlerTests.cs
+++ Modules/Devices/test/Devices.Application.Tests/Tests/Identities/Commands/StartDeletionProcessAsOwner/HandlerTests.cs
@@ -47,7 +47,7 @@ public class HandlerTests : AbstractTestsBase
                 A<CancellationToken>._))
             .MustHaveHappenedOnceExactly();
 
-        A.CallTo(() => mockPushNotificationSender.SendNotification(activeIdentity.Address, A<DeletionProcessStartedPushNotification>._, A<CancellationToken>._)).MustNotHaveHappened();
+        A.CallTo(() => mockPushNotificationSender.SendNotification(activeIdentity.Address, A<DeletionProcessStartedPushNotification>._, A<CancellationToken>._)).MustHaveHappenedOnceExactly();
     }
 
     [Fact]
