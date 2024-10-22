28f256b7510fbf236848b4543bab197a73a84777
Mika Herrmann <mika.herrmann@js-soft.com>
2024-10-22 17:26:25 +0200

    chore: Let Tags controller authorize the clients, but allow anonymous access for the GET /Tags endpoint

--- Modules/Tags/src/Tags.ConsumerApi/Controllers/TagsController.cs
+++ Modules/Tags/src/Tags.ConsumerApi/Controllers/TagsController.cs
@@ -1,11 +1,13 @@
 ﻿using Backbone.BuildingBlocks.API.Mvc;
 using Backbone.Modules.Tags.Application.Tags.Queries.ListTags;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backbone.Modules.Tags.ConsumerApi.Controllers;
 
 [Route("api/v1/[controller]")]
+[Authorize("OpenIddict.Validation.AspNetCore")]
 public class TagsController : ApiControllerBase
 {
     public TagsController(IMediator mediator) : base(mediator)
@@ -13,6 +15,7 @@ public class TagsController : ApiControllerBase
     }
 
     [HttpGet]
+    [AllowAnonymous]
     public async Task<IActionResult> ListTags(CancellationToken cancellationToken)
     {
         var response = await _mediator.Send(new ListTagsQuery(), cancellationToken);
