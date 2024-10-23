9f1d91ee13e7acd30564664492659e81dd44927f
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-10-23 10:53:59 +0200

    test: improve & fix tests

--- Modules/Devices/test/Devices.Application.Tests/Tests/DomainEvents/Incoming/ExternalEventCreatedDomainEventHandlerTests.cs
+++ Modules/Devices/test/Devices.Application.Tests/Tests/DomainEvents/Incoming/ExternalEventCreatedDomainEventHandlerTests.cs
@@ -1,5 +1,7 @@
-﻿using Backbone.BuildingBlocks.Application.PushNotifications;
+﻿using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.UserContext;
+using Backbone.BuildingBlocks.Application.PushNotifications;
 using Backbone.Modules.Devices.Application.DomainEvents.Incoming.ExternalEventCreated;
+using Backbone.Modules.Devices.Application.Infrastructure.Persistence.Repository;
 using Backbone.Modules.Devices.Application.Infrastructure.PushNotifications.ExternalEvents;
 using Backbone.Modules.Devices.Domain.DomainEvents.Incoming.ExternalEventCreated;
 using FakeItEasy;
@@ -13,10 +15,16 @@ public class ExternalEventCreatedDomainEventHandlerTests : AbstractTestsBase
     {
         // Arrange
         var mockPushSender = A.Fake<IPushNotificationSender>();
+        var fakeUserContext = A.Fake<IUserContext>();
+        var fakeIdentitiesRepository = A.Fake<IIdentitiesRepository>();
+        var identity = TestDataGenerator.CreateIdentity();
 
-        var handler = new ExternalEventCreatedDomainEventHandler(mockPushSender);
+        var handler = new ExternalEventCreatedDomainEventHandler(mockPushSender, fakeUserContext, fakeIdentitiesRepository);
 
         var externalEventOwner = CreateRandomIdentityAddress();
+
+        A.CallTo(() => fakeUserContext.GetAddress()).Returns(externalEventOwner);
+        A.CallTo(() => fakeIdentitiesRepository.FindByAddress(externalEventOwner, A<CancellationToken>._, A<bool>._)).Returns(identity);
 
         // Act
         await handler.Handle(new ExternalEventCreatedDomainEvent { Owner = externalEventOwner, IsDeliveryBlocked = false });
@@ -30,10 +38,16 @@ public class ExternalEventCreatedDomainEventHandlerTests : AbstractTestsBase
     {
         // Arrange
         var mockPushSender = A.Fake<IPushNotificationSender>();
+        var fakeUserContext = A.Fake<IUserContext>();
+        var fakeIdentitiesRepository = A.Fake<IIdentitiesRepository>();
+        var identity = TestDataGenerator.CreateIdentity();
 
-        var handler = new ExternalEventCreatedDomainEventHandler(mockPushSender);
+        var handler = new ExternalEventCreatedDomainEventHandler(mockPushSender, fakeUserContext, fakeIdentitiesRepository);
 
         var externalEventOwner = CreateRandomIdentityAddress();
+
+        A.CallTo(() => fakeUserContext.GetAddress()).Returns(externalEventOwner);
+        A.CallTo(() => fakeIdentitiesRepository.FindByAddress(externalEventOwner, A<CancellationToken>._, A<bool>._)).Returns(identity);
 
         // Act
         await handler.Handle(new ExternalEventCreatedDomainEvent { Owner = externalEventOwner, IsDeliveryBlocked = true });
