3252ce541b8c0405b167f9cff8ab71d114e9877a
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-10-23 11:20:26 +0200

    test: add new test

--- Modules/Devices/test/Devices.Application.Tests/Tests/DomainEvents/Incoming/ExternalEventCreatedDomainEventHandlerTests.cs
+++ Modules/Devices/test/Devices.Application.Tests/Tests/DomainEvents/Incoming/ExternalEventCreatedDomainEventHandlerTests.cs
@@ -34,6 +34,31 @@ public class ExternalEventCreatedDomainEventHandlerTests : AbstractTestsBase
     }
 
     [Fact]
+    public async Task Does_not_send_push_notification_if_owner_is_in_status_to_be_deleted()
+    {
+        // Arrange
+        var mockPushSender = A.Fake<IPushNotificationSender>();
+        var fakeUserContext = A.Fake<IUserContext>();
+        var fakeIdentitiesRepository = A.Fake<IIdentitiesRepository>();
+        var identity = TestDataGenerator.CreateIdentity();
+
+        var handler = new ExternalEventCreatedDomainEventHandler(mockPushSender, fakeUserContext, fakeIdentitiesRepository);
+
+        var externalEventOwner = CreateRandomIdentityAddress();
+
+        identity.StartDeletionProcessAsOwner(identity.Devices[0].Id);
+
+        A.CallTo(() => fakeUserContext.GetAddress()).Returns(externalEventOwner);
+        A.CallTo(() => fakeIdentitiesRepository.FindByAddress(externalEventOwner, A<CancellationToken>._, A<bool>._)).Returns(identity);
+
+        // Act
+        await handler.Handle(new ExternalEventCreatedDomainEvent { Owner = externalEventOwner, IsDeliveryBlocked = false });
+
+        // Assert
+        A.CallTo(() => mockPushSender.SendNotification(externalEventOwner, A<ExternalEventCreatedPushNotification>._, A<CancellationToken>._)).MustNotHaveHappened();
+    }
+
+    [Fact]
     public async Task Does_not_send_a_push_notification_when_delivery_of_the_external_event_is_blocked()
     {
         // Arrange
