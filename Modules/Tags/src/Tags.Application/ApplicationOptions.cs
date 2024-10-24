09449ba78d343bfb1b5a66472c277f20bfa609e2
Mika Herrmann <mika.herrmann@js-soft.com>
2024-10-24 15:47:57 +0200

    feat: Use custom validation for tags

--- Modules/Tags/src/Tags.Application/ApplicationOptions.cs
+++ Modules/Tags/src/Tags.Application/ApplicationOptions.cs
@@ -3,32 +3,58 @@ using Backbone.Modules.Tags.Domain;
 
 namespace Backbone.Modules.Tags.Application;
 
+[ContainsValidTags]
 public class ApplicationOptions
 {
-    public List<AttributeInfo> Attributes { get; set; } = [];
-}
-
-public class AttributeInfo
-{
-    [Required]
-    public string Name { get; set; } = string.Empty;
-
     [Required]
     public List<string> SupportedLanguages { get; set; } = [];
 
     public Dictionary<string, Dictionary<string, TagInfo>> TagsForAttributeValueTypes { get; set; } = [];
 }
 
-public static class DisplayNamesValidator
+[AttributeUsage(AttributeTargets.Class)]
+public class ContainsValidTagsAttribute : ValidationAttribute
 {
-    public static ValidationResult? Validate(Dictionary<string, string> displayNames, ValidationContext context)
+    private List<string> _supportedLanguages = [];
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
-        return displayNames.ContainsKey("en") ? ValidationResult.Success : new ValidationResult($"No English display name is provided for Tag {GetTagName(context)}");
+        if (value is not ApplicationOptions applicationOptions) return new ValidationResult($"The attribute can only be used for {nameof(ApplicationOptions)}.");
+
+        if (!applicationOptions.SupportedLanguages.Contains("en")) return new ValidationResult("The tags have to support the English language.");
+
+        _supportedLanguages = applicationOptions.SupportedLanguages;
+        foreach (var (attrName, tags) in applicationOptions.TagsForAttributeValueTypes)
+        {
+            foreach (var (tagName, tag) in tags)
+            {
+                var result = ValidateTag([attrName, tagName], tag, validationContext);
+                if (result != ValidationResult.Success) return result;
+            }
+        }
+
+        return ValidationResult.Success;
     }
 
-    private static string GetTagName(ValidationContext context)
+    private ValidationResult? ValidateTag(IEnumerable<string> nameParts, TagInfo tag, ValidationContext validationContext)
     {
-        var info = (TagInfo)context.ObjectInstance;
-        return info.Tag;
+        var notSupportedLanguages = tag.DisplayNames.Keys.Except(_supportedLanguages).ToList();
+        var notImplementedLanguages = _supportedLanguages.Except(tag.DisplayNames.Keys).ToList();
+
+        if (notSupportedLanguages.Count != 0)
+            return new ValidationResult($"The languages \"{Enumerate(notSupportedLanguages)}\" are unsupported", [GetPathOfProperty(nameParts)]);
+        if (notImplementedLanguages.Count != 0)
+            return new ValidationResult($"A display name for the language(s) \"{Enumerate(notImplementedLanguages)}\" is required.", [GetPathOfProperty(nameParts)]);
+
+        foreach (var (childName, child) in tag.Children)
+        {
+            var result = ValidateTag(nameParts.Append(childName), child, validationContext);
+            if (result != ValidationResult.Success) return result;
+        }
+
+        return ValidationResult.Success;
     }
+
+    private static string GetPathOfProperty(IEnumerable<string> nameParts) => string.Join('.', nameParts);
+    private static string Enumerate(IEnumerable<string> names) => string.Join(',', names);
 }
