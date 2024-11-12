b27b01196672c433b00296cba9b1a18db37fcef1
Timo Notheisen <timo.notheisen@js-soft.com>
2024-11-12 07:32:58 +0100

    fix: don't `Database.EnsureCreatedAsync` in seeders

--- Applications/ConsumerApi/src/QuotasDbContextSeeder.cs
+++ Applications/ConsumerApi/src/QuotasDbContextSeeder.cs
@@ -21,8 +21,6 @@ public class QuotasDbContextSeeder : IDbSeeder<QuotasDbContext>
 
     private async Task SeedEverything(QuotasDbContext context)
     {
-        await context.Database.EnsureCreatedAsync();
-
         await SeedQueuedForDeletionTierMetrics();
     }
 
