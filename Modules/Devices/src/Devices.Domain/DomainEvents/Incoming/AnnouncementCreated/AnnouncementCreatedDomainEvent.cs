b4c253f6503160bcadb3841e6b3e3d9f3e1748b1
Timo Notheisen <timo.notheisen@js-soft.com>
2024-11-13 10:51:54 +0100

    feat: send push notification for new announcement

---
+++ Modules/Devices/src/Devices.Domain/DomainEvents/Incoming/AnnouncementCreated/AnnouncementCreatedDomainEvent.cs
@@ -0,0 +1,17 @@
+﻿using Backbone.BuildingBlocks.Domain.Events;
+
+namespace Backbone.Modules.Devices.Domain.DomainEvents.Incoming.AnnouncementCreated;
+
+public class AnnouncementCreatedDomainEvent : DomainEvent
+{
+    public required string Id { get; set; }
+    public required string Severity { get; set; }
+    public required List<AnnouncementCreatedDomainEventText> Texts { get; set; }
+}
+
+public class AnnouncementCreatedDomainEventText
+{
+    public required string Language { get; set; }
+    public required string Title { get; set; }
+    public required string Body { get; set; }
+}
