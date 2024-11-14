ab007913f8bef266a1b9e4acb5334fe27a815579
Timo Notheisen <timo.notheisen@js-soft.com>
2024-11-14 10:01:44 +0100

    chore: remove commented out code

--- BuildingBlocks/src/BuildingBlocks.Application/PushNotifications/IPushNotificationSender.cs
+++ BuildingBlocks/src/BuildingBlocks.Application/PushNotifications/IPushNotificationSender.cs
@@ -4,9 +4,6 @@ namespace Backbone.BuildingBlocks.Application.PushNotifications;
 
 public interface IPushNotificationSender
 {
-    // Task SendNotification(IPushNotificationWithConstantTextWithDynamicText notification, CancellationToken cancellationToken);
-    // Task SendNotification(IdentityAddress recipient, IPushNotificationWithConstantText notificationWithConstantText, CancellationToken cancellationToken);
-    // Task SendFilteredNotification(IdentityAddress recipient, IPushNotificationWithConstantText notificationWithConstantText, IEnumerable<string> excludedDevices, CancellationToken cancellationToken);
     Task SendNotification(IPushNotificationWithConstantText notification, SendPushNotificationFilter filter, CancellationToken cancellationToken);
 
     Task SendNotification(IPushNotificationWithDynamicText notification, SendPushNotificationFilter filter, Dictionary<string, NotificationText> notificationTexts,
