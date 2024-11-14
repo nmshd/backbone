a32e753a122336dfd7942be720785b633b282371
Mika Herrmann <mika.herrmann@js-soft.com>
2024-11-14 16:26:04 +0100

    feat: Make CreatedBy property in Relationship Audit Log nullable

---
+++ Modules/Relationships/src/Relationships.Infrastructure.Database.SqlServer/Migrations/20241113085648_MakeCreatedByDeviceNullableInRelationshipAuditLogEntry.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backbone.Modules.Relationships.Infrastructure.Database.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeCreatedByDeviceNullableInRelationshipAuditLogEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedByDevice",
+                schema: "Relationships",
+                table: "RelationshipAuditLog",
+                type: "char(20)",
+                unicode: false,
+                fixedLength: true,
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "char(20)",
+                oldUnicode: false,
+                oldFixedLength: true,
+                oldMaxLength: 20);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedByDevice",
+                schema: "Relationships",
+                table: "RelationshipAuditLog",
+                type: "char(20)",
+                unicode: false,
+                fixedLength: true,
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "char(20)",
+                oldUnicode: false,
+                oldFixedLength: true,
+                oldMaxLength: 20,
+                oldNullable: true);
+        }
+    }
+}
