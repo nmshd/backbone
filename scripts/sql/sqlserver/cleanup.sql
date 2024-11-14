26b27f08ebb6e06a75893d3d203cb345fc4a8e6c
Eric Brunner <erich.brunner@live.at>
2024-11-14 09:33:47 +0100

    chore: added database cleanup scripts

---
+++ scripts/sql/sqlserver/cleanup.sql
@@ -0,0 +1,8 @@
+﻿
+DELETE FROM Devices.Identities;
+DELETE FROM Devices.Devices;
+DELETE FROM Relationships.RelationshipTemplates;
+DELETE FROM Relationships.Relationships;
+DELETE FROM Challenges.Challenges;
+DELETE FROM Messages.Messages;
+DELETE FROM Synchronization.DatawalletModifications;
