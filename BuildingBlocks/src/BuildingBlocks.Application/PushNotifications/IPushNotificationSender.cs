7cac4c118a76b2a86d2aa69718320fbc1e69c6a2
Timo Notheisen <timo.notheisen@js-soft.com>
2024-11-14 10:02:26 +0100

    refactor: move records to separate files

--- BuildingBlocks/src/BuildingBlocks.Application/PushNotifications/IPushNotificationSender.cs
+++ BuildingBlocks/src/BuildingBlocks.Application/PushNotifications/IPushNotificationSender.cs
@@ -1,5 +1,3 @@
-using Backbone.DevelopmentKit.Identity.ValueObjects;
-
 namespace Backbone.BuildingBlocks.Application.PushNotifications;
 
 public interface IPushNotificationSender
@@ -9,11 +7,3 @@ public interface IPushNotificationSender
     Task SendNotification(IPushNotificationWithDynamicText notification, SendPushNotificationFilter filter, Dictionary<string, NotificationText> notificationTexts,
         CancellationToken cancellationToken);
 }
-
-public record SendPushNotificationFilter
-{
-    public List<DeviceId> ExcludedDevices { get; set; } = [];
-    public List<IdentityAddress> IncludedIdentities { get; set; } = [];
-}
-
-public record NotificationText(string Title, string Body);
---
+++ BuildingBlocks/src/BuildingBlocks.Application/PushNotifications/SendPushNotificationFilter.cs
@@ -0,0 +1,9 @@
+﻿using Backbone.DevelopmentKit.Identity.ValueObjects;
+
+namespace Backbone.BuildingBlocks.Application.PushNotifications;
+
+public record SendPushNotificationFilter
+{
+    public List<DeviceId> ExcludedDevices { get; set; } = [];
+    public List<IdentityAddress> IncludedIdentities { get; set; } = [];
+}
