18f8ca670838bc7a1c4b709e944621cfa730415f
Eric Brunner <erich.brunner@live.at>
2024-11-15 10:53:29 +0100

    feat: added clean-db backup and restore script

--- scripts/sql/sqlserver/cleanup.sql
+++
@@ -1,8 +0,0 @@
-﻿
-DELETE FROM Devices.Identities;
-DELETE FROM Devices.Devices;
-DELETE FROM Relationships.RelationshipTemplates;
-DELETE FROM Relationships.Relationships;
-DELETE FROM Challenges.Challenges;
-DELETE FROM Messages.Messages;
-DELETE FROM Synchronization.DatawalletModifications;
