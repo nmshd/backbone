bf5fc24ec344069678caf4b2c7a8bbd40b8ab252
Mika Herrmann <mika.herrmann@js-soft.com>
2024-11-20 11:28:22 +0100

    chore: Add unit test for anonymization#

--- Modules/Relationships/src/Relationships.Domain/Aggregates/Relationships/Relationship.DecomposeDueToIdentityDeletionTests.cs
+++ Modules/Relationships/src/Relationships.Domain/Aggregates/Relationships/Relationship.DecomposeDueToIdentityDeletionTests.cs
@@ -72,7 +72,22 @@ public class RelationshipDecomposeDueToIdentityDeletionTests : AbstractTestsBase
     }
 
     [Fact]
-    public void Anonymize_anonymizes_the_audit_logs()
+    public void Anonymizes_the_deleted_identity()
+    {
+        // Arrange
+        var relationship = CreateActiveRelationship(IDENTITY_1, IDENTITY_2);
+        var anonymousIdentity = IdentityAddress.GetAnonymized(DID_DOMAIN_NAME);
+        
+        // Act
+        relationship.DecomposeDueToIdentityDeletion(IDENTITY_1, DID_DOMAIN_NAME);
+        
+        // Assert
+        relationship.From.Should().Be(anonymousIdentity);
+        relationship.FromHasDecomposed.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Anonymizes_the_audit_logs()
     {
         // Arrange
         var relationship = CreateActiveRelationship(IDENTITY_1, IDENTITY_2);
