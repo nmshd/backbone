7958a6a45c75fd2ad03f5c257543a19f39e02491
Timo Notheisen <timo.notheisen@js-soft.com>
2024-11-29 13:23:55 +0100

    chore: add missing file

---
+++ Modules/Devices/src/Devices.Application/DomainEvents/Incoming/BackupDeviceUsed/BackupDeviceUsedDomainEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
+using Backbone.BuildingBlocks.Application.PushNotifications;
+using Backbone.Modules.Devices.Application.Infrastructure.PushNotifications.Device;
+using Backbone.Modules.Devices.Domain.DomainEvents.Outgoing;
+
+namespace Backbone.Modules.Devices.Application.DomainEvents.Incoming.BackupDeviceUsed;
+
+public class BackupDeviceUsedDomainEventHandler : IDomainEventHandler<BackupDeviceUsedDomainEvent>
+{
+    private readonly IPushNotificationSender _pushNotificationSender;
+
+    public BackupDeviceUsedDomainEventHandler(IPushNotificationSender pushNotificationSender)
+    {
+        _pushNotificationSender = pushNotificationSender;
+    }
+
+    public async Task Handle(BackupDeviceUsedDomainEvent @event)
+    {
+        await _pushNotificationSender.SendNotification(new BackupDeviceUsedPushNotification(), SendPushNotificationFilter.AllDevicesOf(@event.IdentityAddress), CancellationToken.None);
+    }
+}
