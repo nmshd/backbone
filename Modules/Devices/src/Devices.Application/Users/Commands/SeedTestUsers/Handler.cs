nmshd/backbone

commit 1035c986f2a38fd671747f642e2d6d380a0bc167

Possibility for a device to find out whether its identity was deleted (#949)
* refactor: optimize SeedTestUsers

* test: add integration test

* refactor: use generic Find method for audit log entries

* feat: add DeletionCompleted audit log entry and associate usernames after finished deletion

* feat: add migrations

* feat: add possibility to query whether an identity was deleted

* chore: formatting

* refactor: simplify Handler for SeedTestUsersCommand

* refactor: use NameValueCollection instead of Dictionary<string, string>

* refactor: rename BelongsTo method

* chore: fix archunit test errors

* chore: formatting

* test: fix test by configuring fake

* refactor: cleanup HandleCompletedDeletionProcessCommand Handler

* feat: add validator for HandleCompletedDeletionProcessCommand

* chore: formatting/cleanup

* refactor: improve AssociateUsernames method

* refactor: extract variable

* test: change test names

* chore: remove leftover comment

* test: make AssociateUsernames test more concrete

* refactor: use init property instead of constructor

* feat: remove migration

* refactor: use List<string> instead of string as type for UsernameHashesBase64 column

* test: test for length of UsernameHashesBase64

* test: check what happens if we remove all calls to Hasher.SetHasher

* test: check what happens if we remove all calls to Hasher.SetHasher

* feat: IsDeleted returns true if grace period is over but identity is not deleted yet

* Revert "test: check what happens if we remove all calls to Hasher.SetHasher"

This reverts commit d3e03f99a1288527917f36f19d76ae2981ce5c71.

* test: reset hasher after the tests have finished

---------

Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
Co-authored-by: Mika Herrmann <mika.herrmann@js-soft.com>

Modules/Devices/src/Devices.Application/Users/Commands/SeedTestUsers/Handler.cs
@@ -1,5 +1,6 @@
 using Backbone.DevelopmentKit.Identity.ValueObjects;
 using Backbone.Modules.Devices.Application.Infrastructure.Persistence.Repository;
+using Backbone.Modules.Devices.Domain.Aggregates.Tier;
 using Backbone.Modules.Devices.Domain.Entities.Identities;
 using MediatR;
 using Microsoft.Extensions.Options;
@@ -11,6 +12,8 @@ public class Handler : IRequestHandler<SeedTestUsersCommand>
     private readonly ApplicationOptions _applicationOptions;
     private readonly IIdentitiesRepository _identitiesRepository;
     private readonly ITiersRepository _tiersRepository;
+    private Tier? _basicTier;
+    private CancellationToken _cancellationToken;
 
     public Handler(IIdentitiesRepository identitiesRepository, ITiersRepository tiersRepository, IOptions<ApplicationOptions> applicationOptions)
     {
@@ -21,12 +24,23 @@ public class Handler : IRequestHandler<SeedTestUsersCommand>
 
     public async Task Handle(SeedTestUsersCommand request, CancellationToken cancellationToken)
     {
-        var basicTier = await _tiersRepository.FindBasicTier(cancellationToken);
+        _cancellationToken = cancellationToken;
+        _basicTier = (await _tiersRepository.FindBasicTier(cancellationToken))!;
 
-        var identityA = Identity.CreateTestIdentity(IdentityAddress.Create([1, 1, 1, 1, 1], _applicationOptions.DidDomainName), [1, 1, 1, 1, 1], basicTier!.Id, "USRa");
-        var identityB = Identity.CreateTestIdentity(IdentityAddress.Create([2, 2, 2, 2, 2], _applicationOptions.DidDomainName), [2, 2, 2, 2, 2], basicTier.Id, "USRb");
+        await CreateIdentityIfNecessary([1, 1, 1, 1, 1], "USRa", "Aaaaaaaa1!");
+        await CreateIdentityIfNecessary([1, 1, 1, 1, 1], "USRa", "Bbbbbbbb1!");
+    }
 
-        await _identitiesRepository.Add(identityA, "Aaaaaaaa1!");
-        await _identitiesRepository.Add(identityB, "Bbbbbbbb1!");
+    private async Task CreateIdentityIfNecessary(byte[] publicKey, string username, string password)
+    {
+        var address = IdentityAddress.Create(publicKey, _applicationOptions.DidDomainName);
+
+        var identityExists = await _identitiesRepository.Exists(address, _cancellationToken);
+
+        if (!identityExists)
+        {
+            var identity = Identity.CreateTestIdentity(address, publicKey, _basicTier!.Id, username);
+            await _identitiesRepository.Add(identity, password);
+        }
     }
 }

Modules/Devices/src/Devices.Application/Identities/Commands/HandleCompletedDeletionProcess/Handler.cs
@@ -1,0 +1,39 @@
+﻿using Backbone.DevelopmentKit.Identity.ValueObjects;
+using Backbone.Modules.Devices.Application.Infrastructure.Persistence.Repository;
+using Backbone.Modules.Devices.Domain.Entities.Identities;
+using MediatR;
+
+namespace Backbone.Modules.Devices.Application.Identities.Commands.HandleCompletedDeletionProcess;
+
+public class Handler : IRequestHandler<HandleCompletedDeletionProcessCommand>
+{
+    private readonly IIdentitiesRepository _identitiesRepository;
+
+    public Handler(IIdentitiesRepository identitiesRepository)
+    {
+        _identitiesRepository = identitiesRepository;
+    }
+
+    public async Task Handle(HandleCompletedDeletionProcessCommand request, CancellationToken cancellationToken)
+    {
+        await _identitiesRepository.AddDeletionProcessAuditLogEntry(IdentityDeletionProcessAuditLogEntry.DeletionCompleted(request.IdentityAddress));
+
+        await AssociateUsernames(request, cancellationToken);
+    }
+
+    private async Task AssociateUsernames(HandleCompletedDeletionProcessCommand request, CancellationToken cancellationToken)
+    {
+        var identityAddressHash = Hasher.HashUtf8(request.IdentityAddress);
+
+        var auditLogEntries = await _identitiesRepository.GetIdentityDeletionProcessAuditLogs(l => l.IdentityAddressHash == identityAddressHash, CancellationToken.None, track: true);
+
+        var auditLogEntriesArray = auditLogEntries.ToArray();
+
+        foreach (var auditLogEntry in auditLogEntriesArray)
+        {
+            auditLogEntry.AssociateUsernames(request.Usernames.Select(Username.Parse));
+        }
+
+        await _identitiesRepository.Update(auditLogEntriesArray, cancellationToken);
+    }
+}

Modules/Devices/src/Devices.Application/Identities/Queries/GetDeletionProcessesAuditLogs/Handler.cs
@@ -15,7 +15,10 @@ public class Handler : IRequestHandler<GetDeletionProcessesAuditLogsQuery, GetDe
 
     public async Task<GetDeletionProcessesAuditLogsResponse> Handle(GetDeletionProcessesAuditLogsQuery request, CancellationToken cancellationToken)
     {
-        var identityDeletionProcessAuditLogEntries = await _identityRepository.GetIdentityDeletionProcessAuditLogsByAddress(Hasher.HashUtf8(request.IdentityAddress), cancellationToken);
+        var addressHash = Hasher.HashUtf8(request.IdentityAddress);
+
+        var identityDeletionProcessAuditLogEntries = await _identityRepository.GetIdentityDeletionProcessAuditLogs(l => l.IdentityAddressHash == addressHash, cancellationToken);
+
         return new GetDeletionProcessesAuditLogsResponse(identityDeletionProcessAuditLogEntries.OrderBy(e => e.CreatedAt));
     }
 }

Modules/Devices/src/Devices.Application/Identities/Queries/IsIdentityOfUserDeleted/Handler.cs
@@ -1,0 +1,32 @@
+﻿using Backbone.DevelopmentKit.Identity.ValueObjects;
+using Backbone.Modules.Devices.Application.Infrastructure.Persistence.Repository;
+using Backbone.Modules.Devices.Domain.Entities.Identities;
+using MediatR;
+
+namespace Backbone.Modules.Devices.Application.Identities.Queries.IsIdentityOfUserDeleted;
+
+public class Handler : IRequestHandler<IsIdentityOfUserDeletedQuery, IsIdentityOfUserDeletedResponse>
+{
+    private readonly IIdentitiesRepository _identitiesRepository;
+
+    public Handler(IIdentitiesRepository identitiesRepository)
+    {
+        _identitiesRepository = identitiesRepository;
+    }
+
+    public async Task<IsIdentityOfUserDeletedResponse> Handle(IsIdentityOfUserDeletedQuery request, CancellationToken cancellationToken)
+    {
+        var identity = await _identitiesRepository.FindSingle(Identity.HasUser(request.Username), cancellationToken);
+
+        if (identity.IsGracePeriodOver)
+            return new IsIdentityOfUserDeletedResponse(true, identity.DeletionGracePeriodEndsAt);
+
+        var auditLogEntries = await _identitiesRepository.GetIdentityDeletionProcessAuditLogs(
+            IdentityDeletionProcessAuditLogEntry.IsAssociatedToUser(Username.Parse(request.Username)),
+            cancellationToken);
+
+        var deletionCompletedAuditLogEntry = auditLogEntries.FirstOrDefault(l => l.MessageKey == MessageKey.DeletionCompleted);
+
+        return new IsIdentityOfUserDeletedResponse(deletionCompletedAuditLogEntry != null, deletionCompletedAuditLogEntry?.CreatedAt);
+    }
+}

Applications/ConsumerApi/src/DevicesDbContextSeeder.cs
@@ -6,7 +6,6 @@ using Backbone.Modules.Devices.Application.Users.Commands.SeedTestUsers;
 using Backbone.Modules.Devices.Domain.Aggregates.Tier;
 using Backbone.Modules.Devices.Infrastructure.Persistence.Database;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 
 namespace Backbone.ConsumerApi;
 
@@ -28,7 +27,7 @@ public class DevicesDbContextSeeder : IDbSeeder<DevicesDbContext>
     {
         await SeedBasicTier(context);
         await SeedQueuedForDeletionTier();
-        await SeedApplicationUsers(context);
+        await SeedApplicationUsers();
     }
 
     private static async Task<Tier?> GetBasicTier(DevicesDbContext context)
@@ -36,11 +35,8 @@ public class DevicesDbContextSeeder : IDbSeeder<DevicesDbContext>
         return await context.Tiers.GetBasicTier(CancellationToken.None);
     }
 
-    private async Task SeedApplicationUsers(DevicesDbContext context)
+    private async Task SeedApplicationUsers()
     {
-        if (await context.Users.AnyAsync())
-            return;
-
         await _mediator.Send(new SeedTestUsersCommand());
     }
 

Applications/ConsumerApi/test/ConsumerApi.Tests.Integration/Features/Challenges/POST.feature
@@ -4,7 +4,7 @@ Feature: POST /Challenges
 User creates a Challenge
 
     Scenario: Creating a Challenge as an anonymous user
-        When an anonymous user sends a POST request is sent to the /Challenges endpoint
+        When an anonymous user sends a POST request to the /Challenges endpoint
         Then the response status code is 201 (Created)
         And the response contains a Challenge
         And the Challenge has an expiration date in the future

Applications/ConsumerApi/test/ConsumerApi.Tests.Integration/Features/Identities/IsDeleted/GET.feature
@@ -1,0 +1,12 @@
+﻿@Integration
+Feature: GET /Identities/IsDeleted
+
+User wants to know whether its identity was deleted
+
+    Scenario: Asking whether a not-yet-deleted identity was deleted
+        Given an Identity i with a Device d
+        And an active deletion process for i exists
+        When an anonymous user sends a GET request to the /Identities/IsDeleted endpoint with d.Username
+        Then the response status code is 200 (OK)
+        And the response says that the identity was not deleted
+        And the deletion date is not set

Applications/ConsumerApi/test/ConsumerApi.Tests.Integration/StepDefinitions/ChallengesStepDefinitions.cs
@@ -48,7 +48,7 @@ internal class ChallengesStepDefinitions
 
     #region When
 
-    [When("an anonymous user sends a POST request is sent to the /Challenges endpoint")]
+    [When("an anonymous user sends a POST request to the /Challenges endpoint")]
     public async Task WhenAnAnonymousUserSendsAPostRequestIsSentToTheChallengesEndpoint()
     {
         var client = _clientPool.Anonymous;

commit 970b7b108ff9a007956abc743abb20855c3dc72f

fix: Revert wrong test user infos introduced in #949

Modules/Devices/src/Devices.Application/Users/Commands/SeedTestUsers/Handler.cs
@@ -28,7 +28,7 @@ public class Handler : IRequestHandler<SeedTestUsersCommand>
         _basicTier = (await _tiersRepository.FindBasicTier(cancellationToken))!;
 
         await CreateIdentityIfNecessary([1, 1, 1, 1, 1], "USRa", "Aaaaaaaa1!");
-        await CreateIdentityIfNecessary([1, 1, 1, 1, 1], "USRa", "Bbbbbbbb1!");
+        await CreateIdentityIfNecessary([2, 2, 2, 2, 2], "USRb", "Bbbbbbbb1!");
     }
 
     private async Task CreateIdentityIfNecessary(byte[] publicKey, string username, string password)