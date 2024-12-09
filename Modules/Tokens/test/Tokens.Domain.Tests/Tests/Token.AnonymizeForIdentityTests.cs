nmshd/backbone

commit 1035c986f2a38fd671747f642e2d6d380a0bc167

Possibility for a device to find out whether its identity was deleted (#949)
* refactor: optimize SeedTestUsers

* test: add integration test

* refactor: use generic Find method for audit log entries

* feat: add DeletionCompleted audit log entry and associate usernames after finished deletion

* feat: add migrations

* feat: add possibility to query whether an identity was deleted

* chore: formatting

* refactor: simplify Handler for SeedTestUsersCommand

* refactor: use NameValueCollection instead of Dictionary<string, string>

* refactor: rename BelongsTo method

* chore: fix archunit test errors

* chore: formatting

* test: fix test by configuring fake

* refactor: cleanup HandleCompletedDeletionProcessCommand Handler

* feat: add validator for HandleCompletedDeletionProcessCommand

* chore: formatting/cleanup

* refactor: improve AssociateUsernames method

* refactor: extract variable

* test: change test names

* chore: remove leftover comment

* test: make AssociateUsernames test more concrete

* refactor: use init property instead of constructor

* feat: remove migration

* refactor: use List<string> instead of string as type for UsernameHashesBase64 column

* test: test for length of UsernameHashesBase64

* test: check what happens if we remove all calls to Hasher.SetHasher

* test: check what happens if we remove all calls to Hasher.SetHasher

* feat: IsDeleted returns true if grace period is over but identity is not deleted yet

* Revert "test: check what happens if we remove all calls to Hasher.SetHasher"

This reverts commit d3e03f99a1288527917f36f19d76ae2981ce5c71.

* test: reset hasher after the tests have finished

---------

Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
Co-authored-by: Mika Herrmann <mika.herrmann@js-soft.com>

Modules/Devices/src/Devices.Application/Users/Commands/SeedTestUsers/Handler.cs
@@ -1,5 +1,6 @@
 using Backbone.DevelopmentKit.Identity.ValueObjects;
 using Backbone.Modules.Devices.Application.Infrastructure.Persistence.Repository;
+using Backbone.Modules.Devices.Domain.Aggregates.Tier;
 using Backbone.Modules.Devices.Domain.Entities.Identities;
 using MediatR;
 using Microsoft.Extensions.Options;
@@ -11,6 +12,8 @@ public class Handler : IRequestHandler<SeedTestUsersCommand>
     private readonly ApplicationOptions _applicationOptions;
     private readonly IIdentitiesRepository _identitiesRepository;
     private readonly ITiersRepository _tiersRepository;
+    private Tier? _basicTier;
+    private CancellationToken _cancellationToken;
 
     public Handler(IIdentitiesRepository identitiesRepository, ITiersRepository tiersRepository, IOptions<ApplicationOptions> applicationOptions)
     {
@@ -21,12 +24,23 @@ public class Handler : IRequestHandler<SeedTestUsersCommand>
 
     public async Task Handle(SeedTestUsersCommand request, CancellationToken cancellationToken)
     {
-        var basicTier = await _tiersRepository.FindBasicTier(cancellationToken);
+        _cancellationToken = cancellationToken;
+        _basicTier = (await _tiersRepository.FindBasicTier(cancellationToken))!;
 
-        var identityA = Identity.CreateTestIdentity(IdentityAddress.Create([1, 1, 1, 1, 1], _applicationOptions.DidDomainName), [1, 1, 1, 1, 1], basicTier!.Id, "USRa");
-        var identityB = Identity.CreateTestIdentity(IdentityAddress.Create([2, 2, 2, 2, 2], _applicationOptions.DidDomainName), [2, 2, 2, 2, 2], basicTier.Id, "USRb");
+        await CreateIdentityIfNecessary([1, 1, 1, 1, 1], "USRa", "Aaaaaaaa1!");
+        await CreateIdentityIfNecessary([1, 1, 1, 1, 1], "USRa", "Bbbbbbbb1!");
+    }
 
-        await _identitiesRepository.Add(identityA, "Aaaaaaaa1!");
-        await _identitiesRepository.Add(identityB, "Bbbbbbbb1!");
+    private async Task CreateIdentityIfNecessary(byte[] publicKey, string username, string password)
+    {
+        var address = IdentityAddress.Create(publicKey, _applicationOptions.DidDomainName);
+
+        var identityExists = await _identitiesRepository.Exists(address, _cancellationToken);
+
+        if (!identityExists)
+        {
+            var identity = Identity.CreateTestIdentity(address, publicKey, _basicTier!.Id, username);
+            await _identitiesRepository.Add(identity, password);
+        }
     }
 }

commit c030085661752ca8a320ee9b0f1db931f77661ff

`ForIdentity` column of Relationship Templates and Tokens are not anonymized during identity deletion (#962)
* fix: Revert wrong test user infos introduced in #949

* feat: Add methods for anonymizing the ForIdentity property

* feat: Add methods for querying and updating relationship templates

* feat: Add domain error for a non-personalized relationship template

* feat: Add command, handler and validator for anonymizing personalized relationship templates for an Identity

* feat: Add anonymization command to IdentityDeleter

* test: Add unit tests for the IdentityDeleter and the relationship template anonymization method

* chore: Make loop more readable

* feat: Add anonymization logic to Token class

* feat: Add didDomainName to Token application options

* feat: Add repository methods for querying and updating tokens

* feat: Add command, handler and validator for anonymizing all tokens for an identity

* feat: Add anonymization command to token identity deleter

* feat: Add unit tests for anonymizing tokens

* chore: Add forIdentity column to bruno POST /Tokens request

* chore: Add didDomainName to test appsettings

---------

Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

Applications/ConsumerApi/src/http/Tokens/Create Token.bru
@@ -13,6 +13,7 @@ post {
 body:json {
   {
     "content": "AAAA",
-    "expiresAt": "2024-12-17"
+    "expiresAt": "2024-12-17",
+    "forIdentity": "did:e:localhost:dids:8234cca0160ff05c785636"
   }
 }

Modules/Devices/src/Devices.Application/Users/Commands/SeedTestUsers/Handler.cs
@@ -28,7 +28,7 @@ public class Handler : IRequestHandler<SeedTestUsersCommand>
         _basicTier = (await _tiersRepository.FindBasicTier(cancellationToken))!;
 
         await CreateIdentityIfNecessary([1, 1, 1, 1, 1], "USRa", "Aaaaaaaa1!");
-        await CreateIdentityIfNecessary([1, 1, 1, 1, 1], "USRa", "Bbbbbbbb1!");
+        await CreateIdentityIfNecessary([2, 2, 2, 2, 2], "USRb", "Bbbbbbbb1!");
     }
 
     private async Task CreateIdentityIfNecessary(byte[] publicKey, string username, string password)

Modules/Relationships/src/Relationships.Application/Identities/IdentityDeleter.cs
@@ -2,6 +2,7 @@
 using Backbone.DevelopmentKit.Identity.ValueObjects;
 using Backbone.Modules.Relationships.Application.Relationships.Commands.DecomposeAndAnonymizeRelationshipsOfIdentity;
 using Backbone.Modules.Relationships.Application.RelationshipTemplates.Commands.AnonymizeRelationshipTemplateAllocationsAllocatedByIdentity;
+using Backbone.Modules.Relationships.Application.RelationshipTemplates.Commands.AnonymizeRelationshipTemplatesForIdentity;
 using Backbone.Modules.Relationships.Application.RelationshipTemplates.Commands.DeleteRelationshipTemplatesOfIdentity;
 using MediatR;
 
@@ -23,6 +24,7 @@ public class IdentityDeleter : IIdentityDeleter
         await _mediator.Send(new DecomposeAndAnonymizeRelationshipsOfIdentityCommand(identityAddress));
         await _deletionProcessLogger.LogDeletion(identityAddress, "Relationships");
         await _mediator.Send(new DeleteRelationshipTemplatesOfIdentityCommand(identityAddress));
+        await _mediator.Send(new AnonymizeRelationshipTemplatesForIdentityCommand(identityAddress));
         await _deletionProcessLogger.LogDeletion(identityAddress, "RelationshipTemplates");
         await _mediator.Send(new AnonymizeRelationshipTemplateAllocationsAllocatedByIdentityCommand(identityAddress));
         await _deletionProcessLogger.LogDeletion(identityAddress, "RelationshipTemplateAllocations");

Modules/Relationships/src/Relationships.Application/Infrastructure/Persistence/Repository/IRelationshipTemplatesRepository.cs
@@ -12,9 +12,12 @@ public interface IRelationshipTemplatesRepository
     Task<DbPaginationResult<RelationshipTemplate>> FindTemplates(IEnumerable<ListRelationshipTemplatesQueryItem> queryItems, IdentityAddress activeIdentity, PaginationFilter paginationFilter,
         CancellationToken cancellationToken, bool track = false);
 
+    Task<IEnumerable<RelationshipTemplate>> FindTemplates(Expression<Func<RelationshipTemplate, bool>> filter, CancellationToken cancellationToken);
+
     Task<RelationshipTemplate?> Find(RelationshipTemplateId id, IdentityAddress identityAddress, CancellationToken cancellationToken, bool track = false);
     Task Add(RelationshipTemplate template, CancellationToken cancellationToken);
     Task Update(RelationshipTemplate template);
+    Task Update(IEnumerable<RelationshipTemplate> templates, CancellationToken cancellationToken);
     Task Delete(Expression<Func<RelationshipTemplate, bool>> filter, CancellationToken cancellationToken);
     Task Delete(RelationshipTemplate template, CancellationToken cancellationToken);
 

Modules/Relationships/src/Relationships.Application/RelationshipTemplates/Commands/AnonymizeRelationshipTemplatesForIdentity/AnonymizeRelationshipTemplatesForIdentityCommand.cs
@@ -1,0 +1,13 @@
+﻿using MediatR;
+
+namespace Backbone.Modules.Relationships.Application.RelationshipTemplates.Commands.AnonymizeRelationshipTemplatesForIdentity;
+
+public class AnonymizeRelationshipTemplatesForIdentityCommand : IRequest
+{
+    public AnonymizeRelationshipTemplatesForIdentityCommand(string identityAddress)
+    {
+        IdentityAddress = identityAddress;
+    }
+
+    public string IdentityAddress { get; set; }
+}

Modules/Relationships/src/Relationships.Application/RelationshipTemplates/Commands/AnonymizeRelationshipTemplatesForIdentity/Handler.cs
@@ -1,0 +1,29 @@
+﻿using Backbone.DevelopmentKit.Identity.ValueObjects;
+using Backbone.Modules.Relationships.Application.Infrastructure.Persistence.Repository;
+using Backbone.Modules.Relationships.Domain.Aggregates.RelationshipTemplates;
+using MediatR;
+using Microsoft.Extensions.Options;
+
+namespace Backbone.Modules.Relationships.Application.RelationshipTemplates.Commands.AnonymizeRelationshipTemplatesForIdentity;
+
+public class Handler : IRequestHandler<AnonymizeRelationshipTemplatesForIdentityCommand>
+{
+    private readonly IRelationshipTemplatesRepository _relationshipTemplatesRepository;
+    private readonly ApplicationOptions _applicationOptions;
+
+    public Handler(IRelationshipTemplatesRepository relationshipTemplatesRepository, IOptions<ApplicationOptions> options)
+    {
+        _relationshipTemplatesRepository = relationshipTemplatesRepository;
+        _applicationOptions = options.Value;
+    }
+
+    public async Task Handle(AnonymizeRelationshipTemplatesForIdentityCommand request, CancellationToken cancellationToken)
+    {
+        var relationshipTemplates = (await _relationshipTemplatesRepository.FindTemplates(RelationshipTemplate.IsFor(IdentityAddress.Parse(request.IdentityAddress)), cancellationToken)).ToList();
+
+        foreach (var relationshipTemplate in relationshipTemplates)
+            relationshipTemplate.AnonymizeForIdentity(_applicationOptions.DidDomainName);
+
+        await _relationshipTemplatesRepository.Update(relationshipTemplates, cancellationToken);
+    }
+}

Modules/Relationships/src/Relationships.Application/RelationshipTemplates/Commands/AnonymizeRelationshipTemplatesForIdentity/Validator.cs
@@ -1,0 +1,14 @@
+﻿using Backbone.BuildingBlocks.Application.Extensions;
+using Backbone.DevelopmentKit.Identity.ValueObjects;
+using FluentValidation;
+
+namespace Backbone.Modules.Relationships.Application.RelationshipTemplates.Commands.AnonymizeRelationshipTemplatesForIdentity;
+
+public class Validator : AbstractValidator<AnonymizeRelationshipTemplatesForIdentityCommand>
+{
+    public Validator()
+    {
+        RuleFor(c => c.IdentityAddress)
+            .ValidId<AnonymizeRelationshipTemplatesForIdentityCommand, IdentityAddress>();
+    }
+}

Modules/Relationships/src/Relationships.Domain/Aggregates/RelationshipTemplates/RelationshipTemplate.cs
@@ -50,7 +50,7 @@ public class RelationshipTemplate : Entity
 
     public DateTime CreatedAt { get; set; }
 
-    public IdentityAddress? ForIdentity { get; set; }
+    public IdentityAddress? ForIdentity { get; private set; }
     public byte[]? Password { get; set; }
 
     public List<RelationshipTemplateAllocation> Allocations { get; set; } = [];
@@ -69,6 +69,15 @@ public class RelationshipTemplate : Entity
         Allocations.Add(new RelationshipTemplateAllocation(Id, identity, device));
     }
 
+    public void AnonymizeForIdentity(string didDomainName)
+    {
+        EnsureIsPersonalized();
+
+        var anonymousIdentity = IdentityAddress.GetAnonymized(didDomainName);
+
+        ForIdentity = anonymousIdentity;
+    }
+
     public bool IsAllocatedBy(IdentityAddress identity)
     {
         return Allocations.Any(x => x.AllocatedBy == identity);
@@ -85,6 +94,11 @@ public class RelationshipTemplate : Entity
     public void EnsureCanBeDeletedBy(IdentityAddress identityAddress)
     {
         if (CreatedBy != identityAddress) throw new DomainActionForbiddenException();
+    }
+
+    public void EnsureIsPersonalized()
+    {
+        if (ForIdentity == null) throw new DomainException(DomainErrors.RelationshipTemplateNotPersonalized());
     }
 
     #region Expressions
@@ -115,6 +129,11 @@ public class RelationshipTemplate : Entity
             relationshipTemplate.CreatedBy == activeIdentity || // The owner shouldn't need a password to get the template
             relationshipTemplate.Allocations.Any(a =>
                 a.AllocatedBy == activeIdentity); // if the template has already been allocated by the active identity, it doesn't need to pass the password again;
+    }
+
+    public static Expression<Func<RelationshipTemplate, bool>> IsFor(IdentityAddress identityAddress)
+    {
+        return relationshipTemplate => relationshipTemplate.ForIdentity == identityAddress;
     }
 
     #endregion

Modules/Relationships/src/Relationships.Domain/DomainErrors.cs
@@ -82,4 +82,10 @@ public static class DomainErrors
         return new DomainError("error.platform.validation.relationship.relationshipTemplateNotAllocated",
             "The relationship template has to be allocated before it can be used to establish a relationship. Send a GET request to the /RelationshipTemplates/{id} endpoint to allocate the template.");
     }
+
+    public static DomainError RelationshipTemplateNotPersonalized()
+    {
+        return new DomainError("error.platform.validation.relationship.relationshipTemplateNotPersonalized",
+            "The relationship template has to be personalized.");
+    }
 }

Modules/Relationships/src/Relationships.Infrastructure/Persistence/Database/Repository/RelationshipTemplatesRepository.cs
@@ -79,10 +79,21 @@ public class RelationshipTemplatesRepository : IRelationshipTemplatesRepository
         return templates;
     }
 
+    public async Task<IEnumerable<RelationshipTemplate>> FindTemplates(Expression<Func<RelationshipTemplate, bool>> filter, CancellationToken cancellationToken)
+    {
+        return await _templates.Where(filter).ToListAsync(cancellationToken);
+    }
+
     public async Task Update(RelationshipTemplate template)
     {
         _templates.Update(template);
         await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task Update(IEnumerable<RelationshipTemplate> templates, CancellationToken cancellationToken)
+    {
+        _templates.UpdateRange(templates);
+        await _dbContext.SaveChangesAsync(cancellationToken);
     }
 
     public async Task<IEnumerable<RelationshipTemplateAllocation>> FindRelationshipTemplateAllocations(Expression<Func<RelationshipTemplateAllocation, bool>> filter,

Modules/Tokens/src/Tokens.Application/ApplicationOptions.cs
@@ -6,6 +6,11 @@ public class ApplicationOptions
 {
     [Required]
     public PaginationOptions Pagination { get; set; } = new();
+
+    [Required]
+    [MinLength(3)]
+    [MaxLength(45)]
+    public string DidDomainName { get; set; } = null!;
 }
 
 public class PaginationOptions

Modules/Tokens/src/Tokens.Application/Identities/IdentityDeleter.cs
@@ -1,5 +1,6 @@
 ﻿using Backbone.BuildingBlocks.Application.Identities;
 using Backbone.DevelopmentKit.Identity.ValueObjects;
+using Backbone.Modules.Tokens.Application.Tokens.Commands.AnonymizeTokensForIdentity;
 using Backbone.Modules.Tokens.Application.Tokens.Commands.DeleteTokensOfIdentity;
 using MediatR;
 
@@ -19,6 +20,7 @@ public class IdentityDeleter : IIdentityDeleter
     public async Task Delete(IdentityAddress identityAddress)
     {
         await _mediator.Send(new DeleteTokensOfIdentityCommand(identityAddress));
+        await _mediator.Send(new AnonymizeTokensForIdentityCommand(identityAddress));
         await _deletionProcessLogger.LogDeletion(identityAddress, "Tokens");
     }
 }

Modules/Tokens/src/Tokens.Application/Infrastructure/Persistence/Repository/ITokensRepository.cs
@@ -14,7 +14,10 @@ public interface ITokensRepository
     Task<DbPaginationResult<Token>> FindTokens(IEnumerable<ListTokensQueryItem> queryItems, IdentityAddress activeIdentity, PaginationFilter paginationFilter,
         CancellationToken cancellationToken, bool track = false);
 
+    Task<IEnumerable<Token>> FindTokens(Expression<Func<Token, bool>> filter, CancellationToken cancellationToken, bool track = false);
+
     Task<Token?> Find(TokenId tokenId, IdentityAddress? activeIdentity, CancellationToken cancellationToken, bool track = false);
+    Task Update(IEnumerable<Token> tokens, CancellationToken cancellationToken);
     Task DeleteTokens(Expression<Func<Token, bool>> filter, CancellationToken cancellationToken);
     Task DeleteToken(Token token, CancellationToken cancellationToken);
 }

Modules/Tokens/src/Tokens.Application/Tokens/Commands/AnonymizeTokensForIdentity/AnonymizeTokensForIdentityCommand.cs
@@ -1,0 +1,13 @@
+﻿using MediatR;
+
+namespace Backbone.Modules.Tokens.Application.Tokens.Commands.AnonymizeTokensForIdentity;
+
+public class AnonymizeTokensForIdentityCommand : IRequest
+{
+    public AnonymizeTokensForIdentityCommand(string identityAddress)
+    {
+        IdentityAddress = identityAddress;
+    }
+
+    public string IdentityAddress { get; set; }
+}

Modules/Tokens/src/Tokens.Application/Tokens/Commands/AnonymizeTokensForIdentity/Handler.cs
@@ -1,0 +1,29 @@
+﻿using Backbone.DevelopmentKit.Identity.ValueObjects;
+using Backbone.Modules.Tokens.Application.Infrastructure.Persistence.Repository;
+using Backbone.Modules.Tokens.Domain.Entities;
+using MediatR;
+using Microsoft.Extensions.Options;
+
+namespace Backbone.Modules.Tokens.Application.Tokens.Commands.AnonymizeTokensForIdentity;
+
+public class Handler : IRequestHandler<AnonymizeTokensForIdentityCommand>
+{
+    private readonly ITokensRepository _tokensRepository;
+    private readonly ApplicationOptions _applicationOptions;
+
+    public Handler(ITokensRepository tokensRepository, IOptions<ApplicationOptions> applicationOptions)
+    {
+        _tokensRepository = tokensRepository;
+        _applicationOptions = applicationOptions.Value;
+    }
+
+    public async Task Handle(AnonymizeTokensForIdentityCommand request, CancellationToken cancellationToken)
+    {
+        var tokens = (await _tokensRepository.FindTokens(Token.IsFor(IdentityAddress.Parse(request.IdentityAddress)), cancellationToken)).ToList();
+
+        foreach (var token in tokens)
+            token.AnonymizeForIdentity(_applicationOptions.DidDomainName);
+
+        await _tokensRepository.Update(tokens, cancellationToken);
+    }
+}

Modules/Tokens/src/Tokens.Application/Tokens/Commands/AnonymizeTokensForIdentity/Validator.cs
@@ -1,0 +1,14 @@
+﻿using Backbone.BuildingBlocks.Application.Extensions;
+using Backbone.DevelopmentKit.Identity.ValueObjects;
+using FluentValidation;
+
+namespace Backbone.Modules.Tokens.Application.Tokens.Commands.AnonymizeTokensForIdentity;
+
+public class Validator : AbstractValidator<AnonymizeTokensForIdentityCommand>
+{
+    public Validator()
+    {
+        RuleFor(c => c.IdentityAddress)
+            .ValidId<AnonymizeTokensForIdentityCommand, IdentityAddress>();
+    }
+}

Modules/Tokens/src/Tokens.Domain/DomainErrors.cs
@@ -1,0 +1,12 @@
+﻿using Backbone.BuildingBlocks.Domain.Errors;
+
+namespace Backbone.Modules.Tokens.Domain;
+
+public class DomainErrors
+{
+    public static DomainError TokenNotPersonalized()
+    {
+        return new DomainError("error.platform.validation.token.tokenNotPersonalized",
+            "The token has to be personalized.");
+    }
+}

Modules/Tokens/src/Tokens.Domain/Entities/Token.cs
@@ -43,7 +43,7 @@ public class Token : Entity
     public IdentityAddress CreatedBy { get; set; }
     public DeviceId CreatedByDevice { get; set; }
 
-    public IdentityAddress? ForIdentity { get; set; }
+    public IdentityAddress? ForIdentity { get; private set; }
     public byte[]? Password { get; set; }
 
     public byte[] Content { get; private set; }
@@ -58,9 +58,23 @@ public class Token : Entity
             CreatedBy == address; // The owner shouldn't need a password to get the template
     }
 
+    public void AnonymizeForIdentity(string didDomainName)
+    {
+        EnsureIsPersonalized();
+
+        var anonymousIdentity = IdentityAddress.GetAnonymized(didDomainName);
+
+        ForIdentity = anonymousIdentity;
+    }
+
     public void EnsureCanBeDeletedBy(IdentityAddress identityAddress)
     {
         if (CreatedBy != identityAddress) throw new DomainActionForbiddenException();
+    }
+
+    public void EnsureIsPersonalized()
+    {
+        if (ForIdentity == null) throw new DomainException(DomainErrors.TokenNotPersonalized());
     }
 
     #region Expressions
@@ -89,6 +103,11 @@ public class Token : Entity
             token.Password == null ||
             token.Password == password ||
             token.CreatedBy == address; // The owner shouldn't need a password to get the template
+    }
+
+    public static Expression<Func<Token, bool>> IsFor(IdentityAddress identityAddress)
+    {
+        return token => token.ForIdentity == identityAddress;
     }
 
     #endregion

Modules/Tokens/src/Tokens.Infrastructure/Persistence/Repository/TokensRepository.cs
@@ -48,6 +48,13 @@ public class TokensRepository : ITokensRepository
         return templates;
     }
 
+    public async Task<IEnumerable<Token>> FindTokens(Expression<Func<Token, bool>> filter, CancellationToken cancellationToken, bool track = false)
+    {
+        return await (track ? _tokensDbSet : _readonlyTokensDbSet)
+            .Where(filter)
+            .ToListAsync(cancellationToken);
+    }
+
     public async Task<Token?> Find(TokenId id, IdentityAddress? activeIdentity, CancellationToken cancellationToken, bool track = false)
     {
         var token = await _readonlyTokensDbSet
@@ -84,6 +91,12 @@ public class TokensRepository : ITokensRepository
     {
         await _tokensDbSet.AddAsync(token);
         await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task Update(IEnumerable<Token> tokens, CancellationToken cancellationToken)
+    {
+        _tokensDbSet.UpdateRange(tokens);
+        await _dbContext.SaveChangesAsync(cancellationToken);
     }
 
     public async Task DeleteTokens(Expression<Func<Token, bool>> filter, CancellationToken cancellationToken)

Modules/Relationships/src/Relationships.Application/Identities/IdentityDeleter.Tests.cs
@@ -1,6 +1,7 @@
 ﻿using Backbone.BuildingBlocks.Application.Identities;
 using Backbone.Modules.Relationships.Application.Relationships.Commands.DecomposeAndAnonymizeRelationshipsOfIdentity;
 using Backbone.Modules.Relationships.Application.RelationshipTemplates.Commands.AnonymizeRelationshipTemplateAllocationsAllocatedByIdentity;
+using Backbone.Modules.Relationships.Application.RelationshipTemplates.Commands.AnonymizeRelationshipTemplatesForIdentity;
 using Backbone.Modules.Relationships.Application.RelationshipTemplates.Commands.DeleteRelationshipTemplatesOfIdentity;
 using FakeItEasy;
 using MediatR;
@@ -27,6 +28,9 @@ public class IdentityDeleterTests : AbstractTestsBase
             A<CancellationToken>._)).MustHaveHappenedOnceExactly();
         A.CallTo(() => mockMediator.Send(
             A<DeleteRelationshipTemplatesOfIdentityCommand>.That.Matches(i => i.IdentityAddress == identityAddress),
+            A<CancellationToken>._)).MustHaveHappenedOnceExactly();
+        A.CallTo(() => mockMediator.Send(
+            A<AnonymizeRelationshipTemplatesForIdentityCommand>.That.Matches(i => i.IdentityAddress == identityAddress),
             A<CancellationToken>._)).MustHaveHappenedOnceExactly();
         A.CallTo(() => mockMediator.Send(
             A<AnonymizeRelationshipTemplateAllocationsAllocatedByIdentityCommand>.That.Matches(i => i.IdentityAddress == identityAddress),

Modules/Relationships/src/Relationships.Domain/Aggregates/RelationshipTemplates/RelationshipTemplate.AnonymizeForIdentityTests.cs
@@ -1,0 +1,43 @@
+﻿using Backbone.BuildingBlocks.Domain.Exceptions;
+using Backbone.DevelopmentKit.Identity.ValueObjects;
+using Backbone.UnitTestTools.Extensions;
+
+namespace Backbone.Modules.Relationships.Domain.Aggregates.RelationshipTemplates;
+
+public class RelationshipTemplateAnonymizeForIdentityTests : AbstractTestsBase
+{
+    private const string DID_DOMAIN_NAME = "localhost";
+
+    [Fact]
+    public void Personalized_template_can_be_anonymized()
+    {
+        // Arrange
+        var creatorIdentityAddress = CreateRandomIdentityAddress();
+        var forIdentityAddress = CreateRandomIdentityAddress();
+        var deviceId = CreateRandomDeviceId();
+        byte[] content = [1, 1, 1, 1, 1, 1, 1, 1];
+        var relationshipTemplate = new RelationshipTemplate(creatorIdentityAddress, deviceId, null, null, content, forIdentityAddress);
+
+        // Act
+        relationshipTemplate.AnonymizeForIdentity(DID_DOMAIN_NAME);
+
+        // Assert
+        relationshipTemplate.ForIdentity.Should().Be(IdentityAddress.GetAnonymized(DID_DOMAIN_NAME));
+    }
+
+    [Fact]
+    public void Non_personalized_template_can_not_be_anonymized()
+    {
+        // Arrange
+        var creatorIdentityAddress = CreateRandomIdentityAddress();
+        var deviceId = CreateRandomDeviceId();
+        byte[] content = [1, 1, 1, 1, 1, 1, 1, 1];
+        var relationshipTemplate = new RelationshipTemplate(creatorIdentityAddress, deviceId, null, _dateTimeNow, content);
+
+        // Act
+        var acting = () => relationshipTemplate.AnonymizeForIdentity(DID_DOMAIN_NAME);
+
+        // Assert
+        acting.Should().Throw<DomainException>().WithError("error.platform.validation.relationship.relationshipTemplateNotPersonalized");
+    }
+}

Modules/Tokens/test/Tokens.Domain.Tests/Tests/Token.AnonymizeForIdentityTests.cs
@@ -1,0 +1,46 @@
+﻿using Backbone.BuildingBlocks.Domain.Exceptions;
+using Backbone.DevelopmentKit.Identity.ValueObjects;
+using Backbone.Modules.Tokens.Domain.Entities;
+using Backbone.UnitTestTools.Extensions;
+
+namespace Backbone.Modules.Tokens.Domain.Tests.Tests;
+
+public class TokenAnonymizeForIdentityTests : AbstractTestsBase
+{
+    private const string DID_DOMAIN_NAME = "localhost";
+
+    [Fact]
+    public void Personalized_token_can_be_anonymized()
+    {
+        // Arrange
+        var creatorIdentityAddress = CreateRandomIdentityAddress();
+        var forIdentityAddress = CreateRandomIdentityAddress();
+        var deviceId = CreateRandomDeviceId();
+        byte[] content = [1, 1, 1, 1, 1, 1, 1, 1];
+        var expiresAt = _dateTimeTomorrow;
+        var relationshipTemplate = new Token(creatorIdentityAddress, deviceId, content, expiresAt, forIdentityAddress);
+
+        // Act
+        relationshipTemplate.AnonymizeForIdentity(DID_DOMAIN_NAME);
+
+        // Assert
+        relationshipTemplate.ForIdentity.Should().Be(IdentityAddress.GetAnonymized(DID_DOMAIN_NAME));
+    }
+
+    [Fact]
+    public void Non_personalized_token_can_not_be_anonymized()
+    {
+        // Arrange
+        var creatorIdentityAddress = CreateRandomIdentityAddress();
+        var deviceId = CreateRandomDeviceId();
+        byte[] content = [1, 1, 1, 1, 1, 1, 1, 1];
+        var expiresAt = _dateTimeTomorrow;
+        var relationshipTemplate = new Token(creatorIdentityAddress, deviceId, content, expiresAt);
+
+        // Act
+        var acting = () => relationshipTemplate.AnonymizeForIdentity(DID_DOMAIN_NAME);
+
+        // Assert
+        acting.Should().Throw<DomainException>().WithError("error.platform.validation.token.tokenNotPersonalized");
+    }
+}