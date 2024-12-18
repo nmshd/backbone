9c9a583a4d773bd5837fb025c61454901a5ff9ee
Mika Herrmann <mika.herrmann@js-soft.com>
2024-12-18 13:24:25 +0100

    feat: Add migration to add allocations and locking to db

---
+++ Modules/Tokens/src/Tokens.Infrastructure/Persistence/Database/EntityConfigurations/TokenAllocationEntityTypeConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Backbone.BuildingBlocks.Infrastructure.Persistence.Database.EntityTypeConfigurations;
+using Backbone.Modules.Tokens.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Backbone.Modules.Tokens.Infrastructure.Persistence.Database.EntityConfigurations;
+
+public class TokenAllocationEntityTypeConfiguration : EntityEntityTypeConfiguration<TokenAllocation>
+{
+    public override void Configure(EntityTypeBuilder<TokenAllocation> builder)
+    {
+        base.Configure(builder);
+
+        builder.ToTable("TokenAllocations");
+        builder.HasKey(x => new { x.TokenId, x.AllocatedBy });
+
+        builder.Property(x => x.AllocatedAt);
+        builder.Property(x => x.AllocatedByDevice);
+    }
+}
