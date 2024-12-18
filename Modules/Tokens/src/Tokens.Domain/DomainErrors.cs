500afb82858ae463d501989da263710c5ad7c0b7
Mika Herrmann <mika.herrmann@js-soft.com>
2024-12-18 13:22:21 +0100

    feat: Add domain logic for allocations and logic

--- Modules/Tokens/src/Tokens.Domain/DomainErrors.cs
+++ Modules/Tokens/src/Tokens.Domain/DomainErrors.cs
@@ -9,4 +9,14 @@ public class DomainErrors
         return new DomainError("error.platform.validation.token.tokenNotPersonalized",
             "The token has to be personalized.");
     }
+
+    public static DomainError NoAllocationForOwner()
+    {
+        return new DomainError("error.platform.validation.token.noAllocationForOwner", "The owner of the token cannot be allocated.");
+    }
+
+    public static DomainError AlreadyAllocated()
+    {
+        return new DomainError("error.platform.validation.token.alreadyAllocated", "The identity already has an allocation for this token.");
+    }
 }
