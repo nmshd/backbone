4cdda3bc809a8f94034b05cb3b78513508640ce5
Eric Brunner <erich.brunner@live.at>
2025-01-14 12:01:36 +0100

    feat: return recipients in admin-api on get request

--- Modules/Announcements/src/Announcements.Application/Announcements/DTOs/AnnouncementDTO.cs
+++ Modules/Announcements/src/Announcements.Application/Announcements/DTOs/AnnouncementDTO.cs
@@ -11,6 +11,7 @@ namespace Backbone.Modules.Announcements.Application.Announcements.DTOs
             ExpiresAt = announcement.ExpiresAt;
             Severity = announcement.Severity;
             Texts = announcement.Texts.Select(t => new AnnouncementTextDTO(t));
+            Recipients = announcement.Recipients.Select(r => new AnnouncementRecipientDTO(r));
         }
 
         public string Id { get; set; }
@@ -18,5 +19,18 @@ namespace Backbone.Modules.Announcements.Application.Announcements.DTOs
         public DateTime? ExpiresAt { get; set; }
         public AnnouncementSeverity Severity { get; set; }
         public IEnumerable<AnnouncementTextDTO> Texts { get; set; }
+        public IEnumerable<AnnouncementRecipientDTO> Recipients { get; set; }
+    }
+
+    public record AnnouncementRecipientDTO
+    {
+        public AnnouncementRecipientDTO(AnnouncementRecipient announcementRecipient)
+        {
+            DeviceId = announcementRecipient.DeviceId;
+            Address = announcementRecipient.Address;
+        }
+
+        public string DeviceId { get; set; }
+        public string Address { get; set; }
     }
 }
