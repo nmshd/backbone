18155289ca01f9b3a8f26bcb0c2f19c6b144d32a
Eric Brunner <erich.brunner@live.at>
2025-01-23 13:11:49 +0100

    feat: added read text loop from cli

--- Applications/AdminCli/src/AdminCli/Commands/Announcements/AnnouncementCommand.cs
+++ Applications/AdminCli/src/AdminCli/Commands/Announcements/AnnouncementCommand.cs
@@ -34,37 +34,99 @@ public class SendAnnouncementCommand : AdminCliDbCommand
         AddOption(expiresAt);
         AddOption(severity);
 
-
         this.SetHandler(SendAnnouncement, DB_PROVIDER_OPTION, DB_CONNECTION_STRING_OPTION, severity, expiresAt);
     }
 
     private async Task SendAnnouncement(string dbProvider, string dbConnectionString, string? severityInput, string? expiresAtInput)
     {
-        var severity = severityInput switch
+        try
         {
-            _ when string.IsNullOrWhiteSpace(severityInput) => AnnouncementSeverity.Low,
-            _ when Enum.TryParse<AnnouncementSeverity>(severityInput, ignoreCase: true, out var parsedSeverity) => parsedSeverity,
-            _ => throw new ArgumentException($@"Specified severity '{severityInput}' is not a valid severity.")
-        };
+            var severity = severityInput switch
+            {
+                _ when string.IsNullOrWhiteSpace(severityInput) => AnnouncementSeverity.Low,
+                _ when Enum.TryParse<AnnouncementSeverity>(severityInput, ignoreCase: true, out var parsedSeverity) => parsedSeverity,
+                _ => throw new ArgumentException($@"Specified severity '{severityInput}' is not a valid severity.")
+            };
 
-        DateTime? expiresAt = expiresAtInput switch
+            DateTime? expiresAt = expiresAtInput switch
+            {
+                _ when string.IsNullOrWhiteSpace(expiresAtInput) => null,
+                _ when DateTime.TryParse(expiresAtInput, out var parsedDateTime) => parsedDateTime,
+                _ => throw new ArgumentException($@"Specified expiration datetime '{expiresAtInput}' is not a valid DateTime.")
+            };
+
+            var texts = ReadTextsFromCommandLineInput();
+
+            if (texts.Count == 0)
+            {
+                Console.WriteLine(@"No texts provided. Exiting...");
+                return;
+            }
+
+            var mediator = _serviceLocator.GetService<IMediator>(dbProvider, dbConnectionString);
+
+            var response = await mediator.Send(new CreateAnnouncementCommand
+            {
+                Texts = texts,
+                Severity = severity,
+                ExpiresAt = expiresAt
+            }, CancellationToken.None);
+
+            Console.WriteLine(@"Announcement sent successfully");
+            Console.WriteLine(JsonSerializer.Serialize(response, JSON_SERIALIZER_OPTIONS));
+        }
+        catch (Exception e)
         {
-            _ when string.IsNullOrWhiteSpace(expiresAtInput) => null,
-            _ when DateTime.TryParse(expiresAtInput, out var parsedDateTime) => parsedDateTime,
-            _ => throw new ArgumentException($@"Specified expiration datetime '{expiresAtInput}' is not a valid DateTime.")
-        };
+            Console.WriteLine($@"An error occurred: {e.Message}");
+        }
+    }
 
-        var mediator = _serviceLocator.GetService<IMediator>(dbProvider, dbConnectionString);
-
-        var response = await mediator.Send(new CreateAnnouncementCommand
+    private static List<CreateAnnouncementCommandText> ReadTextsFromCommandLineInput()
+    {
+        var texts = new List<CreateAnnouncementCommandText>();
+        bool addAnotherLanguage;
+        do
         {
-            Texts = null!,
-            Severity = severity,
-            ExpiresAt = expiresAt
-        }, CancellationToken.None);
+            var language = PromptForInput(@"Enter language (e.g. en, de, it, nl). At least 1 must be in english: ");
+            var title = PromptForInput(@"Enter title: ");
+            var body = PromptForInput(@"Enter body: ");
 
-        Console.WriteLine(@"Announcement sent successfully");
+            if (language == null || title == null || body == null)
+            {
+                break;
+            }
 
-        Console.WriteLine(JsonSerializer.Serialize(response, JSON_SERIALIZER_OPTIONS));
+            texts.Add(new CreateAnnouncementCommandText
+            {
+                Language = language,
+                Title = title,
+                Body = body
+            });
+
+            var input = PromptForInput(@"Do you want to add another language? ([y]es/[n]o): ");
+            addAnotherLanguage = input?.Trim().ToLower() is "yes" or "y";
+        } while (addAnotherLanguage);
+
+        return texts;
+    }
+
+    private static string? PromptForInput(string prompt)
+    {
+        Console.Write(prompt);
+        var input = Console.ReadLine();
+
+        while (string.IsNullOrWhiteSpace(input))
+        {
+            Console.WriteLine($@"Input cannot be empty. Press x to exit.");
+            Console.Write(prompt);
+            input = Console.ReadLine();
+
+            if (input == null || !input.Trim().Equals("x", StringComparison.CurrentCultureIgnoreCase)) continue;
+
+            input = null;
+            break;
+        }
+
+        return input;
     }
 }
