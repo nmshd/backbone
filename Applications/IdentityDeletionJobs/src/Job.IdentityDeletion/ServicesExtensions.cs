9f18a457065112d52b3ee39c8ec892169c52e56b
Timo Notheisen <timo.notheisen@js-soft.com>
2025-01-27 11:25:49 +0100

    chore: cleanup

--- Applications/IdentityDeletionJobs/src/Job.IdentityDeletion/ServicesExtensions.cs
+++ Applications/IdentityDeletionJobs/src/Job.IdentityDeletion/ServicesExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using Backbone.BuildingBlocks.Application.Identities;
-using Backbone.Modules.Challenges.Application.Identities;
 
 namespace Backbone.Job.IdentityDeletion;
 
@@ -7,7 +6,7 @@ public static class ServicesExtensions
 {
     public static IServiceCollection RegisterIdentityDeleters(this IServiceCollection services)
     {
-        services.AddTransient<IIdentityDeleter, IdentityDeleter>();
+        services.AddTransient<IIdentityDeleter, Modules.Challenges.Application.Identities.IdentityDeleter>();
         services.AddTransient<IIdentityDeleter, Modules.Devices.Application.Identities.IdentityDeleter>();
         services.AddTransient<IIdentityDeleter, Modules.Files.Application.Identities.IdentityDeleter>();
         services.AddTransient<IIdentityDeleter, Modules.Messages.Application.Identities.IdentityDeleter>();
