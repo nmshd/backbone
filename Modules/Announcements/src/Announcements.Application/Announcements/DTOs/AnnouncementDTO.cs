088ad5f8bec88a2fc2d52aa2cc1df9c316e99524
Timo Notheisen <timo.notheisen@js-soft.com>
2025-01-28 12:08:43 +0100

    fix: return simple string instead of an object in AnnouncementDTO.cs

--- Modules/Announcements/src/Announcements.Application/Announcements/DTOs/AnnouncementDTO.cs
+++ Modules/Announcements/src/Announcements.Application/Announcements/DTOs/AnnouncementDTO.cs
@@ -11,7 +11,7 @@ namespace Backbone.Modules.Announcements.Application.Announcements.DTOs
             ExpiresAt = announcement.ExpiresAt;
             Severity = announcement.Severity;
             Texts = announcement.Texts.Select(t => new AnnouncementTextDTO(t));
-            Recipients = announcement.Recipients.Select(r => new AnnouncementRecipientDTO(r));
+            Recipients = announcement.Recipients.Select(r => r.Address.ToString());
         }
 
         public string Id { get; set; }
@@ -19,16 +19,6 @@ namespace Backbone.Modules.Announcements.Application.Announcements.DTOs
         public DateTime? ExpiresAt { get; set; }
         public AnnouncementSeverity Severity { get; set; }
         public IEnumerable<AnnouncementTextDTO> Texts { get; set; }
-        public IEnumerable<AnnouncementRecipientDTO> Recipients { get; set; }
-    }
-
-    public record AnnouncementRecipientDTO
-    {
-        public AnnouncementRecipientDTO(AnnouncementRecipient announcementRecipient)
-        {
-            Address = announcementRecipient.Address;
-        }
-
-        public string Address { get; set; }
+        public IEnumerable<string> Recipients { get; set; }
     }
 }
