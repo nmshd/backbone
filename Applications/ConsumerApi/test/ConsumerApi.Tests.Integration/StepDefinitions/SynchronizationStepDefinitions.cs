8a0616a6b7c8253aa61d534eadf7ede7f57ea7f9
Timo Notheisen <timo.notheisen@js-soft.com>
2025-01-28 08:47:49 +0100

    test: remove redundant step definitions

--- Applications/ConsumerApi/test/ConsumerApi.Tests.Integration/StepDefinitions/SynchronizationStepDefinitions.cs
+++ Applications/ConsumerApi/test/ConsumerApi.Tests.Integration/StepDefinitions/SynchronizationStepDefinitions.cs
@@ -14,7 +14,6 @@ internal class SynchronizationStepDefinitions
     private readonly ResponseContext _responseContext;
     private readonly MessagesContext _messagesContext;
     private ApiResponse<ListExternalEventsResponse>? _listExternalEventsOfSyncRunResponse;
-    private ApiResponse<StartSyncRunResponse>? _startSyncRunResponse;
 
     public SynchronizationStepDefinitions(ResponseContext responseContext, MessagesContext messagesContext, ClientPool clientPool)
     {
@@ -49,14 +48,6 @@ internal class SynchronizationStepDefinitions
         _responseContext.WhenResponse = _listExternalEventsOfSyncRunResponse = await client.SyncRuns.ListExternalEventsOfSyncRun(syncRunId);
     }
 
-    [When($"{RegexFor.SINGLE_THING} sends a POST request to the /SyncRuns endpoint")]
-    public async Task WhenSendAPOSTRequestToTheSyncRunsEndpoint(string identityName)
-    {
-        var client = _clientPool.FirstForIdentityName(identityName);
-
-        _responseContext.WhenResponse = _startSyncRunResponse = await client.SyncRuns.StartSyncRun(new StartSyncRunRequest { Type = SyncRunType.ExternalEventSync }, 1);
-    }
-
     #endregion
 
     #region Then
@@ -76,12 +67,6 @@ internal class SynchronizationStepDefinitions
         _listExternalEventsOfSyncRunResponse.Result.Should().ContainSingle(e => e.Type == "MessageReceived");
         var messageReceivedExternalEvent = _listExternalEventsOfSyncRunResponse.Result!.Single(e => e.Type == "MessageReceived");
         messageReceivedExternalEvent.Payload["id"].GetString().Should().Be(message.Id);
-    }
-
-    [Then($"the response says that there are no new external events")]
-    public void ThenTheResponseSaysThatThereAreNoNewExternalEvents()
-    {
-        _startSyncRunResponse!.Result!.Status.Should().Be("NoNewEvents");
     }
 
     #endregion
