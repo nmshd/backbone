fa42f84a70d988f86ae76fcaf62f5d3e49bfa2e9
Timo Notheisen <timo.notheisen@js-soft.com>
2025-01-29 12:46:59 +0100

    ci: more debug logs

--- Modules/Relationships/src/Relationships.Application/Relationships/Commands/DecomposeAndAnonymizeRelationshipsOfIdentity/Handler.cs
+++ Modules/Relationships/src/Relationships.Application/Relationships/Commands/DecomposeAndAnonymizeRelationshipsOfIdentity/Handler.cs
@@ -1,6 +1,7 @@
 using Backbone.Modules.Relationships.Application.Infrastructure.Persistence.Repository;
 using Backbone.Modules.Relationships.Domain.Aggregates.Relationships;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Backbone.Modules.Relationships.Application.Relationships.Commands.DecomposeAndAnonymizeRelationshipsOfIdentity;
@@ -8,11 +9,13 @@ namespace Backbone.Modules.Relationships.Application.Relationships.Commands.Deco
 public class Handler : IRequestHandler<DecomposeAndAnonymizeRelationshipsOfIdentityCommand>
 {
     private readonly IRelationshipsRepository _relationshipsRepository;
+    private readonly ILogger<Handler> _logger;
     private readonly ApplicationOptions _applicationOptions;
 
-    public Handler(IRelationshipsRepository relationshipsRepository, IOptions<ApplicationOptions> applicationOptions)
+    public Handler(IRelationshipsRepository relationshipsRepository, IOptions<ApplicationOptions> applicationOptions, ILogger<Handler> logger)
     {
         _relationshipsRepository = relationshipsRepository;
+        _logger = logger;
         _applicationOptions = applicationOptions.Value;
     }
 
@@ -20,8 +23,12 @@ public class Handler : IRequestHandler<DecomposeAndAnonymizeRelationshipsOfIdent
     {
         var relationships = (await _relationshipsRepository.FindRelationships(Relationship.HasParticipant(request.IdentityAddress), cancellationToken)).ToList();
 
+        _logger.LogError("Decomposing {n} relationships for identity {IdentityAddress}", relationships.Count, request.IdentityAddress);
+
         foreach (var relationship in relationships)
             relationship.DecomposeDueToIdentityDeletion(request.IdentityAddress, _applicationOptions.DidDomainName);
+
+        _logger.LogError("Done decomposing relationships for identity {IdentityAddress}", request.IdentityAddress);
 
         await _relationshipsRepository.Update(relationships);
     }
