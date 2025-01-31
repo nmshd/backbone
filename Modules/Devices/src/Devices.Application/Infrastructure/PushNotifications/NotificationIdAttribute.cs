93c5f76006942336a71cab929c704cbe786b6763
Timo Notheisen <timo.notheisen@js-soft.com>
2025-01-31 12:36:44 +0100

    chore: always throw exception if value of NotificationIdAttribute is too long (not only when DEBUG)

--- Modules/Devices/src/Devices.Application/Infrastructure/PushNotifications/NotificationIdAttribute.cs
+++ Modules/Devices/src/Devices.Application/Infrastructure/PushNotifications/NotificationIdAttribute.cs
@@ -4,10 +4,8 @@ public class NotificationIdAttribute : Attribute
 {
     public NotificationIdAttribute(string value)
     {
-#if DEBUG
         if (value.Length > 64)
             throw new Exception("The value of the NotificationIdAttribute must be less than or equal to 64 characters.");
-#endif
 
         Value = value;
     }
