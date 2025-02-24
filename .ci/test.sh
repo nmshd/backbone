c74d254c043a20392b8fc6b32f37c1ba6766cf92
Mika Herrmann <mika.herrmann@js-soft.com>
2025-02-24 14:37:29 +0100

    test: Print more directories

--- .ci/test.sh
+++ .ci/test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Shell"
+pwd
+
 dotnet restore /p:ContinuousIntegrationBuild=true "Backbone.sln"
 dotnet build /p:ContinuousIntegrationBuild=true --no-restore "Backbone.sln"
 dotnet test /p:ContinuousIntegrationBuild=true --no-restore --no-build --filter "Category!=Integration" --logger "GitHubActions;summary.includeNotFoundTests=false" "Backbone.sln"
