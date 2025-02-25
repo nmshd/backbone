a96f163dd5341b30aa71c54a264a213c42a98376
Mika Herrmann <mika.herrmann@js-soft.com>
2025-02-25 13:09:15 +0100

    chore: Remove test switches

--- .ci/test.sh
+++ .ci/test.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "Shell"
-pwd
-
 dotnet restore /p:ContinuousIntegrationBuild=true "Backbone.sln"
 dotnet build /p:ContinuousIntegrationBuild=true --no-restore "Backbone.sln"
 dotnet test /p:ContinuousIntegrationBuild=true --no-restore --no-build --filter "Category!=Integration" --logger "GitHubActions;summary.includeNotFoundTests=false" "Backbone.sln"
