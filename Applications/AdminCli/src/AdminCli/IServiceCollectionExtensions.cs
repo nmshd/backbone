11f6e60c624c2d36878d9e1928a02c1f8d3ca366
Timo Notheisen <timo.notheisen@js-soft.com>
2025-02-26 12:53:20 +0100

    chore: cleanup dependency registration

---
+++ Applications/AdminCli/src/AdminCli/IServiceCollectionExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System.CommandLine;
+using Backbone.AdminCli.Configuration;
+using Backbone.Infrastructure.EventBus;
+using Backbone.Modules.Devices.Infrastructure.PushNotifications;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Backbone.AdminCli;
+
+public static class IServiceCollectionExtensions
+{
+    public static void AddCliCommands(this IServiceCollection services)
+    {
+        var commands = typeof(Program)
+            .Assembly.GetTypes()
+            .Where(t => t.IsSubclassOf(typeof(Command)) && !t.IsAbstract);
+
+        foreach (var command in commands)
+        {
+            services.AddTransient(command);
+        }
+    }
+
+    public static void AddCommonInfrastructure(this IServiceCollection services)
+    {
+        var serviceProvider = services.BuildServiceProvider();
+#pragma warning disable ASP0000 // We retrieve the Configuration via IOptions here so that it is validated
+        var parsedConfiguration = serviceProvider.GetRequiredService<IOptions<AdminCliConfiguration>>().Value;
+#pragma warning restore ASP0000
+
+        services.AddEventBus(parsedConfiguration.Infrastructure.EventBus);
+        services.AddPushNotifications(parsedConfiguration.Modules.Devices.Infrastructure.PushNotifications);
+    }
+
+    public static IServiceCollection AddDevicesModule(this IServiceCollection services, IConfiguration configuration)
+    {
+        Modules.Devices.Application.Extensions.IServiceCollectionExtensions.AddApplication(services, configuration.GetSection("Modules:Devices:Application"));
+        Modules.Devices.Infrastructure.Persistence.IServiceCollectionExtensions.AddDatabase(services, options =>
+        {
+            options.Provider = configuration["Modules:Devices:Infrastructure:SqlDatabase:Provider"]!;
+            options.ConnectionString = configuration["Modules:Devices:Infrastructure:SqlDatabase:ConnectionString"]!;
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddAnnouncementsModule(this IServiceCollection services, IConfiguration configuration)
+    {
+        Modules.Announcements.Application.Extensions.IServiceCollectionExtensions.AddApplication(services);
+        Modules.Announcements.Infrastructure.Persistence.Database.IServiceCollectionExtensions.AddDatabase(services, options =>
+        {
+            options.Provider = configuration["Modules:Devices:Infrastructure:SqlDatabase:Provider"]!;
+            options.ConnectionString = configuration["Modules:Devices:Infrastructure:SqlDatabase:ConnectionString"]!;
+        });
+
+        return services;
+    }
+}
--- Applications/AdminCli/src/AdminCli/Program.cs
+++ Applications/AdminCli/src/AdminCli/Program.cs
@@ -3,16 +3,12 @@ using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Backbone.AdminCli.Configuration;
 using Backbone.BuildingBlocks.Application.QuotaCheck;
-using Backbone.Infrastructure.EventBus;
-using Backbone.Modules.Announcements.Application.Extensions;
 using Backbone.Modules.Devices.Domain.Entities.Identities;
 using Backbone.Modules.Devices.Infrastructure.OpenIddict;
 using Backbone.Modules.Devices.Infrastructure.Persistence.Database;
-using Backbone.Modules.Devices.Infrastructure.PushNotifications;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 using RootCommand = Backbone.AdminCli.Commands.RootCommand;
 
 namespace Backbone.AdminCli;
@@ -37,18 +33,13 @@ public class Program
     {
         var services = new ServiceCollection();
 
-        services.AddAutofac();
-
         services.AddSingleton(configuration);
+        services.ConfigureAndValidate<AdminCliConfiguration>(configuration.Bind);
 
-        var commands = typeof(Program)
-            .Assembly.GetTypes()
-            .Where(t => t.IsSubclassOf(typeof(Command)) && !t.IsAbstract);
+        services.AddAutofac();
+        services.AddLogging();
 
-        foreach (var command in commands)
-        {
-            services.AddTransient(command);
-        }
+        services.AddCliCommands();
 
         services
             .AddIdentity<ApplicationUser, IdentityRole>()
@@ -66,49 +57,14 @@ public class Program
                 options.AddApplicationStore<CustomOpenIddictEntityFrameworkCoreApplicationStore>();
             });
 
-        services.AddApplication();
-
         services.AddSingleton<IQuotaChecker, AlwaysSuccessQuotaChecker>();
-        services.AddLogging();
 
-        services.AddDevicesModule(configuration);
-        services.AddAnnouncementsModule(configuration);
-
-        services.ConfigureAndValidate<AdminCliConfiguration>(configuration.Bind);
-
-        var serviceProvider = services.BuildServiceProvider();
-#pragma warning disable ASP0000 // We retrieve the Configuration via IOptions here so that it is validated
-        var parsedConfiguration = serviceProvider.GetRequiredService<IOptions<AdminCliConfiguration>>().Value;
-#pragma warning restore ASP0000
-
-        services.AddEventBus(parsedConfiguration.Infrastructure.EventBus);
-        services.AddPushNotifications(parsedConfiguration.Modules.Devices.Infrastructure.PushNotifications);
+        services
+            .AddAnnouncementsModule(configuration)
+            .AddDevicesModule(configuration);
 
         var containerBuilder = new ContainerBuilder();
         containerBuilder.Populate(services);
         return new AutofacServiceProvider(containerBuilder.Build());
-    }
-}
-
-public static class IServiceCollectionExtensions
-{
-    public static void AddDevicesModule(this IServiceCollection services, IConfiguration configuration)
-    {
-        Modules.Devices.Application.Extensions.IServiceCollectionExtensions.AddApplication(services, configuration.GetSection("Modules:Devices:Application"));
-        Modules.Devices.Infrastructure.Persistence.IServiceCollectionExtensions.AddDatabase(services, options =>
-        {
-            options.Provider = configuration["Modules:Devices:Infrastructure:SqlDatabase:Provider"]!;
-            options.ConnectionString = configuration["Modules:Devices:Infrastructure:SqlDatabase:ConnectionString"]!;
-        });
-    }
-
-    public static void AddAnnouncementsModule(this IServiceCollection services, IConfiguration configuration)
-    {
-        Modules.Announcements.Application.Extensions.IServiceCollectionExtensions.AddApplication(services);
-        Modules.Announcements.Infrastructure.Persistence.Database.IServiceCollectionExtensions.AddDatabase(services, options =>
-        {
-            options.Provider = configuration["Modules:Devices:Infrastructure:SqlDatabase:Provider"]!;
-            options.ConnectionString = configuration["Modules:Devices:Infrastructure:SqlDatabase:ConnectionString"]!;
-        });
     }
 }
