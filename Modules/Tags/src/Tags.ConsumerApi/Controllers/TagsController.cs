374629ade96ce0f9137b42ba3fa8ef899c7b09f0
Mika Herrmann <mika.herrmann@js-soft.com>
2025-02-26 12:24:07 +0100

    feat: Support ETag and If-None-Match header

--- Modules/Tags/src/Tags.ConsumerApi/Controllers/TagsController.cs
+++ Modules/Tags/src/Tags.ConsumerApi/Controllers/TagsController.cs
@@ -1,7 +1,10 @@
-﻿using Backbone.BuildingBlocks.API.Mvc;
+﻿using System.Security.Cryptography;
+using System.Text.Json;
+using Backbone.BuildingBlocks.API.Mvc;
 using Backbone.Modules.Tags.Application.Tags.Queries.ListTags;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backbone.Modules.Tags.ConsumerApi.Controllers;
@@ -10,15 +13,27 @@ namespace Backbone.Modules.Tags.ConsumerApi.Controllers;
 [Authorize("OpenIddict.Validation.AspNetCore")]
 public class TagsController : ApiControllerBase
 {
+    private readonly MD5 _hasher;
+
     public TagsController(IMediator mediator) : base(mediator)
     {
+        _hasher = MD5.Create();
     }
 
     [HttpGet]
     [AllowAnonymous]
-    public async Task<IActionResult> ListTags(CancellationToken cancellationToken)
+    public async Task<IActionResult> ListTags([FromHeader(Name = "If-None-Match")] string? ifNoneMatch, CancellationToken cancellationToken)
     {
         var response = await _mediator.Send(new ListTagsQuery(), cancellationToken);
+        var json = JsonSerializer.SerializeToUtf8Bytes(response);
+        var responseHash = _hasher.ComputeHash(json);
+        var hashString = Convert.ToBase64String(responseHash);
+
+        Response.Headers.ETag = hashString;
+
+        if (ifNoneMatch != null && ifNoneMatch.SequenceEqual(hashString))
+            return StatusCode(StatusCodes.Status304NotModified);
+
         return Ok(response);
     }
 }
