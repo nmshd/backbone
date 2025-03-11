9f6f895c36f81fb783bdf66dd9c638e5b2a78045
Timo Notheisen <timo.notheisen@js-soft.com>
2025-03-11 12:29:27 +0100

    test: add test for EventBusGoogleCloudPubSub.GetSubscriptionName

---
+++ BuildingBlocks/test/BuildingBlocks.Infrastructure.Tests/EventBus/GoogleCloudPubSub/EventBusGoogleCloudPubSubTests.cs
@@ -0,0 +1,18 @@
+﻿using Backbone.BuildingBlocks.Infrastructure.EventBus.GoogleCloudPubSub;
+using Backbone.Modules.TestModule.Application.DomainEvents;
+
+namespace Backbone.BuildingBlocks.Infrastructure.Tests.EventBus.GoogleCloudPubSub;
+
+public class EventBusGoogleCloudPubSubTests : AbstractTestsBase
+{
+    [Fact]
+    public void GetSubscriptionName()
+    {
+        // Act
+        var subscriptionName = EventBusGoogleCloudPubSub.GetSubscriptionName<TestDomainEventHandler, TestDomainEvent>("testProject");
+
+        // Assert
+        subscriptionName.ProjectId.Should().Be("testProject");
+        subscriptionName.SubscriptionId.Should().Be("TestModule.Test");
+    }
+}
