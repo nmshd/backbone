cc83f4c1fdee00de60079183355f5d901ec9189a
Timo Notheisen <timo.notheisen@js-soft.com>
2025-03-14 07:30:13 +0100

    feat: add command to Admin CLI

--- Applications/AdminCli/src/AdminCli/Commands/RootCommand.cs
+++ Applications/AdminCli/src/AdminCli/Commands/RootCommand.cs
@@ -1,15 +1,17 @@
 using Backbone.AdminCli.Commands.Announcements;
 using Backbone.AdminCli.Commands.Clients;
 using Backbone.AdminCli.Commands.Tiers;
+using Backbone.AdminCli.Commands.Tokens;
 
 namespace Backbone.AdminCli.Commands;
 
 public class RootCommand : System.CommandLine.RootCommand
 {
-    public RootCommand(ClientCommand clientCommand, TierCommand tierCommand, AnnouncementCommand announcementCommand)
+    public RootCommand(ClientCommand clientCommand, TierCommand tierCommand, AnnouncementCommand announcementCommand, TokenCommand tokenCommand)
     {
         AddCommand(clientCommand);
         AddCommand(tierCommand);
         AddCommand(announcementCommand);
+        AddCommand(tokenCommand);
     }
 }
