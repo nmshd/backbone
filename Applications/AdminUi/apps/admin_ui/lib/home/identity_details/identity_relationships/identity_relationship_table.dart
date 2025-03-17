bc43f2db69f201b94463508f07b63aadf3df1999
Julian König <33655937+jkoenig134@users.noreply.github.com>
2025-03-17 12:19:05 +0100

    Pinned colors in color scheme generation cause too little contrast on some places (#1083)
    * chore: add enmeshed_ui_kit
    
    * feat: update color schemes
    
    * refactor: use custom colors from enmeshed_ui_kit
    
    * chore: remove extension thats included in the ui kit
    
    * refactor: use gaps from ui kit

--- Applications/AdminUi/apps/admin_ui/lib/home/identity_details/identity_relationships/identity_relationship_table.dart
+++ Applications/AdminUi/apps/admin_ui/lib/home/identity_details/identity_relationships/identity_relationship_table.dart
@@ -1,8 +1,8 @@
 import 'package:admin_ui/core/extensions.dart';
 import 'package:data_table_2/data_table_2.dart';
+import 'package:enmeshed_ui_kit/enmeshed_ui_kit.dart';
 import 'package:flutter/material.dart';
 
-import '../../../core/constants.dart';
 import 'identity_relationship_data_table_source.dart';
 
 class IdentityRelationshipTable extends StatefulWidget {
