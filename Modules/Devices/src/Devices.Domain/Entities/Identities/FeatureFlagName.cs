b9e7d834f91abd16dc74138a84b37731ce76059a
Jakob Erben <jakob.erben@js-soft.com>
2025-03-20 10:06:54 +0100

    wip

---
+++ Modules/Devices/src/Devices.Domain/Entities/Identities/FeatureFlagName.cs
@@ -0,0 +1,16 @@
+namespace Backbone.Modules.Devices.Domain.Entities.Identities;
+
+public record FeatureFlagName
+{
+    private FeatureFlagName(string value)
+    {
+        Value = value;
+    }
+
+    public string Value { get; }
+
+    public static FeatureFlagName Parse(string value)
+    {
+        return new FeatureFlagName(value);
+    }
+}
