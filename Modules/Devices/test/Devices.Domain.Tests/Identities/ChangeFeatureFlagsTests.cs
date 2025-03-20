13b9fcb69cf6ccd019f1c17adc14810a839be2a2
Jakob Erben <jakob.erben@js-soft.com>
2025-03-20 13:27:15 +0100

    WIP

--- Modules/Devices/test/Devices.Domain.Tests/Identities/ChangeFeatureFlagsTests.cs
+++ Modules/Devices/test/Devices.Domain.Tests/Identities/ChangeFeatureFlagsTests.cs
@@ -4,7 +4,7 @@ using Backbone.Modules.Devices.Domain.Tests.FluentAssertions.Extensions;
 
 namespace Backbone.Modules.Devices.Domain.Tests.Identities;
 
-public class ChangeFeatureFlagTests : AbstractTestsBase
+public class ChangeFeatureFlagsTests : AbstractTestsBase
 {
     [Fact]
     public void Raises_FeatureFlagsOfIdentityChangedEvent()
@@ -27,22 +27,52 @@ public class ChangeFeatureFlagTests : AbstractTestsBase
         domainEvent.IdentityAddress.Should().Be(identity.Address);
     }
 
-    [Fact]
-    public void Adds_Non_Existing_Feature_Flag()
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void Adds_Non_Existing_Feature_Flag(bool isEnabled)
     {
         // Arrange
         var identity = TestDataGenerator.CreateIdentity();
         var newName = FeatureFlagName.Parse("New");
         var featureFlags = new Dictionary<FeatureFlagName, bool>
         {
-            { newName, true }
+            { newName, isEnabled }
         };
-
 
         // Act
         identity.ChangeFeatureFlags(featureFlags);
 
         // Assert
         identity.FeatureFlags.Should().Contain(newName);
+        identity.FeatureFlags.GetFeatureFlag(newName).IsEnabled.Should().Be(isEnabled);
     }
+
+    [Theory]
+    [InlineData(true,false)]
+    [InlineData(false,true)]
+    [InlineData(true,true)]
+    [InlineData(false,false)]
+    public void Changes_Existing_Feature_Flag(bool oldValue, bool newValue)
+    {
+        // Arrange
+        var identity = TestDataGenerator.CreateIdentity();
+        var name = FeatureFlagName.Parse("First");
+        var featureFlags = new Dictionary<FeatureFlagName, bool>
+        {
+            { name, oldValue }
+        };
+        identity.ChangeFeatureFlags(featureFlags);
+        var featureFlagOverride = new Dictionary<FeatureFlagName, bool>
+        {
+            { name, newValue }
+        };
+
+        // Act
+        identity.ChangeFeatureFlags(featureFlagOverride);
+
+        // Assert
+        identity.FeatureFlags.GetFeatureFlag(name).IsEnabled.Should().Be(newValue);
+    }   
+
 }
