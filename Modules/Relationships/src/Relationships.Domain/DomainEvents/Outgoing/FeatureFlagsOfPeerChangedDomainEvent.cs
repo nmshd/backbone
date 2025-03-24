a59fb361238bff58c6fa445f358079f169956b54
Timo Notheisen <timo.notheisen@js-soft.com>
2025-03-24 09:11:58 +0100

    feat: publish FeatureFlagsOfPeerChangedDomainEvent

---
+++ Modules/Relationships/src/Relationships.Domain/DomainEvents/Outgoing/FeatureFlagsOfPeerChangedDomainEvent.cs
@@ -0,0 +1,14 @@
+﻿using Backbone.BuildingBlocks.Domain.Events;
+using Backbone.DevelopmentKit.Identity.ValueObjects;
+
+namespace Backbone.Modules.Relationships.Domain.DomainEvents.Outgoing;
+
+public class FeatureFlagsOfPeerChangedDomainEvent : DomainEvent
+{
+    public FeatureFlagsOfPeerChangedDomainEvent(IdentityAddress identityAddress)
+    {
+        IdentityAddress = identityAddress.Value;
+    }
+
+    public string IdentityAddress { get; set; }
+}
