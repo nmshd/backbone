5e0673050484d047951a3cdf90bd5a5a0961d3f3
Timo Notheisen <timo.notheisen@js-soft.com>
2025-03-25 09:30:39 +0100

    fix: add migration for Relationships snapshot change

--- Modules/Relationships/src/Relationships.Infrastructure.Database.SqlServer/Migrations/20250325082958_AddReferenceFromRelationshipTemplateAllocationsToRelationshipTemplateToSnapshot.Designer.cs
+++ Modules/Relationships/src/Relationships.Infrastructure.Database.SqlServer/Migrations/20250325082958_AddReferenceFromRelationshipTemplateAllocationsToRelationshipTemplateToSnapshot.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Backbone.Modules.Relationships.Infrastructure.Database.SqlServer.Migrations
 {
     [DbContext(typeof(RelationshipsDbContext))]
-    [Migration("20241113085648_MakeCreatedByDeviceNullableInRelationshipAuditLogEntry")]
-    partial class MakeCreatedByDeviceNullableInRelationshipAuditLogEntry
+    [Migration("20250325082958_AddReferenceFromRelationshipTemplateAllocationsToRelationshipTemplateToSnapshot")]
+    partial class AddReferenceFromRelationshipTemplateAllocationsToRelationshipTemplateToSnapshot
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -21,7 +21,7 @@ namespace Backbone.Modules.Relationships.Infrastructure.Database.SqlServer.Migra
 #pragma warning disable 612, 618
             modelBuilder
                 .HasDefaultSchema("Relationships")
-                .HasAnnotation("ProductVersion", "8.0.11")
+                .HasAnnotation("ProductVersion", "9.0.3")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
@@ -221,11 +221,13 @@ namespace Backbone.Modules.Relationships.Infrastructure.Database.SqlServer.Migra
 
             modelBuilder.Entity("Backbone.Modules.Relationships.Domain.Aggregates.RelationshipTemplates.RelationshipTemplateAllocation", b =>
                 {
-                    b.HasOne("Backbone.Modules.Relationships.Domain.Aggregates.RelationshipTemplates.RelationshipTemplate", null)
+                    b.HasOne("Backbone.Modules.Relationships.Domain.Aggregates.RelationshipTemplates.RelationshipTemplate", "RelationshipTemplate")
                         .WithMany("Allocations")
                         .HasForeignKey("RelationshipTemplateId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
+
+                    b.Navigation("RelationshipTemplate");
                 });
 
             modelBuilder.Entity("Backbone.Modules.Relationships.Domain.Aggregates.Relationships.Relationship", b =>
