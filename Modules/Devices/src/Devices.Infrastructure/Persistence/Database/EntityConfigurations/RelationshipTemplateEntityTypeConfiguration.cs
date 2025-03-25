ebe2cb63075e9be46e8467592209df857ced6d60
Timo Notheisen <timo.notheisen@js-soft.com>
2025-03-25 09:28:39 +0100

    feat: update Devices migration to contain reference to RelationshipTemplate and RelationshipTemplateAllocation

---
+++ Modules/Devices/src/Devices.Infrastructure/Persistence/Database/EntityConfigurations/RelationshipTemplateEntityTypeConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Backbone.BuildingBlocks.Infrastructure.Persistence.Database.EntityTypeConfigurations;
+using Backbone.Modules.Devices.Domain.Aggregates.Relationships;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Backbone.Modules.Devices.Infrastructure.Persistence.Database.EntityConfigurations;
+
+public class RelationshipTemplateEntityTypeConfiguration : EntityEntityTypeConfiguration<RelationshipTemplate>
+{
+    public override void Configure(EntityTypeBuilder<RelationshipTemplate> builder)
+    {
+        base.Configure(builder);
+
+        builder.ToTable("RelationshipTemplates", "Relationships", x => x.ExcludeFromMigrations());
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.CreatedBy);
+
+        builder.HasMany(x => x.Allocations).WithOne(x => x.RelationshipTemplate);
+    }
+}
