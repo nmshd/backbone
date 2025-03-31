98cfbb133692e88e8c915a5084073bdbc55014f9
Timo Notheisen <timo.notheisen@js-soft.com>
2025-03-31 15:30:09 +0200

    fix: don't call AnonymizeMessagesOfIdentityCommand from IdentityDeleter

--- Modules/Messages/src/Messages.Application/DomainEvents/Incoming/RelationshipStatusChanged/RelationshipStatusChangedDomainEventHandler.cs
+++ Modules/Messages/src/Messages.Application/DomainEvents/Incoming/RelationshipStatusChanged/RelationshipStatusChangedDomainEventHandler.cs
@@ -32,6 +32,7 @@ public class RelationshipStatusChangedDomainEventHandler : IDomainEventHandler<R
 
         var anonymizedIdentityAddress = IdentityAddress.GetAnonymized(_applicationConfiguration.DidDomainName);
         var messagesExchangedBetweenRelationshipParticipants = (await _messagesRepository.Find(Message.WasExchangedBetween(@event.Initiator, @event.Peer), CancellationToken.None)).ToList();
+
         foreach (var message in messagesExchangedBetweenRelationshipParticipants)
         {
             message.DecomposeFor(@event.Initiator, @event.Peer, anonymizedIdentityAddress);
