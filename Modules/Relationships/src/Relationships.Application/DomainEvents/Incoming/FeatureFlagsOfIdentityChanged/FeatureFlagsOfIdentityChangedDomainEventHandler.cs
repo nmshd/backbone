e376f3fee6da3e63fff34bcc8ac22befb6b7ee8c
Jakob Erben <jakob.erben@js-soft.com>
2025-04-07 08:11:40 +0200

    feat: send FeatureFlagsOfPeerChanged external event to all active and pending relationships

--- Modules/Relationships/src/Relationships.Application/DomainEvents/Incoming/FeatureFlagsOfIdentityChanged/FeatureFlagsOfIdentityChangedDomainEventHandler.cs
+++ Modules/Relationships/src/Relationships.Application/DomainEvents/Incoming/FeatureFlagsOfIdentityChanged/FeatureFlagsOfIdentityChangedDomainEventHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
 using Backbone.Modules.Relationships.Application.Infrastructure.Persistence.Repository;
+using Backbone.Modules.Relationships.Domain.Aggregates.Relationships;
 using Backbone.Modules.Relationships.Domain.DomainEvents.Incoming;
 using Backbone.Modules.Relationships.Domain.DomainEvents.Outgoing;
 
@@ -8,24 +9,58 @@ namespace Backbone.Modules.Relationships.Application.DomainEvents.Incoming.Featu
 public class FeatureFlagsOfIdentityChangedDomainEventHandler : IDomainEventHandler<FeatureFlagsOfIdentityChangedDomainEvent>
 {
     private readonly IRelationshipTemplatesRepository _relationshipTemplatesRepository;
+    private readonly IRelationshipsRepository _relationshipsRepository;
     private readonly IEventBus _eventBus;
 
-    public FeatureFlagsOfIdentityChangedDomainEventHandler(IRelationshipTemplatesRepository relationshipTemplatesRepository, IEventBus eventBus)
+    public FeatureFlagsOfIdentityChangedDomainEventHandler(IRelationshipTemplatesRepository relationshipTemplatesRepository, IEventBus eventBus, IRelationshipsRepository relationshipsRepository)
     {
         _relationshipTemplatesRepository = relationshipTemplatesRepository;
         _eventBus = eventBus;
+        _relationshipsRepository = relationshipsRepository;
     }
 
     public async Task Handle(FeatureFlagsOfIdentityChangedDomainEvent @event)
     {
-        var allocations = await _relationshipTemplatesRepository.FindRelationshipTemplateAllocations(a => a.RelationshipTemplate.CreatedBy == @event.IdentityAddress, CancellationToken.None);
+        var identitiesToBeNotified = await FindAllIdentitiesToBeNotified(@event);
 
-        var publishEventTasks = allocations.Select(allocation => _eventBus.Publish(new PeerFeatureFlagsChangedDomainEvent
+        var publishEventTasks = identitiesToBeNotified.Select(notifiedAddress => _eventBus.Publish(new PeerFeatureFlagsChangedDomainEvent
         {
             PeerAddress = @event.IdentityAddress,
-            NotifiedIdentityAddress = allocation.AllocatedBy
+            NotifiedIdentityAddress = notifiedAddress
         }));
 
         await Task.WhenAll(publishEventTasks);
+    }
+
+    private async Task<HashSet<string>> FindAllIdentitiesToBeNotified(FeatureFlagsOfIdentityChangedDomainEvent @event)
+    {
+        var identitiesToBeNotified = new HashSet<string>();
+
+        var activeAndPendingRelationships = await
+            _relationshipsRepository.FindRelationships(
+                r => (r.From == @event.IdentityAddress || r.To == @event.IdentityAddress)
+                     && (r.Status == RelationshipStatus.Active || r.Status == RelationshipStatus.Pending),
+                CancellationToken.None);
+
+        foreach (var relationship in activeAndPendingRelationships)
+        {
+            if (relationship.From == @event.IdentityAddress)
+            {
+                identitiesToBeNotified.Add(relationship.To);
+            }
+            else
+            {
+                identitiesToBeNotified.Add(relationship.From);
+            }
+        }
+        
+        var allocations = await _relationshipTemplatesRepository.FindRelationshipTemplateAllocations(a => a.RelationshipTemplate.CreatedBy == @event.IdentityAddress, CancellationToken.None);
+
+        foreach (var relationshipTemplateAllocation in allocations)
+        {
+            identitiesToBeNotified.Add(relationshipTemplateAllocation.AllocatedBy);
+        }
+
+        return identitiesToBeNotified;
     }
 }
