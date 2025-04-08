d0a6f44ec5a954e220a6d2b33ac547137e76c437
Mika Herrmann <mika.herrmann@js-soft.com>
2025-04-08 11:25:14 +0200

    test: Add unit test for created DomainEvent

---
+++ Modules/Relationships/src/Relationships.Domain/Aggregates/RelationshipTemplates/RelationshipTemplate.CollectTests.cs
@@ -0,0 +1,22 @@
+﻿using Backbone.Modules.Relationships.Domain.DomainEvents.Outgoing;
+using Backbone.Modules.Relationships.Domain.TestHelpers;
+
+namespace Backbone.Modules.Relationships.Domain.Aggregates.RelationshipTemplates;
+
+public class RelationshipTemplateCollectTests : AbstractTestsBase
+{
+    [Fact]
+    public void Raises_RelationshipTemplateAllocationsExhaustedDomainEvent_when_allocations_are_maxed_out()
+    {
+        // Arrange
+        var template = TestData.CreateRelationshipTemplate(CreateRandomIdentityAddress(), null, null, 1);
+        template.ClearDomainEvents();
+
+        // Act
+        template.AllocateFor(CreateRandomIdentityAddress(), CreateRandomDeviceId());
+
+        // Assert
+        template.DomainEvents.Should().HaveCount(1);
+        template.Should().HaveASingleDomainEvent<RelationshipTemplateAllocationsExhaustedDomainEvent>();
+    }
+}
