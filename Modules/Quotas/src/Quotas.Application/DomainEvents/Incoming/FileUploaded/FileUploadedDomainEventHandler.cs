c3ad17144023170c3a5bbfcf81d204bf99113529
Timo Notheisen <65653426+tnotheis@users.noreply.github.com>
2025-04-24 11:15:34 +0200

    Event handler for TierQuotaDefinitionDeleted event takes too long (#1121)

--- Modules/Quotas/src/Quotas.Application/DomainEvents/Incoming/FileUploaded/FileUploadedDomainEventHandler.cs
+++ Modules/Quotas/src/Quotas.Application/DomainEvents/Incoming/FileUploaded/FileUploadedDomainEventHandler.cs
@@ -1,5 +1,6 @@
 using Backbone.BuildingBlocks.Application.Abstractions.Infrastructure.EventBus;
 using Backbone.Modules.Quotas.Application.Metrics;
+using Backbone.Modules.Quotas.Domain.Aggregates.Identities;
 using Backbone.Modules.Quotas.Domain.Aggregates.Metrics;
 using Backbone.Modules.Quotas.Domain.DomainEvents.Incoming.FileUploaded;
 
@@ -19,6 +20,6 @@ public class FileUploadedDomainEventHandler : IDomainEventHandler<FileUploadedDo
         var identities = new List<string> { @event.Uploader };
         var metrics = new List<MetricKey> { MetricKey.NUMBER_OF_FILES, MetricKey.USED_FILE_STORAGE_SPACE };
 
-        await _metricStatusesService.RecalculateMetricStatuses(identities, metrics, CancellationToken.None);
+        await _metricStatusesService.RecalculateMetricStatuses(identities, metrics, MetricUpdateType.All, CancellationToken.None);
     }
 }
