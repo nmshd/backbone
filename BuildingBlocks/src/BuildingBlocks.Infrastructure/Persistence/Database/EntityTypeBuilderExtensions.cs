5283a4f1c22fb5bcc15b2b001d3ef35b6a1f00cf
Mika Herrmann <mika.herrmann@js-soft.com>
2025-05-05 12:51:46 +0200

    feat: Add concurrency timestamp handling for both db providers as EntityTypeBuilder extension

---
+++ BuildingBlocks/src/BuildingBlocks.Infrastructure/Persistence/Database/EntityTypeBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Backbone.BuildingBlocks.Infrastructure.Persistence.Database;
+
+public static class EntityTypeBuilderExtensions
+{
+    public static PropertyBuilder<object> HasVersion<TEntity>(this EntityTypeBuilder<TEntity> builder, Expression<Func<TEntity, object>> propertyExpression) where TEntity : class
+    {
+        if (builder.Metadata.Model.IsNpgsql())
+        {
+            return builder.Property(propertyExpression).HasColumnType("xid").HasConversion(
+                v => (uint)v,
+                v => v
+            );
+        }
+        else
+        {
+            return builder.Property(propertyExpression).HasColumnType("rowversion").HasConversion(
+                v => (byte[])v,
+                v => v
+            );
+        }
+    }
+}
