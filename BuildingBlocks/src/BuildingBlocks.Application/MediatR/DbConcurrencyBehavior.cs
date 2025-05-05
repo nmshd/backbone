cb58f8d3b622a4a32820001c242789fa426f4d15
Mika Herrmann <mika.herrmann@js-soft.com>
2025-05-05 12:50:01 +0200

    feat: Add concurrency handling to Tokens module

---
+++ BuildingBlocks/src/BuildingBlocks.Application/MediatR/DbConcurrencyBehavior.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backbone.BuildingBlocks.Application.MediatR;
+
+public class DbConcurrencyBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    private readonly Random _random = new();
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var retryCount = 0;
+        while (true)
+        {
+            try
+            {
+                return await next(cancellationToken);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (retryCount == 10) throw;
+
+                await Task.Delay(TimeSpan.FromMilliseconds(_random.Next(2, 10)), cancellationToken);
+                retryCount++;
+            }
+        }
+    }
+}
