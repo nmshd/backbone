261d83bd7e926ef80d720d6eabaa85c7f745eb46
Timo Notheisen <65653426+tnotheis@users.noreply.github.com>
2025-05-07 10:03:29 +0200

    Announcements for other identities are returned (#1147)

--- Modules/Announcements/src/Announcements.Application/Announcements/Queries/GetAllAnnouncementsInLanguage/GetAllAnnouncementsForActiveIdentityInLanguageQuery.cs
+++ Modules/Announcements/src/Announcements.Application/Announcements/Queries/GetAllAnnouncementsInLanguage/GetAllAnnouncementsForActiveIdentityInLanguageQuery.cs
@@ -2,7 +2,7 @@
 
 namespace Backbone.Modules.Announcements.Application.Announcements.Queries.GetAllAnnouncementsInLanguage;
 
-public class GetAllAnnouncementsInLanguageQuery : IRequest<GetAllAnnouncementsInLanguageResponse>
+public class GetAllAnnouncementsForActiveIdentityInLanguageQuery : IRequest<GetAllAnnouncementsInLanguageResponse>
 {
     public required string Language { get; set; }
 }
