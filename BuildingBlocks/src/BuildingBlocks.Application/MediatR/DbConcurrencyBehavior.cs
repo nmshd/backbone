3d32bf321bafa9c053be0eb395ee299e67cabd9e
Mika Aaron Herrmann <65243124+MH321Productions@users.noreply.github.com>
2025-05-12 11:03:27 +0200

    Consumer Api: Access failed count is not incremented correctly when multiple concurrent requests for the same token are made (#1136)
    * chore: Update SqlServer container to Version 2022
    
    * feat: Add concurrency handling to Tokens module
    
    * feat: Add DbProvider annotation to entity models
    
    * feat: Add concurrency timestamp handling for both db providers as EntityTypeBuilder extension
    
    * feat: Add concurrency timestamp to Token
    
    * chore: Make RNG static
    
    * chore: Simplify concurrency timestamp by using shadow properties
    
    * fix: Change generics to make concurrency behavior work and set the waiting time to 5-10 seconds
    
    * fix: Revert shadow properties and use normal properties again
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ BuildingBlocks/src/BuildingBlocks.Application/MediatR/DbConcurrencyBehavior.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backbone.BuildingBlocks.Application.MediatR;
+
+public class DbConcurrencyBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : notnull
+{
+    private static readonly Random RANDOM = new();
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var retryCount = 0;
+        while (true)
+        {
+            try
+            {
+                return await next(cancellationToken);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (retryCount == 10) throw;
+
+                await Task.Delay(TimeSpan.FromSeconds(RANDOM.Next(5, 10)), cancellationToken);
+                retryCount++;
+            }
+        }
+    }
+}
--- Modules/Tokens/src/Tokens.Infrastructure.Database.SqlServer/Migrations/20250509095310_AddConcurrency.Designer.cs
+++ Modules/Tokens/src/Tokens.Infrastructure.Database.SqlServer/Migrations/20250509095310_AddConcurrency.Designer.cs
@@ -4,6 +4,7 @@ using Backbone.Modules.Tokens.Infrastructure.Persistence.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Backbone.Modules.Tokens.Infrastructure.Database.SqlServer.Migrations
 {
     [DbContext(typeof(TokensDbContext))]
-    partial class TokensDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250509095310_AddConcurrency")]
+    partial class AddConcurrency
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
