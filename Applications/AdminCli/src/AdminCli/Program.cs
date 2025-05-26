65f4ce35c7623c7ee9db604f0ac0a6340729c877
Timo Notheisen <timo.notheisen@js-soft.com>
2025-05-26 12:49:58 +0200

    chore: remove unnecessary project references

--- Applications/AdminCli/src/AdminCli/Program.cs
+++ Applications/AdminCli/src/AdminCli/Program.cs
@@ -10,10 +10,6 @@ using Backbone.Modules.Devices.Domain.Entities.Identities;
 using Backbone.Modules.Devices.Infrastructure.OpenIddict;
 using Backbone.Modules.Devices.Infrastructure.Persistence.Database;
 using Backbone.Modules.Devices.Module;
-using Backbone.Modules.Files.Module;
-using Backbone.Modules.Messages.Module;
-using Backbone.Modules.Relationships.Module;
-using Backbone.Modules.Synchronization.Module;
 using Backbone.Modules.Tokens.Application;
 using Backbone.Modules.Tokens.Module;
 using Microsoft.AspNetCore.Identity;
@@ -75,10 +71,6 @@ public class Program
         services
             .AddModule<AnnouncementsModule, Modules.Announcements.Application.ApplicationConfiguration, Modules.Announcements.Infrastructure.InfrastructureConfiguration>(configuration)
             .AddModule<DevicesModule, Modules.Devices.Application.ApplicationConfiguration, InfrastructureConfiguration>(configuration)
-            .AddModule<FilesModule, Modules.Files.Application.ApplicationConfiguration, Modules.Files.Infrastructure.InfrastructureConfiguration>(configuration)
-            .AddModule<MessagesModule, Modules.Messages.Application.ApplicationConfiguration, Modules.Messages.Infrastructure.InfrastructureConfiguration>(configuration)
-            .AddModule<RelationshipsModule, Modules.Relationships.Application.ApplicationConfiguration, Modules.Relationships.Infrastructure.InfrastructureConfiguration>(configuration)
-            .AddModule<SynchronizationModule, Modules.Synchronization.Application.ApplicationConfiguration, Modules.Synchronization.Infrastructure.InfrastructureConfiguration>(configuration)
             .AddModule<TokensModule, ApplicationConfiguration, Modules.Tokens.Infrastructure.InfrastructureConfiguration>(configuration);
 
 #pragma warning disable ASP0000 // We retrieve the Configuration via IOptions here so that it is validated
