f075918804a128e2fd5d84f402ddd2664f465a97
Timo Notheisen <timo.notheisen@js-soft.com>
2025-06-03 13:04:43 +0200

    feat: add properties to Export classes

--- Applications/AdminCli/src/AdminCli/Commands/Database/Model/FileExport.cs
+++ Applications/AdminCli/src/AdminCli/Commands/Database/Model/FileExport.cs
@@ -4,9 +4,11 @@ public class FileExport
 {
     public required string FileId { get; set; }
     public required string CreatedBy { get; set; }
+    public required string? CreatedByClientId { get; set; }
     public required string? CreatedByClientName { get; set; }
     public required DateTime CreatedAt { get; set; }
     public required string Owner { get; set; }
+    public required string? OwnerClientId { get; set; }
     public required string? OwnerClientName { get; set; }
     public required long CipherSize { get; set; }
     public required DateTime ExpiresAt { get; set; }
--- Applications/AdminCli/src/AdminCli/Commands/Database/Model/MessageExport.cs
+++ Applications/AdminCli/src/AdminCli/Commands/Database/Model/MessageExport.cs
@@ -4,9 +4,11 @@ public class MessageExport
 {
     public required string MessageId { get; set; }
     public required string CreatedBy { get; set; }
+    public required string? CreatedByClientId { get; set; }
     public required string? CreatedByClientName { get; set; }
     public required string RelationshipId { get; set; }
     public required string Recipient { get; set; }
+    public required string? RecipientClientId { get; set; }
     public required string? RecipientClientName { get; set; }
     public required DateTime CreatedAt { get; set; }
     public required DateTime? ReceivedAt { get; set; }
