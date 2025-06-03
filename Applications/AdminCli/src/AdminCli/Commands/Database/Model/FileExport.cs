dd335adf715bda8387f2fb1a8005a1a18af85b9e
Timo Notheisen <65653426+tnotheis@users.noreply.github.com>
2025-06-03 13:16:56 +0200

    Add LastOwnershipClaimAt property to database export (#1193)

--- Applications/AdminCli/src/AdminCli/Commands/Database/Model/FileExport.cs
+++ Applications/AdminCli/src/AdminCli/Commands/Database/Model/FileExport.cs
@@ -6,6 +6,7 @@ public class FileExport
     public required string CreatedBy { get; set; }
     public required string? CreatedByClientName { get; set; }
     public required DateTime CreatedAt { get; set; }
+    public required DateTime? LastOwnershipClaimAt { get; set; }
     public required string Owner { get; set; }
     public required string? OwnerClientName { get; set; }
     public required long CipherSize { get; set; }
