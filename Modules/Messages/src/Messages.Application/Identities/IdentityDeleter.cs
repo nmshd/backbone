1725379bc2267860c9b26e03aeb407f10158e4c0
Timo Notheisen <timo.notheisen@js-soft.com>
2025-06-06 07:27:27 +0200

    chore: add comment

--- Modules/Messages/src/Messages.Application/Identities/IdentityDeleter.cs
+++ Modules/Messages/src/Messages.Application/Identities/IdentityDeleter.cs
@@ -5,6 +5,8 @@ namespace Backbone.Modules.Messages.Application.Identities;
 
 public class IdentityDeleter : IIdentityDeleter
 {
+    // we currently don't have to do anything here, because the messages are anonymized automatically by the RelationshipStatusChangedDomainEventHandler
+    // once the relationship gets decomposed (which happens during the identity deletion)
     public Task Delete(IdentityAddress identityAddress)
     {
         return Task.CompletedTask;
