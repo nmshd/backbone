6cad775ab4046a6548cc36ed5cd173d626442ed6
Mika Herrmann <mika.herrmann@js-soft.com>
2025-06-13 10:00:17 +0200

    feat: Use configuration and cancellation token to send keep alive events

--- Applications/SseServer/src/SseServer/Controllers/EventQueue.cs
+++ Applications/SseServer/src/SseServer/Controllers/EventQueue.cs
@@ -1,24 +1,31 @@
 ﻿using System.Collections.Concurrent;
 using System.Threading.Channels;
+using Microsoft.Extensions.Options;
 
 namespace Backbone.SseServer.Controllers;
 
 public class EventQueue : IEventQueue
 {
+    private const string KEEP_ALIVE_EVENT = "keep-alive";
+
     private readonly ILogger<EventQueue> _logger;
     private readonly ConcurrentDictionary<string, Channel<string>> _channels = new();
+    private readonly Configuration _configuration;
 
-    public EventQueue(ILogger<EventQueue> logger)
+    public EventQueue(ILogger<EventQueue> logger, IOptions<Configuration> options)
     {
         _logger = logger;
+        _configuration = options.Value;
     }
 
-    public void Register(string address)
+    public void Register(string address, CancellationToken cancellationToken)
     {
         var success = _channels.TryAdd(address, Channel.CreateUnbounded<string>());
 
         if (!success)
             throw new ClientAlreadyRegisteredException();
+
+        Task.Run(async () => await SendKeepAliveEvents(address, cancellationToken), cancellationToken);
     }
 
     public void Deregister(string address)
@@ -43,6 +50,17 @@ public class EventQueue : IEventQueue
             throw new Exception($"An sse client for the address {address} is not registered.");
 
         return channel.Reader.ReadAllAsync(cancellationToken);
+    }
+
+    private async Task SendKeepAliveEvents(string address, CancellationToken cancellationToken)
+    {
+        using var timer = new PeriodicTimer(TimeSpan.FromSeconds(_configuration.Sse.KeepAliveEventInterval));
+        while (await timer.WaitForNextTickAsync(cancellationToken))
+        {
+            if (!_channels.ContainsKey(address)) break;
+
+            await EnqueueFor(address, KEEP_ALIVE_EVENT, cancellationToken);
+        }
     }
 }
 
