abc9aea0552025546957b1ee2581d0491b13cb98
Timo Notheisen <timo.notheisen@js-soft.com>
2025-06-25 13:00:23 +0200

    chore: add parameterless constructor to AnnouncementAction

--- Modules/Announcements/src/Announcements.Domain/Entities/AnnouncementAction.cs
+++ Modules/Announcements/src/Announcements.Domain/Entities/AnnouncementAction.cs
@@ -4,6 +4,15 @@ namespace Backbone.Modules.Announcements.Domain.Entities;
 
 public class AnnouncementAction : Entity
 {
+    // ReSharper disable once UnusedMember.Local
+    public AnnouncementAction()
+    {
+        // This constructor is for EF Core only; initializing the properties with null is therefore not a problem
+        Id = null!;
+        DisplayName = null!;
+        Link = null!;
+    }
+
     public AnnouncementAction(Dictionary<AnnouncementLanguage, string> displayName, string link)
     {
         Id = AnnouncementActionId.New();
