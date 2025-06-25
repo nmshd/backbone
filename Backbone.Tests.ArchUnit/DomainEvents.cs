168e8d1e2ca7350987ef28b0402c497b754743ec
Mika Herrmann <mika.herrmann@js-soft.com>
2025-06-25 12:31:24 +0200

    chore: Address change requests

--- Backbone.Tests.ArchUnit/DomainEvents.cs
+++ Backbone.Tests.ArchUnit/DomainEvents.cs
@@ -1,6 +1,8 @@
-﻿using ArchUnitNET.Domain.Extensions;
+﻿using ArchUnitNET.Domain;
+using ArchUnitNET.Domain.Extensions;
 using ArchUnitNET.Fluent.Conditions;
-using ArchUnitNET.xUnit;
+using ArchUnitNET.Fluent.Syntax.Elements.Types;
+using ArchUnitNET.xUnitV3;
 using Backbone.BuildingBlocks.Domain.Events;
 using Backbone.Tooling.Extensions;
 using static ArchUnitNET.Fluent.ArchRuleDefinition;
@@ -9,49 +11,50 @@ namespace Backbone.Backbone.Tests.ArchUnit;
 
 public class DomainEvents
 {
+    private readonly List<IType> _outgoingDomainEvents = Types()
+        .That().AreAssignableTo(typeof(DomainEvent))
+        .And().ResideInNamespace(".Outgoing", true)
+        .GetObjects(Backbone.ARCHITECTURE)
+        .ToList();
+
+    private readonly GivenTypesConjunction _incomingDomainEvents = Types()
+        .That().AreAssignableTo(typeof(DomainEvent))
+        .And().ResideInNamespace(".Incoming.", true);
+
     [Fact]
     public void IncomingDomainEventsShouldNotHaveConstructors()
     {
-        Types()
-            .That().AreAssignableTo(typeof(DomainEvent))
-            .And().ResideInNamespace("Backbone.Modules.*.Domain.DomainEvents.Incoming.*", true)
+        _incomingDomainEvents
             .Should().FollowCustomCondition(type =>
             {
                 if (type.GetConstructors().Any(c => c.Parameters.Any()))
-                    return new ConditionResult(type, false, "should not have constructors");
+                    return new ConditionResult(type, false, "should not have a constructor with parameters");
 
                 return new ConditionResult(type, true);
-            }, "")
-            .Because("otherwise the JSON Serializer would call the constructor instead of serializing directly, which would cause an invalid serialization")
+            }, "not have constructors")
+            .Because(
+                "objects are only created by the JSON Serializer anyway. Adding a constructor with parameters only causes errors during deserialization if the parameter names don't match the property names.")
             .Check(Backbone.ARCHITECTURE);
     }
 
     [Fact]
-    public void OutgoingDomainEventsShouldHaveCorrespondingIncomingDomainEvents()
+    public void IncomingDomainEventsShouldHaveCorrespondingOutgoingDomainEvents()
     {
-        var outgoing = Types()
-            .That().AreAssignableTo(typeof(DomainEvent))
-            .And().ResideInNamespace("Backbone.Modules.*.DomainEvents.Outgoing.*", true)
-            .GetObjects(Backbone.ARCHITECTURE)
-            .ToList();
-
-        Types()
-            .That().AreAssignableTo(typeof(DomainEvent))
-            .And().ResideInNamespace("Backbone.Modules.*.DomainEvents.Incoming.*", true)
+        _incomingDomainEvents
             .Should().FollowCustomCondition(type =>
             {
-                var correspondingTypes = outgoing.Where(t => t.NameMatches(type.Name)).ToList();
+                var correspondingTypes = _outgoingDomainEvents.Where(t => t.NameMatches(type.Name)).ToList();
                 if (correspondingTypes.IsEmpty()) return new ConditionResult(type, false, "does not have a corresponding outgoing domain event");
 
                 foreach (var property in type.GetPropertyMembers())
                 {
                     foreach (var t in correspondingTypes.Where(t => !t.HasPropertyMemberWithName(property.Name)))
-                        return new ConditionResult(type, false, $"does not have a corresponding property for '{property.Name}' in its corresponding domain event ({t.FullName})");
+                        return new ConditionResult(type, false, $"does not have a corresponding property for '{property.Name}' in its corresponding outgoing domain event ({t.FullName})");
                 }
 
                 return new ConditionResult(type, true);
-            }, "")
-            .Because("Reason")
+            }, "have all of their properties found in their corresponding outgoing domain event")
+            .Because("otherwise they would not be serialized correctly from JSON")
             .Check(Backbone.ARCHITECTURE);
     }
 }
