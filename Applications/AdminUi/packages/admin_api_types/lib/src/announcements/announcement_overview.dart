95fcc951d8ebb7ac26c908e8f9ce5936bf5d35d5
Timo Notheisen <65653426+tnotheis@users.noreply.github.com>
2025-07-01 10:59:58 +0200

    Announcement actions (#1206)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: Julian König <julian.koenig@js-soft.com>

--- Applications/AdminUi/packages/admin_api_types/lib/src/announcements/announcement_overview.dart
+++ Applications/AdminUi/packages/admin_api_types/lib/src/announcements/announcement_overview.dart
@@ -1,10 +1,9 @@
 import 'package:json_annotation/json_annotation.dart';
 
+import 'announcement_action.dart';
 import 'announcement_text.dart';
 
-export 'create_announcement_response.dart';
-
-part 'announcements_overview.g.dart';
+part 'announcement_overview.g.dart';
 
 @JsonSerializable()
 class Announcement {
@@ -13,6 +12,7 @@ class Announcement {
   final DateTime? expiresAt;
   final String severity;
   final List<AnnouncementText> texts;
+  final List<AnnouncementAction> actions;
   final String? iqlQuery;
   final bool isSilent;
 
@@ -22,6 +22,7 @@ class Announcement {
     required this.expiresAt,
     required this.severity,
     required this.texts,
+    required this.actions,
     required this.iqlQuery,
     required this.isSilent,
   });
