925798d8011192e8344f8d6e437b221efbb372f7
Timo Notheisen <timo.notheisen@js-soft.com>
2025-07-09 13:50:25 +0200

    refactor: improve IdentityDeletionJobConfiguration

--- Applications/IdentityDeletionJobs/src/Job.IdentityDeletion/IdentityDeletionJobConfiguration.cs
+++ Applications/IdentityDeletionJobs/src/Job.IdentityDeletion/IdentityDeletionJobConfiguration.cs
@@ -6,14 +6,14 @@ namespace Backbone.Job.IdentityDeletion;
 public class IdentityDeletionJobConfiguration
 {
     [Required]
-    public InfrastructureConfiguration Infrastructure { get; set; } = new();
+    public required InfrastructureConfiguration Infrastructure { get; set; }
 
     [Required]
-    public string Worker { get; set; } = null!;
+    public required string Worker { get; set; }
 }
 
 public class InfrastructureConfiguration
 {
     [Required]
-    public EventBusConfiguration EventBus { get; set; } = new();
+    public required EventBusConfiguration EventBus { get; set; }
 }
