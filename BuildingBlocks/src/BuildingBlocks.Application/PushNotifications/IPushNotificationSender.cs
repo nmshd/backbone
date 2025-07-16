e2853b841393003590d4aa58aec3177a1d4a4c7a
Timo Notheisen <timo.notheisen@js-soft.com>
2025-07-16 14:02:04 +0200

    feat: add new relationship properties to DevicesDbContextModelSnapshot

--- BuildingBlocks/src/BuildingBlocks.Application/PushNotifications/IPushNotificationSender.cs
+++ BuildingBlocks/src/BuildingBlocks.Application/PushNotifications/IPushNotificationSender.cs
@@ -4,6 +4,7 @@ public interface IPushNotificationSender
 {
     Task SendNotification(IPushNotification notification, SendPushNotificationFilter filter, CancellationToken cancellationToken);
 
-    Task SendNotification(IPushNotification notification, SendPushNotificationFilter filter, Dictionary<string, NotificationText> notificationTexts,
-        CancellationToken cancellationToken);
+    Task SendNotification(IPushNotification notification, SendPushNotificationFilter filter, Dictionary<string, NotificationText> notificationTexts, CancellationToken cancellationToken);
+
+    Task SendTextOnlyNotification(Dictionary<string, NotificationText> notificationTexts, string notificationId, SendPushNotificationFilter filter, CancellationToken cancellationToken);
 }
