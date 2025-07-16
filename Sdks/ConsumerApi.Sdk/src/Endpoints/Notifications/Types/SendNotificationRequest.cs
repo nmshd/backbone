b353fab998c7926a687deeae3663dbfed2d1bd08
Timo Notheisen <timo.notheisen@js-soft.com>
2025-07-16 14:02:52 +0200

    feat: add endpoint to sdk

---
+++ Sdks/ConsumerApi.Sdk/src/Endpoints/Notifications/Types/SendNotificationRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Backbone.ConsumerApi.Sdk.Endpoints.Notifications.Types;
+
+public class SendNotificationRequest
+{
+    public required string Code { get; init; }
+    public List<string> Recipients { get; init; } = [];
+}
