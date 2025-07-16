18214f71bc0448c22c1723cf6913681fb4bd3bf3
Timo Notheisen <timo.notheisen@js-soft.com>
2025-07-16 14:02:57 +0200

    test: add integration tests

---
+++ Applications/ConsumerApi/test/ConsumerApi.Tests.Integration/Features/Notifications/POST.feature
@@ -0,0 +1,30 @@
+﻿@Integration
+Feature: POST /Notifications
+
+Identity sends a Notification
+
+    Scenario: Sending a Notification
+        Given Identities i1 and i2
+        And an active Relationship r12 between i1 and i2
+        When i1 sends a POST request to the /Notifications endpoint with i2 as recipient
+        Then the response status code is 204 (No Content)
+
+    Scenario: Trying to send a Notification with a non existing code
+        Given Identities i1 and i2
+        And an active Relationship r12 between i1 and i2
+        When i1 sends a POST request to the /Notifications endpoint with i2 as recipient and a non existing code
+        Then the response status code is 400 (Bad Request)
+        And the response content contains an error with the error code "error.platform.validation.notification.codeDoesNotExist"
+
+    Scenario: Trying to send a Notification to the recipient without a relationship
+        Given Identities i1 and i2
+        When i1 sends a POST request to the /Notifications endpoint with i2 as recipient
+        Then the response status code is 400 (Bad Request)
+        And the response content contains an error with the error code "error.platform.validation.notification.noRelationshipToOneOrMoreRecipientsExists"
+
+    Scenario: Trying to send a Notification to the recipient with a rejected relationship
+        Given Identities i1 and i2
+        And a rejected Relationship r between i1 and i2
+        When i1 sends a POST request to the /Notifications endpoint with i2 as recipient
+        Then the response status code is 400 (Bad Request)
+        And the response content contains an error with the error code "error.platform.validation.notification.relationshipToRecipientIsNotInCorrectStatus"
---
+++ Applications/ConsumerApi/test/ConsumerApi.Tests.Integration/StepDefinitions/NotificationsStepDefinitions.cs
@@ -0,0 +1,52 @@
+﻿using Backbone.ConsumerApi.Sdk.Endpoints.Notifications.Types;
+using Backbone.ConsumerApi.Tests.Integration.Contexts;
+using Backbone.ConsumerApi.Tests.Integration.Helpers;
+
+namespace Backbone.ConsumerApi.Tests.Integration.StepDefinitions;
+
+[Binding]
+internal class NotificationsStepDefinitions
+{
+    #region Constructor, Fields, Properties
+
+    private readonly ResponseContext _responseContext;
+    private readonly ClientPool _clientPool;
+
+    public NotificationsStepDefinitions(ResponseContext responseContext, ClientPool clientPool)
+    {
+        _responseContext = responseContext;
+        _clientPool = clientPool;
+    }
+
+    #endregion
+
+    #region When
+
+    [When($"^{RegexFor.SINGLE_THING} sends a POST request to the /Notifications endpoint with {RegexFor.SINGLE_THING} as recipient$")]
+    public async Task WhenIdentitySendsAPostRequestToTheTokensEndpoint(string senderName, string recipientName)
+    {
+        var senderClient = _clientPool.FirstForIdentityName(senderName);
+        var recipientAddress = _clientPool.FirstForIdentityName(recipientName).IdentityData!.Address;
+
+        _responseContext.WhenResponse = await senderClient.Notifications.SendNotification(new SendNotificationRequest
+        {
+            Code = "TestCode",
+            Recipients = [recipientAddress]
+        });
+    }
+
+    #endregion
+
+    [When($"^{RegexFor.SINGLE_THING} sends a POST request to the /Notifications endpoint with {RegexFor.SINGLE_THING} as recipient and a non existing code$")]
+    public async Task WhenISendsAPOSTRequestToTheNotificationsEndpointWithIAsRecipientAndANonExistingCode(string senderName, string recipientName)
+    {
+        var senderClient = _clientPool.FirstForIdentityName(senderName);
+        var recipientAddress = _clientPool.FirstForIdentityName(recipientName).IdentityData!.Address;
+
+        _responseContext.WhenResponse = await senderClient.Notifications.SendNotification(new SendNotificationRequest
+        {
+            Code = "NonExistingCode",
+            Recipients = [recipientAddress]
+        });
+    }
+}
