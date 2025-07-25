f9620fe3f6958c321a2ca580cb59fc6379318d55
Timo Notheisen <timo.notheisen@js-soft.com>
2025-07-25 09:34:31 +0200

    refactor: let RelationshipDTO inherit from RelationshipMetadataDTO

--- Modules/Relationships/src/Relationships.Application/Relationships/DTOs/RelationshipDTO.cs
+++ Modules/Relationships/src/Relationships.Application/Relationships/DTOs/RelationshipDTO.cs
@@ -2,33 +2,14 @@ using Backbone.Modules.Relationships.Domain.Aggregates.Relationships;
 
 namespace Backbone.Modules.Relationships.Application.Relationships.DTOs;
 
-public class RelationshipDTO
+public class RelationshipDTO : RelationshipMetadataDTO
 {
-    public RelationshipDTO(Relationship relationship)
+    public RelationshipDTO(Relationship relationship) : base(relationship)
     {
-        Id = relationship.Id;
-        RelationshipTemplateId = relationship.RelationshipTemplateId?.Value;
-        From = relationship.From;
-        To = relationship.To;
-        CreatedAt = relationship.CreatedAt;
-        Status = relationship.Status;
         CreationContent = relationship.Details.CreationContent;
         CreationResponseContent = relationship.Details.CreationResponseContent;
-        AuditLog = relationship.AuditLog.Select(a => new RelationshipAuditLogEntryDTO(a)).ToList();
     }
-
-    public string Id { get; set; }
-    public string? RelationshipTemplateId { get; set; }
-
-    public string From { get; set; }
-    public string To { get; set; }
 
     public byte[]? CreationContent { get; set; }
     public byte[]? CreationResponseContent { get; set; }
-
-    public DateTime CreatedAt { get; set; }
-
-    public RelationshipStatus Status { get; set; }
-
-    public List<RelationshipAuditLogEntryDTO> AuditLog { get; set; }
 }
