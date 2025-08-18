c80fe3ffe89e362b937cb3710f883488703b7d8c
Julian König <julian.koenig@js-soft.com>
2025-08-18 10:54:07 +0200

    chore: reformat

--- Applications/AdminUi/packages/admin_api_types/lib/src/announcements/create_announcement_response.g.dart
+++ Applications/AdminUi/packages/admin_api_types/lib/src/announcements/create_announcement_response.g.dart
@@ -6,14 +6,18 @@ part of 'create_announcement_response.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-CreateAnnouncementResponse _$CreateAnnouncementResponseFromJson(Map<String, dynamic> json) => CreateAnnouncementResponse(
+CreateAnnouncementResponse _$CreateAnnouncementResponseFromJson(
+  Map<String, dynamic> json,
+) => CreateAnnouncementResponse(
   severity: json['severity'] as String,
   texts: (json['texts'] as List<dynamic>).map(AnnouncementText.fromJson).toList(),
   recipients: (json['recipients'] as List<dynamic>?)?.map((e) => e as String).toList(),
   expiresAt: json['expiresAt'] == null ? null : DateTime.parse(json['expiresAt'] as String),
 );
 
-Map<String, dynamic> _$CreateAnnouncementResponseToJson(CreateAnnouncementResponse instance) => <String, dynamic>{
+Map<String, dynamic> _$CreateAnnouncementResponseToJson(
+  CreateAnnouncementResponse instance,
+) => <String, dynamic>{
   'expiresAt': instance.expiresAt?.toIso8601String(),
   'severity': instance.severity,
   'texts': instance.texts,
