6dfd991d0d347a027d42ca78d40b69fd6a0b105d
Timo Notheisen <timo.notheisen@js-soft.com>
2025-08-25 14:16:19 +0200

    fix: don't calculate usage if quota is set to 0

--- Modules/Quotas/src/Quotas.Domain/Aggregates/Identities/Identity.cs
+++ Modules/Quotas/src/Quotas.Domain/Aggregates/Identities/Identity.cs
@@ -124,11 +124,15 @@ public class Identity : Entity
 
         await Parallel.ForEachAsync(quotasForMetric, cancellationToken, async (quota, _) =>
         {
-            var newUsage = await metricCalculator.CalculateUsage(
-                quota.Period.CalculateBegin(utcNow),
-                quota.Period.CalculateEnd(utcNow),
-                Address,
-                cancellationToken);
+            // if the quota allows 0, we don't need to calculate the usage, it's always exhausted
+            // this is primarily an optimization for when an identity moves to the QueuedForDeletion tier, where all quotas are set to 0
+            var newUsage = quota.Max > 0
+                ? 1
+                : await metricCalculator.CalculateUsage(
+                    quota.Period.CalculateBegin(utcNow),
+                    quota.Period.CalculateEnd(utcNow),
+                    Address,
+                    cancellationToken);
 
             var quotaExhaustion = quota.CalculateExhaustion(newUsage, utcNow);
 
