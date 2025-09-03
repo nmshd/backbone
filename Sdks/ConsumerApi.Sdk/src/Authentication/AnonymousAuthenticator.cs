62feb3d8a527fa4a16665573d0a92c8465fb0849
Timo Notheisen <timo.notheisen@js-soft.com>
2025-09-03 09:51:03 +0200

    chore: fix typo in an error message

--- Sdks/ConsumerApi.Sdk/src/Authentication/AnonymousAuthenticator.cs
+++ Sdks/ConsumerApi.Sdk/src/Authentication/AnonymousAuthenticator.cs
@@ -6,6 +6,6 @@ public class AnonymousAuthenticator : IAuthenticator
 {
     public Task Authenticate(HttpRequestMessage request)
     {
-        throw new Exception("In order to use an authenticated request, you have to provide an user credentials.");
+        throw new Exception("In order to use an authenticated request, you have to provide user credentials.");
     }
 }
