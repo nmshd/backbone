c5ad7aae44e1339a77107d9b62a954c8a0ab6795
Mika Herrmann <mika.herrmann@js-soft.com>
2025-09-09 14:07:24 +0200

    feat: Register deletion verifier, db exporter and sql extractor for dependency injection

--- Applications/IdentityDeletionJobs/src/Job.IdentityDeletion/ServicesExtensions.cs
+++ Applications/IdentityDeletionJobs/src/Job.IdentityDeletion/ServicesExtensions.cs
@@ -1,4 +1,8 @@
 ﻿using Backbone.BuildingBlocks.Application.Identities;
+using Backbone.BuildingBlocks.Infrastructure.Persistence.Database;
+using Backbone.Job.IdentityDeletion.IdentityDeletionVerifier.Exporters;
+using Backbone.Job.IdentityDeletion.IdentityDeletionVerifier.Extractors;
+using PostgresSqlExtractor = Backbone.Job.IdentityDeletion.IdentityDeletionVerifier.Extractors.PostgresSqlExtractor;
 
 namespace Backbone.Job.IdentityDeletion;
 
@@ -15,6 +19,27 @@ public static class ServicesExtensions
         services.AddTransient<IIdentityDeleter, Modules.Relationships.Application.Identities.IdentityDeleter>();
         services.AddTransient<IIdentityDeleter, Modules.Synchronization.Application.Identities.IdentityDeleter>();
         services.AddTransient<IIdentityDeleter, Modules.Tokens.Application.Identities.IdentityDeleter>();
+
+        return services;
+    }
+
+    public static IServiceCollection RegisterDbExporterAndExtractor(this IServiceCollection services, IdentityDeletionJobConfiguration parsedConfiguration)
+    {
+        switch (parsedConfiguration.Infrastructure.SqlDatabase.Provider)
+        {
+            case DatabaseConfiguration.POSTGRES:
+                services.AddTransient<IDbExporter, PostgresDbExporter>();
+                services.AddTransient<ISqlExtractor, PostgresSqlExtractor>();
+                break;
+
+            case DatabaseConfiguration.SQLSERVER:
+                services.AddTransient<IDbExporter, SqlServerDbExporter>();
+                services.AddTransient<ISqlExtractor, SqlserverSqlExtractor>();
+                break;
+
+            default:
+                throw new ArgumentOutOfRangeException($"No database provider registered for \"{parsedConfiguration.Infrastructure.SqlDatabase.Provider}\"");
+        }
 
         return services;
     }
