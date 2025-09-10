9f3f95aab42062a8ae54b8e79d4c2968a6b6c799
Timo Notheisen <timo.notheisen@js-soft.com>
2025-09-10 14:04:29 +0200

    fix: some errors

--- Modules/Synchronization/src/Synchronization.Infrastructure/Persistence/Database/Configurations/SyncErrorEntityTypeConfiguration.cs
+++ Modules/Synchronization/src/Synchronization.Infrastructure/Persistence/Database/Configurations/SyncErrorEntityTypeConfiguration.cs
@@ -1,6 +1,5 @@
 using Backbone.BuildingBlocks.Infrastructure.Persistence.Database.EntityTypeConfigurations;
 using Backbone.Modules.Synchronization.Domain.Entities.Sync;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Backbone.Modules.Synchronization.Infrastructure.Persistence.Database.Configurations;
@@ -15,7 +14,5 @@ public class SyncErrorEntityTypeConfiguration : EntityEntityTypeConfiguration<Sy
 
         builder.Property(x => x.ErrorCode).HasMaxLength(100);
         builder.Property(x => x.CreatedAt);
-
-        builder.HasOne<ExternalEvent>().WithMany().HasForeignKey(x => x.ExternalEventId).OnDelete(DeleteBehavior.Cascade);
     }
 }
