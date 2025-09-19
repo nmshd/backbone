fbfccc66f3adbb0e0cd6b39becf54ef276245f4e
Timo Notheisen <timo.notheisen@js-soft.com>
2025-09-19 12:33:40 +0200

    feat: remove possibility to cancel identity deletion process from Admin UI (the corresponding API doesn't exist anymore anyway)

--- Applications/AdminUi/packages/admin_api_sdk/lib/src/endpoints/identities_endpoint.dart
+++ Applications/AdminUi/packages/admin_api_sdk/lib/src/endpoints/identities_endpoint.dart
@@ -44,9 +44,6 @@ class IdentitiesEndpoint extends Endpoint {
   Future<ApiResponse<IdentityDeletionProcessDetail>> getIdentityDeletionProcess({required String address, required String deletionProcessId}) =>
       get('/api/v1/Identities/$address/DeletionProcesses/$deletionProcessId', transformer: IdentityDeletionProcessDetail.fromJson);
 
-  Future<ApiResponse<void>> cancelDeletionProcess({required String address, required String deletionProcessId}) =>
-      put('/api/v1/Identities/$address/DeletionProcesses/$deletionProcessId/Cancel', transformer: (e) {});
-
   Future<ApiResponse<List<IdentityDeletionProcessAuditLogEntry>>> getIdentityDeletionProcessAuditLogs({required String address}) => get(
     '/api/v1/Identities/$address/DeletionProcesses/AuditLogs',
     transformer: (e) => (e as List).map(IdentityDeletionProcessAuditLogEntry.fromJson).toList(),
