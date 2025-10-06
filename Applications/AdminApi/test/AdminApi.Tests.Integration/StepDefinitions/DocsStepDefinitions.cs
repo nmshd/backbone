b82e18b9c50674c965333dc3b7f1c56546f6d1b9
Timo Notheisen <timo.notheisen@js-soft.com>
2025-10-06 12:46:44 +0200

    test: add Admin API integration tests

---
+++ Applications/AdminApi/test/AdminApi.Tests.Integration/StepDefinitions/DocsStepDefinitions.cs
@@ -0,0 +1,40 @@
+﻿using Backbone.AdminApi.Sdk;
+using Backbone.AdminApi.Tests.Integration.Configuration;
+using Backbone.BuildingBlocks.SDK.Endpoints.Common.Types;
+using Microsoft.Extensions.Options;
+
+namespace Backbone.AdminApi.Tests.Integration.StepDefinitions;
+
+[Binding]
+[Scope(Feature = "GET /docs/index.html")]
+[Scope(Feature = "GET /docs/v1/openapi.json")]
+internal class DocsStepDefinitions
+{
+    private readonly Client _client;
+    private ApiResponse<string>? _whenResponse;
+
+    public DocsStepDefinitions(HttpClientFactory factory, IOptions<HttpClientOptions> options)
+    {
+        _client = Client.Create(factory.CreateClient(), options.Value.ApiKey);
+    }
+
+    [When("^a GET request is sent to the /docs/index.html endpoint$")]
+    public async Task WhenAGETRequestIsSentToTheDocsIndexHtmlEndpoint()
+    {
+        _whenResponse = await _client.Docs.GetSwaggerUi();
+    }
+
+    [When("^a GET request is sent to the /docs/v1/openapi.json endpoint$")]
+    public async Task WhenAGETRequestIsSentToTheDocsV1OpenApiJsonEndpoint()
+    {
+        _whenResponse = await _client.Docs.GetOpenApiSpecV1();
+    }
+
+    [Then(@"the response status code is (\d+) \(.+\)")]
+    public void ThenTheResponseStatusCodeIs(int expectedStatusCode)
+    {
+        _whenResponse.ShouldNotBeNull();
+
+        ((int)_whenResponse!.Status).ShouldBe(expectedStatusCode);
+    }
+}
---
+++ Sdks/AdminApi.Sdk/src/Endpoints/Docs/DocsEndpoint.cs
@@ -0,0 +1,38 @@
+﻿using Backbone.BuildingBlocks.SDK.Endpoints.Common.Types;
+
+namespace Backbone.AdminApi.Sdk.Endpoints.Docs;
+
+public class DocsEndpoint
+{
+    private readonly HttpClient _httpClient;
+
+    public DocsEndpoint(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<ApiResponse<string>> GetSwaggerUi()
+    {
+        return await GetStringContent("/docs/index.html");
+    }
+
+    public async Task<ApiResponse<string>> GetOpenApiSpecV1()
+    {
+        return await GetStringContent("/docs/v1/openapi.json");
+    }
+
+    private async Task<ApiResponse<string>> GetStringContent(string path)
+    {
+        var response = await _httpClient.GetAsync(path);
+
+        var responseContent = await response.Content.ReadAsStringAsync();
+
+        return new ApiResponse<string>
+        {
+            ContentType = response.Content.Headers.ContentType?.ToString(),
+            Result = responseContent,
+            Status = response.StatusCode,
+            RawContent = responseContent
+        };
+    }
+}
