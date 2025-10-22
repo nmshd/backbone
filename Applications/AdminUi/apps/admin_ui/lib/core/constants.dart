15b94644f8254acfb5273f92a2227a788a8b2476
Julian König <julian.koenig@js-soft.com>
2025-10-22 09:19:16 +0200

    chore: add gaps class

--- Applications/AdminUi/apps/admin_ui/lib/core/constants.dart
+++ Applications/AdminUi/apps/admin_ui/lib/core/constants.dart
@@ -1,7 +1,31 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
 
 final bool kIsDesktop =
     !kIsWeb &&
     (defaultTargetPlatform == TargetPlatform.linux ||
         defaultTargetPlatform == TargetPlatform.macOS ||
         defaultTargetPlatform == TargetPlatform.windows);
+
+class Gaps {
+  Gaps._();
+
+  static const SizedBox h4 = SizedBox(height: 4);
+  static const SizedBox h8 = SizedBox(height: 8);
+  static const SizedBox h12 = SizedBox(height: 12);
+  static const SizedBox h16 = SizedBox(height: 16);
+  static const SizedBox h20 = SizedBox(height: 20);
+  static const SizedBox h24 = SizedBox(height: 24);
+  static const SizedBox h32 = SizedBox(height: 32);
+  static const SizedBox h36 = SizedBox(height: 36);
+  static const SizedBox h40 = SizedBox(height: 40);
+  static const SizedBox h44 = SizedBox(height: 44);
+  static const SizedBox h48 = SizedBox(height: 48);
+
+  static const SizedBox w4 = SizedBox(width: 4);
+  static const SizedBox w8 = SizedBox(width: 8);
+  static const SizedBox w16 = SizedBox(width: 16);
+  static const SizedBox w24 = SizedBox(width: 24);
+  static const SizedBox w32 = SizedBox(width: 32);
+  static const SizedBox w40 = SizedBox(width: 40);
+}
