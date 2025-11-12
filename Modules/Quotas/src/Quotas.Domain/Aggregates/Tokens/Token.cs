2d3569a9f50507665b00c0cce8af38a97626ce44
Timo Notheisen <timo.notheisen@js-soft.com>
2025-11-12 08:17:20 +0100

    fix: make Token.CreatedBy nullable in Quotas module

--- Modules/Quotas/src/Quotas.Domain/Aggregates/Tokens/Token.cs
+++ Modules/Quotas/src/Quotas.Domain/Aggregates/Tokens/Token.cs
@@ -1,6 +1,7 @@
 namespace Backbone.Modules.Quotas.Domain.Aggregates.Tokens;
+
 public class Token : ICreatedAt
 {
-    public required string CreatedBy { get; set; }
+    public required string? CreatedBy { get; set; }
     public required DateTime CreatedAt { get; set; }
 }
