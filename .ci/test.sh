408721ab7e0d6eed175fb820518abd336a49e76a
Timo Notheisen <timo.notheisen@js-soft.com>
2025-12-08 07:07:48 +0100

    ci: don't set GitHubActionsTestLogger explicitly

--- .ci/test.sh
+++ .ci/test.sh
@@ -2,4 +2,4 @@
 
 dotnet restore /p:ContinuousIntegrationBuild=true "Backbone.slnx"
 dotnet build /p:ContinuousIntegrationBuild=true --no-restore "Backbone.slnx"
-dotnet test /p:ContinuousIntegrationBuild=true --no-restore --no-build --filter "Category!=Integration" --logger "GitHubActions;summary.includeNotFoundTests=false" "Backbone.slnx"
+dotnet test /p:ContinuousIntegrationBuild=true --no-restore --no-build --filter "Category!=Integration" "Backbone.slnx"
