f10d37da37c5b1af3888f2a23166da78eacacb74
Timo Notheisen <65653426+tnotheis@users.noreply.github.com>
2025-12-09 15:35:15 +0100

    Error in eventhandler when handling a BackupDeviceUsed event (#1376)

--- Modules/Devices/src/Devices.Domain/DomainEvents/Outgoing/BackupDeviceUsedDomainEvent.cs
+++ Modules/Devices/src/Devices.Domain/DomainEvents/Outgoing/BackupDeviceUsedDomainEvent.cs
@@ -5,6 +5,12 @@ namespace Backbone.Modules.Devices.Domain.DomainEvents.Outgoing;
 
 public class BackupDeviceUsedDomainEvent : DomainEvent
 {
+    [Obsolete("For deserialization only", true)]
+    public BackupDeviceUsedDomainEvent()
+    {
+        IdentityAddress = null!;
+    }
+
     public BackupDeviceUsedDomainEvent(IdentityAddress identityAddress)
     {
         IdentityAddress = identityAddress;
