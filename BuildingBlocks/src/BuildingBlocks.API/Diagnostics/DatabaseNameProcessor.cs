efd63b2d1c692248cb8c269ff2a849e84d856f92
Timo Hügel <timo.huegel@js-soft.com>
2026-08-26 15:18:34 +0200

    chore: change database service name in otel from postgres to database

--- BuildingBlocks/src/BuildingBlocks.API/Diagnostics/DatabaseNameProcessor.cs
+++ BuildingBlocks/src/BuildingBlocks.API/Diagnostics/DatabaseNameProcessor.cs
@@ -8,6 +8,6 @@ public sealed class DatabaseNameProcessor : BaseProcessor<Activity>
     public override void OnStart(Activity activity)
     {
         if (activity.Source.Name.Contains("EntityFrameworkCore"))
-            activity.SetTag("peer.service", "postgres");
+            activity.SetTag("peer.service", "database");
     }
 }
